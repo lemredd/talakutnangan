2834f2fb190d14121f9c457a3942bdfeed82019d
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-05-19 20:31:12 +0800

    chore(configuration): add plugin to read paths from configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import vue from "@vitejs/plugin-vue"
 import ssr from "vite-plugin-ssr/plugin"
+import tsconfig from "vite-tsconfig-paths"
 import { UserConfig } from "vite"
 
 const config: UserConfig = {
-	plugins: [vue(), ssr()],
+	plugins: [tsconfig(), vue(), ssr()],
 }
 
 export default config
