c157185e06fcbfc3c6ebf29b62d9692460398f2c
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-05-19 21:13:10 +0800

    chore: remove unused files from previous set ups

--- plugins/client.web_socket.ts
+++
@@ -1,9 +0,0 @@
-import client from "socket.io-client"
-
-export default defineNuxtPlugin(nuxtApp => {
-	return {
-		provide: {
-			webSocketClient: (namespace = "/") => client(namespace)
-		}
-	}
-})
