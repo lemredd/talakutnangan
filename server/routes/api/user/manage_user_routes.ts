eca108ced0e299cd58010ecded00f0c105377087
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-05-20 19:51:35 +0800

    fix: correct the path to log in route

--- server/routes/api/user/manage_user_routes.ts
+++ server/routes/api/user/manage_user_routes.ts
@@ -1,7 +1,7 @@
 import { EntityManager } from "typeorm"
 import { Router as createRouter } from "express"
 import makeGetCreateRoute from "!/routes/api/user/create.get"
-import makePostLoginRoute from "!/routes/api/user/login.post"
+import makePostLoginRoute from "!/routes/api/user/log_in.post"
 
 export default function(manager: EntityManager) {
 	const router = createRouter()
