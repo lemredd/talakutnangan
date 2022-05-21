4235f61bdcc015a9d235699d5a98c5e8aed13cc8
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-05-21 21:24:16 +0800

    types: remove unused types

--- server/types.ts
+++ server/types.ts
@@ -2,10 +2,3 @@
  * Type of databases this application can handle
  */
 export type SourceType = "pgsql" | "mysql" | "memoried_sqlite" | "filed_sqlite" | "test"
-
-export interface Request {}
-
-export interface Response {
-	statusCode: number
-	end: () => void
-}
