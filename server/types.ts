0fb023b32b396055760ea5366f56f0985452f866
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-05-21 20:53:33 +0800

    test: create types for mocking requests and responses

--- server/types.ts
+++ server/types.ts
@@ -2,3 +2,10 @@
  * Type of databases this application can handle
  */
 export type SourceType = "pgsql" | "mysql" | "memoried_sqlite" | "filed_sqlite" | "test"
+
+export interface Request {}
+
+export interface Response {
+	statusCode: number
+	end: () => void
+}
