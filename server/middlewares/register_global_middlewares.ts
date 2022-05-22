89c115e9138e950bb1b55c34649ff9b10d246646
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-05-22 12:32:15 +0800

    feat(session): register session middleware with initial configuration

--- server/middlewares/register_global_middlewares.ts
+++ server/middlewares/register_global_middlewares.ts
@@ -1,9 +1,11 @@
 import createCompression from "./create_compression"
+import createSession from "./create_session"
 import type { Express as ExpressApp } from "express"
 
 export default async function(app: ExpressApp) {
 	const middlewares = [
-		createCompression()
+		createCompression(),
+		createSession()
 	]
 
 	middlewares.forEach(middleware => {
