f71de032c71e52da4e5a8485c1ed9a9817eeeffe
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-05-22 22:49:23 +0800

    feat(components): create initial textual field

---
+++ components/fields/Textual.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="flex flex-row flex-nowrap">
+		<label class="flex-1 flex-grow flex-shrink-0">
+			{{ label }}
+		</label>
+		<input
+			:type="type"
+			:value="modelValue"
+			@input="emitUpdate"
+			:required="required"
+			/>
+	</div>
+</template>
+
+<script setup lang="ts">
+import type { Textual } from "@/fields/types"
+
+const { label, type, modelValue, required = true } = defineProps<{
+	label: string,
+	type: Textual,
+	modelValue: string,
+	required: boolean
+}>()
+
+const emit = defineEmits<{
+	(e: "update:modelValue", modelValue: string ): void
+}>()
+
+function emitUpdate(event: Event) {
+	emit("update:modelValue", (event.target as HTMLInputElement).value)
+}
+</script>
