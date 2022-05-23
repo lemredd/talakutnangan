9f14ec66c4d993bd574fd4dd4337f87bce965f88
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-05-23 23:51:46 +0800

    refactor(integration): refactor the post log in route

--- t/integration/api/user/log_in.spec.ts
+++ t/integration/api/user/log_in.spec.ts
@@ -1,21 +1,13 @@
-import request from "supertest"
 import { StatusCodes } from "http-status-codes"
 
-import User from "!/models/user"
-import createAppHandler from "!/app/create_handler"
-
-import Database from "~/database"
+import App from "~/app"
 import UserFactory from "~/factories/user"
 
-describe("Integration: POST /api/user/log_in", async () => {
-	let app
-
+describe("POST /api/user/log_in", () => {
 	it("can be accessed by guest and request with existing credentials", async () => {
-		const manager = Database.manager
-		app = await createAppHandler(manager)
 		const user = await (new UserFactory()).insertOne()
 
-		const response = await request(app)
+		const response = await App.request
 			.post("/api/user/log_in")
 			.send({
 				email: user.email,
@@ -26,7 +18,7 @@ describe("Integration: POST /api/user/log_in", async () => {
 	})
 
 	it("can be accessed by guest and request with non-existing credentials", async () => {
-		const response = await request(app)
+		const response = await App.request
 			.post("/api/user/log_in")
 			.send({
 				email: "sample@gmail.com",
