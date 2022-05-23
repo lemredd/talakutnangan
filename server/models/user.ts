3836ae75159f5e142d25bc0c1e9b8a0c429e8256
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-05-23 16:55:40 +0800

    feat(authentication): add other columns necessary for registration

--- server/models/user.ts
+++ server/models/user.ts
@@ -2,14 +2,24 @@ import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
 
 @Entity()
 export default class User {
-    @PrimaryGeneratedColumn()
-    id: number
+	@PrimaryGeneratedColumn()
+	id: number
 
-    @Column({
-		 unique: true
-	 })
-    email: string
+	@Column({
+		unique: true
+	})
+	email: string
 
-    @Column()
-    password: string
+	@Column()
+	password: string
+
+	@Column({
+		nullable: true
+	})
+	admitted_at: Date|null
+
+	@Column({
+		nullable: true
+	})
+	email_verified_at: Date|null
 }
