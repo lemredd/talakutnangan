e1f3aad7faa0eb8faadeebc41e9e01798ad5760e
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-05-23 14:33:12 +0800

    feat(authentication): integrate guest guard in user routes

--- server/routes/api/user/manage_user_routes.ts
+++ server/routes/api/user/manage_user_routes.ts
@@ -3,12 +3,14 @@ import { Router as createRouter } from "express"
 import makeGetCreateRoute from "!/routes/api/user/create.get"
 import makePostLoginRoute from "!/routes/api/user/log_in.post"
 import createJSONBodyParser from "!/middlewares/create_json_body_parser"
+import createGuestGuard from "!/middlewares/create_guest_guard"
 
 export default function(manager: EntityManager) {
 	const router = createRouter()
 
 	const prefix = "/user"
 	router.get(`${prefix}/create`, makeGetCreateRoute(manager));
+	router.use(createGuestGuard())
 	router.use(createJSONBodyParser())
 	router.post(
 		`${prefix}/log_in`,
