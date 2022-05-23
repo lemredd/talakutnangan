6695e337d3b9782b58760274394842e07363520c
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-05-23 14:38:25 +0800

    types: enhance the documentation of types in server

--- server/types.ts
+++ server/types.ts
@@ -5,16 +5,25 @@ import User from "!/models/user"
  */
 export type SourceType = "pgsql" | "mysql" | "memoried_sqlite" | "filed_sqlite" | "test"
 
+/**
+ * Type to combine with `Request` type to get the session
+ */
 export interface WithSession {
 	session: {
 		token: string|null
 	}
 }
 
+/**
+ * Type to combine with `Request` type to get the possible user
+ */
 export interface WithPossibleUser extends WithSession {
 	user: User|null
 }
 
+/**
+ * Possible kinds of user that the system can handle
+ */
 export enum UserKind {
 	UnreachableEmployee,
 	ReachableEmployee,
