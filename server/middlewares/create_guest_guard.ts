df4048a64147cef72d539b0db7d162bc208ef290
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-05-25 12:10:40 +0800

    feat(authentication): change the code to guard against authenticated

--- server/middlewares/create_guest_guard.ts
+++ server/middlewares/create_guest_guard.ts
@@ -9,14 +9,17 @@ import type { WithPossibleUser, UserKind } from "!/types"
  */
 export default function(): RequestHandler {
 	return (request: Request & WithPossibleUser, response: Response, next: NextFunction) => {
-		if (request.user === undefined) {
-			return next()
+		if (request.isAuthenticated()) {
+			console.log("already logged in", request.user)
+			response.status(StatusCodes.UNAUTHORIZED)
+
+			return response.json({
+				errors: [
+					"You are not allowed to go to that page."
+				]
+			})
 		}
 
-		return response.status(StatusCodes.UNAUTHORIZED).json({
-			errors: [
-				"You are not allowed to go to that page."
-			]
-		})
+		return next()
 	}
 }
