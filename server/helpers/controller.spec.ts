327e6afcebbaed8f050c7c713e8a729005b9d13c
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-05-26 00:15:06 +0800

    test(unit): test the generation of handlers on base controller

--- server/helpers/controller.spec.ts
+++ server/helpers/controller.spec.ts
@@ -1,6 +1,8 @@
+import { Request, Response } from "express"
+import { getMockReq as makeRequest, getMockRes as makeResponse } from "@jest-mock/express"
+
 import Database from "~/database"
 import Middleware from "!/helpers/middleware"
-import { Request, Response } from "express"
 
 import Controller from "./controller"
 
@@ -37,5 +39,65 @@ describe("Back-end: Base Controller", () => {
 		const { URL, handlers } = (new ControllerC(targetURL, false)).generateRoute("/a/b")
 
 		expect(URL).toBe(`/a/b/${targetURL}`)
+	})
+
+	it("can prepend middleware", () => {
+		const middlewareFunction = jest.fn()
+
+		class MiddlewareA extends Middleware {
+			private intermediate(request: Request, response: Response, next: NextFunction): void {
+				middlewareFunction()
+			}
+		}
+
+		class ControllerD extends Controller {
+			constructor(URL: string, overridesPrefix: boolean = false) {
+				super(URL, overridesPrefix)
+
+				this.prependMiddleware(new MiddlewareA())
+			}
+
+			private handle(request: Request, response: Response): void {}
+		}
+		const targetURL = "/"
+
+		const { URL, handlers } = (new ControllerD(targetURL)).generateRoute()
+
+		expect(handlers).toHaveLength(2)
+
+		const request  = makeRequest()
+		const { res: response, next, } = makeResponse()
+		handlers[0](request, response, next)
+		expect(middlewareFunction).toHaveBeenCalled()
+	})
+
+	it("can append middleware", () => {
+		const middlewareFunction = jest.fn()
+
+		class MiddlewareB extends Middleware {
+			private intermediate(request: Request, response: Response, next: NextFunction): void {
+				middlewareFunction()
+			}
+		}
+
+		class ControllerE extends Controller {
+			constructor(URL: string, overridesPrefix: boolean = false) {
+				super(URL, overridesPrefix)
+
+				this.appendMiddleware(new MiddlewareB())
+			}
+
+			private handle(request: Request, response: Response): void {}
+		}
+		const targetURL = "/"
+
+		const { URL, handlers } = (new ControllerE(targetURL)).generateRoute()
+
+		expect(handlers).toHaveLength(2)
+
+		const request  = makeRequest()
+		const { res: response, next, } = makeResponse()
+		handlers[1](request, response, next)
+		expect(middlewareFunction).toHaveBeenCalled()
 	})
 })
