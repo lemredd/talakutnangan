4047f002494e5b33d9ec321a8b79b1b40d1f2a3b
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-05-25 12:48:18 +0800

    test(integration): test register route

---
+++ t/integration/api/user/register.spec.ts
@@ -0,0 +1,53 @@
+import { StatusCodes } from "http-status-codes"
+import User from "!/models/user"
+
+import App from "~/app"
+import Database from "~/database"
+import UserFactory from "~/factories/user"
+
+describe("POST /api/user/register", () => {
+	it("can be accessed by guest and request with non-existing credentials", async () => {
+		const manager = Database.manager
+		const user = await (new UserFactory()).makeOne()
+
+		const response = await App.request
+			.post("/api/user/register")
+			.send({
+				email: user.email,
+				password: user.password
+			})
+
+		expect(response.statusCode).toBe(StatusCodes.OK)
+		expect(response.body).toHaveProperty("token")
+
+		const foundUser = await manager.findOneBy(User, {
+			email: user.email
+		})
+		expect(foundUser.email).toEqual(user.email)
+	})
+
+	it.todo("can insert already existing email")
+
+	it.todo("can insert invalid credentials")
+
+	it("cannot be accessed by authenticated users", async () => {
+		const user = await (new UserFactory()).insertOne()
+
+		const preresponse = await App.request
+			.post("/api/user/log_in")
+			.send({
+				email: user.email,
+				password: user.password
+			})
+		const response = await App.request
+			.post("/api/user/register")
+			.set("Cookie", preresponse.headers["set-cookie"])
+			.send({
+				email: user.email,
+				password: user.password
+			})
+
+		expect(response.statusCode).toBe(StatusCodes.UNAUTHORIZED)
+		expect(response.body).toHaveProperty("errors")
+	})
+})
