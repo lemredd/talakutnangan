ae999027969fa6b9868db431713ed7ba5caa6bdd
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-05-25 21:22:14 +0800

    test: add method to destroy the request environment for testing

---
+++ server/helpers/request_environment.ts
@@ -0,0 +1,35 @@
+import { EntityManager } from "typeorm"
+
+import { Environment } from "!/types"
+import getEnvironment from "!/helpers/get_environment"
+
+/**
+ * Contains the environment that can be used by the request handlers.
+ */
+export default class RequestEnvironment {
+	static #current: RequestEnvironment|null
+
+	#manager: EntityManager
+	constructor(manager: EntityManager) {
+		this.#manager = manager
+	}
+
+	static get current(): RequestEnvironment {
+		return this.#current
+		|| throw new EvalError("Cannot get an uninitialized request environment.")
+	}
+
+	static intialize(manager: EntityManager) {
+		if(!RequestEnvironment.#current) {
+			RequestEnvironment.#current = new RequestEnvironment(manager)
+		}
+	}
+
+	static destroy() {
+		const environment = getEnvironment()
+
+		if (environment === Environment.UnitTest || environment === Environment.IntegrationTest) {
+			RequestEnvironment.#current = null
+		}
+	}
+}
