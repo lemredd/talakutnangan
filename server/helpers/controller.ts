9508a19946e7a3f8d018b57701e432d76ed784a4
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-05-25 23:19:17 +0800

    feat(server): generate route information on controllers

--- server/helpers/controller.ts
+++ server/helpers/controller.ts
@@ -1,8 +1,62 @@
-import { Request, Response } from "express"
-import RequestEnvironment from "./request_environment";
+import { Request, Response, NextFunction,  } from "express"
 
-export default abstract class {
-	const #environment: RequestEnvironment = RequestEnvironment.current
+import { Route } from "!/types"
+import Middleware from "!/helpers/middleware"
+import RequestEnvironment from "!/helpers/request_environment"
 
-	static abstract handle(request: Request, response: Response): void
+export default abstract class extends Middleware {
+	const #premiddlewares: Middleware[] = []
+	const #postmiddlewares: Middleware[] = []
+	const #overridesPrefix: boolean;
+	const #URL: string;
+
+	constructor(URL: string, overridesPrefix: boolean = false) {
+		this.#URL = URL;
+		this.#overridesPrefix = overridesPrefix;
+	}
+
+	abstract #handle(request: Request, response: Response): void
+
+	#intermediate(request: Request, response: Response, next: NextFunction) {
+		this.handle(request, response)
+
+		next()
+	}
+
+	generateRoute(prefix: string|null = null): Route {
+		const URL = this.#overridesPrefix
+			? this.#URL
+			: `${prefix}/${this.#URL}`
+
+		const handlers = this.#flattenHandlers(this.#premiddlewares)
+
+		if (this.#postmiddlewares.length === 0) {
+			handlers.push(this.#handle)
+		} else {
+			handlers.push(this.#intermediate)
+
+			for (const postHandler of this.this.#flattenHandlers(this.#postmiddlewares)) {
+				handlers.push(postHandler)
+			}
+		}
+
+		return {
+			URL,
+			handlers
+		}
+	}
+
+	#flattenHandlers(middlewares: Middleware[]): RequestHandler[] {
+		const handlers = []
+
+		for (const middleware of middlewares) {
+			const subhandlers = middleware.generateMiddlewareInformation()
+
+			for(const subhandler of subhandlers) {
+				handlers.push(subhandler)
+			}
+		}
+
+		return handlers
+	}
 }
