f7e58e9d66446663e0d2a96275e8608f2935ed89
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-05-25 23:51:44 +0800

    fix(server): fix the declarations and mechanism of base controller

--- server/helpers/controller.ts
+++ server/helpers/controller.ts
@@ -5,37 +5,42 @@ import Middleware from "!/helpers/middleware"
 import RequestEnvironment from "!/helpers/request_environment"
 
 export default abstract class extends Middleware {
-	const #premiddlewares: Middleware[] = []
-	const #postmiddlewares: Middleware[] = []
-	const #overridesPrefix: boolean;
-	const #URL: string;
+	private const premiddlewares: Middleware[] = []
+	private const postmiddlewares: Middleware[] = []
+	private const overridesPrefix: boolean
+	private const URL: string
 
 	constructor(URL: string, overridesPrefix: boolean = false) {
-		this.#URL = URL;
-		this.#overridesPrefix = overridesPrefix;
+		super()
+
+		this.URL = URL
+		this.overridesPrefix = overridesPrefix
 	}
 
-	abstract #handle(request: Request, response: Response): void
+	abstract private handle(request: Request, response: Response): void
 
-	#intermediate(request: Request, response: Response, next: NextFunction) {
+	private intermediate(request: Request, response: Response, next: NextFunction): void {
 		this.handle(request, response)
 
 		next()
 	}
 
 	generateRoute(prefix: string|null = null): Route {
-		const URL = this.#overridesPrefix
-			? this.#URL
-			: `${prefix}/${this.#URL}`
+		const URL = (
+			this.overridesPrefix || prefix === "/" || prefix === null
+			? this.URL
+			: `${prefix}/${this.URL}`
+		)
 
-		const handlers = this.#flattenHandlers(this.#premiddlewares)
+		const handlers = this.flattenHandlers(this.premiddlewares)
 
-		if (this.#postmiddlewares.length === 0) {
-			handlers.push(this.#handle)
+		if (this.postmiddlewares.length === 0) {
+			handlers.push(this.handle)
+
 		} else {
-			handlers.push(this.#intermediate)
+			handlers.push(this.intermediate)
 
-			for (const postHandler of this.this.#flattenHandlers(this.#postmiddlewares)) {
+			for (const postHandler of (this.flattenHandlers(this.postmiddlewares))) {
 				handlers.push(postHandler)
 			}
 		}
@@ -46,7 +51,7 @@ export default abstract class extends Middleware {
 		}
 	}
 
-	#flattenHandlers(middlewares: Middleware[]): RequestHandler[] {
+	private flattenHandlers(middlewares: Middleware[]): RequestHandler[] {
 		const handlers = []
 
 		for (const middleware of middlewares) {
