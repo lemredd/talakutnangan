3e55a2ca39f7493853676ae5866dce53de9cfa65
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-05-25 13:38:13 +0800

    fix(test): ensure the test for getting the list is correct

--- t/integration/api/user/list.get.spec.ts
+++ t/integration/api/user/list.get.spec.ts
@@ -3,19 +3,20 @@ import { StatusCodes } from "http-status-codes"
 import App from "~/app"
 import UserFactory from "~/factories/user"
 
-describe("POST /api/user/list", () => {
-	it("can be accessed by permitted user and get unadmitted users", async () => {
+describe("GET /api/user/list", () => {
+	it("can be accessed by permitted user and get single unadmitted user", async () => {
 		const user = await (new UserFactory()).verified().insertOne()
-		console.log(user)
+
 		const response = await App.request
 			.get("/api/user/list")
 			.query({ criteria: "unadmitted" })
 
 		expect(response.statusCode).toBe(StatusCodes.OK)
 		expect(response.body).toStrictEqual([
-			user
+			JSON.parse(JSON.stringify(user))
 		])
 	})
 
+	it.todo("can be accessed by permitted user and get multiple unadmitted users")
 	it.todo("cannot be accessed by guest users")
 })
