0745b0553f6e0471c66b504924af43fe67d0161d
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-05-25 13:36:22 +0800

    refactor: reduce the syntax by collecting the constraints first

--- server/routes/api/user/list.get.ts
+++ server/routes/api/user/list.get.ts
@@ -1,4 +1,4 @@
-import { EntityManager, IsNull, Not } from "typeorm"
+import { EntityManager, IsNull, Not, FindOptionsWhere } from "typeorm"
 import { StatusCodes } from "http-status-codes"
 import { Request, Response, NextFunction } from "express"
 import User from "!/models/user"
@@ -12,7 +12,7 @@ interface WithQuery {
 export default function(manager: EntityManager) {
 	return async function(request: Request & WithQuery, response: Response, next: NextFunction) {
 		const { criteria = null } = request.query
-		let query = manager.createQueryBuilder(User, "User")
+		const findOptionsWhere: FindOptionsWhere<User> = {}
 
 		switch(criteria) {
 			case "admitted": { // Complete profile and admitted
@@ -21,11 +21,9 @@ export default function(manager: EntityManager) {
 			}
 			case "unadmitted": { // Complete profile but not admitted
 				// TODO: Add constraints to other fields to consider it is a complete profile
-				query = query
-					.where({
-						emailVerifiedAt: Not(IsNull()),
-						admittedAt: IsNull()
-					})
+				findOptionsWhere.emailVerifiedAt = Not(IsNull()),
+				findOptionsWhere.admittedAt = IsNull()
+
 				break
 			}
 			case "incomplete": { // Incomplete profile
@@ -35,7 +33,7 @@ export default function(manager: EntityManager) {
 			default: // All users
 		}
 
-		const users = await query.getMany()
+		const users = await manager.findBy(User, findOptionsWhere)
 
 		// TODO: Hide the signatures of users
 		response.status(StatusCodes.OK).json(users)
