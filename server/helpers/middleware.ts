8aea185f2c15c7866abd4cc165b969f4830a7086
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-05-25 22:21:42 +0800

    feat(server): improve to naming and code of middleware base

--- server/helpers/middleware.ts
+++ server/helpers/middleware.ts
@@ -1,8 +1,13 @@
-import { Request, Response, NextFunction } from "express"
+import { Request, Response, NextFunction, RequestHandler } from "express"
 import RequestEnvironment from "./request_environment";
 
+/**
+ * Base middleware that is being used to organize the routing information of routes
+ */
 export default abstract class {
-	const #environment: RequestEnvironment = RequestEnvironment.current
+	const environment: RequestEnvironment = RequestEnvironment.current
 
-	static abstract handle(request: Request, response: Response, next: NextFunction): void
+	abstract #intermediate(request: Request, response: Response, next: NextFunction): void
+
+	generateMiddlewareInformation(): RequestHandler[] { return [ this.#intermediate ] }
 }
