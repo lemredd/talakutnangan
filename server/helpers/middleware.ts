2d54c3b09ddb8487b12fb1046beb4e8591dbb240
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-05-26 00:37:00 +0800

    feat(server): allow environment to be available to child classes

--- server/helpers/middleware.ts
+++ server/helpers/middleware.ts
@@ -1,12 +1,13 @@
 import { Request, Response, NextFunction, RequestHandler } from "express"
-import RequestEnvironment from "!/helpers/request_environment";
+
 import type { Route } from "!/types"
+import RequestEnvironment from "!/helpers/request_environment";
 
 /**
  * Base middleware that is being used to organize the routing information of routes
  */
 export default abstract class {
-	private const environment: RequestEnvironment = RequestEnvironment.current
+	protected const environment: RequestEnvironment = RequestEnvironment.current
 
 	abstract private intermediate(request: Request, response: Response, next: NextFunction): void
 
