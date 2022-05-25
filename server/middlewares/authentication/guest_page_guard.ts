78ae0bfa5cc40d09a42b3e9ac034faa0c9005649
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-05-26 01:08:01 +0800

    feat(server): create a class-based version of guest page guard

---
+++ server/middlewares/authentication/guest_page_guard.ts
@@ -0,0 +1,31 @@
+import Middleware from "!/helpers/middleware";
+import { StatusCodes } from "http-status-codes"
+import type { RequestHandler, Request, Response, NextFunction } from "express"
+import type { WithPossibleUser, UserKind } from "!/types"
+
+/**
+ * Creates middleware to guards guest pages against authenticated users.
+ *
+ * Useful for home page and some authentication forms.
+ */
+export default class extends Middleware {
+	constructor() { super() }
+
+	private intermediate(
+		request: Request & WithPossibleUser,
+		response: Response,
+		next: NextFunction
+	): void {
+		if (request.isAuthenticated()) {
+			response.status(StatusCodes.UNAUTHORIZED)
+
+			response.json({
+				errors: [
+					"You are not allowed to go to that page."
+				]
+			})
+		} else {
+			next()
+		}
+	}
+}
