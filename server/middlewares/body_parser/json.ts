cdd2f70d8a6f75409188e16a1f41b5248bc1c062
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-05-26 02:09:44 +0800

    feat(server): create a class-based version of JSON body parser

---
+++ server/middlewares/body_parser/json.ts
@@ -0,0 +1,9 @@
+import Middleware from "!/helpers/middleware";
+import express from "express"
+import type { Request, Response, NextFunction } from "express"
+
+export default class extends Middleware {
+	private intermediate(request: Request, response: Response, next: NextFunction): void {
+		express.json()(request, response, next)
+	}
+}
--- server/middlewares/common_middleware_list.ts
+++ server/middlewares/common_middleware_list.ts
@@ -1,12 +1,16 @@
 import GuestPageGuard from "!/middlewares/authentication/guest_page_guard"
 import AuthenticatedPageGuard from "!/middlewares/authentication/authenticated_page_guard"
 
+import JSONBodyParser from "!/middlewares/body_parser/json"
+
 export default class CommonMiddlewareList {
 	static guestPageGuard: GuestPageGuard
 	static basicAuthenticatedPageGuard: AuthenticatedPageGuard
+	static JSONBody: JSONBodyParser
 
 	constructor() {
 		CommonMiddlewareList.guestPageGuard = new GuestPageGuard()
 		CommonMiddlewareList.basicAuthenticatedPageGuard = new AuthenticatedPageGuard(null)
+		CommonMiddlewareList.JSONBody = new JSONBodyParser()
 	}
 }
