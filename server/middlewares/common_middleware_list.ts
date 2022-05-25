fbe106af93064b8d6f8e3a39efca963f5d8bb155
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-05-26 01:42:08 +0800

    feat(server): add basic authenticated page guard

--- server/middlewares/common_middleware_list.ts
+++ server/middlewares/common_middleware_list.ts
@@ -1,9 +1,12 @@
-import GuestPageGuard from "./authentication/guest_page_guard"
+import GuestPageGuard from "!/middlewares/authentication/guest_page_guard"
+import AuthenticatedPageGuard from "!/middlewares/authentication/authenticated_page_guard"
 
 export default class CommonMiddlewareList {
 	static guestPageGuard: GuestPageGuard
+	static basicAuthenticatedPageGuard: AuthenticatedPageGuard
 
 	constructor() {
 		CommonMiddlewareList.guestPageGuard = new GuestPageGuard()
+		CommonMiddlewareList.basicAuthenticatedPageGuard = new AuthenticatedPageGuard(null)
 	}
 }
