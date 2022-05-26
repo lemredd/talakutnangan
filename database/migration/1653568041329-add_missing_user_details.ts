93ccd9c900e63f6a31d845b65bd9fd9cd7333440
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-05-26 22:27:29 +0800

    feat(database): add new migration of user kind

---
+++ database/migration/1653568041329-add_missing_user_details.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+import createEnumColumn from "!/data_source/create_enum_column"
+
+export class addMissingUserDetails1653568041329 implements MigrationInterface {
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.addColumn("user", createEnumColumn(queryRunner.connection, {
+			name: "kind",
+			enum: [ "unreachable_employee", "reachable_employee", "student" ]
+		}))
+	}
+
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.dropColumn("user", "kind")
+	}
+}
