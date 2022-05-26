10b0bfcf6578a6dab73db5a74fb62d0843ff422d
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-05-26 08:36:06 +0800

    fix: remove complicated and improper code from base middleware

--- server/helpers/middleware.ts
+++ server/helpers/middleware.ts
@@ -1,24 +1,14 @@
 import { Request, Response, NextFunction, RequestHandler } from "express"
 
-import type { Route } from "!/types"
 import RequestEnvironment from "!/helpers/request_environment";
 
 /**
  * Base middleware that is being used to organize the routing information of routes
  */
 export default abstract class {
-	protected const environment: RequestEnvironment = RequestEnvironment.current
+	protected environment: RequestEnvironment = RequestEnvironment.current
 
-	abstract private intermediate(request: Request, response: Response, next: NextFunction): void
+	protected abstract intermediate(request: Request, response: Response, next: NextFunction): void
 
 	generateHandlers(): RequestHandler[] { return [ this.intermediate.bind(this) ] }
-
-	generateRoute(URL: string): Route {
-		const handlers = this.generateHandler()
-
-		return {
-			URL,
-			handlers
-		}
-	}
 }
