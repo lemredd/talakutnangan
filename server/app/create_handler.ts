c378ca83153badf7eb4bddf41076bcb8ca2ef80c
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-05-26 08:41:37 +0800

    feat(server): initialize the request environment and common middleware

--- server/app/create_handler.ts
+++ server/app/create_handler.ts
@@ -7,9 +7,14 @@ import manageRoutes from "!/routes/manage_routes"
 import createViteDevServer from "!/app/vite_dev/create_server"
 import manageAuthentication from "!/app/auth/manage_authentication"
 import registerGlobalMiddlewares from "!/middlewares/register_global_middlewares"
+import CommonMiddlewareList from "!/middlewares/common_middleware_list"
+import RequestEnvironment from "!/helpers/request_environment"
 
 export default async function(manager: EntityManager): Promise<express.Express> {
 	const app = express()
+
+	RequestEnvironment.intialize(manager)
+	new CommonMiddlewareList()
 
 	const {
 		viteDevServer: _viteDevServer,
