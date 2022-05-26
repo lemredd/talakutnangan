f0c9822a56bdf038f0e2c6c4cd072ea4cc0c3333
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-05-26 17:18:50 +0800

    test(integration): create different app per route

--- t/integration/api/user/list.get.spec.ts
+++ t/integration/api/user/list.get.spec.ts
@@ -2,8 +2,13 @@ import { StatusCodes } from "http-status-codes"
 
 import App from "~/app"
 import UserFactory from "~/factories/user"
+import Route from "!/routes/api/user/list.get"
 
 describe("GET /api/user/list", () => {
+	beforeAll(async () => {
+		await App.create("/api/user", new Route())
+	})
+
 	it("can be accessed by permitted user and get single unadmitted user", async () => {
 		const user = await (new UserFactory()).verified().insertOne()
 
