766a4fdc002d92612dea1b23dabe0f0876cea8e5
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-05-26 09:40:09 +0800

    test(unit): skip untestable case

--- server/helpers/request_environment.spec.ts
+++ server/helpers/request_environment.spec.ts
@@ -3,13 +3,12 @@ import RequestEnvironment from "./request_environment"
 import Database from "~/database"
 
 describe("Back-end: Request Environment", () => {
-	it("cannot get entity manager without initialization", () => {
+	// Skip because request environment is already initialize in set up
+	it.skip("can get entity manager without initialization", () => {
 		expect(() => RequestEnvironment.current.manager).toThrow()
 	})
 
 	it("can get entity manager after initialization", () => {
-		RequestEnvironment.intialize(Database.manager)
-
 		expect(RequestEnvironment.current.manager).toEqual(Database.manager)
 	})
 })
