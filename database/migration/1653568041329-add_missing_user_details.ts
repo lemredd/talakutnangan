d6da7272df63366a23c5e1c23f79b4b3da08ca1e
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-05-28 20:53:58 +0800

    refactor: make columns for audits

--- database/migration/1653568041329-add_missing_user_details.ts
+++ database/migration/1653568041329-add_missing_user_details.ts
@@ -1,6 +1,9 @@
 import { MigrationInterface, QueryRunner } from "typeorm"
 import makeEnumColumn from "!/data_source/make_enum_column"
 import makeFileColumn from "!/data_source/make_file_column"
+import makeCreatedAtColumn from "!/data_source/make_created_at_column"
+import makeUpdatedAtColumn from "!/data_source/make_updated_at_column"
+import makeDeletedAtColumn from "!/data_source/make_deleted_at_column"
 
 export class addMissingUserDetails1653568041329 implements MigrationInterface {
 	public async up(queryRunner: QueryRunner): Promise<void> {
@@ -13,10 +16,16 @@ export class addMissingUserDetails1653568041329 implements MigrationInterface {
 			name: "signature",
 			isNullable: true
 		}))
+		await queryRunner.addColumn("user", makeCreatedAtColumn(queryRunner.connection))
+		await queryRunner.addColumn("user", makeUpdatedAtColumn(queryRunner.connection))
+		await queryRunner.addColumn("user", makeDeletedAtColumn(queryRunner.connection))
 	}
 
 	public async down(queryRunner: QueryRunner): Promise<void> {
 		await queryRunner.dropColumn("user", "kind")
 		await queryRunner.dropColumn("user", "signature")
+		await queryRunner.dropColumn("user", "createdAt")
+		await queryRunner.dropColumn("user", "updatedAt")
+		await queryRunner.dropColumn("user", "deletedAt")
 	}
 }
