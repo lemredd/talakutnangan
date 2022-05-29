0fee61a8b3bbd9a13a831087be382d2ae039c15c
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-05-29 14:28:54 +0800

    migration(test): rewrite the database in test set up

--- t/helpers/database.ts
+++ t/helpers/database.ts
@@ -1,35 +1,28 @@
-import { DataSource } from "typeorm"
-import User from "%/models/user"
-import { Environment, SourceType } from "!/types"
+import { Sequelize } from "sequelize-typescript"
+import { Environment } from "!/types"
 import getEnvironment from "!/helpers/get_environment"
 import createDataSource from "%/data_source/create_source"
 
 export default class {
-	private static dataSource: DataSource
+	static #dataSource: Sequelize
 
 	static async create(): Promise<void> {
 		if (getEnvironment() === Environment.UnitTest) {
-			this.dataSource = await Promise.resolve(createDataSource("unit test"))
+			this.#dataSource = createDataSource("unit_test")
 		} else {
-			this.dataSource = await Promise.resolve(
-				createDataSource(process.env.DATABASE_TYPE as SourceType)
-			)
+			this.#dataSource = createDataSource(process.env.DATABASE_TYPE as SourceType)
 		}
 	}
 
 	static async clear(): Promise<void> {
-		await this.dataSource.manager.clear(User)
+		await this.dataSource.drop()
 	}
 
 	static async destroy(): Promise<void> {
-		await this.dataSource?.destroy()
+		await this.dataSource.close()
 	}
 
 	static get dataSource() {
 		return this.#dataSource
-	}
-
-	static get manager() {
-		return this.dataSource.manager
 	}
 }
