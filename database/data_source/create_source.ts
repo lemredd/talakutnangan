43e620fe0e28c002a0a73d8f586b64a53d730607
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-05-29 14:17:13 +0800

    migration: rewrite data source creation

--- database/data_source/create_source.ts
+++ database/data_source/create_source.ts
@@ -1,71 +1,17 @@
-import { DataSource, DataSourceOptions } from "typeorm"
+import { Sequelize, SequelizeOptions } from "sequelize-typescript"
+
 import User from "%/models/user"
-import getRoot from "!/helpers/get_root"
-import type { SourceType } from "!/types"
+import type { SourceType } from "%/types"
+import createConfiguration from "%/configuration/create"
 
-export default function(
-	type: SourceType,
-	mustInitialize: boolean = true
-): DataSource|Promise<DataSource> {
-	const dataSourceOptions: { [key: string]: string|number|boolean } = {}
-
-	switch(type) {
-		case "pgsql": {
-			dataSourceOptions.type = "postgres"
-			dataSourceOptions.url = process.env.DATABASE_URL
-			dataSourceOptions.synchronize = false
-			dataSourceOptions.logging = true
-			break
-		}
-		case "mysql": {
-			dataSourceOptions.type = "mysql"
-			dataSourceOptions.host = process.env.DATABASE_HOST
-			dataSourceOptions.port = process.env.DATABASE_PORT as unknown as number
-			dataSourceOptions.database = process.env.DATABASE_NAME
-			dataSourceOptions.username = process.env.DATABASE_USER
-			dataSourceOptions.password = process.env.DATABASE_PASS
-			dataSourceOptions.synchronize = false
-			dataSourceOptions.logging = true
-			break
-		}
-		case "memoried_sqlite": {
-			dataSourceOptions.type = "sqlite"
-			dataSourceOptions.database = ":memory:"
-			dataSourceOptions.synchronize = false
-			dataSourceOptions.logging = true
-			break
-		}
-		case "filed_sqlite": {
-			dataSourceOptions.type = "sqlite"
-			dataSourceOptions.database = process.env.DATABASE_PATH
-			dataSourceOptions.synchronize = false
-			dataSourceOptions.logging = true
-			break
-		}
-		case "unit test": {
-			dataSourceOptions.type = "sqlite"
-			dataSourceOptions.database = ":memory:"
-			dataSourceOptions.synchronize = false
-			break
-		}
-	}
-
-	const root = getRoot()
-	const dataSource = new DataSource({
-		...dataSourceOptions as unknown as DataSourceOptions,
-		entities: [
+export default function(type: SourceType): Sequelize {
+	const configuration: SequelizeOptions = createConfiguration() as SequelizeOptions
+	const sequelize = new Sequelize({
+		...configuration,
+		models: [
 			User
-		],
-		migrations: [
-			`${root}/database/migration/*-*.ts`
 		]
 	})
 
-	if (mustInitialize) {
-		return dataSource.initialize()
-			.then(() => dataSource.runMigrations())
-			.then(() => dataSource)
-	} else {
-		return dataSource
-	}
+	return sequelize
 }
