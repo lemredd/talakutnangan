c28cdb890701689257f8f2ada68b2d0941672e4a
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-05-29 14:07:10 +0800

    types: add some database types for user

---
+++ database/types.ts
@@ -0,0 +1,10 @@
+export const UserKindValues = [ "unreachable_employee", "reachable_employee", "student" ]
+
+/**
+ * Possible kinds of user that the system can handle
+ */
+export enum UserKind {
+	UnreachableEmployee = "unreachable_employee",
+	ReachableEmployee = "reachable_employee",
+	Student = "student"
+}
