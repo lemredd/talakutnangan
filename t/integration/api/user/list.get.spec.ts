5b782792c7a8bc36d8f9ed3ec4c3dcc7f39240ce
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-05-31 13:17:12 +0800

    test(integration): get the new expected values from the database

--- t/integration/api/user/list.get.spec.ts
+++ t/integration/api/user/list.get.spec.ts
@@ -1,4 +1,5 @@
 import { StatusCodes } from "http-status-codes"
+import UserManager from "%/managers/user_manager"
 
 import App from "~/app"
 import UserFactory from "~/factories/user"
@@ -10,6 +11,7 @@ describe("GET /api/user/list", () => {
 	})
 
 	it("can be accessed by permitted user and get single unadmitted user", async () => {
+		const manager = new UserManager()
 		const user = await (new UserFactory()).insertOne()
 
 		const response = await App.request
@@ -17,9 +19,12 @@ describe("GET /api/user/list", () => {
 			.query({ criteria: "unadmitted" })
 
 		expect(response.statusCode).toBe(StatusCodes.OK)
-		expect(response.body).toStrictEqual([
-			JSON.parse(JSON.stringify(user))
-		])
+		const expectedUser = await manager.findWithID(user.id)
+		expect(response.body).toStrictEqual(
+			JSON.parse(JSON.stringify([
+				expectedUser
+			]))
+		)
 	})
 
 	it.todo("can be accessed by permitted user and get multiple unadmitted users")
