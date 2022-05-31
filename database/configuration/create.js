6c850d710a090328ede9bcc537e9d9b62cad3259
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-05-31 11:42:51 +0800

    feat(database): break the database URL into parts to configure properly

--- database/configuration/create.js
+++ database/configuration/create.js
@@ -1,14 +1,22 @@
+const { URL } = require("url")
+
 module.exports = function(databaseType = process.env.DATABASE_TYPE) {
 	const configuration = {}
 
 	switch(databaseType) {
 		case "pgsql": {
-			return process.env.DATABASE_URL
+			const databaseURL = new URL(process.env.DATABASE_URL)
+			configuration.dialect = "postgres"
+			configuration.username = databaseURL.username
+			configuration.password = databaseURL.password
+			configuration.database = databaseURL.pathname
+			configuration.host = databaseURL.hostname
+			configuration.port = +databaseURL.port
 			break
 		}
 
 		case "mysql": {
-			configuration.dialect = databaseType
+			configuration.dialect = "mysql"
 			configuration.username = process.env.DATABASE_USER
 			configuration.password = process.env.DATABASE_PASS
 			configuration.database = process.env.DATABASE_NAME
