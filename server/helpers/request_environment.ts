7b76ce3e7dd33f2bff9c5666d2da2a963549a046
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-01 17:44:42 +0800

    feat(server): add method to get root in request environment

--- server/helpers/request_environment.ts
+++ server/helpers/request_environment.ts
@@ -3,6 +3,7 @@ import { StatusCodes } from "http-status-codes"
 
 import { Environment } from "!/types"
 import getEnvironment from "!/helpers/get_environment"
+import getRoot from "!/helpers/get_root"
 
 /**
  * Contains the environment that can be used by the request handlers.
@@ -26,11 +27,15 @@ export default class RequestEnvironment {
 
 	static get environment(): Environment { return getEnvironment() }
 
+	static get root(): string { return getRoot() }
+
 	static get status() { return StatusCodes }
 
 	// get manager() { return RequestEnvironment.dataSource.manager }
 
 	get environment(): Environment { return getEnvironment() }
+
+	get root(): string { return getRoot() }
 
 	get status() { return StatusCodes }
 }
