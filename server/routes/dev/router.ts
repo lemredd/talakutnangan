8442e022a888b72b14ea77d23f5f925df09376c0
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-01 10:23:03 +0800

    feat(server): remove parameter for parent prefix in development routes

--- server/routes/dev/router.ts
+++ server/routes/dev/router.ts
@@ -2,7 +2,7 @@ import Router from "!/helpers/router"
 import GetEmailVerificationRoute from "!/routes/dev/email_verification.get"
 
 export default class extends Router {
-	constructor(parentPrefix: string) {
+	constructor() {
 		super("/dev")
 
 		this.useController(new GetEmailVerificationRoute())
