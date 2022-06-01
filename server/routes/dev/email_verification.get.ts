fc266c981997880d87ee82ff5b9d96cdda5d7bc1
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-01 09:49:15 +0800

    feat(email): read the email template

--- server/routes/dev/email_verification.get.ts
+++ server/routes/dev/email_verification.get.ts
@@ -1,6 +1,9 @@
+import { readFile } from "fs"
+import { promisify } from "util"
 import { StatusCodes } from "http-status-codes"
 import { Request, Response } from "express"
 import { faker } from "@faker-js/faker"
+import template from "string-placeholder"
 import Controller from "!/helpers/controller"
 
 export default class extends Controller {
@@ -9,7 +12,11 @@ export default class extends Controller {
 	}
 
 	async handle(request: Request, response: Response): Promise<void> {
+		const verificationTemplate = await promisify(readFile)(`${__dirname}/../../../email/email_verification.md`)
+
 		response.status(StatusCodes.OK)
+		response.header("Content-Type", "text/html")
+		response.send(verificationTemplate)
 		response.end()
 	}
 }
