63aa596594417ef641705f8327fc6ca0062dad68
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-02 11:38:10 +0800

    feat(server): indicate the return type of hash function

--- server/helpers/auth/encrypt.ts
+++ server/helpers/auth/encrypt.ts
@@ -1,6 +1,6 @@
 import { genSalt, hash } from "bcryptjs"
 
-export default async function(rawPassword) {
+export default async function(rawPassword): Promise<string> {
 	const salt = await genSalt(10)
 	const hashedPassword = await hash(rawPassword, salt)
 	return hashedPassword
