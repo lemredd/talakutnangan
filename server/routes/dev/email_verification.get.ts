e1af8c792cc31792fccb6dc6cd892e69f6f5b151
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-03 19:52:30 +0800

    feat(email): make transport and double check if it is successful

--- server/routes/dev/email_verification.get.ts
+++ server/routes/dev/email_verification.get.ts
@@ -1,5 +1,6 @@
-import { Request, Response } from "express"
 import { faker } from "@faker-js/faker"
+import { Request, Response } from "express"
+import { createTransport } from "nodemailer"
 import { RawRoute } from "!/types"
 import Controller from "!/routes/bases/controller"
 import convertMarkdownToHTML from "!/helpers/convert_markdown_to_html"
@@ -15,11 +16,30 @@ export default class extends Controller {
 
 	async handle(request: Request, response: Response): Promise<void> {
 		const rawVerification = await specializeTemplateFile("email/email_verification.md", {
-			email: faker.internet.exampleEmail(),
+			email: process.env.EMAIL_USER,
 			homePageURL: faker.internet.url(),
 			emailVerificationURL: faker.internet.url()
 		})
 		const parsedVerification = convertMarkdownToHTML(rawVerification)
+
+		const transport = createTransport({
+			port: +process.env.EMAIL_PORT,
+			host: process.env.EMAIL_HOST,
+			auth: {
+				type: "login",
+				user: process.env.EMAIL_USER,
+				pass: process.env.EMAIL_PASS
+			}
+		})
+
+		transport.verify((error, success) => {
+			if (error) {
+				console.error("There is a problem on connecting with the e-mail server.")
+				console.error(`Error [${error.name}]: ${error.message}`)
+			} else {
+				console.log("Connected to the e-mail server")
+			}
+		})
 
 		response.status(this.status.OK)
 		response.header("Content-Type", "text/html")
