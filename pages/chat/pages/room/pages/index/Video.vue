1d5c3cc752913f2586d16df704ec7d373c289704
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-06-03 22:23:55 +0800

    feat: add device userMedia on call

---
+++ pages/chat/pages/room/pages/index/Video.vue
@@ -0,0 +1,21 @@
+<template>
+<video ref="videoElement"></video>
+</template>
+
+<style>
+
+</style>
+
+<script setup lang="ts">
+import { onMounted, ref, Ref } from 'vue';
+
+const videoElement: Ref<HTMLVideoElement | null> = ref(null)
+
+onMounted(() => {
+    navigator.mediaDevices.getUserMedia({ video: true })
+    .then(stream => {
+        videoElement.value.srcObject = stream
+        videoElement.value.addEventListener("loadedmetadata", () => videoElement.value.play())
+    })
+})
+</script>
--- pages/chat/pages/room/pages/index/index.page.vue
+++ pages/chat/pages/room/pages/index/index.page.vue
@@ -1,41 +1,61 @@
 <template>
-	<div>
-		<header>
-			<h1>Chat Room</h1>
-
-		</header>
-		<form @submit.prevent="joinRoom" v-if="!hasEmail" class="flex flex-row flex-nowrap">
+	<div v-if="!hasEmail">
+		<form @submit.prevent="joinRoom" class="flex flex-row flex-nowrap">
 			<label class="flex-1 flex-grow flex-shrink-0">
 				E-mail: <input type="email" ref="emailField" class="bg-gray-500"/>
 			</label>
 		</form>
-		<form @submit.prevent="sendMessage" v-if="hasEmail">
-			<label class="flex-1 flex-grow flex-shrink-0">
-				Message:
-				<input type="text" ref="chatBox"/>
-			</label>
-			<output>
-				<p v-for="{ time, email, content } in messages">
-					{{ time }} {{ email }}: {{ content }}
-				</p>
-			</output>
-		</form>
+	</div>
+
+	<div v-else>
+		<header>
+			<h1>Chat Room</h1>
+			<div class="btns">
+				<button class="bg-gray-200 px-3 py-1" @click="initiateCall" :disabled="isCalling">
+					call
+				</button>
+			</div>
+		</header>
+		<div class="chatbox grid grid-cols-[2fr,1fr]">
+			<div class="videos" v-if="isCalling">
+				<div class="participants">
+				<Video :id="email"></Video>
+				</div>
+			</div>
+
+			<form @submit.prevent="sendMessage">
+				<label class="flex-1 flex-grow flex-shrink-0">
+					Message:
+					<input type="text" ref="chatBox" class="rounded border-1 border-gray-500 focus:outline-none"/>
+				</label>
+				<output>
+					<p v-for="{ time, email, content } in messages">
+						{{ time }} {{ email }}: {{ content }}
+					</p>
+				</output>
+			</form>
+		</div>
 	</div>
 </template>
 
-<style lang="scss">
-
+<style scoped lang="scss">
+video {
+	width: 100%;
+	height: 100%;
+}
 </style>
 
 <script setup lang="ts">
-import { ref, computed } from "vue"
+import { ref, computed, VNode} from "vue"
 import makeClient from "socket.io-client"
 import { usePageContext } from "#/usePageContext"
+import Video from "./Video.vue";
 
 const clientWebSocket = makeClient()
 const pageContext = usePageContext()
 const emailField = ref(null)
 const email = ref("")
+const isCalling = ref(false)
 const hasEmail = computed(() => email.value !== "")
 
 const chatBox = ref(null)
@@ -44,6 +64,10 @@ const messages = ref([])
 clientWebSocket.on("receive_message", message => {
 	messages.value.push(message)
 })
+
+function initiateCall() {
+	isCalling.value = true
+}
 
 function joinRoom() {
 	const input = emailField.value as HTMLInputElement
