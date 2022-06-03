7d7e92fc53cebd0edf9da85ebf59d0ce485e7974
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-03 21:19:42 +0800

    feat(server): make method to check if the app is currently in test

--- server/helpers/request_environment.ts
+++ server/helpers/request_environment.ts
@@ -2,8 +2,8 @@ import { Sequelize } from "sequelize-typescript"
 import { StatusCodes } from "http-status-codes"
 
 import { Environment } from "!/types"
-import getEnvironment from "!/helpers/get_environment"
 import getRoot from "!/helpers/get_root"
+import getEnvironment from "!/helpers/get_environment"
 
 /**
  * Contains the environment that can be used by the request handlers.
@@ -23,19 +23,22 @@ export default class RequestEnvironment {
 		}
 	}
 
-	// static get manager() { return RequestEnvironment.dataSource.manager }
-
 	static get environment(): Environment { return getEnvironment() }
 
 	static get root(): string { return getRoot() }
 
 	static get status() { return StatusCodes }
 
-	// get manager() { return RequestEnvironment.dataSource.manager }
+	static get isOnTest(): boolean {
+		const environment = this.environment
+		return environment === Environment.UnitTest || environment === Environment.IntegrationTest
+	}
 
 	get environment(): Environment { return getEnvironment() }
 
 	get root(): string { return getRoot() }
 
 	get status() { return StatusCodes }
+
+	get isOnTest(): boolean { return RequestEnvironment.isOnTest }
 }
