0b9aad68138e3eb6a0ce6d2757ce46d6ad3affc8
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-04 20:52:33 +0800

    feat(server): add route to verify user account

---
+++ server/routes/api/user/verify.get.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express"
+
+import UserManager from "%/managers/user_manager"
+import Controller from "!/routes/bases/controller"
+import { RawRoute } from "!/types"
+
+interface WithQuery {
+	query: {
+		to: string
+	}
+}
+
+export default class extends Controller {
+	getRawRoute(): RawRoute {
+		return {
+			method: "get",
+			baseURL: "verify"
+		}
+	}
+
+	async handle(request: Request & WithQuery, response: Response): Promise<void> {
+		// TODO: Validate query parameter
+		const { to } = request.query
+		const manager = new UserManager()
+		const verifiedCount = await manager.verify(to)
+
+		response.status(verifiedCount > 0 ? this.status.ACCEPTED : this.status.NOT_MODIFIED).end()
+	}
+}
