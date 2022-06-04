4f73660ecae1dddad50d8aaa1af472e09900aaf9
Angelo Magtoto <19201.magtoto.angelo.l@gmail.com>
2022-06-04 18:20:10 +0800

    feat(forum): initial setup

---
+++ pages/forum/data.ts
@@ -0,0 +1,43 @@
+var posts =
+[
+    {
+        id:1,
+        user: "User 1",
+        title: "Morbi blandit cursus risus at",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        voteStatus: false
+    },
+    {
+        id:2,
+        user: "User 2",
+        title: "Morbi blandit cursus risus at",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        voteStatus: false
+    },
+    {  
+        id:3,
+        user: "User 3",
+        title: "Morbi blandit cursus risus at",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        voteStatus: false
+    },
+    {
+        id:4,
+        user: "User 4",
+        title: "Morbi blandit cursus risus at",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        voteStatus: false
+    },
+    {
+        id:4,
+        user: "User 5",
+        title: "Morbi blandit cursus risus at",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        voteStatus: false
+    }
+];
+
+export function getPosts()
+{
+    return posts;
+}
---
+++ pages/forum/index.page.vue
@@ -0,0 +1,63 @@
+<template>
+	<div>
+        <!--header start-->
+        <div class="top-bar">
+            <div><img src="./images/emptyImage.png"></div>
+            <h4>
+                Forum App
+            </h4>
+        </div>
+        <!--header end-->
+
+        <!--body start-->
+        <div class="main">
+            <br/>
+            <div class="post" v-for="post in posts" v-bind:key="post.id">
+                <div class="container">
+                    <div class="left">
+                        <h2 class = "title">
+                        {{ post.user }}
+                        </h2>
+                    </div>
+                    <div class="middle">
+                        <h2 class = "title">
+                        {{ post.title }}
+                        </h2>
+                    </div>
+                    <div class="right">
+                        <label class="switch">
+                            <input type="checkbox">
+                            <span class="slider"></span>
+                        </label>
+                    </div>
+                </div>
+                <p v-bind:class="`${post.id}`">
+                    {{ post.desc }}
+                </p>
+                <br/>
+
+            </div>
+        </div>
+        <!--body end-->
+
+        <!--footer start-->
+        <footer>
+            <p>Footer space<br>
+            <a href="./forum">email@example.com</a></p>
+        </footer>
+        <!--footer end-->
+	</div>
+</template>
+
+<style lang="scss">
+@import "./index";
+</style>
+
+<script setup lang="ts">
+import { getPosts } from "./data";
+
+var posts = getPosts();
+
+var userVotes = 0;
+
+</script>
