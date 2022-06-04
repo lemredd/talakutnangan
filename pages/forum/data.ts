0f194ed16bb49394cf58b4236f61e93b201b03bc
Angelo Magtoto <19201.magtoto.angelo.l@gmail.com>
2022-06-04 23:15:55 +0800

    chore(forum): restructure dummy post data

--- pages/forum/data.ts
+++ pages/forum/data.ts
@@ -5,35 +5,65 @@ var posts =
         user: "User 1",
         title: "Morbi blandit cursus risus at",
         desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        voteStatus: false
+        voters: [
+            "Names 1","Name 2", "Name 3"
+        ],
+        voteCount: function()
+        {
+            return this.voters.length;
+        }
     },
     {
         id:2,
         user: "User 2",
         title: "Morbi blandit cursus risus at",
         desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        voteStatus: false
+        voters: [
+            "Names 1","Name 2", "Name 3"
+        ],
+        voteCount: function()
+        {
+            return this.voters.length;
+        }
     },
     {  
         id:3,
         user: "User 3",
         title: "Morbi blandit cursus risus at",
         desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        voteStatus: false
+        voters: [
+            "Names 1","Name 2", "Name 3"
+        ],
+        voteCount: function()
+        {
+            return this.voters.length;
+        }
     },
     {
         id:4,
         user: "User 4",
         title: "Morbi blandit cursus risus at",
         desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        voteStatus: false
+        voters: [
+            "Names 1","Name 2", "Name 3"
+        ],
+        voteCount: function()
+        {
+            return this.voters.length;
+        }
     },
     {
         id:4,
         user: "User 5",
         title: "Morbi blandit cursus risus at",
         desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        voteStatus: false
+        voters: [
+            "Names 1","Name 2", "Name 3"
+        ],
+        voteCount: function()
+        {
+            return this.voters.length;
+        }
     }
 ];
 
