be3a80b4ddb970d020b0867025b46418a254bea3
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-05 22:58:49 +0800

    feat(server): match the path exactly

--- server/helpers/extract_route_info.ts
+++ server/helpers/extract_route_info.ts
@@ -1,12 +1,12 @@
 import { posix } from "path"
 import getRoot from "!/helpers/get_root"
-import { Method, RouteInformation } from "!/types/independent"
+import { Purpose, Method, RouteInformation } from "!/types/independent"
 
 export default function(currentPath: string, routeRoot = `${getRoot()}/server/app/routes`)
 	: Partial<RouteInformation> {
 	const relativePath = posix.relative(routeRoot, currentPath)
-	const [ _, rawPurpose, rawPath, method ] = /^(\w+)(.*?\/\w+?)\.(\w+?)\.ts$/.exec(relativePath)
-	const purpose = rawPurpose === "api" ? "api" : rawPurpose === "dev" ? "dev" : "enhancer"
+	const [ _, rawPurpose, rawPath, method ] = /^(api|dev)?(.*?\w+?)\.(\w+?)\.ts$/.exec(relativePath)
+	const purpose = <Purpose>(rawPurpose || "enhancer")
 	const path = "/" + (purpose === "enhancer" ? rawPath : posix.join(purpose, rawPath))
 
 	return {
