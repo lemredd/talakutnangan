f525d0043f5babaf756d0d4c859fbeb0d6b454af
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-05 23:29:34 +0800

    feat(server)!: rewrite some base controllers
    Post controller is not necessary anymore since route information can be
    derived from file path.

--- server/bases/controllers/json_controller.ts
+++ server/bases/controllers/json_controller.ts
@@ -1,12 +1,12 @@
 import Middleware from "!/bases/middleware"
-import PostController from "!/bases/controllers/post_controller"
+import Controller from "!/bases/controller"
 import CommonMiddlewareList from "!/middlewares/common_middleware_list"
 import BodyValidation from "!/middlewares/authorization/body_validation"
 
-export default abstract class extends PostController {
-	getPremiddlewares(): Middleware[] {
+export default abstract class extends Controller {
+	get middlewares(): Middleware[] {
 		return [
-			...super.getPremiddlewares(),
+			...super.middlewares,
 			CommonMiddlewareList.JSONBody,
 			new BodyValidation(this.validationRules)
 		]
