315e4835ca67f27d1c89e06fcf81a16efc3e36f6
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-05 23:02:10 +0800

    feat(server)!: remove handler generation of middlewares

--- server/app/register_global_middlewares.ts
+++ server/app/register_global_middlewares.ts
@@ -14,6 +14,6 @@ export default async function(app: ExpressApp) {
 	]
 
 	middlewares.forEach(middleware => {
-		app.use(...middleware.generateHandlers())
+		app.use(middleware.intermediate.bind(middleware))
 	})
 }
--- server/bases/middleware.ts
+++ server/bases/middleware.ts
@@ -1,12 +1,9 @@
-import { Request, Response, NextFunction, RequestHandler } from "express"
-
-import RequestEnvironment from "!/helpers/request_environment";
+import RequestEnvironment from "!/helpers/request_environment"
+import { Request, Response, NextFunction } from "!/types/dependent"
 
 /**
  * Base middleware that is being used to organize the routing information of routes
  */
 export default abstract class extends RequestEnvironment {
 	abstract intermediate(request: Request, response: Response, next: NextFunction): void
-
-	generateHandlers(): RequestHandler[] { return [ this.intermediate.bind(this) ] }
 }
