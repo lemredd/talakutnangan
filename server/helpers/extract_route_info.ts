2c1fa26fa40b605f4ffee751b75d0764cffa7614
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-05 22:49:45 +0800

    feat(server): enhance helper to detect the route purpose

--- server/helpers/extract_route_info.ts
+++ server/helpers/extract_route_info.ts
@@ -4,11 +4,14 @@ import { Method, RouteInformation } from "!/types/independent"
 
 export default function(currentPath: string, routeRoot = `${getRoot()}/server/app/routes`)
 	: Partial<RouteInformation> {
-	const relativePath = "/" + posix.relative(routeRoot, currentPath)
-	const [ _, path, method ] = /^(.+?\/\w+?)\.(\w+?)\.ts$/.exec(relativePath)
+	const relativePath = posix.relative(routeRoot, currentPath)
+	const [ _, rawPurpose, rawPath, method ] = /^(\w+)(.*?\/\w+?)\.(\w+?)\.ts$/.exec(relativePath)
+	const purpose = rawPurpose === "api" ? "api" : rawPurpose === "dev" ? "dev" : "enhancer"
+	const path = "/" + (purpose === "enhancer" ? rawPath : posix.join(purpose, rawPath))
 
 	return {
 		method: <Method><unknown>method,
-		path
+		path,
+		purpose
 	}
 }
