8535560c2c89b0e2d4c8b4d8a800037eeaff98f1
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-06 21:04:55 +0800

    feat(CLI): style some texts

--- server/cli/list_routes.ts
+++ server/cli/list_routes.ts
@@ -1,3 +1,4 @@
+import chalk from "chalk"
 import RouterManager from "!/app/routes/router_manager"
 
 function main() {
@@ -7,13 +8,20 @@ function main() {
 	const allInformation = routerManager.allRouteInformation
 		.map(({ purpose, method, path }) => ({
 			purpose: purpose === "api"
-				? "API"
-				: purpose.slice(0, 1).toLocaleUpperCase() + purpose.slice(1),
+				? "     " + chalk.white.bgBlue("API")
+				: purpose === "dev"
+					? "     " + chalk.white.bgRed(
+						purpose.slice(0, 1).toLocaleUpperCase() + purpose.slice(1))
+					: chalk.white.bgGreen(purpose.slice(0, 1).toLocaleUpperCase() + purpose.slice(1)),
 			method: method.toLocaleUpperCase(),
 			path
 		}))
 
-	console.table(allInformation)
+	for(const { purpose, method, path } of allInformation) {
+		console.log(
+			`\t${purpose} ${method.padStart(5, " ")} ${path}`
+		)
+	}
 }
 
 main()
