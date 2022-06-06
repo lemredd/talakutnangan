fe04bf192e8e5cb34ecb2722d573467922f69ca7
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-06 20:08:18 +0800

    feat(server): rename controller for patching user details

--- server/app/routes/api/user/router.ts
+++ server/app/routes/api/user/router.ts
@@ -4,8 +4,8 @@ import GetList from "!/app/routes/api/user/list.get"
 import GetCreate from "!/app/routes/api/user/create.get"
 import PostLogIn from "!/app/routes/api/user/log_in.post"
 import PostLogOut from "!/app/routes/api/user/log_out.post"
-import PatchUpdate from "!/app/routes/api/user/update.patch"
 import PostRegister from "!/app/routes/api/user/register.post"
+import PatchUpdate from "!/app/routes/api/user/update(id).patch"
 import GetLogInFailure from "!/app/routes/api/user/log_in_failure.get"
 
 export default class extends Router {
