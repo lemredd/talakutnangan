b51ddd47b14c538da6c85c7ba98906c45e14dc54
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-06 18:22:02 +0800

    fix(server): do path operations properly not based on POSIX
    It does not work properly with my operating system so replacing the
    string is the remedy.

--- server/helpers/extract_route_info.ts
+++ server/helpers/extract_route_info.ts
@@ -1,13 +1,14 @@
-import { posix } from "path"
+import { relative, resolve, join, sep } from "path"
 import getRoot from "!/helpers/get_root"
 import { Purpose, Method, RouteInformation } from "!/types/independent"
 
-export default function(currentPath: string, routeRoot = `${getRoot()}/server/app/routes`)
+export default function(currentPath: string, routeRoot = resolve(getRoot(), "server/app/routes"))
 	: Partial<RouteInformation> {
-	const relativePath = posix.relative(routeRoot, currentPath)
+	const relativePath = relative(routeRoot, currentPath)
 	const [ _, rawPurpose, rawPath, method ] = /^(api|dev)?(.*?\w+?)\.(\w+?)\.ts$/.exec(relativePath)
 	const purpose = <Purpose>(rawPurpose || "enhancer")
-	const path = "/" + (purpose === "enhancer" ? rawPath : posix.join(purpose, rawPath))
+	const dirtyPath = "/" + (purpose === "enhancer" ? rawPath : join(purpose, rawPath))
+	const path = dirtyPath.replace(/\\/g, "/")
 
 	return {
 		method: <Method><unknown>method,
