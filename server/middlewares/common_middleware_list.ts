d8512381a408d1e3786b008952bcc75a28c5d196
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-07 21:27:07 +0800

    feat(server): add variants of known only policy

--- server/middlewares/common_middleware_list.ts
+++ server/middlewares/common_middleware_list.ts
@@ -1,12 +1,15 @@
+import { UserKind } from "%/types"
+import JSONBodyParser from "!/middlewares/body_parser/json"
 import GuestOnlyPolicy from "!/middlewares/authentication/guest_only_policy"
 import KnownOnlyPolicy from "!/middlewares/authentication/known_only_policy"
-
-import JSONBodyParser from "!/middlewares/body_parser/json"
 import EmailVerificationSender from "!/middlewares/authentication/email_verification_sender"
 
 export default class CommonMiddlewareList {
 	static guestOnlyPolicy: GuestOnlyPolicy
 	static knownOnlyPolicy: KnownOnlyPolicy
+	static unreachableEmployeeOnlyPolicy: KnownOnlyPolicy
+	static reachableEmployeeOnlyPolicy: KnownOnlyPolicy
+	static studentOnlyPolicy: KnownOnlyPolicy
 	static JSONBody: JSONBodyParser
 	static emailVerificationSender: EmailVerificationSender
 
@@ -14,6 +17,14 @@ export default class CommonMiddlewareList {
 		if (CommonMiddlewareList.guestOnlyPolicy === undefined) {
 			CommonMiddlewareList.guestOnlyPolicy = new GuestOnlyPolicy()
 			CommonMiddlewareList.knownOnlyPolicy = new KnownOnlyPolicy(null)
+			CommonMiddlewareList.unreachableEmployeeOnlyPolicy= new KnownOnlyPolicy(
+				UserKind.UnreachableEmployee
+			)
+			CommonMiddlewareList.reachableEmployeeOnlyPolicy= new KnownOnlyPolicy(
+				UserKind.ReachableEmployee
+			)
+			CommonMiddlewareList.studentOnlyPolicy= new KnownOnlyPolicy(UserKind.Student)
+
 			CommonMiddlewareList.JSONBody = new JSONBodyParser()
 			CommonMiddlewareList.emailVerificationSender = new EmailVerificationSender()
 		}
