ae08dee5254a202a2a7e1b6e0c04fed3fef71050
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-08 22:35:01 +0800

    test: create department factory

---
+++ t/helpers/factories/department.ts
@@ -0,0 +1,35 @@
+import { faker } from "@faker-js/faker"
+import Department from "%/models/department"
+
+export default class DepartmentFactory {
+	#mayAdmit = true
+
+	async generate() {
+		const departmentName = faker.random.words()
+		return {
+			name: departmentName,
+			acronym: departmentName.split(" ").map(name => name.slice(0, 1)).join(""),
+			mayAdmit: this.#mayAdmit
+		}
+	}
+
+	async makeOne() {
+		const department = await Department.build(await this.generate())
+		return department
+	}
+
+	async insertOne() {
+		const department = await Department.create(await this.generate())
+		return department
+	}
+
+	mayAdmit(): DepartmentFactory {
+		this.#mayAdmit = true
+		return this
+	}
+
+	mayNotAdmit(): DepartmentFactory {
+		this.#mayAdmit = false
+		return this
+	}
+}
