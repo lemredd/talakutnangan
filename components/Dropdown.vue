7064f7d80d8846fe4af69d8cc0814b306c9fd4ef
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-06-09 15:43:14 +0800

    feat: create dropdown component

---
+++ components/Dropdown.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="dropdown-container dark:bg-dark-700">
+		<slot></slot>
+	</div>
+</template>
+
+<style scoped lang="scss">
+.dropdown-container {
+	box-shadow: 4px 4px 10px rgba(0,0,0,0.5);
+	border: 1px solid white;
+}
+</style>
+
+<script setup lang="ts">
+
+
+</script>
