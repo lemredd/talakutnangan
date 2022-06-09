8ea9ce82b825aaf602e44fa10bd22282f935be2e
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-09 15:31:34 +0800

    fix(database): put the modification of column inside the callback

--- database/migrations/20220607144900-link-department-to-users.js
+++ database/migrations/20220607144900-link-department-to-users.js
@@ -1,32 +1,33 @@
 "use strict";
 module.exports = {
 	async up(queryInterface, Sequelize) {
-		const transaction = await queryInterface.sequelize.transaction();
-		try {
-			await queryInterface.addColumn(
-				"Users",
-				"departmentID",
-				{
-					type: Sequelize.DataTypes.BIGINT
-				},
-				{ transaction }
-			);
+		queryInterface.sequelize.transaction(async transaction => {
+			try {
+				await queryInterface.addColumn(
+					"Users",
+					"departmentID",
+					{
+						type: Sequelize.DataTypes.BIGINT
+					},
+					{ transaction }
+				);
 
-			await queryInterface.addConstraint("Users", {
-				fields: [ "departmentID" ],
-				type: "foreign key",
-				name: "department_id_constraint",
-				references: {
-					table: "Departments",
-					field: "id"
-				},
-				onDelete: "cascade"
-			});
-			await transaction.commit();
-		} catch (err) {
-			await transaction.rollback();
-			throw err;
-		}
+				await queryInterface.addConstraint("Users", {
+					fields: [ "departmentID" ],
+					type: "foreign key",
+					name: "department_id_constraint",
+					references: {
+						table: "Departments",
+						field: "id"
+					},
+					onDelete: "cascade"
+				});
+				await transaction.commit();
+			} catch (err) {
+				await transaction.rollback();
+				throw err;
+			}
+		});
 	},
 	async down(queryInterface, Sequelize) {
 		await queryInterface.removeColumn("Users", "departmentID");
