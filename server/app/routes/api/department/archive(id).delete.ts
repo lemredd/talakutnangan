06abfcc12b231e8201c0ffde2bf2391c2be95574
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-09 20:09:14 +0800

    feat(server): add route to archive department

---
+++ server/app/routes/api/department/archive(id).delete.ts
@@ -0,0 +1,30 @@
+import { IDParameter, Request as BaseRequest, Response } from "!/types/dependent"
+
+import Middleware from "!/bases/middleware"
+import Controller from "!/bases/controller"
+import DepartmentManager from "%/managers/department_manager"
+import CommonMiddlewareList from "!/middlewares/common_middleware_list"
+
+interface WithID {
+	params: IDParameter
+}
+export type Request = BaseRequest & WithID
+
+export default class extends Controller {
+	get filePath(): string { return __filename }
+
+	get middlewares(): Middleware[] {
+		return [
+			CommonMiddlewareList.knownOnlyPolicy,
+			...super.middlewares
+		]
+	}
+
+	async handle(request: Request, response: Response): Promise<void> {
+		const { id } = request.params
+		const manager = new DepartmentManager()
+		const deleteCount = await manager.archive(+id)
+
+		response.status(deleteCount > 0? this.status.ACCEPTED : this.status.NOT_MODIFIED).end()
+	}
+}
--- server/app/routes/api/department/router.ts
+++ server/app/routes/api/department/router.ts
@@ -2,6 +2,7 @@ import Router from "!/bases/router"
 import GetRead from "!/app/routes/api/department/read.get"
 import PostCreate from "!/app/routes/api/department/create.post"
 import PatchUpdate from "!/app/routes/api/department/update(id).patch"
+import DeleteArchive from "!/app/routes/api/department/archive(id).delete"
 
 export default class extends Router {
 	constructor() {
@@ -10,7 +11,8 @@ export default class extends Router {
 		this.useControllers([
 			new GetRead(),
 			new PostCreate(),
-			new PatchUpdate()
+			new PatchUpdate(),
+			new DeleteArchive()
 		])
 	}
 }
