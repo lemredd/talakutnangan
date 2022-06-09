1dec62d5d71f688d1fab48458a054ff9bbc80522
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-09 20:28:41 +0800

    feat(server): add route to restore department

--- server/app/routes/api/department/router.ts
+++ server/app/routes/api/department/router.ts
@@ -2,6 +2,7 @@ import Router from "!/bases/router"
 import GetRead from "!/app/routes/api/department/read.get"
 import PostCreate from "!/app/routes/api/department/create.post"
 import PutUpdate from "!/app/routes/api/department/update(id).put"
+import PatchRestore from "!/app/routes/api/department/restore(id).patch"
 import DeleteArchive from "!/app/routes/api/department/archive(id).delete"
 
 export default class extends Router {
@@ -10,8 +11,9 @@ export default class extends Router {
 
 		this.useControllers([
 			new GetRead(),
-			new PostCreate(),
 			new PutUpdate(),
+			new PostCreate(),
+			new PatchRestore(),
 			new DeleteArchive()
 		])
 	}
