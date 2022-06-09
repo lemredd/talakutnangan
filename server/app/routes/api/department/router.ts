86f91aed49a1ec64f68afc4bd682a1846ffbc898
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-09 19:49:16 +0800

    feat(server): add route to create department

--- server/app/routes/api/department/router.ts
+++ server/app/routes/api/department/router.ts
@@ -1,12 +1,14 @@
 import Router from "!/bases/router"
 import GetRead from "!/app/routes/api/department/read.get"
+import PostCreate from "!/app/routes/api/department/create.post"
 
 export default class extends Router {
 	constructor() {
 		super()
 
 		this.useControllers([
-			new GetRead()
+			new GetRead(),
+			new PostCreate()
 		])
 	}
 }
