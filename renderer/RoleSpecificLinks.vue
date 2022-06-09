c8329f119dce65e598bbf59ec2cd8c7898516f61
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-06-09 12:00:44 +0800

    feat(shell): define and determine role specifiers

--- renderer/RoleSpecificLinks.vue
+++ renderer/RoleSpecificLinks.vue
@@ -1,7 +1,45 @@
+		<Link v-for="link in determineRoleLinks.links" :key="link.name" :href="link.path">
+			<span class="material-icons">
+				{{ link.icon }}
+			</span>
+		</Link>
+
+<style>
+
+</style>
+
+<script setup lang="ts">
 import { computed } from "vue";
 import Link from "./Link.vue"
+
 type Props = {
 	role: string
 }
 const props = defineProps<Props>()
 const role = props.role
+const roleSpecifiers = [
+	{
+		role: "guest",
+		links: [
+			{
+				name: "login",
+				path: "/log_in",
+				icon: "account_circle"
+			}
+		]
+	},
+	{
+		role: "student",
+		links: [
+			{
+				name: "login",
+				path: "/log_in",
+				icon: "account_circle"
+			}
+		]
+	}
+]
+const determineRoleLinks = computed(() => {
+	return roleSpecifiers.filter(specifier => specifier.role === role)[0]
+})
+</script>
