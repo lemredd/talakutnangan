84f78b1cdcdb6d960524a45eaada54596070ab3e
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-10 21:22:36 +0800

    feat(server): make base class for controller-like classes

---
+++ server/bases/controller-like.ts
@@ -0,0 +1,44 @@
+import Middleware from "!/bases/middleware"
+import extractRouteInfo from "!/helpers/extract_route_info"
+
+import { RouteHandlers } from "!/types/hybrid"
+import { RouteInformation } from "!/types/independent"
+import { Request, Response, NextFunction } from "!/types/dependent"
+
+export default abstract class extends Middleware {
+	/**
+	 * Returns the path of the controller. It should return `__filename`
+	 */
+	abstract get filePath(): string
+
+	/**
+	 * Returns the middlewares to be used before that main handle will execute.
+	 */
+	protected get middlewares(): Middleware[] {
+		return []
+	}
+
+	/**
+	 * Returns the jobs to run after the main handle executed.
+	 */
+	protected get postJobs(): Middleware[] {
+		return []
+	}
+
+	get description(): string|null { return null }
+
+	get routeInformation(): RouteInformation {
+		return <RouteInformation>{
+			...extractRouteInfo(this.filePath),
+			description: this.description
+		}
+	}
+
+	get handlers(): RouteHandlers {
+		return {
+			middlewares: this.middlewares,
+			controller: this.intermediate.bind(this),
+			postJobs: this.postJobs
+		}
+	}
+}
