bcedbdb9c3c8f0dfeba971267765473f705ad585
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-06-10 11:54:14 +0800

    test(shell): test role links availability

--- components/PageShell/RoleSpecificLinks.spec.ts
+++ components/PageShell/RoleSpecificLinks.spec.ts
@@ -12,3 +12,24 @@ describe("Component: Page Shell/Role Specific Links", () => {
 			}
 		})
 	})
+
+	it("should specify the right link/s for a specific role", async () => {
+		const link = wrapper.find(".role-links link-stub:first-of-type")
+		const linkHref = link.attributes("href")
+		const specifiedRole = wrapper.props().role
+
+		switch (specifiedRole) {
+			case "student_or_employee":
+				expect(linkHref).toBe("/consultations")
+				break
+			case "user_manager":
+				expect(linkHref).toBe("/consultations")
+				break
+			case "admin":
+				expect(linkHref).toBe("/manage")
+				break
+			default:
+				expect(linkHref).toBe("/log_in")
+		}
+	})
+
