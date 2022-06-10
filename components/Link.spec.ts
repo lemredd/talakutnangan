b9356e21cfcdb22a490f497c94daf530df8359c9
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-06-10 14:07:12 +0800

    test(shell): test link activeness

--- components/Link.spec.ts
+++ components/Link.spec.ts
@@ -2,7 +2,10 @@ import { mount } from "@vue/test-utils"
 import Link from "./Link.vue"
 
 
+
 describe("Component: Link", () => {
+	it("Should be active based on current path", async () => {
+		const path = "/"
 		const wrapper = mount(Link, {
 			attrs: {
 				href: path
@@ -15,4 +18,25 @@ describe("Component: Link", () => {
 				}
 			}
 		})
+
+		expect(wrapper.attributes("class")).toContain("active")
 	})
+
+	it("Should not be active if path mismatches", async () => {
+		const path = "/"
+		const wrapper = mount(Link, {
+			attrs: {
+				href: "/about"
+			},
+			global: {
+				provide: {
+					pageContext: {
+						urlPathname: path
+					}
+				}
+			}
+		})
+
+		expect(wrapper.attributes("class")).not.toContain("active")
+	})
+})
