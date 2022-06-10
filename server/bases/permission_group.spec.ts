de2dd3739aa16556072a803d4442474ab0183b11
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-10 13:43:18 +0800

    test(unit): update test to pass union on created class

--- server/bases/permission_group.spec.ts
+++ server/bases/permission_group.spec.ts
@@ -4,10 +4,12 @@ import PermissionGroup from "./permission_group"
 
 describe("Back-end: Base Permission Group", () => {
 	it("cannot evaluate without required name in role", async () => {
-		class PermissionGroupA extends PermissionGroup {
+		type AvailablePermissions = "" | ""
+
+		class PermissionGroupA extends PermissionGroup<AvailablePermissions> {
 			get name(): string { return "groupA" }
 
-			get permissions(): PermissionMap {
+			get permissions(): PermissionMap<AvailablePermissions> {
 				return new Map()
 			}
 		}
