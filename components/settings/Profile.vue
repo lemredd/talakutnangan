410d075e68b12780be73a6464a723e9d2ac4ff7a
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-06-11 00:09:16 +0800

    feat(user): implement uploading of pictures

--- components/settings/Profile.vue
+++ components/settings/Profile.vue
@@ -7,9 +7,12 @@
 		<div class ="p-5">
 			<h3 class="display-name text-lg">Profile Name</h3>
 			<div class="picture-container p-1 w-35 h-35 rounded-0.8rem bg-dark-100 flex justify-center">
-				<img v-if="profileInfo.profilePic" src="">
+				<img v-if="profileInfo.profilePic" :src="profileInfo.profilePic">
 				<div class="no-image flex flex-col items-center justify-center" v-else>
-					<button class="material-icons">add_circle</button>
+					<label for="input-profile-pic" >
+						<span class="material-icons">add_circle</span>
+						<input type="file" accept="image/" id="input-profile-pic" class="input-pic" @change="loadImage($event, 'profilePic')">
+					</label>
 					<small class="text-center">
 						Drag and drop or upload image
 					</small>
@@ -19,9 +22,12 @@
 		<div class ="p-5">
 			<h3 class="display-name text-lg">Profile Name</h3>
 			<div class="picture-container p-1 w-35 h-35 rounded-0.8rem bg-dark-100 flex justify-center">
-				<img v-if="profileInfo.signature" src="">
+				<img v-if="profileInfo.signature" :src="profileInfo.signature">
 				<div class="no-image flex flex-col items-center justify-center" v-else>
-					<button class="material-icons">add_circle</button>
+					<label for="input-signature" >
+						<span class="material-icons">add_circle</span>
+						<input type="file" accept="image/" id="input-signature" class="input-pic" @change="loadImage($event, 'signature')">
+					</label>
 					<small class="text-center">
 						Drag and drop or upload image
 					</small>
@@ -30,7 +36,7 @@
 		</div>
 		<div class ="p-5 dark-mode-toggle">
 			<h3 class="display-name text-lg col-span-full">Dark Mode</h3>
-			<p class="name">Lorem5</p>
+			<p class="name">Click to toggle dark mode</p>
 			<label for="dark-mode-toggle">
 				<span class="material-icons-outlined">
 					{{ `toggle_${isDarkModeEnabled ? "on" : "off"}` }}
@@ -46,7 +52,13 @@ form {
 	display: flex;
 	flex-direction: column;
 	max-width: 480px;
+
+	.input-pic {
+		display: none;
+	}
 }
+
+
 
 .dark-mode-toggle {
 	display: grid;
@@ -76,7 +88,6 @@ form {
 
 <script setup lang="ts">
 import { ref } from "vue"
-const isDarkModeEnabled = ref(true)
 
 
 const { userInfo } = defineProps<{
@@ -86,7 +97,17 @@ const { userInfo } = defineProps<{
 }>()
 const profileInfo = userInfo.profile
 
+const isDarkModeEnabled = ref(true)
+
 function toggleDarkMode() {
 	isDarkModeEnabled.value = !isDarkModeEnabled.value
+}
+
+function loadImage(e, type) {
+	const [file] = e.target.files
+	const fileObjectURL = URL.createObjectURL(file)
+
+	if (type === "signature") profileInfo.signature = fileObjectURL
+	if (type === "profilePic") profileInfo.profilePic = fileObjectURL
 }
 </script>
