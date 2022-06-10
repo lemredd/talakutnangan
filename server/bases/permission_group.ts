64b33ce2287e965ca7ae254a0b3352995e716fb1
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-10 13:42:28 +0800

    feat(server): accept unions in permission groups for validation

--- server/bases/permission_group.ts
+++ server/bases/permission_group.ts
@@ -5,7 +5,7 @@ import type { PermissionMap } from "!/types/independent"
  *
  * Useful to check if a certain operation is safe to be executed.
  */
-export default abstract class {
+export default abstract class<T> {
 	/**
 	 * Name of the permission group
 	 */
@@ -14,7 +14,7 @@ export default abstract class {
 	/**
 	 * Map of permissions under the group
 	 */
-	abstract get permissions(): PermissionMap
+	abstract get permissions(): PermissionMap<T>
 
 	/**
 	 * Returns true if role is permitted. Otherwise, false.
@@ -24,7 +24,7 @@ export default abstract class {
 	 * @param role An object which contains flags named under a permission group name.
 	 * @param permissionName Name of the permission to check if it is allowed in the role.
 	 */
-	mayAllow(role: object, permissionName: string): boolean {
+	mayAllow(role: object, permissionName: T): boolean {
 		if (this.name in role) {
 			// TODO: Create a recursive function to check the flags
 			return false
