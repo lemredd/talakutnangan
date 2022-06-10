d702827d09ce975d71956ebf276f8fdacb3c9c71
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-10 23:37:53 +0800

    feat(server): rename method to return the policy

--- server/app/routes/chat/room(uuid).get.ts
+++ server/app/routes/chat/room(uuid).get.ts
@@ -1,9 +1,9 @@
 import PageMiddleware from "!/bases/page_middleware"
-import Middleware from "!/bases/middleware"
+import Policy from "!/bases/policy"
 import CommonMiddlewareList from "!/middlewares/common_middleware_list"
 
 export default class extends PageMiddleware {
 	get filePath(): string { return __filename }
 
-	get middleware(): Middleware { return CommonMiddlewareList.knownOnlyPolicy }
+	get policy(): Policy { return CommonMiddlewareList.knownOnlyPolicy }
 }
--- server/bases/page_middleware.ts
+++ server/bases/page_middleware.ts
@@ -1,18 +1,18 @@
 import { Request, Response, NextFunction } from "!/types/dependent"
 
-import Middleware from "!/bases/middleware"
+import Policy from "!/bases/policy"
 import ControllerLike from "!/bases/controller-like"
 
 export default abstract class extends ControllerLike {
 	/**
-	 * Returns the middleware that will enhance a certain page route. It is treated as the main
+	 * Returns the policy that will enhance a certain page route. It is treated as the main
 	 * handler.
 	 */
-	abstract get middleware(): Middleware
+	abstract get policy(): Policy
 
 	get endHandler(): null { return null }
 
 	async intermediate(request: Request, response: Response, next: NextFunction): Promise<void> {
-		return this.middleware.intermediate(request, response, next)
+		return this.policy.intermediate(request, response, next)
 	}
 }
