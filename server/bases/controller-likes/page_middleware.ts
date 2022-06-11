82eec44acccaba2ed434676188db42b80eb66983
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-12 00:40:53 +0800

    feat(server): rewrite page middleware

--- server/bases/controller-likes/page_middleware.ts
+++ server/bases/controller-likes/page_middleware.ts
@@ -1,18 +1,17 @@
 import { Request, Response, NextFunction } from "!/types/dependent"
 
-import Policy from "!/bases/policy"
+import Validation from "!/bases/validation"
 import ControllerLike from "!/bases/controller-like"
 
 export default abstract class extends ControllerLike {
-	/**
-	 * Returns the policy that will enhance a certain page route. It is treated as the main
-	 * handler.
-	 */
-	abstract get policy(): Policy
-
 	get endHandler(): null { return null }
 
+	get bodyParser(): null { return null }
+
+	get validations(): Validation[] { return [] }
+
 	async intermediate(request: Request, response: Response, next: NextFunction): Promise<void> {
-		return this.policy.intermediate(request, response, next)
+		// TODO: Pass the details to be used by client
+		next()
 	}
 }
