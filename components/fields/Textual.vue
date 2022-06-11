5da2bac0e5a40b3c3304ce12057bcd24f74aeec0
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-06-11 14:30:11 +0800

    feat(user): make overlay

--- components/fields/Textual.vue
+++ components/fields/Textual.vue
@@ -21,6 +21,15 @@
 			</button>
 	</div>
 
+	<div class="overlay">
+		<div class="content bg-white text-black">
+			<header>Header</header>
+			<main>
+				123
+				456
+			</main>
+		</div>
+	</div>
 </template>
 
 <style scoped lang="scss">
@@ -38,6 +47,24 @@
 	}
 }
 
+.overlay {
+	position:fixed;
+	inset: 0;
+	height: 100vh;
+	width: 100%;
+
+	background-color: rgba(0,0,0,0.3);
+
+	.content {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		padding: 1em;
+		width: max-content;
+		height: max-content;
+	}
+}
 </style>
 
 <script setup lang="ts">
