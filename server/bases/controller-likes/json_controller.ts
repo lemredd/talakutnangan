6818c940cc89ca3fdf9facfa6dfd9d127cae3eee
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-12 00:36:08 +0800

    feat(server): rewrite JSON controller

--- server/bases/controller-likes/json_controller.ts
+++ server/bases/controller-likes/json_controller.ts
@@ -1,13 +1,16 @@
 import Middleware from "!/bases/middleware"
-import Controller from "!/bases/controller-like/controller"
+import Validation from "!/bases/validation"
+import Controller from "!/bases/controller-likes/controller"
 import CommonMiddlewareList from "!/middlewares/common_middleware_list"
 import BodyValidation from "!/middlewares/authorization/body_validation"
 
 export default abstract class extends Controller {
-	get middlewares(): Middleware[] {
+	get bodyParser(): Middleware {
+		return CommonMiddlewareList.JSONBody
+	}
+
+	get validations(): Validation[] {
 		return [
-			...super.middlewares,
-			CommonMiddlewareList.JSONBody,
 			new BodyValidation(this.bodyValidationRules)
 		]
 	}
