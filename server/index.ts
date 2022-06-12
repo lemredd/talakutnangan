8f4b3ccf9aeb46f31fc77c55f8d08734595f65a4
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-12 19:30:27 +0800

    feat(logger): log the initialization of HTTP server as successful

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,16 @@
 import "reflect-metadata"
 import "dotenv/config"
 
-import { Server as HTTPServer } from "http"
+import { Server as HTTPServer } from "node:http"
 
+import { SourceType } from "%/types"
+
+import Log from "!/helpers/log"
 import createWSServer from "!/ws/create_server"
 import Router from "!/app/routes/router_manager"
 import createAppHandler from "!/app/create_handler"
-import initializeSingletons from "!/helpers/initialize_singletons"
-
-import { SourceType } from "%/types"
 import createDataSource from "%/data_source/create_source"
+import initializeSingletons from "!/helpers/initialize_singletons"
 
 startServer()
 
@@ -25,5 +26,5 @@ async function startServer() {
 
 	const port = process.env.WEB_PORT || 3000
 	httpServer.listen(port)
-	console.log(`Server running at http://localhost:${port}`)
+	Log.success("server", `HTTP server running at http://localhost:${port}`)
 }
