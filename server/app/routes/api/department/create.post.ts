b97ab2995f990b48ed4ddc5741c2cbaa77928b99
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-12 15:15:31 +0800

    refactor: rewrite the department route to create

--- server/app/routes/api/department/create.post.ts
+++ server/app/routes/api/department/create.post.ts
@@ -1,18 +1,16 @@
 import { Request, Response } from "!/types/dependent"
 
-import Middleware from "!/bases/middleware"
-import JSONController from "!/bases/controller-likes/json_controller-like/controller"
+import Policy from "!/bases/policy"
 import DepartmentManager from "%/managers/department_manager"
+import JSONController from "!/common_controllers/json_controller"
 import CommonMiddlewareList from "!/middlewares/common_middleware_list"
 
 export default class extends JSONController {
 	get filePath(): string { return __filename }
 
-	get middlewares(): Middleware[] {
-		return [
-			CommonMiddlewareList.knownOnlyPolicy,
-			...super.middlewares
-		]
+	get policy(): Policy {
+		// TODO: Use a permission-based policy
+		return CommonMiddlewareList.knownOnlyPolicy
 	}
 
 	get bodyValidationRules(): object {
