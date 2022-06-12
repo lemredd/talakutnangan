fdcc8f8c7e76edb41f955f58c36806ec90a4853e
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-12 14:05:49 +0800

    feat(server): rewrite user update route to only update user details

--- server/app/routes/api/user/update(id).patch.ts
+++ server/app/routes/api/user/update(id).patch.ts
@@ -1,54 +1,24 @@
-import type { IDParameter, Query, Request as BaseRequest, Response } from "!/types/dependent"
+import type { AuthenticatedIDRequest, Response } from "!/types/dependent"
 
-import Controller from "!/bases/controller-like/controller"
-import Middleware from "!/bases/middleware"
+import Policy from "!/bases/policy"
 import UserManager from "%/managers/user_manager"
+import ModelBoundController from "!/specialized/model_bound_controller"
 import CommonMiddlewareList from "!/middlewares/common_middleware_list"
 
-interface ExpectedQuery extends Query {
-	confirm: string
-}
-
-interface WithUpdate {
-	params: IDParameter,
-	query: ExpectedQuery
-}
-
-export type Request = BaseRequest & WithUpdate
-
-export default class extends Controller {
+export default class extends ModelBoundController {
 	get filePath(): string { return __filename }
 
-	get middlewares(): Middleware[] {
-		return [
-			...super.middlewares,
-			CommonMiddlewareList.knownOnlyPolicy
-		]
+	get policy(): Policy {
+		// TODO: Base the policy from permission
+		return CommonMiddlewareList.knownOnlyPolicy
 	}
 
-	async handle(request: Request, response: Response): Promise<void> {
+	async handle(request: AuthenticatedIDRequest, response: Response): Promise<void> {
 		const manager = new UserManager()
 		const { id } = request.params
-		const { confirm = "0" } = request.query
 
-		// TODO: Make parameter validation
+		// TODO: Update user details
 
-		// TODO: Check if the user can admit
-		if (confirm) {
-			// TODO: Check if within the department
-			const affectedCount = await manager.admit(+id, true)
-
-			response.status(affectedCount > 0 ? this.status.ACCEPTED : this.status.NOT_MODIFIED).end()
-
-			// ?: This code does not work for some reason. This is why manual checking is needed
-			// await manager.update(
-			// 	User,
-			// 	{ id: +id, admittedAt: null },
-			// 	{ admittedAt: (new Date()).toISOString() }
-			// )
-		} else {
-			// TODO: Update user details
-			response.status(this.status.INTERNAL_SERVER_ERROR)
-		}
+		response.status(this.status.NOT_IMPLEMENTED)
 	}
 }
