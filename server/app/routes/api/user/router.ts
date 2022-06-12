074299a243f4126a4c7fc3ad3da56091c1a56dc7
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-12 13:13:59 +0800

    feat(server): remove register route

--- server/app/routes/api/user/router.ts
+++ server/app/routes/api/user/router.ts
@@ -3,7 +3,6 @@ import GetList from "!/app/routes/api/user/list.get"
 import PostLogIn from "!/app/routes/api/user/log_in.post"
 import PostImport from "!/app/routes/api/user/import.post"
 import PostLogOut from "!/app/routes/api/user/log_out.post"
-import PostRegister from "!/app/routes/api/user/register.post"
 import PatchUpdate from "!/app/routes/api/user/update(id).patch"
 import GetLogInFailure from "!/app/routes/api/user/log_in_failure.get"
 import PatchResetPassword from "!/app/routes/api/user/reset_password(id).patch"
@@ -18,7 +17,6 @@ export default class extends Router {
 			new PostImport(),
 			new PostLogOut(),
 			new PatchUpdate(),
-			new PostRegister(),
 			new GetLogInFailure(),
 			new PatchResetPassword()
 		])
