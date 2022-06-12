49588fa25f4e79106965e209fc38965e4ccf2840
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-12 15:23:18 +0800

    feat(server): add policy to join chat room

--- server/app/routes/chat/join.get.ts
+++ server/app/routes/chat/join.get.ts
@@ -2,16 +2,23 @@ import { v4 } from "uuid"
 
 import { Request, Response } from "!/types/dependent"
 
-import Controller from "!/bases/controller-like/controller"
+import Policy from "!/bases/policy"
+import Validation from "!/bases/validation"
+import Controller from "!/bases/controller-likes/controller"
+import CommonMiddlewareList from "!/middlewares/common_middleware_list"
 
 export default class extends Controller {
 	get filePath(): string { return __filename }
+
+	get policy(): Policy { return CommonMiddlewareList.knownOnlyPolicy }
+
+	get bodyParser(): null { return null }
+
+	get validations(): Validation[] { return [] }
 
 	async handle(request: Request, response: Response): Promise<void> {
 		response.writeHead(this.status.MOVED_TEMPORARILY, {
 			Location: `/chat/room/${v4()}`
 		})
-
-		response.end()
 	}
 }
