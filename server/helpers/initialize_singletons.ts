de882e261a6ce61a5306f13d6966f6a7f87e5657
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-12 23:27:27 +0800

    fix(server): change the mechanism of checking e-mail variables

--- server/helpers/initialize_singletons.ts
+++ server/helpers/initialize_singletons.ts
@@ -1,22 +1,26 @@
 import Transport from "!/helpers/email/transport";
+import RequestEnvironment from "!/helpers/request_environment"
 import CommonMiddlewareList from "!/middlewares/common_middleware_list";
 
 export default function() {
 	if (
-		process.env.EMAIL_HOST === undefined
-		|| process.env.EMAIL_PORT === undefined
-		|| process.env.EMAIL_USER === undefined
-		|| process.env.EMAIL_PASS === undefined
+		process.env.EMAIL_HOST !== undefined
+		&& process.env.EMAIL_PORT !== undefined
+		&& process.env.EMAIL_USER !== undefined
+		&& process.env.EMAIL_PASS !== undefined
 	) {
-		console.error("Some e-mail variables are not defined");
-		process.exit()
+		Transport.initialize(
+			process.env.EMAIL_HOST,
+			+process.env.EMAIL_PORT,
+			process.env.EMAIL_USER,
+			process.env.EMAIL_PASS
+		)
+		CommonMiddlewareList.initialize()
+	} else {
+		if (!RequestEnvironment.isOnTest) {
+			console.error("Some e-mail variables are not defined");
+			process.exit(1)
+		}
 	}
 
-	Transport.initialize(
-		process.env.EMAIL_HOST,
-		+process.env.EMAIL_PORT,
-		process.env.EMAIL_USER,
-		process.env.EMAIL_PASS
-	)
-	CommonMiddlewareList.initialize()
 }
