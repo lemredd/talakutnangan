ca0677167d08c39168959db4202bbfe5c0d29836
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-12 14:07:04 +0800

    feat(server)!: change the HTTP method to use to update the user

--- server/app/routes/api/user/router.ts
+++ server/app/routes/api/user/router.ts
@@ -3,7 +3,7 @@ import GetList from "!/app/routes/api/user/list.get"
 import PostLogIn from "!/app/routes/api/user/log_in.post"
 import PostImport from "!/app/routes/api/user/import.post"
 import PostLogOut from "!/app/routes/api/user/log_out.post"
-import PatchUpdate from "!/app/routes/api/user/update(id).patch"
+import PutUpdate from "!/app/routes/api/user/update(id).put"
 import GetLogInFailure from "!/app/routes/api/user/log_in_failure.get"
 import PatchResetPassword from "!/app/routes/api/user/reset_password(id).patch"
 
@@ -14,9 +14,9 @@ export default class extends Router {
 		this.useControllers([
 			new GetList(),
 			new PostLogIn(),
+			new PutUpdate(),
 			new PostImport(),
 			new PostLogOut(),
-			new PatchUpdate(),
 			new GetLogInFailure(),
 			new PatchResetPassword()
 		])
