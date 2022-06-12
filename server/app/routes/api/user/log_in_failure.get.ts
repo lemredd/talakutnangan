2a403cf5f29093912437007219e2c5f4887f6e29
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-12 12:54:00 +0800

    feat(server): add policy to log in failure route

--- server/app/routes/api/user/log_in_failure.get.ts
+++ server/app/routes/api/user/log_in_failure.get.ts
@@ -1,15 +1,20 @@
 import { Request, Response } from "!/types/dependent"
 
-import Controller from "!/bases/controller-like/controller"
+import Policy from "!/bases/policy"
+import Validation from "!/bases/validation"
+import Controller from "!/bases/controller-likes/controller"
+import CommonMiddlewareList from "!/middlewares/common_middleware_list"
 
 export default class extends Controller {
 	get filePath(): string { return __filename }
 
+	get policy(): Policy { return CommonMiddlewareList.guestOnlyPolicy }
+
+	get bodyParser(): null { return null }
+
+	get validations(): Validation[] { return [] }
+
 	async handle(request: Request, response: Response): Promise<void> {
-		response.status(this.status.UNAUTHORIZED).json({
-			email: [
-				"User cannot be found"
-			]
-		})
+		response.status(this.status.UNAUTHORIZED)
 	}
 }
