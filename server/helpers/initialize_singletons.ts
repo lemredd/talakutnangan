08fbc33156a7745d9f890d66e2d09624509d65ff
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-12 16:03:13 +0800

    feat(server): terminate if one of the e-mail variables is not defined

--- server/helpers/initialize_singletons.ts
+++ server/helpers/initialize_singletons.ts
@@ -2,6 +2,16 @@ import Transport from "!/helpers/email/transport";
 import CommonMiddlewareList from "!/middlewares/common_middleware_list";
 
 export default function() {
+	if (
+		process.env.EMAIL_HOST === undefined
+		|| process.env.EMAIL_PORT === undefined
+		|| process.env.EMAIL_USER === undefined
+		|| process.env.EMAIL_PASS === undefined
+	) {
+		console.error("Some e-mail variables are not defined");
+		process.exit()
+	}
+
 	Transport.initialize(
 		process.env.EMAIL_HOST,
 		+process.env.EMAIL_PORT,
