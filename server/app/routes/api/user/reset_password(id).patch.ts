bc6d895d7284ab117dc3b51c9386dc2937290c1f
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-12 14:01:10 +0800

    feat(server): add validation to reset password route

--- server/app/routes/api/user/reset_password(id).patch.ts
+++ server/app/routes/api/user/reset_password(id).patch.ts
@@ -1,30 +1,20 @@
-import type { IDParameter, Request as BaseRequest, Response } from "!/types/dependent"
+import type { AuthenticatedIDRequest, Response } from "!/types/dependent"
 
-import Controller from "!/bases/controller"
-import Middleware from "!/bases/middleware"
+import Policy from "!/bases/policy"
 import UserManager from "%/managers/user_manager"
+import ModelBoundController from "!/specialized/model_bound_controller"
 import CommonMiddlewareList from "!/middlewares/common_middleware_list"
 
-interface WithUpdate {
-	params: IDParameter
-}
-
-export type Request = BaseRequest & WithUpdate
-
-export default class extends Controller {
+export default class extends ModelBoundController {
 	get filePath(): string { return __filename }
 
-	get middlewares(): Middleware[] {
-		return [
-			...super.middlewares,
-			// TODO: Use policy to that authenticates overall level update
-			CommonMiddlewareList.knownOnlyPolicy
-		]
+	get policy(): Policy {
+		// TODO: Use policy to that authenticates overall level update
+		return CommonMiddlewareList.knownOnlyPolicy
 	}
 
-	async handle(request: Request, response: Response): Promise<void> {
+	async handle(request: AuthenticatedIDRequest, response: Response): Promise<void> {
 		const manager = new UserManager()
-		// TODO: Make parameter validation
 		const { id } = request.params
 		// TODO: Generate password using student number or random
 		const newPassword = "12345678"
