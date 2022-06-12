6086fbdf8005a53fd2e6c9972a6a9a25bbc0ad65
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-12 18:49:30 +0800

    feat(logger): log the initialization of web socket as successful

--- server/ws/create_server.ts
+++ server/ws/create_server.ts
@@ -1,11 +1,13 @@
 import { Server } from "http"
 import { Server as WebSocketServer } from "socket.io"
-import registerChatEvents from "./register_chat_events"
+
+import Log from "!/helpers/log"
+import registerChatEvents from "!/ws/register_chat_events"
 
 export default function(httpServer: Server) {
 	const wsServer = new WebSocketServer(httpServer)
 
-	console.log("Initialized web socket server")
+	Log.success("Server", "Initialized web socket server")
 
 	registerChatEvents(wsServer)
 
