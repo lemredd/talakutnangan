568ff17edaab28be03867e890f048be52c5a7f52
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-12 19:44:28 +0800

    chore: make creation of database as asynchronous

--- database/data_source/create_source.ts
+++ database/data_source/create_source.ts
@@ -6,7 +6,7 @@ import User from "%/models/user"
 import Department from "%/models/department"
 import createConfiguration from "%/configuration/create"
 
-export default function(type: SourceType): Sequelize {
+export default async function(type: SourceType): Promise<Sequelize> {
 	const configuration: SequelizeOptions = createConfiguration(type) as SequelizeOptions
 	const sequelize = new Sequelize({
 		...configuration,
