c99b3c95e69340d1d7962a6399704c70c8e549e5
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-12 12:19:53 +0800

    chore: dedicate directory for specialized classes

--- server/specialized/json_controller.ts
+++ server/specialized/json_controller.ts
@@ -5,7 +5,7 @@ import CommonMiddlewareList from "!/middlewares/common_middleware_list"
 import BodyValidation from "!/middlewares/authorization/body_validation"
 
 /**
- * Base class for controllers which accept JSON as their request body.
+ * Specialized controller class which accept JSON as their request body.
  */
 export default abstract class extends Controller {
 	get bodyParser(): Middleware {
