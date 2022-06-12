0a74b204e6ed756295105d8fe01e3e65ae482bf1
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-13 00:28:55 +0800

    feat(logger): force passed error is the expected type

--- database/data_source/create_source.ts
+++ database/data_source/create_source.ts
@@ -25,7 +25,7 @@ export default async function(type: SourceType): Promise<Sequelize> {
 		Log.success("server", "Connected to the database server")
 	} catch(error) {
 		Log.errorMessage("server", "Cannot connect to the database")
-		Log.error("server", error)
+		Log.error("server", error as Error)
 	}
 
 	return sequelize
