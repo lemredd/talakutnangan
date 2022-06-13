a23eecc9e515c287e4cf14df5252354a5c7c7e43
Angelo Magtoto <19201.magtoto.angelo.l@gmail.com>
2022-06-13 17:53:30 +0800

    feat(forum): added dropdown for posts

--- pages/forum/index.page.vue
+++ pages/forum/index.page.vue
@@ -14,8 +14,35 @@
             <br/>
 
             <div class="post" v-for="post in posts" v-bind:key="post.id" :hidden="secludePostDiv(post)">
-                <div class="container">
+
+                 <div class="post-container flex justify-between w-[100%] pb-[5em]">
+                    <div class="post-title">
+						<h2 class="font-bold">
+							Post 
+                        </h2>
+                    </div>
+                    <div class="">
+
+                    </div>
+                    <div class="controls relative">
+						<button class="material-icons" @click="togglePostMenu">more_vert</button>
+						<PostMenu class="postmenu absolute top-[2em] right-0 flex flex-col" v-if="isPostMenuShown">
+                            <button v-if="dummyUser===post.user">
+                                Edit
+                            </button>
+                            <button v-if="dummyUser===post.user">
+                                Delete
+                            </button>
+                            <button v-if="dummyUser!==post.user">
+                                Report
+                            </button>
+                        </PostMenu>
+                    </div>
+
+                </div>
+                <div class="post-container">
                     <div class="left">
+                        <div><img src="./images/emptyUser.png"></div>
                         <h2 class = "title">
                         {{ post.user }}
                         </h2>
@@ -24,7 +51,7 @@
                         <h2 class = "title">
                         {{ post.title }}
                         {{ post.badWordExist() }}
-                        
+
                         </h2>
                     </div>
                     <div class="right">
@@ -71,15 +98,27 @@
 </style>
 
 <script setup lang="ts">
-import { posts } from "./post";
-import { secludedPosts } from "./post";
-import { voteCountUpdate } from "./post";
-import { determineUserVoted } from "./post";
-import { upVote } from "./post";
-import { downVoteCountUpdate } from "./post";
-import { determineUserDownVoted } from "./post";
-import { downVote } from "./post";
-import { totalVotes } from "./post";
-import { secludePostDiv } from "./post";
+import { ref } from "vue"
+import {
+	posts,
+    secludedPosts,
+    voteCountUpdate,
+    determineUserVoted,
+    upVote,
+    downVoteCountUpdate,
+    determineUserDownVoted,
+    downVote,
+    totalVotes,
+    secludePostDiv,
+    dummyUser
+} from "./post";
+import PostMenu from "@/Dropdown.vue";
 
+
+var isPostMenuShown = ref(false);
+
+function togglePostMenu()
+{
+    isPostMenuShown.value=!isPostMenuShown.value;
+}
 </script>
