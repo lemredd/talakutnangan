0a94d5168953489c574400a49b445c4029d1825b
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-14 08:13:44 +0800

    types: create type file for dependent types

---
+++ database/types/dependent.ts
@@ -0,0 +1,7 @@
+/**
+ * @module DatabaseDependentTypes
+ * @description This module exports types that are independent from any third-party packages.
+ */
+import { FindAndCountOptions as BaseFindAndCountOptions } from "sequelize"
+
+export type FindAndCountOptions = BaseFindAndCountOptions
