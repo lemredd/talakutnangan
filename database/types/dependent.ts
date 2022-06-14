79e9a02c5e120523d5501a56d829c00b216ea3ec
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-14 21:58:33 +0800

    types(database): re-export used types from other packages

--- database/types/dependent.ts
+++ database/types/dependent.ts
@@ -2,6 +2,16 @@
  * @module DatabaseDependentTypes
  * @description This module exports types that are independent from any third-party packages.
  */
-import { FindAndCountOptions as BaseFindAndCountOptions } from "sequelize"
+import {
+	FindOptions as BaseFindOptions,
+	FindAndCountOptions as BaseFindAndCountOptions
+} from "sequelize"
+import type {
+	Model as BaseModel,
+	ModelCtor as BaseModelCtor
+} from "sequelize-typescript"
 
-export type FindAndCountOptions = BaseFindAndCountOptions
+export type Model = BaseModel
+export type FindOptions<T> = BaseFindOptions<T>
+export type ModelCtor<T extends BaseModel> = BaseModelCtor<T>
+export type FindAndCountOptions<T> = BaseFindAndCountOptions<T>
