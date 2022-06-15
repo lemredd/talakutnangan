31299662db27cb1ea19c789d46b50bab9c3c5cf5
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-15 16:18:22 +0800

    feat(permissions): make comment permission group

---
+++ server/app/auth/comment_permissions.ts
@@ -0,0 +1,60 @@
+import type {
+	PermissionMap,
+	PermissionInfo,
+	LevelPermission,
+	OperationPermission
+} from "!/types/independent"
+
+import {
+	VIEW,
+	CREATE,
+	UPDATE,
+	WRITE_OWN_SCOPE,
+	ARCHIVE_AND_RESTORE,
+	WRITE_OVERALL_SCOPE,
+	READ_OVERALL_SCOPE,
+	READ_DEPARTMENT_SCOPE,
+	WRITE_DEPARTMENT_SCOPE
+} from "!/types/independent"
+
+import PermissionGroup from "!/bases/permission_group"
+
+const commentColumnName = "comment_flags"
+
+type CommentFlags = { [commentColumnName]: number }
+type Permissions =
+	| OperationPermission
+	| Exclude<LevelPermission, "readOwnScope">
+	| "vote"
+
+/**
+ * Permission group for comments.
+ *
+ * This is safe to use in client-side.
+ */
+export default class extends PermissionGroup<CommentFlags, Permissions> {
+	get name(): string { return commentColumnName }
+
+	get permissions(): PermissionMap<Permissions> {
+		return new Map<Permissions, PermissionInfo<Permissions>>([
+			[ "view",              { flag: VIEW, permissionDependencies: [] } ],
+			[ "create",            { flag: CREATE, permissionDependencies: [ "view" ] } ],
+			[ "update",            { flag: UPDATE, permissionDependencies: [ "view" ] } ],
+			[ "archiveAndRestore", { flag: ARCHIVE_AND_RESTORE, permissionDependencies: [ "view" ] } ],
+
+			[ "readDepartmentScope",  { flag: READ_DEPARTMENT_SCOPE, permissionDependencies: [ ] } ],
+			[ "readOverallScope",     { flag: READ_OVERALL_SCOPE, permissionDependencies: [ ] } ],
+			[ "writeOwnScope",        { flag: WRITE_OWN_SCOPE, permissionDependencies: [ ] } ],
+			[ "writeDepartmentScope", {
+				flag: WRITE_DEPARTMENT_SCOPE,
+				permissionDependencies: [ "readDepartmentScope" ]
+			} ],
+			[ "writeOverallScope", {
+				flag: WRITE_OVERALL_SCOPE,
+				permissionDependencies: [ "readOverallScope" ]
+			} ],
+
+			[ "vote", { flag: 0x0100, permissionDependencies: [ "view" ] } ],
+		])
+	}
+}
