1fa5c4f70ed0a50d8dd84ee8600aad754d097cde
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-06-15 20:19:10 +0800

    test(navigation): localize wrappers

--- components/PageShell/RoleSpecificLinks.spec.ts
+++ components/PageShell/RoleSpecificLinks.spec.ts
@@ -1,11 +1,10 @@
-import { mount, VueWrapper } from "@vue/test-utils"
+import { mount } from "@vue/test-utils"
 import RoleSpecificLinks from "./RoleSpecificLinks.vue"
-
-let wrapper: VueWrapper
+import { ref } from "vue"
 
 describe("Component: Page Shell/Role Specific Links", () => {
-	beforeAll(() => {
-		wrapper = mount(RoleSpecificLinks, {
+	it("should have a menu button that is clickable", async () => {
+		const wrapper = mount(RoleSpecificLinks, {
 			shallow: true,
 			global: {
 				stubs: {
@@ -16,9 +15,23 @@ describe("Component: Page Shell/Role Specific Links", () => {
 				role: "student_or_employee"
 			}
 		})
+		const menuButton = wrapper.find("#menu-btn")
+		await menuButton.trigger("click")
+
+		expect(wrapper.emitted()).toHaveProperty("click")
 	})
 
-	it("should have a menu button that is clickable", async () => {
+	it("should specify the right link/s for a non-guest role", async () => {
+		const wrapper = mount(RoleSpecificLinks, {
+			shallow: true,
+			global: {
+				stubs: {
+					RoleLinksList: false
+				},
+			},
+			props: {
+				role: "student_or_employee"
+			}
 		const menuButton = wrapper.find("#menu-btn")
 		await menuButton.trigger("click")
 
@@ -26,6 +39,16 @@ describe("Component: Page Shell/Role Specific Links", () => {
 	})
 
 	it("should specify the right link/s for a specific role", async () => {
+		const wrapper = mount(RoleSpecificLinks, {
+			shallow: true,
+			global: {
+				stubs: {
+					RoleLinksList: false
+				},
+			props: {
+				role: "guest"
+			}
+		})
 		const link = wrapper.find("link-stub:first-of-type")
 		const linkHref = link.attributes("href")
 		const specifiedRole = wrapper.props().role
