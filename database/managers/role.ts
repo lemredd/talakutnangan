4ea92a64778aa7ae4218516c801c6a007380e139
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-15 11:48:36 +0800

    feat(database): simplify the role model manager

--- database/managers/role.ts
+++ database/managers/role.ts
@@ -1,66 +1,20 @@
-import { Op } from "sequelize"
-
 import type { ModelCtor, FindAndCountOptions } from "%/types/dependent"
 import type { CommonConstraints, RawRole, Pipe } from "%/types/independent"
 
 import Role from "%/models/role"
 import BaseManager from "%/managers/base"
+import limit from "%/managers/helpers/limit"
 import offset from "%/managers/helpers/offset"
+import search_name from "%/managers/helpers/search_name"
 
 export default class extends BaseManager<Role, RawRole> {
 	get model(): ModelCtor<Role> { return Role }
 
 	get listPipeline(): Pipe<FindAndCountOptions<Role>, CommonConstraints>[] {
 		return [
-			offset
-		]
-	}
-
-	async search(
-		query: string,
-		offset: number,
-		limit: number
-	): Promise<{ roles: Role[], count: number }> {
-		const { rows, count } = await Role.findAndCountAll({
-			where: {
-				name: {
-					[Op.like]: `%${query}%`
-				}
-			},
+			search_name,
 			offset,
 			limit
-		})
-
-		return { roles: rows, count }
-	}
-
-	async create(details: RawRole): Promise<Role> {
-		return await Role.create({ ...details })
-	}
-
-	async update(id: number, details: RawRole): Promise<number> {
-		const [ affectedCount ] = await Role.update({
-			...details
-		}, {
-			where: { id }
-		})
-
-		return affectedCount
-	}
-
-	async archive(id: number): Promise<number> {
-		const deleteCount = await Role.destroy({
-			where: { id }
-		})
-
-		return deleteCount
-	}
-
-	async restore(id: number): Promise<void> {
-		await Role.restore({
-			where: {
-				id
-			}
-		})
+		]
 	}
 }
