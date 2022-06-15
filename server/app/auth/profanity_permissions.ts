062c76d84445c8ea2488b2c324ce1dec64395f65
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-15 16:23:17 +0800

    feat(permissions): make profanity permission group

---
+++ server/app/auth/profanity_permissions.ts
@@ -0,0 +1,48 @@
+import type {
+	PermissionMap,
+	PermissionInfo,
+	LevelPermission,
+	OperationPermission
+} from "!/types/independent"
+
+import {
+	VIEW,
+	CREATE,
+	UPDATE,
+	ARCHIVE_AND_RESTORE,
+	WRITE_OVERALL_SCOPE,
+	READ_OVERALL_SCOPE
+} from "!/types/independent"
+
+import PermissionGroup from "!/bases/permission_group"
+
+const profanityColumnName = "profanity_flags"
+
+type ProfanityFlags = { [profanityColumnName]: number }
+type Permissions =
+	| OperationPermission
+	| Extract<LevelPermission, "readOverallScope" | "writeOverallScope">
+
+/**
+ * Permission group for profanity.
+ *
+ * This is safe to use in client-side.
+ */
+export default class extends PermissionGroup<ProfanityFlags, Permissions> {
+	get name(): string { return profanityColumnName }
+
+	get permissions(): PermissionMap<Permissions> {
+		return new Map<Permissions, PermissionInfo<Permissions>>([
+			[ "view",              { flag: VIEW, permissionDependencies: [] } ],
+			[ "create",            { flag: CREATE, permissionDependencies: [ "view" ] } ],
+			[ "update",            { flag: UPDATE, permissionDependencies: [ "view" ] } ],
+			[ "archiveAndRestore", { flag: ARCHIVE_AND_RESTORE, permissionDependencies: [ "view" ] } ],
+
+			[ "readOverallScope",     { flag: READ_OVERALL_SCOPE, permissionDependencies: [ ] } ],
+			[ "writeOverallScope", {
+				flag: WRITE_OVERALL_SCOPE,
+				permissionDependencies: [ "readOverallScope" ]
+			} ]
+		])
+	}
+}
