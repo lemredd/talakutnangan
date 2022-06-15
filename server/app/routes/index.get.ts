9b5ae5014a1b26a52baee575221bbc2107261f41
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-15 08:12:52 +0800

    feat(server): create a sample payload in the index page middleware

--- server/app/routes/index.get.ts
+++ server/app/routes/index.get.ts
@@ -1,7 +1,15 @@
+import { Request } from "!/types/dependent"
+import { Serializable } from "%/types/independent"
 import PageMiddleware from "!/bases/controller-likes/page_middleware"
 
 export default class extends PageMiddleware {
 	get filePath(): string { return __filename }
 
 	get policy(): null { return null }
+
+	getClientPayload(request: Request): Serializable {
+		return {
+			data: "Hello world!"
+		}
+	}
 }
