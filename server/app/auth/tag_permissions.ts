5e354d32b8de2d06ade1514d60d1d6089fbb4bae
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-15 16:06:30 +0800

    feat(permissions): make tag permission group

---
+++ server/app/auth/tag_permissions.ts
@@ -0,0 +1,37 @@
+import type {
+	PermissionMap,
+	PermissionInfo,
+	OperationPermission,
+} from "!/types/independent"
+
+import {
+	VIEW,
+	CREATE,
+	UPDATE,
+	ARCHIVE_AND_RESTORE
+} from "!/types/independent"
+
+import PermissionGroup from "!/bases/permission_group"
+
+const tagColumnName = "tag_flags"
+
+type TagFlags = { [tagColumnName]: number }
+type Permissions = OperationPermission
+
+/**
+ * Permission group for tag.
+ *
+ * This is safe to use in client-side.
+ */
+export default class extends PermissionGroup<TagFlags, Permissions> {
+	get name(): string { return tagColumnName }
+
+	get permissions(): PermissionMap<Permissions> {
+		return new Map<Permissions, PermissionInfo<Permissions>>([
+			[ "view",              { flag: VIEW, permissionDependencies: [] } ],
+			[ "create",            { flag: CREATE, permissionDependencies: [ "view" ] } ],
+			[ "update",            { flag: UPDATE, permissionDependencies: [ "view" ] } ],
+			[ "archiveAndRestore", { flag: ARCHIVE_AND_RESTORE, permissionDependencies: [ "view" ] } ],
+		])
+	}
+}
