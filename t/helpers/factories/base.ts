0f050991163a512e2e00c605bd374c270a4ce138
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-15 13:07:28 +0800

    chore: apply type for generated data in base

--- t/helpers/factories/base.ts
+++ t/helpers/factories/base.ts
@@ -1,9 +1,10 @@
+import type { GeneratedData } from "~/types/dependent"
 import type { Model, ModelCtor, CreationAttributes } from "%/types/dependent"
 
 export default abstract class Factory<T extends Model> {
 	abstract get model(): ModelCtor<T>
 
-	abstract generate(): Promise<CreationAttributes<T>>
+	abstract generate(): GeneratedData<T>
 
 	async makeOne(): Promise<T> {
 		return await this.model.build(await this.generate())
