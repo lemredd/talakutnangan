ad7743049dd57673596e58155282399562594bab
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-15 16:05:34 +0800

    feat(permissions): make semester permission group

---
+++ server/app/auth/semester_permissions.ts
@@ -0,0 +1,37 @@
+import type {
+	PermissionMap,
+	PermissionInfo,
+	OperationPermission,
+} from "!/types/independent"
+
+import {
+	VIEW,
+	CREATE,
+	UPDATE,
+	ARCHIVE_AND_RESTORE
+} from "!/types/independent"
+
+import PermissionGroup from "!/bases/permission_group"
+
+const semesterColumnName = "semester_flags"
+
+type SemesterFlags = { [semesterColumnName]: number }
+type Permissions = OperationPermission
+
+/**
+ * Permission group for semester.
+ *
+ * This is safe to use in client-side.
+ */
+export default class extends PermissionGroup<SemesterFlags, Permissions> {
+	get name(): string { return semesterColumnName }
+
+	get permissions(): PermissionMap<Permissions> {
+		return new Map<Permissions, PermissionInfo<Permissions>>([
+			[ "view",              { flag: VIEW, permissionDependencies: [] } ],
+			[ "create",            { flag: CREATE, permissionDependencies: [ "view" ] } ],
+			[ "update",            { flag: UPDATE, permissionDependencies: [ "view" ] } ],
+			[ "archiveAndRestore", { flag: ARCHIVE_AND_RESTORE, permissionDependencies: [ "view" ] } ],
+		])
+	}
+}
