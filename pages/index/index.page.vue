9dd1500cf0d65751d66cbdfc8cf7bae752952afd
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-15 08:30:59 +0800

    feat: use the page props in index page

--- pages/index/index.page.vue
+++ pages/index/index.page.vue
@@ -5,8 +5,13 @@
 		<li>Rendered to HTML.</li>
 		<li>Interactive. <Counter /></li>
 	</ul>
+
+	<p>Page properties: {{ JSON.stringify(pageProps) }}</p>
 </template>
 
 <script lang="ts" setup>
 import Counter from "@@/index/Counter.vue"
+import { usePageContext } from "#/usePageContext"
+
+const pageProps = usePageContext().pageProps
 </script>
