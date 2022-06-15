9c463515eb27ed8f138ed5c444007a32de6b56ef
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-15 13:24:06 +0800

    test: add method to make or insert multiple models

--- t/helpers/factories/base.ts
+++ t/helpers/factories/base.ts
@@ -1,5 +1,5 @@
-import type { GeneratedData } from "~/types/dependent"
-import type { Model, ModelCtor, CreationAttributes } from "%/types/dependent"
+import type { Model, ModelCtor } from "%/types/dependent"
+import type { GeneratedData, MultipleGeneratedData } from "~/types/dependent"
 
 export default abstract class Factory<T extends Model> {
 	abstract get model(): ModelCtor<T>
@@ -7,10 +7,32 @@ export default abstract class Factory<T extends Model> {
 	abstract generate(): GeneratedData<T>
 
 	async makeOne(): Promise<T> {
-		return await this.model.build(await this.generate())
+		return this.model.build(await this.generate())
 	}
 
 	async insertOne(): Promise<T> {
 		return await this.model.create(await this.generate())
+	}
+
+	async generateMany(count: number): MultipleGeneratedData<T> {
+		const generatedData = []
+
+		for(let i = 0; i < count; i++) {
+			generatedData.push(this.generate())
+		}
+
+		return await Promise.all(generatedData)
+	}
+
+	async makeMany(count: number): Promise<T[]> {
+		const generatedMultipleData = await this.generateMany(count)
+
+		return this.model.bulkBuild(generatedMultipleData)
+	}
+
+	async insertMany(count: number): Promise<T[]> {
+		const generatedMultipleData = await this.generateMany(count)
+
+		return await this.model.bulkCreate(generatedMultipleData)
 	}
 }
