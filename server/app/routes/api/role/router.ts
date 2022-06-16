fe29c0fa09bfd1f5e0c02dc014996c9aaf6c6032
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-17 00:34:21 +0800

    feat(server): make archive role controller

--- server/app/routes/api/role/router.ts
+++ server/app/routes/api/role/router.ts
@@ -1,6 +1,7 @@
 import Router from "!/bases/router"
 import PutUpdate from "!/app/routes/api/role/update.put"
 import PostCreate from "!/app/routes/api/role/create.post"
+import DeleteArchive from "!/app/routes/api/role/archive(id).delete"
 
 export default class extends Router {
 	constructor() {
@@ -8,7 +9,8 @@ export default class extends Router {
 
 		this.useControllers([
 			new PutUpdate(),
-			new PostCreate()
+			new PostCreate(),
+			new DeleteArchive()
 		])
 	}
 }
