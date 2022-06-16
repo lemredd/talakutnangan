3416c764c44f78acb224b6573373e4c136cdafe5
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-16 20:53:38 +0800

    feat(database): make a singleton for the source

---
+++ database/data_source/database.ts
@@ -0,0 +1,11 @@
+import { Sequelize } from "sequelize-typescript"
+import { SourceType } from "%/types/independent"
+import createSource from "%/data_source/create_source"
+
+export default class Database {
+	private static source: Sequelize
+
+	static async initialize(sourceType: SourceType) {
+		this.source = await createSource(sourceType)
+	}
+}
