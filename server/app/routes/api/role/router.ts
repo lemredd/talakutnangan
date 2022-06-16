163e3fa795f34ecaa10777d65c252617e7852d38
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-17 00:36:57 +0800

    feat(server): make restore role controller

--- server/app/routes/api/role/router.ts
+++ server/app/routes/api/role/router.ts
@@ -1,6 +1,7 @@
 import Router from "!/bases/router"
 import PutUpdate from "!/app/routes/api/role/update.put"
 import PostCreate from "!/app/routes/api/role/create.post"
+import PatchRestore from "!/app/routes/api/role/restore(id).patch"
 import DeleteArchive from "!/app/routes/api/role/archive(id).delete"
 
 export default class extends Router {
@@ -10,6 +11,7 @@ export default class extends Router {
 		this.useControllers([
 			new PutUpdate(),
 			new PostCreate(),
+			new PatchRestore(),
 			new DeleteArchive()
 		])
 	}
