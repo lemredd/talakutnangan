da365bed1ebd3bdb031beb617bd8febed05b07f0
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-17 01:39:16 +0800

    fix(server): correct the path to role manager

--- server/app/routes/api/role/restore(id).patch.ts
+++ server/app/routes/api/role/restore(id).patch.ts
@@ -1,7 +1,7 @@
 import { AuthenticatedIDRequest, Response } from "!/types/dependent"
 
 import Policy from "!/bases/policy"
-import RoleManager from "%/managers/department"
+import RoleManager from "%/managers/role"
 import CommonMiddlewareList from "!/middlewares/common_middleware_list"
 import ModelBoundController from "!/common_controllers/model_bound_controller"
 
