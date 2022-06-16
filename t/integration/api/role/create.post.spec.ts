39765ae72567ff4c5abd4c5728a7683b294d35f0
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-17 00:12:50 +0800

    test(integration): ensure create role route works

---
+++ t/integration/api/role/create.post.spec.ts
@@ -0,0 +1,59 @@
+import { StatusCodes } from "http-status-codes"
+
+import App from "~/app"
+import RoleFactory from "~/factories/role"
+
+import Route from "!/app/routes/api/role/create.post"
+
+describe("POST /api/role/create", () => {
+	beforeAll(async () => {
+		await App.create(new Route())
+	})
+
+	it("can be accessed by authenticated user", async () => {
+		const { user, cookie } = await App.makeAuthenticatedCookie()
+		const role = await (new RoleFactory()).makeOne()
+
+		const response = await App.request
+			.post("/api/role/create")
+			.set("Cookie", cookie)
+			.send({
+				name: role.name,
+				departmentFlags: role.departmentFlags,
+				roleFlags: role.roleFlags,
+				semesterFlags: role.semesterFlags,
+				tagFlags: role.tagFlags,
+				postFlags: role.postFlags,
+				commentFlags: role.commentFlags,
+				profanityFlags: role.profanityFlags,
+				userFlags: role.userFlags,
+				auditTrailFlags: role.auditTrailFlags
+			})
+
+		expect(response.statusCode).toBe(StatusCodes.CREATED)
+		expect(response.body.name).toBe(role.name)
+	})
+
+	it.todo("cannot accept invalid values")
+
+	it("cannot be accessed by guest users", async () => {
+		const role = await (new RoleFactory()).makeOne()
+
+		const response = await App.request
+			.post("/api/role/create")
+			.send({
+				name: role.name,
+				departmentFlags: role.departmentFlags,
+				roleFlags: role.roleFlags,
+				semesterFlags: role.semesterFlags,
+				tagFlags: role.tagFlags,
+				postFlags: role.postFlags,
+				commentFlags: role.commentFlags,
+				profanityFlags: role.profanityFlags,
+				userFlags: role.userFlags,
+				auditTrailFlags: role.auditTrailFlags
+			})
+
+		expect(response.statusCode).toBe(StatusCodes.UNAUTHORIZED)
+	})
+})
