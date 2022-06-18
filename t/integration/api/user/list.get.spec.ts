6b819a3916fce3c2928be2c1d75f343ef9b93688
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-18 18:33:55 +0800

    test(integration): update the test for listing the users

--- t/integration/api/user/list.get.spec.ts
+++ t/integration/api/user/list.get.spec.ts
@@ -23,12 +23,24 @@ describe("GET /api/user/list", () => {
 		expect(response.statusCode).toBe(StatusCodes.OK)
 		const expectedUser = await manager.findWithID(student.id)
 		const refreshedAdmin = await manager.findWithID(admin.id)
-		expect(response.body).toStrictEqual(
-			JSON.parse(JSON.stringify([
+		expect(response.body).toStrictEqual({
+			data: [
 				refreshedAdmin,
 				expectedUser
-			]))
-		)
+			].map(model => {
+				const { id, name, email, kind, signature } = model!.toJSON()
+				return {
+					type: "user",
+					id,
+					attributes: {
+						name,
+						email,
+						kind,
+						signature: ""
+					}
+				}
+			})
+		})
 	})
 
 	it.todo("can be accessed by permitted user and get multiple complete users")
