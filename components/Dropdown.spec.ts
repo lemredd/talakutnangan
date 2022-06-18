38f0a8b7488e58e2c42bf78aa9a38f7467e122f4
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-06-18 17:20:53 +0800

    test(navigation): test dropdown button click

--- components/Dropdown.spec.ts
+++ components/Dropdown.spec.ts
@@ -4,10 +4,16 @@ import Dropdown from "./Dropdown.vue"
 
 describe("Component: Dropdown", () => {
 	it("Should close if click emitted outside of dropdown", async () => {
-		const wrapper = mount(Dropdown)
+		const wrapper = mount(Dropdown, {
+			props: {
+				purpose: ""
+			}
+		})
+		const toggler = wrapper.find("#dropdown-btn")
+		await toggler.trigger("click")
 		const invisibleCloser = wrapper.find(".invisible-closer")
-
 		await invisibleCloser.trigger("click")
-		expect(wrapper.emitted()).toHaveProperty("close")
+
+		expect(wrapper.emitted()).toHaveProperty("click")
 	})
 })
