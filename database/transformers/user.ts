158176f6456eedc165f3b1301868581e6cea73c8
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-18 15:49:16 +0800

    feat(database): make transformer for user model

---
+++ database/transformers/user.ts
@@ -0,0 +1,23 @@
+import User from "%/models/user"
+import Transformer from "%/transformers/base"
+import Serializer from "%/transformers/serializer"
+import type { AttributesObject, TransformerOptions } from "%/types/dependent"
+
+export default class extends Transformer<User, void> {
+	constructor() {
+		super()
+		this.type = "user"
+	}
+
+	transform(model: User|User[], options: TransformerOptions): AttributesObject {
+		const safeObject = Serializer.whitelist(model, [
+			"id",
+			"name",
+			"email",
+			"kind",
+			"signature"
+		])
+
+		return safeObject
+	}
+}
