ac5820178f7e3386d5612c0318de31b9a0b0d23e
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-19 02:48:06 +0800

    perf(test): apply mock requester to test authentication-based policy

--- server/middlewares/authentication/authentication-based_policy.spec.ts
+++ server/middlewares/authentication/authentication-based_policy.spec.ts
@@ -1,60 +1,52 @@
-import { StatusCodes } from "http-status-codes"
-import { getMockReq as makeRequest, getMockRes as makeResponse } from "@jest-mock/express"
-
-import type { MockResponse } from "!/types/test"
-import type { Request, Response } from "!/types/dependent"
+import MockRequester from "~/set-ups/mock_requester"
 
 import AuthenticationBasedPolicy from "./authentication-based_policy"
 
 describe("Middleware: Authenticated-Based Policy", () => {
+	const requester  = new MockRequester()
+
 	it("can allow guest users if guests are expected", async () => {
 		const authenticatedGuard = new AuthenticationBasedPolicy(false)
-		const request  = makeRequest<Request>()
-		const { res: response, next, } = makeResponse()
-		request.isAuthenticated = jest.fn().mockReturnValue(false)
+		requester.customizeRequest({
+			isAuthenticated: jest.fn().mockReturnValue(false)
+		})
 
-		await authenticatedGuard.intermediate(request, response, next)
+		requester.runMiddleware(authenticatedGuard.intermediate.bind(authenticatedGuard))
 
-		expect(next).toHaveBeenCalled()
+		requester.expectSuccess()
 	})
 
 	it("can deny known users if guest are expected", async () => {
 		const authenticatedGuard = new AuthenticationBasedPolicy(false)
-		const request  = makeRequest<Request>()
-		const { res: response, next, } = makeResponse()
-		request.isAuthenticated = jest.fn().mockReturnValue(true)
+		requester.customizeRequest({
+			isAuthenticated: jest.fn().mockReturnValue(true)
+		})
 
-		await authenticatedGuard.intermediate(request, response, next)
+		requester.runMiddleware(authenticatedGuard.intermediate.bind(authenticatedGuard))
 
-		const mockResponse = <MockResponse><unknown>response
-		expect(mockResponse.status).toHaveBeenCalled()
-		expect(mockResponse.status.mock.calls[0]).toEqual([ StatusCodes.UNAUTHORIZED ])
-		expect(mockResponse.json).toHaveBeenCalled()
+		requester.expectFailure(requester.status.UNAUTHORIZED)
 	})
 
 	it("can allow known users if known are expected", async () => {
 		const authenticatedGuard = new AuthenticationBasedPolicy(true)
-		const request  = makeRequest<Request>()
-		const { res: response, next, } = makeResponse()
-		request.isAuthenticated = jest.fn().mockReturnValue(true)
+		requester.customizeRequest({
+			isAuthenticated: jest.fn().mockReturnValue(true)
+		})
 
-		await authenticatedGuard.intermediate(request, response, next)
+		requester.runMiddleware(authenticatedGuard.intermediate.bind(authenticatedGuard))
 
-		expect(next).toHaveBeenCalled()
+		requester.expectSuccess()
 	})
 
 
 	it("can deny guest users if known are expected", async () => {
 		const authenticatedGuard = new AuthenticationBasedPolicy(true)
-		const request  = makeRequest<Request>()
-		const { res: response, next, } = makeResponse()
-		request.isAuthenticated = jest.fn().mockReturnValue(false)
+		requester.customizeRequest({
+			isAuthenticated: jest.fn().mockReturnValue(false)
+		})
 
-		await authenticatedGuard.intermediate(request, response, next)
+		requester.runMiddleware(authenticatedGuard.intermediate.bind(authenticatedGuard))
 
-		const mockResponse = <MockResponse><unknown>response
-		expect(mockResponse.status).toHaveBeenCalled()
-		expect(mockResponse.status.mock.calls[0]).toEqual([ StatusCodes.UNAUTHORIZED ])
-		expect(mockResponse.json).toHaveBeenCalled()
+		requester.expectFailure(requester.status.UNAUTHORIZED)
 	})
 })
