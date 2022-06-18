ca533b31d9be9db28a79f343128b710d1a96b4cc
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-19 02:19:16 +0800

    test(unit): allow request to be reusable in kind-based policy test

--- server/middlewares/authentication/kind-based_policy.spec.ts
+++ server/middlewares/authentication/kind-based_policy.spec.ts
@@ -3,18 +3,21 @@ import { getMockReq as makeRequest, getMockRes as makeResponse } from "@jest-moc
 
 import { UserKind } from "$/types/database"
 import type { MockResponse } from "!/types/test"
-import type { Request, Response } from "!/types/dependent"
+import type { Request } from "!/types/dependent"
 
 import UserFactory from "~/factories/user"
 
 import KindBasedPolicy from "./kind-based_policy"
 
 describe("Middleware: Kind-Based Policy", () => {
+	const request  = makeRequest<Request>()
+	const { res: response, next, mockClear } = makeResponse()
+
+	afterEach(() => mockClear())
+
 	it("can allow unreachable employees only as expected", async () => {
 		const user = await (new UserFactory()).beUnreachableEmployee().insertOne()
 		const pageGuard = new KindBasedPolicy(UserKind.UnreachableEmployee)
-		const request  = makeRequest<Request>()
-		const { res: response, next, } = makeResponse()
 		request.user = user
 		request.isAuthenticated = jest.fn().mockReturnValue(true)
 
@@ -26,8 +29,6 @@ describe("Middleware: Kind-Based Policy", () => {
 	it("can allow reachable employees only as expected", async () => {
 		const user = await (new UserFactory()).beReachableEmployee().insertOne()
 		const pageGuard = new KindBasedPolicy(UserKind.ReachableEmployee)
-		const request  = makeRequest<Request>()
-		const { res: response, next, } = makeResponse()
 		request.user = user
 		request.isAuthenticated = jest.fn().mockReturnValue(true)
 
@@ -39,8 +40,6 @@ describe("Middleware: Kind-Based Policy", () => {
 	it("can allow students only as expected", async () => {
 		const user = await (new UserFactory()).beStudent().insertOne()
 		const pageGuard = new KindBasedPolicy(UserKind.Student)
-		const request  = makeRequest<Request>()
-		const { res: response, next, } = makeResponse()
 		request.user = user
 		request.isAuthenticated = jest.fn().mockReturnValue(true)
 
@@ -52,8 +51,6 @@ describe("Middleware: Kind-Based Policy", () => {
 	it("can deny students if reachable employees are expected", async () => {
 		const user = await (new UserFactory()).beStudent().insertOne()
 		const pageGuard = new KindBasedPolicy(UserKind.ReachableEmployee)
-		const request  = makeRequest<Request>()
-		const { res: response, next, } = makeResponse()
 		request.user = user
 		request.isAuthenticated = jest.fn().mockReturnValue(true)
 
