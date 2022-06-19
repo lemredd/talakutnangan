2c10309e98757d5cb0add174f2928d3e50a1a3fc
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-19 18:50:31 +0800

    test: mock consola instance for unit tests

--- t/helpers/set-ups/log.set_up.ts
+++ t/helpers/set-ups/log.set_up.ts
@@ -1,7 +1,14 @@
 import "dotenv/config"
+import consola from "consola"
 
 import Log from "!/helpers/log"
 
 beforeAll(() => {
+	consola.wrapAll()
+	consola.mockTypes(() => jest.fn())
 	Log.initialize()
+})
+
+beforeEach(() => {
+	consola.mockTypes(() => jest.fn())
 })
--- t/helpers/set-ups/singletons.set_up.ts
+++ t/helpers/set-ups/singletons.set_up.ts
@@ -1,6 +1,21 @@
+import "dotenv/config"
+import consola from "consola"
+import { Environment } from "$/types/server"
 import getDataSourceType from "~/set-ups/get_data_source_type"
+import RequestEnvironment from "!/helpers/request_environment"
 import initializeSingletons from "!/helpers/initialize_singletons"
 
 beforeAll(async () => {
+	if (RequestEnvironment.environment === Environment.UnitTest) {
+		consola.wrapAll()
+		consola.mockTypes(() => jest.fn())
+	}
+
 	await initializeSingletons(getDataSourceType())
+})
+
+beforeEach(() => {
+	if (RequestEnvironment.environment === Environment.UnitTest) {
+		consola.mockTypes(() => jest.fn())
+	}
 })
