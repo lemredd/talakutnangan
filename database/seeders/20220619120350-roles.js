dfd1650bb28fff6a77a18d9810d842a7af231769
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-19 20:41:56 +0800

    config(database): make initial seeder

---
+++ database/seeders/20220619120350-roles.js
@@ -0,0 +1,59 @@
+// @ts-check
+"use strict";
+
+module.exports = {
+	/**
+	 * @param {import("sequelize").QueryInterface} queryInterface
+	 * @param {import("sequelize").Sequelize} Sequelize
+	 */
+	async up (queryInterface, Sequelize) {
+		// TODO: Automatically generate the roles
+		/**
+		 * @type {(import("sequelize").Attributes<import("../models/role")>)[]}
+		 * @const
+		 */
+		const roles = [
+			{
+				name: "admin",
+				departmentFlags: 0x10F,
+				roleFlags: 0x0F,
+				semesterFlags: 0x0F,
+				tagFlags: 0x0F,
+				postFlags: 0x1FF,
+				commentFlags: 0xFD,
+				profanityFlags: 0x3F,
+				userFlags: 0x1FF,
+				auditTrailFlags: 0x1
+			},
+			{
+				name: "dean",
+				departmentFlags: 0x05,
+				roleFlags: 0x01,
+				semesterFlags: 0x01,
+				tagFlags: 0x0F,
+				postFlags: 0x1AF,
+				commentFlags: 0x1AF,
+				profanityFlags: 0x11,
+				userFlags: 0x0AF,
+				auditTrailFlags: 0x0
+			}
+		]
+
+		roles.forEach(role => {
+			// @ts-ignore
+			role.createdAt = new Date()
+			// @ts-ignore
+			role.updatedAt = new Date()
+		})
+		// @ts-ignore
+		await queryInterface.bulkInsert("Roles", roles, {});
+	},
+
+	/**
+	 * @param {import("sequelize").QueryInterface} queryInterface
+	 * @param {import("sequelize").Sequelize} Sequelize
+	 */
+	async down (queryInterface, Sequelize) {
+		// TODO
+	}
+};
