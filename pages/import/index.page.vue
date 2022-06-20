9f275b3aedc91f0f425ec9b2ec01b31f6c879dda
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-20 08:36:24 +0800

    feat(register): make initial code for uploading CSV files

---
+++ pages/import/index.page.vue
@@ -0,0 +1,23 @@
+<template>
+	<form @submit.prevent="importData">
+		<input type="file" accept="text/csv" name="csv_file"/>
+	</form>
+</template>
+
+<script setup lang="ts">
+function importData(event: Event) {
+	const form = event.target as HTMLFormElement
+	const formData = new FormData(form)
+
+	fetch("/api/user/import", {
+		method: "POST",
+		headers: {
+			"Content-Type": "multipart/form-data"
+		},
+		body: formData
+	}).then(response => {
+		console.log(response)
+	})
+}
+
+</script>
