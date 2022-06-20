41095fcf0832abdca4a5c56c734df289bf0a709e
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-20 12:01:06 +0800

    feat(server): make test route to check the contents of parsed body

---
+++ server/app/routes/t/multipart.post.ts
@@ -0,0 +1,35 @@
+import { Buffer } from "node:buffer"
+import { Request, Response } from "!/types/dependent"
+
+import MultipartController from "!/common_controllers/multipart_controller"
+
+ export interface WithImport {
+	body: {
+		importedCSV: Buffer
+	}
+}
+
+export default class extends MultipartController {
+	get filePath(): string { return __filename }
+
+	// TODO: Use a permission-based policy
+	get policy(): null { return null }
+
+	get bodyValidationRules(): object {
+		return {}
+	}
+
+	async handle(request: Request, response: Response): Promise<void> {
+		const info: { [key:string]: string } = {}
+		const body = request.body
+
+		for (const field in body) {
+			if (Object.prototype.hasOwnProperty.call(body, field)) {
+				const value = body[field];
+				info[field] = value
+			}
+		}
+
+		response.status(this.status.OK).json(info)
+	}
+}
