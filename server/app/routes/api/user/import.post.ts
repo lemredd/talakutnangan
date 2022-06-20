1baa41b9a55c911c40f237dc2e8fc205328385b1
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-20 19:33:29 +0800

    feat(server): apply CSV parser to read the file

--- server/app/routes/api/user/import.post.ts
+++ server/app/routes/api/user/import.post.ts
@@ -1,9 +1,11 @@
-import { Buffer } from "node:buffer"
+import { Buffer } from "buffer"
+import { OptionalMiddleware } from "$/types/server"
 import { Request, Response } from "!/types/dependent"
 
 import Policy from "!/bases/policy"
-import Middleware from "!/bases/middleware"
 import UserManager from "%/managers/user"
+import Middleware from "!/bases/middleware"
+import CSVParser from "!/middlewares/body_parser/csv"
 import CommonMiddlewareList from "!/middlewares/common_middleware_list"
 import MultipartController from "!/common_controllers/multipart_controller"
 
@@ -19,8 +21,14 @@ export default class extends MultipartController {
 	// TODO: Use a permission-based policy
 	get policy(): Policy { return CommonMiddlewareList.knownOnlyPolicy }
 
+	get postParseMiddlewares(): OptionalMiddleware[] {
+		return [
+			new CSVParser("importedCSV")
+		]
+	}
+
 	get bodyValidationRules(): object {
-		// Create validator for buffers
+		// TODO: Create validator for buffers
 		return {
 			importedCSV: [ "required" ]
 		}
@@ -29,10 +37,9 @@ export default class extends MultipartController {
 	async handle(request: Request, response: Response): Promise<void> {
 		const manager = new UserManager()
 
-		// TODO: READ the CSV file
 		// TODO: Pass the the converted CSV file to user manager
 
-		response.status(this.status.NOT_IMPLEMENTED)
+		response.status(this.status.NOT_IMPLEMENTED).json(request.body)
 	}
 
 	// TODO: Send e-mails to new users
