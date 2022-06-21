7203909e8a623d83a1a3825b57de5d06e07ecb39
AteKitty07 <kiritogregorio@gmail.com>
2022-06-21 22:10:06 +0800

    config(typescript): add path alias for assets

--- renderer/PageShell.vue
+++ renderer/PageShell.vue
@@ -3,7 +3,7 @@
 		<div v-if="!isLoggingIn" class="navigation dark:bg-dark-700">
 			<div class="container">
 				<a href="/" class="logo">
-					<img src="./Logo.svg" alt="logo" />
+					<img src="@assets/Logo.svg" alt="logo" />
 					<h1 class="ml-1">TALAKUTNANGAN</h1>
 				</a>
 
