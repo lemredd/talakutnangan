ef20358baed026b00f2c6a3cd20c5ffd9563b3f0
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-06-22 22:43:54 +0800

    style(footer): use windiCSS flexbox

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,45 +1,32 @@
 <template>
-<footer>
-    <div class="links">
-        <div v-for="N in 4" class ="footer1">
-            <h1>Footer</h1>
-            <ul>
-                <li v-for = "N in 3">Sub Footer</li>
-            </ul>
-        </div>
-    </div>
-    <div class="copyright">
-        <span class="material-icons footer1">copyright</span>
-        <span>2022 Talakutnangan, All rights reserved.</span>
-    </div>
-</footer>
+	<footer class="flex flex-col justify-around">
+		<div class="links flex justify-around">
+			<div v-for="N in 4" class ="footer-list">
+				<h1>Footer</h1>
+				<ul>
+					<li v-for = "N in 3">Sub Footer</li>
+				</ul>
+			</div>
+		</div>
+		<div class="copyright flex items-center justify-center">
+			<span class="material-icons footer1">copyright</span>
+			<span>2022 Talakutnangan, All rights reserved.</span>
+		</div>
+	</footer>
 </template>
 <style scoped lang="scss">
 footer {
-    display:flex;
-    flex-direction: column;
-    justify-content: space-around;
-    background-color: #313131;
-    color: #fff;
-    padding: -1em 3em;
-
-    .links {
-        display:flex;
-        justify-content: space-around;
-    }
+	background-color: #313131;
+	color: #fff;
+	padding: 1em 3em;
 }
+
 .copyright{
-    display:flex;
-    align-items: center;
-    justify-content: center;
-    background-color: #313131;
-    color: #fff;
-
+	margin-top: 1em;
+	color: #fff;
 }
-.footer1 {
-        
-        padding: 5px;
-        
+.footer-list {
+	padding: 5px;
 }
 
 </style>
