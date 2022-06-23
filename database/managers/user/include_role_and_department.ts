4db9d750036b9d6daa14743d7778db66903004bf
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-23 21:31:19 +0800

    feat(database): make pipe to include role and department

---
+++ database/managers/user/include_role_and_department.ts
@@ -0,0 +1,26 @@
+import type { FindOptions } from "%/types/dependent"
+
+import Log from "!/helpers/log"
+
+import Role from "%/models/role"
+import Department from "%/models/department"
+
+/**
+ * Includes role and department models upon getting user models
+ */
+export default function<T>(
+	currentState: FindOptions<T>,
+	_constraints: { [key: string]: any }
+): FindOptions<T> {
+	const newState = { ...currentState }
+
+	if (newState.include !== undefined) {
+		newState.include = []
+	}
+
+	newState.include!.push(Role, Department)
+
+	Log.trace("pipeline", "applied role and department includer")
+
+	return newState
+}
