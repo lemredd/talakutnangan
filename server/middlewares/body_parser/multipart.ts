05eee2b963f56f0724c92fababe65e2d52ed7d33
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-23 19:08:55 +0800

    feat(server): allow parsing multi-dimensional fields

--- server/middlewares/body_parser/multipart.ts
+++ server/middlewares/body_parser/multipart.ts
@@ -1,5 +1,6 @@
 import { Buffer } from "buffer"
 import busboy from "busboy"
+import { parse } from "qs"
 import Middleware from "!/bases/middleware"
 import type { Request, Response, NextFunction } from "!/types/dependent"
 
@@ -8,7 +9,8 @@ import type { Request, Response, NextFunction } from "!/types/dependent"
  */
 export default class FormBodyParser extends Middleware {
 	async intermediate(request: Request, response: Response, next: NextFunction): Promise<void> {
-		const newBody: { [key: string]: any } = {}
+		const bufferedFiles: { [key: string]: any } = {}
+		const formQueries: string[] = []
 		const parser = busboy({ headers: request.headers })
 
 		parser.on("file", (name, stream, info) => {
@@ -17,16 +19,20 @@ export default class FormBodyParser extends Middleware {
 				rawBuffer.push(...data)
 			})
 			.on("close", () => {
-				newBody[name] = { buffer: Buffer.from(rawBuffer), info }
+				bufferedFiles[name] = { buffer: Buffer.from(rawBuffer), info }
 			})
 		})
 
 		parser.on("field", (name, value, _info) => {
-			newBody[name] = value
+			formQueries.push(`${name}=${value}`)
 		});
 
 		parser.on("close", () => {
-			request.body = newBody
+			const parsedFormQuery = parse(formQueries.join("&"))
+			request.body = {
+				...bufferedFiles,
+				...parsedFormQuery
+			}
 
 			next()
 		})
