bba0a7a23174914a45f86791690a8ba0d827c189
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-24 20:58:16 +0800

    feat(database): log successful actions in base manager

--- database/managers/base.ts
+++ database/managers/base.ts
@@ -10,6 +10,7 @@ import type {
 	CreationAttributes,
 	FindAndCountOptions
 } from "%/types/dependent"
+import Log from "!/helpers/log"
 import Transformer from "%/transformers/base"
 import Serializer from "%/transformers/serializer"
 import runThroughPipeline from "$/helpers/run_through_pipeline"
@@ -27,6 +28,8 @@ export default abstract class Manager<T extends Model, U> {
 	async findWithID(id: number): Promise<Serializable> {
 		const model = await this.model.findOne(<FindOptions<T>>{ where: { id } })
 
+		Log.success("manager", "done searching for a model using ID")
+
 		return this.serialize(model)
 	}
 
@@ -35,11 +38,17 @@ export default abstract class Manager<T extends Model, U> {
 
 		const rows = await this.model.findAll(options)
 
+		Log.success("manager", "done listing models according to constraints")
+
 		return this.serialize(rows)
 	}
 
 	async create(details: U & CreationAttributes<T>): Promise<T> {
-		return await this.model.create(details)
+		const model = await this.model.create(details)
+
+		Log.success("manager", "done creating a model")
+
+		return model
 	}
 
 	async update(id: number, details: U & Attributes<T>): Promise<number> {
@@ -47,19 +56,27 @@ export default abstract class Manager<T extends Model, U> {
 			where: { id }
 		})
 
+		Log.success("manager", "done updating a model")
+
 		return affectedCount
 	}
 
 	async archive(id: number): Promise<number> {
-		return await this.model.destroy(<DestroyOptions<T>>{
+		const destroyCount = await this.model.destroy(<DestroyOptions<T>>{
 			where: { id }
 		})
+
+		Log.success("manager", "done archiving a model")
+
+		return destroyCount
 	}
 
 	async restore(id: number): Promise<void> {
-		return await this.model.restore(<RestoreOptions<T>>{
+		await this.model.restore(<RestoreOptions<T>>{
 			where: { id }
 		})
+
+		Log.success("manager", "done restoring a model")
 	}
 
 	protected serialize(models: T|T[]|null): Serializable {
