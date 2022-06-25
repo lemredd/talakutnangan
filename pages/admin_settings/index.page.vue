900f5ffab6d56301fcfc9559cba12b3a41439644
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-06-25 19:39:02 +0800

    feat(admin): make admin settings page

---
+++ pages/admin_settings/index.page.vue
@@ -0,0 +1,22 @@
+<template>
+	<header>
+		<h1>administrative settings</h1>
+	</header>
+
+	<main>
+
+	</main>
+</template>
+
+<style scoped lang="scss">
+header h1 {
+	font-size: 2em;
+	font-weight: 700;
+	text-align: center;
+	text-transform: uppercase;
+}
+</style>
+
+<script setup lang="ts">
+import Tab from "@/Tab.vue"
+</script>
