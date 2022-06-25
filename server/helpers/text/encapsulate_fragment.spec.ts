abf8c703912a8d1d87523f89439825ac83e52cba
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-25 23:25:01 +0800

    test(unit): update the test to function for encapsulating fragments

--- server/helpers/text/encapsulate_fragment.spec.ts
+++ server/helpers/text/encapsulate_fragment.spec.ts
@@ -3,26 +3,11 @@ import encapsulateFragment from "./encapsulate_fragment"
 describe("Helpers: Encapsulate fragment", () => {
 	it("can encapsulate fragment", () => {
 		const title = "Hello"
-		const styles = "p { color: red; }"
+		const styles = "p { color: red }"
 		const fragment = "<p>World!</p>"
 
 		const document = encapsulateFragment(title, styles, fragment)
 
-		expect(document).toBe(`
-	<!DOCTYPE html>
-	<html>
-		<head>
-			<meta charset="UTF-8" />
-			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-			<title>${title}</title>
-			<style>
-				${styles}
-			</style>
-		</head>
-		<body>
-			${fragment}
-		</body>
-	</html>
-	`)
+		expect(document).toBe(`<!DOCTYPE html><html><head><meta charset="UTF-8"/><meta content="width=device-width,initial-scale=1" name="viewport"/><title>${title}</title><style>${styles.split(" ").join("")}</style></head><body>${fragment}</body></html>`)
 	})
 })
