a38547286fe34030989b0a575dabb25e0a51cc56
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-26 08:25:40 +0800

    feat(server): include default styles upon encapsulating the fragment

--- server/helpers/text/encapsulate_fragment.ts
+++ server/helpers/text/encapsulate_fragment.ts
@@ -1,6 +1,25 @@
+import { readFile } from "fs"
+import { promisify } from "util"
 import { minify } from "html-minifier"
 
-export default function(title: string, styles: string, fragment: string): string {
+import getRoot from "!/helpers/get_root"
+
+/**
+ * Encapsulates the HTML fragment converted from Markdown syntax.
+ *
+ * @param title Title of the document. Usually, a subject of the e-mail message
+ * @param styles Additional styles to apply aside from the defaults
+ * @param fragment HTML fragement from converted Markdown syntax
+ * @param defaultStylePath Path to default style. Defaults to the default style for e-mail messages.
+ */
+export default async function(
+	title: string,
+	styles: string,
+	fragment: string,
+	defaultStylePath: string = `${getRoot()}/email/defult.css`
+): Promise<string> {
+	const defaultStyles = (await promisify(readFile)(defaultStylePath)).toString()
+
 	return minify(`
 	<!DOCTYPE html>
 	<html>
@@ -9,6 +28,7 @@ export default function(title: string, styles: string, fragment: string): string
 			<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 			<title>${title}</title>
 			<style>
+				${defaultStyles}
 				${styles}
 			</style>
 		</head>
