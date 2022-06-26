5156ac090b4fc07554653f4ddd2fe408384d9e70
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-26 10:14:37 +0800

    types(server): make new type for notifying new users

--- server/types/independent.ts
+++ server/types/independent.ts
@@ -4,9 +4,23 @@
  * cannot be shareable since they are only used within the server.
  */
 
+import type { UserKind } from "$/types/database"
+
 /**
  * Shape of middleware arguments for e-mail verification.
  */
 export interface EmailVerificationArguments {
 	emailsToContact: string[]
+}
+
+/**
+ * Shape of middleware arguments for notifying new users about their password.
+ */
+export interface NewUserNotificationArguments {
+	userDetails: {
+		name: string,
+		email: string,
+		kind: UserKind,
+		password: string
+	}[]
 }
