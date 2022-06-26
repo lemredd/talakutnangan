c363bd099db0966271a8757b2ca9346a62285316
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-26 10:00:09 +0800

    feat(server): allow usage of WindiCSS features in e-mail messages

--- server/helpers/text/encapsulate_fragment.ts
+++ server/helpers/text/encapsulate_fragment.ts
@@ -1,6 +1,9 @@
 import { readFile } from "fs"
 import { promisify } from "util"
 import { minify } from "html-minifier"
+import { Processor } from "windicss/lib"
+import { CSSParser } from "windicss/utils/parser"
+import { HTMLParser } from "windicss/utils/parser"
 
 import getRoot from "!/helpers/get_root"
 
@@ -16,11 +19,41 @@ export default async function(
 	title: string,
 	styles: string,
 	fragment: string,
-	defaultStylePath: string = `${getRoot()}/email/default.css`
+	defaultStylePath: string = `${getRoot()}/email/default.scss`
 ): Promise<string> {
 	const defaultStyles = (await promisify(readFile)(defaultStylePath)).toString()
+	const precombinedRawStyles = `${defaultStyles}${styles}`
+	const rawHTML = makeRawHTML(title, precombinedRawStyles, fragment)
 
-	return minify(`
+	// Please see: https://windicss.org/integrations/javascript.html
+	const processor = new Processor()
+	const classes = new HTMLParser(rawHTML)
+		.parseClasses()
+		.map(className => className.result)
+		.join(" ")
+	const processedPrecombinedStyles = new CSSParser(precombinedRawStyles, processor).parse()
+
+	const preflightSheet = processor.preflight(rawHTML)
+	const interpretedSheet = processor.interpret(classes).styleSheet
+	const combinedStyles = interpretedSheet
+		.extend(preflightSheet, true)
+		.extend(processedPrecombinedStyles, true)
+	const builtStyles = combinedStyles.build(true)
+
+	const styleOptimizedHTML = makeRawHTML(title, builtStyles, fragment)
+	return minify(styleOptimizedHTML, {
+		collapseInlineTagWhitespace: true,
+		collapseWhitespace: true,
+		keepClosingSlash: true,
+		minifyCSS: true,
+		removeEmptyElements: true,
+		sortAttributes: true,
+		sortClassName: true
+	})
+}
+
+function makeRawHTML(title: string, styles: string, fragment: string): string {
+	return `
 	<!DOCTYPE html>
 	<html>
 		<head>
@@ -28,7 +61,6 @@ export default async function(
 			<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 			<title>${title}</title>
 			<style>
-				${defaultStyles}
 				${styles}
 			</style>
 		</head>
@@ -38,13 +70,5 @@ export default async function(
 			</div>
 		</body>
 	</html>
-	`, {
-		collapseInlineTagWhitespace: true,
-		collapseWhitespace: true,
-		keepClosingSlash: true,
-		minifyCSS: true,
-		removeEmptyElements: true,
-		sortAttributes: true,
-		sortClassName: true
-	})
+	`
 }
