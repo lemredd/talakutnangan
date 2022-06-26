ebd095e0ad7c4de775b4d3408f07b08f2226aa0e
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-26 08:30:10 +0800

    test(unit): check for important texts instead

--- server/helpers/text/encapsulate_fragment.spec.ts
+++ server/helpers/text/encapsulate_fragment.spec.ts
@@ -1,13 +1,15 @@
 import encapsulateFragment from "./encapsulate_fragment"
 
 describe("Helpers: Encapsulate fragment", () => {
-	it("can encapsulate fragment", () => {
+	it("can encapsulate fragment", async () => {
 		const title = "Hello"
 		const styles = "p { color: red }"
 		const fragment = "<p>World!</p>"
 
-		const document = encapsulateFragment(title, styles, fragment)
+		const document = await encapsulateFragment(title, styles, fragment)
 
-		expect(document).toBe(`<!DOCTYPE html><html><head><meta charset="UTF-8"/><meta content="width=device-width,initial-scale=1" name="viewport"/><title>${title}</title><style>${styles.split(" ").join("")}</style></head><body>${fragment}</body></html>`)
+		expect(document).toContain(`<title>${title}</title>`)
+		expect(document).toContain("p{color:red}")
+		expect(document).toContain(fragment)
 	})
 })
