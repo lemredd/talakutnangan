3f7cbbb6951e56191b5a5816afcd57634b3c4537
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-27 22:48:48 +0800

    feat(server): link other fake model to users

--- server/app/routes/dev/sample_user_list.get.ts
+++ server/app/routes/dev/sample_user_list.get.ts
@@ -1,15 +1,27 @@
 import { Request, Response } from "!/types/dependent"
 
 import UserFactory from "~/factories/user"
+import RoleFactory from "~/factories/role"
+import DepartmentFactory from "~/factories/department"
+
 import UserTransformer from "%/transformers/user"
 import Serializer from "%/transformers/serializer"
+
 import DevController from "!/common_controllers/dev_controller"
 
 export default class extends DevController {
 	get filePath(): string { return __filename }
 
 	async handle(request: Request, response: Response): Promise<void> {
-		const list = await new UserFactory().makeMany(25)
+		const department = await new DepartmentFactory().makeOne()
+		const role = await new RoleFactory().makeOne()
+		department.id = 0
+		role.id = 0
+		const list = (await new UserFactory().in(department).makeMany(25)).map(user => {
+			user.department = department
+			user.roles = [ role ]
+			return user
+		})
 		const transformer = new UserTransformer()
 
 		const serializedList = Serializer.serialize(list, transformer, {})
