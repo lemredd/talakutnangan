4c8dd4afb0ac6a617b66b1677d011ad210187a0d
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-27 22:18:28 +0800

    chore: convert indention to tabs

--- pages/user_list/index.page.vue
+++ pages/user_list/index.page.vue
@@ -1,52 +1,53 @@
 <template>
-    <h1 class="text-2xl m-2 text-light-200">Students of (Institute)</h1>
-    <div>
-        <div class="bg-dark-100 h-10">
-            <div class=" flex">
-            <input
-                class="m-2 bg-dark-300 rounded-md w-40 text-light-200 text-xs h-6"
-                type="text"
-                v-model="input"
-            />
-            <img class="img1 bg-dark-900 rounded-md h-6 p-1.5" :src="SearchIcon"/>
-            </div>
-        </div>
-    </div>
-    <div class="text-2xl m-2 text-light-100" v-for="student in filteredList" :key="student.name">
-        <div class="user-entry flex justify-between border-b p-b-2 basis-70 place-items-center">
-            <span class="user-name text-lg w-50">{{ student.name }}</span>
-            <span class="user-email text-xs">{{ student.email }}</span>
-            <div class="btns">
-                <button class=" btn1 text-dark-100 rounded-md w-20 text-base h-7">Update</button>
-            </div>
+	 <h1 class="text-2xl m-2 text-light-200">Students of (Institute)</h1>
+	 <div>
+		  <div class="bg-dark-100 h-10">
+				<div class=" flex">
+				<input
+					 class="m-2 bg-dark-300 rounded-md w-40 text-light-200 text-xs h-6"
+					 type="text"
+					 v-model="input"
+				/>
+				<img class="img1 bg-dark-900 rounded-md h-6 p-1.5" :src="SearchIcon"/>
+				</div>
+		  </div>
+	 </div>
+	 <div class="text-2xl m-2 text-light-100" v-for="student in filteredList" :key="student.name">
+		  <div class="user-entry flex justify-between border-b p-b-2 basis-70 place-items-center">
+				<span class="user-name text-lg w-50">{{ student.name }}</span>
+				<span class="user-email text-xs">{{ student.email }}</span>
+				<div class="btns">
+					 <button class=" btn1 text-dark-100 rounded-md w-20 text-base h-7">Update</button>
+				</div>
 
-        </div>
-    </div>
+		  </div>
+	 </div>
 
-    <div class="text-light-200" v-if="input && !filteredList.length">
-        <p>No results found!</p>
-    </div>
+	 <div class="text-light-200" v-if="input && !filteredList.length">
+		  <p>No results found!</p>
+	 </div>
 </template>
 
 <style>
 
 body {
-    padding: 20px;
-    min-height: 100vh;
-    background-color: rgb(37, 40, 46);
+	 padding: 20px;
+	 min-height: 100vh;
+	 background-color: rgb(37, 40, 46);
 }
 
 .btn1 {
-background:white;
+	background:white;
 }
+
 .btn1:hover{
-background: gray;
+	background: gray;
 }
 
 .img1{
 
-    margin-top:8px;
-    margin-left:-15px;
+	 margin-top:8px;
+	 margin-left:-15px;
 }
 
 </style>
@@ -56,38 +57,38 @@ import { ref, computed } from "vue"
 import SearchIcon from "@@/user_list/search_icon.png"
 let input = ref("")
 const students = ref([
-    {
-        name: "Juan Dela Cruz",
-        email: "Email@email.com",
-    },
-    {
-        name: "Alice Dela Cruz",
-        email: "Email@email.com",
-    },
-    {
-        name: "Jun Dela Cruz",
-        email: "Email@email.com",
+	 {
+		  name: "Juan Dela Cruz",
+		  email: "Email@email.com",
+	 },
+	 {
+		  name: "Alice Dela Cruz",
+		  email: "Email@email.com",
+	 },
+	 {
+		  name: "Jun Dela Cruz",
+		  email: "Email@email.com",
 
-    },
-        {
-        name: "Jay Dela Cruz",
-        email: "Email@email.com",
+	 },
+		  {
+		  name: "Jay Dela Cruz",
+		  email: "Email@email.com",
 
-    },
-        {
-        name: "Jose Dela Cruz",
-        email: "Email@email.com",
+	 },
+		  {
+		  name: "Jose Dela Cruz",
+		  email: "Email@email.com",
 
-    },
-        {
-        name: "Marie Dela Cruz",
-        email: "Email@email.com",
-    }
+	 },
+		  {
+		  name: "Marie Dela Cruz",
+		  email: "Email@email.com",
+	 }
 ])
 
 const filteredList = computed(() => {
-    return students.value.filter((student) =>
-        student.name.toLowerCase().includes(input.value.toLowerCase())
-    );
+	 return students.value.filter((student) =>
+		  student.name.toLowerCase().includes(input.value.toLowerCase())
+	 );
 })
 </script>
