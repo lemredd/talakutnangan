60ff12e3fec8e1eba8cf3684ef79bdffbc09bd70
lemredd <lmoa.jhdp@gmail.com>
2022-06-28 23:03:30 +0800

    refactor(dean): provide roles as filter list

--- pages/studemp_list/index.page.vue
+++ pages/studemp_list/index.page.vue
@@ -8,8 +8,18 @@
 import UsersManager from "@/user_management/UsersManager.vue";
 import { ManagerKind } from "@/user_management/types";
 import { users } from "./data"
-import { provide } from "vue";
+import { onBeforeMount, provide, ref } from "vue";
 
 const managerKind = "dean" as ManagerKind
 provide("managerKind", managerKind)
+
+const roles = ref<string[]>([])
+
+onBeforeMount(function() {
+    const rolesNoDuplicate = new Set([...roles.value])
+    users.forEach(user => rolesNoDuplicate.add(user.role))
+
+    roles.value = [...rolesNoDuplicate]
+})
+provide("filterList", roles)
 </script>
