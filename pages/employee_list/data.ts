9876681f54ea6d5e7d25f28debd60de6fc08effe
lemredd <lmoa.jhdp@gmail.com>
2022-06-28 22:35:37 +0800

    chore: change role to job title

--- pages/employee_list/data.ts
+++ pages/employee_list/data.ts
@@ -3,36 +3,42 @@ export const users = [
 		id: 0,
 		email: "Email@email.com",
 		name: "Juan Dela Cruz",
-		role: "student"
+		jobTitle: "Cashier"
 	},
 	{
 		id: 1,
 		email: "Email@email.com",
 		name: "Alice Dela Cruz",
-		role: "professor"
+		jobTitle: "Registrar"
 	},
 	{
 		id: 2,
 		email: "Email@email.com",
 		name: "Jun Dela Cruz",
-		role: "professor"
+		jobTitle: "Health Services"
 	},
 	{
 		id: 3,
 		email: "Email@email.com",
 		name: "Jay Dela Cruz",
-		role: "professor"
+		jobTitle: "Admission"
 	},
 	{
 		id: 4,
 		email: "Email@email.com",
 		name: "Jose Dela Cruz",
-		role: "secretary"
+		jobTitle: "Registrar"
 	},
 	{
 		id: 5,
 		email: "Email@email.com",
 		name: "Marie Dela Cruz",
-		role: "student"
+		jobTitle: "Cashier"
+	},
+	{
+		id: 6,
+		email: "Email@email.com",
+		name: "Lorem Dela Cruz",
+		jobTitle: "Health Services"
 	},
 ]
