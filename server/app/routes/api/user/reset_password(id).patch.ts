164e2a05c26ee7b439079ddc99bd37ab6cb9e530
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-29 23:25:05 +0800

    chore(permission): apply the usage of permission list

--- server/app/routes/api/user/reset_password(id).patch.ts
+++ server/app/routes/api/user/reset_password(id).patch.ts
@@ -2,8 +2,8 @@ import type { AuthenticatedIDRequest, Response } from "!/types/dependent"
 
 import Policy from "!/bases/policy"
 import UserManager from "%/managers/user"
-import UserPermissions from "$/permissions/user_permissions"
 import { RESET_PASSWORD } from "$/permissions/user_combinations"
+import { user as permissionGroup } from "$/permissions/permission_list"
 import ModelBoundController from "!/common_controllers/model_bound_controller"
 import PermissionBasedPolicy from "!/middlewares/authentication/permission-based_policy"
 
@@ -11,7 +11,7 @@ export default class extends ModelBoundController {
 	get filePath(): string { return __filename }
 
 	get policy(): Policy {
-		return new PermissionBasedPolicy(new UserPermissions(), RESET_PASSWORD)
+		return new PermissionBasedPolicy(permissionGroup, RESET_PASSWORD)
 	}
 
 	async handle(request: AuthenticatedIDRequest, response: Response): Promise<void> {
--- t/integration/api/user/reset_password.patch.spec.ts
+++ t/integration/api/user/reset_password.patch.spec.ts
@@ -5,20 +5,18 @@ import User from "%/models/user"
 import RoleFactory from "~/factories/role"
 import UserFactory from "~/factories/user"
 import compare from "!/helpers/auth/compare"
-import UserPermissions from "$/permissions/user_permissions"
 import { RESET_PASSWORD } from "$/permissions/user_combinations"
+import { user as permissionGroup } from "$/permissions/permission_list"
 import Route from "!/app/routes/api/user/reset_password(id).patch"
 
 describe("PATCH /api/user/reset_password/:id", () => {
-	const userPermissions = new UserPermissions()
-
 	beforeAll(async () => {
 		await App.create(new Route())
 	})
 
 	it("can be accessed by permitted user and admit other user", async () => {
 		const adminRole = await new RoleFactory()
-			.userFlags(userPermissions.generateMask(...RESET_PASSWORD))
+			.userFlags(permissionGroup.generateMask(...RESET_PASSWORD))
 			.insertOne()
 		const { user: admin, cookie } = await App.makeAuthenticatedCookie(adminRole)
 		const student = await (new UserFactory()).insertOne()
@@ -35,7 +33,7 @@ describe("PATCH /api/user/reset_password/:id", () => {
 
 	it("cannot be accessed by not permitted users", async () => {
 		const otherRole = await new RoleFactory()
-			.userFlags(userPermissions.generateMask("view"))
+			.userFlags(permissionGroup.generateMask("view"))
 			.insertOne()
 		const { user: other, cookie } = await App.makeAuthenticatedCookie(otherRole)
 		const student = await (new UserFactory()).insertOne()
