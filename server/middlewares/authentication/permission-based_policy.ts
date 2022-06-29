6452c03211f1a8ad2f3f91ee117e2309cb8f704a
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-29 21:26:35 +0800

    intrn(middleware): improve the permission-based policy

--- server/middlewares/authentication/permission-based_policy.ts
+++ server/middlewares/authentication/permission-based_policy.ts
@@ -1,4 +1,6 @@
-import type { Request } from "!/types/dependent"
+import { deserialise } from "kitsu-core"
+import type { Serializable } from "$/types/database"
+import type { AuthenticatedRequest } from "!/types/dependent"
 
 import PermissionGroup from "$/permissions/base"
 import AuthenticationBasedPolicy from "!/middlewares/authentication/authentication-based_policy"
@@ -9,22 +11,26 @@ import AuthenticationBasedPolicy from "!/middlewares/authentication/authenticati
  * Automatically requires user to be authenticated.
  */
 export default class<T extends { [key:string]: number }, U> extends AuthenticationBasedPolicy {
+	private permissions: U[]
 	private permissionGroup: PermissionGroup<T, U>
 
 	/**
 	 * @param permissionGroup Specific permission which will dictate if user is allowed or not.
 	 */
-	constructor(permissionGroup: PermissionGroup<T, U>) {
+	constructor(permissionGroup: PermissionGroup<T, U>, permissions: U[]) {
 		super(true)
+		this.permissions = permissions
 		this.permissionGroup = permissionGroup
 	}
 
-	mayAllow(request: Request): boolean {
-		// TODO: Pass the role of the user
+	mayAllow(request: AuthenticatedRequest): boolean {
+		const user = deserialise(request.user)
+		const roles = user.data.roles.data
 		return super.mayAllow(request)
-			// && request.user.roles
-			// 	.reduce((previousPermission, role) => {
-			// 		return this.permissionGroup.mayAllow(role, this.permission)
-			// 	})
+			&& roles.reduce((previousPermission: boolean, role: Serializable) => {
+				return previousPermission || this.permissionGroup.mayAllow(
+					role as T,
+					...this.permissions)
+			}, false)
 	}
 }
