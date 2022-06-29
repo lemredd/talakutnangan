9e05d86cc3538c27ca3b8baf477a4aca71c9a8a1
lemredd <lmoa.jhdp@gmail.com>
2022-06-29 20:49:24 +0800

    feat(admin): add user management with dummy data

--- components/admin_settings/Users.vue
+++ components/admin_settings/Users.vue
@@ -1,8 +1,43 @@
 <template>
+	<UsersManager :users="users" />
 </template>
 
 <style>
 </style>
 
 <script setup lang="ts">
+import { onBeforeMount, onMounted, provide, ref } from "vue"
+import { deserialise } from "kitsu-core"
+
+import UsersManager from "@/user_management/UsersManager.vue"
+import type { ManagerKind } from "@/user_management/types"
+
+const managerKind = "admin" as ManagerKind
+provide("managerKind", managerKind)
+
+interface RawUser {
+	id: number,
+	name: string,
+	email: string,
+	kind: string,
+	signature: string
+}
+const users = ref<RawUser[]>([])
+const roles = ref<string[]>([])
+onMounted(() => {
+	// fetch("/api/user/list")
+	fetch("/dev/sample_user_list")
+	.then(response => response.json())
+	.then(response => {
+		const deserializedData = deserialise(response).data
+		const rolesNoDuplicate = new Set([...roles.value])
+		users.value = deserializedData
+
+		users.value.forEach((user: RawUser | any) => user.roles.data.forEach((role: any) => rolesNoDuplicate.add(role.name)))
+		roles.value = [...rolesNoDuplicate]
+		// Check the console for other available info from server
+		console.log(deserializedData)
+	})
+})
+provide("filterList", roles.value)
 </script>
