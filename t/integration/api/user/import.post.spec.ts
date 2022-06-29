286ffdd02256fd5357434a7c7f27e7b3b6552e4e
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-29 18:12:49 +0800

    intg(user): ensure to received errors for invalid file uploads

--- t/integration/api/user/import.post.spec.ts
+++ t/integration/api/user/import.post.spec.ts
@@ -31,5 +31,25 @@ describe("POST /api/user/import", () => {
 		expect(response.body.included).toHaveLength(4)
 	})
 
+	it("cannot upload by text field", async () => {
+		const role = await (new RoleFactory()).insertOne()
+		const IBCE = await (new DepartmentFactory().name(() => "I B C E")).insertOne()
+		const IASTE = await (new DepartmentFactory().name(() => "I A S T E")).insertOne()
+		const IHTM = await (new DepartmentFactory().name(() => "I H T M")).insertOne()
+		const { user: admin, cookie } = await App.makeAuthenticatedCookie()
+		const path = `${RequestEnvironment.root}/t/data/valid_student_details.csv`
+
+		const response = await App.request
+			.post("/api/user/import")
+			.field("kind", "student")
+			.field("roles[]", [ role.name ])
+			.field("importedCSV", path)
+			.set("Cookie", cookie)
+
+		expect(response.statusCode).toBe(RequestEnvironment.status.BAD_REQUEST)
+		expect(response.body).toHaveLength(1)
+		expect(response.body).toHaveProperty([ 0, "field" ], "importedCSV")
+	})
+
 	it.todo("can upload invalid student details")
 })
