af1ae503df504f2c40f8a6cb834e6fe9e24800c4
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-29 17:13:37 +0800

    intrn(validator): register buffer rule

--- server/app/auth/register_custom_validators.ts
+++ server/app/auth/register_custom_validators.ts
@@ -1,9 +1,11 @@
 import { extend } from "node-input-validator"
+import buffer from "!/app/auth/buffer"
 import acronym from "!/app/auth/acronym"
 
 export default async function() {
 	const customValidators: { [key: string]: (..._a: any[]) => Promise<boolean>|boolean }  = {
-		acronym
+		acronym,
+		buffer
 	}
 
 	for (const name in customValidators) {
