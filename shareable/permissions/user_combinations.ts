2492fcac978b6d3796476ceda4f5f9d6648466c3
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-29 23:36:13 +0800

    share(permissions): make other important combination of user permissions

--- shareable/permissions/user_combinations.ts
+++ shareable/permissions/user_combinations.ts
@@ -1,5 +1,30 @@
 import { Permissions } from "$/permissions/user_permissions"
 
+export const LIST_USERS_ON_OWN_DEPARTMENT: Permissions[] = [
+	"view",
+	"readDepartmentScope"
+]
+
+export const LIST_USERS_ON_ALL_DEPARTMENT: Permissions[] = [
+	"view",
+	"readOverallScope"
+]
+
+export const UPDATE_USERS_ON_OWN_DEPARTMENT: Permissions[] = [
+	"update",
+	"writeDepartmentScope"
+]
+
+export const UPDATE_USERS_ON_ALL_DEPARTMENT: Permissions[] = [
+	"update",
+	"writeOverallScope"
+]
+
+export const IMPORT_USERS: Permissions[] = [
+	"create",
+	"writeOverallScope"
+]
+
 export const RESET_PASSWORD: Permissions[] = [
 	"resetPassword",
 	"writeOverallScope"
