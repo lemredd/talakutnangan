e83f41f7b5b3ed21edff7dcbcfb7b3f3ae56c02a
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-06-30 22:27:26 +0800

    share(permission): make initial department permission combination

---
+++ shareable/permissions/department_combinations.ts
@@ -0,0 +1,21 @@
+import { Permissions } from "$/permissions/department_permissions"
+
+export const READ: Permissions[] = [
+	"view"
+]
+
+export const CREATE: Permissions[] = [
+	"create"
+]
+
+export const UPDATE: Permissions[] = [
+	"update"
+]
+
+export const ARCHIVE_AND_RESTORE: Permissions[] = [
+	"archiveAndRestore"
+]
+
+export const MERGE: Permissions[] = [
+	"mergeDepartment"
+]
