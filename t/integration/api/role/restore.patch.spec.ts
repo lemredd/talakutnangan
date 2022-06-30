8a39d337602820a02a0b4744ae8a5df70eb9e044
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-01 00:17:45 +0800

    intg(role): check if policies work in route to restore role

--- t/integration/api/role/restore.patch.spec.ts
+++ t/integration/api/role/restore.patch.spec.ts
@@ -1,8 +1,9 @@
-import { StatusCodes } from "http-status-codes"
-
 import App from "~/set-ups/app"
 import Role from "%/models/role"
 import RoleFactory from "~/factories/role"
+import RequestEnvironment from "!/helpers/request_environment"
+import { ARCHIVE_AND_RESTORE } from "$/permissions/role_combinations"
+import { role as permissionGroup } from "$/permissions/permission_list"
 
 import Route from "!/app/routes/api/role/restore(id).patch"
 
@@ -12,7 +13,10 @@ describe("PATCH /api/role/restore/:id", () => {
 	})
 
 	it("can be accessed by authenticated user", async () => {
-		const { user, cookie } = await App.makeAuthenticatedCookie()
+		const adminRole = await new RoleFactory()
+			.roleFlags(permissionGroup.generateMask(...ARCHIVE_AND_RESTORE))
+			.insertOne()
+		const { user, cookie } = await App.makeAuthenticatedCookie(adminRole)
 		const role = await (new RoleFactory()).insertOne()
 		const id = role.id
 		await role.destroy()
@@ -21,7 +25,7 @@ describe("PATCH /api/role/restore/:id", () => {
 			.patch(`/api/role/restore/${role.id}`)
 			.set("Cookie", cookie)
 
-		expect(response.statusCode).toBe(StatusCodes.NO_CONTENT)
+		expect(response.statusCode).toBe(RequestEnvironment.status.NO_CONTENT)
 		expect(response.body).toStrictEqual({})
 		expect((await Role.findOne({ where: { id } }))!.deletedAt).toBeNull()
 	})
@@ -29,12 +33,24 @@ describe("PATCH /api/role/restore/:id", () => {
 	it.todo("cannot restore non-existing")
 	it.todo("cannot restore existing")
 
+	it("cannot be accessed without correct permission", async () => {
+		const { user: admin, cookie } = await App.makeAuthenticatedCookie()
+		const role = await (new RoleFactory()).insertOne()
+		await role.destroy()
+
+		const response = await App.request
+			.patch(`/api/role/restore/${role.id}`)
+			.set("Cookie", cookie)
+
+		expect(response.statusCode).toBe(RequestEnvironment.status.UNAUTHORIZED)
+	})
+
 	it("cannot be accessed by guest users", async () => {
 		const role = await (new RoleFactory()).insertOne()
 
 		const response = await App.request
 			.patch(`/api/role/restore/${role.id}`)
 
-		expect(response.statusCode).toBe(StatusCodes.UNAUTHORIZED)
+		expect(response.statusCode).toBe(RequestEnvironment.status.UNAUTHORIZED)
 	})
 })
