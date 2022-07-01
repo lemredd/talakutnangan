b3fa5a2868b2b6ff0a053ede877eb8470fc3d24a
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-01 21:51:04 +0800

    intrn(back-end): make error class for authorization

--- common_back-end/errors/authorization.ts
+++ common_back-end/errors/authorization.ts
@@ -1,3 +1,17 @@
-export default class extends Error {
-	message = "You are not allowed to access the path."
+import BaseError from "$!/errors/base"
+
+/**
+ * Used when there is a problem on authorizing the client.
+ */
+export default class AuthorizationError extends BaseError {
+	static CODE: string = "1"
+	static STATUS: number = 401
+
+	constructor(message: string = "User is not allowed to invoke the action") {
+		super(
+			AuthorizationError.CODE,
+			AuthorizationError.STATUS,
+			"Authorization Error",
+			message)
+	}
 }
