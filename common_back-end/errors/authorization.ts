fbcb4377616b73fd0bbf546cfce9114a4fff394f
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-01 23:43:46 +0800

    refactor(back-end): use the request environment status for readability

--- common_back-end/errors/authorization.ts
+++ common_back-end/errors/authorization.ts
@@ -1,11 +1,12 @@
 import BaseError from "$!/errors/base"
+import RequestEnvironment from "$/helpers/request_environment"
 
 /**
  * Used when there is a problem on authorizing the client.
  */
 export default class AuthorizationError extends BaseError {
 	static CODE: string = "1"
-	static STATUS: number = 401
+	static STATUS: number = RequestEnvironment.status.UNAUTHORIZED
 
 	constructor(message: string = "User is not allowed to invoke the action") {
 		super(
