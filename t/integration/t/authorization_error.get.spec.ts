d08c17830a2b3263d61fd2cbc974ef0cd491ae03
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-01 23:55:01 +0800

    fix(test): correct the structure to be expected from server

--- t/integration/t/authorization_error.get.spec.ts
+++ t/integration/t/authorization_error.get.spec.ts
@@ -15,18 +15,16 @@ describe("GET /t/authorization_error", () => {
 			.accept("application/vnd.api+json")
 
 		expect(response.statusCode).toBe(RequestEnvironment.status.UNAUTHORIZED)
-		expect(response.body).toHaveProperty("data")
+		expect(response.body).toHaveProperty("errors")
 		expect(response.body).toStrictEqual({
-			data: {
-				errors: [
-					{
-						status: RequestEnvironment.status.UNAUTHORIZED,
-						code: "1",
-						title: "Authorization Error",
-						detail: "Sample authorization error"
-					}
-				]
-			}
+			errors: [
+				{
+					status: RequestEnvironment.status.UNAUTHORIZED,
+					code: "1",
+					title: "Authorization Error",
+					detail: "Sample authorization error"
+				}
+			]
 		})
 	})
 })
