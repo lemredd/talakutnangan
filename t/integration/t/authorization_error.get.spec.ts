8d69ed5542ab3df282e07eeda983e2889c98f89e
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-01 23:47:12 +0800

    intg(server): check if the error handler works as expected

---
+++ t/integration/t/authorization_error.get.spec.ts
@@ -0,0 +1,32 @@
+import RequestEnvironment from "!/helpers/request_environment"
+
+import App from "~/set-ups/app"
+
+import Route from "!/app/routes/t/authorization_error.get"
+
+describe("GET /t/authorization_error", () => {
+	beforeAll(async () => {
+		await App.create(new Route(), false)
+	})
+
+	it("should output error properly", async () => {
+		const response = await App.request
+			.get("/t/authorization_error")
+			.accept("application/vnd.api+json")
+
+		expect(response.statusCode).toBe(RequestEnvironment.status.UNAUTHORIZED)
+		expect(response.body).toHaveProperty("data")
+		expect(response.body).toStrictEqual({
+			data: {
+				errors: [
+					{
+						status: RequestEnvironment.status.UNAUTHORIZED,
+						code: "1",
+						title: "Authorization Error",
+						detail: "Sample authorization error"
+					}
+				]
+			}
+		})
+	})
+})
