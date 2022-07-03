91be13c74cd98915590bac02408c73f79f6670b8
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-03 14:32:40 +0800

    intrn(back-end): allow supplying the redirect URL

--- common_back-end/errors/base.ts
+++ common_back-end/errors/base.ts
@@ -3,13 +3,23 @@ import type { UnitError } from "$/types/server"
 export default class extends Error {
 	private code: string
 	private status: number
+	private redirectLink: string|null
 
-	constructor(code: string, status: number, title: string, message: string) {
+	constructor(
+		code: string,
+		status: number,
+		title: string,
+		message: string,
+		redirectLink: string|null = null
+	) {
 		super(message)
 		this.code = code
 		this.status = status
 		this.name = title
+		this.redirectLink = redirectLink
 	}
+
+	get redirectURL(): string|null { return this.redirectLink }
 
 	toJSON(): UnitError {
 		return {
