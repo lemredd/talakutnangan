936140c86c591f303422e5ebf791e08f66751863
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-04 21:52:21 +0800

    intrn(renderer): ensure the rendered error page remains after hydration

--- renderer/_default.page.client.ts
+++ renderer/_default.page.client.ts
@@ -1,8 +1,11 @@
 import "virtual:windi.css"
 import { getPage } from "vite-plugin-ssr/client"
-import { createApp } from "./app"
-import type { PageContext } from "./types"
 import type { PageContextBuiltInClient } from "vite-plugin-ssr/client"
+
+import type { PageContext } from "#/types"
+
+import { createApp } from "#/app"
+import ErrorPage from "#/_error.page.vue"
 
 hydrate()
 
@@ -10,6 +13,15 @@ async function hydrate() {
 	// We do Server Routing, but we can also do Client Routing by using `useClientRouter()`
 	// instead of `getPage()`, see https://vite-plugin-ssr.com/useClientRouter
 	const pageContext = await getPage<PageContextBuiltInClient & PageContext>()
+
+	if (pageContext.pageProps.parsedUnitError) {
+		pageContext.Page = ErrorPage
+		pageContext.pageProps = {
+			is404: false,
+			...pageContext.pageProps
+		}
+	}
+
 	const app = createApp(pageContext)
 	app.mount("#app")
 }
