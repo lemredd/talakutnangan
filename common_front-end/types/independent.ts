87c767e1fde9251b554df8196ee46afb8e3e6da6
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-15 21:11:43 +0800

    intrn(client): restructure some authenticated-only link information

---
+++ common_front-end/types/independent.ts
@@ -0,0 +1,5 @@
+/**
+ * @module IndependentClientTypes
+ * @description This module contains types that are independent from any third-party package. But
+ * cannot be shareable since they are only used within the client.
+ */
