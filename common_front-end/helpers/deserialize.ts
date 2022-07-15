5767330afc8aae593612bbf44fef85a4f307ab39
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-15 22:06:35 +0800

    intrn(client): pass through the null values from deserialization

--- common_front-end/helpers/deserialize.ts
+++ common_front-end/helpers/deserialize.ts
@@ -1,6 +1,6 @@
 import { deserialise } from "kitsu-core"
 import type { Serializable } from "$/types/database"
 
-export default function(serializedInfo: Serializable): Serializable {
-	return deserialise(serializedInfo)
+export default function(serializedInfo: Serializable|null): Serializable|null {
+	return deserialise(serializedInfo) || null
 }
