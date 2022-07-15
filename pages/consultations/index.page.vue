9c2a345b3aa531461b5e527cc7492766d84b0000
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-07-16 01:32:32 +0800

    feat(chat): allow consultation selection

--- pages/consultations/index.page.vue
+++ pages/consultations/index.page.vue
@@ -18,7 +18,7 @@
 					<img class="mx-auto" :src="SadIcon" />
 					<h2>There are no consultations yet...</h2>
 				</div>
-				<div v-for="consultation in consultations" class="consultation p-2 grid grid-rows-2 grid-cols-[repeat(2,minmax(0,max-content))] hover:bg-gray-300 justify-between">
+				<div v-for="consultation in consultations" @click="pickConsultation(consultation.id)" class="consultation p-2 grid grid-rows-2 grid-cols-[repeat(2,minmax(0,max-content))] hover:bg-gray-300 justify-between">
 					<!-- TODO: should contain profile picture?  -->
 					<h3 class="consultation-title col-span-full font-400">{{ consultation.title }}</h3>
 					<small class="last-chat span">
@@ -33,9 +33,50 @@
 			</div>
 		</section>
 
-		<section class="selected-consultation right">
-			placeholder
-			<!-- TODO(mobile): should view once consultation is clicked in picker (by route) -->
+		<section class="selected-consultation flex flex-col right">
+			<div class="selected-consultation-header border-b p-3 grid grid-rows-2 grid-cols-[repeat(2,minmax(0,max-content))] justify-between">
+				<div class="selected-consultation-title">
+					{{ selectedConsultation.title }}
+				</div>
+				<div class="selected-consultation-user-status row-start-2">
+					User is online
+					<!-- TODO: must base on user active status -->
+				</div>
+				<div class="controls row-span-full self-center">
+					<button class="material-icons">video_camera_back</button> <!-- TODO: Apply functionality -->
+					<button class="material-icons">more_horiz</button> <!-- TODO: Apply functionality -->
+				</div>
+			</div>
+			<div class="selected-consultation-chats px-3 py-5 flex-1 overflow-y-scroll">
+				<div class="selected-consultation-new">
+					<p><strong>This is a new consultation.</strong> here are some additional details</p>
+					<ul class="selected-consultation-additional-details bg-gray-300 p-5">
+						<li>Ticket: {{ selectedConsultation.ticket }}</li>
+						<li>Status: {{ selectedConsultation.status }}</li>
+						<li><a href="#">View printable form (PDF)</a></li> <!-- TODO: Apply functionality -->
+					</ul>
+				</div>
+
+				<div class="chat-entry" v-for="chat in selectedConsultation.chats">
+					{{ chat }}
+				</div>
+				<!-- TODO_others: properly place chat entries -->
+			</div>
+			<div class="user-controls border-t p-3 flex">
+					<div class="left-controls">
+						<button class="material-icons">more_horiz</button> <!-- TODO: Apply functionality -->
+						<button class="material-icons">photo_camera</button> <!-- TODO: Apply functionality -->
+						<button class="material-icons">image</button> <!-- TODO: Apply functionality -->
+					</div>
+					<div class="message-box flex-1 border">
+						<input type="text">
+					</div>
+					<div class="right-controls">
+						<button class="material-icons">sentiment_satisfied</button> <!-- TODO: Apply functionality -->
+						<button class="material-icons">send</button> <!-- TODO: Apply functionality -->
+					</div>
+				</div>
+			<!-- TODO_others(mobile): should view once consultation is clicked in picker (by route) -->
 		</section>
 	</div>
 </template>
@@ -64,7 +105,6 @@ footer {
 .consultations-container {
 	section {
 		@include useContentBaseHeight;
-		overflow-y: auto;
 
 		&.left {
 			min-width: 100%;
@@ -90,7 +130,7 @@ footer {
 		min-width: 40% !important;
 	}
 	.right {
-		display: initial !important;
+		display: flex !important;
 	}
 }
 </style>
@@ -99,10 +139,6 @@ footer {
 import SadIcon from "./sadicon.png"
 import { computed, ref } from "vue"
 let input = ref("")
-import { ref } from "vue";
-let input = ref("");
-const consult = [
-	{
 
 type Consultation = {
 	// TODO: type will change soon
@@ -123,9 +159,58 @@ const consultations = ref<Consultation[]>([
 		employeeId: 0,
 		ticket: 0,
 		title: "Sample Consultation",
+		chats: [
+			"sample chat",
+			"sample chat",
+			"sample chat",
+			"sample chat",
+			"sample chat",
+			"sample chat",
+			"sample chat",
+			"sample chat",
+			"sample chat",
+			"sample chat",
+			"sample chat",
+			"sample chat",
+			"sample chat",
+			"sample chat",
+			"sample chat",
+			"sample chat",
+			"sample chat",
+			"sample chat",
+			"sample chat",
+			"sample chat",
+			"sample chat",
+			"sample chat",
+			"sample chat",
+			"sample chat",
+			"sample chat",
+			"sample chat",
+			"sample chat",
+			"sample chat",
+			"sample chat",
+			"sample chat",
+			"sample chat",
+			"sample chat",
+			"sample chat",
+			"sample chat",
+			"sample chat",
+			"sample chat",
+			"sample chat"
+		],
 		isGroup: false,
 		status: "ongoing"
 	}
 ])
+const selectedConsultationId = ref<number>(0)
+const selectedConsultation = computed(function() {
+	const selected = consultations.value.filter(function(consultation: Consultation) {
+		return consultation.id === selectedConsultationId.value
+	})
+
+	return selected[0]
+})
+function pickConsultation(consultationId: number) {
+	selectedConsultationId.value = consultationId
 }
 </script>
