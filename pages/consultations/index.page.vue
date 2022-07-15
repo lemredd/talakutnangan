c597dd7c94b7e0f8d5a3e0df0be584b48f3bc865
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-07-15 22:31:22 +0800

    rfmt(chat): rename page folder

---
+++ pages/consultations/index.page.vue
@@ -0,0 +1,104 @@
+<template>
+	<div class="consultations-container flex">
+		<section class="left">
+			<div class="">
+				<div class="p-3 md:justify-end md:flex" >
+						<p class="r">Consultations</p>
+						<button class="material-icons flex">arrow_back_ios</button>
+						<button class="material-icons flex">search</button>
+						<button class="material-icons flex">add</button>
+				</div>
+				<img class="img" :src="SadIcon" />
+				<p>Click add button to create a consultation</p>
+				<br><p>The overflow property controls what happens to content that is too big to fit into an area.</p>
+						<br><p>The overflow property controls what happens to content that is too big to fit into an area.</p>
+						<br><p>The overflow property controls what happens to content that is too big to fit into an area.</p>
+						<br><p>The overflow property controls what happens to content that is too big to fit into an area.</p>
+						<br><p>The overflow property controls what happens to content that is too big to fit into an area.</p>
+						<br><p>The overflow property controls what happens to content that is too big to fit into an area.</p>
+						<br><p>The overflow property controls what happens to content that is too big to fit into an area.</p>
+						<br><p>The overflow property controls what happens to content that is too big to fit into an area.</p>
+						<br><p>The overflow property controls what happens to content that is too big to fit into an area.</p>
+						<br><p>The overflow property controls what happens to content that is too big to fit into an area.</p>
+						<br><p>The overflow property controls what happens to content that is too big to fit into an area.</p>
+						<br><p>The overflow property controls what happens to content that is too big to fit into an area.</p>
+						<br><p>The overflow property controls what happens to content that is too big to fit into an area.</p>
+						<br><p>The overflow property controls what happens to content that is too big to fit into an area.</p>
+						<br><p>The overflow property controls what happens to content that is too big to fit into an area.</p>
+			</div>
+		</section>
+
+		<section class="right">
+			<p>Sample</p>
+			<br><p>The overflow property controls what happens to content that is too big to fit into an area.</p>
+			<br><p>The overflow property controls what happens to content that is too big to fit into an area.</p>
+			<br><p>The overflow property controls what happens to content that is too big to fit into an area.</p>
+			<br><p>The overflow property controls what happens to content that is too big to fit into an area.</p>
+			<br><p>The overflow property controls what happens to content that is too big to fit into an area.</p>
+			<br><p>The overflow property controls what happens to content that is too big to fit into an area.</p>
+			<br><p>The overflow property controls what happens to content that is too big to fit into an area.</p>
+			<br><p>The overflow property controls what happens to content that is too big to fit into an area.</p>
+			<br><p>The overflow property controls what happens to content that is too big to fit into an area.</p>
+			<br><p>The overflow property controls what happens to content that is too big to fit into an area.</p>
+			<br><p>The overflow property controls what happens to content that is too big to fit into an area.</p>
+			<br><p>The overflow property controls what happens to content that is too big to fit into an area.</p>
+			<br><p>The overflow property controls what happens to content that is too big to fit into an area.</p>
+			<br><p>The overflow property controls what happens to content that is too big to fit into an area.</p>
+			<br><p>The overflow property controls what happens to content that is too big to fit into an area.</p>
+			<br><p>The overflow property controls what happens to content that is too big to fit into an area.</p>
+		</section>
+	</div>
+</template>
+
+<style lang="scss">
+.content {
+	padding: 0 !important;
+
+	.container {
+		max-width: none;
+		margin: 0 !important;
+	}
+}
+footer {
+	display: none !important;
+}
+</style>
+<style scoped lang="scss">
+.consultations-container {
+	section {
+		height: calc(100vh - 20px - 56px);
+		overflow-y: auto;
+
+		&.left {
+			flex: 1;
+
+			width: min-content;
+		}
+		&.right {
+		}
+	}
+}
+
+.r {
+	margin-right:130px;
+}
+
+.img{
+	position:relative;
+	margin-top: 200px;;
+}
+</style>
+
+<script setup lang="ts">
+import { ref } from "vue";
+let input = ref("");
+const consult = [
+	{
+
+	}
+];
+function data() {
+	return consult;
+}
+import SadIcon from "./sadicon.png"
+</script>
