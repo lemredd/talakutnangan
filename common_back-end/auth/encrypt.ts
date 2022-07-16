6037d26f44fb72aea603c74212e15a8f8be63805
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-16 20:44:10 +0800

    intrn(back-end): make encrypt function

---
+++ common_back-end/auth/encrypt.ts
@@ -0,0 +1,26 @@
+import { promisify } from "node:util"
+
+export default async function(data: string): Promise<string> {
+	const {
+		scrypt,
+		createCipheriv
+	} = await import("node:crypto")
+
+	const algorithm = process.env.SECURITY_ENCRYPTION_ALGORITHM || "aes-128-cbc"
+	const salt = process.env.SECURITY_ENCRYPTION_SALT || "01234567890123456789012345678901"
+	const password = process.env.SECURITY_ENCRYPTION_PASSWORD || "01234567890123456789012345678901"
+	const generateKey = promisify(scrypt)
+	const key = await generateKey(password, salt, 16)
+	const cipher = createCipheriv(algorithm, key as Buffer, null)
+
+	return await new Promise<string>(resolve => {
+		cipher.setEncoding("hex")
+
+		let encryptedText = ""
+		cipher.on("data", encryptedData => encryptedText += encryptedData);
+		cipher.on("end", () => resolve(encryptedText));
+
+		cipher.write(data)
+		cipher.end()
+	})
+}
