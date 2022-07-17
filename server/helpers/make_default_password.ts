4c5125056a545ca1e5b8bda1d2df9a1bd9b6033c
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-17 16:07:53 +0800

    intrn(server): prepare function for making default password

---
+++ server/helpers/make_default_password.ts
@@ -0,0 +1,9 @@
+import type { UserProfile } from "$/types/common_front-end"
+
+/**
+ * Helper function to generate default password.
+ * @param userProfile Profile of the user where to derive the password.
+ */
+export default function(userProfile: UserProfile): string {
+	return ""
+}
