2d838fbd87b9f789701fc40d5c7b39c583271293
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-17 20:42:03 +0800

    intrn(transformer): make transformer for student details

---
+++ database/transformers/student_detail.ts
@@ -0,0 +1,18 @@
+import type { AttributesObject, TransformerOptions } from "%/types/dependent"
+
+import Transformer from "%/transformers/base"
+import Serializer from "%/transformers/serializer"
+import StudentDetail from "%/models/student_detail"
+
+export default class extends Transformer<StudentDetail, void> {
+	constructor() {
+		super()
+		this.type = "student_detail"
+	}
+
+	transform(model: StudentDetail|StudentDetail[], options: TransformerOptions): AttributesObject {
+		const safeObject = Serializer.whitelist(model, [ "studentNumber" ])
+
+		return safeObject
+	}
+}
