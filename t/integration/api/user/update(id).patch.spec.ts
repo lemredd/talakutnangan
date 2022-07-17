f6104792e6955a74a2f23979339a5deb6ead5a90
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-17 12:25:40 +0800

    test(user): ensure other users can update other users if permitted

--- t/integration/api/user/update(id).patch.spec.ts
+++ t/integration/api/user/update(id).patch.spec.ts
@@ -10,8 +10,11 @@ import RoleFactory from "~/factories/role"
 import User from "%/models/user"
 import Transport from "!/helpers/email/transport"
 import Route from "!/app/routes/api/user/update(id).patch"
-import { UPDATE_OWN_DATA } from "$/permissions/user_combinations"
 import { user as permissionGroup } from "$/permissions/permission_list"
+import {
+	UPDATE_OWN_DATA,
+	UPDATE_ANYONE_ON_OWN_DEPARTMENT
+} from "$/permissions/user_combinations"
 
 describe("PATCH /api/user/update/:id", () => {
 	beforeAll(async () => {
@@ -55,6 +58,29 @@ describe("PATCH /api/user/update/:id", () => {
 		expect(previousMessages[0]).toHaveProperty("message.subject", "Email Verification")
 		expect(previousMessages[0].message.text).toContain(updatedStudent!.email)
 		expect(previousMessages[0].message.text).toContain("/user/verify")
+	})
+
+	it("can be accessed by department head and update others e-mail", async () => {
+		const headRole = await new RoleFactory()
+			.userFlags(permissionGroup.generateMask(...UPDATE_ANYONE_ON_OWN_DEPARTMENT))
+			.insertOne()
+
+		const { user: head, cookie } = await App.makeAuthenticatedCookie(headRole)
+		const student = await (new UserFactory()).insertOne()
+		const newStudent = await (new UserFactory()).makeOne()
+
+		const response = await App.request
+			.patch(`/api/user/update/${student.id}`)
+			.field("name", newStudent.name)
+			.field("email", student.email)
+			.set("Cookie", cookie)
+			.accept(JSON_API_MEDIA_TYPE)
+
+		expect(response.statusCode).toBe(RequestEnvironment.status.NO_CONTENT)
+
+		const updatedStudent = await User.findOne({ where: { id: student.id }})
+		expect(updatedStudent!.emailVerifiedAt).not.toBeNull()
+		expect(updatedStudent!.name).toBe(newStudent.name)
 	})
 
 	// it.todo("can be accessed by permitted user and admit multiple users")
