ccde021d7d7d3c17d2178cae0705f1a4dff4ca65
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-17 15:10:51 +0800

    test(email): make dev route to test the email verification

--- server/app/routes/dev/router.ts
+++ server/app/routes/dev/router.ts
@@ -1,13 +1,12 @@
 import Router from "!/bases/router"
 import GetLogInAsAdminRoute from "!/app/routes/dev/log_in_as_admin.get"
-import GetSampleServerErrorRoute from "!/app/routes/dev/sample_server_error.get"
 import GetSampleUserListRoute from "!/app/routes/dev/sample_user_list.get"
 import GetEmailVerificationRoute from "!/app/routes/dev/email_verification.get"
+import GetSampleServerErrorRoute from "!/app/routes/dev/sample_server_error.get"
+import GetMakeUnverifiedUserRoute from "!/app/routes/dev/make_unverified_user.get"
 import GetNewUserNotificationRoute from "!/app/routes/dev/new_user_notification.get"
 
 export default class extends Router {
-	get prefix():string { return "/dev" }
-
 	constructor() {
 		super()
 
@@ -16,6 +15,7 @@ export default class extends Router {
 			new GetSampleUserListRoute(),
 			new GetSampleServerErrorRoute(),
 			new GetEmailVerificationRoute(),
+			new GetMakeUnverifiedUserRoute(),
 			new GetNewUserNotificationRoute()
 		])
 	}
