3e3198c3d38ff979defe17354acab3d1630ba4b3
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-17 18:53:54 +0800

    unit(helper): make sure default password for employees is correct

--- server/helpers/make_default_password.spec.ts
+++ server/helpers/make_default_password.spec.ts
@@ -30,4 +30,45 @@ describe("Helpers: Make default password", () => {
 
 		expect(password).toBe("0000-000")
 	})
+
+	it("can generate for reachable employee", () => {
+		const userProfile: UserProfile = {
+			data: {
+				type: "user",
+				id: 1,
+				name: "",
+				email: "hello.world@example.com",
+				kind: "reachable_employee",
+				roles: {
+					data: []
+				},
+				employeeSchedules: {
+					data: []
+				}
+			},
+		}
+
+		const password = makeDefaultPassword(userProfile)
+
+		expect(password).toBe("hello.world")
+	})
+
+	it("can generate for unreachable employee", () => {
+		const userProfile: UserProfile = {
+			data: {
+				type: "user",
+				id: 1,
+				name: "",
+				email: "foo.bar@example.com",
+				kind: "unreachable_employee",
+				roles: {
+					data: []
+				}
+			},
+		}
+
+		const password = makeDefaultPassword(userProfile)
+
+		expect(password).toBe("foo.bar")
+	})
 })
