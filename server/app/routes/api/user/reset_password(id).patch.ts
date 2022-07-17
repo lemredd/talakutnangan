c925b60ad123fe664a935e913796657e1d325f88
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-17 19:11:04 +0800

    feat(user): use the default password for password reset

--- server/app/routes/api/user/reset_password(id).patch.ts
+++ server/app/routes/api/user/reset_password(id).patch.ts
@@ -1,7 +1,10 @@
+import type { UserProfile } from "$/types/common_front-end"
 import type { AuthenticatedIDRequest, Response } from "!/types/dependent"
 
 import Policy from "!/bases/policy"
 import UserManager from "%/managers/user"
+import deserialize from "$/helpers/deserialize"
+import makeDefaultPassword from "!/helpers/make_default_password"
 import { RESET_PASSWORD } from "$/permissions/user_combinations"
 import { user as permissionGroup } from "$/permissions/permission_list"
 import ModelBoundController from "!/common_controllers/model_bound_controller"
@@ -18,9 +21,9 @@ export default class extends ModelBoundController {
 
 	async handle(request: AuthenticatedIDRequest, response: Response): Promise<void> {
 		const manager = new UserManager()
-		const { id } = request.params
-		// TODO: Generate password using student number or random
-		const newPassword = "12345678"
+		const id = +request.params.id
+		const userProfile = deserialize(await manager.findWithID(id)) as UserProfile
+		const newPassword = makeDefaultPassword(userProfile)
 		const isSuccess = await manager.resetPassword(+id, newPassword)
 
 		if (isSuccess) {
