343c06b16367f68653cef618b0b962a65498becc
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-17 16:24:30 +0800

    types(back-end): add specific types of profile data

--- shareable/types/common_front-end.ts
+++ shareable/types/common_front-end.ts
@@ -4,7 +4,7 @@
  * from other packages. However, they can be used by the other parts of the repository.
  */
 
-import type { Serializable, UserKind } from "$/types/database"
+import type { Serializable, Day } from "$/types/database"
 
 /**
  * Shape of serialized roles
@@ -21,12 +21,44 @@ interface Roles extends Serializable {
  * Shape of serialized user profile if authenticated
  */
 export interface UserProfile extends Serializable {
-	data: Serializable & {
-		type: "user",
-		id: number,
-		kind: UserKind,
-		name: string,
-		email: string,
-		roles: Roles
+	data: StudentProfileData | ReachableEmployeeProfileData | UnreachableEmployeeProfileData
+}
+
+interface ProfileData extends Serializable {
+	type: "user",
+	id: number,
+	name: string,
+	email: string,
+	roles: Roles
+}
+
+/**
+ * Shape of profile data for students
+ */
+export interface StudentProfileData extends ProfileData {
+	kind: "student",
+	studentDetail: {
+		studentNumber: number
 	}
+}
+
+interface EmployeeSchedule extends Serializable {
+	scheduleStart: number,
+	scheduleEnd: number,
+	dayName: Day
+}
+
+/**
+ * Shape of profile data for reachable employees
+ */
+export interface ReachableEmployeeProfileData extends ProfileData {
+	kind: "reachable_employee",
+	employeeSchedules: EmployeeSchedule[]
+}
+
+/**
+ * Shape of profile data for unreachable employees
+ */
+export interface UnreachableEmployeeProfileData extends ProfileData {
+	kind: "unreachable_employee"
 }
