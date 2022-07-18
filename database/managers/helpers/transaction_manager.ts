391e098685ecbc295bee15cda4c3e84713c2cd06
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-18 14:37:41 +0800

    intrn(manager): make a transaction manager

--- database/managers/helpers/transaction_manager.ts
+++ database/managers/helpers/transaction_manager.ts
@@ -6,29 +6,31 @@ import Database from "%/data_source/database"
  * Manages the transaction to be implementation-agnostic
  */
 export default class {
-	private transaction: Transaction|null = null
+	private currentTransaction: Transaction|null = null
 
 	async initialize() {
-		if (this.transaction === null) {
+		if (this.currentTransaction === null) {
 			// For informred decision, please read:
 			// https://medium.com/nerd-for-tech/understanding-database-isolation-levels-c4ebcd55c6b9
-			this.transaction = await Database.dataSource.transaction({
+			this.currentTransaction = await Database.dataSource.currentTransaction({
 				isolationLevel: Transaction.ISOLATION_LEVELS.READ_COMMITTED
 			})
 		}
 	}
 
+	get transaction(): Transaction { return this.currentTransaction }
+
 	async destroySuccessfully() {
-		if (this.transaction !== null) {
-			await this.transaction.commit()
-			this.transaction = null
+		if (this.currentTransaction !== null) {
+			await this.currentTransaction.commit()
+			this.currentTransaction = null
 		}
 	}
 
 	async destroyIneffectually() {
-		if (this.transaction !== null) {
-			await this.transaction.rollback()
-			this.transaction = null
+		if (this.currentTransaction !== null) {
+			await this.currentTransaction.rollback()
+			this.currentTransaction = null
 		}
 	}
 }
