a6c01ce1c7bc9d3875ae90c1ae2430e765c903ec
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-19 17:20:28 +0800

    test: make a test route for ID parameter validation

---
+++ server/app/routes/t/validation/id_parameter(id).get.ts
@@ -0,0 +1,26 @@
+import type { Request, Response } from "!/types/dependent"
+
+import Validation from "!/bases/validation"
+import UserManager from "%/managers/user"
+import Controller from "!/bases/controller-likes/controller"
+import IDParameterValidation from "!/middlewares/authorization/id_parameter_validation"
+
+export default class extends Controller {
+	get filePath(): string { return __filename }
+
+	get policy(): null { return null }
+
+	get bodyParser(): null { return null }
+
+	get validations(): Validation[] {
+		return [
+			new IDParameterValidation([
+				[ "id", UserManager ]
+			])
+		]
+	}
+
+	async handle(request: Request, response: Response): Promise<void> {
+		response.status(this.status.OK)
+	}
+}
