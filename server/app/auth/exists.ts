6e92fa28b94ce23ec2e16637dafd1c43a0649c92
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-19 16:24:26 +0800

    intrn(validator): make validator to check if data exists

---
+++ server/app/auth/exists.ts
@@ -0,0 +1,34 @@
+import type { Validator } from "node-input-validator"
+
+/**
+ * Function to validate if the value exists in the database.
+ *
+ * Accepts table name and column name to check if the value exists to that area.
+ */
+export default function(
+	{ value, args }: { value: any, args: any[] },
+	validator: Validator
+): boolean {
+	if (value === undefined) {
+		if (args.length < 2) {
+			throw new Error("Number of arguments passed to `exists` rule is insufficient")
+		}
+
+		const rawManager = args[0]
+		const columnName = args[1]
+
+		try {
+			// TODO: Get transaction manager from cache
+			const manager = new rawManager()
+			const foundModel = manager.findOneOnColumn(columnName, value)
+
+			// TODO: Store found model in cache
+			return foundModel.data
+		} catch(error) {
+			throw new Error("Manager cannot be instantiated")
+		}
+	} else {
+		// Skip the validation if the field does not exists
+		return true
+	}
+}
