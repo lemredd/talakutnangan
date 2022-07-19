099033f10e6a916d5887a364de1b70b129bb9ceb
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-19 22:29:54 +0800

    intrn(communicator): add method to post the data

--- common_front-end/communicators/fetcher.ts
+++ common_front-end/communicators/fetcher.ts
@@ -1,9 +1,11 @@
 import type { Axios } from "axios"
 import axios from "axios"
 
+import type { Response } from "$@/types/independent"
+import { JSON_API_MEDIA_TYPE } from "!/types/independent"
+
 import RequestEnvironment from "$!/singletons/request_environment"
 
-import type { Response } from "$@/types/independent"
 
 /**
  * General class to isolate the third-party library used for communicate from the rest of the
@@ -23,6 +25,29 @@ export default class extends RequestEnvironment {
 	static getJSON(baseURL: string): Promise<Response> {
 		return this.instance.request({
 			url: baseURL
+		})
+		.then(response => {
+			return {
+				body: response.data,
+				status: response.status
+			}
+		})
+		.catch(error => {
+			return {
+				body: error.response.data,
+				status: error.response.status
+			}
+		})
+	}
+
+	static postJSON(basePath: string, data: Serializable): Promise<Response> {
+		return this.instance.request({
+			url: basePath,
+			headers: {
+				"Content-Type": JSON_API_MEDIA_TYPE,
+				"Accept": JSON_API_MEDIA_TYPE
+			},
+			data
 		})
 		.then(response => {
 			return {
