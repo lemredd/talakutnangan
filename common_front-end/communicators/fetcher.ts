2f7306c6085e7a5e9b4784b44ce25fd250218e0c
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-19 21:41:39 +0800

    intrn(communicator): allow fetcher to initialize the fetcher for tests

--- common_front-end/communicators/fetcher.ts
+++ common_front-end/communicators/fetcher.ts
@@ -1,4 +1,7 @@
+import type { Axios } from "axios"
 import axios from "axios"
+
+import RequestEnvironment from "$!/singletons/request_environment"
 
 import type { Response } from "$@/types/independent"
 
@@ -6,9 +9,19 @@ import type { Response } from "$@/types/independent"
  * General class to isolate the third-party library used for communicate from the rest of the
  * client-side code.
  */
-export default class {
+export default class extends RequestEnvironment {
+	private static instance: Axios = axios.create()
+
+	static initialize() {
+		if (this.isOnTest) {
+			this.instance = axios.create({
+				url: "http://localhost:16000/"
+			})
+		}
+	}
+
 	static getJSON(baseURL: string): Promise<Response> {
-		return axios.request({
+		return this.instance.request({
 			url: baseURL
 		})
 		.then(response => {
