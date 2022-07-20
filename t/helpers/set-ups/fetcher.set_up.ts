77597e55ac60ee4ef31da61251b817e31b994dfb
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-20 14:49:39 +0800

    test(set up): initialize the user fetcher

--- t/helpers/set-ups/fetcher.set_up.ts
+++ t/helpers/set-ups/fetcher.set_up.ts
@@ -1,5 +1,7 @@
 import Fetcher from "$@/communicators/fetcher"
+import UserFetcher from "$@/communicators/user"
 
 beforeEach(() => {
 	Fetcher.initialize("/api")
+	UserFetcher.initialize("/api")
 })
