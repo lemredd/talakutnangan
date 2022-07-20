0f85fd6e90acadebfed4b09dcd114536415b70bc
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-20 08:54:19 +0800

    fix(communicator): use the correct environment

--- common_front-end/communicators/fetcher.ts
+++ common_front-end/communicators/fetcher.ts
@@ -2,7 +2,7 @@ import type { Serializable } from "$/types/database"
 import type { Response } from "$@/types/independent"
 import { JSON_API_MEDIA_TYPE } from "$/types/server"
 
-import RequestEnvironment from "$!/singletons/request_environment"
+import RequestEnvironment from "$/helpers/request_environment"
 
 /**
  * General class to isolate the third-party library used for communicate from the rest of the
