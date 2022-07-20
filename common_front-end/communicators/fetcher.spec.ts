d22a46609d1d321cbe002050665b5acb6c29485f
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-20 10:49:05 +0800

    unit(communicator): check if the archiving and restoring resource works

--- common_front-end/communicators/fetcher.spec.ts
+++ common_front-end/communicators/fetcher.spec.ts
@@ -54,6 +54,50 @@ describe("Communicator: Fetcher", () => {
 		})
 	})
 
+	it("can archive resource", async () => {
+		fetchMock.mockResponseOnce(JSON.stringify({
+			data: { type: "user", "attributes": {} },
+		}),
+		{ status: RequestEnvironment.status.OK })
+
+		const response = await Fetcher.archive("user", [ 1 ])
+
+		expect(response).toHaveProperty("body.data")
+		expect(response).toHaveProperty("status", RequestEnvironment.status.OK)
+		const request = (fetch as jest.Mock<any, any>).mock.calls[0][0]
+		expect(request).toHaveProperty("method", "DELETE")
+		expect(request).toHaveProperty("url", "/api/user/archive")
+		expect(request.headers.get("Content-Type")).toBe(JSON_API_MEDIA_TYPE)
+		expect(request.headers.get("Accept")).toBe(JSON_API_MEDIA_TYPE)
+		expect(request.json()).resolves.toStrictEqual({
+			data: [
+				{ type: "user", id: 1 }
+			]
+		})
+	})
+
+	it("can restore resource", async () => {
+		fetchMock.mockResponseOnce(JSON.stringify({
+			data: { type: "user", "attributes": {} },
+		}),
+		{ status: RequestEnvironment.status.OK })
+
+		const response = await Fetcher.restore("user", [ 2 ])
+
+		expect(response).toHaveProperty("body.data")
+		expect(response).toHaveProperty("status", RequestEnvironment.status.OK)
+		const request = (fetch as jest.Mock<any, any>).mock.calls[0][0]
+		expect(request).toHaveProperty("method", "PATCH")
+		expect(request).toHaveProperty("url", "/api/user/restore")
+		expect(request.headers.get("Content-Type")).toBe(JSON_API_MEDIA_TYPE)
+		expect(request.headers.get("Accept")).toBe(JSON_API_MEDIA_TYPE)
+		expect(request.json()).resolves.toStrictEqual({
+			data: [
+				{ type: "user", id: 2 }
+			]
+		})
+	})
+
 	it("can retrieve JSON from server by GET", async () => {
 		fetchMock.mockResponseOnce(JSON.stringify({
 			data: { type: "user", "attributes": {} },
@@ -98,6 +142,23 @@ describe("Communicator: Fetcher", () => {
 		expect(response).toHaveProperty("status", RequestEnvironment.status.OK)
 		const request = (fetch as jest.Mock<any, any>).mock.calls[0][0]
 		expect(request).toHaveProperty("method", "PATCH")
+		expect(request).toHaveProperty("url", "/api/sample/1")
+		expect(request.headers.get("Content-Type")).toBe(JSON_API_MEDIA_TYPE)
+		expect(request.headers.get("Accept")).toBe(JSON_API_MEDIA_TYPE)
+		expect(request.json()).resolves.toStrictEqual({ hello: "world" })
+	})
+
+	it("can retrieve JSON from server by DELETE", async () => {
+		fetchMock.mockResponseOnce(JSON.stringify({
+			data: { type: "department", "attributes": {} },
+		}), { status: RequestEnvironment.status.OK })
+
+		const response = await Fetcher.deleteJSON("sample/:id", { id: 1 }, { hello: "world" })
+
+		expect(response).toHaveProperty("body.data")
+		expect(response).toHaveProperty("status", RequestEnvironment.status.OK)
+		const request = (fetch as jest.Mock<any, any>).mock.calls[0][0]
+		expect(request).toHaveProperty("method", "DELETE")
 		expect(request).toHaveProperty("url", "/api/sample/1")
 		expect(request.headers.get("Content-Type")).toBe(JSON_API_MEDIA_TYPE)
 		expect(request.headers.get("Accept")).toBe(JSON_API_MEDIA_TYPE)
