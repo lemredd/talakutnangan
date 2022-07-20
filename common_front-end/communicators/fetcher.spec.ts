77c193efb9d2b37114e50516e5f321bed54eacf9
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-20 09:57:40 +0800

    unit(communicator): ensure creating and updating resource works properly

--- common_front-end/communicators/fetcher.spec.ts
+++ common_front-end/communicators/fetcher.spec.ts
@@ -2,6 +2,34 @@ import RequestEnvironment from "$/helpers/request_environment"
 import Fetcher from "./fetcher"
 
 describe("Communicator: Fetcher", () => {
+	it("can create resource", async () => {
+		fetchMock.mockResponseOnce(JSON.stringify({
+			data: { type: "user", "attributes": {} },
+		}),
+		{ status: RequestEnvironment.status.OK })
+
+		const response = await Fetcher.create("user", { name: "A" })
+
+		expect(response).toHaveProperty("body.data")
+		expect(response).toHaveProperty("status", RequestEnvironment.status.OK)
+		const request = (fetch as jest.Mock<any, any>).mock.calls[0][0]
+		expect(request).toHaveProperty("url", "/user/create")
+	})
+
+	it("can upate resource", async () => {
+		fetchMock.mockResponseOnce(JSON.stringify({
+			data: { type: "user", "attributes": {} },
+		}),
+		{ status: RequestEnvironment.status.OK })
+
+		const response = await Fetcher.update("user", 1, { name: "A" })
+
+		expect(response).toHaveProperty("body.data")
+		expect(response).toHaveProperty("status", RequestEnvironment.status.OK)
+		const request = (fetch as jest.Mock<any, any>).mock.calls[0][0]
+		expect(request).toHaveProperty("url", "/user/update/1")
+	})
+
 	it("can retrieve JSON from server by GET", async () => {
 		fetchMock.mockResponseOnce(JSON.stringify({
 			data: { type: "user", "attributes": {} },
