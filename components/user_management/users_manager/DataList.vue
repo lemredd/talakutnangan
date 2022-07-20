1d34d16db6a4ddf1f725feed7fabe22f45c93f53
AteKitty07 <kiritogregorio@gmail.com>
2022-07-20 16:36:22 +0800

    chore(user): generalize class names

--- components/user_management/users_manager/DataList.vue
+++ components/user_management/users_manager/DataList.vue
@@ -1,9 +1,9 @@
 <template>
 	<!-- TODO(lead): generalize data properties. Current setup only reads type `User` properties -->
 	<div class="data-row" v-for="data in filteredList" :key="data.name">
-		<span class="user-name">{{ data.name }}</span>
-		<span class="user-email">{{ data.email }}</span>
-		<span :class="`user-${roleOrJobTitleClasses}`">{{ isListForServiceEmployees ? data.jobTitle : data.role }}</span>
+		<span class="data-name">{{ data.name }}</span>
+		<span class="data-email">{{ data.email }}</span>
+		<span :class="`data-${roleOrJobTitleClasses}`">{{ isListForServiceEmployees ? data.jobTitle : data.role }}</span>
 		<div class="btns">
 			<button class="btn1">Update</button>
 		</div>
