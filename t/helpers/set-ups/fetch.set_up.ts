7dcc1df1264e20e3e2825dc7bdfd390f5cfc0c3a
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-20 10:17:00 +0800

    test(set up): reset the calls in fetch

--- t/helpers/set-ups/fetch.set_up.ts
+++ t/helpers/set-ups/fetch.set_up.ts
@@ -3,3 +3,9 @@ import FetchMock from "jest-fetch-mock"
 beforeAll(() => {
 	FetchMock.enableMocks()
 })
+
+beforeEach(() => {
+	// See https://www.npmjs.com/package/jest-fetch-mock#reset-mocks-between-tests-with-fetchresetmocks
+	// @ts-ignore
+	fetch.resetMocks()
+})
