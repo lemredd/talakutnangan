af5ffdf32b2235e83a8a16cf56718b8b05e17069
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-20 19:50:32 +0800

    intrn(validator): prevent acronym validation if source does not exist

--- server/app/auth/acronym.ts
+++ server/app/auth/acronym.ts
@@ -14,21 +14,25 @@ export default function({ value, args }: { value: string, args: string[] }, vali
 	const fieldName = args[0]
 	const source: string = validator.inputs[fieldName]
 
-	const separatedSource = source
-		.split(" ")
-		.filter(word => word.slice(0, 1) === word.slice(0, 1).toLocaleUpperCase())
-	const acronymSubstrings = value
-		.split("")
-		.reduce<string[]>((previousSubstrings, currentCharacter) => {
-			if(currentCharacter === currentCharacter.toLocaleUpperCase()) {
-				return [ ...previousSubstrings, currentCharacter ]
-			} else {
-				const lastString = previousSubstrings.pop() || ""
-				return [ ...previousSubstrings, lastString+currentCharacter ]
-			}
-		}, [])
+	if (source) {
+		const separatedSource = source
+			.split(" ")
+			.filter(word => word.slice(0, 1) === word.slice(0, 1).toLocaleUpperCase())
+		const acronymSubstrings = value
+			.split("")
+			.reduce<string[]>((previousSubstrings, currentCharacter) => {
+				if(currentCharacter === currentCharacter.toLocaleUpperCase()) {
+					return [ ...previousSubstrings, currentCharacter ]
+				} else {
+					const lastString = previousSubstrings.pop() || ""
+					return [ ...previousSubstrings, lastString+currentCharacter ]
+				}
+			}, [])
 
-	return separatedSource.reduce<boolean>((previousValidation, currentSource, i) => {
-		return previousValidation && currentSource.startsWith(acronymSubstrings[i])
-	}, separatedSource.length === acronymSubstrings.length)
+		return separatedSource.reduce<boolean>((previousValidation, currentSource, i) => {
+			return previousValidation && currentSource.startsWith(acronymSubstrings[i])
+		}, separatedSource.length === acronymSubstrings.length)
+	} else {
+		throw new Error("Missing source value to validate the acronym")
+	}
 }
