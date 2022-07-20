ab2cf1a5588abc6f868310e466e92dad6e99ba63
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-20 15:42:36 +0800

    feat(department): make page to create a department

---
+++ pages/department/create.page.vue
@@ -0,0 +1,31 @@
+<template>
+	<form>
+		<label class="block">
+			Full name:
+			<input class="border-solid" type="text" v-model="fullName"/>
+		</label>
+		<label class="block">
+			Acronym:
+			<input class="border-solid" type="text" v-model="acronym"/>
+		</label>
+		<label class="block">
+			May admit students:
+			<input type="checkbox" v-model="mayAdmit"/>
+		</label>
+	</form>
+</template>
+
+<style>
+</style>
+
+<script setup lang="ts">
+import { computed, inject, ref, Ref } from "vue"
+
+import type { DeserializedPageContext } from "$@/types/independent"
+
+const pageContext = inject("pageContext") as DeserializedPageContext
+
+const fullName = ref("")
+const acronym = ref("")
+const mayAdmit = ref(false)
+</script>
