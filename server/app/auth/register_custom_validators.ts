ce6d127202e76eb14d825b1b18df2ed37735de68
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-21 20:52:03 +0800

    intrn(validator): register unique validator

--- server/app/auth/register_custom_validators.ts
+++ server/app/auth/register_custom_validators.ts
@@ -3,13 +3,15 @@ import buffer from "!/app/auth/buffer"
 import exists from "!/app/auth/exists"
 import acronym from "!/app/auth/acronym"
 import notExists from "!/app/auth/not_exists"
+import unique from "!/app/auth/unique"
 
 export default async function() {
 	const customValidators: { [key: string]: (..._a: any[]) => Promise<boolean>|boolean }  = {
 		acronym,
 		buffer,
 		exists,
-		notExists
+		notExists,
+		unique
 	}
 
 	for (const name in customValidators) {
