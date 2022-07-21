d90f5b73f5f3bcc5a2c22e2193b724b92f7d42a1
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-21 13:21:22 +0800

    dx(command): allow pulling to default remote

--- execute.ps1
+++ execute.ps1
@@ -35,7 +35,7 @@ Pulls the all branches from remote and prunes remotely-deleted branches.
 
 .PARAMETER Remote
 Only works if one of `-Push` or `-Pull` switches is on.
-Specifies which remote to ush or pull.
+Specifies which remote to push or pull.
 
 .PARAMETER Test
 Switch to runs tests.
@@ -106,7 +106,7 @@ Param(
 	[Parameter(ParameterSetName="PushRepo", Position=1)]
 	[Parameter(ParameterSetName="PullRepo", Position=1)]
 	[string]
-	$Remote = "origin",
+	$Remote = "",
 
 	[Parameter(ParameterSetName="Test", Position=0)]
 	[switch]
@@ -174,5 +174,9 @@ if ($Push) {
 
 if ($Pull) {
 	$currentBranch = & git branch --show-current
-	& git pull --prune $($Remote) $($currentBranch)
+	if ($Remote -eq "") {
+		& git pull --prune
+	} else {
+		& git pull --prune $($Remote) $($currentBranch)
+	}
 }
