fa08ff791a514ba42e3de499e6e364d6053bef20
AteKitty07 <kiritogregorio@gmail.com>
2022-07-22 18:39:36 +0800

    style(user): adjust flex styles

--- components/user_management/users_manager/DataList.vue
+++ components/user_management/users_manager/DataList.vue
@@ -1,11 +1,11 @@
 <template>
 	<!-- TODO(lead): generalize data properties. Current setup only reads type `User` properties -->
 	<div class="data-row" v-for="data in filteredList" :key="data.name">
-		<span class="user-name">{{ data.name }}</span>
-		<span class="user-email">{{ data.email }}</span>
-		<span :class="`user-${roleOrJobTitleClasses}`">{{ isListForServiceEmployees ? data.jobTitle : data.role }}</span>
+		<span class="data-name">{{ data.name }}</span>
+		<span class="data-email">{{ data.email }}</span>
+		<span :class="`data-${roleOrJobTitleClasses}`">{{ isListForServiceEmployees ? data.jobTitle : data.role }}</span>
 		<div class="btns">
-			<button class="btn1">Update</button>
+			<button class="btn1 ">Update</button>
 		</div>
 	</div>
 
@@ -22,16 +22,21 @@
 	padding-bottom: .5rem;
 	font-size: 1.5rem;
 
-	.data-name {
-		font-size: 1.125rem;
+	.data-name { 
+		@apply sm:flex sm:flex-1 justify-start;
+		font-size: 1rem;
 		@screen sm {
-			width: 20%;
+			width: 40vw
 		}
 	}
 	.data-email, .data-role, .data-job-title {
+		@apply sm:flex sm:flex-1 justify-self-start;
 		font-size: 0.75rem;
 	}
-
+	.btns{
+		@apply sm:flex sm:flex-1 justify-end;
+		min-width: 200px;
+	}
 	.btn1 {
 		@apply dark:bg-dark-300 bg-light-600 rounded-md w-20 text-base h-7;
 	}
