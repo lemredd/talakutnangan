ade58024fe9d2d52976215bd328df851d5e750a3
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-22 09:46:03 +0800

    feat(department)!: change the mechanism of route to restore department

--- server/app/routes/api/department/router.ts
+++ server/app/routes/api/department/router.ts
@@ -2,7 +2,7 @@ import Router from "!/bases/router"
 import GetList from "!/app/routes/api/department/list.get"
 import PostCreate from "!/app/routes/api/department/create.post"
 import PatchUpdate from "!/app/routes/api/department/update(id).patch"
-import PatchRestore from "!/app/routes/api/department/restore(id).patch"
+import PatchRestore from "!/app/routes/api/department/restore.patch"
 import DeleteArchive from "!/app/routes/api/department/archive.delete"
 
 export default class extends Router {
