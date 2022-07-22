70873bbe2261700aa959a276da02f8453b793c25
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-22 22:04:46 +0800

    unit(department): ensure validation in restoring department works
    Note: This commit fails as expected failures are nto working.

---
+++ server/app/routes/api/department/restore.patch.spec.ts
@@ -0,0 +1,87 @@
+import ErrorBag from "$!/errors/error_bag"
+import MockRequester from "~/set-ups/mock_requester"
+import DepartmentFactory from "~/factories/department"
+import registerCustomValidators from "!/app/auth/register_custom_validators"
+import Controller from "./restore.patch"
+
+const BODY_VALIDATION_INDEX = 0
+
+describe("Controller: PATCH /api/department/restore", () => {
+	const requester = new MockRequester()
+
+	beforeAll(() => {
+		registerCustomValidators()
+	})
+
+	it("can accept valid info", async () => {
+		const controller = new Controller()
+		const validations = controller.validations
+		const bodyValidation = validations[BODY_VALIDATION_INDEX]
+		const bodyValidationFunction = bodyValidation.intermediate.bind(bodyValidation)
+		const department = await (new DepartmentFactory()).insertOne()
+		await department.destroy({ force: false })
+		requester.customizeRequest({
+			body: {
+				data: [
+					{
+						type: "department",
+						id: department.id
+					}
+				]
+			}
+		})
+
+		await requester.runMiddleware(bodyValidationFunction)
+
+		requester.expectSuccess()
+	})
+
+	it("cannot accept existing resources", async () => {
+		const controller = new Controller()
+		const validations = controller.validations
+		const bodyValidation = validations[BODY_VALIDATION_INDEX]
+		const bodyValidationFunction = bodyValidation.intermediate.bind(bodyValidation)
+		const department = await (new DepartmentFactory()).insertOne()
+		requester.customizeRequest({
+			body: {
+				data: [
+					{
+						type: "department",
+						id: department.id
+					}
+				]
+			}
+		})
+
+		await requester.runMiddleware(bodyValidationFunction)
+
+		const body = requester.expectFailure(ErrorBag).toJSON()
+		expect(body).toHaveLength(1)
+		expect(body).toHaveProperty("0.source.pointer", "data.0.id")
+	})
+
+	it("cannot accept non-existent resources", async () => {
+		const controller = new Controller()
+		const validations = controller.validations
+		const bodyValidation = validations[BODY_VALIDATION_INDEX]
+		const bodyValidationFunction = bodyValidation.intermediate.bind(bodyValidation)
+		const department = await (new DepartmentFactory()).insertOne()
+		await department.destroy({ force: true })
+		requester.customizeRequest({
+			body: {
+				data: [
+					{
+						type: "department",
+						id: department.id
+					}
+				]
+			}
+		})
+
+		await requester.runMiddleware(bodyValidationFunction)
+
+		const body = requester.expectFailure(ErrorBag).toJSON()
+		expect(body).toHaveLength(1)
+		expect(body).toHaveProperty("0.source.pointer", "data.0.id")
+	})
+})
