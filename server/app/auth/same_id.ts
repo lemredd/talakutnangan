af91ab9fd88e1ba7a4a51eb9438f4d600025c95a
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-23 15:18:33 +0800

    intrn(validator): register same ID validator

---
+++ server/app/auth/same_id.ts
@@ -0,0 +1,27 @@
+import type { Validator } from "node-input-validator"
+import accessDeepPath from "!/helpers/access_deep_path"
+
+import type { BaseManagerClass } from "!/types/independent"
+
+/**
+ * Function to validate if the ID in the body is the same as parameter ID.
+ *
+ * Accepts the path to parameter ID.
+ */
+export default async function(
+	{ value, args }: { value: any, args: any[] },
+	validator: Validator
+): Promise<boolean> {
+	if (value !== undefined) {
+		if (args.length < 1) {
+			throw new Error("Number of arguments passed to `sameID` rule is insufficient")
+		}
+
+		const IDPath = args[0]
+		const parameterID = +accessDeepPath(validator.inputs, IDPath)
+		return !Number.isNaN(parameterID) && parameterID === value
+	} else {
+		// Skip the validation if the field does not exists
+		return true
+	}
+}
