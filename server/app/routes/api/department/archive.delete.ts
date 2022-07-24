66e43d39e71053ecbb5cac966b7d6e84986c497a
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-24 18:04:27 +0800

    intrn(department): add method to check if new validation works

--- server/app/routes/api/department/archive.delete.ts
+++ server/app/routes/api/department/archive.delete.ts
@@ -1,12 +1,19 @@
+import { DescriptorMaker } from "!/types/hybrid"
+import { Descriptor } from "!/types/independent"
 import { AuthenticatedRequest, Response } from "!/types/dependent"
 
 import Policy from "!/bases/policy"
 import DepartmentManager from "%/managers/department"
 import NoContentResponseInfo from "!/response_infos/no_content"
 import JSONController from "!/common_controllers/json_controller"
+
 import { ARCHIVE_AND_RESTORE } from "$/permissions/department_combinations"
 import { department as permissionGroup } from "$/permissions/permission_list"
 import PermissionBasedPolicy from "!/middlewares/authentication/permission-based_policy"
+
+import Exists from "!/app/validators/manager/exists"
+import ArrayValidator from "!/app/validators/base/array"
+import EqualString from "!/app/validators/comparison/equal_string"
 
 export default class extends JSONController {
 	get filePath(): string { return __filename }
@@ -24,6 +31,15 @@ export default class extends JSONController {
 			"data.*.type": [ "required", "string", "equals:department" ],
 			"data.*.id": [ "required", "numeric", [ "exists", DepartmentManager, "id" ] ]
 		}
+	}
+
+	makeBodyRuleGenerator(): DescriptorMaker {
+		return (request: AuthenticatedRequest): Descriptor => ({
+			data: new ArrayValidator({
+				type: new EqualString("department"),
+				exists: new Exists(DepartmentManager, "id")
+			})
+		})
 	}
 
 	async handle(request: AuthenticatedRequest, response: Response): Promise<NoContentResponseInfo> {
