c2a8739f810a1b8ef96659a80fbbd592a37e707f
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-24 22:47:01 +0800

    intrn(validator): make validate utility function

---
+++ server/app/validators/validate.ts
@@ -0,0 +1,51 @@
+import type { ValidationConstraints, Request } from "!/types/dependent"
+import type { ErrorPointer, FieldRules } from "!/types/independent"
+import runThroughPipeline from "$/helpers/run_through_pipeline"
+
+export default async function(fields: FieldRules, request: Request, input: { [key:string]: any })
+: Promise<object> {
+	const sanitizedInputs: { [key:string]: any } = {}
+	const errors: ErrorPointer[] = []
+
+	for (const field in fields) {
+		if (Object.prototype.hasOwnProperty.call(fields, field)) {
+			const rules = fields[field]
+
+			try {
+				const constraints: ValidationConstraints = {
+					request,
+					field,
+					...rules.constraints
+				}
+				const sanitizedInput = await runThroughPipeline(
+					input[field],
+					constraints,
+					rules.pipes
+				)
+
+				sanitizedInputs[field] = sanitizedInput
+			} catch(error) {
+				if (error instanceof Error) {
+					errors.push({
+						field,
+						messageMaker: (
+							field: string,
+							value: any
+						): string => `Unexpected error happened while validating ${field}`
+					})
+				} else {
+					errors.push(error as ErrorPointer)
+				}
+			}
+		}
+	}
+
+	if (errors.length > 0) {
+		throw errors.map(error => ({
+			field: error.field,
+			message: error.messageMaker(error.field, input[error.field])
+		}))
+	}
+
+	return sanitizedInputs
+}
