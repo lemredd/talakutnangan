a256db2018d15104fa74d9967f294585f08e6567
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-25 00:44:41 +0800

    unit(validator): ensure array validator pipe works as expected

---
+++ server/app/validators/base/array.spec.ts
@@ -0,0 +1,78 @@
+import integer from "!/app/validators/base/integer"
+import array from "./array"
+
+describe("Validator pipe: array", () => {
+	it("can accept valid input", async () => {
+		const value = Promise.resolve([ 2, 3 ])
+		const constraints = {
+			request: null,
+			field: "hello",
+			array: {
+				rules: {
+					pipes: [ integer ],
+					constraints: {}
+				}
+			}
+		}
+
+		const sanitizeValue = await array(value, constraints)
+
+		expect(sanitizeValue).toEqual([ 2, 3 ])
+	})
+
+	it("cannot accept few input", async () => {
+		const value = Promise.resolve([ 2 ])
+		const constraints = {
+			request: null,
+			field: "hello",
+			array: {
+				rules: {
+					pipes: [ integer ],
+					constraints: {}
+				},
+				minimum: 2
+			}
+		}
+
+		const error = array(value, constraints)
+
+		expect(error).rejects.toHaveProperty("field", "hello")
+	})
+
+	it("cannot accept greate input", async () => {
+		const value = Promise.resolve([ 2 ])
+		const constraints = {
+			request: null,
+			field: "hello",
+			array: {
+				rules: {
+					pipes: [ integer ],
+					constraints: {}
+				},
+				maximum: 0
+			}
+		}
+
+		const error = array(value, constraints)
+
+		expect(error).rejects.toHaveProperty("field", "hello")
+	})
+
+	it("cannot accept invalid input", async () => {
+		const value = Promise.resolve([ 2, "abc" ])
+		const constraints = {
+			request: null,
+			field: "hello",
+			array: {
+				rules: {
+					pipes: [ integer ],
+					constraints: {}
+				}
+			}
+		}
+
+		const error = array(value, constraints)
+
+		expect(error).rejects.toHaveProperty("0.field", "hello.1")
+	})
+})
