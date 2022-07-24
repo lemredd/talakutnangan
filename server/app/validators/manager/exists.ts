53988d5c5ddf82a40b3344657099d23acc147d54
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-24 18:51:38 +0800

    intrn(validator): cast the type accordingly

--- server/app/validators/manager/exists.ts
+++ server/app/validators/manager/exists.ts
@@ -1,14 +1,23 @@
 import type { RuleData } from "!/types/dependent"
-import type { GeneralObject, BaseManagerClass } from "!/types/independent"
+import type {
+	GeneralObject,
+	BaseManagerClass,
+	MetaValidationConstraints
+} from "!/types/independent"
 
+import Validator from "!/app/validators/base/base"
+import StringValidator from "!/app/validators/base/string"
 import IntegerValidator from "!/app/validators/base/integer"
+import addProperCaster from "!/app/validators/base/add_proper_caster"
 
-export default class ExistsValidator extends IntegerValidator {
+export default class ExistsValidator extends Validator {
 	private managerClass: BaseManagerClass
 	private columnName: string
+	private expectedType: "string"|"integer"
 
-	constructor(managerClass: BaseManagerClass, columnName: string) {
-		super()
+	constructor(type: "string"|"integer", managerClass: BaseManagerClass, columnName: string) {
+		super(type)
+		this.expectedType = type
 		this.managerClass = managerClass
 		this.columnName = columnName
 	}
@@ -34,5 +43,14 @@ export default class ExistsValidator extends IntegerValidator {
 		}
 
 		return data
+	}
+
+	protected get metaObject(): MetaValidationConstraints {
+		return addProperCaster(
+			super.metaObject,
+			this.expectedType === "string"
+				? StringValidator.CASTER
+				: IntegerValidator.CASTER
+		)
 	}
 }
