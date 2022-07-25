3994bcae21b0d97f5209a6e79148092155a4d5e9
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-25 16:27:34 +0800

    intrn(validator): make buffer validator

---
+++ server/app/validators/base/buffer.spec.ts
@@ -0,0 +1,77 @@
+import makeInitialState from "!/app/validators/make_initial_state"
+import buffer from "./buffer"
+
+describe("Validator pipe: buffer", () => {
+	it("can accept valid input", async () => {
+		const value = Promise.resolve(makeInitialState({
+			buffer: Buffer.from(""),
+			info: {
+				mimeType: "text/plain"
+			}
+		}))
+		const constraints = {
+			request: null,
+			source: null,
+			field: "hello",
+			buffer: {
+				allowedMimeTypes: [ "text/plain" ],
+				maxSize: 0
+			}
+		}
+
+		const sanitizeValue = (await buffer(value, constraints)).value
+
+		expect(sanitizeValue).toStrictEqual({
+			buffer: Buffer.from(""),
+			info: {
+				mimeType: "text/plain"
+			}
+		})
+	})
+
+	it("cannot accept large buffer", async () => {
+		const value = Promise.resolve(makeInitialState({
+			buffer: Buffer.from("000000"),
+			info: {
+				mimeType: "text/plain"
+			}
+		}))
+		const constraints = {
+			request: null,
+			source: null,
+			field: "hello",
+			buffer: {
+				allowedMimeTypes: [ "text/css" ],
+				maxSize: 5
+			}
+		}
+
+		const error = buffer(value, constraints)
+
+		expect(error).rejects.toHaveProperty("field", "hello")
+		expect(error).rejects.toHaveProperty("messageMaker")
+	})
+
+	it("cannot accept invalid media type", async () => {
+		const value = Promise.resolve(makeInitialState({
+			buffer: Buffer.from(""),
+			info: {
+				mimeType: "text/plain"
+			}
+		}))
+		const constraints = {
+			request: null,
+			source: null,
+			field: "hello",
+			buffer: {
+				allowedMimeTypes: [ "text/css" ],
+				maxSize: 0
+			}
+		}
+
+		const error = buffer(value, constraints)
+
+		expect(error).rejects.toHaveProperty("field", "hello")
+		expect(error).rejects.toHaveProperty("messageMaker")
+	})
+})
