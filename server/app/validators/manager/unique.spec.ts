010fa108bb5eaca5b174c065d1d1bb3fbb1ae4f3
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-25 15:13:17 +0800

    intrn(server): make unique validator pipe

---
+++ server/app/validators/manager/unique.spec.ts
@@ -0,0 +1,79 @@
+import UserManager from "%/managers/user"
+import UserFactory from "~/factories/user"
+import makeInitialState from "!/app/validators/make_initial_state"
+import unique from "./unique"
+
+describe("Validator: unique", () => {
+	it("can accept valid input", async () => {
+		const user = await (new UserFactory()).insertOne()
+		const newUser = await (new UserFactory()).makeOne()
+		const value = Promise.resolve(makeInitialState(newUser.name))
+		const constraints = {
+			request: null,
+			source: {
+				id: user.id
+			},
+			field: "hello",
+			manager: {
+				className: UserManager,
+				columnName: "name"
+			},
+			unique: {
+				IDPath: "id"
+			}
+		}
+
+		const sanitizeValue = (await unique(value, constraints)).value
+
+		expect(sanitizeValue).toEqual(newUser.name)
+	})
+
+	it("cannot accept existing value", async () => {
+		const user = await (new UserFactory()).insertOne()
+		const value = Promise.resolve(makeInitialState(user.name))
+		const constraints = {
+			request: null,
+			source: {
+				id: user.id
+			},
+			field: "hello",
+			manager: {
+				className: UserManager,
+				columnName: "name"
+			},
+			unique: {
+				IDPath: "id"
+			}
+		}
+
+		const error = unique(value, constraints)
+
+		expect(error).rejects.toHaveProperty("field", "hello")
+		expect(error).rejects.toHaveProperty("messageMaker")
+	})
+
+	it("cannot accept archived value", async () => {
+		const user = await (new UserFactory()).insertOne()
+		const value = Promise.resolve(makeInitialState(user.name))
+		const constraints = {
+			request: null,
+			source: {
+				id: user.id
+			},
+			field: "hello",
+			manager: {
+				className: UserManager,
+				columnName: "name"
+			},
+			unique: {
+				IDPath: "id"
+			}
+		}
+		await user.destroy({ force: false })
+
+		const error = unique(value, constraints)
+
+		expect(error).rejects.toHaveProperty("field", "hello")
+		expect(error).rejects.toHaveProperty("messageMaker")
+	})
+})
---
+++ server/app/validators/manager/unique.ts
@@ -0,0 +1,45 @@
+import type {
+	ValidationState,
+	ValidationConstraints,
+	UniqueRuleConstraints
+} from "!/types/independent"
+
+import accessDeepPath from "!/helpers/access_deep_path"
+import makeDeveloperError from "!/app/validators/make_developer_error"
+
+/**
+ * Validator to check if data unique/will be unique in the database
+ */
+ export default async function(
+	currentState: Promise<ValidationState>,
+	constraints: ValidationConstraints & Partial<UniqueRuleConstraints>
+): Promise<ValidationState> {
+	const state = await currentState
+
+	if(state.maySkip) return state
+
+	if (constraints.manager === undefined || constraints.unique === undefined) {
+		throw makeDeveloperError(constraints.field)
+	}
+
+	// TODO: Get transaction manager from cache
+	const manager = new constraints.manager.className()
+	const foundModel = await manager.findOneOnColumn(constraints.manager.columnName, state.value, {
+		filter: {
+			existence: "all"
+		}
+	})
+
+	const id = accessDeepPath(constraints.source, constraints.unique.IDPath)
+
+	// TODO: Store found model in cache
+	if (foundModel.data === null || (foundModel.data as any).id === id) {
+		return state
+	} else {
+		throw {
+			field: constraints.field,
+			messageMaker: (field: string, value: string) =>
+				`The ${value} in field "${field}" does not exists in the database".`
+		}
+	}
+}
