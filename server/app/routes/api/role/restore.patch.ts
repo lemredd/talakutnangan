9519049df8c9ab589c42184652bfc26de8ad4029
Angelo Magtoto <19201.magtoto.angelo.l@gmail.com>
2022-07-26 22:01:22 +0800

    intrn(role): update the validators/routes for role

--- server/app/routes/api/role/restore.patch.ts
+++ server/app/routes/api/role/restore.patch.ts
@@ -1,4 +1,5 @@
 import { AuthenticatedRequest, Response } from "!/types/dependent"
+import { FieldRules } from "!/types/independent"
 
 import Policy from "!/bases/policy"
 import RoleManager from "%/managers/role"
@@ -7,6 +8,15 @@ import JSONController from "!/common_controllers/json_controller"
 import { ARCHIVE_AND_RESTORE } from "$/permissions/department_combinations"
 import { role as permissionGroup } from "$/permissions/permission_list"
 import PermissionBasedPolicy from "!/middlewares/authentication/permission-based_policy"
+import { FieldRulesMaker } from "!/types/hybrid"
+import object from "!/app/validators/base/object"
+import required from "!/app/validators/base/required"
+import same from "!/app/validators/comparison/same"
+import string from "!/app/validators/base/string"
+import length from "!/app/validators/comparison/length"
+import array from "!/app/validators/base/array"
+import integer from "!/app/validators/base/integer"
+import archived from "!/app/validators/manager/archived"
 
 export default class extends JSONController {
 	get filePath(): string { return __filename }
@@ -24,6 +34,44 @@ export default class extends JSONController {
 			"data.*.type": [ "required", "string", "equals:role" ],
 			"data.*.id": [ "required", "numeric", [ "archived", RoleManager ] ]
 		}
+	}
+
+	makeBodyRuleGenerator(): FieldRulesMaker {
+		return (request: AuthenticatedRequest): FieldRules => ({
+			data: {
+				pipes: [ required, array, length ],
+				constraints: {
+					array: {
+						rules: {
+							pipes: [ required, object ],
+							constraints: {
+								object: {
+									type: {
+										pipes: [ required, string, same ],
+										constraints: {
+											same: "role"
+										}
+									},
+									id: {
+										pipes: [ required, integer, archived ],
+										constraints: {
+											manager: {
+												className: RoleManager,
+												columnName: "id"
+											}
+										}
+									}
+								}
+							}
+						}
+					},
+					length: {
+						minimum: 1,
+						maximum: 24 // This is possible to change in the future
+					}
+				}
+			}
+		})
 	}
 
 	async handle(request: AuthenticatedRequest, response: Response): Promise<NoContentResponseInfo> {
