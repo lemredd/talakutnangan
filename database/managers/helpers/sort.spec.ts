6a653c2a8c2216052794e637f5d12a466838b762
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-27 00:10:50 +0800

    fix(test): correct the path to specific options

--- database/managers/helpers/sort.spec.ts
+++ database/managers/helpers/sort.spec.ts
@@ -11,8 +11,8 @@ describe("Database Pipe: Sift by existence", () => {
 		const options = sort({}, { sort: [ "name" ] })
 		const foundUsers = await User.findAll(options)
 
-		expect(options).toHaveProperty("order.0", "name")
-		expect(options).toHaveProperty("order.1", "ASC")
+		expect(options).toHaveProperty("order.0.0", "name")
+		expect(options).toHaveProperty("order.0.1", "ASC")
 		expect(foundUsers).toHaveLength(3)
 		expect(foundUsers).toHaveProperty("0.id", users[0].id)
 		expect(foundUsers).toHaveProperty("1.id", users[1].id)
@@ -27,8 +27,8 @@ describe("Database Pipe: Sift by existence", () => {
 		const options = sort({}, { sort: [ "name" ] })
 		const foundUsers = await User.findAll(options)
 
-		expect(options).toHaveProperty("order.0", "name")
-		expect(options).toHaveProperty("order.1", "DESC")
+		expect(options).toHaveProperty("order.0.0", "name")
+		expect(options).toHaveProperty("order.0.1", "DESC")
 		expect(foundUsers).toHaveLength(3)
 		expect(foundUsers).toHaveProperty("0.id", users[2].id)
 		expect(foundUsers).toHaveProperty("1.id", users[1].id)
