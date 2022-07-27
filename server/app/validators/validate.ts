1c0be02f246293c10549614cb27d89a57f733fa3
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-07-27 20:15:51 +0800

    fix(server): remove outdated code
    Co-authored-by: Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>

--- server/app/validators/validate.ts
+++ server/app/validators/validate.ts
@@ -20,33 +20,14 @@ export default async function(
 	const errors: ErrorPointer[] = []
 
 	for (const field in fields) {
-		if (Object.prototype.hasOwnProperty.call(fields, field)) {
-			const rules = fields[field]
+		const rules = fields[field]
 
-			try {
-				const constraints: ValidationConstraints = {
-					request,
-					source: originalInput,
-					field,
-					...rules.constraints
-				}
-				const sanitizedInput = await runThroughPipeline(
-					Promise.resolve(makeInitialState(input[field])),
-					constraints,
-					rules.pipes
-				)
-
-				sanitizedInputs[field] = sanitizedInput.value
-			} catch(error) {
-				const flattendedErrors: (ErrorPointer|Error)[] = []
-
-				if (Array.isArray(error)) {
-					flattendedErrors.push(...error)
-				} else {
-					flattendedErrors.push(error as ErrorPointer)
-				}
-
-				errors.push(...unifyErrors(field, flattendedErrors))
+		try {
+			const constraints: ValidationConstraints = {
+				request,
+				source: originalInput,
+				field,
+				...rules.constraints
 			}
 			const sanitizedInput = await runThroughPipeline(
 				Promise.resolve(makeInitialState(input[field])),
