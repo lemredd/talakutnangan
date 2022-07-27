9ef73fa48337a237c18b4aeab5fad3c664ebaf1b
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-27 11:08:24 +0800

    fix(command): enclose the pattern properly

--- execute.ps1
+++ execute.ps1
@@ -44,7 +44,7 @@ Switch to runs tests.
 Only required if `-Test` switch is on.
 It contains the name of test suite to run.
 
-.PARAMETER Regex
+.PARAMETER Path
 Only works if `-Test` switch is on.
 Limits the files to test base from regular expression.
 
@@ -134,7 +134,7 @@ Param(
 
 	[Parameter(ParameterSetName="Test", Position=2)]
 	[string]
-	$Regex = "",
+	$Path = "",
 
 	[Parameter(ParameterSetName="Test", Position=3)]
 	[switch]
@@ -169,9 +169,9 @@ if ($Test) {
 		$configuration = "jest.$($name).config.json"
 	}
 
-	$regexFlag = ""
-	if ($Regex -ne "") {
-		$watchFlag = "--testRegex $($Regex)"
+	$regexFlag = '".*"'
+	if ($Regex) {
+		$regexFlag = '"'+$Regex+'"'
 	}
 
 	$watchFlag = ""
@@ -179,7 +179,8 @@ if ($Test) {
 		$watchFlag = "--watch"
 	}
 
-	& npx cross-env NODE_ENV=$($type)_test jest -c ${configuration} $($regexFlag) $($watchFlag)
+	Write-output "npx cross-env NODE_ENV=$($type)_test jest -c ${configuration} --testRegex $($regexFlag) $($watchFlag)"
+	& npx cross-env NODE_ENV=$($type)_test jest -c ${configuration} --testRegex $($regexFlag) $($watchFlag)
 }
 
 if ($Push) {
