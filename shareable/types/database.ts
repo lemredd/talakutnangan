8a08dbb9f3f4d014e0c310deac3594691dfafde4
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-27 16:15:50 +0800

    types(share): add type for common page options

--- shareable/types/database.ts
+++ shareable/types/database.ts
@@ -107,9 +107,20 @@ const rawDays = [ ...days ] as const
 export type Day = typeof rawDays[number]
 
 /**
- * Shape of expected common filter
+ * Expected shape of the common page options
+ */
+export interface Page {
+	page: {
+		offset: number,
+		limit: number
+	}
+}
+
+/**
+ * Shape of expected common filter options
  */
 export type Filter = ExistenceFilter
+
 export type UserFilter =
 	& Filter
 	& DepartmentFilter
@@ -124,6 +135,9 @@ export interface ExistenceFilter extends Serializable {
 	}
 }
 
+/**
+ * Expected shape of the common sort options
+ */
 export interface Sort extends Serializable {
 	sort: string[]
 }
