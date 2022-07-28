73c35c86c1bfa73ff29b0539215d14530cbc51af
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-29 00:38:25 +0800

    intrn(transformer): transform the signature into URL

--- database/transformers/signature.ts
+++ database/transformers/signature.ts
@@ -1,7 +1,8 @@
+import type { AttributesObject, TransformerOptions } from "%/types/dependent"
 import Signature from "%/models/signature"
 import Transformer from "%/transformers/base"
+import URLMaker from "$!/singletons/url_maker"
 import Serializer from "%/transformers/serializer"
-import type { AttributesObject, TransformerOptions } from "%/types/dependent"
 
 export default class extends Transformer<Signature, { raw: boolean }> {
 	constructor() {
@@ -10,17 +11,31 @@ export default class extends Transformer<Signature, { raw: boolean }> {
 	}
 
 	transform(model: Signature|Signature[], options: TransformerOptions<{ raw: boolean }>): AttributesObject {
-		const whiteListedProperties = [ "id" ]
-
 		if (options.extra === undefined) {
 			options.extra = { raw: false }
 		}
 
-		const safeObject = Serializer.whitelist(model, [
-			"id",
-			"signature"
-		])
+		const safeObject = Serializer.whitelist(model, [ "id" ])
 
+		if (options.extra.raw) {
+			if (model instanceof Array) {
+				safeObject.data = (safeObject.data as any[]).map(data => {
+					return {
+						...data as AttributesObject,
+						links: {
+							self: URLMaker.makeURLFromPath("/api/signature/:id", { id: data.id })
+						}
+					}
+				})
+			} else {
+				safeObject.data = {
+					...safeObject.data as AttributesObject,
+					links: {
+						self: URLMaker.makeURLFromPath("/api/signature/:id", { id: model.id })
+					}
+				}
+			}
+		}
 
 		return safeObject
 	}
