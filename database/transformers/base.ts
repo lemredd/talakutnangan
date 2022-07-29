8175351e3c787b1010f0822204d9e98cd2f3b5fa
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-29 21:12:19 +0800

    fix(transformer): transform the included data instead

--- database/transformers/base.ts
+++ database/transformers/base.ts
@@ -2,29 +2,18 @@ import type { GeneralObject } from "$/types/server"
 import type { Serializable } from "$/types/database"
 
 import { Transformer as BaseTransformer } from "jsonapi-fractal"
-import isPlainObject from "$!/helpers/is_plain_object"
 
 export default abstract class Transformer<T, U> extends BaseTransformer<T, U> {
 	protected subtransformers: GeneralObject<Transformer<any, any>> = {}
 
 	finalizeTransform(transformedData: Serializable): Serializable {
-		if (
-			transformedData.data !== undefined
-			&& isPlainObject(transformedData.data)
-			&& isPlainObject((transformedData.data as GeneralObject).relationships)
-		) {
-			for (const relationship in this.subtransformers) {
-				if (Object.prototype.hasOwnProperty.call(this.subtransformers, relationship)) {
-					const transformer = this.subtransformers[relationship]
-					const resource = transformedData[relationship]
+		if (transformedData.included !== undefined) {
+			transformedData.included = (transformedData.included as GeneralObject[]).map(data => {
+				const transformer = this.subtransformers[data.type]
+				const resource = data as Serializable
 
-					if (resource !== undefined && isPlainObject(resource)) {
-						transformedData[relationship] = transformer.finalizeTransform(
-							resource as Serializable
-						)
-					}
-				}
-			}
+				return transformer.finalizeTransform(resource)
+			})
 		}
 
 		return transformedData
