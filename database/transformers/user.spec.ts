c5bbe3198b744727a6c73186b381737fddcda8fd
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-30 00:49:20 +0800

    unit(transformer): ensure password status exists or not accordingly

--- database/transformers/user.spec.ts
+++ database/transformers/user.spec.ts
@@ -1,6 +1,7 @@
 import type { GeneralObject } from "$/types/server"
 import UserFactory from "~/factories/user"
 import SignatureFactory from "~/factories/signature"
+import StudentDetailFactory from "~/factories/student_detail"
 import URLMaker from "$!/singletons/url_maker"
 import Serializer from "%/transformers/serializer"
 import Transformer from "./user"
@@ -11,17 +12,24 @@ describe("Transformer: User", () => {
 	})
 
 	it("can transform into normal resource object", async () => {
-		const model = await new UserFactory().insertOne()
+		const model = await new UserFactory().beStudent().insertOne()
 		const signature = await new SignatureFactory().user(() => Promise.resolve(model)).insertOne()
+		const studentDetail = await new StudentDetailFactory()
+			.user(() => Promise.resolve(model))
+			.insertOne()
+		model.password = studentDetail.studentNumber
+		model.save()
 		model.signature = signature
+		model.studentDetail = studentDetail
 		const transformer = new Transformer()
 
 		const object = Serializer.serialize(model, transformer, {})
 
 		expect(object).toHaveProperty("data.type", "user")
 		expect(object).toHaveProperty("data.id", model.id)
+		expect(object).toHaveProperty("meta.hasDefaultPassword", true)
 		expect(object).toHaveProperty(
-			"included.0.links.self",
+			"included.1.links.self",
 			`http://localhost:16000/api/signature/${signature.id}`
 		)
 	})
@@ -31,23 +39,32 @@ describe("Transformer: User", () => {
 		const signatureA = await new SignatureFactory()
 			.user(() => Promise.resolve(models[0]))
 			.insertOne()
+		const studentDetailA = await new StudentDetailFactory()
+			.user(() => Promise.resolve(models[0]))
+			.insertOne()
 		models[0].signature = signatureA
+		models[0].studentDetail = studentDetailA
 		const signatureB = await new SignatureFactory()
 			.user(() => Promise.resolve(models[1]))
 			.insertOne()
+		const studentDetailB = await new StudentDetailFactory()
+			.user(() => Promise.resolve(models[1]))
+			.insertOne()
 		models[1].signature = signatureB
+		models[1].studentDetail = studentDetailB
 		const transformer = new Transformer()
 
 		const object = Serializer.serialize(models, transformer, {})
 
 		expect(object).toHaveProperty("data.0.type", "user")
 		expect(object).toHaveProperty("data.0.id", models[0].id)
+		expect(object).not.toHaveProperty("meta.hasDefaultPassword")
 		expect(object).toHaveProperty(
-			"included.0.links.self",
+			"included.2.links.self",
 			`http://localhost:16000/api/signature/${signatureA.id}`
 		)
 		expect(object).toHaveProperty(
-			"included.1.links.self",
+			"included.3.links.self",
 			`http://localhost:16000/api/signature/${signatureB.id}`
 		)
 	})
