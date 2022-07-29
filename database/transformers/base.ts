aeccda49229a048ce40fc5b2c5b36643265906a8
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-29 20:33:14 +0800

    intrn(transformer): add base property to contain the sub-transformers

--- database/transformers/base.ts
+++ database/transformers/base.ts
@@ -1,8 +1,11 @@
+import type { GeneralObject } from "$/types/server"
 import type { Serializable } from "$/types/database"
 
 import { Transformer as BaseTransformer } from "jsonapi-fractal"
 
 export default abstract class Transformer<T, U> extends BaseTransformer<T, U> {
+	protected subtransformers: GeneralObject<BaseTransformer<any, any>> = {}
+
 	finalizeTransform(transformedData: Serializable): Serializable {
 		return transformedData
 	}
