705ed0ef915800aa8837f5b344d2e12114df213a
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-29 21:04:44 +0800

    intrn(transformer): finalize the transformation of the relationships

--- database/transformers/base.ts
+++ database/transformers/base.ts
@@ -2,11 +2,31 @@ import type { GeneralObject } from "$/types/server"
 import type { Serializable } from "$/types/database"
 
 import { Transformer as BaseTransformer } from "jsonapi-fractal"
+import isPlainObject from "$!/helpers/is_plain_object"
 
 export default abstract class Transformer<T, U> extends BaseTransformer<T, U> {
-	protected subtransformers: GeneralObject<BaseTransformer<any, any>> = {}
+	protected subtransformers: GeneralObject<Transformer<any, any>> = {}
 
 	finalizeTransform(transformedData: Serializable): Serializable {
+		if (
+			transformedData.data !== undefined
+			&& isPlainObject(transformedData.data)
+			&& isPlainObject((transformedData.data as GeneralObject).relationships)
+		) {
+			for (const relationship in this.subtransformers) {
+				if (Object.prototype.hasOwnProperty.call(this.subtransformers, relationship)) {
+					const transformer = this.subtransformers[relationship]
+					const resource = transformedData[relationship]
+
+					if (resource !== undefined && isPlainObject(resource)) {
+						transformedData[relationship] = transformer.finalizeTransform(
+							resource as Serializable
+						)
+					}
+				}
+			}
+		}
+
 		return transformedData
 	}
 }
