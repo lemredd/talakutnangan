24a847d0a10928a7edf4d2d17115b7787377de00
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-30 12:58:41 +0800

    intrn(helper): make method to set new cache to cache manager

--- common_back-end/helpers/cache_client.ts
+++ common_back-end/helpers/cache_client.ts
@@ -1,3 +1,4 @@
+import setDeepPath from "$!/helpers/set_deep_path"
 import CacheManager from "$!/singletons/cache_manager"
 import accessDeepPath from "$!/helpers/access_deep_path"
 
@@ -10,5 +11,13 @@ export default class {
 
 	getCache(path: string): any|null {
 		return accessDeepPath(CacheManager.getCache(this.clientKey), path, null)
+	}
+
+	setCache(path: string, value: any): void {
+		const currentCache = this.getCache(path) ?? {}
+
+		const newCache = setDeepPath(currentCache, path, value)
+
+		CacheManager.setCache(this.clientKey, newCache)
 	}
 }
