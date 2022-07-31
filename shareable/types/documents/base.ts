544ffb6e6f3d30854c5991867d715ee6d60435ed
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-31 14:05:41 +0800

    types(share): allow inserting extended attributes

--- shareable/types/documents/base.ts
+++ shareable/types/documents/base.ts
@@ -10,8 +10,8 @@ export interface ResourceIdentifier extends PrimaryData {
 
 export interface Attributes extends GeneralObject {}
 
-export type Resource<T extends ResourceIdentifier> = T & {
-	attributes: Attributes
+export type Resource<T extends ResourceIdentifier, U extends Attributes> = T & {
+	attributes: U
 }
 
 export type DeserializedResource<T extends ResourceIdentifier, U extends Attributes> =
@@ -24,13 +24,15 @@ export interface DataDocument<T extends PrimaryData|PrimaryData[]> extends Seria
 
 export interface CompleteDataDocument<
 	T extends ResourceIdentifier,
-	U extends Resource<T>
-> extends DataDocument<U> {}
+	U extends Attributes,
+	V extends Resource<T, U>
+> extends DataDocument<V> {}
 
 export interface CompleteDataListDocument<
 	T extends ResourceIdentifier,
-	U extends Resource<T>
-> extends DataDocument<U[]> {}
+	U extends Attributes,
+	V extends Resource<T, U>
+> extends DataDocument<V[]> {}
 
 export interface DeserializedCompleteDataDocument<
 	T extends ResourceIdentifier,
