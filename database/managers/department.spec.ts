4f5910124b3156ee89e0b08807598e1269035e6b
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-31 23:01:30 +0800

    unit(manager): remove some tests due to removal of non-standard pipes

--- database/managers/department.spec.ts
+++ database/managers/department.spec.ts
@@ -2,50 +2,9 @@ import DepartmentManager from "./department"
 import DepartmentFactory from "~/factories/department"
 
 describe("Database: Department Read Operations", () => {
-	it("can search department with matching query", async () => {
-		const manager = new DepartmentManager()
-		const department = await (new DepartmentFactory()).insertOne()
-		const incompleteName = department.fullName.slice(1)
+	it.todo("can count single department")
 
-		const departments = await manager.list({
-			fullName: incompleteName,
-			filter: {
-				existence: "exists"
-			},
-			sort: [],
-			page: {
-				offset: 0,
-				limit: 5
-			}
-		})
+	it.todo("can count single department with zero users")
 
-		expect(departments).toHaveProperty("data")
-		expect(departments.data).toHaveLength(1)
-		expect(departments).toHaveProperty("data.0.type", "department")
-		expect(departments).toHaveProperty("data.0.id", department.id)
-		expect(departments).toHaveProperty("data.0.attributes.acronym", department.acronym)
-		expect(departments).toHaveProperty("data.0.attributes.fullName", department.fullName)
-		expect(departments).toHaveProperty("data.0.attributes.mayAdmit", department.mayAdmit)
-	})
-
-	it("cannot search department with non-matching query", async () => {
-		const manager = new DepartmentManager()
-		const department = await (new DepartmentFactory()).insertOne()
-		const incorrectName = department.fullName + "1"
-
-		const departments = await manager.list({
-			fullName: incorrectName,
-			filter: {
-				existence: "exists"
-			},
-			sort: [],
-			page: {
-				offset: 0,
-				limit: 5
-			}
-		})
-
-		expect(departments).toHaveProperty("data")
-		expect(departments.data).toHaveLength(0)
-	})
+	it.todo("can count multiple departments")
 })
--- database/managers/role.spec.ts
+++ database/managers/role.spec.ts
@@ -4,48 +4,6 @@ import UserFactory from "~/factories/user"
 import RoleManager from "./role"
 
 describe("Database: Role read operations", () => {
-	it("can search role with matching query", async () => {
-		const manager = new RoleManager()
-		const role = await (new RoleFactory()).insertOne()
-		const incompleteName = role.name.slice(1)
-
-		const roles = await manager.list({
-			name: incompleteName,
-			filter: {
-				existence: "exists"
-			},
-			sort: [],
-			page: {
-				offset: 0,
-				limit: 5
-			}
-		})
-
-		expect(roles).toHaveProperty("data")
-		expect(roles.data).toHaveLength(1)
-	})
-
-	it("cannot search role with non-matching query", async () => {
-		const manager = new RoleManager()
-		const role = await (new RoleFactory()).insertOne()
-		const incorrectName = role.name + "1"
-
-		const roles = await manager.list({
-			name: incorrectName,
-			filter: {
-				existence: "exists"
-			},
-			sort: [],
-			page: {
-				offset: 0,
-				limit: 5
-			}
-		})
-
-		expect(roles).toHaveProperty("data")
-		expect(roles.data).toHaveLength(0)
-	})
-
 	it("can count single role", async () => {
 		const manager = new RoleManager()
 		const role = await new RoleFactory().insertOne()
