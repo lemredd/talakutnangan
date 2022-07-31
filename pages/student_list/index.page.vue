c57aec99c485707273d54499224f0c2c433b9f41
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-07-31 10:49:15 +0800

    chore: update code from outdated branch

--- pages/student_list/index.page.vue
+++ pages/student_list/index.page.vue
@@ -1,6 +1,6 @@
 <template>
 	<h1 class="text-2xl m-2 dark:text-light-200">Students of (Institute)</h1>
-	<UsersManager :data="users" :has-dropdown-filter="true" />
+	<UsersManager :resource="users" :has-dropdown-filter="true" />
 </template>
 
 <style>
@@ -10,42 +10,24 @@
 import { onMounted, provide, ref } from "vue"
 import { deserialise } from "kitsu-core"
 
-import UsersManager from "@/user_management/DataManager.vue"
-import type { ManagerKind } from "@/user_management/types"
+import UsersManager from "@/resource_management/resource_manager.vue"
+import type { ManagerKind } from "@/resource_management/types"
+import { UserProfile } from "$/types/common_front-end"
 
 const managerKind = "secretary" as ManagerKind
 provide("managerKind", managerKind)
 
-interface RawUser {
-	id: number,
-	name: string,
-	email: string,
-	kind: string,
-	signature: string
-}
-const users = ref<RawUser[]>([])
-const roles = ref<string[]>([])
+const users = ref<UserProfile[]>([])
 onMounted(() => {
 	// TODO: fetch("/api/user/list") soon
 	fetch("/dev/sample_user_list")
 	.then(response => response.json())
 	.then(response => {
 		const deserializedData = deserialise(response).data
-		const rolesNoDuplicate = new Set([...roles.value])
 		users.value = deserializedData
 
-		users.value.forEach((user: RawUser | any) => user.roles.data.forEach((role: any) => rolesNoDuplicate.add(role.name)))
-		roles.value = [...rolesNoDuplicate]
-		users.value.push({
-			id: users.value.length-1,
-			name: "Jarlem Redd J. de Peralta",
-			email: "email.example.com",
-			kind: "Student",
-			signature:"ooo"
-		})
 		// Check the console for other available info from server
 		console.log(deserializedData)
 	})
 })
-provide("filterList", roles.value)
 </script>
