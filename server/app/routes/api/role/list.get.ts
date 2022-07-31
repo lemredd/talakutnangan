7b452e9a5b2ed61a4e612f14c9e2380b6c7c25dc
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-07-31 10:52:36 +0800

    intrn(controller): return list response instead of responding directly

--- server/app/routes/api/role/list.get.ts
+++ server/app/routes/api/role/list.get.ts
@@ -4,14 +4,13 @@ import { Request, Response } from "!/types/dependent"
 
 import Policy from "!/bases/policy"
 import RoleManager from "%/managers/role"
+import ListResponse from "!/response_infos/list"
 import QueryController from "!/common_controllers/query_controller"
 
 import { READ } from "$/permissions/role_combinations"
 import { role as permissionGroup } from "$/permissions/permission_list"
 import PermissionBasedPolicy from "!/middlewares/authentication/permission-based_policy"
 
-import object from "!/app/validators/base/object"
-import boolean from "!/app/validators/base/boolean"
 import makeListRules from "!/app/rule_sets/make_list"
 import nullable from "!/app/validators/base/nullable"
 import string from "!/app/validators/base/string"
@@ -41,12 +40,12 @@ export default class extends QueryController {
 		})
 	}
 
-	async handle(request: Request, response: Response): Promise<void> {
+	async handle(request: Request, response: Response): Promise<ListResponse> {
 		const constraints = { ...request.query }
 
 		const manager = new RoleManager()
 		const roles = await manager.list(constraints)
 
-		response.status(this.status.OK).json(roles)
+		return new ListResponse(roles)
 	}
 }
