55f755d371287cc7e296dc909172f3f620326df8
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-01 14:39:33 +0800

    types(front-end): supply role fetcher with the special types

--- common_front-end/fetchers/role.ts
+++ common_front-end/fetchers/role.ts
@@ -1,7 +1,29 @@
-import type { RoleAttributes } from "$/types/documents/role"
+import type { Serializable } from "$/types/general"
+import type { RoleQueryParameters } from "$/types/query"
+import type {
+	RoleResourceIdentifier,
+	RoleAttributes,
+	RoleResource,
+	DeserializedRoleResource,
+	RoleDocument,
+	RoleListDocument,
+	DeserializedRoleDocument,
+	DeserializedRoleListDocument
+} from "$/types/documents/role"
 import Fetcher from "$@/fetchers/fetcher"
 
-export default class RoleFetcher extends Fetcher<RoleAttributes> {
+export default class RoleFetcher extends Fetcher<
+	RoleResourceIdentifier,
+	RoleAttributes,
+	RoleResource,
+	DeserializedRoleResource,
+	RoleDocument,
+	RoleListDocument,
+	DeserializedRoleDocument,
+	DeserializedRoleListDocument,
+	Serializable,
+	RoleQueryParameters
+> {
 	static initialize(basePath: string) {
 		super.initialize(basePath, "role")
 	}
