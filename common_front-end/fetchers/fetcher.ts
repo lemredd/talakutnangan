b2edafa562978de81370839bd84d8b5a1dce9277
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-01 14:32:21 +0800

    types(front-end): require base fetcher to have generic arguments

--- common_front-end/fetchers/fetcher.ts
+++ common_front-end/fetchers/fetcher.ts
@@ -1,8 +1,17 @@
 import type { Serializable } from "$/types/general"
 import type { Response } from "$@/types/independent"
 import { JSON_API_MEDIA_TYPE } from "$/types/server"
-import type { Attributes } from "$/types/documents/base"
 import type { CommonQueryParameters } from "$/types/query"
+import type {
+	ResourceIdentifier,
+	Attributes,
+	Resource,
+	DeserializedResource,
+	ResourceDocument,
+	ResourceListDocument,
+	DeserializedResourceDocument,
+	DeserializedResourceListDocument
+} from "$/types/documents/base"
 
 import stringifyQuery from "$@/fetchers/stringify_query"
 import RequestEnvironment from "$/helpers/request_environment"
@@ -13,8 +22,16 @@ import specializedPath from "$/helpers/specialize_path"
  * client-side code.
  */
 export default class Fetcher<
-	T extends Attributes,
-	U extends CommonQueryParameters = CommonQueryParameters
+	T extends ResourceIdentifier,
+	U extends Attributes,
+	V extends Resource<T, U>,
+	W extends DeserializedResource<T, U>,
+	X extends ResourceDocument<T, U, V>,
+	Y extends ResourceListDocument<T, U, V>,
+	Z extends DeserializedResourceDocument<T, U, W>,
+	A extends DeserializedResourceListDocument<T, U, W>,
+	B extends Serializable,
+	C extends CommonQueryParameters = CommonQueryParameters
 > extends RequestEnvironment {
 	protected static basePath: string = ""
 	protected static type: string = ""
@@ -33,53 +50,55 @@ export default class Fetcher<
 		this.type = type
 	}
 
-	create(attributes: T): Promise<Response> {
+	create(attributes: T): Promise<Response<T, U, V, W, X>> {
 		return this.postJSON(`${this.type}/create`, {
 			data: {
 				type: this.type,
 				attributes
 			}
-		})
+		}) as Promise<Response<T, U, V, W, X>>
 	}
 
-	list(parameters: U): Promise<Response> {
+	list(parameters: C): Promise<Response<T, U, V, W, Y>> {
 		const commaDelimitedSort = {
 			...parameters,
 			sort: parameters.sort.join(",")
 		}
-		return this.getJSON(`${this.type}/list?${stringifyQuery(commaDelimitedSort)}`)
+		return this.getJSON(
+			`${this.type}/list?${stringifyQuery(commaDelimitedSort)}`
+		) as Promise<Response<T, U, V, W, Y>>
 	}
 
-	update(id: number, attributes: T): Promise<Response> {
+	update(id: number, attributes: T): Promise<Response<T, U, V, W, null>> {
 		return this.patchJSON(`${this.type}/update/:id`, { id }, {
 			data: {
 				type: this.type,
 				id,
 				attributes
 			}
-		})
+		}) as Promise<Response<T, U, V, W, null>>
 	}
 
-	archive(IDs: number[]): Promise<Response> {
+	archive(IDs: number[]): Promise<Response<T, U, V, W, null>>  {
 		return this.deleteJSON(`${this.type}/archive`, {}, {
 			data: IDs.map(id => ({ type: this.type, id }))
-		})
+		}) as Promise<Response<T, U, V, W, null>>
 	}
 
-	restore(IDs: number[]): Promise<Response> {
+	restore(IDs: number[]): Promise<Response<T, U, V, W, null>>  {
 		return this.patchJSON(`${this.type}/restore`, {}, {
 			data: IDs.map(id => ({ type: this.type, id }))
-		})
+		}) as Promise<Response<T, U, V, W, null>>
 	}
 
-	getJSON(path: string): Promise<Response> {
+	getJSON(path: string): Promise<Response<T, U, V, W, X|Y|B|null>> {
 		return this.requestJSON(path, {
 			method: "GET",
 			headers: this.makeJSONHeaders()
 		})
 	}
 
-	postJSON(path: string, data: Serializable): Promise<Response> {
+	postJSON(path: string, data: Serializable): Promise<Response<T, U, V, W, X|Y|B|null>> {
 		return this.requestJSON(path, {
 			method: "POST",
 			headers: this.makeJSONHeaders(),
@@ -91,7 +110,7 @@ export default class Fetcher<
 		pathTemplate: string,
 		IDs: { [key:string]: number },
 		data: Serializable
-	): Promise<Response> {
+	): Promise<Response<T, U, V, W, X|Y|B|null>> {
 		const path = specializedPath(pathTemplate, IDs)
 
 		return this.requestJSON(path, {
@@ -105,7 +124,7 @@ export default class Fetcher<
 		pathTemplate: string,
 		IDs: { [key:string]: number },
 		data: Serializable
-	): Promise<Response> {
+	): Promise<Response<T, U, V, W, X|Y|B|null>> {
 		const path = specializedPath(pathTemplate, IDs)
 
 		return this.requestJSON(path, {
@@ -115,7 +134,8 @@ export default class Fetcher<
 		})
 	}
 
-	private async requestJSON(path: string, request: RequestInit): Promise<Response> {
+	private async requestJSON(path: string, request: RequestInit)
+	: Promise<Response<any, any, any, any, any>> {
 		const completePath = `${this.basePath}/${path}`
 		return await fetch(new Request(completePath, request)).then(async response => {
 			return {
