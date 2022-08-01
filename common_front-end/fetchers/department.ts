958130eb2aeb5f7af238cf349f37b7e20f85de63
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-01 14:38:06 +0800

    types(front-end): supply department fetcher with the special types

--- common_front-end/fetchers/department.ts
+++ common_front-end/fetchers/department.ts
@@ -1,7 +1,29 @@
-import type { DepartmentAttributes } from "$/types/documents/department"
+import type { Serializable } from "$/types/general"
+import type { CommonQueryParameters } from "$/types/query"
+import type {
+	DepartmentResourceIdentifier,
+	DepartmentAttributes,
+	DepartmentResource,
+	DeserializedDepartmentResource,
+	DepartmentDocument,
+	DepartmentListDocument,
+	DeserializedDepartmentDocument,
+	DeserializedDepartmentListDocument
+} from "$/types/documents/department"
 import Fetcher from "$@/fetchers/fetcher"
 
-export default class DepartmentFetcher extends Fetcher<DepartmentAttributes> {
+export default class DepartmentFetcher extends Fetcher<
+	DepartmentResourceIdentifier,
+	DepartmentAttributes,
+	DepartmentResource,
+	DeserializedDepartmentResource,
+	DepartmentDocument,
+	DepartmentListDocument,
+	DeserializedDepartmentDocument,
+	DeserializedDepartmentListDocument,
+	Serializable,
+	CommonQueryParameters
+> {
 	static initialize(basePath: string) {
 		super.initialize(basePath, "department")
 	}
