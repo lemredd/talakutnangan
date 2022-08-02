3a0ee4805321b7b700108c859c48dbd5cc2acfca
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-02 13:21:52 +0800

    refactor(validation): use a generator to validate ID parameters

--- server/middlewares/validation/id_parameter.ts
+++ server/middlewares/validation/id_parameter.ts
@@ -1,28 +1,30 @@
-import { Request } from "!/types/dependent"
+import type { Request } from "!/types/dependent"
+import type { FieldRules } from "!/types/validation"
 import type { BaseManagerClass } from "!/types/independent"
 import RouteParameterValidation from "!/middlewares/validation/route_parameter"
+
+import integer from "!/app/validators/base/integer"
+import exists from "!/app/validators/manager/exists"
+import required from "!/app/validators/base/required"
 
 type ParameterInfo = [ string, BaseManagerClass ]
 
 export default class extends RouteParameterValidation {
 	constructor(IDs: ParameterInfo[]) {
-		super(IDs.reduce<{ [key:string]: any[] }>((previousValidationRules, info) => {
-			return {
+		super((request: Request): FieldRules => IDs.reduce<FieldRules>(
+			(previousValidationRules, info) => ({
 				...previousValidationRules,
-				[info[0]]: [
-					"required",
-					"numeric",
-					[
-						"exists",
-						info[1],
-						"id"
-					]
-				]
-			}
-		}, {}))
-	}
-
-	getSubject(request: Request): object {
-		return request.params
+				[info[0]]: {
+					pipes: [ required, integer, exists ],
+					constraints: {
+						manager: {
+							className: info[1],
+							columnName: "id"
+						}
+					}
+				}
+			}),
+			{}
+		))
 	}
 }
