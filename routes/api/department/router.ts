cda7104637733b1553ead0974cbe4ef7c23c80b0
Arukane <19201.magtoto.angelo.l@gmail.com>
2022-08-02 18:08:53 +0800

    intrn(department): register route for counting associated users

--- routes/api/department/router.ts
+++ routes/api/department/router.ts
@@ -4,6 +4,7 @@ import PostCreate from "!%/api/department/create.post"
 import PatchUpdate from "!%/api/department/update(id).patch"
 import PatchRestore from "!%/api/department/restore.patch"
 import DeleteArchive from "!%/api/department/archive.delete"
+import GetCountUsers from "!%/api/department/count_users.get"
 
 export default class extends Router {
 	constructor() {
@@ -14,7 +15,8 @@ export default class extends Router {
 			new PostCreate(),
 			new PatchUpdate(),
 			new PatchRestore(),
-			new DeleteArchive()
+			new DeleteArchive(),
+			new GetCountUsers()
 		])
 	}
 }
