d9f3c703b30d0d9cd8cbcba642e53eadeee1aec7
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-02 15:33:20 +0800

    test: add validation for multipart test route

--- routes/t/multipart.post.ts
+++ routes/t/multipart.post.ts
@@ -1,6 +1,10 @@
 import type { FieldRules } from "!/types/validation"
 import type { Request, Response } from "!/types/dependent"
 
+import array from "!/app/validators/base/array"
+import buffer from "!/app/validators/base/buffer"
+import nullable from "!/app/validators/base/nullable"
+
 import MultipartController from "!/common_controllers/multipart_controller"
 
 export default class extends MultipartController {
@@ -9,7 +13,23 @@ export default class extends MultipartController {
 	get policy(): null { return null }
 
 	makeBodyRuleGenerator(request: Request): FieldRules {
-		return {}
+		return {
+			importedCSV: {
+				pipes: [ buffer ],
+				constraints: {
+					buffer: {
+						allowedMimeTypes: [ "text/csv" ],
+						maxSize: 1024 * 1024 * 10 // 10 MB
+					}
+				}
+			},
+			roles: {
+				pipes: [ nullable, array ],
+				constraints: {
+					nullable: { defaultValue: [] }
+				}
+			}
+		}
 	}
 
 	async handle(request: Request, response: Response): Promise<void> {
