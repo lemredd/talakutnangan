9c0073452957088dd14a1273c0d05e61dadc8e96
Arukane <19201.magtoto.angelo.l@gmail.com>
2022-08-02 19:33:41 +0800

    intg(department): add integration testing for counting user

---
+++ t/integration/api/department/count_users.get.spec.ts
@@ -0,0 +1,47 @@
+import { JSON_API_MEDIA_TYPE } from "$/types/server"
+
+import App from "~/set-ups/app"
+import RoleFactory from "~/factories/role"
+import DepartmentFactory from "~/factories/department"
+import { READ } from "$/permissions/department_combinations"
+import RequestEnvironment from "$!/singletons/request_environment"
+import { department as permissionGroup } from "$/permissions/permission_list"
+
+import Route from "!%/api/department/count_users.get"
+
+describe("GET /api/department/count_users", () => {
+	beforeAll(async () => {
+		await App.create(new Route())
+	})
+
+	it("can be accessed by permitted user and get user count", async () => {
+		const adminDepartment = await new DepartmentFactory().insertOne()
+		const adminRole = await new RoleFactory()
+			.roleFlags(permissionGroup.generateMask(...READ))
+			.insertOne()
+		const { user: admin, cookie } = await App.makeAuthenticatedCookie(
+			adminRole,
+			userFactory => userFactory.in(adminDepartment)
+		)
+
+		const response = await App.request
+			.get("/api/department/count_users")
+			.send({
+				data: [
+					{
+						type: "department",
+						id: adminDepartment.id
+					}
+				]
+			})
+			.set("Cookie", cookie)
+			.type(JSON_API_MEDIA_TYPE)
+			.accept(JSON_API_MEDIA_TYPE)
+
+		expect(response.statusCode).toBe(RequestEnvironment.status.OK)
+		expect(response.body).toHaveProperty("data.0.type", "department")
+		expect(response.body).toHaveProperty("data.0.id", adminDepartment.id)
+		expect(response.body).toHaveProperty("data.0.meta.userCount", 1)
+		expect(response.body).not.toHaveProperty("data.1")
+	})
+})
