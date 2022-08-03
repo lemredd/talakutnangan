6467edc9f7d34f8eb34ff93c71134401ab87fcdb
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-08-03 23:34:57 +0800

    refactor(user): use user fetcher
    Co-authored-by: Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>

--- pages/studemp_list/index.page.vue
+++ pages/studemp_list/index.page.vue
@@ -5,29 +5,44 @@
 </template>
 
 <script setup lang="ts">
-import type { DeserializedUserResource } from "$/types/documents/user"
-
-import { onMounted, provide, ref } from "vue"
+import { onMounted, provide, ref, inject } from "vue"
+import type { DeserializedUserProfile, DeserializedUserResource } from "$/types/documents/user"
+import type { PageContext } from "#/types"
 
 import deserialize from "$/helpers/deserialize"
 import Manager from "@/resource_management/manager"
 import UsersManager from "@/resource_management/resource_manager.vue"
 import RoleFetcher from "$@/fetchers/role"
+import UserFetcher from "$@/fetchers/user"
 
-provide("managerKind", new Manager("dean"))
+const pageContext = inject("pageContext") as PageContext
+
+provide("managerKind", new Manager(pageContext.pageProps.userProfile! as DeserializedUserProfile))
+
 
 // Fetcher Initializers
 RoleFetcher.initialize("/api")
 
 const users = ref<DeserializedUserResource[]>([])
 onMounted(() => {
+const currentUserProfile = (pageContext.pageProps.userProfile! as DeserializedUserProfile).data
+	const currentUserDepartment = currentUserProfile!.department.data.id
 
-	// TODO: fetch("/api/user/list") soon
-	fetch("/dev/sample_user_list")
-	.then(response => response.json())
-	.then(response => {
-		const deserializedData = deserialize(response)!.data as DeserializedUserResource[]
-		users.value = deserializedData
+	new UserFetcher().list({
+		filter: {
+			slug: "",
+			department: currentUserDepartment,
+			role: "*",
+			kind: "*",
+			existence: "exists"
+		},
+		sort: [ "name" ],
+		page: {
+			offset: 0,
+			limit: 10
+		}
+	}).then(({ body: deserializedUserList }) => {
+		users.value = deserializedUserList.data
 	})
 })
 </script>
