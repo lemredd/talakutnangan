b8767bc1e0f24ac003f1670f4f3f648996a0a83e
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-03 15:07:02 +0800

    intrn(policy): add policy to limit operation to owner

---
+++ server/policies/belongs_to_current_user.spec.ts
@@ -0,0 +1,56 @@
+import UserTransformer from "%/transformers/user"
+import Serializer from "%/transformers/serializer"
+
+import RoleFactory from "~/factories/role"
+import UserFactory from "~/factories/user"
+import MockRequester from "~/set-ups/mock_requester"
+import AuthorizationError from "$!/errors/authorization"
+import UserPermissions from "$/permissions/user"
+
+import BelongsToCurrentUserPolicy from "./belongs_to_current_user"
+
+describe("Policy: Belongs to current user", () => {
+	const requester  = new MockRequester()
+	const transformer = new UserTransformer()
+	const permissions = new UserPermissions()
+
+	it("can allow user", async () => {
+		const role = await new RoleFactory().userFlags(permissions.generateMask("view")).insertOne()
+		const user = await (new UserFactory().attach(role)).insertOne()
+		const pageGuard = new BelongsToCurrentUserPolicy()
+		requester.customizeRequest({
+			user: Serializer.serialize(user, transformer, {}),
+			isAuthenticated: jest.fn().mockReturnValue(true),
+			params: {
+				id: user.id+""
+			}
+		})
+
+		await requester.runMiddleware(pageGuard.intermediate.bind(pageGuard))
+
+		requester.expectSuccess()
+	})
+
+	it("cannot allow different user", async () => {
+		const role = await new RoleFactory().userFlags(permissions.generateMask("view")).insertOne()
+		const user = await (new UserFactory().attach(role)).insertOne()
+		const pageGuard = new BelongsToCurrentUserPolicy()
+		requester.customizeRequest({
+			user: Serializer.serialize(user, transformer, {}),
+			isAuthenticated: jest.fn().mockReturnValue(true),
+			params: {
+				id: (user.id + 1)+""
+			}
+		})
+
+		await requester.runMiddleware(pageGuard.intermediate.bind(pageGuard))
+
+		await requester.runMiddleware(pageGuard.intermediate.bind(pageGuard))
+
+		requester.expectNext([
+			[
+				(error: any) => expect(error).toBeInstanceOf(AuthorizationError)
+			]
+		])
+	})
+})
---
+++ server/policies/belongs_to_current_user.ts
@@ -0,0 +1,27 @@
+import type { AuthenticatedIDRequest } from "!/types/dependent"
+import type { DeserializedUserProfile } from "$/types/documents/user"
+
+import deserialize from "$/helpers/deserialize"
+import AuthorizationError from "$!/errors/authorization"
+import AuthenticationBasedPolicy from "!/policies/authentication-based"
+
+/**
+ * Creates a policy to limit the operation to the owner of the resource.
+ *
+ * Requires that the resource has been validated ID route parameter which is then compared if the
+ * current logged in user has the same primary ID.
+ */
+export default class extends AuthenticationBasedPolicy {
+	constructor() {
+		super(true)
+	}
+
+	async authorize(request: AuthenticatedIDRequest): Promise<void> {
+		await super.authorize(request)
+
+		const user = deserialize(request.user) as DeserializedUserProfile
+		if (user.data.id !== +request.params.id) {
+			throw new AuthorizationError("Only the owner of the resource can do the operation.")
+		}
+	}
+}
