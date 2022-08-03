154312849e3cec8a90cbb7b0b717e09ea4973ce6
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-03 17:40:42 +0800

    intrn(response): allow OK responses to have different content type

--- server/response_infos/ok.ts
+++ server/response_infos/ok.ts
@@ -1,10 +1,11 @@
-import ResponseInfo from "!/bases/response_info"
-
 import type { Serializable } from "$/types/general"
+import { JSON_API_MEDIA_TYPE } from "$/types/server"
+
+import ResponseInfo from "!/bases/response_info"
 import RequestEnvironment from "$/helpers/request_environment"
 
 export default class extends ResponseInfo {
-	constructor(body: Serializable) {
-		super(RequestEnvironment.status.OK, body)
+	constructor(body: Serializable, type: string = JSON_API_MEDIA_TYPE) {
+		super(RequestEnvironment.status.OK, type, body)
 	}
 }
