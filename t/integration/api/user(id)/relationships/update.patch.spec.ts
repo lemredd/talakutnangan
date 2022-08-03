e40469a547e5a5bea6d12b6514f974db4b26b77c
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-03 17:24:55 +0800

    intg(signature): correct the test code for attaching signature

--- t/integration/api/user(id)/relationships/update.patch.spec.ts
+++ t/integration/api/user(id)/relationships/update.patch.spec.ts
@@ -22,53 +22,23 @@ describe("PATCH /api/user/:id/relationships/signature/update", () => {
 	it("can upload valid student details", async () => {
 		jest.setTimeout(10000)
 
-		const adminRole = await new RoleFactory()
+		const studentRole = await new RoleFactory()
 			.userFlags(permissionGroup.generateMask(...UPDATE_OWN_DATA))
 			.insertOne()
-		const { user: admin, cookie } = await App.makeAuthenticatedCookie(adminRole, userFactory => {
+		const { user: admin, cookie } = await App.makeAuthenticatedCookie(studentRole, userFactory => {
 			return userFactory.beStudent()
 		})
 		const path = `${RequestEnvironment.root}/t/data/logo_bg_transparent.png`
 
 		const response = await App.request
-			.post(`/api/user/${admin.id}/relationships/signature/update`)
+			.patch(`/api/user/${admin.id}/relationships/signature/update`)
 			.field("data[type]", "signature")
 			.attach("data[attributes][signature]", path)
 			.set("Cookie", cookie)
+			.type(MULTIPART_MEDIA_TYPE)
+			.accept(JSON_API_MEDIA_TYPE)
 
+			console.log(response.body, "\n\n\n\\n\n\n\n\n\n")
 		expect(response.statusCode).toBe(RequestEnvironment.status.OK)
-		expect(response.body.data).toHaveLength(3)
-		expect(response.body.included).toHaveLength(7)
-
-		await flushPromises() // Policy middleware runs
-		await flushPromises() // Middleware intermediate runs
-		await flushPromises() // E-mail template read for first user
-		await flushPromises() // E-mail template read for second user
-		await flushPromises() // E-mail template read for third user
-		await flushPromises() // E-mail message transmission to first user
-		await flushPromises() // E-mail message transmission to second user
-		await flushPromises() // E-mail message transmission to third user
-
-		// Waiting for all transmissions to finish
-		await new Promise(resolve => setTimeout(resolve, 1000))
-
-		const previousMessages = Transport.consumePreviousMessages()
-		expect(previousMessages).toHaveLength(3)
-		expect(previousMessages[0]).toHaveProperty("message")
-		expect(previousMessages[0]).toHaveProperty("message.subject", "New User in Talakutnangan")
-		expect(previousMessages[0].message.text).toContain("Juan Dela Cruz")
-		expect(previousMessages[0].message.text).toContain("j.delacruz20111@mcc.edu.ph")
-		expect(previousMessages[0].message.text).toContain("student")
-		expect(previousMessages[0].message.text).toContain("1920-111")
-		expect(previousMessages[1]).toHaveProperty("message.subject", "New User in Talakutnangan")
-		expect(previousMessages[1].message.text).toContain("Alice Garcia")
-		expect(previousMessages[1].message.text).toContain("a.garcia20112@mcc.edu.ph")
-		expect(previousMessages[1].message.text).toContain("student")
-		expect(previousMessages[1].message.text).toContain("1920-112")
-		expect(previousMessages[2]).toHaveProperty("message.subject", "New User in Talakutnangan")
-		expect(previousMessages[2].message.text).toContain("Bob Marquis")
-		expect(previousMessages[2].message.text).toContain("n.marquis20113@mcc.edu.ph")
-		expect(previousMessages[2].message.text).toContain("student")
-		expect(previousMessages[2].message.text).toContain("1920-113")
 	})
 })
