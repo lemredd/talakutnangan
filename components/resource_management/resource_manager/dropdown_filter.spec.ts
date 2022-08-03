dc8acfc78639901d70f7c2863acd5e7096cc3668
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-03 12:41:20 +0800

    unit(component): update the test for dropdown filter

--- components/resource_management/resource_manager/dropdown_filter.spec.ts
+++ components/resource_management/resource_manager/dropdown_filter.spec.ts
@@ -1,29 +1,35 @@
+import type { DeserializedUserProfile } from "$/types/documents/user"
+
 import { mount } from "@vue/test-utils"
 import flushPromises from "flush-promises"
 
-import RequestEnvironment from "$/helpers/request_environment"
-import RoleTransformer from "%/transformers/role"
 import RoleFactory from "~/factories/role"
+import UserFactory from "~/factories/user"
+import RoleTransformer from "%/transformers/role"
+import DepartmentFactory from "~/factories/department"
+import RequestEnvironment from "$/helpers/request_environment"
 
 import DropdownFilter from "./dropdown_filter.vue"
 import Serializer from "%/transformers/serializer"
+
+import { user as permissionGroup } from "$/permissions/permission_list"
+import { READ_ANYONE_ON_OWN_DEPARTMENT } from "$/permissions/user_combinations"
+
 import Manager from "../manager"
 
 describe("UI Component: Dropdown Filter", () => {
 	describe("Role Filters", () => {
-		async function listFilters() {
-			const transformer = new RoleTransformer()
-
-			const roles = await new RoleFactory().makeMany(5)
-			const serializedRoles = Serializer.serialize(roles, transformer, {})
-			fetchMock.mockResponseOnce(JSON.stringify(serializedRoles),
-			{ status: RequestEnvironment.status.OK })
-
-			return serializedRoles
-		}
-
 		it("Should list all possible roles", async () => {
-			await listFilters()
+			fetchMock.mockResponseOnce(
+				JSON.stringify(await new RoleFactory().deserializedMany(5)),
+				{ status: RequestEnvironment.status.OK }
+			)
+
+			const department = await new DepartmentFactory().mayAdmit().insertOne()
+			const deanRole = await new RoleFactory()
+				.userFlags(permissionGroup.generateMask(...READ_ANYONE_ON_OWN_DEPARTMENT))
+				.insertOne()
+			const user = await new UserFactory().in(department).attach(deanRole).deserializedOne()
 
 			const wrapper = mount(DropdownFilter, {
 				shallow: true,
@@ -32,7 +38,7 @@ describe("UI Component: Dropdown Filter", () => {
 				},
 				global: {
 					provide: {
-						managerKind: new Manager("dean")
+						managerKind: new Manager(user as DeserializedUserProfile)
 					}
 				}
 			})
