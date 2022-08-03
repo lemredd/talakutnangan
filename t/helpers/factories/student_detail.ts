405db6dbd5b90ba544662fb544e6f0651c860450
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-03 11:14:09 +0800

    test(factory): add required methods and types in student detail factory

--- t/helpers/factories/student_detail.ts
+++ t/helpers/factories/student_detail.ts
@@ -1,18 +1,35 @@
-import { faker } from "@faker-js/faker"
-
 import type { ModelCtor } from "%/types/dependent"
 import type { GeneratedData } from "~/types/dependent"
+import type {
+	StudentDetailResourceIdentifier,
+	StudentDetailAttributes,
+	DeserializedStudentDetailResource,
+	DeserializedStudentDetailDocument,
+	DeserializedStudentDetailListDocument
+} from "$/types/documents/student_detail"
+
+import { faker } from "@faker-js/faker"
 
 import User from "%/models/user"
-import StudentDetail from "%/models/student_detail"
 import UserFactory from "~/factories/user"
 import BaseFactory from "~/factories/base"
+import StudentDetail from "%/models/student_detail"
+import StudentDetailTransformer from "%/transformers/student_detail"
 
-export default class SignatureFactory extends BaseFactory<StudentDetail> {
+export default class StudentDetailFactory extends BaseFactory<
+	StudentDetail,
+	StudentDetailResourceIdentifier,
+	StudentDetailAttributes,
+	DeserializedStudentDetailResource,
+	DeserializedStudentDetailDocument,
+	DeserializedStudentDetailListDocument
+> {
 	#user: () => Promise<User>  =  () => new UserFactory().insertOne()
 	#studentNumber: () => string = () => `${faker.random.numeric(4)}-${faker.random.numeric(4)}`
 
 	get model(): ModelCtor<StudentDetail> { return StudentDetail }
+
+	get transformer(): StudentDetailTransformer { return new StudentDetailTransformer() }
 
 	async generate(): GeneratedData<StudentDetail> {
 		return {
@@ -21,12 +38,12 @@ export default class SignatureFactory extends BaseFactory<StudentDetail> {
 		}
 	}
 
-	studentNumber(generator: () => string): SignatureFactory {
+	studentNumber(generator: () => string): StudentDetailFactory {
 		this.#studentNumber = generator
 		return this
 	}
 
-	user(generator: () => Promise<User>): SignatureFactory {
+	user(generator: () => Promise<User>): StudentDetailFactory {
 		this.#user = generator
 		return this
 	}
