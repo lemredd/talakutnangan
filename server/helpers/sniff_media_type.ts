4a907f5aec438672f77fe87c5a96d3c7e79c2004
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-04 08:36:34 +0800

    intrn(server): use another package to sniff the media type

--- server/helpers/sniff_media_type.ts
+++ server/helpers/sniff_media_type.ts
@@ -1,5 +1,12 @@
-import { fileTypeFromBuffer } from "file-type"
+import { Magic, MAGIC_MIME_TYPE } from "mmmagic"
 
-export default async function(buffer: ArrayBuffer): Promise<string|undefined> {
-	return (await fileTypeFromBuffer(buffer))?.mime
+const magic = new Magic(MAGIC_MIME_TYPE)
+
+export default async function(buffer: Buffer): Promise<string|undefined> {
+	return await new Promise((resolve, reject) => {
+		magic.detect(buffer, (error, result) => {
+			if (error) reject(error)
+			else resolve(result as string)
+		})
+	})
 }
