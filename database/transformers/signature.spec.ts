dd0340904f2ef49f3e1cb1a3250681b5175f2a9e
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-04 13:06:30 +0800

    fix(test): correct the URL to get the signature

--- database/transformers/signature.spec.ts
+++ database/transformers/signature.spec.ts
@@ -31,7 +31,7 @@ describe("Transformer: Signature", () => {
 		expect(object).toHaveProperty("data.id", model.id)
 		expect(object).toHaveProperty(
 			"data.links.self",
-			"http://localhost:16000/api/signature/"+model.id
+			"http://localhost:16000/api/signature/read/"+model.id
 		)
 	})
 
@@ -45,13 +45,13 @@ describe("Transformer: Signature", () => {
 		expect(object).toHaveProperty("data.0.id", models[0].id)
 		expect(object).toHaveProperty(
 			"data.0.links.self",
-			"http://localhost:16000/api/signature/"+models[0].id
+			"http://localhost:16000/api/signature/read/"+models[0].id
 		)
 		expect(object).toHaveProperty("data.1.type", "signature")
 		expect(object).toHaveProperty("data.1.id", models[1].id)
 		expect(object).toHaveProperty(
 			"data.1.links.self",
-			"http://localhost:16000/api/signature/"+models[1].id
+			"http://localhost:16000/api/signature/read/"+models[1].id
 		)
 	})
 })
