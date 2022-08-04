45542f6337574486e62ed0ca3b48e8bf8238237c
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-04 12:34:03 +0800

    intrn(transformer): correct the path to read the signature

--- t/integration/api/user(id)/relationships/update.patch.spec.ts
+++ t/integration/api/user(id)/relationships/update.patch.spec.ts
@@ -43,7 +43,7 @@ describe("PATCH /api/user/:id/relationships/signature/update", () => {
 		expect(response.body).toHaveProperty("data.id")
 		expect(response.body).toHaveProperty(
 			"data.links.self",
-			"http://localhost:16000/api/signature/"+response.body.data.id
+			"http://localhost:16000/api/signature/read/"+response.body.data.id
 		)
 	})
 })
