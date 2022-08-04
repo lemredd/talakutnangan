d0237ebfc2d49da3150e78bdddeb7cfc0a0e2c76
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-04 12:55:03 +0800

    intg(signature): ensure route to read signature works as expected

---
+++ t/integration/api/signature/read(id).get.spec.ts
@@ -0,0 +1,38 @@
+import { MULTIPART_MEDIA_TYPE, JSON_API_MEDIA_TYPE } from "$/types/server"
+
+import "~/set-ups/email.set_up"
+
+import App from "~/set-ups/app"
+import RoleFactory from "~/factories/role"
+import SignatureFactory from "~/factories/signature"
+import RequestEnvironment from "$!/singletons/request_environment"
+
+import { READ_OWN } from "$/permissions/user_combinations"
+import { user as permissionGroup } from "$/permissions/permission_list"
+
+import Route from "!%/api/signature/read(id).get"
+
+describe("GET /api/signature/read/:id", () => {
+	beforeAll(async () => {
+		await App.create(new Route())
+	})
+
+	it("can read signature", async () => {
+		const signature = await new SignatureFactory().insertOne()
+		const studentRole = await new RoleFactory()
+			.userFlags(permissionGroup.generateMask(...READ_OWN))
+			.insertOne()
+		const { user: admin, cookie } = await App.makeAuthenticatedCookie(
+			studentRole,
+			userFactory => {
+				return userFactory.beStudent()
+			})
+
+		const response = await App.request
+			.get(`/api/signature/read/${signature.id}`)
+			.set("Cookie", cookie)
+
+		expect(response.statusCode).toBe(RequestEnvironment.status.OK)
+		expect(response.body).toStrictEqual(signature.signature)
+	})
+})
