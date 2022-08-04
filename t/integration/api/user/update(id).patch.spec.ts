01332bfd1de3a314c87e2915e8143aecb30034d3
Angelo Magtoto <19201.magtoto.angelo.l@gmail.com>
2022-08-04 18:58:48 +0800

    fix(test): correct the inputs for test
    Co-authored-by: Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>

--- t/integration/api/user/update(id).patch.spec.ts
+++ t/integration/api/user/update(id).patch.spec.ts
@@ -35,7 +35,7 @@ describe("PATCH /api/user/update/:id", () => {
 					id: student.id,
 					attributes: {
 						name: student.name,
-						email: student.email
+						email: newStudent.email
 					}
 				}
 			})
@@ -76,12 +76,18 @@ describe("PATCH /api/user/update/:id", () => {
 
 		const response = await App.request
 			.patch(`/api/user/update/${student.id}`)
-			.field("data[type]", "user")
-			.field("data[id]", student.id)
-			.field("data[attributes][name]", newStudent.name)
-			.field("data[attributes][email]", student.email)
 			.set("Cookie", cookie)
-			.type(MULTIPART_MEDIA_TYPE)
+			.send({
+				data: {
+					type: "user",
+					id: student.id,
+					attributes: {
+						name: newStudent.name,
+						email: student.email
+					}
+				}
+			})
+			.type(JSON_API_MEDIA_TYPE)
 			.accept(JSON_API_MEDIA_TYPE)
 
 		console.log(response.body)
