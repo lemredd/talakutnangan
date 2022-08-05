89a959291e8a57dfe395443bc1f4af6cdd6905a7
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-08-05 15:50:34 +0800

    intrn(UI): make suspensible component
    * this loads asynchronous resource with fallback template

---
+++ components/suspensible.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="suspensible">
+		<div class="loaded" v-if="asyncResource">
+			<slot></slot>
+		</div>
+
+		<div class="loading" v-else>
+			Loading...
+		</div>
+	</div>
+</template>
+
+<style>
+</style>
+
+<script setup lang="ts">
+import type { PossibleResources } from "$@/types/independent"
+
+const { asyncResource } = defineProps<{
+	asyncResource: PossibleResources | PossibleResources[]
+}>()
+</script>
