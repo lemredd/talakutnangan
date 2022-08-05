4896d6982f099866a19bc771e00bc2d21c16fd4b
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-08-05 15:50:49 +0800

    unit(UI): test suspensible component

---
+++ components/suspensible.spec.ts
@@ -0,0 +1,53 @@
+import { mount } from "@vue/test-utils"
+import RoleFactory from "~/factories/role"
+import { user as permissionGroup } from "$/permissions/permission_list"
+import { READ_ANYONE_ON_OWN_DEPARTMENT } from "$/permissions/user_combinations"
+import Suspensible from "./suspensible.vue"
+import RoleFetcher from "$@/fetchers/role"
+import RequestEnvironment from "$/helpers/request_environment"
+
+describe("UI Component: Suspensible", () => {
+	it("Should load asynchronous resource", async () => {
+		const sampleResource = await new RoleFactory()
+		.userFlags(permissionGroup.generateMask(...READ_ANYONE_ON_OWN_DEPARTMENT))
+		.serializedOne()
+
+		fetchMock.mockResponseOnce(JSON.stringify({
+			data: sampleResource
+		}), { status: RequestEnvironment.status.OK })
+
+		const fetcher = new RoleFetcher()
+		const response = await fetcher.read(sampleResource.data.id)
+
+		const wrapper = mount(Suspensible, {
+			props: {
+				asyncResource: response.body.data
+			}
+		})
+
+		const loaded = wrapper.find(".loaded")
+		expect(wrapper.html()).toContain(loaded.html())
+	})
+
+	it.todo("Should fall back if resource is not fully loaded"/* , async () => {
+		const sampleResource = await new RoleFactory()
+		.userFlags(permissionGroup.generateMask(...READ_ANYONE_ON_OWN_DEPARTMENT))
+		.serializedOne()
+
+		fetchMock.mockResponseOnce(JSON.stringify({
+			data: sampleResource
+		}), { status: RequestEnvironment.status.OK })
+
+		const fetcher = new RoleFetcher()
+		const response = await fetcher.read(sampleResource.data.id)
+
+		const wrapper = mount(Suspensible, {
+			props: {
+				asyncResource: response.body.data
+			}
+		})
+
+		const loading = wrapper.find(".loading")
+		expect(wrapper.html()).toContain(loading.html())
+	} */)
+})
