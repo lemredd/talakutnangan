99f7d61d8a4088336c892ebb5776c2fcc03a51a7
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-05 11:15:57 +0800

    intrn(parser): allow CSV parser to parse buffers in deep paths

--- server/middlewares/body_parser/csv.spec.ts
+++ server/middlewares/body_parser/csv.spec.ts
@@ -25,9 +25,35 @@ describe("Middleware: CSV Form Data Transformation", () => {
 		await requester.runMiddleware(parser.intermediate.bind(parser))
 
 		const successfulRequest = requester.expectSuccess()
-		expect(successfulRequest.body).toHaveProperty([ "importedCSV", 0, "department" ], "IBCE")
-		expect(successfulRequest.body).toHaveProperty([ "importedCSV", 1, "department" ], "IASTE")
-		expect(successfulRequest.body).toHaveProperty([ "importedCSV", 2, "department" ], "IHTM")
+		expect(successfulRequest.body).toHaveProperty("importedCSV.0.department", "IBCE")
+		expect(successfulRequest.body).toHaveProperty("importedCSV.1.department", "IASTE")
+		expect(successfulRequest.body).toHaveProperty("importedCSV.2.department", "IHTM")
+	})
+
+	it("can parse body in deep path properly", async () => {
+		const parser = new CSVParser("meta.importedCSV")
+		const rawBody = [
+			'﻿Student Number,Name,Email,Department',
+			'1920-111,Juan Dela Cruz,j.delacruz20111@mcc.edu.ph,IBCE',
+			'1920-112,Alice Garcia,a.garcia20112@mcc.edu.ph,IASTE',
+			'1920-113,Bob Marquis,n.marquis20113@mcc.edu.ph,IHTM',
+		].join("\r\n")
+		requester.customizeRequest({
+			body: {
+				meta: {
+					importedCSV: {
+						buffer: Buffer.from(rawBody)
+					}
+				}
+			}
+		})
+
+		await requester.runMiddleware(parser.intermediate.bind(parser))
+
+		const successfulRequest = requester.expectSuccess()
+		expect(successfulRequest.body).toHaveProperty("meta.importedCSV.0.department", "IBCE")
+		expect(successfulRequest.body).toHaveProperty("meta.importedCSV.1.department", "IASTE")
+		expect(successfulRequest.body).toHaveProperty("meta.importedCSV.2.department", "IHTM")
 	})
 
 	it("cannot process invalid body", async () => {
--- server/middlewares/body_parser/csv.ts
+++ server/middlewares/body_parser/csv.ts
@@ -7,12 +7,15 @@ import Log from "$!/singletons/log"
 import BaseError from "$!/errors/base"
 import ParserError from "$!/errors/parser"
 import Middleware from "!/bases/middleware"
+import setDeepPath from "$!/helpers/set_deep_path"
+import accessDeepPath from "$!/helpers/access_deep_path"
 import convertToCamelCase from "$/helpers/convert_to_camel_case"
 
 /**
  * Used to parse CSV files attached.
  *
  * Assumes request's body was parsed buy `Multipart` middleware.
+ * Assumes the fields to be converted exists.
  */
 export default class CSVParser extends Middleware {
 	private parser = parse({
@@ -30,47 +33,48 @@ export default class CSVParser extends Middleware {
 	async intermediate(request: Request, response: Response, next: NextFunction): Promise<void> {
 		try {
 			for (const field of this.rawFields) {
-				// TODO: Throw error if the field does not exists or does not match validation
-				const buffer: Buffer = request.body[field].buffer
+				const buffer: Buffer = accessDeepPath(request.body, `${field}.buffer`)
 
-				request.body[field] = await new Promise<GeneralObject<string>[]>((resolve, reject) => {
-					const rows: { [key:string]: string }[] = []
-					const reader = () => {
-						Log.trace("middleware", `reading the rows in the CSV file in "${field}" field`)
-						let row;
-						while((row = this.parser.read()) !== null) {
-							rows.push(row)
+				setDeepPath(request.body, field, await new Promise<GeneralObject<string>[]>(
+					(resolve, reject) => {
+						const rows: { [key:string]: string }[] = []
+						const reader = () => {
+							Log.trace("middleware", `reading the rows in the CSV file in "${field}" field`)
+							let row;
+							while((row = this.parser.read()) !== null) {
+								rows.push(row)
+							}
 						}
-					}
-					const thrower = (error: Error) => {
-						Log.errorMessage(
-							"middleware",
-							`encountered error in processing CSV file  in "${field}" field`
-						)
-						Log.error("middleware", error)
+						const thrower = (error: Error) => {
+							Log.errorMessage(
+								"middleware",
+								`encountered error in processing CSV file  in "${field}" field`
+							)
+							Log.error("middleware", error)
 
-						unlisten()
-						reject(error)
+							unlisten()
+							reject(error)
+						}
+						const closer = () => {
+							Log.trace(
+								"middleware",
+								`resolving the read rows in the CSV file io "${field}" field`
+							)
+							unlisten()
+							resolve(rows)
+						}
+						const unlisten = () => {
+							this.parser.off("readable", reader)
+							this.parser.off("error", thrower)
+							this.parser.off("end", closer)
+						}
+						this.parser.on("readable", reader)
+						this.parser.on("error", thrower)
+						this.parser.on("end", closer)
+						this.parser.write(buffer)
+						this.parser.end()
 					}
-					const closer = () => {
-						Log.trace(
-							"middleware",
-							`resolving the read rows in the CSV file io "${field}" field`
-						)
-						unlisten()
-						resolve(rows)
-					}
-					const unlisten = () => {
-						this.parser.off("readable", reader)
-						this.parser.off("error", thrower)
-						this.parser.off("end", closer)
-					}
-					this.parser.on("readable", reader)
-					this.parser.on("error", thrower)
-					this.parser.on("end", closer)
-					this.parser.write(buffer)
-					this.parser.end()
-				})
+				))
 
 				Log.success("middleware", `parsed the CSV file in "${field}" field`)
 			}
