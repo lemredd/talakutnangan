265d2ba291c5070223f13dcd31d082e821cdcb09
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-05 10:55:29 +0800

    unit(parser): identify the error to expect for invalid CSV

--- server/middlewares/body_parser/csv.spec.ts
+++ server/middlewares/body_parser/csv.spec.ts
@@ -29,5 +29,24 @@ describe("Middleware: CSV Form Data Transformation", () => {
 		expect(successfulRequest.body).toHaveProperty([ "importedCSV", 2, "department" ], "IHTM")
 	})
 
-	it.todo("cannot process invalid body")
+	it("cannot process invalid body", async () => {
+		const parser = new CSVParser("importedCSV")
+		const rawBody = [
+			'﻿Student Number,Name,Email,Department',
+			'1920-111,Juan Dela Cruz,j.delacruz20111@mcc.edu.ph,IBCE',
+			'1920-112,Alice Garcia,a.garcia20112@mcc.edu.phIASTE',
+			'1920-113,Bob Marquis,n.marquis20113@mcc.edu.ph,IHTM',
+		].join("\r\n")
+		requester.customizeRequest({
+			body: {
+				importedCSV: {
+					buffer: Buffer.from(rawBody)
+				}
+			}
+		})
+
+		await requester.runMiddleware(parser.intermediate.bind(parser))
+
+		requester.expectFailure(Error)
+	})
 })
