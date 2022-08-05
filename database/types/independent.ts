27b719f18eab050a78d73f7a56b5dd1d902b8370
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-05 12:05:21 +0800

    types(database): treat common raw bulk data to be serializable

--- database/types/independent.ts
+++ database/types/independent.ts
@@ -5,11 +5,12 @@
  */
 
 import type { Day } from "$/types/database"
+import type { Serializable } from "$/types/general"
 
 /**
  * Common shape to expect for creating profiles in bulk
  */
-export interface CommonRawBulkData {
+export interface CommonRawBulkData extends Serializable {
 	name: string,
 	email: string,
 	department: string,
