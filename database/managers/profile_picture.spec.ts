64837a36d94e3a86129ef9f86886d796acd50a20
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-06 15:43:32 +0800

    unit(manager): test the profile picture manger properly

--- database/managers/profile_picture.spec.ts
+++ database/managers/profile_picture.spec.ts
@@ -4,12 +4,14 @@ import ProfilePictureFactory from "~/factories/profile_picture"
 import ProfilePictureManager from "./profile_picture"
 
 describe("Database: Profile picture update operations", () => {
-	it("can create profile picture if not yet existing", async () => {
+	it("can update profile picture", async () => {
 		const manager = new ProfilePictureManager()
-		const profilePicture = await new ProfilePictureFactory().makeOne()
+		const newProfilePicture = await new ProfilePictureFactory().makeOne()
 		const user = await new UserFactory().insertOne()
 
-		const affectedCount= await manager.updateContents(user.id, profilePicture.file)
+		await new ProfilePictureFactory().user(async () => user).insertOne()
+
+		const affectedCount= await manager.updateContents(user.id, newProfilePicture.file)
 
 		expect(affectedCount).toBe(1)
 	})
