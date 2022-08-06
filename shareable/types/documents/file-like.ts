8898530183ab3d2178ce61f7317ca9417154e76d
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-07 00:52:20 +0800

    types(share): remove unused types
    Document types for other file-like types need to extend the base
    document types for compatibility.

--- shareable/types/documents/file-like.ts
+++ shareable/types/documents/file-like.ts
@@ -1,54 +1,11 @@
-import type {
-	Resource,
-	Attributes,
-	ResourceIdentifier,
-	DeserializedResource,
-	ResourceDocument,
-	ResourceListDocument,
-	DeserializedResourceDocument,
-	DeserializedResourceListDocument
-} from "$/types/documents/base"
-
-export interface FileLikeResourceIdentifier extends ResourceIdentifier {}
+import type { Attributes } from "$/types/documents/base"
 
 export interface FileLikeAttributes<T = string> extends Attributes {
 	file?: T
 }
 
-export interface FileLikeResource<T = string> extends Resource<
-	FileLikeResourceIdentifier,
-	FileLikeAttributes<T>
-> {
+export interface FileLikeResourceLinks {
 	links?: {
 		self: string
 	}
 }
-
-export interface DeserializedFileLikeResource extends DeserializedResource<
-	FileLikeResourceIdentifier,
-	FileLikeAttributes
-> {}
-
-export interface FileLikeDocument<T = Buffer> extends ResourceDocument<
-	FileLikeResourceIdentifier,
-	FileLikeAttributes<T>,
-	FileLikeResource<T>
-> {}
-
-export interface FileLikeListDocument extends ResourceListDocument<
-	FileLikeResourceIdentifier,
-	FileLikeAttributes,
-	FileLikeResource
-> {}
-
-export interface DeserializedFileLikeDocument extends DeserializedResourceDocument<
-	FileLikeResourceIdentifier,
-	FileLikeAttributes,
-	DeserializedFileLikeResource
-> {}
-
-export interface DeserializedFileLikeListDocument extends DeserializedResourceListDocument<
-	FileLikeResourceIdentifier,
-	FileLikeAttributes,
-	DeserializedFileLikeResource
-> {}
