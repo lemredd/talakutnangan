ae215964c3b6fecd63946ecfe113abc1a681ebf7
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-08-06 16:00:10 +0800

    intrn(UI): make and test dropdown select component

---
+++ components/fields/dropdown_select.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="read-scope">
+		<label for="read-scope">{{ label }}</label>
+		<select id="read-scope" @change="$emit('selectedOptionChanged', $event)">
+			<option
+				v-for="option in options" :value="option">
+					{{
+						isOptionString(option)
+						? camelToSentence(option).toLocaleLowerCase()
+						: option
+					}}
+			</option>
+		</select>
+	</div>
+</template>
+
+<style>
+</style>
+
+<script setup lang="ts">
+import camelToSentence from "$@/helpers/camel_to_sentence"
+
+const {
+	options,
+	label
+} = defineProps<{
+	options: any[]
+	label: string
+}>()
+
+function isOptionString(option: any): option is string {
+	return typeof option === "string"
+}
+
+defineEmits<{
+	(e: "selectedOptionChanged", event: Event): void
+}>()
+</script>
