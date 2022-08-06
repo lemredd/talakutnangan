06564fe52bd458e52f2b4ac200312299517489ce
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-07 00:37:30 +0800

    test(factory): generalize the file-like factory properly

--- t/helpers/factories/file-like.ts
+++ t/helpers/factories/file-like.ts
@@ -1,33 +1,33 @@
+import type { Model } from "%/types/dependent"
 import type { FileLikeTransformerOptions } from "%/types/independent"
 import type {
-	FileLikeResourceIdentifier,
-	FileLikeAttributes,
-	DeserializedFileLikeResource,
-	DeserializedFileLikeDocument,
-	DeserializedFileLikeListDocument
-} from "$/types/documents/file-like"
+	ResourceIdentifier,
+	Attributes,
+	DeserializedResource,
+	DeserializedResourceDocument,
+	DeserializedResourceListDocument
+} from "$/types/documents/base"
 
 import dataURIToBuffer from "data-uri-to-buffer"
 import type { MimeBuffer } from "data-uri-to-buffer"
 import { faker } from "@faker-js/faker"
 
 import BaseFactory from "~/factories/base"
-import FileLike from "%/models/file-like"
 
-export default abstract class FileLikeFactory extends BaseFactory<
-	FileLike,
-	FileLikeResourceIdentifier,
-	FileLikeAttributes,
-	DeserializedFileLikeResource,
-	DeserializedFileLikeDocument,
-	DeserializedFileLikeListDocument,
-	FileLikeTransformerOptions
-> {
+export default abstract class FileLikeFactory<
+	T extends Model,
+	U extends ResourceIdentifier,
+	V extends Attributes,
+	W extends DeserializedResource<U, V>,
+	X extends DeserializedResourceDocument<U, V, W>,
+	Y extends DeserializedResourceListDocument<U, V, W>,
+	Z extends FileLikeTransformerOptions = FileLikeTransformerOptions
+> extends BaseFactory<T, U, V, W, X, Y, Z> {
 	protected fileContentsGenerator: () => MimeBuffer = () => dataURIToBuffer(
 		faker.image.dataUri()
 	)
 
-	fileContents(generator: () => MimeBuffer): FileLikeFactory {
+	fileContents(generator: () => MimeBuffer): FileLikeFactory<T, U, V, W, X, Y, Z> {
 		this.fileContentsGenerator = generator
 		return this
 	}
