46b44aa3a5a0a25966734e100be5dd4fe2e6947c
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-08-06 16:28:44 +0800

    intrn(UI): add default option

--- components/fields/dropdown_select.vue
+++ components/fields/dropdown_select.vue
@@ -2,6 +2,7 @@
 	<div class="read-scope">
 		<label for="read-scope">{{ label }}</label>
 		<select id="read-scope" @change="$emit('selectedOptionChanged', $event)">
+			<option value="" selected></option>
 			<option
 				v-for="option in options" :value="option">
 					{{
