7c3ab6daf6621462cc2b68fcd55113598417a287
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-06 23:05:51 +0800

    intrn(transformer): make a file-like transformer

---
+++ database/transformers/file-like.ts
@@ -0,0 +1,60 @@
+import type { GeneralObject, Serializable } from "$/types/general"
+import type { FileLikeTransformerOptions } from "%/types/independent"
+import type { AttributesObject, TransformerOptions } from "%/types/dependent"
+
+import FileLike from "%/models/file-like"
+import Transformer from "%/transformers/base"
+import URLMaker from "$!/singletons/url_maker"
+import Serializer from "%/transformers/serializer"
+
+export default abstract class extends Transformer<FileLike, FileLikeTransformerOptions> {
+	transform(
+		model: FileLike|FileLike[],
+		options: TransformerOptions<FileLikeTransformerOptions>
+	): AttributesObject {
+		if (options.extra === undefined) {
+			options.extra = { raw: false }
+		}
+
+		const whiteListedAttributes = [ "id" ]
+
+		if (options.extra.raw) {
+			whiteListedAttributes.push("fileContents")
+		}
+
+		const safeObject = Serializer.whitelist(model, whiteListedAttributes)
+
+		return safeObject
+	}
+
+	finalizeTransform(model: FileLike|FileLike[]|null, resource: Serializable): Serializable {
+		resource = super.finalizeTransform(model, resource)
+
+		if (resource.data !== undefined && resource.data !== null) {
+			const templatePath = `/api/${this.type}/read/:id`
+
+			if (resource.data instanceof Array) {
+				resource.data = (resource.data as GeneralObject[]).map(data => {
+					if (data.attributes.file === undefined) {
+						data.links = {
+							self: URLMaker.makeURLFromPath(templatePath, {
+								id: data.id
+							})
+						}
+					}
+					return data
+				})
+			} else {
+				if ((resource.data as GeneralObject).attributes.file === undefined) {
+					(resource.data as Serializable).links = {
+						self: URLMaker.makeURLFromPath(templatePath, {
+							id: (resource.data as GeneralObject).id
+						})
+					}
+				}
+			}
+		}
+
+		return resource
+	}
+}
