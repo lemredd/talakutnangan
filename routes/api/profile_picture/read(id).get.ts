48dbc856a6551690685a7b9235f4f69a39470b32
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-06 17:27:43 +0800

    feat(profile picture): add route to get the profile picture

---
+++ routes/api/profile_picture/read(id).get.ts
@@ -0,0 +1,65 @@
+import type { ProfilePictureDocument } from "$/types/documents/profile_picture"
+import type { AuthenticatedIDRequest, Response } from "!/types/dependent"
+
+import Log from "$!/singletons/log"
+import Policy from "!/bases/policy"
+import UserManager from "%/managers/user"
+import Validation from "!/bases/validation"
+import OkResponseInfo from "!/response_infos/ok"
+import sniffMediaType from "!/helpers/sniff_media_type"
+import Controller from "!/bases/controller-likes/controller"
+import ProfilePictureManager from "%/managers/profile_picture"
+
+import {
+	READ_OWN,
+	READ_ANYONE_ON_OWN_DEPARTMENT,
+	READ_ANYONE_ON_ALL_DEPARTMENTS
+} from "$/permissions/user_combinations"
+import IDParameterValidation from "!/validations/id_parameter"
+import PermissionBasedPolicy from "!/policies/permission-based"
+import { user as permissionGroup } from "$/permissions/permission_list"
+
+export default class extends Controller {
+	get filePath(): string { return __filename }
+
+	get policy(): Policy {
+		return new PermissionBasedPolicy(permissionGroup, [
+			READ_OWN,
+			READ_ANYONE_ON_OWN_DEPARTMENT,
+			READ_ANYONE_ON_ALL_DEPARTMENTS
+		])
+	}
+
+	get bodyParser(): null { return null }
+
+	get validations(): Validation[] {
+		return [
+			new IDParameterValidation([
+				[ "id", UserManager ]
+			])
+		]
+	}
+
+	async handle(request: AuthenticatedIDRequest, response: Response)
+	: Promise<OkResponseInfo> {
+		const manager = new ProfilePictureManager(request.transaction, request.cache)
+		const { id } = request.params
+
+		const profilePictureDocument = await manager.findWithID(
+			+id,
+			{
+				filter: {
+					existence: "*"
+				}
+			},
+			{ raw: true }
+		) as ProfilePictureDocument
+
+		const profile_picture = profilePictureDocument.data.attributes.file as unknown as Buffer
+		const type = await sniffMediaType(profile_picture)
+
+		Log.success("controller", "successfully got the profile picture")
+
+		return new OkResponseInfo(profile_picture, type)
+	}
+}
