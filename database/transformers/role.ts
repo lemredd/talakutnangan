6d26604f6fdafa01fb115177c8057ad41f1d72ab
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-08-09 23:14:39 +0800

    intrn(transformer): include delete date
    Co-authored-by: Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>

--- database/transformers/role.ts
+++ database/transformers/role.ts
@@ -21,7 +21,8 @@ export default class extends Transformer<Role, void> {
 			"commentFlags",
 			"profanityFlags",
 			"userFlags",
-			"auditTrailFlags"
+			"auditTrailFlags",
+			"deletedAt"
 		])
 
 		return safeObject
