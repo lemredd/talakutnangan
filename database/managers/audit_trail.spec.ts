5160e2a61a2e156e085ac6307b8b60479f2d12cf
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-10 01:27:26 +0800

    intrn(manager): integrate other query pipes for audit trail manager

---
+++ database/managers/audit_trail.spec.ts
@@ -0,0 +1,21 @@
+import UserFactory from "~/factories/user"
+import Factory from "~/factories/audit_trail"
+import ProfilePictureFactory from "~/factories/profile_picture"
+
+import Manager from "./audit_trail"
+
+describe("Database: Audit trail read operations", () => {
+	it("can update profile picture", async () => {
+		const manager = new Manager()
+		const user = await new UserFactory().insertOne()
+		const model = await new Factory().user(async() => user).insertOne()
+		const profilePicture = await new ProfilePictureFactory().user(async() => user).insertOne()
+		model.user = user
+		model.user.profilePicture = profilePicture
+
+		const serializedModel = await manager.findWithID(model.id)
+
+		expect(serializedModel).toHaveProperty("included.0.id", user.id)
+		expect(serializedModel).toHaveProperty("included.1.id", profilePicture.id)
+	})
+})
--- database/managers/audit_trail.ts
+++ database/managers/audit_trail.ts
@@ -1,13 +1,29 @@
-import type { ModelCtor } from "%/types/dependent"
+import type { Pipe } from "$/types/database"
 import type { CommonQueryParameters } from "$/types/query"
+import type { ModelCtor, FindAndCountOptions } from "%/types/dependent"
 import type { AuditTrailAttributes } from "$/types/documents/audit_trail"
 
 import BaseManager from "%/managers/base"
 import AuditTrail from "%/models/audit_trail"
+import includeUser from "%/queries/audit_trail/include_user"
 import AuditTrailTransformer from "%/transformers/audit_trail"
 
 export default class  extends BaseManager<AuditTrail, AuditTrailAttributes, CommonQueryParameters> {
 	get model(): ModelCtor<AuditTrail> { return AuditTrail }
 
 	get transformer(): AuditTrailTransformer { return new AuditTrailTransformer() }
+
+	get singleReadPipeline(): Pipe<FindAndCountOptions<AuditTrail>, CommonQueryParameters>[] {
+		return [
+			includeUser,
+			...super.singleReadPipeline
+		]
+	}
+
+	get listPipeline(): Pipe<FindAndCountOptions<AuditTrail>, CommonQueryParameters>[] {
+		return [
+			includeUser,
+			...super.listPipeline
+		]
+	}
 }
