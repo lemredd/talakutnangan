8b41ac2f587c122fe2c94d15298d582bae97244c
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-08-09 23:21:50 +0800

    intrn(server): remove unnecessary properties

--- routes/api/role/read(id).get.ts
+++ routes/api/role/read(id).get.ts
@@ -61,11 +61,6 @@ export default class extends BoundJSONController {
 			filter: {
 				existence: "exists",
 				department: "*"
-			},
-			sort: ["id"],
-			page: {
-				offset: 0,
-				limit: 1
 			}
 		})
 
