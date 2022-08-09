36baffa86a7fc29409a1df61def1aafa0e5c3a9f
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-08-09 23:17:58 +0800

    unit(page): test changing of role name

--- pages/role/id.page.spec.ts
+++ pages/role/id.page.spec.ts
@@ -1,5 +1,11 @@
 import { mount, flushPromises } from "@vue/test-utils"
+
+
+import Page from "./id.page.vue"
+import "~/set-ups/database.set_up"
+import RoleFetcher from "$@/fetchers/role"
 import RoleFactory from "~/factories/role"
+import RequestEnvironment from "$/helpers/request_environment"
 import {
 	tag,
 	user,
@@ -7,13 +13,9 @@ import {
 	comment,
 	semester,
 	profanity,
-	auditTrail,
 	department,
 	role
 } from "$/permissions/permission_list"
-import Page from "./id.page.vue"
-import RoleFetcher from "$@/fetchers/role"
-import RequestEnvironment from "$/helpers/request_environment"
 
 describe("UI Page: Read resource by ID", () => {
 	it("Should load resource by ID", async () => {
@@ -71,8 +73,72 @@ describe("UI Page: Read resource by ID", () => {
 
 		await flushPromises()
 
-		const fetchedRoleName = wrapper.find(".role-name h1")
+		const fetchedRoleName = wrapper.find(".role-name input").getRootNodes()
 		const expectedRoleName = response.body.data.name
-		expect(fetchedRoleName.html()).toContain(expectedRoleName)
+		expect((fetchedRoleName[0] as HTMLInputElement).value).toEqual(expectedRoleName)
 	})
+
+	it.skip("Can edit role name", async () => {
+		const sampleResource = await new RoleFactory()
+		.departmentFlags(department.generateMask("view"))
+					.roleFlags(role.generateMask("view"))
+					.semesterFlags(semester.generateMask("view"))
+					.tagFlags(tag.generateMask("view", "create", "update", "archiveAndRestore"))
+					.postFlags(post.generateMask(
+						"view",
+						"create",
+						"update",
+						"archiveAndRestore",
+						"readDepartmentScope",
+						"writeDepartmentScope",
+						"tag"
+					))
+					.commentFlags(comment.generateMask(
+						"view",
+						"create",
+						"update",
+						"archiveAndRestore",
+						"readDepartmentScope",
+						"writeDepartmentScope",
+						"vote"
+					))
+					.profanityFlags(profanity.generateMask("view", "readOverallScope"))
+					.userFlags(user.generateMask(
+						"view",
+						"create",
+						"update",
+						"archiveAndRestore",
+						"readDepartmentScope",
+						"writeDepartmentScope"
+					))
+					.auditTrailFlags(0)
+		.serializedOne()
+
+		fetchMock.mockResponse(JSON.stringify(sampleResource), { status: RequestEnvironment.status.OK })
+
+		const fetcher = new RoleFetcher()
+		const response = await fetcher.read(sampleResource.data.id)
+
+		const wrapper = mount(Page, {
+			global: {
+				provide: {
+					pageContext: {
+						routeParams: {
+							id: 0
+						}
+					}
+				}
+			}
+		})
+
+		await flushPromises()
+
+		const fetchedRoleName = wrapper.find(".role-name input")
+		await fetchedRoleName.setValue("Another Role Name")
+		const newRoleName = (fetchedRoleName.getRootNodes()[0] as HTMLInputElement).value
+		// TODO: test update submission and expect new role name to successfully push in database
+	})
+
+	it.todo("Should be archivable")
+	it.todo("Should be restorable")
 })
