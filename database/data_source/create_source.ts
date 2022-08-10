bbf4c601064b5b111fb2f0051a159f79d59c17b2
Angelo Magtoto <19201.magtoto.angelo.l@gmail.com>
2022-08-10 22:38:57 +0800

    intrn(database): register consultation model

--- database/data_source/create_source.ts
+++ database/data_source/create_source.ts
@@ -5,8 +5,9 @@ import type { SourceType } from "$/types/database"
 import Log from "$!/singletons/log"
 import Role from "%/models/role"
 import User from "%/models/user"
-import Department from "%/models/department"
 import Signature from "%/models/signature"
+import Department from "%/models/department"
+import Consultation from "%/models/consultation"
 import AttachedRole from "%/models/attached_role"
 import StudentDetail from "%/models/student_detail"
 import ProfilePicture from "%/models/profile_picture"
@@ -25,6 +26,7 @@ export default async function(type: SourceType): Promise<Sequelize> {
 			User,
 			Signature,
 			Department,
+			Consultation,
 			AttachedRole,
 			StudentDetail,
 			ProfilePicture,
