95d79d09e091e752b719c4edf322d22c911f0c9c
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-11 00:11:27 +0800

    types(share): add identifier documents for roles

--- shareable/types/documents/role.ts
+++ shareable/types/documents/role.ts
@@ -6,7 +6,9 @@ import type {
 	ResourceDocument,
 	ResourceListDocument,
 	DeserializedResourceDocument,
-	DeserializedResourceListDocument
+	DeserializedResourceListDocument,
+	IdentifierDocument,
+	IdentifierListDocument
 } from "$/types/documents/base"
 
 export interface RoleResourceIdentifier extends ResourceIdentifier {
@@ -59,3 +61,9 @@ export interface DeserializedRoleListDocument extends DeserializedResourceListDo
 	RoleAttributes,
 	DeserializedRoleResource
 > {}
+
+export interface RoleIdentifierDocument
+extends IdentifierDocument<RoleResourceIdentifier> {}
+
+export interface RoleIdentifierListDocument
+extends IdentifierListDocument<RoleResourceIdentifier> {}
