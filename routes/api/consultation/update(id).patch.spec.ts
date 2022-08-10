841f73d2a6968fddf95690249a95a19f4aafe33a
Angelo Magtoto <19201.magtoto.angelo.l@gmail.com>
2022-08-10 21:41:35 +0800

    unit(route): test for consultation update patch

---
+++ routes/api/consultation/update(id).patch.spec.ts
@@ -0,0 +1,43 @@
+import ErrorBag from "$!/errors/error_bag"
+import MockRequester from "~/set-ups/mock_requester"
+import ConsultationFactory from "~/factories/consultation"
+import Controller from "./update(id).patch"
+
+const ID_VALIDATION_INDEX = 0
+const BODY_VALIDATION_INDEX = 1
+
+describe("Controller: PATCH /api/consultation/update/:id", () => {
+	const requester = new MockRequester()
+
+	it("can accept valid info with new details", async () => {
+		const controller = new Controller()
+		const validations = controller.validations
+		const bodyValidation = validations[BODY_VALIDATION_INDEX]
+		const bodyValidationFunction = bodyValidation.intermediate.bind(bodyValidation)
+		const consultation = await new ConsultationFactory().insertOne()
+		const newDepartment = await new ConsultationFactory().makeOne()
+		requester.customizeRequest({
+			body: {
+				data: {
+					type: "consultation",
+					attributes: {
+						attachedRoleID: consultation.attachedRoleID,
+						reason: consultation.reason,
+						status: consultation.status,
+						actionTaken: consultation.actionTaken,
+						scheduledStartDatetime: consultation.scheduledStartDatetime,
+						endDatetime: consultation.scheduledStartDatetime,
+						//TODO Message
+						//TODO Consultation Requesters
+						//TODO Chat Message Activity
+						deletedAt: null
+								}
+				}
+			}
+		})
+
+		await requester.runMiddleware(bodyValidationFunction)
+
+		requester.expectSuccess()
+	})
+})
