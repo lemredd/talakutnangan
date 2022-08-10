16d1ee6ff07ddb06384ad2818eeb9645e60ab8f2
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-11 00:25:05 +0800

    intrn(user): count the users after fetching the roles

--- pages/admin/resource_config/roles.page.vue
+++ pages/admin/resource_config/roles.page.vue
@@ -19,20 +19,20 @@ import type { DeserializedRoleResource } from "$/types/documents/role"
 import type { PageContext } from "#/types"
 import type { DeserializedUserProfile } from "$/types/documents/user"
 
+import RoleFetcher from "$@/fetchers/role"
 import AdminSettingsHeader from "@/tabbed_page_header.vue"
 import Manager from "@/resource_management/manager"
 import RolesManager from "@/resource_management/resource_manager.vue"
 import SearchFilter from "@/resource_management/resource_manager/search_bar.vue"
 import RolesList from "@/resource_management/resource_manager/resource_list.vue"
-import RoleFetcher from "$@/fetchers/role"
-import deserialize from "$/helpers/deserialize"
 
 const pageContext = inject("pageContext") as PageContext
 
 provide("managerKind", new Manager(pageContext.pageProps.userProfile! as DeserializedUserProfile))
-provide("tabs", ["Users", "Roles", "Departments"])
+provide("tabs", ["Users", "Roles", "Roles"])
 
 RoleFetcher.initialize("/api")
+const fetcher = new RoleFetcher()
 
 const roles = ref<DeserializedRoleResource[]>([])
 const filteredList = ref<DeserializedRoleResource[]>([])
@@ -40,23 +40,48 @@ const filteredList = ref<DeserializedRoleResource[]>([])
 function getFilteredList(resource: PossibleResources[]) {
 	filteredList.value = resource as DeserializedRoleResource[]
 }
-
-onMounted(async () => {
-	await new RoleFetcher().list({
+async function fetchRoleInfos(offset: number) {
+	await fetcher.list({
 		filter: {
 			existence: "exists",
 			department: "*"
 		},
 		page: {
 			limit: 10,
-			offset: 0
+			offset,
 		},
 		sort: ["name"]
+	}).then(response => {
+		const deserializedData = response.body.data as DeserializedRoleResource[]
+		const IDsToCount = deserializedData.map(data => data.id)
+
+		if (deserializedData.length === 0) return
+
+		roles.value = [ ...roles.value, ...deserializedData ]
+
+		return countUsersPerRole(IDsToCount)
 	})
-	.then(response => {
-		const { body } = response
-		const deserializedData = deserialize(body)!.data as DeserializedRoleResource[]
-		roles.value = deserializedData
+}
+
+async function countUsersPerRole(IDsToCount: number[]) {
+	await fetcher.countUsers(IDsToCount).then(response => {
+		const deserializedData = response.body.data
+		const originalData = [ ...roles.value ]
+
+		for (const identifierData of deserializedData) {
+			const { id, meta } = identifierData
+
+			const index = originalData.findIndex(data => data.id === id)
+			originalData[index].meta = meta
+		}
+
+		roles.value = originalData
+
+		return fetchRoleInfos(originalData.length)
 	})
+}
+
+onMounted(async () => {
+	await fetchRoleInfos(0)
 })
 </script>
