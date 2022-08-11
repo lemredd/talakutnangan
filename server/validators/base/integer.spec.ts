79fc4b1559c4980c02b9a0233d86127d06cb6beb
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-11 11:47:43 +0800

    intrn(validator): put the cast value if explicitly stated

--- server/validators/base/integer.spec.ts
+++ server/validators/base/integer.spec.ts
@@ -2,18 +2,27 @@ import makeInitialState from "!/validators/make_initial_state"
 import integer from "./integer"
 
 describe("Validator pipe: integer", () => {
-	it("can accept valid input", async () => {
-		const value = Promise.resolve(makeInitialState(2))
-		const constraints = { request: null, source: null, field: "hello" }
+	it("can accept valid input", async() => {
+		const value = "2"
+		const state = Promise.resolve(makeInitialState(value))
+		const constraints = {
+			"request": null,
+			"source": null,
+			"field": "hello"
+		}
 
-		const sanitizeValue = (await integer(value, constraints)).value
+		const sanitizeValue = (await integer(state, constraints)).value
 
-		expect(sanitizeValue).toEqual(2)
+		expect(sanitizeValue).toEqual(value)
 	})
 
-	it("cannot accept invalid input", async () => {
+	it("cannot accept invalid input", () => {
 		const value = Promise.resolve(makeInitialState("hello"))
-		const constraints = { request: null, source: null, field: "hello" }
+		const constraints = {
+			"request": null,
+			"source": null,
+			"field": "hello"
+		}
 
 		const error = integer(value, constraints)
 
--- server/validators/base/integer.ts
+++ server/validators/base/integer.ts
@@ -1,24 +1,33 @@
-import type { ValidationState, ValidationConstraints } from "!/types/validation"
+import type {
+	IntegerConstraints,
+	ValidationState,
+	ValidationConstraints
+} from "!/types/validation"
 
 /**
  * Validator to check if data is a valid integer
  */
 export default async function(
 	currentState: Promise<ValidationState>,
-	constraints: ValidationConstraints
+	constraints: ValidationConstraints & IntegerConstraints
 ): Promise<ValidationState> {
 	const state = await currentState
 
-	if(state.maySkip) return state
+	if (state.maySkip) return state
 
-	state.value = Number(state.value)
+	const castedValue = Number(state.value)
 
-	if (Number.isInteger(state.value)) {
-		return state
-	} else {
-		throw {
-			field: constraints.field,
-			messageMaker: (field: string) => `Field "${field}" must be an integer.`
+	if (Number.isInteger(castedValue)) {
+		if (constraints.integer && constraints.integer.shouldCast) {
+			state.value = castedValue
 		}
+	} else {
+		const error = {
+			"field": constraints.field,
+			"messageMaker": (field: string) => `Field "${field}" must be an integer.`
+		}
+		throw error
 	}
+
+	return state
 }
