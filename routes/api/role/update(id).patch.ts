1f377e8f9dc7b1b4937f5b8abed95e561a5e41fc
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-11 13:20:27 +0800

    feat(role)!: apply resource document maker to accept role information
    It is a breaking change since ID required to be string.

--- routes/api/role/update(id).patch.ts
+++ routes/api/role/update(id).patch.ts
@@ -8,11 +8,11 @@ import NoContentResponseInfo from "!/response_infos/no_content"
 import BoundJSONController from "!/controllers/bound_json_controller"
 
 import { UPDATE } from "$/permissions/role_combinations"
-import { role as permissionGroup } from "$/permissions/permission_list"
 import PermissionBasedPolicy from "!/policies/permission-based"
 import {
 	tag,
 	user,
+	role as permissionGroup,
 	post,
 	comment,
 	semester,
@@ -20,14 +20,13 @@ import {
 	auditTrail
 } from "$/permissions/permission_list"
 
-import object from "!/validators/base/object"
 import string from "!/validators/base/string"
 import integer from "!/validators/base/integer"
-import same from "!/validators/comparison/same"
 import unique from "!/validators/manager/unique"
 import range from "!/validators/comparison/range"
 import regex from "!/validators/comparison/regex"
 import required from "!/validators/base/required"
+import makeResourceDocumentRules from "!/rule_sets/make_resource_document"
 
 export default class extends BoundJSONController {
 	get filePath(): string { return __filename }
@@ -39,103 +38,86 @@ export default class extends BoundJSONController {
 	}
 
 	makeBodyRuleGenerator(request: Request): FieldRules {
-		return {
-			data: {
-				pipes: [ required, object ],
-				constraints: {
-					object: {
-						type: {
-							pipes: [ required, string, same ],
-							constraints: {
-								same: {
-									value: "role"
-								}
-							}
-						},
-						id: {
-							pipes: [ required, integer ],
-							constraints: {}
-						},
-						attributes: {
-							pipes: [ required, object ],
-							constraints: {
-								object: {
-									name: {
-										pipes: [ required, string, regex, unique ],
-										constraints: {
-											regex: { match: /^([A-Z][a-z-_]+ )*[A-Z][a-z-_]+$/ },
-											manager: {
-												className: RoleManager,
-												columnName: "name"
-											},
-											unique: {
-												IDPath: "data.id"
-											}
-										}
-									},
-									semesterFlags: {
-										pipes: [ required, integer, range ],
-										constraints: {
-											range: {
-												minimum: 0, maximum: semester.generateSuperMask()
-											}
-										}
-									},
-									tagFlags: {
-										pipes: [ required, integer, range ],
-										constraints: {
-											range: {
-												minimum: 0, maximum: tag.generateSuperMask()
-											}
-										}
-									},
-									postFlags: {
-										pipes: [ required, integer, range ],
-										constraints: {
-											range: {
-												minimum: 0, maximum: post.generateSuperMask()
-											}
-										}
-									},
-									commentFlags: {
-										pipes: [ required, integer, range ],
-										constraints: {
-											range: {
-												minimum: 0, maximum: comment.generateSuperMask()
-											}
-										}
-									},
-									profanityFlags: {
-										pipes: [ required, integer, range ],
-										constraints: {
-											range: {
-												minimum: 0, maximum: profanity.generateSuperMask()
-											}
-										}
-									},
-									userFlags: {
-										pipes: [ required, integer, range ],
-										constraints: {
-											range: {
-												minimum: 0, maximum: user.generateSuperMask()
-											}
-										}
-									},
-									auditTrailFlags: {
-										pipes: [ required, integer, range ],
-										constraints: {
-											range: {
-												minimum: 0, maximum: auditTrail.generateSuperMask()
-											}
-										}
-									},
-								}
-							}
-						}
+		const attributes: FieldRules = {
+			"name": {
+				"constraints": {
+					"regex": { "match": /^([A-Z][a-z-_]+ )*[A-Z][a-z-_]+$/u },
+					"manager": {
+						"className": RoleManager,
+						"columnName": "name"
+					},
+					"unique": {
+						"IDPath": "data.id"
+					}
+				},
+				"pipes": [ required, string, regex, unique ]
+			},
+			"semesterFlags": {
+				"pipes": [ required, integer, range ],
+				"constraints": {
+					"range": {
+						"minimum": 0,
+						"maximum": semester.generateSuperMask()
+					}
+				}
+			},
+			"tagFlags": {
+				"pipes": [ required, integer, range ],
+				"constraints": {
+					"range": {
+						"minimum": 0,
+						"maximum": tag.generateSuperMask()
+					}
+				}
+			},
+			"postFlags": {
+				"pipes": [ required, integer, range ],
+				"constraints": {
+					"range": {
+						"minimum": 0,
+						"maximum": post.generateSuperMask()
+					}
+				}
+			},
+			"commentFlags": {
+				"pipes": [ required, integer, range ],
+				"constraints": {
+					"range": {
+						"minimum": 0,
+						"maximum": comment.generateSuperMask()
+					}
+				}
+			},
+			"profanityFlags": {
+				"pipes": [ required, integer, range ],
+				"constraints": {
+					"range": {
+						"minimum": 0,
+						"maximum": profanity.generateSuperMask()
+					}
+				}
+			},
+			"userFlags": {
+				"pipes": [ required, integer, range ],
+				"constraints": {
+					"range": {
+						"minimum": 0,
+						"maximum": user.generateSuperMask()
+					}
+				}
+			},
+			"auditTrailFlags": {
+				"pipes": [ required, integer, range ],
+				"constraints": {
+					"range": {
+						"minimum": 0,
+						"maximum": auditTrail.generateSuperMask()
 					}
 				}
 			}
 		}
+
+		return makeResourceDocumentRules("role", attributes)
 	}
 
 	get manager(): BaseManagerClass { return RoleManager }
