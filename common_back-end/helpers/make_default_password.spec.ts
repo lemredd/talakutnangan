ef595e0094a571f75e9cc1008468114480f8d0ba
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-11 15:35:55 +0800

    unit(back-end): ensure ID is string to pass the tests

--- common_back-end/helpers/make_default_password.spec.ts
+++ common_back-end/helpers/make_default_password.spec.ts
@@ -4,35 +4,35 @@ import makeDefaultPassword from "./make_default_password"
 describe("Helpers: Make default password", () => {
 	it("can generate for student", () => {
 		const userProfile: DeserializedUserProfile = {
-			data: {
-				type: "user",
-				id: 1,
-				name: "",
-				email: "",
-				kind: "student",
-				prefersDark: false,
-				roles: {
-					data: []
+			"data": {
+				"type": "user",
+				"id": "1",
+				"name": "",
+				"email": "",
+				"kind": "student",
+				"prefersDark": false,
+				"roles": {
+					"data": []
 				},
-				studentDetail: {
-					data: {
-						id: 1,
-						type: "student_detail",
-						studentNumber: "0000-000"
+				"studentDetail": {
+					"data": {
+						"id": "1",
+						"type": "student_detail",
+						"studentNumber": "0000-000"
 					}
 				},
-				department: {
-					data: {
-						id: 1,
-						type: "department",
-						mayAdmit: true,
-						acronym: "A",
-						fullName: "A"
+				"department": {
+					"data": {
+						"id": "1",
+						"type": "department",
+						"mayAdmit": true,
+						"acronym": "A",
+						"fullName": "A"
 					}
 				}
 			},
-			meta: {
-				hasDefaultPassword: false
+			"meta": {
+				"hasDefaultPassword": false
 			}
 		}
 
@@ -43,31 +43,31 @@ describe("Helpers: Make default password", () => {
 
 	it("can generate for reachable employee", () => {
 		const userProfile: DeserializedUserProfile = {
-			data: {
-				type: "user",
-				id: 1,
-				name: "",
-				email: "hello.world@example.com",
-				kind: "reachable_employee",
-				prefersDark: false,
-				roles: {
-					data: []
+			"data": {
+				"type": "user",
+				"id": "1",
+				"name": "",
+				"email": "hello.world@example.com",
+				"kind": "reachable_employee",
+				"prefersDark": false,
+				"roles": {
+					"data": []
 				},
-				employeeSchedules: {
-					data: []
+				"employeeSchedules": {
+					"data": []
 				},
-				department: {
-					data: {
-						id: 1,
-						type: "department",
-						mayAdmit: true,
-						acronym: "A",
-						fullName: "A"
+				"department": {
+					"data": {
+						"id": "1",
+						"type": "department",
+						"mayAdmit": true,
+						"acronym": "A",
+						"fullName": "A"
 					}
 				}
 			},
-			meta: {
-				hasDefaultPassword: false
+			"meta": {
+				"hasDefaultPassword": false
 			}
 		}
 
@@ -78,28 +78,28 @@ describe("Helpers: Make default password", () => {
 
 	it("can generate for unreachable employee", () => {
 		const userProfile: DeserializedUserProfile = {
-			data: {
-				type: "user",
-				id: 1,
-				name: "",
-				email: "foo.bar@example.com",
-				kind: "unreachable_employee",
-				prefersDark: false,
-				roles: {
-					data: []
+			"data": {
+				"type": "user",
+				"id": "1",
+				"name": "",
+				"email": "foo.bar@example.com",
+				"kind": "unreachable_employee",
+				"prefersDark": false,
+				"roles": {
+					"data": []
 				},
-				department: {
-					data: {
-						id: 1,
-						type: "department",
-						mayAdmit: true,
-						acronym: "A",
-						fullName: "A"
+				"department": {
+					"data": {
+						"id": "1",
+						"type": "department",
+						"mayAdmit": true,
+						"acronym": "A",
+						"fullName": "A"
 					}
 				}
 			},
-			meta: {
-				hasDefaultPassword: false
+			"meta": {
+				"hasDefaultPassword": false
 			}
 		}
 
