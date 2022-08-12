a95cc3258db930d3a19b8b1f3cd6a947aa86e659
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-13 02:15:29 +0800

    intrn(manager): make signature manager to be file-like

--- database/managers/signature.spec.ts
+++ database/managers/signature.spec.ts
@@ -5,35 +5,35 @@ import SignatureFactory from "~/factories/signature"
 import SignatureManager from "./signature"
 
 describe("Database: Signature update operations", () => {
-	it("can create signature if not yet existing", async () => {
+	it("can create signature if not yet existing", async() => {
 		const manager = new SignatureManager()
 		const signature = await new SignatureFactory().makeOne()
 		const user = await new UserFactory().insertOne()
 
-		const resource = await manager.attach(user.id, signature.signature)
+		const resource = await manager.attach(user.id, signature.fileContents)
 
 		expect(resource).toHaveProperty("data.type", "signature")
-		expect(resource).not.toHaveProperty("data.attributes.signature")
+		expect(resource).not.toHaveProperty("data.attributes.fileContents")
 		expect(resource).toHaveProperty("data.links.self")
 	})
 
-	it("can create signature and delete the previous", async () => {
+	it("can create signature and delete the previous", async() => {
 		const manager = new SignatureManager()
 		const newSignature = await new SignatureFactory().makeOne()
 		const user = await new UserFactory().insertOne()
 		const oldSignature = await new SignatureFactory()
-			.user(() => Promise.resolve(user))
-			.insertOne()
+		.user(() => Promise.resolve(user))
+		.insertOne()
 
-		const resource = await manager.attach(user.id, newSignature.signature)
+		const resource = await manager.attach(user.id, newSignature.fileContents) as any
 
 		expect(await Signature.findOne({
-			where: { id: oldSignature.id }
+			"where": { "id": oldSignature.id }
 		})).toBeNull()
 		expect(resource).toHaveProperty("data.type", "signature")
 		expect(resource).toHaveProperty("data.id")
-		expect((resource as any).data.id).not.toEqual(oldSignature.id)
-		expect(resource).not.toHaveProperty("data.attributes.signature")
+		expect(resource.data.id).not.toEqual(oldSignature.id)
+		expect(resource).not.toHaveProperty("data.attributes.fileContents")
 		expect(resource).toHaveProperty("data.links.self")
 	})
 })
