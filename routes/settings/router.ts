79b65e4e9f943d267fe8e35bdc419443af402706
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-08-12 15:16:35 +0800

    intrn(server): give policies to sub-routes

--- routes/settings/router.ts
+++ routes/settings/router.ts
@@ -1,5 +1,7 @@
 import Router from "!/bases/router"
 import GetIndex from "!%/settings/index.get"
+import GetAccount from "!%/settings/account.get"
+import GetProfile from "!%/settings/profile.get"
 
 export default class extends Router {
 
@@ -8,6 +10,8 @@ export default class extends Router {
 
 		this.useControllers([
 			new GetIndex(),
+			new GetAccount(),
+			new GetProfile()
 		])
 	}
 }
