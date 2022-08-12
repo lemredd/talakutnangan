41d70c28500c2094663e18b1738204215ac28d26
Angelo Magtoto <19201.magtoto.angelo.l@gmail.com>
2022-08-12 14:16:08 +0800

    server(route): correct import resource

--- routes/api/consultation/router.ts
+++ routes/api/consultation/router.ts
@@ -1,9 +1,9 @@
 import Router from "!/bases/router"
-import GetList from "!%/api/department/list.get"
-import PostCreate from "!%/api/department/create.post"
-import PatchUpdate from "!%/api/department/update(id).patch"
-import PatchRestore from "!%/api/department/restore.patch"
-import DeleteArchive from "!%/api/department/archive.delete"
+import GetList from "!%/api/consultation/list.get"
+import PostCreate from "!%/api/consultation/create.post"
+import PatchUpdate from "!%/api/consultation/update(id).patch"
+import PatchRestore from "!%/api/consultation/restore.patch"
+import DeleteArchive from "!%/api/consultation/archive.delete"
 
 export default class extends Router {
 	constructor() {
