17f492363da423193cff0f98954c993546248172
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-13 13:43:27 +0800

    refactor(signature): change the parent controller to read

--- routes/api/signature/read(id).get.ts
+++ routes/api/signature/read(id).get.ts
@@ -1,25 +1,24 @@
+import type { BaseManagerClass } from "!/types/independent"
 import type { SignatureDocument } from "$/types/documents/signature"
 import type { AuthenticatedIDRequest, Response } from "!/types/dependent"
 
 import Log from "$!/singletons/log"
 import Policy from "!/bases/policy"
 import UserManager from "%/managers/user"
-import Validation from "!/bases/validation"
 import OkResponseInfo from "!/response_infos/ok"
+import BoundController from "!/controllers/bound"
 import SignatureManager from "%/managers/signature"
 import sniffMediaType from "!/helpers/sniff_media_type"
-import Controller from "!/bases/controller-likes/controller"
 
+import PermissionBasedPolicy from "!/policies/permission-based"
+import { user as permissionGroup } from "$/permissions/permission_list"
 import {
 	READ_OWN,
 	READ_ANYONE_ON_OWN_DEPARTMENT,
 	READ_ANYONE_ON_ALL_DEPARTMENTS
 } from "$/permissions/user_combinations"
-import IDParameterValidation from "!/validations/id_parameter"
-import PermissionBasedPolicy from "!/policies/permission-based"
-import { user as permissionGroup } from "$/permissions/permission_list"
 
-export default class extends Controller {
+export default class extends BoundController {
 	get filePath(): string { return __filename }
 
 	get policy(): Policy {
@@ -30,15 +29,7 @@ export default class extends Controller {
 		])
 	}
 
-	get bodyParser(): null { return null }
-
-	get validations(): Validation[] {
-		return [
-			new IDParameterValidation([
-				[ "id", UserManager ]
-			])
-		]
-	}
+	get manager(): BaseManagerClass { return UserManager }
 
 	async handle(request: AuthenticatedIDRequest, unusedResponse: Response)
 	: Promise<OkResponseInfo> {
