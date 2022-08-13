55b5c9cdccbd05d495b94bedccc3028633d99cd6
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-13 21:09:33 +0800

    intrn(validator): make validator for matching password

---
+++ server/validators/manager/matches_password.ts
@@ -0,0 +1,42 @@
+import type { AuthenticatedRequest } from "!/types/dependent"
+import type { DeserializedUserProfile } from "$/types/documents/user"
+import type { ValidationState, ValidationConstraints } from "!/types/validation"
+
+import UserManager from "%/managers/user"
+import deserialize from "$/helpers/deserialize"
+
+/**
+ * Validator to check if the password matches the password of currently logged in user.
+ */
+export default async function(
+	currentState: Promise<ValidationState>,
+	constraints: ValidationConstraints<AuthenticatedRequest>
+): Promise<ValidationState> {
+	const state = await currentState
+
+	if (state.maySkip) return state
+
+	const userProfile = deserialize(constraints.request.user) as DeserializedUserProfile
+	const { email } = userProfile.data
+	const password = state.value
+
+	const manager = new UserManager(
+		constraints.request.transaction,
+		constraints.request.cache
+	)
+	const foundModel = await manager.findWithCredentials(email, password)
+
+	if (foundModel !== null) {
+		return state
+	}
+
+	const error = {
+		"field": constraints.field,
+		"messageMaker": (
+			unusedField: string,
+			unusedValue: string
+		) => "The password does not match the password of currently logged in user."
+	}
+
+	throw error
+}
