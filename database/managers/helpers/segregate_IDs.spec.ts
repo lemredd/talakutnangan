182b5eb704cb3653068005254a7a1a5ff5f26a0c
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-14 03:04:54 +0800

    intrn(database): make helper function to segregate IDs

---
+++ database/managers/helpers/segregate_IDs.spec.ts
@@ -0,0 +1,35 @@
+import { Op } from "sequelize"
+
+import segregateIDs from "./segragate_IDs"
+
+describe("Database: Condition Builder", () => {
+	it("can determine new IDs", () => {
+		const originalIDs = [ 1, 2 ]
+		const modifiedIDs = [ 1, 2, 3 ]
+
+		const segregatedIDs = segregateIDs(originalIDs, modifiedIDs)
+
+		expect(segregatedIDs.newIDs).toEqual([ 3 ])
+		expect(segregatedIDs.deletedIDs).toEqual([])
+	})
+
+	it("can determine deleted IDs", () => {
+		const originalIDs = [ 1, 2 ]
+		const modifiedIDs = [ 1 ]
+
+		const segregatedIDs = segregateIDs(originalIDs, modifiedIDs)
+
+		expect(segregatedIDs.newIDs).toEqual([])
+		expect(segregatedIDs.deletedIDs).toEqual([ 2 ])
+	})
+
+	it("can determine new and deleted IDs", () => {
+		const originalIDs = [ 1, 2 ]
+		const modifiedIDs = [ 2, 3 ]
+
+		const segregatedIDs = segregateIDs(originalIDs, modifiedIDs)
+
+		expect(segregatedIDs.newIDs).toEqual([ 3 ])
+		expect(segregatedIDs.deletedIDs).toEqual([ 1 ])
+	})
+})
