b54945344485a87bfc2471be262f31da5d360c77
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-15 16:27:17 +0800

    unit(employee schedule): ensure route to list employee schedules works

---
+++ routes/api/employee_schedule/list.get.spec.ts
@@ -0,0 +1,51 @@
+import ErrorBag from "$!/errors/error_bag"
+import UserFactory from "~/factories/user"
+import MockRequester from "~/set-ups/mock_requester"
+
+import Controller from "./list.get"
+
+const QUERY_VALIDATION_INDEX = 0
+
+describe("Controller: GET /api/employee_schedule", () => {
+	const requester = new MockRequester()
+
+	it("can accept valid info", async() => {
+		const controller = new Controller()
+		const { validations } = controller
+		const queryValidation = validations[QUERY_VALIDATION_INDEX]
+		const queryValidationFunction = queryValidation.intermediate.bind(queryValidation)
+		const user = await new UserFactory().beReachableEmployee().insertOne()
+		requester.customizeRequest({
+			"query": {
+				"filter": {
+					"existence": "*",
+					"user": String(user.id)
+				}
+			}
+		})
+
+		await requester.runMiddleware(queryValidationFunction)
+
+		requester.expectSuccess()
+	})
+
+	it("cannot accept invalid info", async() => {
+		const controller = new Controller()
+		const { validations } = controller
+		const queryValidation = validations[QUERY_VALIDATION_INDEX]
+		const queryValidationFunction = queryValidation.intermediate.bind(queryValidation)
+		requester.customizeRequest({
+			"query": {
+				"filter": {
+					"existence": "*"
+				}
+			}
+		})
+
+		await requester.runMiddleware(queryValidationFunction)
+
+		const body = requester.expectFailure(ErrorBag).toJSON()
+		expect(body).toHaveLength(1)
+		expect(body).toHaveProperty("0.source.parameter", "filter.user")
+	})
+})
