d035f050c0e2902b42f07a2c91c6de085c903c90
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-15 16:09:19 +0800

    intrn(manager): integrate pipe to sift employee schedules by user

--- database/managers/employee_schedule.ts
+++ database/managers/employee_schedule.ts
@@ -1,9 +1,11 @@
-import type { ModelCtor } from "%/types/dependent"
+import type { Pipe } from "$/types/database"
 import type { EmployeeScheduleQueryParameters } from "$/types/query"
+import type { ModelCtor, FindAndCountOptions } from "%/types/dependent"
 import type { EmployeeScheduleAttributes } from "$/types/documents/employee_schedule"
 
 import BaseManager from "%/managers/base"
 import EmployeeSchedule from "%/models/employee_schedule"
+import siftByUser from "%/queries/employee_schedule/sift_by_user"
 import EmployeeScheduleTransformer from "%/transformers/employee_schedule"
 
 export default class extends BaseManager<
@@ -14,4 +16,14 @@ export default class extends BaseManager<
 	get model(): ModelCtor<EmployeeSchedule> { return EmployeeSchedule }
 
 	get transformer(): EmployeeScheduleTransformer { return new EmployeeScheduleTransformer() }
+
+	get listPipeline(): Pipe<
+		FindAndCountOptions<EmployeeSchedule>,
+		EmployeeScheduleQueryParameters<number>
+	>[] {
+		return [
+			siftByUser,
+			...super.listPipeline
+		]
+	}
 }
