0872cee5333a70b22342be9c766cd334843dea20
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-15 13:44:13 +0800

    unit(employee schedule): ensure validation works to create

---
+++ routes/api/employee_schedule/create.post.spec.ts
@@ -0,0 +1,85 @@
+import ErrorBag from "$!/errors/error_bag"
+import UserFactory from "~/factories/user"
+import MockRequester from "~/set-ups/mock_requester"
+import EmployeeScheduleFactory from "~/factories/employee_schedule"
+import Controller from "./create.post"
+
+const BODY_VALIDATION_INDEX = 1
+
+describe("Controller: POST /api/employee_schedule", () => {
+	const requester = new MockRequester()
+
+	it("can accept valid info", async() => {
+		const controller = new Controller()
+		const { validations } = controller
+		const bodyValidation = validations[BODY_VALIDATION_INDEX]
+		const bodyValidationFunction = bodyValidation.intermediate.bind(bodyValidation)
+		const user = await new UserFactory().beReachableEmployee().insertOne()
+		const employeeeSchedule = await new EmployeeScheduleFactory()
+		.user(() => Promise.resolve(user))
+		.makeOne()
+		requester.customizeRequest({
+			"body": {
+				"data": {
+					"type": "employee_schedule",
+					"attributes": {
+						"dayName": employeeeSchedule.dayName,
+						"scheduleEnd": employeeeSchedule.scheduleEnd,
+						"scheduleStart": employeeeSchedule.scheduleStart
+					},
+					"relationships": {
+						"user": {
+							"data": {
+								"type": "user",
+								"id": String(user.id)
+							}
+						}
+					}
+				}
+			}
+		})
+
+		await requester.runMiddleware(bodyValidationFunction)
+
+		// requester.expectSuccess()
+		const body = requester.expectFailure(ErrorBag).toJSON()
+		console.log(body, "\n\n\n\n")
+	})
+
+	it("cannot accept invalid name", async() => {
+		const controller = new Controller()
+		const { validations } = controller
+		const bodyValidation = validations[BODY_VALIDATION_INDEX]
+		const bodyValidationFunction = bodyValidation.intermediate.bind(bodyValidation)
+		const user = await new UserFactory().beReachableEmployee().insertOne()
+		const employeeeSchedule = await new EmployeeScheduleFactory()
+		.user(() => Promise.resolve(user))
+		.makeOne()
+		requester.customizeRequest({
+			"body": {
+				"data": {
+					"type": "employee_schedule",
+					"attributes": {
+						"dayName": "tue",
+						"scheduleEnd": employeeeSchedule.scheduleEnd,
+						"scheduleStart": employeeeSchedule.scheduleStart
+					},
+					"relationships": {
+						"user": {
+							"data": {
+								"type": "user",
+								"id": String(user.id)
+							}
+						}
+					}
+				}
+			}
+		})
+
+		await requester.runMiddleware(bodyValidationFunction)
+
+		const body = requester.expectFailure(ErrorBag).toJSON()
+		expect(body).toHaveLength(1)
+		expect(body).toHaveProperty("0.source.pointer", "data.attributes.dayName")
+	})
+})
