833eba03d4d29de699c8d2d6366aa5630d60b990
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-16 21:55:40 +0800

    intg(audit trail): anticipate the recorded log in

--- t/integration/api/audit_trail/list.get.spec.ts
+++ t/integration/api/audit_trail/list.get.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable id-length */
 import { JSON_API_MEDIA_TYPE } from "$/types/server"
 
 import App from "~/set-ups/app"
@@ -11,65 +12,83 @@ import { auditTrail as permissionGroup } from "$/permissions/permission_list"
 import Route from "!%/api/audit_trail/list.get"
 
 describe("GET /api/audit_trail", () => {
-	beforeAll(async () => {
+	beforeAll(async() => {
 		await App.create(new Route())
 	})
 
-	it("can be accessed by permitted user and get audit trails", async () => {
+	it("can be accessed by permitted user and get audit trails", async() => {
 		const anyRole = await new RoleFactory()
-			.auditTrailFlags(permissionGroup.generateMask(...READ))
-			.insertOne()
-		const auditTrails = (await (new AuditTrailFactory()).insertMany(3)).sort((a, b) => {
-			return a.id === b.id ? 0 : a.id < b.id ? -1 : 1
-		})
-		const { user: admin, cookie } = await App.makeAuthenticatedCookie(anyRole)
+		.auditTrailFlags(permissionGroup.generateMask(...READ))
+		.insertOne()
+		const auditTrails = (await new AuditTrailFactory().insertMany(3)).sort(
+			(a, b) => {
+				// eslint-disable-next-line no-nested-ternary
+				const comparison = a.id === b.id ? 0 : a.id < b.id ? -1 : 1
+				return comparison
+			}
+		)
+		const { cookie } = await App.makeAuthenticatedCookie(anyRole)
 
 		const response = await App.request
-			.get("/api/audit_trail")
-			.query({ sort: "id" })
-			.set("Cookie", cookie)
-			.type(JSON_API_MEDIA_TYPE)
-			.accept(JSON_API_MEDIA_TYPE)
+		.get("/api/audit_trail")
+		.query({ "sort": "id" })
+		.set("Cookie", cookie)
+		.type(JSON_API_MEDIA_TYPE)
+		.accept(JSON_API_MEDIA_TYPE)
 
 		expect(response.statusCode).toBe(RequestEnvironment.status.OK)
 		expect(response.body).toHaveProperty("data.0.type", "audit_trail")
 		expect(response.body).toHaveProperty(
 			"data.0.attributes.actionName",
-			auditTrails[0].actionName)
+			auditTrails[0].actionName
+		)
 		expect(response.body).toHaveProperty(
 			"data.1.attributes.actionName",
-			auditTrails[1].actionName)
+			auditTrails[1].actionName
+		)
 		expect(response.body).toHaveProperty(
 			"data.2.attributes.actionName",
-			auditTrails[2].actionName)
+			auditTrails[2].actionName
+		)
+		expect(response.body).toHaveProperty("data.3.attributes.actionName", "user.log_in")
 	})
 
-	it("can be accessed by permitted user but get audit_trails in descending order", async () => {
+	it("can be accessed by permitted user but get audit_trails in descending order", async() => {
 		const anyRole = await new RoleFactory()
-			.auditTrailFlags(permissionGroup.generateMask(...READ))
-			.insertOne()
-		const auditTrails = (await (new AuditTrailFactory()).insertMany(3)).sort((a, b) => {
-			return a.actionName.localeCompare(b.actionName)
-		})
-		const { user: admin, cookie } = await App.makeAuthenticatedCookie(anyRole)
+		.auditTrailFlags(permissionGroup.generateMask(...READ))
+		.insertOne()
+		const auditTrails = await new AuditTrailFactory().insertMany(3)
+		const { cookie } = await App.makeAuthenticatedCookie(anyRole)
 
 		const response = await App.request
-			.get("/api/audit_trail")
-			.query({ sort: "-actionName" })
-			.set("Cookie", cookie)
-			.type(JSON_API_MEDIA_TYPE)
-			.accept(JSON_API_MEDIA_TYPE)
+		.get("/api/audit_trail")
+		.query({ "sort": "-actionName" })
+		.set("Cookie", cookie)
+		.type(JSON_API_MEDIA_TYPE)
+		.accept(JSON_API_MEDIA_TYPE)
 
+		auditTrails.push(await new AuditTrailFactory().actionName(() => "user.log_in").makeOne())
+
+		const sortedAuditTrails = auditTrails.sort(
+			(a, b) => a.actionName.localeCompare(b.actionName)
+		)
 		expect(response.statusCode).toBe(RequestEnvironment.status.OK)
 		expect(response.body).toHaveProperty("data.0.type", "audit_trail")
 		expect(response.body).toHaveProperty(
 			"data.0.attributes.actionName",
-			auditTrails[2].actionName)
+			sortedAuditTrails[3].actionName
+		)
 		expect(response.body).toHaveProperty(
 			"data.1.attributes.actionName",
-			auditTrails[1].actionName)
+			sortedAuditTrails[2].actionName
+		)
 		expect(response.body).toHaveProperty(
 			"data.2.attributes.actionName",
-			auditTrails[0].actionName)
+			sortedAuditTrails[1].actionName
+		)
+		expect(response.body).toHaveProperty(
+			"data.3.attributes.actionName",
+			sortedAuditTrails[0].actionName
+		)
 	})
 })
