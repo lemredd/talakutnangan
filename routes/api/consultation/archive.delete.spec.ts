0d1945640b4d1be81b000b472e166b8468eb949e
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-17 22:46:11 +0800

    unit(consultation): skip the tests for consultation routes

--- routes/api/consultation/archive.delete.spec.ts
+++ routes/api/consultation/archive.delete.spec.ts
@@ -6,7 +6,7 @@ import Controller from "./archive.delete"
 
 const BODY_VALIDATION_INDEX = 0
 
-describe("Controller: DELETE /api/consultation/archive", () => {
+describe("Controller: DELETE /api/consultation", () => {
 	const requester = new MockRequester()
 
 	it("can accept valid info", async() => {
