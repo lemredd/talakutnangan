0871dd75127c8459a9f6bfbca01a9b776b830bbe
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-17 21:17:52 +0800

    chore(manager): apply lint rules

--- database/managers/consultation.ts
+++ database/managers/consultation.ts
@@ -1,21 +1,17 @@
 import type { ModelCtor } from "%/types/dependent"
-import type { Serializable } from "$/types/general"
 import type { CommonQueryParameters } from "$/types/query"
-import type {
-	ConsultationAttributes,
-	ConsultationResourceIdentifier
-} from "$/types/documents/consultation"
+import type { ConsultationAttributes } from "$/types/documents/consultation"
 
-import User from "%/models/user"
 import BaseManager from "%/managers/base"
-import trimRight from "$/helpers/trim_right"
 import Consultation from "%/models/consultation"
-import Condition from "%/managers/helpers/condition"
 import ConsultationTransformer from "%/transformers/consultation"
 
-export default class  extends BaseManager<Consultation, ConsultationAttributes, CommonQueryParameters> {
+export default class extends BaseManager<
+	Consultation,
+	ConsultationAttributes,
+	CommonQueryParameters
+> {
 	get model(): ModelCtor<Consultation> { return Consultation }
 
 	get transformer(): ConsultationTransformer { return new ConsultationTransformer() }
-
 }
