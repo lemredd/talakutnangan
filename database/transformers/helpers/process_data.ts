f71055dbd42e96e6a0b0bb0428f461ae5dd13744
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-17 14:18:55 +0800

    intrn(database): make function to be procedural style

--- database/transformers/helpers/process_data.ts
+++ database/transformers/helpers/process_data.ts
@@ -3,10 +3,10 @@ type PossibleCompundData<T> = UnitData<T>|UnitData<T>[]
 
 export default function<T>(
 	data: PossibleCompundData<T>,
-	processor: (unitData: UnitData<T>) => UnitData<T>
-): PossibleCompundData<T> {
+	processor: (unitData: UnitData<T>) => void
+): void {
 	if (data === null) return processor(null)
-	else if (Array.isArray(data)) return data.map(processor)
+	else if (Array.isArray(data)) return data.forEach(processor)
 
 	return processor(data)
 }
