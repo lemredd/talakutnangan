0907b446a14793cef5293d6eaca4336d9dc5b2e1
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-17 22:29:44 +0800

    intrn(model): allow consultant info to be optional

--- database/models/consultation.ts
+++ database/models/consultation.ts
@@ -60,14 +60,14 @@ export default class Consultation extends Model {
 		attachedRoleID!: number
 
 	@BelongsTo(() => AttachedRole)
-		consultantInfo!: AttachedRole
+		consultantInfo?: AttachedRole
 
 	@BelongsToMany(() => User, () => Consulter)
 		consulters!: User[]
 
-	get consultant(): User { return this.consultantInfo.user }
+	get consultant(): User|null { return this.consultantInfo?.user ?? null }
 
-	get consultantRole(): Role { return this.consultantInfo.role }
+	get consultantRole(): Role|null { return this.consultantInfo?.role ?? null }
 
 	/*
 	 * @HasMany(() => Message)
