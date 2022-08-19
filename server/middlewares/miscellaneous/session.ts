2784f18a831b3704bcdff19199c4760bc6436e89
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-19 20:27:39 +0800

    intrn(middleware): use the database as a storage for session details

--- server/middlewares/miscellaneous/session.ts
+++ server/middlewares/miscellaneous/session.ts
@@ -1,21 +1,25 @@
-import createSessionMiddleware from "express-session"
+import createSessionMiddleware, { Store } from "express-session"
+import makeSequelizeStore from "connect-session-sequelize"
 import type { Request, Response, NextFunction } from "!/types/dependent"
 
 import Middleware from "!/bases/middleware"
+import Database from "%/data_source/database"
 
 export default class Session extends Middleware {
 	private static session = createSessionMiddleware({
-		name: process.env.SESSION_NAME || "talakutnangan_session",
-		secret: process.env.SESSION_SECRET || "12345678",
-		resave: false,
-		saveUninitialized: false,
-		cookie: {
-			 maxAge: process.env.SESSION_DURATION as unknown as number || 15 * 60 * 1000
-		}
+		"name": process.env.SESSION_NAME || "talakutnangan_session",
+		"secret": process.env.SESSION_SECRET || "12345678",
+		"resave": false,
+		"saveUninitialized": false,
+		"cookie": {
+			"maxAge": process.env.SESSION_DURATION as unknown as number || 15 * 60 * 1000
+		},
+		"store": new (makeSequelizeStore(Store))({ "db": Database.dataSource })
 	})
 
-	async intermediate(request: Request, response: Response, next: NextFunction): Promise<void> {
+	intermediate(request: Request, response: Response, next: NextFunction): Promise<void> {
 		// @ts-ignore
 		Session.session(request, response, next)
+		return Promise.resolve()
 	}
 }
