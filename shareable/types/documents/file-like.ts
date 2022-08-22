514cd8be577695ce3c378d49a97fecc0ec712b53
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-22 16:38:15 +0800

    types(share): make type to convert file-like format to base format

--- shareable/types/documents/file-like.ts
+++ shareable/types/documents/file-like.ts
@@ -2,7 +2,9 @@ import type { Format as BaseFormat, Attributes } from "$/types/documents/base"
 
 export type Format = BaseFormat|"raw"
 
+export type FormatBaser<T extends Format = "serialized"> = T extends "raw" ? "serialized": T
+
 export interface FileLikeAttributes<T extends Format = "serialized">
-extends Attributes<T extends "raw" ? "serialized": T> {
+extends Attributes<FormatBaser<T>> {
 	fileContents: T extends "raw" ? Buffer : string
 }
