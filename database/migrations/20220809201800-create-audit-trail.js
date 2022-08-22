4424ea5d2f0fce8a0ce4011ac49fe4415c114a94
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-23 00:34:50 +0800

    intrn(model): make model for chat message

--- database/migrations/20220809201800-create-audit-trail.js
+++ database/migrations/20220809201800-create-audit-trail.js
@@ -1,61 +1,60 @@
-"use strict";
 module.exports = {
 	async up(queryInterface, Sequelize) {
 		await queryInterface.sequelize.transaction(async transaction => {
 			try {
 				await queryInterface.createTable("AuditTrails", {
-					id: {
-						allowNull: false,
-						autoIncrement: true,
-						primaryKey: true,
-						type: Sequelize.BIGINT
+					"id": {
+						"allowNull": false,
+						"autoIncrement": true,
+						"primaryKey": true,
+						"type": Sequelize.BIGINT
 					},
-					userID: {
-						allowNull: true,
-						defaultValue: null,
-						type: Sequelize.BIGINT,
-						references: {
-							model: "Users",
-							key: "id"
+					"userID": {
+						"allowNull": true,
+						"defaultValue": null,
+						"type": Sequelize.BIGINT,
+						"references": {
+							"model": "Users",
+							"key": "id"
 						},
-						onDelete: "cascade",
-						onUpdate: "cascade"
+						"onDelete": "cascade",
+						"onUpdate": "cascade"
 					},
-					actionName: {
-						type: Sequelize.STRING,
-						allowNull: false
+					"actionName": {
+						"type": Sequelize.STRING,
+						"allowNull": false
 					},
-					extra: {
-						type: Sequelize.JSON,
-						allowNull: false
+					"extra": {
+						"type": Sequelize.JSON,
+						"allowNull": false
 					},
-					createdAt: {
-						allowNull: false,
-						type: Sequelize.DATE
+					"createdAt": {
+						"allowNull": false,
+						"type": Sequelize.DATE
 					},
-					updatedAt: {
-						allowNull: false,
-						type: Sequelize.DATE
+					"updatedAt": {
+						"allowNull": false,
+						"type": Sequelize.DATE
 					},
-					deletedAt: {
-						allowNull: true,
-						type: Sequelize.DATE
+					"deletedAt": {
+						"allowNull": true,
+						"type": Sequelize.DATE
 					}
-				});
+				})
 			} catch (err) {
-				await transaction.rollback();
-				throw err;
+				await transaction.rollback()
+				throw err
 			}
-		});
+		})
 	},
-	async down(queryInterface, Sequelize) {
+	async down(queryInterface, unusedSequelize) {
 		await queryInterface.sequelize.transaction(async transaction => {
 			try {
-				await queryInterface.dropTable("AuditTrails", { transaction });
+				await queryInterface.dropTable("AuditTrails", { transaction })
 			} catch (err) {
-				await transaction.rollback();
-				throw err;
+				await transaction.rollback()
+				throw err
 			}
-		});
+		})
 	}
-};
+}
---
+++ database/models/chat_message.ts
@@ -0,0 +1,44 @@
+import {
+	Table,
+	Model,
+	Column,
+	DataType,
+	BelongsTo,
+	ForeignKey
+} from "sequelize-typescript"
+
+import type { GeneralObject } from "$/types/general"
+import User from "%/models/user"
+import Consultation from "%/models/consultation"
+
+@Table({
+	"paranoid": true,
+	"timestamps": true
+})
+export default class ChatMessage extends Model {
+	@Column({
+		"allowNull": false,
+		"type": DataType.JSON
+	})
+		message!: GeneralObject
+
+	@ForeignKey(() => User)
+	@Column({
+		"allowNull": false,
+		"type": DataType.BIGINT
+	})
+		userID!: number
+
+	@BelongsTo(() => User)
+		user!: User
+
+	@ForeignKey(() => Consultation)
+	@Column({
+		"allowNull": false,
+		"type": DataType.BIGINT
+	})
+		consultationID!: number
+
+	@BelongsTo(() => Consultation)
+		consultation!: Consultation
+}
