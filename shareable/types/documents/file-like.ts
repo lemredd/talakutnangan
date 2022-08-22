c8994e8190438efdfb00ba0665d4721d61022ec1
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-22 16:32:46 +0800

    intrn(transformer)!: embed the URL directly to attributes

--- shareable/types/documents/file-like.ts
+++ shareable/types/documents/file-like.ts
@@ -1,14 +1,8 @@
 import type { Format as BaseFormat, Attributes } from "$/types/documents/base"
 
-type Format = BaseFormat|"raw"
+export type Format = BaseFormat|"raw"
 
 export interface FileLikeAttributes<T extends Format = "serialized">
 extends Attributes<T extends "raw" ? "serialized": T> {
-	fileContents: T extends "raw" ? Buffer : undefined
-}
-
-export interface FileLikeResourceLinks<T extends Format = "serialized"> {
-	links: T extends "raw" ? undefined : {
-		self: string
-	}
+	fileContents: T extends "raw" ? Buffer : string
 }
