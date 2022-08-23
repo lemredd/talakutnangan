cd4208d4719c52e2ba1641e9f294bb71ef27d840
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-23 11:33:37 +0800

    intrn(user): extract the roles to be used as options

--- pages/user/import.page.vue
+++ pages/user/import.page.vue
@@ -1,21 +1,34 @@
 <template>
 	<form @submit.prevent="importData">
-		<input type="file" accept="text/csv" name="importedCSV"/>
+		<input
+			type="file"
+			accept="text/csv"
+			name="importedCSV"/>
 		<input type="submit" value="Import"/>
 	</form>
 </template>
 
 <script setup lang="ts">
+import { inject, ref } from "vue"
+
+import type { PageContext } from "#/types"
+import type { DeserializedRoleListDocument } from "$/types/documents/role"
+
+const pageContext = inject("pageContext") as PageContext
+const { pageProps } = pageContext
+const { "roles": rawRoles } = pageProps
+const roles = ref<DeserializedRoleListDocument>(rawRoles as DeserializedRoleListDocument)
+
 function importData(event: Event) {
 	const form = event.target as HTMLFormElement
 	const formData = new FormData(form)
 
 	fetch("/api/user/import", {
-		method: "POST",
-		headers: {
+		"method": "POST",
+		"headers": {
 			"Content-Type": "multipart/form-data"
 		},
-		body: formData
+		"body": formData
 	}).then(response => {
 		console.log(response)
 	})
