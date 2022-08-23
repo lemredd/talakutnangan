5a03236f54b4802df6815c276037aa2ed4a1de9b
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-23 11:13:41 +0800

    intrn(user): put the roles in page props

---
+++ routes/enhancer/user/import.get.ts
@@ -0,0 +1,43 @@
+import type { Request } from "!/types/dependent"
+import type { Serializable } from "$/types/general"
+
+import Policy from "!/bases/policy"
+import Validation from "!/bases/validation"
+import PageMiddleware from "!/bases/controller-likes/page_middleware"
+
+import PermissionBasedPolicy from "!/policies/permission-based"
+import { IMPORT_USERS } from "$/permissions/user_combinations"
+import { user as permissionGroup } from "$/permissions/permission_list"
+
+import RoleManager from "%/managers/role"
+
+export default class extends PageMiddleware {
+	get filePath(): string { return __filename }
+
+	get policy(): Policy {
+		return new PermissionBasedPolicy(permissionGroup, [ IMPORT_USERS ])
+	}
+
+	get bodyParser(): null { return null }
+
+	get validations(): Validation[] { return [] }
+
+	async getPageProps(request: Request): Promise<Serializable> {
+		const manager = new RoleManager(request.transaction, request.cache)
+		const pageProps = {
+			"roles": await manager.list({
+				"filter": {
+					"department": "*",
+					"existence": "exists"
+				},
+				"page": {
+					"limit": 10,
+					"offset": 0
+				},
+				"sort": [ "name" ]
+			})
+		}
+
+		return pageProps
+	}
+}
--- routes/enhancer/user/router.ts
+++ routes/enhancer/user/router.ts
@@ -1,4 +1,5 @@
 import Router from "!/bases/router"
+import GetImport from "!%/enhancer/user/import.get"
 import GetVerify from "!%/enhancer/user/verify(data).get"
 import GetStudentsList from "!%/enhancer/user/students_list.get"
 import GetEmployeeList from "!%/enhancer/user/employees_list.get"
@@ -9,6 +10,7 @@ export default class extends Router {
 		super()
 
 		this.useControllers([
+			new GetImport(),
 			new GetVerify(),
 			new GetStudentsList(),
 			new GetEmployeeList(),
