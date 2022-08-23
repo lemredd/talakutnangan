462b64550b12461ffb2e7bb741d961fca4d61c01
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-23 11:47:18 +0800

    feat(user): show to the user the available roles

--- pages/user/import.page.vue
+++ pages/user/import.page.vue
@@ -1,5 +1,9 @@
 <template>
 	<form @submit.prevent="importData">
+		<SelectableOptionsField
+			v-model="chosenRole"
+			:options="roleNames"
+			placeholder="Please select role to attach"/>
 		<input
 			type="file"
 			accept="text/csv"
@@ -9,15 +13,19 @@
 </template>
 
 <script setup lang="ts">
-import { inject, ref } from "vue"
+import { inject, ref, computed } from "vue"
 
 import type { PageContext } from "#/types"
 import type { DeserializedRoleListDocument } from "$/types/documents/role"
+
+import SelectableOptionsField from "@/fields/selectable_options.vue"
 
 const pageContext = inject("pageContext") as PageContext
 const { pageProps } = pageContext
 const { "roles": rawRoles } = pageProps
 const roles = ref<DeserializedRoleListDocument>(rawRoles as DeserializedRoleListDocument)
+const roleNames = computed(() => roles.value.data.map(data => data.name))
+const chosenRole = ref(roleNames.value[0])
 
 function importData(event: Event) {
 	const form = event.target as HTMLFormElement
