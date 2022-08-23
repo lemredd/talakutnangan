8dfe239c9f591edc95a7c9cbefadac53e9971e58
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-23 15:20:30 +0800

    feat(user): make options to select the kind of users to import

--- pages/user/import.page.vue
+++ pages/user/import.page.vue
@@ -4,11 +4,19 @@
 			v-model="chosenRoleIDs"
 			label="Add the roles to attach"
 			:options="roleNames"/>
-		<input
-			type="file"
-			accept="text/csv"
-			name="importedCSV"/>
-		<input type="submit" value="Import"/>
+		<SelectableOptionsField
+			v-model="chosenKind"
+			label="What kind of users to import?"
+			:options="kindNames"/>
+		<div>
+			<input
+				type="file"
+				accept="text/csv"
+				name="importedCSV"/>
+		</div>
+		<div>
+			<input type="submit" value="Import"/>
+		</div>
 	</form>
 </template>
 
@@ -18,7 +26,10 @@ import { inject, ref, computed } from "vue"
 import type { PageContext } from "#/types"
 import type { OptionInfo } from "$@/types/component"
 import type { DeserializedRoleListDocument } from "$/types/documents/role"
+import { UserKindValues } from "$/types/database"
 
+import convertToSentenceCase from "$/helpers/convert_to_sentence_case"
+import SelectableOptionsField from "@/fields/selectable_options.vue"
 import MultiSelectableOptionsField from "@/fields/multi-selectable_options.vue"
 
 const pageContext = inject("pageContext") as PageContext
@@ -29,7 +40,13 @@ const roleNames = computed<OptionInfo[]>(() => roles.value.data.map(data => ({
 	"label": data.name,
 	"value": data.id
 })))
-const chosenRoleIDs = ref([])
+const chosenRoleIDs = ref<string[]>([])
+
+const kindNames = UserKindValues.map(kind => ({
+	"label": convertToSentenceCase(kind),
+	"value": kind
+}))
+const chosenKind = ref<string>(kindNames[0].value)
 
 function importData(event: Event) {
 	const form = event.target as HTMLFormElement
