a5e0cb90e5557106fd5fabfb46dbaabaeb79d4fe
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-23 12:25:17 +0800

    intrn(component): make component to show selected options

---
+++ components/fields/multi-selectable_options.vue
@@ -0,0 +1,32 @@
+<template>
+	<div>
+		<h3>{{ label }}</h3>
+		<ul class="selected-options">
+			<li v-for="option in selectedOptions" :key="option.value">
+				{{
+					option.label ?? option.value
+				}}
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue"
+import type { OptionInfo } from "$@/types/component"
+
+const {
+	options,
+	modelValue,
+	label
+} = defineProps<{
+	options: readonly OptionInfo[]
+	modelValue: string[]
+	label?: string
+}>()
+
+const selectedOptions = computed<OptionInfo[]>(() => {
+	const chosenOptions = options.filter(option => modelValue.includes(option.value)) as OptionInfo[]
+	return chosenOptions
+})
+</script>
