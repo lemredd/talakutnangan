3e40dfbd78b2bdea9aa4cfdec7bb49282af455e5
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-24 19:48:04 +0800

    intrn(back-end): make developer error

--- common_back-end/errors/parser.ts
+++ common_back-end/errors/parser.ts
@@ -5,10 +5,10 @@ import RequestEnvironment from "$/helpers/request_environment"
  * Used when there is a problem on parsing the data.
  */
 export default class ParserError extends BaseError {
-	static CODE: string = "5"
+	static CODE = "5"
 	static STATUS: number = RequestEnvironment.status.BAD_REQUEST
 
-	constructor(message: string = "Data could not be parsed properly") {
+	constructor(message = "Data could not be parsed properly") {
 		super(
 			ParserError.CODE,
 			ParserError.STATUS,
