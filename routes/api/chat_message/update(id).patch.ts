81f0d237496ddde52ba78f8792c378888e1e5929
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-24 20:38:47 +0800

    feat(chat message): simplify route to update message

--- routes/api/chat_message/update(id).patch.ts
+++ routes/api/chat_message/update(id).patch.ts
@@ -1,10 +1,8 @@
+import type { FieldRules } from "!/types/validation"
 import type { Request, Response } from "!/types/dependent"
-import type { FieldRules, Rules } from "!/types/validation"
 import type { BaseManagerClass } from "!/types/independent"
 
-import UserManager from "%/managers/user"
 import ChatMessageManager from "%/managers/chat_message"
-import ConsultationManager from "%/managers/consultation"
 import NoContentResponseInfo from "!/response_infos/no_content"
 import DoubleBoundJSONController from "!/controllers/double_bound_json"
 
@@ -12,10 +10,7 @@ import Policy from "!/bases/policy"
 import CommonMiddlewareList from "!/middlewares/common_middleware_list"
 
 import object from "!/validators/base/object"
-import exists from "!/validators/manager/exists"
-import required from "!/validators/base/required"
 import makeResourceDocumentRules from "!/rule_sets/make_resource_document"
-import makeResourceIdentifierRules from "!/rule_sets/make_resource_identifier"
 
 export default class extends DoubleBoundJSONController {
 	get filePath(): string { return __filename }
@@ -34,47 +29,7 @@ export default class extends DoubleBoundJSONController {
 			}
 		}
 
-		const relationships: Rules = {
-			"constraints": {
-				"object": {
-					"consultation": {
-						"constraints": {
-							"object": {
-								"data": {
-									"constraints": {
-										"object": makeResourceIdentifierRules(
-											"consultation",
-											exists,
-											ConsultationManager
-										)
-									},
-									"pipes": [ required, object ]
-								}
-							}
-						},
-						"pipes": [ required, object ]
-					},
-					"user": {
-						"constraints": {
-							"object": {
-								"data": {
-									"constraints": {
-										"object": makeResourceIdentifierRules("user", exists, UserManager)
-									},
-									"pipes": [ required, object ]
-								}
-							}
-						},
-						"pipes": [ required, object ]
-					}
-				}
-			},
-			"pipes": [ required, object ]
-		}
-
-		return makeResourceDocumentRules("chat_message", attributes, {
-			"extraDataQueries": { relationships }
-		})
+		return makeResourceDocumentRules("chat_message", attributes)
 	}
 
 	get manager(): BaseManagerClass { return ChatMessageManager }
