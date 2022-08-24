c600f69d3071f1e354aa56ef62f532686f31b791
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-25 01:13:42 +0800

    fix(middleware): restore some mechanism to throw parser error properly

--- server/middlewares/body_parser/csv.ts
+++ server/middlewares/body_parser/csv.ts
@@ -4,7 +4,8 @@ import type { Request } from "!/types/dependent"
 import type { GeneralObject } from "$/types/general"
 
 import Log from "$!/singletons/log"
-import setDeepPath from "$!/helpers/set_deep_path"
+import BaseError from "$!/errors/base"
+import ParserError from "$!/errors/parser"
 import RequestFilter from "!/bases/request_filter"
 import accessDeepPath from "$!/helpers/access_deep_path"
 import convertToCamelCase from "$/helpers/convert_to_camel_case"
@@ -26,65 +27,75 @@ export default class CSVParser extends RequestFilter {
 	async filterRequest(request: Request): Promise<void> {
 		const promisedFields: Promise<{ field: string, value: any }>[] = []
 
-		for (const field of this.rawFields) {
-			const parser = parse({
-				"bom": true,
-				"columns": headers => headers.map((header: string) => convertToCamelCase(header))
-			})
+		try {
+			for (const field of this.rawFields) {
+				const parser = parse({
+					"bom": true,
+					"columns": headers => headers.map((header: string) => convertToCamelCase(header))
+				})
 
-			const buffer: Buffer = accessDeepPath(request.body, `${field}.buffer`)
+				const buffer: Buffer = accessDeepPath(request.body, `${field}.buffer`)
 
-			const promise = new Promise<GeneralObject<string>[]>((resolve, reject) => {
-				const rows: { [key:string]: string }[] = []
-				const reader = () => {
-					Log.trace("middleware", `reading the rows in the CSV file in "${field}" field`)
-					let row = null
-					while ((row = parser.read()) !== null) {
-						rows.push(row)
+				const promise = new Promise<GeneralObject<string>[]>((resolve, reject) => {
+					const rows: { [key:string]: string }[] = []
+					const reader = () => {
+						Log.trace("middleware", `reading the rows in the CSV file in "${field}" field`)
+						let row = null
+						while ((row = parser.read()) !== null) {
+							rows.push(row)
+						}
 					}
-				}
-				const thrower = (error: Error) => {
-					Log.errorMessage(
-						"middleware",
-						`encountered error in processing CSV file  in "${field}" field`
-					)
-					Log.error("middleware", error)
+					const thrower = (error: Error) => {
+						Log.errorMessage(
+							"middleware",
+							`encountered error in processing CSV file  in "${field}" field`
+						)
+						Log.error("middleware", error)
 
-					// eslint-disable-next-line no-use-before-define
-					unlisten()
-					reject(error)
-				}
-				const closer = () => {
-					Log.trace(
-						"middleware",
-						`resolving the read rows in the CSV file io "${field}" field`
-					)
-					// eslint-disable-next-line no-use-before-define
-					unlisten()
-					resolve(rows)
-				}
-				const unlisten = () => {
-					parser.off("readable", reader)
-					parser.off("error", thrower)
-					parser.off("end", closer)
-				}
-				parser.on("readable", reader)
-				parser.on("error", thrower)
-				parser.on("end", closer)
-				parser.write(buffer)
-				parser.end()
-			}).then(value => ({
-				field,
-				value
-			}))
+						// eslint-disable-next-line no-use-before-define
+						unlisten()
+						reject(error)
+					}
+					const closer = () => {
+						Log.trace(
+							"middleware",
+							`resolving the read rows in the CSV file io "${field}" field`
+						)
+						// eslint-disable-next-line no-use-before-define
+						unlisten()
+						resolve(rows)
+					}
+					const unlisten = () => {
+						parser.off("readable", reader)
+						parser.off("error", thrower)
+						parser.off("end", closer)
+					}
+					parser.on("readable", reader)
+					parser.on("error", thrower)
+					parser.on("end", closer)
+					parser.write(buffer)
+					parser.end()
+				}).then(value => ({
+					field,
+					value
+				}))
 
-			promisedFields.push(promise)
-			Log.success("middleware", `parsed the CSV file in "${field}" field`)
+				promisedFields.push(promise)
+				Log.success("middleware", `parsed the CSV file in "${field}" field`)
+			}
+
+			(await Promise.all(promisedFields)).forEach(info => {
+				const { field, value } = info
+				setDeepPath(request.body, field, value)
+			})
+		} catch (error) {
+			if (error instanceof BaseError) {
+				throw error
+			} else if (error instanceof Error) {
+				throw new ParserError(error.message)
+			} else {
+				throw error
+			}
 		}
-
-		(await Promise.all(promisedFields)).forEach(info => {
-			const { field, value } = info
-			setDeepPath(request.body, field, value)
-		})
 	}
 }
