78326fdea93b82ede4561d0f9e475e625309604c
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-24 23:30:26 +0800

    refactor(base): redo the mechanism to catch errors

--- server/bases/request_filter.ts
+++ server/bases/request_filter.ts
@@ -6,13 +6,18 @@ import Middleware from "!/bases/middleware"
  * Base class for request filters.
  *
  * Request filters only use the request for vlidation, policies, logging, and others and guarantees
- * not to respond. If there are errors encountered, they will be thrown.
+ * not to respond. If there are errors encountered, they should be thrown.
  */
 export default abstract class extends Middleware {
 	abstract filterRequest(request: Request): Promise<void>
 
 	async intermediate(request: Request, unusedResponse: Response, next: NextFunction)
 	: Promise<void> {
-		return await this.filterRequest(request).then(() => next()).catch(next)
+		try {
+			await this.filterRequest(request)
+			next()
+		} catch (error) {
+			next(error)
+		}
 	}
 }
