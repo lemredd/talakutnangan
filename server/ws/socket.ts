1e3b1626d9a4524f2555fcc43c7be381f04a056f
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-24 20:05:11 +0800

    intrn(server): make method for handling mechanisms

--- server/ws/socket.ts
+++ server/ws/socket.ts
@@ -1,18 +1,39 @@
 import { Server as WebSocketServer } from "socket.io"
 
 import Log from "$!/singletons/log"
+import Developer from "$!/errors/developer"
 import RequestEnvironment from "$!/singletons/request_environment"
 
 export default class Socket extends RequestEnvironment {
-	private static server: WebSocketServer|null = null
+	private static rawServer: WebSocketServer|null = null
 
 	static initialize(server: WebSocketServer) {
-		Socket.server = server
+		this.rawServer = server
 
 		Log.trace("app", "initialized web socket")
 	}
 
 	static emitToClients(namespace: string, eventName: string, ...data: any) {
-		this.server?.of(namespace).emit(eventName, data)
+		this.runDependingOnEnvironment(
+			() => this.server.of(namespace).emit(eventName, data),
+			() => {}
+		)
+	}
+
+	private static get server(): WebSocketServer {
+		if (this.rawServer) return this.rawServer
+
+		throw new Developer(
+			"Web socket server was not initialized.",
+			"Some services are not working at the moment."
+		)
+	}
+
+	private static runDependingOnEnvironment(liveMechanism: () => void, testMechanism: () => void) {
+		if (this.isOnTest) {
+			testMechanism()
+		} else {
+			liveMechanism()
+		}
 	}
 }
