a820f04afe0b4a8e8bf402658a88522ecd6503fb
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-24 11:41:38 +0800

    refactor(middleware): separate the employee schedule policy

--- routes/api/employee_schedule/restore.patch.ts
+++ routes/api/employee_schedule/restore.patch.ts
@@ -1,21 +1,11 @@
 import type { FieldRules } from "!/types/validation"
-import type { DeserializedUserDocument } from "$/types/documents/user"
-import type { AuthenticatedRequest, Request, Response } from "!/types/dependent"
+import type { Request, Response } from "!/types/dependent"
 
 import Policy from "!/bases/policy"
-import deserialize from "$/helpers/deserialize"
 import JSONController from "!/controllers/json"
-import AuthorizationError from "$!/errors/authorization"
 import NoContentResponseInfo from "!/response_infos/no_content"
 import EmployeeScheduleManager from "%/managers/employee_schedule"
-
-import PermissionBasedPolicy from "!/policies/permission-based"
-import { user as permissionGroup } from "$/permissions/permission_list"
-import {
-	UPDATE_OWN_DATA,
-	UPDATE_ANYONE_ON_OWN_DEPARTMENT,
-	UPDATE_ANYONE_ON_ALL_DEPARTMENTS
-} from "$/permissions/user_combinations"
+import CommonMiddlewareList from "!/middlewares/common_middleware_list"
 
 import archived from "!/validators/manager/archived"
 import makeResourceIdentifierListDocumentRules
@@ -25,28 +15,7 @@ export default class extends JSONController {
 	get filePath(): string { return __filename }
 
 	get policy(): Policy {
-		return new PermissionBasedPolicy(permissionGroup, [
-			UPDATE_OWN_DATA,
-			UPDATE_ANYONE_ON_OWN_DEPARTMENT,
-			UPDATE_ANYONE_ON_ALL_DEPARTMENTS
-		], (request: AuthenticatedRequest) => {
-			const user = deserialize(request.user) as DeserializedUserDocument
-			const roles = user.data.roles.data
-			const hasWidePermission = permissionGroup.hasOneRoleAllowed(
-				roles,
-				[ UPDATE_ANYONE_ON_OWN_DEPARTMENT, UPDATE_ANYONE_ON_ALL_DEPARTMENTS ]
-			)
-
-			if (!hasWidePermission) {
-				if (user.data.kind !== "reachable_employee") {
-					return Promise.reject(
-						new AuthorizationError("Action is not available to the current user.")
-					)
-				}
-			}
-
-			return Promise.resolve()
-		})
+		return CommonMiddlewareList.employeeSchedulePolicy
 	}
 
 	makeBodyRuleGenerator(unusedRequest: Request): FieldRules {
--- server/middlewares/common_middleware_list.ts
+++ server/middlewares/common_middleware_list.ts
@@ -1,9 +1,24 @@
+import type { GeneralObject } from "$/types/general"
+import type { AuthenticatedRequest } from "!/types/dependent"
+import type { DeserializedUserDocument } from "$/types/documents/user"
+import type { Permissions as UserPermissions } from "$/permissions/user"
+
 import KindBasedPolicy from "!/policies/kind-based"
 import JSONBodyParser from "!/middlewares/body_parser/json"
+import PermissionBasedPolicy from "!/policies/permission-based"
 import MultipartParser from "!/middlewares/body_parser/multipart"
 import AuthenticationBasedPolicy from "!/policies/authentication-based"
 import EmailVerification from "!/middlewares/email_sender/email_verification"
 import NewUserNotification from "!/middlewares/email_sender/new_user_notification"
+
+import deserialize from "$/helpers/deserialize"
+import { user } from "$/permissions/permission_list"
+import AuthorizationError from "$!/errors/authorization"
+import {
+	UPDATE_OWN_DATA,
+	UPDATE_ANYONE_ON_OWN_DEPARTMENT,
+	UPDATE_ANYONE_ON_ALL_DEPARTMENTS
+} from "$/permissions/user_combinations"
 
 export default class CommonMiddlewareList {
 	static guestOnlyPolicy: AuthenticationBasedPolicy
@@ -11,6 +26,8 @@ export default class CommonMiddlewareList {
 	static unreachableEmployeeOnlyPolicy: KindBasedPolicy
 	static reachableEmployeeOnlyPolicy: KindBasedPolicy
 	static studentOnlyPolicy: KindBasedPolicy
+	static employeeSchedulePolicy: PermissionBasedPolicy<GeneralObject<number>, UserPermissions, any>
+
 	static JSONBody: JSONBodyParser
 	static multipart: MultipartParser
 	static emailVerification: EmailVerification
@@ -27,6 +44,28 @@ export default class CommonMiddlewareList {
 				"reachable_employee"
 			)
 			CommonMiddlewareList.studentOnlyPolicy = new KindBasedPolicy("student")
+			CommonMiddlewareList.employeeSchedulePolicy = new PermissionBasedPolicy(user, [
+				UPDATE_OWN_DATA,
+				UPDATE_ANYONE_ON_OWN_DEPARTMENT,
+				UPDATE_ANYONE_ON_ALL_DEPARTMENTS
+			], (request: AuthenticatedRequest) => {
+				const currentUser = deserialize(request.user) as DeserializedUserDocument
+				const roles = currentUser.data.roles.data
+				const hasWidePermission = user.hasOneRoleAllowed(
+					roles,
+					[ UPDATE_ANYONE_ON_OWN_DEPARTMENT, UPDATE_ANYONE_ON_ALL_DEPARTMENTS ]
+				)
+
+				if (!hasWidePermission) {
+					if (currentUser.data.kind !== "reachable_employee") {
+						return Promise.reject(
+							new AuthorizationError("Action is not available to the current user.")
+						)
+					}
+				}
+
+				return Promise.resolve()
+			})
 
 			CommonMiddlewareList.JSONBody = new JSONBodyParser()
 			CommonMiddlewareList.multipart = new MultipartParser()
