a552bc159d42048c2185f30dfef7895ce8812491
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-25 13:02:11 +0800

    intrn(rule set)!: accept object for identifier document rule makers

--- server/rule_sets/make_resource_identifier_list_document.ts
+++ server/rule_sets/make_resource_identifier_list_document.ts
@@ -1,5 +1,5 @@
-import type { FieldRules, Pipe } from "!/types/validation"
 import type { BaseManagerClass } from "!/types/independent"
+import type { FieldRules, Rules, Pipe } from "!/types/validation"
 
 import makeDataDocumentRules from "!/rule_sets/make_data_document"
 import makeResourceIdentifierRules from "!/rule_sets/make_resource_identifier"
@@ -10,20 +10,31 @@ import makeResourceIdentifierRules from "!/rule_sets/make_resource_identifier"
  * @param typeName Name of the type to expect
  * @param validator Pipe to use to validate the ID.
  * @param ClassName Manager class where to validate the ID if it exists
- * @param extraQueries If theare additional queries
+ * @param options Optional arguments
+ * @param options.postIDRules Additional rules for the ID.
+ * @param options.mustCast If the ID in the identifier should be casted.
+ * @param options.extraQueries If there are additional queries
  */
 export default function(
 	typeName: string,
 	validator: Pipe,
 	ClassName: BaseManagerClass,
-	extraIdentifierQueries: FieldRules = {},
-	extraQueries: FieldRules = {}
+	{
+		postIDRules = { "pipes": [] },
+		extraIdentifierQueries = {},
+		extraQueries = {}
+	}: Partial<{
+		postIDRules: Rules
+		extraIdentifierQueries: FieldRules
+		extraQueries: FieldRules
+	}> = {}
 ): FieldRules {
 	return makeDataDocumentRules(
 		false,
 		makeResourceIdentifierRules(typeName, validator, ClassName, {
 			"extraQueries": extraIdentifierQueries,
-			"mustCastID": false
+			"mustCastID": false,
+			postIDRules
 		}),
 		extraQueries
 	)
