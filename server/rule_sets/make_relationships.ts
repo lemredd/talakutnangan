79b201cdc7b2296c514d58740ebeb1056ea3e591
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-25 13:33:38 +0800

    intrn(rule set): make relationship rule creation more readable

--- server/rule_sets/make_relationships.ts
+++ server/rule_sets/make_relationships.ts
@@ -1,10 +1,14 @@
+import type { Relationship } from "!/types/rule_set"
 import type { FieldRules, Rules } from "!/types/validation"
 
 import object from "!/validators/base/object"
 import required from "!/validators/base/required"
+import makeResourceIdentifierDocumentRules from "!/rule_sets/make_resource_identifier_document"
+import makeResourceIdentifierListDocumentRules
+	from "!/rule_sets/make_resource_identifier_list_document"
 
 export default function(
-	relationships: FieldRules,
+	relationships: Relationship[],
 	{
 		postRelationshipValidation = { "pipes": [] } as Rules
 	}: Partial<{
@@ -14,7 +18,33 @@ export default function(
 	return {
 		"relationships": {
 			"constraints": {
-				"object": relationships,
+				"object": relationships.map(relationship => {
+					const {
+						isArray,
+						typeName,
+						validator,
+						ClassName,
+						options
+					} = relationship
+					if (isArray) {
+						return makeResourceIdentifierDocumentRules(
+							typeName,
+							validator,
+							ClassName,
+							options ?? {}
+						)
+					}
+
+					return makeResourceIdentifierListDocumentRules(
+						typeName,
+						validator,
+						ClassName,
+						options ?? {}
+					)
+				}).reduce((previousFields, currentField) => ({
+					...previousFields,
+					...currentField
+				}), {}),
 				...postRelationshipValidation.constraints ?? {}
 			},
 			"pipes": [ required, object, ...postRelationshipValidation.pipes ]
--- server/types/rule_set.ts
+++ server/types/rule_set.ts
@@ -1,7 +1,20 @@
-import type { FieldRules, Rules } from "!/types/validation"
+import type { BaseManagerClass } from "!/types/independent"
+import type { FieldRules, Rules, Pipe } from "!/types/validation"
 
 export type IdentifierDocumentOptions = Partial<{
 	postIDRules: Rules
 	extraIdentifierQueries: FieldRules
 	extraQueries: FieldRules
 }>
+
+export interface Relationship{
+	isArray: boolean,
+	typeName: string,
+	validator: Pipe,
+	ClassName: BaseManagerClass,
+	options?: Partial<{
+		postIDRules: Rules
+		extraIdentifierQueries: FieldRules
+		extraQueries: FieldRules
+	}>
+}
