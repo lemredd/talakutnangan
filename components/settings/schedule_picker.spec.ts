7bc0da9122b3532601f6f4bef133e1fbe4158352
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-08-27 01:11:41 +0800

    unit(component): ensure 24-hour format emission

--- components/settings/schedule_picker.spec.ts
+++ components/settings/schedule_picker.spec.ts
@@ -15,3 +15,19 @@ describe("Component: Schedule Picker", () => {
 		expect(endTimeSelector.value).toBe("05")
 	})
 
+	it("should emit 24-hour format value", async() => {
+		const wrapper = mount(SchedulePicker, {
+			"props": {
+				"day": "Monday",
+				"endTime": "17:00",
+				"startTime": "08:00"
+			}
+		})
+		const endTimeSelector = wrapper.find("#end select")
+
+		await endTimeSelector.setValue("06")
+
+		const passedNewTime = wrapper.emitted("passNewTime") as Array<Array<any>>
+		expect(passedNewTime[0]).toEqual([ "18:00" ])
+	})
+})
