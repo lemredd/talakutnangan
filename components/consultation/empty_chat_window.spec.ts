f18d2f68d03c003fcc87dc40d15de5b15b9afaff
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-26 12:30:34 +0800

    intrn(component): make component for empty chat window

---
+++ components/consultation/empty_chat_window.spec.ts
@@ -0,0 +1,41 @@
+import { shallowMount, flushPromises } from "@vue/test-utils"
+
+import Component from "./empty_chat_window.vue"
+
+describe("Component: consultation/empty_chat_window", () => {
+	it("should show student content", () => {
+		const wrapper = shallowMount<any>(Component, {
+			"props": {
+				"userProfile": {
+					"data": {
+						"kind": "student"
+					}
+				}
+			}
+		})
+
+		const header = wrapper.find("h1.student")
+		const content = wrapper.find("p.student")
+
+		expect(header.exists()).toBeTruthy()
+		expect(content.exists()).toBeTruthy()
+	})
+
+	it("should show reachable employee content", () => {
+		const wrapper = shallowMount<any>(Component, {
+			"props": {
+				"userProfile": {
+					"data": {
+						"kind": "reachable_employee"
+					}
+				}
+			}
+		})
+
+		const header = wrapper.find("h1.reachable_employee")
+		const content = wrapper.find("p.reachable_employee")
+
+		expect(header.exists()).toBeTruthy()
+		expect(content.exists()).toBeTruthy()
+	})
+})
