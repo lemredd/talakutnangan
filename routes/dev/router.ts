1a74826c85ef81c70441dfa7bab70375c4615f23
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-26 11:50:40 +0800

    intrn(server): make route to log in as student

--- routes/dev/router.ts
+++ routes/dev/router.ts
@@ -3,8 +3,10 @@ import GetEmailsRoute from "!%/dev/emails.get"
 import GetServerInfoRoute from "!%/dev/server_info.get"
 import GetLogInAsDeanRoute from "!%/dev/log_in_as_dean.get"
 import GetLogInAsAdminRoute from "!%/dev/log_in_as_admin.get"
+import GetLogInAsStudentRoute from "!%/dev/log_in_as_student.get"
 import GetLogInAsSecretaryRoute from "!%/dev/log_in_as_secretary.get"
 import GetLogInAsServiceHeadRoute from "!%/dev/log_in_as_service_head.get"
+
 import GetSampleUserListRoute from "!%/dev/sample_user_list.get"
 import GetSampleServerErrorRoute from "!%/dev/sample_server_error.get"
 import GetMakeUnverifiedUserRoute from "!%/dev/make_unverified_user.get"
@@ -19,8 +21,10 @@ export default class extends Router {
 			new GetServerInfoRoute(),
 			new GetLogInAsDeanRoute(),
 			new GetLogInAsAdminRoute(),
+			new GetLogInAsStudentRoute(),
 			new GetLogInAsSecretaryRoute(),
 			new GetLogInAsServiceHeadRoute(),
+
 			new GetSampleUserListRoute(),
 			new GetSampleServerErrorRoute(),
 			new GetMakeUnverifiedUserRoute(),
