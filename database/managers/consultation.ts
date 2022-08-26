eaeffb7c05a17c0ccff0a30d8a59002a5fd10968
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-26 23:15:38 +0800

    intrn(manager): integrate range filter to consultation

--- database/managers/consultation.ts
+++ database/managers/consultation.ts
@@ -1,5 +1,5 @@
 import type { Pipe } from "$/types/database"
-import type { CommonQueryParameters } from "$/types/query"
+import type { ConsultationQueryParameters } from "$/types/query"
 import type { ModelCtor, FindAndCountOptions } from "%/types/dependent"
 import type { ConsultationAttributes } from "$/types/documents/consultation"
 
@@ -7,19 +7,21 @@ import BaseManager from "%/managers/base"
 import Model from "%/models/consultation"
 import Transformer from "%/transformers/consultation"
 
+import siftByRange from "%/queries/consultation/sift_by_range"
 import includeDefaults from "%/queries/consultation/include_defaults"
 
 export default class extends BaseManager<
 	Model,
 	ConsultationAttributes<"deserialized">,
-	CommonQueryParameters
+	ConsultationQueryParameters
 > {
 	get model(): ModelCtor<Model> { return Model }
 
 	get transformer(): Transformer { return new Transformer() }
 
-	get listPipeline(): Pipe<FindAndCountOptions<Model>, CommonQueryParameters>[] {
+	get listPipeline(): Pipe<FindAndCountOptions<Model>, ConsultationQueryParameters>[] {
 		return [
+			siftByRange,
 			includeDefaults,
 			...super.listPipeline
 		]
