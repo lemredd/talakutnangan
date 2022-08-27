4b15459bf37e794c20873c6e533c4084a1ed4c62
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-27 11:47:12 +0800

    fix(employee schedule): fix the validator to list employee schedules

--- routes/api/employee_schedule/list.get.ts
+++ routes/api/employee_schedule/list.get.ts
@@ -1,6 +1,7 @@
 import type { FieldRules } from "!/types/validation"
 import type { Request, Response } from "!/types/dependent"
 import type { EmployeeScheduleQueryParameters } from "$/types/query"
+import { DayValues } from "$/types/database"
 
 import Policy from "!/bases/policy"
 import UserManager from "%/managers/user"
@@ -8,10 +9,19 @@ import ListResponse from "!/response_infos/list"
 import QueryController from "!/controllers/query"
 import EmployeeScheduleManager from "%/managers/employee_schedule"
 import CommonMiddlewareList from "!/middlewares/common_middleware_list"
+import convertTimeToMinutes from "$/helpers/time/convert_time_to_minutes"
 
 import makeIDRules from "!/rule_sets/make_id"
+import object from "!/validators/base/object"
+import string from "!/validators/base/string"
+import integer from "!/validators/base/integer"
 import exists from "!/validators/manager/exists"
 import makeListRules from "!/rule_sets/make_list"
+import required from "!/validators/base/required"
+import nullable from "!/validators/base/nullable"
+import range from "!/validators/comparison/range"
+import oneOf from "!/validators/comparison/one-of"
+import skipAsterisk from "!/validators/comparison/skip_asterisk"
 
 export default class extends QueryController {
 	get filePath(): string { return __filename }
@@ -23,19 +33,60 @@ export default class extends QueryController {
 	makeQueryRuleGenerator(unusedRequest: Request): FieldRules {
 		return makeListRules(
 			EmployeeScheduleManager,
-			makeIDRules({
-				"IDName": "user",
-				"mustCast": true,
-				"postRules": {
+			{
+				...makeIDRules({
+					"IDName": "user",
+					"mustCast": true,
+					"postRules": {
+						"constraints": {
+							"manager": {
+								"className": UserManager,
+								"columnName": "id"
+							}
+						},
+						"pipes": [ exists ]
+					}
+				}),
+				"day": {
 					"constraints": {
-						"manager": {
-							"className": UserManager,
-							"columnName": "id"
+						"nullable": {
+							"defaultValue": "*"
+						},
+						"oneOf": {
+							"values": [ ...DayValues ]
 						}
 					},
-					"pipes": [ exists ]
+					"pipes": [ nullable, skipAsterisk, string, oneOf ]
+				},
+				"employeeScheduleRange": {
+					"constraints": {
+						"nullable": {
+							"defaultValue": "*"
+						},
+						"object": {
+							"end": {
+								"constraints": {
+									"range": {
+										"maximum": convertTimeToMinutes("23:59"),
+										"minimum": convertTimeToMinutes("00:01")
+									}
+								},
+								"pipes": [ required, integer, range ]
+							},
+							"start": {
+								"constraints": {
+									"range": {
+										"maximum": convertTimeToMinutes("23:58"),
+										"minimum": convertTimeToMinutes("00:00")
+									}
+								},
+								"pipes": [ required, integer, range ]
+							}
+						}
+					},
+					"pipes": [ nullable, skipAsterisk, object ]
 				}
-			})
+			}
 		)
 	}
 
