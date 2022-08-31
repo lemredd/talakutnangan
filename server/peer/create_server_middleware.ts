a229060b0d945f052928b053da259917fdfdbe4e
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-08-31 14:56:55 +0800

    intrn(server): use the peer middleware automatically

--- server/peer/create_server_middleware.ts
+++ server/peer/create_server_middleware.ts
@@ -1,13 +1,16 @@
 import type { Server } from "http"
+import type { Express } from "express"
 import type { PeerServer } from "!/types/dependent"
 
 import { ExpressPeerServer } from "peer"
 
-export default function(httpServer: Server): PeerServer {
+export default function(app: Express, httpServer: Server): PeerServer {
 	const peerServer = ExpressPeerServer(httpServer, {
-		"path": "/",
+		"path": "/server",
 		"proxied": process.env.WEB_PROXY === "true"
 	})
+
+	app.use("/p2p", peerServer)
 
 	return peerServer
 }
