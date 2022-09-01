1faff3f0cc2b6a7b7a9914a376ed39438613836e
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-01 18:55:35 +0800

    types(share): pass the type of ID to consultation filter

--- shareable/types/query.ts
+++ shareable/types/query.ts
@@ -156,4 +156,4 @@ export type ConsultationQueryParameters<T extends number|string = string> =
 
 export type ChatMessageActivityQueryParameters<T extends number|string = string> =
 	& CommonQueryParameters
-	& ConsultationFilter
+	& ConsultationFilter<T>
