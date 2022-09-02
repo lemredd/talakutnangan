63a5949f4672bc826f3da1f9475330dd5eda9472
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-02 21:23:30 +0800

    share(helper): make stub singleton

---
+++ shareable/helpers/singletons/stub.ts
@@ -0,0 +1,43 @@
+import type { GeneralObject } from "$/types/general"
+
+import RequestEnvironment from "$!/singletons/request_environment"
+
+type PreviousCallInfo = { functionName: string, arguments: GeneralObject<any> }
+
+/**
+ * Handler for stubbed mechnisms to spy on procedures.
+ *
+ * Based from `Socket` sinfgleton in server.
+ */
+export default class Stub extends RequestEnvironment {
+	private static previousCallInfos: (PreviousCallInfo|undefined)[] = []
+
+	static consumePreviousCalls(): PreviousCallInfo[] {
+		return this.runConditionally(
+			(): PreviousCallInfo[] => {
+				throw new Error(
+					"Stubber cannot monitor previous calls in non-test environment."
+				)
+			},
+			() => {
+				const previousCalls = this.previousCallInfos.filter(Boolean) as PreviousCallInfo[]
+				this.previousCallInfos = []
+				// eslint-disable-next-line no-undefined
+				return [ previousCalls, undefined ]
+			}
+		)
+	}
+
+	static runConditionally<T = void>(
+		liveMechanism: () => T,
+		testMechanism: () => [T, PreviousCallInfo|undefined]
+	): T {
+		if (this.isOnTest) {
+			const [ valueToReturn, callInfo ] = testMechanism()
+			this.previousCallInfos.push(callInfo)
+			return valueToReturn
+		}
+
+		return liveMechanism()
+	}
+}
