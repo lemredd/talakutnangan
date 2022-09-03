fdbd895a1f762f7d51e056b836a3214c29697e92
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-03 19:07:03 +0800

    intg(consultation): prepare tests to check requests

--- pages/consultation/read.page.spec.ts
+++ pages/consultation/read.page.spec.ts
@@ -1,9 +1,12 @@
-import { mount } from "@vue/test-utils"
+import { mount, flushPromises } from "@vue/test-utils"
+
+import { JSON_API_MEDIA_TYPE } from "$/types/server"
 
 import UserFactory from "~/factories/user"
 import Stub from "$/helpers/singletons/stub"
 import Factory from "~/factories/consultation"
 import ChatMessageFactory from "~/factories/chat_message"
+import RequestEnvironment from "$/helpers/request_environment"
 import ChatMessageActivity from "%/models/chat_message_activity"
 import UserProfileTransformer from "%/transformers/user_profile"
 import ChatMessageTransformer from "%/transformers/chat_message"
@@ -13,10 +16,14 @@ import Page from "./read.page.vue"
 
 describe("UI Page: Read resource by ID", () => {
 	it("should load resource by ID", async() => {
+		const OTHER_CONSULTATION_COUNT = 3
+		const ALL_CONSULTATION_COUNT = OTHER_CONSULTATION_COUNT + 1
+		const MESSAGE_COUNT = 5
+
 		const userFactory = new UserFactory()
 		const userModel = await userFactory.insertOne()
 		const factory = new Factory()
-		const models = await factory.insertMany(3)
+		const models = await factory.insertMany(OTHER_CONSULTATION_COUNT)
 		const model = await factory.insertOne()
 		const allModels = [ model, ...models ]
 		const allModelIterator = allModels.values()
@@ -34,7 +41,7 @@ describe("UI Page: Read resource by ID", () => {
 		) as ChatMessageActivity
 		const chatMessageModels = await chatMessageFactory
 		.chatMessageActivity(() => Promise.resolve(activityOfModel))
-		.insertMany(5)
+		.insertMany(MESSAGE_COUNT)
 
 		const userResource = userFactory.deserialize(
 			userModel,
@@ -42,7 +49,7 @@ describe("UI Page: Read resource by ID", () => {
 			new UserProfileTransformer()
 		)
 		const resource = factory.deserialize(model)
-		const resources = factory.deserialize(models)
+		const resources = factory.deserialize([ model, ...models ])
 		const chatMessageActivityResources = chatMessageActivityFactory
 		.deserialize(chatMessageActivityModels)
 		const previewMessageResources = chatMessageFactory.deserialize(
@@ -51,6 +58,17 @@ describe("UI Page: Read resource by ID", () => {
 			new ChatMessageTransformer({ "included": [ "user", "consultation" ] })
 		)
 		const chatMessageResources = chatMessageFactory.deserialize(chatMessageModels)
+
+		fetchMock.mockResponse(
+			JSON.stringify({
+				"data": [],
+				"meta": {
+					"count": 0
+				}
+			}),
+			{ "status": RequestEnvironment.status.OK }
+		)
+
 		const wrapper = mount(Page, {
 			"global": {
 				"provide": {
@@ -70,16 +88,30 @@ describe("UI Page: Read resource by ID", () => {
 
 		const consultationList = wrapper.find(".consultations-list")
 		const chatWindow = wrapper.find(".chat-window")
+		await flushPromises()
 
 		expect(consultationList.exists()).toBeTruthy()
 		expect(chatWindow.exists()).toBeTruthy()
+
+		const castFetch = fetch as jest.Mock<any, any>
+		const [ [ request ] ] = castFetch.mock.calls
+		expect(request).toHaveProperty("method", "GET")
+		expect(request).toHaveProperty("url", "/api/consultation")
+		expect(request.headers.get("Content-Type")).toBe(JSON_API_MEDIA_TYPE)
+		expect(request.headers.get("Accept")).toBe(JSON_API_MEDIA_TYPE)
+		const consultationListRequestBody = await request.json()
+		expect(consultationListRequestBody).toHaveProperty("page.offset", ALL_CONSULTATION_COUNT)
 	})
 
 	it("can visit resource by ID", async() => {
+		const OTHER_CONSULTATION_COUNT = 3
+		const ALL_CONSULTATION_COUNT = OTHER_CONSULTATION_COUNT + 1
+		const MESSAGE_COUNT = 5
+
 		const userFactory = new UserFactory()
 		const userModel = await userFactory.insertOne()
 		const factory = new Factory()
-		const models = await factory.insertMany(3)
+		const models = await factory.insertMany(OTHER_CONSULTATION_COUNT)
 		const model = await factory.insertOne()
 		const allModels = [ model, ...models ]
 		const allModelIterator = allModels.values()
@@ -97,7 +129,7 @@ describe("UI Page: Read resource by ID", () => {
 		) as ChatMessageActivity
 		const chatMessageModels = await chatMessageFactory
 		.chatMessageActivity(() => Promise.resolve(activityOfModel))
-		.insertMany(5)
+		.insertMany(MESSAGE_COUNT)
 
 		const userResource = userFactory.deserialize(
 			userModel,
@@ -105,7 +137,7 @@ describe("UI Page: Read resource by ID", () => {
 			new UserProfileTransformer()
 		)
 		const resource = factory.deserialize(model)
-		const resources = factory.deserialize(models)
+		const resources = factory.deserialize([ model, ...models ])
 		const chatMessageActivityResources = chatMessageActivityFactory
 		.deserialize(chatMessageActivityModels)
 		const previewMessageResources = chatMessageFactory.deserialize(
@@ -114,6 +146,17 @@ describe("UI Page: Read resource by ID", () => {
 			new ChatMessageTransformer({ "included": [ "user", "consultation" ] })
 		)
 		const chatMessageResources = chatMessageFactory.deserialize(chatMessageModels)
+
+		fetchMock.mockResponse(
+			JSON.stringify({
+				"data": [],
+				"meta": {
+					"count": 0
+				}
+			}),
+			{ "status": RequestEnvironment.status.OK }
+		)
+
 		const wrapper = mount(Page, {
 			"global": {
 				"provide": {
@@ -133,9 +176,19 @@ describe("UI Page: Read resource by ID", () => {
 		const consultationListItem = wrapper.find(".consultation:nth-child(2)")
 
 		await consultationListItem.trigger("click")
+		await flushPromises()
 
 		const previousCalls = Stub.consumePreviousCalls()
 		expect(previousCalls).toHaveProperty("0.functionName", "assignPath")
-		expect(previousCalls).toHaveProperty("0.arguments", [ `/consultation/${models[1].id}` ])
+		expect(previousCalls).toHaveProperty("0.arguments", [ `/consultation/${models[0].id}` ])
+
+		const castFetch = fetch as jest.Mock<any, any>
+		const [ [ request ] ] = castFetch.mock.calls
+		expect(request).toHaveProperty("method", "GET")
+		expect(request).toHaveProperty("url", "/api/consultation")
+		expect(request.headers.get("Content-Type")).toBe(JSON_API_MEDIA_TYPE)
+		expect(request.headers.get("Accept")).toBe(JSON_API_MEDIA_TYPE)
+		const consultationListRequestBody = await request.json()
+		expect(consultationListRequestBody).toHaveProperty("page.offset", ALL_CONSULTATION_COUNT)
 	})
 })
