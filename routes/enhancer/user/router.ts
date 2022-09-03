773b3b5cecf27b982f2ec7cbf4b143cd0ad84515
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-09-03 19:36:38 +0800

    feat(server): make route for `/user/list`

--- routes/enhancer/user/router.ts
+++ routes/enhancer/user/router.ts
@@ -1,4 +1,5 @@
 import Router from "!/bases/router"
+import GetList from "!%/enhancer/user/list.get"
 import GetImport from "!%/enhancer/user/import.get"
 import GetLogIn from "!%/enhancer/user/log_in.get"
 import GetVerify from "!%/enhancer/user/verify(data).get"
@@ -11,6 +12,7 @@ export default class extends Router {
 		super()
 
 		this.useControllers([
+			new GetList(),
 			new GetLogIn(),
 			new GetImport(),
 			new GetVerify(),
