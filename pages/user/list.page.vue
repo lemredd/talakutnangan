9a221f69856179ce55535f63923b0904ba77a405
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-09-03 20:14:12 +0800

    rfmt(page): use elements from similar pages

--- pages/user/list.page.vue
+++ pages/user/list.page.vue
@@ -3,9 +3,18 @@
 	<h1 v-else class="resource-config-header">
 		{{ determineTitle }}
 	</h1>
+
+	<UsersManager :resource="users">
+		<template #search-filter>
+			<SearchFilter :resource="users" @filter-resource-by-search="getFilteredList"/>
+		</template>
+
+		<UsersList :filtered-list="filteredList"/>
+	</UsersManager>
 </template>
 
 <style scoped lang="scss">
+
 .resource-config-header {
 	font-size: 1.75em;
 	text-transform: uppercase;
@@ -13,30 +22,69 @@
 </style>
 
 <script setup lang="ts">
-import { computed, inject } from "vue"
+import { computed, inject, onMounted, provide, ref } from "vue"
 
-import type { DeserializedUserProfile } from "$/types/documents/user"
+import type { PageContext } from "$/types/renderer"
+import type { PossibleResources } from "$@/types/independent"
+import type { DeserializedUserResource, DeserializedUserProfile } from "$/types/documents/user"
 
 import Manager from "$/helpers/manager"
-import type { PageContext } from "$/types/renderer"
 import AdminConfigHeader from "@/tabbed_page_header.vue"
+import UsersManager from "@/resource_management/resource_manager.vue"
+import SearchFilter from "@/resource_management/resource_manager/search_bar.vue"
+import UsersList from "@/resource_management/resource_manager/resource_list.vue"
+
+import UserFetcher from "$@/fetchers/user"
+import RoleFetcher from "$@/fetchers/role"
+import DepartmentFetcher from "$@/fetchers/department"
 
 const pageContext = inject("pageContext") as PageContext<"deserialized", "consultations">
 const { pageProps } = pageContext
 const userProfile = pageProps.userProfile as DeserializedUserProfile
 const currentResourceManager = new Manager(userProfile)
-const currentUserDepartment = userProfile.data.department.data.fullName
+const currentUserDepartment = userProfile.data.department.data
+
+provide("managerKind", new Manager(userProfile))
+provide("tabs", [ "Users", "Roles", "Departments" ])
+
+UserFetcher.initialize("/api")
+RoleFetcher.initialize("/api")
+DepartmentFetcher.initialize("/api")
 
 const determineTitle = computed(() => {
-	console.log(currentResourceManager.isInstituteLimited())
-
 	if (currentResourceManager.isInstituteLimited()) {
-		return `Users of ${currentUserDepartment}`
+		return `Users of ${currentUserDepartment.fullName}`
 	}
 	if (currentResourceManager.isStudentServiceLimited()) {
-		return `Employees of ${currentUserDepartment}`
+		return `Employees of ${currentUserDepartment.fullName}`
 	}
 
 	return "Administrator Configuration"
+})
+
+const users = ref<DeserializedUserResource[]>([])
+const filteredList = ref<DeserializedUserResource[]>([])
+
+function getFilteredList(resource: PossibleResources[]) {
+	filteredList.value = resource as DeserializedUserResource[]
+}
+
+onMounted(() => {
+	new UserFetcher().list({
+		"filter": {
+			"department": currentResourceManager.isAdmin() ? "*" : currentUserDepartment.id,
+			"existence": "exists",
+			"kind": "*",
+			"role": "*",
+			"slug": ""
+		},
+		"page": {
+			"limit": 10,
+			"offset": 0
+		},
+		"sort": [ "name" ]
+	}).then(({ "body": deserializedUserList }) => {
+		users.value = deserializedUserList.data
+	})
 })
 </script>
