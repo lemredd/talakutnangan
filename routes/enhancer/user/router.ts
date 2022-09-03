583f3b7f7495eb8f5b7ac102c3975e41b0f5476e
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-09-03 20:19:43 +0800

    feat(server)!: remove unnecessary routes
    * these are removed as their purpose are now unified by `user/list`

--- routes/enhancer/user/router.ts
+++ routes/enhancer/user/router.ts
@@ -3,9 +3,6 @@ import GetList from "!%/enhancer/user/list.get"
 import GetImport from "!%/enhancer/user/import.get"
 import GetLogIn from "!%/enhancer/user/log_in.get"
 import GetVerify from "!%/enhancer/user/verify(data).get"
-import GetStudentsList from "!%/enhancer/user/students_list.get"
-import GetEmployeeList from "!%/enhancer/user/employees_list.get"
-import GetInstituteUsersList from "!%/enhancer/user/institute_users.get"
 
 export default class extends Router {
 	constructor() {
@@ -15,10 +12,7 @@ export default class extends Router {
 			new GetList(),
 			new GetLogIn(),
 			new GetImport(),
-			new GetVerify(),
-			new GetStudentsList(),
-			new GetEmployeeList(),
-			new GetInstituteUsersList()
+			new GetVerify()
 		])
 	}
 }
