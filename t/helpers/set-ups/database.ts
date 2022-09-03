46b18ff089b7283708aba01d66b36a82d850f6b2
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-03 17:34:12 +0800

    chore(lint): apply some lint rules

--- t/helpers/set-ups/database.ts
+++ t/helpers/set-ups/database.ts
@@ -1,29 +1,38 @@
+/* eslint-disable max-len */
 import { Sequelize } from "sequelize-typescript"
+
 import Database from "%/data_source/database"
+import isUndefined from "$/helpers/type_guards/is_undefined"
 import getDataSourceType from "~/set-ups/get_data_source_type"
 
 export default class {
 	static #dataSource: Sequelize|undefined
 
 	static async create(): Promise<void> {
-		if (Database.dataSource === undefined) {
+		if (isUndefined(Database.dataSource)) {
 			await Database.initialize(getDataSourceType())
 		}
 
 		this.#dataSource = Database.dataSource
 
-		await this.#dataSource.sync({ force: true })
+		await this.#dataSource.sync({ "force": true })
 	}
 
 	static async clear(): Promise<void> {
-		// See: https://github.com/sequelize/sequelize/issues/11289
-		// See: https://github.com/DefinitelyTyped/DefinitelyTyped/blob/098668baad17230742eaa9da5a10c2e338e7b71d/types/sequelize/index.d.ts#L3564
-		await this.dataSource?.truncate({ force: true, cascade: true })
+		/*
+		 * See: https://github.com/sequelize/sequelize/issues/11289
+		 * See: https://github.com/DefinitelyTyped/DefinitelyTyped/blob/098668baad17230742eaa9da5a10c2e338e7b71d/types/sequelize/index.d.ts#L3564
+		 */
+		await this.dataSource?.truncate({
+			"cascade": true,
+			"force": true
+		})
 	}
 
 	static async destroy(): Promise<void> {
 		await this.dataSource?.close()
 		// @ts-ignore
+		// eslint-disable-next-line no-undefined
 		this.#dataSource = undefined
 	}
 
