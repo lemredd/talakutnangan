aa296700c0d1041cca37d52f55c272d2115e5ab8
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-04 19:55:45 +0800

    unit(role): prepare test to provide updates to external groups

--- components/role/flag_selector.spec.ts
+++ components/role/flag_selector.spec.ts
@@ -1,42 +1,62 @@
 import { mount } from "@vue/test-utils"
 import FlagSelector from "./flag_selector.vue"
 
-import { post } from "$/permissions/permission_list"
-import { ref } from "vue"
+import { post, comment } from "$/permissions/permission_list"
 
-describe("Component: Role/Flag Selector", () => {
+describe("Component: role/flag_selector", () => {
 	it("should check flag depependency/ies", async() => {
 		const wrapper = mount(FlagSelector, {
 			"props": {
-				"header": "Post",
 				"basePermissionGroup": post,
-				"flags": 0
+				"flags": 0,
+				"header": "Post"
 			}
 		})
 
 		const dependentCheckbox = wrapper.find("input[value='create']")
-		const dependencyCheckbox = wrapper.find("input[value='view']")
 		await dependentCheckbox.trigger("change")
 
-		const updates = wrapper.emitted("update:flags")
-		const expectedFlagValue = 3
-		expect(updates).toHaveLength(1)
-		expect(updates![0]).toEqual([ expectedFlagValue ])
+		const internalUpdates = wrapper.emitted("update:flags")
+		const expectedFlagValue = post.generateMask("create", "view")
+		expect(internalUpdates).toHaveLength(1)
+		expect(internalUpdates).toHaveProperty("0.0", expectedFlagValue)
 	})
+
 	it("should increase flags on selection of access level", async() => {
 		const wrapper = mount(FlagSelector, {
 			"props": {
-				"header": "Post",
 				"basePermissionGroup": post,
-				"flags": 0
+				"flags": 0,
+				"header": "Post"
 			}
 		})
 
-		const readAccessLevelFlags = wrapper.find("#read-scope")
+		const readAccessLevelFlags = wrapper.find(".read-scope")
 		await readAccessLevelFlags.setValue("readDepartmentScope")
 
-		const updates = wrapper.emitted("update:flags")
+		const internalUpdates = wrapper.emitted("update:flags")
 		const expectedFlagValue = post.generateMask("readDepartmentScope")
-		expect(updates![0]).toEqual([ expectedFlagValue ])
+		expect(internalUpdates).toHaveProperty("0.0", expectedFlagValue)
+	})
+
+	it("should check dependent flags on other groups", async() => {
+		const wrapper = mount(FlagSelector, {
+			"props": {
+				"basePermissionGroup": comment,
+				"flags": 0,
+				"header": "Comment"
+			}
+		})
+
+		const dependentCheckbox = wrapper.find("input[value='view']")
+		await dependentCheckbox.trigger("change")
+
+		const internalUpdates = wrapper.emitted("update:flags")
+		const expectedFlagValue = comment.generateMask("view")
+		expect(internalUpdates).toHaveProperty("0.0", expectedFlagValue)
+		const externalUpdates = wrapper.emitted("update_dependency_flags")
+		expect(externalUpdates).toHaveProperty("0.0.0.group", post)
+		expect(externalUpdates).toHaveProperty("0.0.0.permissionDependencies", [ "view" ])
+		expect(externalUpdates).not.toHaveProperty("0.0.1")
 	})
 })
