449eb46c0b35cc69bb1465c02b6b50266157ca74
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-04 20:02:54 +0800

    unit(role): prepare test to uncheck dependent flags

--- components/role/flag_selector.spec.ts
+++ components/role/flag_selector.spec.ts
@@ -1,11 +1,11 @@
-import { mount } from "@vue/test-utils"
+import { shallowMount } from "@vue/test-utils"
 import FlagSelector from "./flag_selector.vue"
 
 import { post, comment } from "$/permissions/permission_list"
 
 describe("Component: role/flag_selector", () => {
-	it("should check flag depependency/ies", async() => {
-		const wrapper = mount(FlagSelector, {
+	it("should check internal flag depependency/ies", async() => {
+		const wrapper = shallowMount(FlagSelector, {
 			"props": {
 				"basePermissionGroup": post,
 				"flags": 0,
@@ -22,8 +22,8 @@ describe("Component: role/flag_selector", () => {
 		expect(internalUpdates).toHaveProperty("0.0", expectedFlagValue)
 	})
 
-	it("should increase flags on selection of access level", async() => {
-		const wrapper = mount(FlagSelector, {
+	it("should increase internal flags on selection of access level", async() => {
+		const wrapper = shallowMount(FlagSelector, {
 			"props": {
 				"basePermissionGroup": post,
 				"flags": 0,
@@ -39,8 +39,8 @@ describe("Component: role/flag_selector", () => {
 		expect(internalUpdates).toHaveProperty("0.0", expectedFlagValue)
 	})
 
-	it("should check dependent flags on other groups", async() => {
-		const wrapper = mount(FlagSelector, {
+	it("should check external dependency flags", async() => {
+		const wrapper = shallowMount(FlagSelector, {
 			"props": {
 				"basePermissionGroup": comment,
 				"flags": 0,
@@ -54,9 +54,49 @@ describe("Component: role/flag_selector", () => {
 		const internalUpdates = wrapper.emitted("update:flags")
 		const expectedFlagValue = comment.generateMask("view")
 		expect(internalUpdates).toHaveProperty("0.0", expectedFlagValue)
-		const externalUpdates = wrapper.emitted("update_dependency_flags")
+		const externalUpdates = wrapper.emitted("updateDependencyFlags")
 		expect(externalUpdates).toHaveProperty("0.0.0.group", post)
 		expect(externalUpdates).toHaveProperty("0.0.0.permissionDependencies", [ "view" ])
+		expect(externalUpdates).not.toHaveProperty("0.0.1")
+	})
+
+	it("should uncheck internal dependent flags", async() => {
+		const wrapper = shallowMount(FlagSelector, {
+			"props": {
+				"basePermissionGroup": post,
+				"flags": post.generateMask("create", "view"),
+				"header": "Post"
+			}
+		})
+
+		const dependentCheckbox = wrapper.find("input[value='view']")
+		await dependentCheckbox.trigger("change")
+
+		const internalUpdates = wrapper.emitted("update:flags")
+		const expectedFlagValue = 0
+		expect(internalUpdates).toHaveLength(1)
+		expect(internalUpdates).toHaveProperty("0.0", expectedFlagValue)
+	})
+
+	it("should uncheck external dependency flags", async() => {
+		const wrapper = shallowMount(FlagSelector, {
+			"props": {
+				"basePermissionGroup": post,
+				"dependentGroups": [ comment ],
+				"flags": post.generateMask("view"),
+				"header": "Comment"
+			}
+		})
+
+		const dependentCheckbox = wrapper.find("input[value='view']")
+		await dependentCheckbox.trigger("change")
+
+		const internalUpdates = wrapper.emitted("update:flags")
+		const expectedFlagValue = 0
+		expect(internalUpdates).toHaveProperty("0.0", expectedFlagValue)
+		const externalUpdates = wrapper.emitted("updateDependencyFlags")
+		expect(externalUpdates).toHaveProperty("0.0.0.group", comment)
+		expect(externalUpdates).toHaveProperty("0.0.0.permissionDependents", [ "view" ])
 		expect(externalUpdates).not.toHaveProperty("0.0.1")
 	})
 })
