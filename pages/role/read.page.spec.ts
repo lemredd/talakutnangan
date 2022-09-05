b12eed3c3511a0b088f94c7cd6eca750c502f2d0
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-05 23:28:54 +0800

    intg(role)!: fix tests for checking if role name has been edited

--- pages/role/read.page.spec.ts
+++ pages/role/read.page.spec.ts
@@ -130,7 +130,7 @@ describe("UI Page: Read resource by ID", () => {
 		expect(castCreateCommentPermissionCheckbox.checked).toBeFalsy()
 	})
 
-	it.skip("can edit role name", async() => {
+	it("can edit role name", async() => {
 		const sampleResource = await new RoleFactory()
 		.departmentFlags(department.generateMask("view"))
 		.roleFlags(role.generateMask("view"))
@@ -164,7 +164,7 @@ describe("UI Page: Read resource by ID", () => {
 			"writeDepartmentScope"
 		))
 		.auditTrailFlags(0)
-		.serializedOne(true)
+		.deserializedOne(true)
 		const newSampleModel = await new RoleFactory().makeOne()
 
 		fetchMock.mockResponseOnce("{}", { "status": RequestEnvironment.status.NO_CONTENT })
@@ -181,28 +181,32 @@ describe("UI Page: Read resource by ID", () => {
 			}
 		})
 
-		const roleName = wrapper.find(".role-name input")
+		const roleName = wrapper.find(".role-name input[type='text']")
 		const submit = wrapper.find("[type='submit']")
 
 		await roleName.setValue(newSampleModel.name)
-		await submit.setValue("submit")
+		await submit.trigger("submit")
+		await wrapper.vm.$nextTick()
 		await flushPromises()
 
 		// TODO?: test update submission and expect new role name to successfully push in database
 		const castFetch = fetch as jest.Mock<any, any>
 		const [ [ request ] ] = castFetch.mock.calls
-		expect(request).toHaveProperty("method", "POST")
-		expect(request).toHaveProperty("url", "/api/user")
+		expect(request).toHaveProperty("method", "PATCH")
+		expect(request).toHaveProperty("url", `/api/role/${sampleResource.data.id}`)
 		expect(request.headers.get("Content-Type")).toBe(JSON_API_MEDIA_TYPE)
 		expect(request.headers.get("Accept")).toBe(JSON_API_MEDIA_TYPE)
-		expect(request.json()).resolves.toStrictEqual({
+		const requestBody = await request.json()
+		const { type, id, ...attributes } = sampleResource.data
+		expect(requestBody).toStrictEqual({
 			"data": {
 				"attributes": {
-					...sampleResource.data.attributes,
+					...attributes,
+					"deletedAt": null,
 					"name": newSampleModel.name
 				},
-				"id": sampleResource.data.id,
-				"type": "role"
+				id,
+				type
 			}
 		})
 	})
