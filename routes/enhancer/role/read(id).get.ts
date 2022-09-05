687a53592a12961cd57dbaf0cc4f68f4cbd6c1ce
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-05 12:31:20 +0800

    intrn(server): load the role data immediately

--- routes/enhancer/role/read(id).get.ts
+++ routes/enhancer/role/read(id).get.ts
@@ -1,8 +1,16 @@
+import type { Serializable } from "$/types/general"
+import type { AuthenticatedRequest } from "!/types/dependent"
+
 import Policy from "!/bases/policy"
-import { UPDATE } from "$/permissions/role_combinations"
+import Manager from "%/managers/role"
+import Validation from "!/bases/validation"
+import present from "!/validators/manager/present"
+import IDParameterValidator from "!/validations/id_parameter"
 import PageMiddleware from "!/bases/controller-likes/page_middleware"
-import { role as permissionGroup } from "$/permissions/permission_list"
+
+import { UPDATE } from "$/permissions/role_combinations"
 import PermissionBasedPolicy from "!/policies/permission-based"
+import { role as permissionGroup } from "$/permissions/permission_list"
 
 export default class extends PageMiddleware {
 	get filePath(): string { return __filename }
@@ -11,5 +19,28 @@ export default class extends PageMiddleware {
 		return new PermissionBasedPolicy(permissionGroup, [
 			UPDATE
 		])
+	}
+
+	get validations(): Validation[] {
+		return [
+			new IDParameterValidator([
+				[ "id", Manager, present ]
+			])
+		]
+	}
+
+	async getPageProps(request: AuthenticatedRequest): Promise<Serializable> {
+		const { id } = request.params
+		const manager = new Manager(request.transaction, request.cache)
+
+		const role = await manager.findWithID(Number(id), {
+			"filter": {
+				"existence": "*"
+			}
+		})
+
+		return {
+			role
+		}
 	}
 }
