93649f035a99e9842a6f5dd84bde22359df8b2cd
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-05 23:04:36 +0800

    unit(field): ensure checkbox update when model value updates

--- components/fields/checkbox.spec.ts
+++ components/fields/checkbox.spec.ts
@@ -1,11 +1,11 @@
-import { mount } from "@vue/test-utils"
-import Checkbox from "./checkbox.vue"
+import { shallowMount } from "@vue/test-utils"
+import Component from "./checkbox.vue"
 
 describe("Component: fields/checkbox", () => {
 	it("can update", async() => {
 		const values = [] as string[]
 		const checkboxValue = "view"
-		const wrapper = mount(Checkbox, {
+		const wrapper = shallowMount(Component, {
 			"props": {
 				"label": "View",
 				"modelValue": values,
@@ -20,5 +20,27 @@ describe("Component: fields/checkbox", () => {
 		expect(updates).toHaveLength(1)
 		const updatedValues = updates[0] as string[]
 		expect(updatedValues[0].includes(checkboxValue)).toBeTruthy()
+	})
+
+	it("should check upon prop update", async() => {
+		const values = [ "view", "create" ]
+		const checkboxValue = "create"
+		const wrapper = shallowMount<any>(Component, {
+			"props": {
+				"label": "View",
+				"modelValue": values,
+				"value": checkboxValue
+			}
+		})
+
+		const newValues = [ "view" ]
+		await wrapper.setProps({
+			"label": "View",
+			"modelValue": newValues,
+			"value": checkboxValue
+		})
+
+		const field = wrapper.find("input").element as HTMLInputElement
+		expect(field.checked).toBeFalsy()
 	})
 })
