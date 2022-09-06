672bb46d3e5fcf07718f973a550be343fe4ca236
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-06 22:48:28 +0800

    intg(chat message): correct the fields to expect

--- t/integration/api/chat_message/create_with_file.post.spec.ts
+++ t/integration/api/chat_message/create_with_file.post.spec.ts
@@ -22,24 +22,24 @@ describe("POST /api/chat_message/create_with_file", () => {
 			normalRole,
 			userFactory => userFactory.beReachableEmployee())
 		const chatMessageActivity = await new ChatMessageActivityFactory().insertOne()
-		const model = await new Factory()
+		const resource = await new Factory()
 		.chatMessageActivity(() => Promise.resolve(chatMessageActivity))
 		.serializedOne()
-		const path = `${RequestEnvironment.root}/t/data/valid_student_details.csv`
+		const path = `${RequestEnvironment.root}/t/data/logo_bg_transparent.png`
 
 		const response = await App.request
 		.post("/api/chat_message/create_with_file")
 		.set("Cookie", cookie)
 		.field("data[type]", "chat_message")
-		.field("data[attributes][kind]", model.data.attributes.kind)
-		.field("data[attributes][data]", model.data.attributes.data)
+		.field("data[attributes][kind]", resource.data.attributes.kind)
+		.field("data[attributes][data][value]", resource.data.attributes.data.value)
 		.field("data[relationships][chatMessageActivity][data][type]", "chat_message_activity")
 		.field("data[relationships][chatMessageActivity][data][id]", String(chatMessageActivity.id))
 		.attach("meta[fileContents]", path)
 		.accept(JSON_API_MEDIA_TYPE)
 
 		expect(response.statusCode).toBe(RequestEnvironment.status.CREATED)
-		expect(response.body.data.attributes.data).toStrictEqual(model.data.attributes.data)
+		expect(response.body.data.attributes.data).toStrictEqual(resource.data.attributes.data)
 		const previousCalls = Socket.consumePreviousCalls()
 		expect(previousCalls[0].functionName).toBe("emitToClients")
 		expect(previousCalls[0].arguments).toHaveProperty("eventName", "create")
@@ -49,10 +49,10 @@ describe("POST /api/chat_message/create_with_file", () => {
 		)
 		expect(previousCalls[0].arguments).toHaveProperty(
 			"data.0.data.attributes.data",
-			model.data.attributes.data
+			resource.data.attributes.data
 		)
 		expect(previousCalls[0].arguments).toHaveProperty(
-			"included.0.data.type",
+			"data.0.included.0.type",
 			"attached_chat_file"
 		)
 	})
