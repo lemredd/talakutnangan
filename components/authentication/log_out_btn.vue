e4922b9c5b221ef8135f36cc45408bbc3534e0b6
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-06 15:15:55 +0800

    refactor(component): use external function for better testing

--- components/authentication/log_out_btn.vue
+++ components/authentication/log_out_btn.vue
@@ -1,23 +1,26 @@
 <template>
 	<Anchor
 		id="log-out-btn"
+		:href="null"
 		role="button"
-		@click="logOut">
+		@click.prevent="logOut">
 		<span class="material-icons">logout</span>
 		<span class="label btn-label">Logout</span>
 	</Anchor>
 </template>
 
 <script setup lang="ts">
-import Anchor from "@/anchor.vue"
 import UserFetcher from "$@/fetchers/user"
+import assignPath from "$@/external/assign_path"
+
+import Anchor from "@/anchor.vue"
 
 UserFetcher.initialize("/api")
 
 function logOut() {
 	new UserFetcher().logOut()
 	.then(() => {
-		window.location.assign("/user/log_in")
+		assignPath("/user/log_in")
 	})
 }
 </script>
