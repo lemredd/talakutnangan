2fc403da4809f73a5af0fb5b5efce61ff49363ce
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-07 14:48:33 +0800

    unit(database): ensure conversion can handle empty hours

--- database/managers/helpers/convert_minutes_to_time_object.spec.ts
+++ database/managers/helpers/convert_minutes_to_time_object.spec.ts
@@ -1,13 +1,24 @@
 import convertMinutesToTimeObject from "./convert_minutes_to_time_object"
 
 describe("Database: Convert minutes to time object", () => {
-	it("can convert minutes to time object", () => {
+	it("can convert hours to time object", () => {
 		const ONE_HOUR_AND_THIRTY_MINUTES = 90
 
 		const time = convertMinutesToTimeObject(ONE_HOUR_AND_THIRTY_MINUTES)
 
 		expect(time).toStrictEqual({
 			"hours": 1,
+			"minutes": 30
+		})
+	})
+
+	it("can convert minutes to time object", () => {
+		const THIRTY_MINUTES = 30
+
+		const time = convertMinutesToTimeObject(THIRTY_MINUTES)
+
+		expect(time).toStrictEqual({
+			"hours": 0,
 			"minutes": 30
 		})
 	})
