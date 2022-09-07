d0a3167b375c0af7e32910e4f7a928b1259c8566
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-07 15:21:31 +0800

    intrn(database): store the source type for future reference

--- database/data_source/database.ts
+++ database/data_source/database.ts
@@ -4,9 +4,11 @@ import createSource from "%/data_source/create_source"
 
 export default class Database {
 	private static source: Sequelize
+	private static sourceType: SourceType
 
 	static async initialize(sourceType: SourceType) {
 		this.source = await createSource(sourceType)
+		this.sourceType = sourceType
 	}
 
 	static get dataSource(): Sequelize {
