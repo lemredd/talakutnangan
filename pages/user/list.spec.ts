c5d1823cc815ac078be2f3a1bce2340a7ee9e018
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-09-08 16:35:30 +0800

    unit(page): ensure header for non-admin users

--- pages/user/list.spec.ts
+++ pages/user/list.spec.ts
@@ -9,28 +9,32 @@ import RoleFactory from "~/factories/role"
 import UserFactory from "~/factories/user"
 import DepartmentFactory from "~/factories/department"
 
-
 import RequestEnvironment from "$/helpers/request_environment"
 import { user as permissionGroup } from "$/permissions/permission_list"
-import { READ_ANYONE_ON_ALL_DEPARTMENTS } from "$/permissions/user_combinations"
+import {
+	READ_ANYONE_ON_ALL_DEPARTMENTS,
+	READ_ANYONE_ON_OWN_DEPARTMENT
+} from "$/permissions/user_combinations"
 
 import Page from "./list.page.vue"
 
 describe("Page: user/list", () => {
 	describe("Content header", () => {
-		it("Should be display correct header for admin", async() => {
+		it("Should display correct header for admin", async() => {
 			const department = await new DepartmentFactory().mayAdmit()
 			.insertOne()
-			const deanRole = await new RoleFactory()
+			const role = await new RoleFactory()
 			.userFlags(permissionGroup.generateMask(...READ_ANYONE_ON_ALL_DEPARTMENTS))
 			.insertOne()
 			const user = await new UserFactory().in(department)
-			.attach(deanRole)
+			.attach(role)
 			.deserializedOne()
 			const userProfile = user as DeserializedUserProfile
 
 			fetchMock.mockResponse(
-				JSON.stringify([ userProfile.data ]),
+				JSON.stringify({
+					"data": [ userProfile.data ]
+				}),
 				{ "status": RequestEnvironment.status.OK }
 			)
 			const wrapper = mount(Page, {
@@ -52,6 +56,43 @@ describe("Page: user/list", () => {
 			const header = wrapper.find("header h1")
 
 			expect(header.text()).toContain("Admin")
+		})
+		it("Should display correct header for department limited resource manager", async() => {
+			const department = await new DepartmentFactory().mayAdmit()
+			.insertOne()
+			const role = await new RoleFactory()
+			.userFlags(permissionGroup.generateMask(...READ_ANYONE_ON_OWN_DEPARTMENT))
+			.insertOne()
+			const user = await new UserFactory().in(department)
+			.attach(role)
+			.deserializedOne()
+			const userProfile = user as DeserializedUserProfile
+
+			fetchMock.mockResponse(
+				JSON.stringify({
+					"data": [ userProfile.data ]
+				}),
+				{ "status": RequestEnvironment.status.OK }
+			)
+			const wrapper = mount(Page, {
+				"global": {
+					"provide": {
+						"pageContext": {
+							"pageProps": {
+								userProfile
+							}
+						},
+						"managerKind": new Manager(userProfile)
+					},
+					"stubs": {
+						"AdminConfigHeader": false
+					}
+				},
+				"shallow": true
+			})
+			const header = wrapper.find("h1")
+
+			expect(header.text()).toContain(department.fullName)
 		})
 	})
 })
