1ea8299031f08891005b70da7a4966aa874aa255
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-09-08 16:13:51 +0800

    unit(page): ensure page content header for `admin`

--- pages/user/list.spec.ts
+++ pages/user/list.spec.ts
@@ -8,40 +8,50 @@ import RoleFactory from "~/factories/role"
 import UserFactory from "~/factories/user"
 import DepartmentFactory from "~/factories/department"
 
+
+import RequestEnvironment from "$/helpers/request_environment"
 import { user as permissionGroup } from "$/permissions/permission_list"
 import { READ_ANYONE_ON_ALL_DEPARTMENTS } from "$/permissions/user_combinations"
 
 import Page from "./list.page.vue"
 
 describe("Page: user/list", () => {
-	it("should be admin (for the lack of better term, this will be changed)", async() => {
-		const department = await new DepartmentFactory().mayAdmit()
-		.insertOne()
-		const deanRole = await new RoleFactory()
-		.userFlags(permissionGroup.generateMask(...READ_ANYONE_ON_ALL_DEPARTMENTS))
-		.insertOne()
-		const user = await new UserFactory().in(department)
-		.attach(deanRole)
-		.deserializedOne()
-		const userProfile = user as DeserializedUserProfile
+	describe("Content header", () => {
+		it("Should be display correct header for admin", async() => {
+			const department = await new DepartmentFactory().mayAdmit()
+			.insertOne()
+			const deanRole = await new RoleFactory()
+			.userFlags(permissionGroup.generateMask(...READ_ANYONE_ON_ALL_DEPARTMENTS))
+			.insertOne()
+			const user = await new UserFactory().in(department)
+			.attach(deanRole)
+			.deserializedOne()
+			const userProfile = user as DeserializedUserProfile
 
-		const wrapper = mount(Page, {
-			"global": {
-				"provide": {
-					"pageContext": {
-						"pageProps": {
-							userProfile
-						}
+			fetchMock.mockResponse(
+				JSON.stringify([ userProfile.data ]),
+				{ "status": RequestEnvironment.status.OK }
+			)
+
+			const wrapper = mount(Page, {
+				"global": {
+					"provide": {
+						"pageContext": {
+							"pageProps": {
+								userProfile
+							}
+						},
+						"managerKind": new Manager(userProfile)
 					},
-					"managerKind": new Manager(userProfile)
+					"stubs": {
+						"AdminConfigHeader": false
+					}
 				},
-				"stubs": {
-					"AdminConfigHeader": false
-				}
-			},
-			"shallow": true
-		})
+				"shallow": true
+			})
+			const header = wrapper.find("header h1")
 
-		console.log(wrapper.html())
+			expect(header.text()).toContain("Admin")
+		})
 	})
 })
