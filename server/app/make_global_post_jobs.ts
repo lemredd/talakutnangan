e6645fa6ebee5f57ccf1cb18252019d1e7eebfab
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-09 20:33:44 +0800

    refactor(server): separate middleware makers

--- server/app/make_global_post_jobs.ts
+++ server/app/make_global_post_jobs.ts
@@ -2,6 +2,7 @@ import Middleware from "!/bases/middleware"
 import CacheDestroyer from "!/middlewares/miscellaneous/cache_destroyer"
 import TransactionCommitter from "!/middlewares/miscellaneous/transaction_committer"
 
+// eslint-disable-next-line require-await
 export default async function(): Promise<Middleware[]> {
 	return [
 		new TransactionCommitter(),
