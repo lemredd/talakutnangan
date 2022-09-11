2094354ac7c3fd5e7998c6001624ee5663bd7a56
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-11 13:57:46 +0800

    fix(chat message): use triple bound JSON controller to validate owners

--- routes/api/chat_message/update(id).patch.ts
+++ routes/api/chat_message/update(id).patch.ts
@@ -1,5 +1,5 @@
-import type { FieldRules } from "!/types/validation"
 import type { Request, Response } from "!/types/dependent"
+import type { FieldRules } from "!/types/validation"
 import type { BaseManagerClass } from "!/types/independent"
 import type { ChatMessageActivityDocument } from "$/types/documents/chat_message_activity"
 
@@ -7,7 +7,7 @@ import Socket from "!/ws/socket"
 import Manager from "%/managers/chat_message"
 import { chatMessageKind } from "!/constants/regex"
 import NoContentResponseInfo from "!/response_infos/no_content"
-import DoubleBoundJSONController from "!/controllers/double_bound_json"
+import TripleBoundJSONController from "!/controllers/triple_bound_json"
 import ChatMessageActivityManager from "%/managers/chat_message_activity"
 import makeConsultationChatNamespace from "$/namespace_makers/consultation_chat"
 
@@ -20,7 +20,7 @@ import required from "!/validators/base/required"
 import anyObject from "!/validators/base/any_object"
 import makeResourceDocumentRules from "!/rule_sets/make_resource_document"
 
-export default class extends DoubleBoundJSONController {
+export default class extends TripleBoundJSONController {
 	get filePath(): string { return __filename }
 
 	get policy(): Policy {
@@ -41,7 +41,6 @@ export default class extends DoubleBoundJSONController {
 				"pipes": [ required, string, regex ]
 			}
 		}
-
 
 		return makeResourceDocumentRules("chat_message", attributes)
 	}
