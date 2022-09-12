f12b87ac00984f8dc6fa31311a0104fa40e9f354
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-12 12:34:05 +0800

    unit(chat message): prepare tests to make route to list chat messages

--- routes/api/chat_message/list.get.spec.ts
+++ routes/api/chat_message/list.get.spec.ts
@@ -1,11 +1,15 @@
 import ErrorBag from "$!/errors/error_bag"
+import UserFactory from "~/factories/user"
+import Factory from "~/factories/chat_message"
 import MockRequester from "~/set-ups/mock_requester"
+import ConsultationFactory from "~/factories/consultation"
+import ChatMessageActivityFactory from "~/factories/chat_message_activity"
 
 import Controller from "./list.get"
 
 const QUERY_VALIDATION_INDEX = 0
 
-describe("Controller: GET /api/audit_trail", () => {
+describe("Controller: GET /api/chat_message", () => {
 	const requester = new MockRequester()
 
 	it("can accept valid info", async() => {
@@ -13,12 +17,24 @@ describe("Controller: GET /api/audit_trail", () => {
 		const { validations } = controller
 		const queryValidation = validations[QUERY_VALIDATION_INDEX]
 		const queryValidationFunction = queryValidation.intermediate.bind(queryValidation)
+		const userFactory = new UserFactory()
+		const user = await userFactory.insertOne()
+		const consultation = await new ConsultationFactory().insertOne()
+		const activity = await new ChatMessageActivityFactory()
+		.consultation(() => Promise.resolve(consultation))
+		.user(() => Promise.resolve(user))
+		.insertOne()
+		await new Factory()
+		.chatMessageActivity(() => Promise.resolve(activity))
+		.insertOne()
 		requester.customizeRequest({
 			"query": {
 				"filter": {
+					"consultationIDs": [ String(consultation.id) ],
 					"existence": "*"
 				}
-			}
+			},
+			"user": userFactory.serialize(user)
 		})
 
 		await requester.runMiddleware(queryValidationFunction)
@@ -31,18 +47,22 @@ describe("Controller: GET /api/audit_trail", () => {
 		const { validations } = controller
 		const queryValidation = validations[QUERY_VALIDATION_INDEX]
 		const queryValidationFunction = queryValidation.intermediate.bind(queryValidation)
+		const userFactory = new UserFactory()
+		const user = await userFactory.insertOne()
 		requester.customizeRequest({
 			"query": {
 				"filter": {
 					"existence": "hello"
 				}
-			}
+			},
+			"user": userFactory.serialize(user)
 		})
 
 		await requester.runMiddleware(queryValidationFunction)
 
 		const body = requester.expectFailure(ErrorBag).toJSON()
 		expect(body).toHaveLength(1)
-		expect(body).toHaveProperty("0.source.parameter", "filter.existence")
+		expect(body).toHaveProperty("0.source.parameter", "filter.consultationIDs")
+		expect(body).toHaveProperty("1.source.parameter", "filter.existence")
 	})
 })
