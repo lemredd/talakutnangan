ff2dc167531a6b5d6b2b54fe6d91d5bf8b990b83
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-12 23:04:44 +0800

    types(back-end): make types for shared manager state

--- common_back-end/types/dependent.ts
+++ common_back-end/types/dependent.ts
@@ -1,5 +1,17 @@
 import { Transaction } from "sequelize"
 
+import CacheClient from "$!/helpers/cache_client"
+
 export interface TransactionObject { transaction?: Transaction }
 
 export interface LockedTransactionObject extends TransactionObject { lock?: boolean }
+
+export interface TransactionManagerInterface {
+	transactionObject: TransactionObject
+	lockedTransactionObject: LockedTransactionObject
+}
+
+export interface SharedManagerState {
+	client: CacheClient,
+	transaction: TransactionManagerInterface
+}
