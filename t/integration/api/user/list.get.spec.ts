65d8bc44f39f976aecdec1e94bb7a38c5b6de9b4
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-13 16:51:25 +0800

    intg(chat message): make integration test to ensure listing of messages

--- t/integration/api/user/list.get.spec.ts
+++ t/integration/api/user/list.get.spec.ts
@@ -10,23 +10,23 @@ import { READ_ANYONE_ON_ALL_DEPARTMENTS } from "$/permissions/user_combinations"
 import Route from "!%/api/user/list.get"
 
 describe("GET /api/user", () => {
-	beforeAll(async () => {
+	beforeAll(async() => {
 		await App.create(new Route())
 	})
 
-	it("can be accessed by permitted user and get single user", async () => {
+	it("can be accessed by permitted user and get single user", async() => {
 		const adminRole = await new RoleFactory()
-			.userFlags(permissionGroup.generateMask(...READ_ANYONE_ON_ALL_DEPARTMENTS))
-			.insertOne()
-		const { user: admin, cookie } = await App.makeAuthenticatedCookie(adminRole)
+		.userFlags(permissionGroup.generateMask(...READ_ANYONE_ON_ALL_DEPARTMENTS))
+		.insertOne()
+		const { "user": admin, cookie } = await App.makeAuthenticatedCookie(adminRole)
 		const student = await new UserFactory().beStudent().insertOne()
 
 		const response = await App.request
-			.get("/api/user")
-			.query({ filter: { existence: "exists" } })
-			.set("Cookie", cookie)
-			.type(JSON_API_MEDIA_TYPE)
-			.accept(JSON_API_MEDIA_TYPE)
+		.get("/api/user")
+		.query({ "filter": { "existence": "exists" } })
+		.set("Cookie", cookie)
+		.type(JSON_API_MEDIA_TYPE)
+		.accept(JSON_API_MEDIA_TYPE)
 
 		console.log(response.body)
 		expect(response.statusCode).toBe(RequestEnvironment.status.OK)
@@ -34,20 +34,21 @@ describe("GET /api/user", () => {
 		expect(response.body).toHaveProperty("data.1.attributes.email", student.email)
 	})
 
-	it("can be accessed by permitted user and get no user if empty", async () => {
+	it("can be accessed by permitted user and get no user if empty", async() => {
 		const adminRole = await new RoleFactory()
-			.userFlags(permissionGroup.generateMask(...READ_ANYONE_ON_ALL_DEPARTMENTS))
-			.insertOne()
-		const { user: admin, cookie } = await App.makeAuthenticatedCookie(adminRole)
+		.userFlags(permissionGroup.generateMask(...READ_ANYONE_ON_ALL_DEPARTMENTS))
+		.insertOne()
+		const { cookie } = await App.makeAuthenticatedCookie(adminRole)
 		const student = await new UserFactory().beStudent().insertOne()
 		await student.destroy()
 
 		const response = await App.request
-			.get("/api/user")
-			.query({ filter: { existence: "exists", kind: "student" } })
-			.set("Cookie", cookie)
-			.type(JSON_API_MEDIA_TYPE)
-			.accept(JSON_API_MEDIA_TYPE)
+		.get("/api/user")
+		.query({ "filter": { "existence": "exists",
+			"kind": "student" } })
+		.set("Cookie", cookie)
+		.type(JSON_API_MEDIA_TYPE)
+		.accept(JSON_API_MEDIA_TYPE)
 
 		expect(response.statusCode).toBe(RequestEnvironment.status.OK)
 		expect(response.body).toHaveProperty("data", [])
