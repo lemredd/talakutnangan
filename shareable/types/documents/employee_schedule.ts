f871ab2ac984663a31dba1010c052366996e9211
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-13 20:10:14 +0800

    types(share): make identifier relationship types for employee schedule

--- shareable/types/documents/employee_schedule.ts
+++ shareable/types/documents/employee_schedule.ts
@@ -1,15 +1,20 @@
 import type { Day } from "$/types/database"
 import type { Serializable } from "$/types/general"
-import type { UserIdentifierDocument } from "$/types/documents/user"
+import type { UserIdentifierDocument, DeserializedUserDocument } from "$/types/documents/user"
 import type {
 	Completeness,
 	Format,
 
 	Resource,
-	Relationships,
 	Attributes,
 	ResourceIdentifier,
 	DeserializedResource,
+
+	DeriveRelationships,
+	DeriveRelationshipNames,
+	GeneralRelationshipData,
+	DeriveDeserializedRelationships,
+	PartialOrPickDeserializedRelationship,
 
 	ResourceDocument,
 	ResourceListDocument,
@@ -31,7 +36,22 @@ export interface EmployeeScheduleAttributes<T extends Format = "serialized"> ext
 	dayName: Day
 }
 
-export type EmployeeScheduleRelationships = Relationships<{ "user": UserIdentifierDocument }>
+interface EmployeeScheduleRelationshipData<unusedT extends Completeness = "read">
+extends GeneralRelationshipData {
+	user: {
+		serialized: UserIdentifierDocument,
+		deserialized: DeserializedUserDocument
+	}
+}
+
+export type EmployeeScheduleRelationshipNames
+= DeriveRelationshipNames<EmployeeScheduleRelationshipData>
+
+export type EmployeeScheduleRelationships<T extends Completeness = "read">
+= DeriveRelationships<EmployeeScheduleRelationshipData<T>>
+
+export type DeserializedEmployeeScheduleRelationships<T extends Completeness = "read">
+= DeriveDeserializedRelationships<EmployeeScheduleRelationshipData<T>>
 
 export type EmployeeScheduleResource<T extends Completeness = "read", > = Resource<
 	T,
@@ -41,9 +61,17 @@ export type EmployeeScheduleResource<T extends Completeness = "read", > = Resour
 	T extends "create" ? EmployeeScheduleRelationships : Serializable
 )
 
-export type DeserializedEmployeeScheduleResource = DeserializedResource<
+export type DeserializedEmployeeScheduleResource<
+	T extends EmployeeScheduleRelationshipNames|undefined = undefined
+> = DeserializedResource<
 	EmployeeScheduleResourceIdentifier<"read">,
 	EmployeeScheduleAttributes<"deserialized">
+> & PartialOrPickDeserializedRelationship<
+	EmployeeScheduleRelationshipData<"read">,
+	DeserializedEmployeeScheduleRelationships<"read">,
+	EmployeeScheduleRelationshipNames,
+	T extends EmployeeScheduleRelationshipNames ? true : false,
+	T extends EmployeeScheduleRelationshipNames ? T : EmployeeScheduleRelationshipNames
 >
 
 export type EmployeeScheduleDocument<T extends Completeness = "read"> = ResourceDocument<
@@ -60,16 +88,20 @@ export type EmployeeScheduleListDocument<T extends Completeness = "read"> = Reso
 	EmployeeScheduleResource<T>
 >
 
-export type DeserializedEmployeeScheduleDocument = DeserializedResourceDocument<
+export type DeserializedEmployeeScheduleDocument<
+	T extends EmployeeScheduleRelationshipNames|undefined = undefined
+> = DeserializedResourceDocument<
 	EmployeeScheduleResourceIdentifier<"read">,
 	EmployeeScheduleAttributes<"deserialized">,
-	DeserializedEmployeeScheduleResource
+	DeserializedEmployeeScheduleResource<T>
 >
 
-export type DeserializedEmployeeScheduleListDocument = DeserializedResourceListDocument<
+export type DeserializedEmployeeScheduleListDocument<
+	T extends EmployeeScheduleRelationshipNames|undefined = undefined
+> = DeserializedResourceListDocument<
 	EmployeeScheduleResourceIdentifier<"read">,
 	EmployeeScheduleAttributes<"deserialized">,
-	DeserializedEmployeeScheduleResource
+	DeserializedEmployeeScheduleResource<T>
 >
 
 export type EmployeeScheduleIdentifierDocument
