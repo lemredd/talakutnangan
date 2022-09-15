870e37c14e70fe539390e222d8def50103268b96
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-15 22:07:41 +0800

    intg(attached chat file): prepare test to delete attached chat message

--- t/integration/api/role/archive.delete.spec.ts
+++ t/integration/api/role/archive.delete.spec.ts
@@ -1,8 +1,8 @@
 import { JSON_API_MEDIA_TYPE } from "$/types/server"
 
 import App from "~/set-ups/app"
-import Role from "%/models/role"
-import RoleFactory from "~/factories/role"
+import Model from "%/models/role"
+import Factory from "~/factories/role"
 import RequestEnvironment from "$!/singletons/request_environment"
 import { ARCHIVE_AND_RESTORE } from "$/permissions/role_combinations"
 import { role as permissionGroup } from "$/permissions/permission_list"
@@ -10,29 +10,32 @@ import { role as permissionGroup } from "$/permissions/permission_list"
 import Route from "!%/api/role/archive.delete"
 
 describe("DELETE /api/role", () => {
-	beforeAll(async () => {
+	beforeAll(async() => {
 		await App.create(new Route())
 	})
 
-	it("can be accessed by authenticated user", async () => {
-		const adminRole = await new RoleFactory()
-			.roleFlags(permissionGroup.generateMask(...ARCHIVE_AND_RESTORE))
-			.insertOne()
-		const { user, cookie } = await App.makeAuthenticatedCookie(adminRole)
-		const role = await (new RoleFactory()).insertOne()
+	it("can be accessed by authenticated user", async() => {
+		const adminRole = await new Factory()
+		.roleFlags(permissionGroup.generateMask(...ARCHIVE_AND_RESTORE))
+		.insertOne()
+		const { cookie } = await App.makeAuthenticatedCookie(adminRole)
+		const model = await new Factory().insertOne()
 
 		const response = await App.request
-			.delete("/api/role")
-			.send({
-				data: [
-					{ type: "role", id: String(role.id) }
-				]
-			})
-			.set("Cookie", cookie)
-			.type(JSON_API_MEDIA_TYPE)
-			.accept(JSON_API_MEDIA_TYPE)
+		.delete("/api/role")
+		.send({
+			"data": [
+				{
+					"id": String(model.id),
+					"type": "role"
+				}
+			]
+		})
+		.set("Cookie", cookie)
+		.type(JSON_API_MEDIA_TYPE)
+		.accept(JSON_API_MEDIA_TYPE)
 
 		expect(response.statusCode).toBe(RequestEnvironment.status.NO_CONTENT)
-		expect(await Role.findOne({ where: { id: role.id } })).toBeNull()
+		expect(await Model.findOne({ "where": { "id": model.id } })).toBeNull()
 	})
 })
