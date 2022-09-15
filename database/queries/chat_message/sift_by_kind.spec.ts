82724e4ce434e44d18b7b3f5c53e5ded95a56276
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-15 16:18:34 +0800

    unit(query): prepare the tests for filtering message by kind

---
+++ database/queries/chat_message/sift_by_kind.spec.ts
@@ -0,0 +1,50 @@
+import Model from "%/models/chat_message"
+import Factory from "~/factories/chat_message"
+
+import siftByKind from "./sift_by_kind"
+
+describe("Database Pipe: Sift by kind", () => {
+	it("can find all models", async() => {
+		const model = await new Factory().insertOne()
+
+		const options = siftByKind({}, {
+			"filter": {
+				"chatMessageKind": "*"
+			}
+		})
+		const foundModels = await Model.findAll(options)
+
+		expect(foundModels).toHaveLength(1)
+		expect(foundModels).toHaveProperty("0.id", model.id)
+	})
+
+	it("can find on specific model", async() => {
+		const model = await new Factory().insertOne()
+
+		const options = siftByKind({}, {
+			"filter": {
+				"chatMessageKind": model.kind
+			}
+		})
+		const foundModels = await Model.findAll(options)
+
+		expect(options).toHaveProperty("include.0.where")
+		expect(foundModels).toHaveLength(1)
+		expect(foundModels).toHaveProperty("0.id", model.id)
+	})
+
+	it("cannot find on specific model", async() => {
+		const model = await new Factory().insertOne()
+
+		const options = siftByKind({}, {
+			"filter": {
+				// Repeat the kind name so there will be no match
+				"chatMessageKind": model.kind + model.kind
+			}
+		})
+		const foundModels = await Model.findAll(options)
+
+		expect(options).toHaveProperty("include.0.where")
+		expect(foundModels).toHaveLength(0)
+	})
+})
