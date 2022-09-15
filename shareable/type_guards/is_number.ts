0ac3ad2c818900c9b355d01198229e4d2c71163f
Ardrin Gregorio <kiritogregorio@gmail.com>
2022-09-15 22:20:42 +0800

    refactor(type guards): import loadash dependecy

--- shareable/type_guards/is_number.ts
+++ shareable/type_guards/is_number.ts
@@ -1,3 +1,4 @@
-export default function(value: any): value is number {
-	return typeof value === "number"
+import isNumber from "lodash.isnumber"
+export default function(value: any): value is boolean {
+	return isNumber(value)
 }
