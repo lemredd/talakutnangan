c2933bcee488cc84a0e2fed04fd89286b1b25d59
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-15 16:27:17 +0800

    feat(chat message): update the rule to validate the chat message kinds

--- routes/api/chat_message/list.get.ts
+++ routes/api/chat_message/list.get.ts
@@ -9,10 +9,11 @@ import QueryController from "!/controllers/query"
 
 import CommonMiddlewareList from "!/middlewares/common_middleware_list"
 
-import string from "!/validators/base/string"
 import required from "!/validators/base/required"
 import nullable from "!/validators/base/nullable"
 import makeListRules from "!/rule_sets/make_list"
+import length from "!/validators/comparison/length"
+import stringArray from "!/validators/hybrid/string_array"
 import skipAsterisk from "!/validators/comparison/skip_asterisk"
 import makeMultiIDBasedFilterRules from "!/rule_sets/make_multi-id-based_filter"
 import doesBelongToCurrentUser from "!/validators/manager/does_belong_to_current_user"
@@ -36,9 +37,14 @@ export default class extends QueryController {
 			}),
 			"chatMessageKind": {
 				"constraints": {
+					"length": {
+						// TODO: Find the best length
+						"maximum": 24,
+						"minimum": 1
+					},
 					"nullable": { "defaultValue": "*" }
 				},
-				"pipes": [ nullable, skipAsterisk, string ]
+				"pipes": [ nullable, skipAsterisk, stringArray, length ]
 			}
 		}, {
 			"defaultSortColumn": "-createdAt"
