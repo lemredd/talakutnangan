b1e6e785e1e14c0ee1606d722b02bac3046c5005
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-15 23:01:50 +0800

    unit(attached chat file): update tests to delete attached chat files

--- routes/api/attached_chat_file/destroy.delete.spec.ts
+++ routes/api/attached_chat_file/destroy.delete.spec.ts
@@ -1,6 +1,9 @@
 import ErrorBag from "$!/errors/error_bag"
+import UserFactory from "~/factories/user"
 import MockRequester from "~/set-ups/mock_requester"
 import Factory from "~/factories/attached_chat_file"
+import ChatMessageFactory from "~/factories/chat_message"
+import ChatMessageActivityFactory from "~/factories/chat_message_activity"
 
 import Controller from "./destroy.delete"
 
@@ -14,7 +17,15 @@ describe("Controller: DELETE /api/attached_chat_file", () => {
 		const { validations } = controller
 		const bodyValidation = validations[BODY_VALIDATION_INDEX]
 		const bodyValidationFunction = bodyValidation.intermediate.bind(bodyValidation)
-		const model = await new Factory().insertOne()
+		const userFactory = new UserFactory()
+		const user = await userFactory.insertOne()
+		const chatMessageActivity = await new ChatMessageActivityFactory()
+		.user(() => Promise.resolve(user))
+		.insertOne()
+		const chatMessage = await new ChatMessageFactory()
+		.chatMessageActivity(() => Promise.resolve(chatMessageActivity))
+		.insertOne()
+		const model = await new Factory().chatMessage(() => Promise.resolve(chatMessage)).insertOne()
 		requester.customizeRequest({
 			"body": {
 				"data": [
@@ -23,7 +34,8 @@ describe("Controller: DELETE /api/attached_chat_file", () => {
 						"type": "attached_chat_file"
 					}
 				]
-			}
+			},
+			"user": userFactory.serialize(user)
 		})
 
 		await requester.runMiddleware(bodyValidationFunction)
@@ -31,37 +43,20 @@ describe("Controller: DELETE /api/attached_chat_file", () => {
 		requester.expectSuccess()
 	})
 
-	it("cannot accept already-destroyed resources", async() => {
+	it("cannot accept absent resources", async() => {
 		const controller = new Controller()
 		const { validations } = controller
 		const bodyValidation = validations[BODY_VALIDATION_INDEX]
 		const bodyValidationFunction = bodyValidation.intermediate.bind(bodyValidation)
-		const model = await new Factory().insertOne()
-		await model.destroy({ "force": false })
-		requester.customizeRequest({
-			"body": {
-				"data": [
-					{
-						"id": String(model.id),
-						"type": "attached_chat_file"
-					}
-				]
-			}
-		})
-
-		await requester.runMiddleware(bodyValidationFunction)
-
-		const body = requester.expectFailure(ErrorBag).toJSON()
-		expect(body).toHaveLength(1)
-		expect(body).toHaveProperty("0.source.pointer", "data.0.id")
-	})
-
-	it("cannot delete non-existent resources", async() => {
-		const controller = new Controller()
-		const { validations } = controller
-		const bodyValidation = validations[BODY_VALIDATION_INDEX]
-		const bodyValidationFunction = bodyValidation.intermediate.bind(bodyValidation)
-		const model = await new Factory().insertOne()
+		const userFactory = new UserFactory()
+		const user = await userFactory.insertOne()
+		const chatMessageActivity = await new ChatMessageActivityFactory()
+		.user(() => Promise.resolve(user))
+		.insertOne()
+		const chatMessage = await new ChatMessageFactory()
+		.chatMessageActivity(() => Promise.resolve(chatMessageActivity))
+		.insertOne()
+		const model = await new Factory().chatMessage(() => Promise.resolve(chatMessage)).insertOne()
 		await model.destroy({ "force": true })
 		requester.customizeRequest({
 			"body": {
@@ -71,7 +66,8 @@ describe("Controller: DELETE /api/attached_chat_file", () => {
 						"type": "attached_chat_file"
 					}
 				]
-			}
+			},
+			"user": userFactory.serialize(user)
 		})
 
 		await requester.runMiddleware(bodyValidationFunction)
