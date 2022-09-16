232aa3f26f0a248db381c3fc6845ec6210e1c403
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-16 22:55:52 +0800

    intg(role): ensure user has other roles before archiving target roles

--- t/integration/api/role/archive.delete.spec.ts
+++ t/integration/api/role/archive.delete.spec.ts
@@ -3,6 +3,7 @@ import { JSON_API_MEDIA_TYPE } from "$/types/server"
 import App from "~/set-ups/app"
 import Model from "%/models/role"
 import Factory from "~/factories/role"
+import UserFactory from "~/factories/user"
 import RequestEnvironment from "$!/singletons/request_environment"
 import { ARCHIVE_AND_RESTORE } from "$/permissions/role_combinations"
 import { role as permissionGroup } from "$/permissions/permission_list"
@@ -20,6 +21,8 @@ describe("DELETE /api/role", () => {
 		.insertOne()
 		const { cookie } = await App.makeAuthenticatedCookie(adminRole)
 		const model = await new Factory().insertOne()
+		const otherModel = await new Factory().insertOne()
+		await new UserFactory().attach(model).attach(otherModel).insertOne()
 
 		const response = await App.request
 		.delete("/api/role")
