d05a81d2508bd7297c69ec123b4c37cb0dc59ef4
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-18 03:06:31 +0800

    unit(user): request the password update directly to password field

--- components/settings/change_password_field.spec.ts
+++ components/settings/change_password_field.spec.ts
@@ -1,4 +1,8 @@
-import { shallowMount } from "@vue/test-utils"
+import { nextTick } from "vue"
+import { shallowMount, flushPromises } from "@vue/test-utils"
+
+import RequestEnvironment from "$/singletons/request_environment"
+
 import Component from "./change_password_field.vue"
 
 describe("Component: settings/change_password_field", () => {
@@ -8,6 +12,17 @@ describe("Component: settings/change_password_field", () => {
 		const CONFIRM_NEW_PASSWORD = "!"
 		const wrapper = shallowMount(Component, {
 			"global": {
+				"provide": {
+					"pageContext": {
+						"pageProps": {
+							"user": {
+								"data": {
+									"id": "1"
+								}
+							}
+						}
+					}
+				},
 				"stubs": {
 					"Overlay": false,
 					"SensitiveTextField": false
@@ -37,8 +52,23 @@ describe("Component: settings/change_password_field", () => {
 
 	it("can save user password", async() => {
 		const OLD_PASSWORD = "Hello"
+		const NEW_PASSWORD = "World"
+		const CONFIRM_NEW_PASSWORD = "!"
+		const userID = "1"
+		fetchMock.mockResponseOnce("", { "status": RequestEnvironment.status.NO_CONTENT })
 		const wrapper = shallowMount(Component, {
 			"global": {
+				"provide": {
+					"pageContext": {
+						"pageProps": {
+							"user": {
+								"data": {
+									"id": userID
+								}
+							}
+						}
+					}
+				},
 				"stubs": {
 					"Overlay": false,
 					"SensitiveTextField": false
@@ -52,14 +82,27 @@ describe("Component: settings/change_password_field", () => {
 		})
 		const editButton = wrapper.find(".input-and-controls button")
 		const oldInput = wrapper.find("input:nth-child(1)")
+		const newInput = wrapper.find("input:nth-child(2)")
+		const confirmationInput = wrapper.find("input:nth-child(3)")
 		const saveButton = wrapper.find(".overlay-footer button")
 
 		await editButton.trigger("click")
 		await oldInput.setValue(OLD_PASSWORD)
+		await newInput.setValue(NEW_PASSWORD)
+		await confirmationInput.setValue(CONFIRM_NEW_PASSWORD)
 		await saveButton.trigger("click")
+		await flushPromises()
+		await nextTick()
 
-		const updates = wrapper.emitted()
-		expect(updates).toHaveProperty("savePassword")
-		expect(oldInput.attributes("value")).toBe("")
+		const castFetch = fetch as jest.Mock<any, any>
+		const [ [ firstRequest ] ] = castFetch.mock.calls
+		expect(firstRequest).toHaveProperty("method", "PATCH")
+		expect(firstRequest).toHaveProperty("url", `/api/user/${userID}/update_password`)
+		const firstRequestBody = await firstRequest.json()
+		expect(firstRequestBody).toHaveProperty("data.attributes.password", NEW_PASSWORD)
+		expect(firstRequestBody).toHaveProperty("data.id", userID)
+		expect(firstRequestBody).toHaveProperty("data.type", "user")
+		expect(firstRequestBody).toHaveProperty("meta.confirmPassword", CONFIRM_NEW_PASSWORD)
+		expect(firstRequestBody).toHaveProperty("meta.oldPassword", OLD_PASSWORD)
 	})
 })
