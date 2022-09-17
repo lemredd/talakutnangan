50eb84841395ce5e1a4f2973ae802e71f10e18a9
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-17 14:42:26 +0800

    unit(consultation): prepare tests to start the consultation properly

--- components/consultation/chat_window.spec.ts
+++ components/consultation/chat_window.spec.ts
@@ -2,13 +2,15 @@ import { shallowMount, flushPromises } from "@vue/test-utils"
 import RequestEnvironment from "$/singletons/request_environment"
 import type { DeserializedConsultationResource } from "$/types/documents/consultation"
 import type { DeserializedChatMessageListDocument } from "$/types/documents/chat_message"
+import type {
+	DeserializedChatMessageActivityResource
+} from "$/types/documents/chat_message_activity"
 
 import Component from "./chat_window.vue"
 
 describe("Component: consultation/chat_window", () => {
 	it("should start consultation", async() => {
 		const scheduledStartAt = new Date()
-		// eslint-disable-next-line no-undef
 		fetchMock.mockResponseOnce("", { "status": RequestEnvironment.status.NO_CONTENT })
 		const fakeConsultation = {
 			"actionTaken": null,
@@ -22,10 +24,17 @@ describe("Component: consultation/chat_window", () => {
 		const fakeChatMessage = {
 			"data": []
 		} as DeserializedChatMessageListDocument
+		const fakeChatMessageActivity = {
+			"id": "1",
+			"receivedMessageAt": new Date(),
+			"seenMessageAt": new Date(),
+			"type": "chat_message_activity"
+		} as DeserializedChatMessageActivityResource
 		const wrapper = shallowMount<any>(Component, {
 			"props": {
 				"chatMessages": fakeChatMessage,
-				"consultation": fakeConsultation
+				"consultation": fakeConsultation,
+				"ownChatMessageActivity": fakeChatMessageActivity
 			}
 		})
 
@@ -36,16 +45,27 @@ describe("Component: consultation/chat_window", () => {
 		const events = wrapper.emitted("updatedConsultationAttributes")
 		expect(events).toHaveLength(1)
 		const castFetch = fetch as jest.Mock<any, any>
-		const [ [ request ] ] = castFetch.mock.calls
-		expect(request).toHaveProperty("method", "PATCH")
-		expect(request).toHaveProperty("url", "/api/consultation/1")
-		const body = await request.json()
-		expect(body).toHaveProperty("data.attributes.actionTaken", null)
-		expect(body).toHaveProperty("data.attributes.finishedAt", null)
-		expect(body).toHaveProperty("data.attributes.reason", "")
-		expect(body).toHaveProperty("data.attributes.scheduledStartAt", scheduledStartAt.toJSON())
-		expect(body).not.toHaveProperty("data.attributes.startedAt", null)
-		expect(body).toHaveProperty("data.id", "1")
-		expect(body).toHaveProperty("data.type", "consultation")
+		const [ [ firstRequest ], [ secondRequest ] ] = castFetch.mock.calls
+		expect(firstRequest).toHaveProperty("method", "PATCH")
+		expect(firstRequest).toHaveProperty("url", "/api/consultation/1")
+		const firstRequestBody = await firstRequest.json()
+		expect(firstRequestBody).toHaveProperty("data.attributes.actionTaken", null)
+		expect(firstRequestBody).toHaveProperty("data.attributes.finishedAt", null)
+		expect(firstRequestBody).toHaveProperty("data.attributes.reason", "")
+		expect(firstRequestBody).toHaveProperty(
+			"data.attributes.scheduledStartAt",
+			scheduledStartAt.toJSON()
+		)
+		expect(firstRequestBody).not.toHaveProperty("data.attributes.startedAt", null)
+		expect(firstRequestBody).toHaveProperty("data.id", "1")
+		expect(firstRequestBody).toHaveProperty("data.type", "consultation")
+		expect(secondRequest).toHaveProperty("method", "POST")
+		expect(secondRequest).toHaveProperty("url", "/api/chat_message")
+		const secondRequestBody = await secondRequest.json()
+		expect(secondRequestBody).toHaveProperty("data.attributes.kind", "status")
+		expect(secondRequestBody).toHaveProperty(
+			"data.attributes.relationships.chatMessageActivity.data.",
+			fakeChatMessageActivity.id
+		)
 	})
 })
