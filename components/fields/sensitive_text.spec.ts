a9472f07486ccc9a87863dda68a8505ceac4974d
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-18 02:12:37 +0800

    unit(field): reduce the responsibility of sensitive text field

--- components/fields/sensitive_text.spec.ts
+++ components/fields/sensitive_text.spec.ts
@@ -2,10 +2,28 @@ import { shallowMount } from "@vue/test-utils"
 import Component from "@/fields/sensitive_text.vue"
 
 describe("Component: fields/sensitive_text", () => {
-	it("should verify user password", async() => {
+	it("can update", async() => {
+		const samplePassword = "Hello world!"
 		const wrapper = shallowMount(Component, {
 			"props": {
-				"friendlyFieldName": "password",
+				"label": "Password",
+				"modelValue": "",
+				"required": true
+			}
+		})
+		const field = wrapper.find("input[type=password]")
+
+		await field.setValue(samplePassword)
+
+		const updates = wrapper.emitted("update:modelValue") as string[]
+		expect(updates).toHaveLength(1)
+		expect(updates[0]).toEqual([ samplePassword ])
+	})
+
+	it("should request for edit", async() => {
+		const wrapper = shallowMount(Component, {
+			"props": {
+				"editable": true,
 				"label": "Password",
 				"modelValue": "",
 				"required": true
@@ -13,8 +31,10 @@ describe("Component: fields/sensitive_text", () => {
 		})
 
 		const editButton = wrapper.find("button")
+
 		await editButton.trigger("click")
-		const overlay = wrapper.find("overlay-stub")
-		expect(overlay.html()).toBeTruthy()
+
+		const updates = wrapper.emitted("requestEdit") as string[]
+		expect(updates).toHaveLength(1)
 	})
 })
