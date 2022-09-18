4db2e8200492b281933560f4c6eab3a0d7b180d2
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-18 10:15:31 +0800

    chore(user): remove the props of other passwords
    Update password is direct to the server.

--- components/settings/update_password_field.spec.ts
+++ components/settings/update_password_field.spec.ts
@@ -5,51 +5,7 @@ import RequestEnvironment from "$/singletons/request_environment"
 
 import Component from "./update_password_field.vue"
 
-describe("Component: settings/change_password_field", () => {
-	it("should verify user password", async() => {
-		const OLD_PASSWORD = "Hello"
-		const NEW_PASSWORD = "World"
-		const CONFIRM_NEW_PASSWORD = "!"
-		const wrapper = shallowMount(Component, {
-			"global": {
-				"provide": {
-					"pageContext": {
-						"pageProps": {
-							"user": {
-								"data": {
-									"id": "1"
-								}
-							}
-						}
-					}
-				},
-				"stubs": {
-					"Overlay": false,
-					"SensitiveTextField": false
-				}
-			},
-			"props": {
-				"confirmNewPassword": "",
-				"newPassword": "",
-				"oldPassword": ""
-			}
-		})
-		const editButton = wrapper.find(".input-and-controls button")
-		const oldInput = wrapper.find("input:nth-child(1)")
-		const newInput = wrapper.find("input:nth-child(2)")
-		const confirmationInput = wrapper.find("input:nth-child(3)")
-
-		await editButton.trigger("click")
-		await oldInput.setValue(OLD_PASSWORD)
-		await newInput.setValue(NEW_PASSWORD)
-		await confirmationInput.setValue(CONFIRM_NEW_PASSWORD)
-
-		const updates = wrapper.emitted()
-		expect(updates).toHaveProperty("update:oldPassword.0", [ OLD_PASSWORD ])
-		expect(updates).toHaveProperty("update:newPassword.0", [ NEW_PASSWORD ])
-		expect(updates).toHaveProperty("update:confirmNewPassword.0", [ CONFIRM_NEW_PASSWORD ])
-	})
-
+describe("Component: settings/update_password_field", () => {
 	it("can save user password", async() => {
 		const OLD_PASSWORD = "Hello"
 		const NEW_PASSWORD = "World"
@@ -73,11 +29,6 @@ describe("Component: settings/change_password_field", () => {
 					"Overlay": false,
 					"SensitiveTextField": false
 				}
-			},
-			"props": {
-				"confirmNewPassword": "",
-				"newPassword": "",
-				"oldPassword": ""
 			}
 		})
 		const editButton = wrapper.find(".input-and-controls button")
