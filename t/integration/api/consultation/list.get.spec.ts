f35bbffe9f8c217296f43dfb669626e339bc298d
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-19 08:51:39 +0800

    intg(consultation): correct the test for listing consultation

--- t/integration/api/consultation/list.get.spec.ts
+++ t/integration/api/consultation/list.get.spec.ts
@@ -3,13 +3,13 @@ import { JSON_API_MEDIA_TYPE } from "$/types/server"
 import App from "~/set-ups/app"
 
 import RoleFactory from "~/factories/role"
-import Factory from "~/factories/chat_message"
+import Factory from "~/factories/consultation"
 import RequestEnvironment from "$!/singletons/request_environment"
 import ChatMessageActivityFactory from "~/factories/chat_message_activity"
 
-import Route from "!%/api/chat_message/list.get"
+import Route from "!%/api/consultation/list.get"
 
-describe("GET /api/chat_message", () => {
+describe("GET /api/consultation", () => {
 	beforeAll(async() => {
 		await App.create(new Route())
 	})
@@ -19,28 +19,27 @@ describe("GET /api/chat_message", () => {
 		const { "user": employee, cookie } = await App.makeAuthenticatedCookie(
 			normalRole,
 			userFactory => userFactory.beReachableEmployee())
-		const chatMessageActivity = await new ChatMessageActivityFactory()
+		const model = await new Factory().insertOne()
+		await new ChatMessageActivityFactory()
 		.user(() => Promise.resolve(employee))
+		.consultation(() => Promise.resolve(model))
 		.insertOne()
-		const { consultation } = chatMessageActivity
-		const model = await new Factory()
-		.chatMessageActivity(() => Promise.resolve(chatMessageActivity))
-		.serializedOne(true)
 
 		const response = await App.request
-		.get("/api/chat_message")
+		.get("/api/consultation")
 		.set("Cookie", cookie)
 		.query({
 			"filter": {
-				"consultationIDs": consultation.id,
-				"existence": "exists"
+				"existence": "exists",
+				"user": String(employee.id)
 			}
 		})
 		.type(JSON_API_MEDIA_TYPE)
 		.accept(JSON_API_MEDIA_TYPE)
 
+		console.log(response.body)
 		expect(response.statusCode).toBe(RequestEnvironment.status.OK)
-		expect(response.body).toHaveProperty("data.0.id", model.data.id)
+		expect(response.body).toHaveProperty("data.0.id", model.id)
 		expect(response.body).not.toHaveProperty("data.1")
 	})
 })
