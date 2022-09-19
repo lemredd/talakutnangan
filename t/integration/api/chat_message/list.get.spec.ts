a9046c596d8d8d473a65446209dc6f4329829853
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-19 12:58:26 +0800

    intg(chat message): correct the test to list resource

--- t/integration/api/chat_message/list.get.spec.ts
+++ t/integration/api/chat_message/list.get.spec.ts
@@ -4,6 +4,7 @@ import App from "~/set-ups/app"
 
 import RoleFactory from "~/factories/role"
 import Factory from "~/factories/chat_message"
+import ConsultationFactory from "~/factories/consultation"
 import RequestEnvironment from "$!/singletons/request_environment"
 import ChatMessageActivityFactory from "~/factories/chat_message_activity"
 
@@ -19,20 +20,21 @@ describe("GET /api/chat_message", () => {
 		const { "user": employee, cookie } = await App.makeAuthenticatedCookie(
 			normalRole,
 			userFactory => userFactory.beReachableEmployee())
-		const chatMessageActivity = await new ChatMessageActivityFactory()
+		const consultation = await new ConsultationFactory().insertOne()
+		const activity = await new ChatMessageActivityFactory()
 		.user(() => Promise.resolve(employee))
+		.consultation(() => Promise.resolve(consultation))
 		.insertOne()
-		const { consultation } = chatMessageActivity
 		const model = await new Factory()
-		.chatMessageActivity(() => Promise.resolve(chatMessageActivity))
-		.serializedOne(true)
+		.chatMessageActivity(() => Promise.resolve(activity))
+		.insertOne()
 
 		const response = await App.request
 		.get("/api/chat_message")
 		.set("Cookie", cookie)
 		.query({
 			"filter": {
-				"consultationIDs": consultation.id,
+				"consultationIDs": String(consultation.id),
 				"existence": "exists"
 			}
 		})
@@ -40,7 +42,9 @@ describe("GET /api/chat_message", () => {
 		.accept(JSON_API_MEDIA_TYPE)
 
 		expect(response.statusCode).toBe(RequestEnvironment.status.OK)
-		expect(response.body).toHaveProperty("data.0.id", model.data.id)
+		expect(response.body).toHaveProperty("data.0.id", String(model.id))
+		expect(response.body).toHaveProperty("data.0.type", "chat_message")
+		expect(response.body).toHaveProperty("data.0.kind", model.kind)
 		expect(response.body).not.toHaveProperty("data.1")
 	})
 })
