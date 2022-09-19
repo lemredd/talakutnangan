10b6f91c85fe5365770b689b56fb2f28baefecfe
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-19 13:31:48 +0800

    intg(chat message): add test for getting preview messages

--- t/integration/api/chat_message/list.get.spec.ts
+++ t/integration/api/chat_message/list.get.spec.ts
@@ -44,7 +44,43 @@ describe("GET /api/chat_message", () => {
 		expect(response.statusCode).toBe(RequestEnvironment.status.OK)
 		expect(response.body).toHaveProperty("data.0.id", String(model.id))
 		expect(response.body).toHaveProperty("data.0.type", "chat_message")
-		expect(response.body).toHaveProperty("data.0.kind", model.kind)
+		expect(response.body).toHaveProperty("data.0.attributes.kind", model.kind)
+		expect(response.body).not.toHaveProperty("data.1")
+	})
+
+	it("can be accessed by authenticated user forto get preview messages", async() => {
+		const normalRole = await new RoleFactory().insertOne()
+		const { "user": employee, cookie } = await App.makeAuthenticatedCookie(
+			normalRole,
+			userFactory => userFactory.beReachableEmployee())
+		const consultation = await new ConsultationFactory().insertOne()
+		const activity = await new ChatMessageActivityFactory()
+		.user(() => Promise.resolve(employee))
+		.consultation(() => Promise.resolve(consultation))
+		.insertOne()
+		await new Factory()
+		.chatMessageActivity(() => Promise.resolve(activity))
+		.insertOne()
+		const model = await new Factory()
+		.chatMessageActivity(() => Promise.resolve(activity))
+		.insertOne()
+
+		const response = await App.request
+		.get("/api/chat_message")
+		.set("Cookie", cookie)
+		.query({
+			"filter": {
+				"consultationIDs": String(consultation.id),
+				"previewMessageOnly": true
+			}
+		})
+		.type(JSON_API_MEDIA_TYPE)
+		.accept(JSON_API_MEDIA_TYPE)
+
+		expect(response.statusCode).toBe(RequestEnvironment.status.OK)
+		expect(response.body).toHaveProperty("data.0.id", String(model.id))
+		expect(response.body).toHaveProperty("data.0.type", "chat_message")
+		expect(response.body).toHaveProperty("data.0.attributes.kind", model.kind)
 		expect(response.body).not.toHaveProperty("data.1")
 	})
 })
