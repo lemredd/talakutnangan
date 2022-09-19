162e9eb77c8c8fa41de83d11c7d86b84db5456f2
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-19 14:56:44 +0800

    types(share): add new kind of message

--- shareable/types/message.ts
+++ shareable/types/message.ts
@@ -1,5 +1,6 @@
 import type { Format } from "$/types/documents/base"
 import type { ChatMessageAttributes } from "$/types/documents/chat_message"
+import type { DeserializedAttachedChatFileDocument } from "$/types/documents/attached_chat_file"
 
 /**
  * Shape of text messages.
@@ -22,6 +23,20 @@ export interface StatusMessage<T extends Format = "serialized"> extends ChatMess
 }
 
 /**
+ * Shape of file messages.
+ */
+export interface FileMessage<T extends Format = "serialized"> extends ChatMessageAttributes<T> {
+	kind: "file",
+	data: {
+		subkind: "image"|"video"|"audio"|"unknown"
+		name: string
+	}
+}
+
+/**
  * Union of all kinds of messages.
  */
-export type Message<T extends Format = "serialized"> = TextMessage<T>|StatusMessage<T>
+export type Message<T extends Format = "serialized"> =
+	| TextMessage<T>
+	| StatusMessage<T>
+	| FileMessage<T>
