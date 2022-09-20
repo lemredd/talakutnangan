a5a64b0fdebc91272a85c7ee288bb88fd95ce03c
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-20 15:30:42 +0800

    intrn(front-end): complete the mechanism to add listener

--- common_front-end/helpers/timer_manager.ts
+++ common_front-end/helpers/timer_manager.ts
@@ -1,3 +1,4 @@
+import type { DeserializedConsultationResource } from "$/types/documents/consultation"
 import type {
 	TimerListeners,
 	ConsultationEventNames,
@@ -5,7 +6,7 @@ import type {
 } from "$@/types/dependent"
 
 import RequestEnvironment from "$/singletons/request_environment"
-import type { DeserializedConsultationResource } from "$/types/documents/consultation"
+import calculateMillisecondDifference from "$@/helpers/calculate_millisecond_difference"
 
 /**
  * Manages the timers of different consultations.
@@ -16,11 +17,36 @@ export default class TimerManager extends RequestEnvironment {
 	private static listeners: TimerListeners = []
 
 	static listenConsultationTimeEvent<T extends ConsultationEventNames>(
-		unusedresource: DeserializedConsultationResource,
-		unusedeventName: T,
-		unusedlistener: ConsultationEventListeners[T]
+		resource: DeserializedConsultationResource,
+		eventName: T,
+		listener: ConsultationEventListeners[T]
 	): void {
-		//
+		const resourceID = resource.id
+
+		if (resource.startedAt === null) {
+			throw new Error("Consultation should have started before it can be managed.")
+		}
+
+		let foundIndex = TimerManager.listeners.findIndex(existingListener => {
+			const doesMatchResource = existingListener.consultation.id === resourceID
+			return doesMatchResource
+		})
+
+		if (foundIndex < -1) {
+			const differenceFromNow = calculateMillisecondDifference(new Date(), resource.startedAt)
+			TimerManager.listeners.push({
+				"consultation": resource,
+				"listeners": {
+					"consumedTime": [] as ConsultationEventListeners["consumedTime"][],
+					"finish": [] as ConsultationEventListeners["finish"][]
+				},
+				"remainingMillisecondsBeforeInactivity": differenceFromNow
+			})
+
+			foundIndex = TimerManager.listeners.length
+		}
+
+		TimerManager.listeners[foundIndex].listeners[eventName].push(listener)
 	}
 
 	static nextInterval(): void {
