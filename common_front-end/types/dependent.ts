0a93d38896d24bd8f81d6fa6161b3a5c2f2f2ab0
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-20 13:36:13 +0800

    types(front-end): make into array since consultation to allow iteration

--- common_front-end/types/dependent.ts
+++ common_front-end/types/dependent.ts
@@ -3,10 +3,10 @@ import type { DeserializedConsultationResource } from "$/types/documents/consult
 
 export type SocketListeners = GeneralObject<(...parameters: any[]) => void>
 
-export type TimerListeners = GeneralObject<{
+export type TimerListeners = ({
 	"consultation": DeserializedConsultationResource,
 	"remainingMillisecondsBeforeInactivity": number,
 	"listeners": {
 		"finish": ((consultation: DeserializedConsultationResource) => void)[]
 	}
-}>
+})[]
