3cfb4191efd390c532fcbba6ccd523603de7e5e4
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-09-20 15:52:09 +0800

    intg(page): ensure placement of display name

---
+++ pages/settings/profile.spec.ts
@@ -0,0 +1,50 @@
+import { ref } from "vue"
+import { shallowMount } from "@vue/test-utils"
+
+import type { DeserializedUserProfile } from "$/types/documents/user"
+
+import RoleFactory from "~/factories/role"
+import UserFactory from "~/factories/user"
+import DepartmentFactory from "~/factories/department"
+import ProfilePictureFactory from "~/factories/profile_picture"
+
+import ProfilePictureFetcher from "$@/fetchers/profile_picture"
+import { user as permissionGroup } from "$/permissions/permission_list"
+import { READ_ANYONE_ON_OWN_DEPARTMENT } from "$/permissions/user_combinations"
+
+import Page from "./profile.page.vue"
+
+describe("Page: settings/profile", () => {
+	describe("User data placement", () => {
+		it("should place display name", async() => {
+			const department = await new DepartmentFactory().mayNotAdmit()
+			.insertOne()
+			const role = await new RoleFactory()
+			.userFlags(permissionGroup.generateMask(...READ_ANYONE_ON_OWN_DEPARTMENT))
+			.insertOne()
+			const user = await new UserFactory().in(department)
+			.beUnreachableEmployee()
+			.attach(role)
+			.deserializedOne()
+			const userProfile = user as DeserializedUserProfile<"roles"|"department">
+
+			const wrapper = shallowMount(Page, {
+				"global": {
+					"provide": {
+						"bodyClasses": ref([]),
+						"pageContext": {
+							"pageProps": {
+								userProfile
+							}
+						}
+					}
+				}
+			})
+
+			const displayNameField = wrapper.findComponent({ "name": "TextualField" })
+
+			expect(displayNameField.props().modelValue).toEqual(user.data.name)
+			expect(displayNameField.html()).toContain(user.data.name)
+		})
+	})
+})
