ba367fe6535aeb365ed4cd85899da9ecf4446b2f
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-09-20 19:07:39 +0800

    intg(page): ensure editing of name

--- pages/settings/profile.spec.ts
+++ pages/settings/profile.spec.ts
@@ -47,4 +47,55 @@ describe("Page: settings/profile", () => {
 			expect(displayNameField.html()).toContain(user.data.name)
 		})
 	})
+
+	describe("Editing", () => {
+		it("can edit display name", async() => {
+			const department = await new DepartmentFactory().mayNotAdmit()
+			.insertOne()
+			const role = await new RoleFactory()
+			.userFlags(permissionGroup.generateMask(...READ_ANYONE_ON_OWN_DEPARTMENT))
+			.insertOne()
+			const user = await new UserFactory().in(department)
+			.beUnreachableEmployee()
+			.attach(role)
+			.deserializedOne()
+			const userProfile = user as DeserializedUserProfile<"roles"|"department">
+
+			const wrapper = shallowMount(Page, {
+				"global": {
+					"provide": {
+						"bodyClasses": ref([]),
+						"pageContext": {
+							"pageProps": {
+								userProfile
+							}
+						}
+					},
+					"stubs": {
+						"TextualField": false
+					}
+				}
+			})
+
+			const displayNameField = wrapper.findComponent({ "name": "TextualField" })
+			const displayNameBtn = displayNameField.find("button")
+			const displayNameInput = displayNameField.find("input")
+
+			fetchMock.mockResponseOnce(
+				JSON.stringify({}),
+				{ "status": RequestEnvironment.status.NO_CONTENT }
+			)
+			fetchMock.mockResponseOnce(
+				JSON.stringify({
+					"data": []
+				}),
+				{ "status": RequestEnvironment.status.OK }
+			)
+
+			await displayNameBtn.trigger("click")
+			await displayNameInput.setValue("Something")
+			await displayNameBtn.trigger("click")
+
+			expect(displayNameField.props().modelValue).toEqual("Something")
+		})
 })
