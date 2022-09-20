caa9e97363d8d6aa865311da7501802fabffb649
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-09-20 15:52:37 +0800

    chore: rename component and its test file

--- components/helpers/picture.spec.ts
+++ components/helpers/picture.spec.ts
@@ -1,8 +1,8 @@
 import { shallowMount } from "@vue/test-utils"
 
-import Component from "./profile_picture.vue"
+import Component from "./picture.vue"
 
-describe("Component: helpers/profile_picture_item", () => {
+describe("Component: helpers/picture_item", () => {
 	it("should show if user has profile picture", () => {
 		const sampleURL = "/images/profile.png"
 		const wrapper = shallowMount<any>(Component, {
