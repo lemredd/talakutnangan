9a00a87f8fafe6366387abf775d4570590002875
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-09-21 16:28:49 +0800

    unit(component): ensure file picking emission
    * todo: ensure type of emitted value

--- components/settings/picture_picker.spec.ts
+++ components/settings/picture_picker.spec.ts
@@ -2,6 +2,8 @@ import { shallowMount } from "@vue/test-utils"
 
 import PicturePicker from "./picture_picker.vue"
 
+global.URL.createObjectURL = jest.fn()
+
 describe("Component: Picture Picker", () => {
 	it("should give the proper input ID", () => {
 		const title = "sample-title"
@@ -35,5 +37,24 @@ describe("Component: Picture Picker", () => {
 		const pictureComponent = wrapper.findComponent({ "name": "Picture" })
 
 		expect(pictureComponent).toBeTruthy()
+	})
+
+	it("should emit event after uploading image", async() => {
+		const title = "sample-title"
+		const wrapper = shallowMount<any>(PicturePicker, {
+			"props": {
+				"picture": null,
+				title
+			}
+		})
+
+		const fileInput = wrapper.find(`#input-${title}`)
+
+		await fileInput.setValue("")
+
+		const pickedFileEmission = wrapper.emitted("pickedFile")
+		expect(pickedFileEmission).toBeTruthy()
+		// TODO(lead): ensure type of emitted value
+		// expect(typeof pickedFileEmission[0][0]).toStrictEqual("string")
 	})
 })
