ac842b9e5b040f0b3240308b6dc72c8d7d288cf5
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-09-21 22:18:06 +0800

    intg(page): test read and create `profilePicture`
    Co-authored-by: Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>

--- pages/settings/profile.spec.ts
+++ pages/settings/profile.spec.ts
@@ -1,22 +1,23 @@
+/* eslint-disable vue/sort-keys */
 import { ref } from "vue"
-import { shallowMount } from "@vue/test-utils"
+import { flushPromises, shallowMount } from "@vue/test-utils"
 
 import type { DeserializedUserProfile } from "$/types/documents/user"
+
+import Page from "./profile.page.vue"
 
 import RoleFactory from "~/factories/role"
 import UserFactory from "~/factories/user"
 import DepartmentFactory from "~/factories/department"
-import ProfilePictureFactory from "~/factories/profile_picture"
 
-import ProfilePictureFetcher from "$@/fetchers/profile_picture"
 import { user as permissionGroup } from "$/permissions/permission_list"
 import { READ_ANYONE_ON_OWN_DEPARTMENT } from "$/permissions/user_combinations"
 
-import Page from "./profile.page.vue"
+import Stub from "$/singletons/stub"
 import RequestEnvironment from "$/singletons/request_environment"
 
 describe("Page: settings/profile", () => {
-	describe("User data placement", () => {
+	describe("Reading", () => {
 		it("should place display name", async() => {
 			const department = await new DepartmentFactory().mayNotAdmit()
 			.insertOne()
@@ -46,6 +47,49 @@ describe("Page: settings/profile", () => {
 
 			expect(displayNameField.props().modelValue).toEqual(user.data.name)
 			expect(displayNameField.html()).toContain(user.data.name)
+		})
+		it("can display profile picture", async() => {
+			const sampleURL = "/images/profile.png"
+			const profilePicture = {
+				"data": {
+					"fileContents": sampleURL
+				}
+			}
+			const department = await new DepartmentFactory().mayNotAdmit()
+			.insertOne()
+			const role = await new RoleFactory()
+			.userFlags(permissionGroup.generateMask(...READ_ANYONE_ON_OWN_DEPARTMENT))
+			.insertOne()
+			const user = await new UserFactory().in(department)
+			.beUnreachableEmployee()
+			.attach(role)
+			.deserializedOne(true)
+			const userProfile = user as DeserializedUserProfile<"roles"|"department">
+
+			const wrapper = shallowMount(Page, {
+				"global": {
+					"provide": {
+						"bodyClasses": ref([]),
+						"pageContext": {
+							"pageProps": {
+								"userProfile": {
+									"data": {
+										...userProfile,
+										profilePicture
+									}
+								}
+							}
+						}
+					},
+					"stubs": {
+						"PicturePicker": false,
+						"Picture": false
+					}
+				}
+			})
+			const picture = wrapper.findComponent({ "name": "Picture" })
+
+			expect(picture.attributes().src).toEqual(sampleURL)
 		})
 	})
 
@@ -98,6 +142,68 @@ describe("Page: settings/profile", () => {
 			await displayNameBtn.trigger("click")
 
 			expect(displayNameField.props().modelValue).toEqual("Something")
+		})
+		it("can create profile picture", async() => {
+			const sampleURL = "/images/profile.png"
+			const department = await new DepartmentFactory().mayNotAdmit()
+			.insertOne()
+			const role = await new RoleFactory()
+			.userFlags(permissionGroup.generateMask(...READ_ANYONE_ON_OWN_DEPARTMENT))
+			.insertOne()
+			const user = await new UserFactory().in(department)
+			.beUnreachableEmployee()
+			.attach(role)
+			.deserializedOne(true)
+			const userProfile = user as DeserializedUserProfile<"roles"|"department">
+
+			const wrapper = shallowMount(Page, {
+				"global": {
+					"provide": {
+						"bodyClasses": ref([]),
+						"pageContext": {
+							"pageProps": {
+								userProfile
+							}
+						}
+					},
+					"stubs": {
+						"PicturePicker": false
+					}
+				}
+			})
+			const fileInput = wrapper.find("#input-profile-picture")
+
+			fetchMock.mockResponseOnce(
+				JSON.stringify({
+					"data": {
+						"type": "profile_picture",
+						"id": 1,
+						"attributes": {
+							"fileContents": sampleURL
+						}
+					}
+				}),
+				{ "status": RequestEnvironment.status.CREATED }
+			)
+			fetchMock.mockResponseOnce(
+				JSON.stringify({
+					"data": []
+				}),
+				{ "status": RequestEnvironment.status.OK }
+			)
+			await fileInput.setValue("")
+			await flushPromises()
+
+			const fetcher = fetch as jest.Mock<any, any>
+			const [ [ request ] ] = fetcher.mock.calls
+			expect(request).toHaveProperty(
+				"url",
+				`/api/user/${user.data.id}/relationships/profile_picture`
+			)
+
+			const previousCalls = Stub.consumePreviousCalls()
+			expect(previousCalls).toHaveProperty("0.functionName", "assignPath")
+			expect(previousCalls).toHaveProperty("0.arguments.0", "/settings/profile")
 		})
 		it("can edit dark mode preference", async() => {
 			const department = await new DepartmentFactory().mayNotAdmit()
