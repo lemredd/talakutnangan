98f53216c560901f44d0df21c12853daea56e435
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-23 18:35:12 +0800

    fix(consultation): fix the path to file upload

--- components/consultation/chat_window/user_controller.vue
+++ components/consultation/chat_window/user_controller.vue
@@ -69,7 +69,7 @@ import convertTimeToMilliseconds from "$/time/convert_time_to_milliseconds"
 import ConsultationTimerManager from "$@/helpers/consultation_timer_manager"
 import calculateMillisecondDifference from "$@/helpers/calculate_millisecond_difference"
 
-import FileUpload from "$@/consultation/file_upload.vue"
+import FileUpload from "$@/consultation/user_controller/file_upload.vue"
 
 const currentChatMessageActivity = inject(
 	CHAT_MESSAGE_ACTIVITY
