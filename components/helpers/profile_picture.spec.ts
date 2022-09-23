937bf1d8210c80c34a1baf120611aab5a23adfa0
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-09-23 20:28:18 +0800

    unit(component): correct descriptions

--- components/helpers/profile_picture.spec.ts
+++ components/helpers/profile_picture.spec.ts
@@ -12,7 +12,7 @@ import DepartmentFactory from "~/factories/department"
 
 import Component from "./profile_picture.vue"
 
-describe("Component: helpers/picture_picture", () => {
+describe("Component: helpers/profile_picture", () => {
 	it("should show if user has profile picture", async() => {
 		const sampleURL = "/images/profile.png"
 		const profilePicture = {
@@ -54,7 +54,7 @@ describe("Component: helpers/picture_picture", () => {
 		expect(source).toBe(sampleURL)
 	})
 
-	it("should show if user has profile picture", async() => {
+	it("should not show if user has no profile picture", async() => {
 		const department = await new DepartmentFactory().mayNotAdmit()
 		.insertOne()
 		const role = await new RoleFactory()
