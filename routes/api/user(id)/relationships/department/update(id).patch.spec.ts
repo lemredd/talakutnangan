f6131fc377be402b3c544a6bb96a2a50bca79618
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-26 22:22:08 +0800

    unit(user): expect the ID in route parameters

--- routes/api/user(id)/relationships/department/update(id).patch.spec.ts
+++ routes/api/user(id)/relationships/department/update(id).patch.spec.ts
@@ -15,7 +15,7 @@ describe("Controller: PATCH /api/model/:id/department", () => {
 		const { validations } = controller
 		const bodyValidation = validations[BODY_VALIDATION_INDEX]
 		const bodyValidationFunction = bodyValidation.intermediate.bind(bodyValidation)
-		const model = await new Factory().beStudent().serializedOne()
+		const model = await new Factory().beStudent().insertOne()
 		const department = await new DepartmentFactory().mayAdmit().insertOne()
 		requester.customizeRequest({
 			"body": {
@@ -24,7 +24,9 @@ describe("Controller: PATCH /api/model/:id/department", () => {
 					"type": "department"
 				}
 			},
-			"user": model
+			"params": {
+				"id": String(model.id)
+			}
 		})
 
 		await requester.runMiddleware(bodyValidationFunction)
@@ -37,7 +39,7 @@ describe("Controller: PATCH /api/model/:id/department", () => {
 		const { validations } = controller
 		const bodyValidation = validations[BODY_VALIDATION_INDEX]
 		const bodyValidationFunction = bodyValidation.intermediate.bind(bodyValidation)
-		const model = await new Factory().beStudent().serializedOne()
+		const model = await new Factory().beStudent().insertOne()
 		const department = await new DepartmentFactory().mayNotAdmit().insertOne()
 		requester.customizeRequest({
 			"body": {
@@ -46,13 +48,15 @@ describe("Controller: PATCH /api/model/:id/department", () => {
 					"type": "department"
 				}
 			},
-			"user": model
+			"params": {
+				"id": String(model.id)
+			}
 		})
 
 		await requester.runMiddleware(bodyValidationFunction)
 
 		const body = requester.expectFailure(ErrorBag).toJSON()
 		expect(body).toHaveLength(1)
-		expect(body).toHaveProperty("0.source.pointer", "body.data.id")
+		expect(body).toHaveProperty("0.source.pointer", "data.id")
 	})
 })
