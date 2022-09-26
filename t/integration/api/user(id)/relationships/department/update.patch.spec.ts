4097cd1f1150bcf48f82b51930b6f6e5ab9a02ed
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-26 23:04:03 +0800

    intg(user): ensure route to update department of user works

--- t/integration/api/user(id)/relationships/department/update.patch.spec.ts
+++ t/integration/api/user(id)/relationships/department/update.patch.spec.ts
@@ -3,39 +3,34 @@ import { JSON_API_MEDIA_TYPE } from "$/types/server"
 import App from "~/setups/app"
 import Factory from "~/factories/user"
 import RoleFactory from "~/factories/role"
+import DepartmentFactory from "~/factories/department"
 import RequestEnvironment from "$!/singletons/request_environment"
 
 import { user as permissionGroup } from "$/permissions/permission_list"
 import { UPDATE_ANYONE_ON_ALL_DEPARTMENTS } from "$/permissions/user_combinations"
 
-import Route from "!%/api/user(id)/relationships/role/update.patch"
+import Route from "!%/api/user(id)/relationships/department/update.patch"
 
-describe("PATCH /api/user/:id/relationships/role", () => {
+describe("PATCH /api/user/:id/relationships/department", () => {
 	beforeAll(async() => {
 		await App.create(new Route())
 	})
 
-	it("can update attached roles", async() => {
+	it("can update department", async() => {
 		const adminRole = await new RoleFactory()
 		.userFlags(permissionGroup.generateMask(...UPDATE_ANYONE_ON_ALL_DEPARTMENTS))
 		.insertOne()
 		const { cookie } = await App.makeAuthenticatedCookie(adminRole)
-		const roles = await new RoleFactory().insertMany(3)
-		const model = await new Factory().attach(roles[0]).attach(roles[1]).insertOne()
+		const department = await new DepartmentFactory().insertOne()
+		const model = await new Factory().insertOne()
 
 		const response = await App.request
-		.patch(`/api/user/${model.id}/relationships/role`)
+		.patch(`/api/user/${model.id}/relationships/department`)
 		.send({
-			"data": [
-				{
-					"id": String(roles[1].id),
-					"type": "role"
-				},
-				{
-					"id": String(roles[2].id),
-					"type": "role"
-				}
-			]
+			"data": {
+				"id": String(department.id),
+				"type": "department"
+			}
 		})
 		.set("Cookie", cookie)
 		.type(JSON_API_MEDIA_TYPE)
