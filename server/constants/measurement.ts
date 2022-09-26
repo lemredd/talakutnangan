8f008ad4fcc137feaa8c3d32bab002ea7cdc3d65
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-26 20:58:09 +0800

    intrn(server): reduce the minimum file size

--- server/constants/measurement.ts
+++ server/constants/measurement.ts
@@ -1,5 +1,5 @@
 import convertByteStringToBytes from "!/helpers/data/convert_byte_string_to_bytes"
 
-export const MINIMUM_FILE_SIZE = convertByteStringToBytes("1kB")
+export const MINIMUM_FILE_SIZE = convertByteStringToBytes("100B")
 
 export const MAXIMUM_FILE_SIZE = convertByteStringToBytes("20MB")
