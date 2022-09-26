d2bc2a96d6388305c972f2b7b5ead1d7469e7d3f
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-26 19:18:13 +0800

    intrn(server): make the common file size constant

---
+++ server/constants/measurement.ts
@@ -0,0 +1,5 @@
+import convertByteStringToBytes from "!/helpers/data/convert_byte_string_to_bytes"
+
+export const MINIMUM_FILE_SIZE = convertByteStringToBytes("1kB")
+
+export const MAXIMUM_FILE_SIZE = convertByteStringToBytes("20MB")
