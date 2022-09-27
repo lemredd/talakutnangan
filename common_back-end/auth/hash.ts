5aff7ee2953926346bb1e52d43204d4fb5560295
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-27 16:36:07 +0800

    intrn(migration): make seeder to hash password of admin

--- common_back-end/auth/hash.ts
+++ common_back-end/auth/hash.ts
@@ -1,7 +1,7 @@
 import { genSalt, hash } from "bcryptjs"
 
 export default async function(rawPassword: string): Promise<string> {
-	const salt = await genSalt(+process.env.SECURITY_HASH_SALT_ROUNDS! || 10)
+	const salt = await genSalt(Number(process.env.SECURITY_HASH_SALT_ROUNDS || "10"))
 	const hashedPassword = await hash(rawPassword, salt)
 	return hashedPassword
 }
