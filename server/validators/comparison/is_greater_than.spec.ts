b805334b1e964bd1011bafc8daea03881872f77e
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-29 21:21:04 +0800

    unit(validator): prepare validator to compare if greater than

---
+++ server/validators/comparison/is_greater_than.spec.ts
@@ -0,0 +1,78 @@
+import makeInitialState from "!/validators/make_initial_state"
+import isGreaterThan from "./is_greater_than"
+
+describe("Validator pipe: Is greater than", () => {
+	it("can accept valid input", async() => {
+		const value = Promise.resolve(makeInitialState(5))
+		const constraints = {
+			"field": "hello",
+			"isGreaterThan": {
+				"value": 4
+			},
+			"request": null,
+			"source": null
+		}
+
+		const sanitizeValue = (await isGreaterThan(value, constraints)).value
+
+		expect(sanitizeValue).toEqual(3)
+	})
+
+	it("can accept valid pointer", async() => {
+		const value = Promise.resolve(makeInitialState(3))
+		const constraints = {
+			"field": "hello",
+			"isGreaterThan": {
+				"value": "user.id"
+			},
+			"request": null,
+			"source": {
+				"user": {
+					"id": 1
+				}
+			}
+		}
+
+		const sanitizeValue = (await isGreaterThan(value, constraints)).value
+
+		expect(sanitizeValue).toEqual(3)
+	})
+
+	it("can accept date", async() => {
+		const currentDate = new Date()
+		const value = Promise.resolve(makeInitialState(new Date()))
+		const constraints = {
+			"field": "hello",
+			"isGreaterThan": {
+				"value": "user.id"
+			},
+			"request": null,
+			"source": {
+				"user": {
+					"id": new Date(Date.now() - 1)
+				}
+			}
+		}
+
+		const sanitizeValue = (await isGreaterThan(value, constraints)).value
+
+		expect(sanitizeValue).toEqual(currentDate)
+	})
+
+	it("cannot accept invalid input", () => {
+		const value = Promise.resolve(makeInitialState("foo"))
+		const constraints = {
+			"field": "hello",
+			"isGreaterThan": {
+				"value": "world"
+			},
+			"request": null,
+			"source": null
+		}
+
+		const error = isGreaterThan(value, constraints)
+
+		expect(error).rejects.toHaveProperty("field", "hello")
+		expect(error).rejects.toHaveProperty("messageMaker")
+	})
+})
