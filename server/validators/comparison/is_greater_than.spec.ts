c09d9b9a07c2f5c51adbbe63921e4439bc7f4dd0
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-09-29 22:13:13 +0800

    unit(validator): prepare test to ignore null

--- server/validators/comparison/is_greater_than.spec.ts
+++ server/validators/comparison/is_greater_than.spec.ts
@@ -59,7 +59,51 @@ describe("Validator pipe: Is greater than", () => {
 		expect(sanitizeValue).toEqual(currentDate)
 	})
 
-	it("cannot accept invalid input", () => {
+	it("can ignore pointed null", async() => {
+		const value = Promise.resolve(makeInitialState(3))
+		const constraints = {
+			"field": "hello",
+			"isGreaterThan": {
+				"pointer": "user.id"
+			},
+			"request": null,
+			"source": {
+				"user": {
+					"id": null
+				}
+			}
+		}
+
+		const sanitizeValue = (await isGreaterThan(value, constraints)).value
+
+		expect(sanitizeValue).toEqual(3)
+	})
+
+	it("cannot ignore undefined", async() => {
+		const value = Promise.resolve(makeInitialState(3))
+		const constraints = {
+			"field": "hello",
+			"isGreaterThan": {
+				"pointer": "user.id"
+			},
+			"request": null,
+			"source": {
+				"user": {
+					// eslint-disable-next-line no-undefined
+					"id": undefined
+				}
+			}
+		}
+
+		try {
+			await isGreaterThan(value, constraints)
+		} catch (error) {
+			expect(error).toHaveProperty("field", "hello")
+			expect(error).toHaveProperty("messageMaker")
+		}
+	})
+
+	it("cannot accept invalid input", async() => {
 		const value = Promise.resolve(makeInitialState("foo"))
 		const constraints = {
 			"field": "hello",
@@ -70,9 +114,11 @@ describe("Validator pipe: Is greater than", () => {
 			"source": null
 		}
 
-		const error = isGreaterThan(value, constraints)
-
-		expect(error).rejects.toHaveProperty("field", "hello")
-		expect(error).rejects.toHaveProperty("messageMaker")
+		try {
+			await isGreaterThan(value, constraints)
+		} catch (error) {
+			expect(error).toHaveProperty("field", "hello")
+			expect(error).toHaveProperty("messageMaker")
+		}
 	})
 })
