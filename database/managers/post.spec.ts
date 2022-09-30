855e3f9091c80bf9fe117b4710feb066c2b5a3b9
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-01 01:14:27 +0800

    unit(manager): prepare test manager of post

--- database/managers/post.spec.ts
+++ database/managers/post.spec.ts
@@ -1,8 +1,37 @@
-// import PostManager from "./post"
-// import PostFactory from "~/factories/post"
+import Factory from "~/factories/post"
+import UserFactory from "~/factories/user"
+import AttachedRoleFactory from "~/factories/attached_role"
 
-describe("Database: Department Read Operations", () => {
-	it.todo("can search post with matching query")
+import Manager from "./post"
 
-	it.todo("cannot search post with non-matching query")
+describe("Database Manager: Post read operations", () => {
+	it("can check if model belongs to user", async() => {
+		const manager = new Manager()
+		const user = await new UserFactory().insertOne()
+		const attachedRole = new AttachedRoleFactory().user(() => Promise.resolve(user)).insertOne()
+		const model = await new Factory().posterInfo(() => attachedRole).insertOne()
+
+		const doesBelong = await manager.isModelBelongsTo(model.id, user.id, manager.modelChainToUser)
+
+		expect(doesBelong).toBeTruthy()
+	})
+})
+
+describe("Database Manager: Miscellaneous post operations", () => {
+	it("can get sortable columns", () => {
+		// Include in test to alert in case there are new columns to decide whether to expose or not
+		const manager = new Manager()
+
+		const { sortableColumns } = manager
+
+		expect(sortableColumns).toStrictEqual([
+			"-createdAt",
+			"-contents",
+			"-updatedAt",
+			"-deletedAt",
+			"createdAt",
+			"contents",
+			"deletedAt"
+		])
+	})
 })
