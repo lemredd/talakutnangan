6913ab59bc7f8430e6ba3a568394d7e8c5b19f5f
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-01 00:56:26 +0800

    test(factory): make general factory for text content-like

---
+++ t/helpers/factories/text_content-like.ts
@@ -0,0 +1,35 @@
+import type { Model } from "%/types/dependent"
+import type {
+	ResourceIdentifier,
+	Attributes,
+	DeserializedResource,
+	DeserializedResourceDocument,
+	DeserializedResourceListDocument,
+	Resource,
+	ResourceDocument,
+	ResourceListDocument
+} from "$/types/documents/base"
+
+import { faker } from "@faker-js/faker"
+
+import BaseFactory from "~/factories/base"
+
+export default abstract class TextContentLikeFactory<
+	T extends Model,
+	U extends ResourceIdentifier<"read">,
+	V extends Attributes<"serialized">,
+	W extends Attributes<"deserialized">,
+	X extends Resource<"read", U, V>,
+	Y extends DeserializedResource<U, W>,
+	Z extends ResourceDocument<"read", U, V, X>,
+	A extends ResourceListDocument<"read", U, V, X>,
+	B extends DeserializedResourceDocument<U, W, Y>,
+	C extends DeserializedResourceListDocument<U, W, Y>
+> extends BaseFactory<T, U, V, W, X, Y, Z, A, B, C> {
+	protected contents: () => string = () => faker.lorem.paragraphs(3)
+
+	fileContents(generator: () => string): TextContentLikeFactory<T, U, V, W, X, Y, Z, A, B, C> {
+		this.contents = generator
+		return this
+	}
+}
