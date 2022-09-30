03458efbeb21c4c0ebf0e83e231d442963cea226
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-01 01:46:54 +0800

    intrn(manager): redo the post manager

--- database/managers/post.ts
+++ database/managers/post.ts
@@ -1,10 +1,34 @@
-import type { ModelCtor, FindAndCountOptions } from "%/types/dependent"
-import Post from "%/models/post"
+import type { CommonQueryParameters } from "$/types/query"
+import type { PostAttributes } from "$/types/documents/post"
+import type { Model as BaseModel, ModelCtor } from "%/types/dependent"
+
+import User from "%/models/user"
+import Model from "%/models/post"
 import BaseManager from "%/managers/base"
-import PostTransformer from "%/transformers/post"
+import Transformer from "%/transformers/post"
+import AttachedRole from "%/models/attached_role"
 
-// export default class PostManager extends BaseManager<Post, RawPost> {
-// 	get model(): ModelCtor<Post> { return Post }
+export default class extends BaseManager<
+	Model,
+	PostAttributes<"deserialized">,
+	CommonQueryParameters
+> {
+	get model(): ModelCtor<Model> { return Model }
 
-// 	get transformer(): PostTransformer { return new PostTransformer() }
-// }
+	get transformer(): Transformer { return new Transformer() }
+
+	get exposableColumns(): string[] {
+		const excludedColumns = [ "approvedAt", "attachedRoleID", "deletedAt" ]
+		return super.exposableColumns.filter(columnName => {
+			const isIncluded = !excludedColumns.includes(columnName)
+			return isIncluded
+		})
+	}
+
+	get modelChainToUser(): readonly ModelCtor<BaseModel>[] {
+		return [
+			AttachedRole,
+			User
+		]
+	}
+}
