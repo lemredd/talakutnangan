94670001121b4c2e779149c53cb732366c4bff78
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-01 14:26:44 +0800

    share(constant): make the links of other models

--- shareable/constants/template_links.ts
+++ shareable/constants/template_links.ts
@@ -1,4 +1,5 @@
-import makeTemplateLink from "$/helpers/make_template_link"
+import makeTemplateLink from "$/helpers/MAKE_TEMPLATE_LINK"
+
 /**
  * Use by most of the template links.
  */
@@ -9,4 +10,20 @@ export const LINK_PREFIX = "/api"
  */
 export const UPDATE_PASSWORD_LINK = "/api/user/:id/update_password"
 
+export const USER_LINK = makeTemplateLink(LINK_PREFIX, "user")
+
+export const ROLE_LINK = makeTemplateLink(LINK_PREFIX, "role")
+
+export const DEPARTMENT_LINK = makeTemplateLink(LINK_PREFIX, "department")
+
+export const PROFILE_PICTURE_LINK = makeTemplateLink(LINK_PREFIX, "profile_picture")
+
+export const SIGNATURE_LINK = makeTemplateLink(LINK_PREFIX, "signature")
+
+export const EMPLOYEE_SCHEDULE_LINK = makeTemplateLink(LINK_PREFIX, "employee_schedule")
+
 export const CONSULTATION_LINK = makeTemplateLink(LINK_PREFIX, "consultation")
+
+export const CHAT_MESSAGE_LINK = makeTemplateLink(LINK_PREFIX, "chat_message")
+
+export const CHAT_MESSAGE_ACTIVITY_LINK = makeTemplateLink(LINK_PREFIX, "chat_message_activity")
