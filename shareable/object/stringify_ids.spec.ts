e70309db20a04fc162347f08a1f0c8bba1cc7e6b
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-10-01 19:45:51 +0800

    share(object): make id string caster
    * is also ensured
    
    Co-authored-by: Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>

--- shareable/object/stringify_ids.spec.ts
+++ shareable/object/stringify_ids.spec.ts
@@ -1,25 +1,31 @@
-import helper from "./convert_dates"
+import helper from "./stringify_ids"
 
-describe("Helpers: Convert dates", () => {
-	it("can convert At to dates", () => {
+describe("Helpers: Stringify IDs", () => {
+	it("can convert IDs to string", () => {
 		const value = {
 			"createdAt": "1995-12-17T03:22:00",
 			"deletedAt": "1995-12-17T03:22:00",
 			"hello": {
+				"count": 3,
+				"id": 1,
 				"name": "sample name 2",
 				"subcreatedAt": "1995-12-17T03:19:00",
 				"subdeletedAt": "1995-12-17T03:20:00"
 			},
+			"id": 1,
 			"name": "sample name"
 		}
 		const expectedValue = {
-			"createdAt": new Date("1995-12-17T03:22:00"),
-			"deletedAt": new Date("1995-12-17T03:22:00"),
+			"createdAt": "1995-12-17T03:22:00",
+			"deletedAt": "1995-12-17T03:22:00",
 			"hello": {
+				"count": 3,
+				"id": "1",
 				"name": "sample name 2",
-				"subcreatedAt": new Date("1995-12-17T03:19:00"),
-				"subdeletedAt": new Date("1995-12-17T03:20:00")
+				"subcreatedAt": "1995-12-17T03:19:00",
+				"subdeletedAt": "1995-12-17T03:20:00"
 			},
+			"id": "1",
 			"name": "sample name"
 		}
 
---
+++ shareable/object/stringify_ids.ts
@@ -0,0 +1,20 @@
+import type { GeneralObject } from "$/types/general"
+import isPlainObject from "$/type_guards/is_plain_object"
+
+/**
+ * Converts IDs into string.
+ * @param value Object to convert
+ */
+export default function stringifyIDs(value: GeneralObject): GeneralObject {
+	const objectName = value
+	for (const property in value) {
+		if (Object.hasOwn(value, property)) {
+			if (property === "id") {
+				objectName[property] = String(objectName[property])
+			} else if (isPlainObject(objectName[property])) {
+				objectName[property] = stringifyIDs(objectName[property])
+			}
+		}
+	}
+	return objectName
+}
