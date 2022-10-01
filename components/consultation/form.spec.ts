cbe1dcd996b899791e1cf08863f074ec2af4f55a
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-10-01 14:43:10 +0800

    unit(component): ensure consultant selectable role

--- components/consultation/form.spec.ts
+++ components/consultation/form.spec.ts
@@ -1,5 +1,5 @@
 import { nextTick } from "vue"
-import { shallowMount } from "@vue/test-utils"
+import { shallowMount, flushPromises } from "@vue/test-utils"
 
 import { JSON_API_MEDIA_TYPE } from "$/types/server"
 import type { UserListDocument } from "$/types/documents/user"
@@ -11,9 +11,10 @@ import RequestEnvironment from "$/singletons/request_environment"
 
 import Component from "./form.vue"
 
+jest.useFakeTimers()
+
 describe("Component: consultation/form", () => {
 	it("can search students", async() => {
-		jest.useFakeTimers()
 		const students = {
 			"data": [
 				{
@@ -134,5 +135,62 @@ describe("Component: consultation/form", () => {
 		}`)
 		expect(firstRequest.headers.get("Content-Type")).toBe(JSON_API_MEDIA_TYPE)
 		expect(firstRequest.headers.get("Accept")).toBe(JSON_API_MEDIA_TYPE)
+	})
+	it("can select employee's available roles", async() => {
+		const roles = {
+			"data": [
+				{
+					"id": 1,
+					"name": "role"
+				}
+			]
+		}
+		const employees = {
+			"data": [
+				{
+					"attributes": {
+						"email": "",
+						"kind": "reachable_employee",
+						"name": "Employee A",
+						"prefersDark": true,
+						roles
+					},
+					"id": "2",
+					"type": "user"
+				}
+			]
+		} as UserListDocument
+		fetchMock.mockResponseOnce(
+			JSON.stringify(employees),
+			{ "status": RequestEnvironment.status.OK }
+		)
+
+		const wrapper = shallowMount<any>(Component, {
+			"global": {
+				"stubs": {
+					"Overlay": false,
+					"SearchableChip": false
+				}
+			},
+			"props": {
+				"isShown": true
+			}
+		})
+
+		const consultantBox = wrapper.find(".consultant")
+		const consultantSearchField = consultantBox.findComponent({ "name": "NonSensitiveTextField" })
+		await consultantSearchField.setValue(employees.data[0].attributes.name)
+		jest.advanceTimersByTime(DEBOUNCED_WAIT_DURATION)
+
+		await flushPromises()
+		const employeeChip = wrapper.find(".chip")
+		await employeeChip.trigger("click")
+
+		await nextTick()
+		const selectableConsultantRoles = wrapper.findComponent({ "name": "SelectableOptionsField" })
+		expect(selectableConsultantRoles.exists()).toBeTruthy()
+
+		await selectableConsultantRoles.setValue(roles.data[0].name)
+		expect(selectableConsultantRoles.props("modelValue")).toEqual(roles.data[0].name)
 	})
 })
