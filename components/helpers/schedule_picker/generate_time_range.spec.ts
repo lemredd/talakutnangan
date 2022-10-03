50b4f01b71add28bdf5ff1417e794730d4696c2e
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-10-03 18:12:49 +0800

    intrn(front-end): generate time with limitations

--- components/helpers/schedule_picker/generate_time_range.spec.ts
+++ components/helpers/schedule_picker/generate_time_range.spec.ts
@@ -11,6 +11,15 @@ describe("Helper: Time range generator", () => {
 	})
 
 	it("can generate time range with given limits", () => {
-		const
+		const limitations = {
+			"start": 0,
+			"end": 780
+		}
+		const generatedTimes = helper(limitations)
+		const expectedStartValue = "12:00"
+		const expectedEndValue = "01:00"
+
+		expect(generatedTimes[0]).toEqual(expectedStartValue)
+		expect(generatedTimes[generatedTimes.length - 1]).toEqual(expectedEndValue)
 	})
 })
--- components/helpers/schedule_picker/generate_time_range.ts
+++ components/helpers/schedule_picker/generate_time_range.ts
@@ -1,27 +1,67 @@
 import twoDigits from "@/helpers/schedule_picker/two_digits"
+import getTimePart from "./get_time_part"
 
-type Limitations = {
-	"hour"?: {
-		"start": number
-		"end": number
-	},
-	"minute"?: {
-		"start": number
-		"end": number
-	}
-}
+type Limitations = { "start": number, "end": number }
 
 export default function(limitations?: Limitations) {
-	const hourEnd = 11
-	const minuteEnd = 60
-	const start = 0
 	const time = []
 
-	for (let i = start; i <= hourEnd; i++) {
-		for (let interval = 15, j = start; j < minuteEnd; j += interval) {
-			time.push(`${i === 0 ? hourEnd + 1 : twoDigits(i)}:${twoDigits(j)}`)
+	if (limitations) {
+		const defaultStart = 0
+		const defaultMinuteEnd = 60
+		const defaultHourEnd = 11
+
+		const {
+			"start": givenStartLimitation,
+			"end": givenEndLimitation
+		} = limitations as Limitations
+		const hourStart = limitations
+			? Number(getTimePart(givenStartLimitation, "hour"))
+			: defaultStart
+		const hourEnd = limitations
+			? Number(getTimePart(givenEndLimitation, "hour"))
+			: defaultHourEnd
+		const minuteStart = limitations
+			? Number(getTimePart(givenStartLimitation, "minute"))
+			: defaultStart
+		const minuteEnd = limitations
+			? Number(getTimePart(givenEndLimitation, "minute"))
+			: defaultMinuteEnd
+
+		console.log("hours", hourStart, hourEnd, "\n\n\n\n\n")
+		console.log("minutes", minuteStart, minuteEnd, "\n\n\n\n\n")
+
+		for (let i = hourStart; i <= hourEnd; i++) {
+			const isOnHourStart = i === hourStart
+			const isOnHourEnd = i === hourEnd
+			console.log("conditions", isOnHourStart, isOnHourEnd, "\n\n\n")
+			for (
+				let
+					end = isOnHourEnd ? minuteEnd : defaultMinuteEnd,
+					interval = 15,
+					j = isOnHourStart ? minuteStart : defaultStart;
+				j < end;
+				j += interval
+			) {
+				console.log(
+					"time to push",
+					`${i === 0 ? hourEnd + 1 : twoDigits(i)}:${twoDigits(j)}`,
+					"\n\n\n")
+				time.push(`${i === 0 ? hourEnd + 1 : twoDigits(i)}:${twoDigits(j)}`)
+			}
+		}
+	} else {
+		const hourEnd = 11
+		const minuteEnd = 60
+		const start = 0
+
+		for (let i = start; i <= hourEnd; i++) {
+			for (let interval = 15, j = start; j < minuteEnd; j += interval) {
+				time.push(`${i === 0 ? hourEnd + 1 : twoDigits(i)}:${twoDigits(j)}`)
+			}
 		}
 	}
 
+	console.log("time", time, "\n\n\n")
 	return time
 }
