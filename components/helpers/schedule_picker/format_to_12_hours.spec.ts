d5f21d7cf90dc82961962b4bd21fbdf8436847d1
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-10-03 18:58:21 +0800

    unit(front-end): ensure last hour
    Co-authored-by: Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>

--- components/helpers/schedule_picker/format_to_12_hours.spec.ts
+++ components/helpers/schedule_picker/format_to_12_hours.spec.ts
@@ -17,6 +17,13 @@ describe("Helper: 12 Hour formatter", () => {
 		expect(helper(hour)).toEqual(expectedValue)
 	})
 
+	it("should return 12 for the last hour", () => {
+		const hour = 24
+		const expectedValue = 12
+
+		expect(helper(hour)).toEqual(expectedValue)
+	})
+
 	it("should remain the same if hour is less than or equal to NOON", () => {
 		const hour = 11
 
