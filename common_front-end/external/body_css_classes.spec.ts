5836688220b49b9121be729ea4d305c0981f6625
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-05 14:08:38 +0800

    unit(external): prepare unit test toggling scroll

--- common_front-end/external/body_css_classes.spec.ts
+++ common_front-end/external/body_css_classes.spec.ts
@@ -35,4 +35,56 @@ describe("External: Body CSS Classes", () => {
 		expect(previousCalls).toHaveProperty("1.arguments", [])
 		expect(external.bodyClasses).toStrictEqual([])
 	})
+
+	it("can disable scroll on dark theme", () => {
+		const external = new External([ "dark" ])
+
+		external.scroll(false)
+
+		const previousCalls = Stub.consumePreviousCalls()
+		expect(previousCalls).toHaveProperty("0.functionName", "constructor")
+		expect(previousCalls).toHaveProperty("0.arguments.0", [ "dark" ])
+		expect(previousCalls).toHaveProperty("1.functionName", "scroll")
+		expect(previousCalls).toHaveProperty("1.arguments.0", [ false ])
+		expect(external.bodyClasses).toStrictEqual([ "dark", "unscrollable" ])
+	})
+
+	it("can disable scroll on light theme", () => {
+		const external = new External([])
+
+		external.scroll(false)
+
+		const previousCalls = Stub.consumePreviousCalls()
+		expect(previousCalls).toHaveProperty("0.functionName", "constructor")
+		expect(previousCalls).toHaveProperty("0.arguments.0", [])
+		expect(previousCalls).toHaveProperty("1.functionName", "scroll")
+		expect(previousCalls).toHaveProperty("1.arguments.0", [ false ])
+		expect(external.bodyClasses).toStrictEqual([ "unscrollable" ])
+	})
+
+	it("can enable scroll on dark theme", () => {
+		const external = new External([ "dark", "unscrollable" ])
+
+		external.scroll(true)
+
+		const previousCalls = Stub.consumePreviousCalls()
+		expect(previousCalls).toHaveProperty("0.functionName", "constructor")
+		expect(previousCalls).toHaveProperty("0.arguments.0", [ "dark" ])
+		expect(previousCalls).toHaveProperty("1.functionName", "scroll")
+		expect(previousCalls).toHaveProperty("1.arguments.0", [ true ])
+		expect(external.bodyClasses).toStrictEqual([ "dark" ])
+	})
+
+	it("can enable scroll on light theme", () => {
+		const external = new External([ "unscrollable" ])
+
+		external.scroll(true)
+
+		const previousCalls = Stub.consumePreviousCalls()
+		expect(previousCalls).toHaveProperty("0.functionName", "constructor")
+		expect(previousCalls).toHaveProperty("0.arguments.0", [])
+		expect(previousCalls).toHaveProperty("1.functionName", "scroll")
+		expect(previousCalls).toHaveProperty("1.arguments.0", [ true ])
+		expect(external.bodyClasses).toStrictEqual([])
+	})
 })
