2a37778a2c70b3b3d008b55df707e268eca5bb83
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-10-06 20:37:35 +0800

    fix(consultation): listen on dynamic namespaces
    Co-authored-by: Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>

--- server/ws/register_chat_events.ts
+++ server/ws/register_chat_events.ts
@@ -2,8 +2,6 @@ import Log from "$!/singletons/log"
 import { Server } from "socket.io"
 
 export default function(wsServer: Server) {
-	const messages: string[] = []
-
 	wsServer.on("connection", (socket) => {
 		socket.onAny((event, ...args) => {
 			Log.debug("server", `Listening ${event} with ${JSON.stringify(args)}`)
@@ -11,4 +9,23 @@ export default function(wsServer: Server) {
 
 		Log.debug("server", "catch all handle")
 	})
+
+	function listenOnDynamicNamespace(namespace: RegExp, logMessage: string) {
+		wsServer.of(namespace).on("connection", () => {
+			Log.debug("server", logMessage)
+		})
+	}
+
+	listenOnDynamicNamespace(
+		/^\/consultation\/\d+\/chat$/u,
+		"listening for chat"
+	)
+	listenOnDynamicNamespace(
+		/^\/consultation\/\d+\/self$/u,
+		"listening for consultation in self"
+	)
+	listenOnDynamicNamespace(
+		/^\/consultation\/\d+\/chat_activity$/u,
+		"listening for consultation chat activity"
+	)
 }
