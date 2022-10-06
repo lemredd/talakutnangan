623a22e32502c2f7c48e90fd40a1aecd3ec28e1d
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-10-06 21:05:36 +0800

    fix(consultation): accept null value
    Co-authored-by: Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>

--- components/consultation/listeners/register_chat.ts
+++ components/consultation/listeners/register_chat.ts
@@ -33,13 +33,12 @@ export default function(
 ) {
 	let isWindowShown = false
 	function updateReceivedMessageAt(): void {
-		const lastSeenMessageAt = currentChatMessageActivityResource.value.seenMessageAt as Date
+		const lastSeenMessageAt = currentChatMessageActivityResource.value.seenMessageAt
 		chatMessageActivityFetcher.update(currentChatMessageActivityResource.value.id, {
 			"receivedMessageAt": new Date().toJSON(),
-			"seenMessageAt": lastSeenMessageAt.toJSON()
+			"seenMessageAt": lastSeenMessageAt?.toJSON() ?? null
 		})
 	}
-
 
 	DocumentVisibility.addEventListener(newState => {
 		isWindowShown = newState === "visible"
