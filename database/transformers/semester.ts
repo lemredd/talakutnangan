974cee8b02963cc1502254c2f5f6f0517928e312
Arukane <19201.magtoto.angelo.l@gmail.com>
2022-10-06 17:52:08 +0800

    intrn(database): create transformer for semester

---
+++ database/transformers/semester.ts
@@ -0,0 +1,24 @@
+import type { AttributesObject, TransformerOptions } from "%/types/dependent"
+
+import Semester from "%/models/semester"
+import Transformer from "%/transformers/base"
+import Serializer from "%/transformers/serializer"
+
+export default class extends Transformer<Semester, void> {
+	constructor() {
+		super("semester")
+	}
+
+	transform(model: Semester|Semester[], unusedOptions: TransformerOptions): AttributesObject {
+		const safeObject = Serializer.whitelist(model, [
+			"id",
+			"name",
+			"semesterOrder",
+			"startAt",
+			"endAt",
+			"createdAt"
+		])
+
+		return safeObject
+	}
+}
