dc752fc14cd110aa1ceb182b4f40467a96a60864
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-10-07 23:07:01 +0800

    rfmt(consultation): copy elements from `list`

--- components/consultation/empty_list.vue
+++ components/consultation/empty_list.vue
@@ -1,5 +1,13 @@
 <template>
-	<div class="consultations-list">
+	<div class="consultations-list left">
+		<ConsultationForm :is-shown="isAddingSchedule" @close="toggleAddingSchedule"/>
+
+		<button
+			v-if="isUserAStudent"
+			class="material-icons add-btn"
+			@click="toggleAddingSchedule">
+			add
+		</button>
 		<div class="mx-auto max-w-[max-content] no-consultations">
 			<img class="mx-auto" :src="SadIcon"/>
 			<h2>There are no consultations yet...</h2>
@@ -7,7 +15,70 @@
 	</div>
 </template>
 
+<style scoped lang="scss">
+	.left {
+		@apply dark:bg-dark-700 bg-white;
+		position: fixed;
+		overflow-y: scroll;
+		left: 0; right: 0;
+		z-index: 1000;
+
+		// TODO: find a way to make mixin `useContentBaseHeight` work
+		height: calc(100vh - 56px);
+
+		@screen md {
+			position: relative;
+			width: 40vw;
+			max-width: 360px;
+			@apply flex flex-col;
+		}
+
+		.consultations-list-header {
+			@apply p-3;
+
+			.no-search-bar {
+				@apply flex flex-1;
+
+				h2 { @apply flex-1 uppercase; }
+			}
+
+			.is-searching {
+				@apply flex flex-1;
+
+				.list-search-bar { @apply flex flex-1; }
+			}
+		}
+
+		.add-btn {
+			@apply absolute bottom-5 right-5;
+			@apply rounded-full border border-gray-600 p-3;
+			@apply text-lg;
+		}
+	}
+</style>
+
 <script setup lang="ts">
+import { inject, ref, Ref, computed } from "vue"
+
+import type { PageContext } from "$/types/renderer"
+
+import { BODY_CLASSES } from "$@/constants/provided_keys"
+
+import BodyCSSClasses from "$@/external/body_css_classes"
+
+import ConsultationForm from "@/consultation/form.vue"
+
 import SadIcon from "@assets/sadicon.png"
 
+const pageContext = inject("pageContext") as PageContext<"deserialized">
+
+const isAddingSchedule = ref<boolean>(false)
+const rawBodyClasses = inject(BODY_CLASSES) as Ref<BodyCSSClasses>
+function toggleAddingSchedule() {
+	rawBodyClasses.value.scroll(isAddingSchedule.value)
+	isAddingSchedule.value = !isAddingSchedule.value
+}
+
+const { "pageProps": { userProfile } } = pageContext
+const isUserAStudent = computed(() => userProfile?.data?.kind === "student")
 </script>
