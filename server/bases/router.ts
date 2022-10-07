5ee4caf3f88f8b434ce772531c60bd6ee17aa521
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-07 20:43:41 +0800

    intrn(server base): put some operations in asynchronous mode

--- server/bases/router.ts
+++ server/bases/router.ts
@@ -5,10 +5,21 @@ import ControllerLike from "!/bases/controller-like"
 import RequestEnvironment from "$/singletons/request_environment"
 
 export default abstract class Router extends RequestEnvironment {
+	private asyncRegistrations: Promise<void>[] = []
 	private routes: UsableRoute[] = []
+
+	useControllersAsync(promise: Promise<ControllerLike[]>): void {
+		const asyncRegistration = promise.then(controllers => this.useControllers(controllers))
+		this.asyncRegistrations.push(asyncRegistration)
+	}
 
 	useControllers(controllers: ControllerLike[]): void {
 		controllers.forEach(controller => this.useController(controller))
+	}
+
+	useRoutersAsync(promise: Promise<Router[]>): void {
+		const asyncRegistration = promise.then(routers => this.useRouters(routers))
+		this.asyncRegistrations.push(asyncRegistration)
 	}
 
 	useRouters(routers: Router[]): void {
@@ -17,15 +28,18 @@ export default abstract class Router extends RequestEnvironment {
 
 	useController(controller: ControllerLike): void {
 		const information = controller.routeInformation
-		const handlers = controller.handlers
+		const { handlers } = controller
 		this.routes.push({
-			information,
-			handlers
+			handlers,
+			information
 		})
 	}
 
 	useRouter(router: Router): void {
-		this.routes.push(...router.allUsableRoutes)
+		const asyncRegistration = router.allUsableRoutes.then(allUsableRoutes => {
+			this.routes.push(...allUsableRoutes)
+		})
+		this.asyncRegistrations.push(asyncRegistration)
 	}
 
 	get allRouteInformation(): RouteInformation[] {
@@ -38,7 +52,10 @@ export default abstract class Router extends RequestEnvironment {
 		return allRouteInformation
 	}
 
-	get allUsableRoutes(): UsableRoute[] {
-		return [ ...this.routes ]
+	get allUsableRoutes(): Promise<UsableRoute[]> {
+		return Promise.all(this.asyncRegistrations).then(() => {
+			this.asyncRegistrations = []
+			return [ ...this.routes ]
+		})
 	}
 }
