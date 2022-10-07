73af8888e1f20292f52180f984e838234c418cff
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-10-07 22:03:08 +0800

    intrn(consultation): accept user directly

--- components/consultation/list/profile_picture_item.spec.ts
+++ components/consultation/list/profile_picture_item.spec.ts
@@ -2,19 +2,16 @@ import { shallowMount } from "@vue/test-utils"
 
 import Component from "./profile_picture_item.vue"
 
-
 describe("Component: consultation/list/profile_picture_item", () => {
 	it("should show if user has profile picture", () => {
 		const sampleURL = "/images/profile.png"
 		const wrapper = shallowMount<any>(Component, {
 			"props": {
-				"activity": {
-					"user": {
-						"data": {
-							"profilePicture": {
-								"data": {
-									"fileContents": sampleURL
-								}
+				"user": {
+					"data": {
+						"profilePicture": {
+							"data": {
+								"fileContents": sampleURL
 							}
 						}
 					}
@@ -30,10 +27,8 @@ describe("Component: consultation/list/profile_picture_item", () => {
 	it("should not show if user has profile picture", () => {
 		const wrapper = shallowMount<any>(Component, {
 			"props": {
-				"activity": {
-					"user": {
-						"data": {}
-					}
+				"user": {
+					"data": {}
 				}
 			}
 		})
--- components/consultation/list/profile_picture_item.vue
+++ components/consultation/list/profile_picture_item.vue
@@ -5,20 +5,17 @@
 <script setup lang="ts">
 import { computed } from "vue"
 
-import type {
-	DeserializedChatMessageActivityResource
-} from "$/types/documents/chat_message_activity"
+import { DeserializedUserDocument } from "$/types/documents/user"
 
 import Icon from "@assets/icon.png"
 import hasProfilePicture from "@/helpers/profile_picture/has_uploaded_profile_picture"
 
 const props = defineProps<{
-	activity: DeserializedChatMessageActivityResource<"user">
+	user: DeserializedUserDocument
 }>()
 
-const { user } = props.activity
 const profilePictureURL = computed(() => {
-	const { profilePicture } = user.data
+	const { profilePicture } = props.user.data
 
 	if (hasProfilePicture(profilePicture)) {
 		return profilePicture.data.fileContents
