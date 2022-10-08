54e65dae184602f400da28a7616b304e7c37a6d5
Angelo Magtoto <19201.magtoto.angelo.l@gmail.com>
2022-10-08 21:07:59 +0800

    unit(database): ensure semester manager works

---
+++ database/managers/semester.spec.ts
@@ -0,0 +1,21 @@
+import Manager from "./post"
+
+describe("Database Manager: Miscellaneous semester operations", () => {
+	it("can get sortable columns", () => {
+		// Include in test to alert in case there are new columns to decide whether to expose or not
+		const manager = new Manager()
+
+		const { sortableColumns } = manager
+
+		expect(sortableColumns).toStrictEqual([
+			"-content",
+			"-createdAt",
+			"-id",
+			"-updatedAt",
+			"content",
+			"createdAt",
+			"id",
+			"updatedAt"
+		])
+	})
+})
