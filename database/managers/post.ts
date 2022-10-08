d7deddd85b76051dd583382f0fa4f41ed318b067
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-08 15:03:47 +0800

    intrn(manager): make method to create post with resource

--- database/managers/post.ts
+++ database/managers/post.ts
@@ -1,12 +1,17 @@
+import type { Serializable } from "$/types/general"
 import type { CommonQueryParameters } from "$/types/query"
-import type { PostAttributes } from "$/types/documents/post"
 import type { Model as BaseModel, ModelCtor } from "%/types/dependent"
+import type { PostAttributes, PostResource } from "$/types/documents/post"
 
 import User from "%/models/user"
 import Model from "%/models/post"
+import Log from "$!/singletons/log"
 import BaseManager from "%/managers/base"
+import Department from "%/models/department"
 import Transformer from "%/transformers/post"
+import DatabaseError from "$!/errors/database"
 import AttachedRole from "%/models/attached_role"
+import AttachedRoleManager from "%/managers/attached_role"
 
 export default class extends BaseManager<
 	Model,
@@ -18,7 +23,7 @@ export default class extends BaseManager<
 	get transformer(): Transformer { return new Transformer() }
 
 	get exposableColumns(): string[] {
-		const excludedColumns = [ "approvedAt", "attachedRoleID", "deletedAt" ]
+		const excludedColumns = [ "approvedAt", "attachedRoleID", "departmentID", "deletedAt" ]
 		return super.exposableColumns.filter(columnName => {
 			const isIncluded = !excludedColumns.includes(columnName)
 			return isIncluded
@@ -30,5 +35,64 @@ export default class extends BaseManager<
 			AttachedRole,
 			User
 		]
+	}
+
+	async createUsingResource(
+		details: PostResource<"create">,
+		requesterID: number,
+		transformerOptions: void = {} as unknown as void
+	): Promise<Serializable> {
+		try {
+			const { attributes, relationships } = details
+
+			const {
+				"poster": {
+					"data": {
+						"id": userID
+					}
+				},
+				"posterRole": {
+					"data": {
+						"id": roleID
+					}
+				},
+				"department": {
+					"data": {
+						"id": departmentID
+					}
+				}
+			} = relationships
+
+			const attachedRoleManager = new AttachedRoleManager({
+				"cache": this.cache,
+				"transaction": this.transaction
+			})
+			const attachedRole = await attachedRoleManager.findAttachedRole(
+				Number(userID),
+				Number(roleID)
+			)
+
+			if (attachedRole === null) {
+				throw new DatabaseError("User has incorrect role.")
+			}
+
+			const model = await this.model.create(
+				{
+					...attributes,
+					"attachedRoleID": attachedRole.id,
+					departmentID
+				},
+				this.transaction.transactionObject
+			)
+
+			model.posterInfo = attachedRole
+			model.department = await Department.findByPk(Number(departmentID)) as Department
+
+			Log.success("manager", "done creating a model")
+
+			return this.serialize(model, transformerOptions, this.transformer)
+		} catch (error) {
+			throw this.makeBaseError(error)
+		}
 	}
 }
