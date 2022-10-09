30eaccf9b33f4da9f61716791025daa6a4f2b0c5
Angelo Magtoto <19201.magtoto.angelo.l@gmail.com>
2022-10-09 21:05:54 +0800

    intrn(database): remove both create table on down migration for post and semester

--- database/migrations/20221006032100-create-semester.js
+++ database/migrations/20221006032100-create-semester.js
@@ -47,46 +47,10 @@ module.exports = {
 			}
 		})
 	},
-	async down(queryInterface, Sequelize) {
+	async down(queryInterface, unusedSequelize) {
 		await queryInterface.sequelize.transaction(async transaction => {
 			try {
 				await queryInterface.dropTable("Semesters", { transaction })
-				await queryInterface.createTable("Semesters", {
-					"id": {
-						"allowNull": false,
-						"autoIncrement": true,
-						"primaryKey": true,
-						"type": Sequelize.BIGINT
-					},
-					"name": {
-						"allowNull": false,
-						"type": Sequelize.STRING
-					},
-					"semesterOrders": {
-						"allowNull": false,
-						"type": Sequelize.ENUM([ "first", "second", "third" ])
-					},
-					"startAt": {
-						"allowNull": false,
-						"type": Sequelize.DATE
-					},
-					"endAt": {
-						"allowNull": false,
-						"type": Sequelize.DATE
-					},
-					"createdAt": {
-						"allowNull": false,
-						"type": Sequelize.DATE
-					},
-					"updatedAt": {
-						"allowNull": false,
-						"type": Sequelize.DATE
-					},
-					"deletedAt": {
-						"allowNull": true,
-						"type": Sequelize.DATE
-					}
-				}, { transaction })
 			} catch (err) {
 				await transaction.rollback()
 				throw err
