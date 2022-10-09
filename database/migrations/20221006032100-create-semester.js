37c84c0d2ea9703099b557ac16f38fec216658ee
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-09 23:56:04 +0800

    fix(migration): remove the requirement of dropping the table to exist

--- database/migrations/20221006032100-create-semester.js
+++ database/migrations/20221006032100-create-semester.js
@@ -4,7 +4,6 @@ module.exports = {
 	async up(queryInterface, Sequelize) {
 		await queryInterface.sequelize.transaction(async transaction => {
 			try {
-				await queryInterface.dropTable("Semesters", { transaction })
 				await queryInterface.createTable("Semesters", {
 					"id": {
 						"allowNull": false,
