d5448ec2266091df1e7b06cdb2b7f9d485b0f1ad
Angelo Magtoto <19201.magtoto.angelo.l@gmail.com>
2022-10-09 21:44:29 +0800

    intrn(route): updated semester route

--- routes/api/semester/router.ts
+++ routes/api/semester/router.ts
@@ -1,8 +1,8 @@
 import Router from "!/bases/router"
-import PostCreate from "!%/api/department/create.post"
-import DeleteArchive from "!%/api/department/archive.delete"
-import PatchUpdate from "!%/api/department/update(id).patch"
-import PatchRestore from "!%/api/department/restore.patch"
+import PostCreate from "!%/api/semester/create.post"
+import DeleteArchive from "!%/api/semester/archive.delete"
+import PatchUpdate from "!%/api/semester/update(id).patch"
+import PatchRestore from "!%/api/semester/restore.patch"
 
 export default class extends Router {
 	constructor() {
