eec376aee072ca2e2f25e91fd0be7188f8223edc
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-09 23:57:27 +0800

    fix(migration): correct the column name for semester order

--- database/migrations/20221006032100-create-semester.js
+++ database/migrations/20221006032100-create-semester.js
@@ -15,7 +15,7 @@ module.exports = {
 						"allowNull": false,
 						"type": Sequelize.STRING
 					},
-					"semesterOrders": {
+					"semesterOrder": {
 						"allowNull": false,
 						"type": Sequelize.ENUM([ "first", "second", "third" ])
 					},
