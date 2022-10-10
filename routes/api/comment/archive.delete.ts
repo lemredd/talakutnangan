a483398a191630e11a94aca781ede7d80925f6ac
Angelo Magtoto <19201.magtoto.angelo.l@gmail.com>
2022-10-11 01:09:56 +0800

    intrn(route): update permission policy for comment archive route

--- routes/api/comment/archive.delete.ts
+++ routes/api/comment/archive.delete.ts
@@ -1,12 +1,17 @@
 import type { FieldRules } from "!/types/validation"
 import type { Request, Response } from "!/types/dependent"
 
-import Policy from "!/bases/policy"
 import JSONController from "!/controllers/json"
 import NoContentResponseInfo from "!/response_infos/no_content"
 import CommentActivityManager from "%/managers/comment"
 
-import CommonMiddlewareList from "!/middlewares/common_middleware_list"
+import PermissionBasedPolicy from "!/policies/permission-based"
+import { comment as permissionGroup } from "$/permissions/permission_list"
+import {
+	ARCHIVE_AND_RESTORE_SOCIAL_COMMENT_ON_OWN_DEPARTMENT,
+	ARCHIVE_AND_RESTORE_PUBLIC_COMMENT_ON_ANY_DEPARTMENT,
+	ARCHIVE_AND_RESTORE_PERSONAL_COMMENT_ON_OWN_DEPARTMENT
+} from "$/permissions/comment_combinations"
 
 import exists from "!/validators/manager/exists"
 import makeResourceIdentifierListDocumentRules
@@ -15,8 +20,12 @@ import makeResourceIdentifierListDocumentRules
 export default class extends JSONController {
 	get filePath(): string { return __filename }
 
-	get policy(): Policy {
-		return CommonMiddlewareList.consultationParticipantsOnlyPolicy
+	get policy(): PermissionBasedPolicy<any, any> {
+		return new PermissionBasedPolicy(permissionGroup, [
+			ARCHIVE_AND_RESTORE_SOCIAL_COMMENT_ON_OWN_DEPARTMENT,
+			ARCHIVE_AND_RESTORE_PUBLIC_COMMENT_ON_ANY_DEPARTMENT,
+			ARCHIVE_AND_RESTORE_PERSONAL_COMMENT_ON_OWN_DEPARTMENT
+		])
 	}
 
 	makeBodyRuleGenerator(unusedRequest: Request): FieldRules {
