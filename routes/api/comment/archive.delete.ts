cd6c32fa11bab3ca7b020231b00f6f3794b59f20
Angelo Magtoto <19201.magtoto.angelo.l@gmail.com>
2022-10-10 15:51:50 +0800

    intrn(route): comment archive route

---
+++ routes/api/comment/archive.delete.ts
@@ -0,0 +1,38 @@
+import type { FieldRules } from "!/types/validation"
+import type { Request, Response } from "!/types/dependent"
+
+import Policy from "!/bases/policy"
+import JSONController from "!/controllers/json"
+import NoContentResponseInfo from "!/response_infos/no_content"
+import CommentActivityManager from "%/managers/comment"
+
+import CommonMiddlewareList from "!/middlewares/common_middleware_list"
+
+import exists from "!/validators/manager/exists"
+import makeResourceIdentifierListDocumentRules
+	from "!/rule_sets/make_resource_identifier_list_document"
+
+export default class extends JSONController {
+	get filePath(): string { return __filename }
+
+	get policy(): Policy {
+		return CommonMiddlewareList.consultationParticipantsOnlyPolicy
+	}
+
+	makeBodyRuleGenerator(unusedRequest: Request): FieldRules {
+		return makeResourceIdentifierListDocumentRules(
+			"comment",
+			exists,
+			CommentActivityManager
+		)
+	}
+
+	async handle(request: Request, unusedResponse: Response): Promise<NoContentResponseInfo> {
+		const manager = new CommentActivityManager(request)
+
+		const IDs = request.body.data.map((identifier: { id: number }) => identifier.id)
+		await manager.archiveBatch(IDs)
+
+		return new NoContentResponseInfo()
+	}
+}
