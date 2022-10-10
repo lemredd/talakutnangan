612b01d7ba5b4972ea0bbf692423bc5e42a10115
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-10-10 20:34:04 +0800

    fix(server): fill missing bracket

--- routes/api/post/router.ts
+++ routes/api/post/router.ts
@@ -8,12 +8,17 @@ export default class extends Router {
 	constructor() {
 		super()
 
-		this.useControllersAsync(new Promise(resolve => {
-			resolve([
-			new PostCreate(),
-			new DeleteArchive(),
-			new PatchUpdate(),
-			new PatchRestore()
-		]))
+		this.useControllersAsync(
+			new Promise(
+				resolve => {
+					resolve([
+						new PostCreate(),
+						new DeleteArchive(),
+						new PatchUpdate(),
+						new PatchRestore()
+					])
+				}
+			)
+		)
 	}
 }
