c188e8f1c4b9777916713e52ad55ca018a85d98c
Angelo Magtoto <19201.magtoto.angelo.l@gmail.com>
2022-10-11 18:53:12 +0800

    intrn(route): post list route

---
+++ routes/api/post/list.get.ts
@@ -0,0 +1,42 @@
+import type { FieldRules } from "!/types/validation"
+import type { Request, Response } from "!/types/dependent"
+import type { CommonQueryParameters } from "$/types/query"
+
+import Policy from "!/bases/policy"
+import ListResponse from "!/response_infos/list"
+import PostManager from "%/managers/post"
+import QueryController from "!/controllers/query"
+
+import {
+	READ_ANYONE_ON_ALL_DEPARTMENTS,
+	READ_ANYONE_ON_OWN_DEPARTMENT
+} from "$/permissions/post_combinations"
+
+import PermissionBasedPolicy from "!/policies/permission-based"
+import { post as permissionGroup } from "$/permissions/permission_list"
+
+import makeListRules from "!/rule_sets/make_list"
+
+export default class extends QueryController {
+	get filePath(): string { return __filename }
+
+	get policy(): Policy {
+		return new PermissionBasedPolicy(permissionGroup, [
+			READ_ANYONE_ON_ALL_DEPARTMENTS,
+			READ_ANYONE_ON_OWN_DEPARTMENT
+		])
+	}
+
+	makeQueryRuleGenerator(unusedRequest: Request): FieldRules {
+		return makeListRules(PostManager, {})
+	}
+
+	async handle(request: Request, unusedResponse: Response): Promise<ListResponse> {
+		const constraints = { ...request.query }
+
+		const manager = new PostManager(request)
+		const auditTrails = await manager.list(constraints as CommonQueryParameters)
+
+		return new ListResponse(auditTrails)
+	}
+}
