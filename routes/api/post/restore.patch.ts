6497a78b8664977fafb00d96fbf20eff055ead35
Angelo Magtoto <19201.magtoto.angelo.l@gmail.com>
2022-10-11 11:57:07 +0800

    chore(route): fix the policy of archive and restore for post

--- routes/api/post/restore.patch.ts
+++ routes/api/post/restore.patch.ts
@@ -4,9 +4,15 @@ import type { Request, Response } from "!/types/dependent"
 import Policy from "!/bases/policy"
 import JSONController from "!/controllers/json"
 import NoContentResponseInfo from "!/response_infos/no_content"
-import PostManager from "%/managers/post"
+import Manager from "%/managers/post"
 
-import CommonMiddlewareList from "!/middlewares/common_middleware_list"
+import PermissionBasedPolicy from "!/policies/permission-based"
+import { post as permissionGroup } from "$/permissions/permission_list"
+import {
+	ARCHIVE_AND_RESTORE_SOCIAL_POST_ON_OWN_DEPARTMENT,
+	ARCHIVE_AND_RESTORE_PUBLIC_POST_ON_ANY_DEPARTMENT,
+	ARCHIVE_AND_RESTORE_PERSONAL_POST_ON_OWN_DEPARTMENT
+} from "$/permissions/post_combinations"
 
 import archived from "!/validators/manager/archived"
 import makeResourceIdentifierListDocumentRules
@@ -16,19 +22,23 @@ export default class extends JSONController {
 	get filePath(): string { return __filename }
 
 	get policy(): Policy {
-		return CommonMiddlewareList.consultationParticipantsOnlyPolicy
+		return new PermissionBasedPolicy(permissionGroup, [
+			ARCHIVE_AND_RESTORE_SOCIAL_POST_ON_OWN_DEPARTMENT,
+			ARCHIVE_AND_RESTORE_PUBLIC_POST_ON_ANY_DEPARTMENT,
+			ARCHIVE_AND_RESTORE_PERSONAL_POST_ON_OWN_DEPARTMENT
+		])
 	}
 
 	makeBodyRuleGenerator(unusedRequest: Request): FieldRules {
 		return makeResourceIdentifierListDocumentRules(
 			"post",
 			archived,
-			PostManager
+			Manager
 		)
 	}
 
 	async handle(request: Request, unusedResponse: Response): Promise<NoContentResponseInfo> {
-		const manager = new PostManager(request)
+		const manager = new Manager(request)
 
 		const IDs = request.body.data.map((identifier: { id: number }) => identifier.id)
 		await manager.restoreBatch(IDs)
