ebfe93a4698c6ead9a2c630a8282a979a81ed2f2
Angelo Magtoto <19201.magtoto.angelo.l@gmail.com>
2022-10-11 18:29:17 +0800

    unit(database): ensure comment votes manager works

---
+++ database/managers/comment_vote.spec.ts
@@ -0,0 +1,23 @@
+import Manager from "./comment_vote"
+
+describe("Database Manager: Miscellaneous comment vote operations", () => {
+	it("can get sortable columns", () => {
+		// Include in test to alert in case there are new columns to decide whether to expose or not
+		const manager = new Manager()
+
+		const { sortableColumns } = manager
+
+		expect(sortableColumns).toStrictEqual([
+			"-createdAt",
+			"-deletedAt",
+			"-departmentID",
+			"-id",
+			"-updatedAt",
+			"createdAt",
+			"deletedAt",
+			"departmentID",
+			"id",
+			"updatedAt"
+		])
+	})
+})
