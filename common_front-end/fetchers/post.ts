7cbe4ef6fe30e26d1705dd8a25ba09b08b1c5695
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-11 18:29:40 +0800

    intrn(fetcher): remove unnecessary method for post fetcher

--- common_front-end/fetchers/post.ts
+++ common_front-end/fetchers/post.ts
@@ -1,5 +1,3 @@
-import { JSON_API_MEDIA_TYPE } from "$/types/server"
-import type { Response } from "$@/types/independent"
 import type { PostQueryParameters } from "$/types/query"
 import type {
 	PostResourceIdentifier,
@@ -32,27 +30,5 @@ export default class PostFetcher extends BaseFetcher<
 > {
 	constructor() {
 		super(POST_LINK)
-	}
-
-	async createWithFile(details: FormData): Promise<Response<
-		PostResourceIdentifier<"read">,
-		PostAttributes<"serialized">,
-		PostAttributes<"deserialized">,
-		PostResource,
-		DeserializedPostResource,
-		DeserializedPostDocument
-	>> {
-		const headers = new Headers({ "Accept": JSON_API_MEDIA_TYPE })
-
-		return await this.handleResponse(
-			this.postTo(this.links.unbound, details, headers)
-		) as Response<
-			PostResourceIdentifier<"read">,
-			PostAttributes<"serialized">,
-			PostAttributes<"deserialized">,
-			PostResource,
-			DeserializedPostResource,
-			DeserializedPostDocument
-		>
 	}
 }
