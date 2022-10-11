5bd5c6f02843dc447fb4fb1ac65b288621858762
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-11 10:45:51 +0800

    feat(authentication): include change password form in index page

--- pages/index/index.page.vue
+++ pages/index/index.page.vue
@@ -2,12 +2,16 @@
 	<div>
 		<Opening/>
 		<Instructions/>
+		<ChangePassword/>
 	</div>
 </template>
+
 <script lang="ts" setup>
+import { usePageContext } from "#/usePageContext"
+
 import Opening from "@/guest_homepage/Opening.vue"
 import Instructions from "@/guest_homepage/Instructions.vue"
-import { usePageContext } from "#/usePageContext"
+import ChangePassword from "@/authentication/change_password.vue"
 
 const { "pageProps": unusedPageProps } = usePageContext()
 </script>
