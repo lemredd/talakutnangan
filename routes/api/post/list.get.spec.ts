7be696cd0461d5e13c0952bfa5777f87b6adb26f
Angelo Magtoto <19201.magtoto.angelo.l@gmail.com>
2022-10-11 18:53:54 +0800

    unit(route): ensure list get for post works

---
+++ routes/api/post/list.get.spec.ts
@@ -0,0 +1,48 @@
+import ErrorBag from "$!/errors/error_bag"
+import MockRequester from "~/setups/mock_requester"
+
+import Controller from "./list.get"
+
+const QUERY_VALIDATION_INDEX = 0
+
+describe("Controller: GET /api/post", () => {
+	const requester = new MockRequester()
+
+	it("can accept valid info", async() => {
+		const controller = new Controller()
+		const { validations } = controller
+		const queryValidation = validations[QUERY_VALIDATION_INDEX]
+		const queryValidationFunction = queryValidation.intermediate.bind(queryValidation)
+		requester.customizeRequest({
+			"query": {
+				"filter": {
+					"existence": "*"
+				}
+			}
+		})
+
+		await requester.runMiddleware(queryValidationFunction)
+
+		requester.expectSuccess()
+	})
+
+	it("cannot accept invalid info", async() => {
+		const controller = new Controller()
+		const { validations } = controller
+		const queryValidation = validations[QUERY_VALIDATION_INDEX]
+		const queryValidationFunction = queryValidation.intermediate.bind(queryValidation)
+		requester.customizeRequest({
+			"query": {
+				"filter": {
+					"existence": "hello"
+				}
+			}
+		})
+
+		await requester.runMiddleware(queryValidationFunction)
+
+		const body = requester.expectFailure(ErrorBag).toJSON()
+		expect(body).toHaveLength(1)
+		expect(body).toHaveProperty("0.source.parameter", "filter.existence")
+	})
+})
