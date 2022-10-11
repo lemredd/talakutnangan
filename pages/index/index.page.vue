136b9347dd1f5e5d4d8a526afb369438ada264d9
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-11 10:48:26 +0800

    feat(authentication): show change password form id logged in

--- pages/index/index.page.vue
+++ pages/index/index.page.vue
@@ -2,16 +2,21 @@
 	<div>
 		<Opening/>
 		<Instructions/>
-		<ChangePassword/>
+		<ChangePassword v-if="isLoggedIn"/>
 	</div>
 </template>
 
 <script lang="ts" setup>
 import { usePageContext } from "#/usePageContext"
 
+import type { PageContext } from "$/types/renderer"
+
 import Opening from "@/guest_homepage/Opening.vue"
 import Instructions from "@/guest_homepage/Instructions.vue"
 import ChangePassword from "@/authentication/change_password.vue"
 
-const { "pageProps": unusedPageProps } = usePageContext()
+const { pageProps } = usePageContext() as PageContext<"serialized">
+const { userProfile } = pageProps
+
+const isLoggedIn = userProfile === null
 </script>
