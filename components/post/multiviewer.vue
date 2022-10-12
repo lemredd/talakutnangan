4b0fae596de5f37b08273387f2e76695224c8366
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-12 16:02:17 +0800

    intrn(post): separate the viewer from loop

--- components/post/multiviewer.vue
+++ components/post/multiviewer.vue
@@ -3,67 +3,7 @@
 		v-for="(post, i) in posts"
 		:key="post.id"
 		class="post">
-		<div v-if="post">
-			<div class="post-container flex justify-between w-[100%] pb-[5em]">
-				<div class="post-title">
-					<h2 class="font-bold">
-						Post
-					</h2>
-				</div>
-				<div class="">
-				</div>
-				<Menu :post="post"/>
-			</div>
-			<UpdatePostForm
-				v-model="posts[i]"
-				:is-shown="mustUpdate"
-				@submit="submitChangesSeparately"/>
-			<div class="post-container" :hidden="post.isPostShown">
-				<div class="left">
-					<div><img src="@assets/emptyUser.png"/></div>
-					<h2 class="title">
-						{{ post.user }}
-					</h2>
-				</div>
-				<div class="middle">
-					<h2 class="title">
-						{{ post.title }}
-						{{ post.badWordExist() }}
-					</h2>
-				</div>
-				<div class="right">
-					<h2 class="title">
-						{{ voteCountUpdate(post) }}
-					</h2>
-					<label class="switch">
-						<input
-							type="checkbox"
-							:checked="determineUserVoted(post)"
-							class="switch"
-							@click="upVote($event, post)"/>
-						<span class="slider"></span>
-					</label>
-					<h2 class="title">
-						{{ downVoteCountUpdate(post) }}
-					</h2>
-					<label class="switch">
-						<input
-							type="checkbox"
-							:checked="determineUserDownVoted(post)"
-							class="switch"
-							@click="downVote($event, post)"/>
-						<span class="slider"></span>
-					</label>
-
-					<h2 class="title">
-						{{ totalVotes(post) }}
-					</h2>
-				</div>
-				<p :class="`${post.id}`">
-					{{ post.desc }}
-				</p>
-			</div>
-		</div>
+		<Viewer v-model="posts[i]"/>
 		<br/>
 	</div>
 </template>
@@ -75,23 +15,11 @@
 </style>
 
 <script setup lang="ts">
-import { ref, computed } from "vue"
-import {
-	voteCountUpdate,
-	determineUserVoted,
-	upVote,
-	downVoteCountUpdate,
-	determineUserDownVoted,
-	downVote,
-	totalVotes
-} from "../../pages/forum/post"
-
-import Menu from "@/post/viewer/menu.vue"
-import UpdatePostForm from "./viewer/update_post_form.vue"
+import { computed } from "vue"
 
 import type { DeserializedPostResource } from "$/types/documents/post"
 
-import makeSwitch from "$@/helpers/make_switch"
+import Viewer from "@/post/multiviewer/viewer.vue"
 
 const props = defineProps<{
 	modelValue: DeserializedPostResource<"poster"|"posterRole">[]
@@ -110,13 +38,4 @@ const posts = computed<DeserializedPostResource<"poster"|"posterRole">[]>({
 		emit("update:modelValue", newValue)
 	}
 })
-
-const {
-	"state": mustUpdate,
-	"on": openEditForm
-} = makeSwitch(false)
-
-async function submitChangesSeparately(postID: string) {
-
-}
 </script>
---
+++ components/post/multiviewer/viewer.vue
@@ -0,0 +1,108 @@
+<template>
+	<div>
+		<div class="post-container flex justify-between w-[100%] pb-[5em]">
+			<div class="post-title">
+				<h2 class="font-bold">
+					Post
+				</h2>
+			</div>
+			<div class="">
+			</div>
+			<Menu :post="post"/>
+		</div>
+		<UpdatePostForm
+			v-model="post"
+			:is-shown="mustUpdate"
+			@submit="submitChangesSeparately"/>
+		<div class="post-container" :hidden="post.isPostShown">
+			<div class="left">
+				<div><img src="@assets/emptyUser.png"/></div>
+				<h2 class="title">
+					{{ post.user }}
+				</h2>
+			</div>
+			<div class="middle">
+				<h2 class="title">
+					{{ post.title }}
+					{{ post.badWordExist() }}
+				</h2>
+			</div>
+			<div class="right">
+				<h2 class="title">
+					{{ voteCountUpdate(post) }}
+				</h2>
+				<label class="switch">
+					<input
+						type="checkbox"
+						:checked="determineUserVoted(post)"
+						class="switch"
+						@click="upVote($event, post)"/>
+					<span class="slider"></span>
+				</label>
+				<h2 class="title">
+					{{ downVoteCountUpdate(post) }}
+				</h2>
+				<label class="switch">
+					<input
+						type="checkbox"
+						:checked="determineUserDownVoted(post)"
+						class="switch"
+						@click="downVote($event, post)"/>
+					<span class="slider"></span>
+				</label>
+
+				<h2 class="title">
+					{{ totalVotes(post) }}
+				</h2>
+			</div>
+			<p :class="`${post.id}`">
+				{{ post.desc }}
+			</p>
+		</div>
+	</div>
+</template>
+
+<style>
+
+</style>
+
+<script setup lang="ts">
+import { ref, computed } from "vue"
+
+import {
+	voteCountUpdate,
+	determineUserVoted,
+	upVote,
+	downVoteCountUpdate,
+	determineUserDownVoted,
+	downVote,
+	totalVotes
+} from "../../../pages/forum/post"
+
+import type { DeserializedPostResource } from "$/types/documents/post"
+
+import Menu from "@/post/multiviewer/viewer/menu.vue"
+import UpdatePostForm from "@/post/multiviewer/viewer/update_post_form.vue"
+
+import makeSwitch from "$@/helpers/make_switch"
+
+const props = defineProps<{
+	modelValue: DeserializedPostResource<"poster"|"posterRole">
+}>()
+
+interface CustomEvents {
+	(event: "update:modelValue", post: DeserializedPostResource<"poster"|"posterRole">): void
+}
+const emit = defineEmits<CustomEvents>()
+
+const post = ref<DeserializedPostResource<"poster"|"posterRole">>(props.modelValue)
+
+const {
+	"state": mustUpdate,
+	"on": openEditForm
+} = makeSwitch(false)
+
+async function submitChangesSeparately(postID: string) {
+
+}
+</script>
