5d0ba055c3f64354582880504b63471cba148d47
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-10-12 12:23:03 +0800

    intrn(front-end): generalize chat message kinds

---
+++ components/consultation/helpers/chat_message_kinds.spec.ts
@@ -0,0 +1,108 @@
+import { DeserializedChatMessageResource } from "$/types/documents/chat_message"
+import {
+	isMessageKindFile,
+	isMessageKindStatus,
+	isMessageKindText
+} from "./chat_message_kinds"
+
+
+describe("Helper: Chat message kinds", () => {
+	it("can determine if message kind is file", () => {
+		const CURRENT_TIME = new Date()
+		const message = {
+			"createdAt": CURRENT_TIME,
+			"data": {
+				"value": ""
+			},
+			"id": "0",
+			"kind": "file",
+			"type": "chat_message",
+			"updatedAt": CURRENT_TIME,
+			"user": {
+				"data": {
+					"email": "",
+					"id": "1",
+					"kind": "reachable_employee",
+					"name": "A",
+					"prefersDark": true,
+					"profilePicture": {
+						"data": {
+							"fileContents": "http://example.com/image_a",
+							"id": "1",
+							"type": "profile_picture"
+						}
+					},
+					"type": "user"
+				}
+			}
+		} as DeserializedChatMessageResource<"user">
+
+		expect(isMessageKindFile(message)).toBeTruthy()
+	})
+
+	it("can determine if message kind is status", () => {
+		const CURRENT_TIME = new Date()
+		const message = {
+			"createdAt": CURRENT_TIME,
+			"data": {
+				"value": ""
+			},
+			"id": "0",
+			"kind": "status",
+			"type": "chat_message",
+			"updatedAt": CURRENT_TIME,
+			"user": {
+				"data": {
+					"email": "",
+					"id": "1",
+					"kind": "reachable_employee",
+					"name": "A",
+					"prefersDark": true,
+					"profilePicture": {
+						"data": {
+							"fileContents": "http://example.com/image_a",
+							"id": "1",
+							"type": "profile_picture"
+						}
+					},
+					"type": "user"
+				}
+			}
+		} as DeserializedChatMessageResource<"user">
+
+		expect(isMessageKindStatus(message)).toBeTruthy()
+	})
+
+	it("can determine if message kind is text", () => {
+		const CURRENT_TIME = new Date()
+		const message = {
+			"createdAt": CURRENT_TIME,
+			"data": {
+				"value": ""
+			},
+			"id": "0",
+			"kind": "text",
+			"type": "chat_message",
+			"updatedAt": CURRENT_TIME,
+			"user": {
+				"data": {
+					"email": "",
+					"id": "1",
+					"kind": "reachable_employee",
+					"name": "A",
+					"prefersDark": true,
+					"profilePicture": {
+						"data": {
+							"fileContents": "http://example.com/image_a",
+							"id": "1",
+							"type": "profile_picture"
+						}
+					},
+					"type": "user"
+				}
+			}
+		} as DeserializedChatMessageResource<"user">
+
+		expect(isMessageKindText(message)).toBeTruthy()
+	})
+})
