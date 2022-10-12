f2f68e4bf105342750f1284cfaea3ecc4e735bba
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-10-12 22:44:44 +0800

    unit(chat): ensure non-image file preview

--- components/consultation/chat_window/user_controller/file_upload.spec.ts
+++ components/consultation/chat_window/user_controller/file_upload.spec.ts
@@ -17,7 +17,7 @@ describe("Component: User controller/File Upload", () => {
 	describe("image upload", () => {
 		const accept = "image/png"
 		it("can preview uploaded file", async() => {
-			const wrapper = shallowMount(Component, {
+			const wrapper = shallowMount<any>(Component, {
 				"global": {
 					"provide": {
 						[CHAT_MESSAGE_ACTIVITY]: readonly(ref({ "id": "1" }))
@@ -85,7 +85,113 @@ describe("Component: User controller/File Upload", () => {
 					}
 				}
 			}
-			const wrapper = shallowMount(Component, {
+			const wrapper = shallowMount<any>(Component, {
+				"global": {
+					"provide": {
+						[CHAT_MESSAGE_ACTIVITY]: readonly(ref({ "id": "1" }))
+					},
+					"stubs": {
+						"Overlay": false
+					}
+				},
+				"props": {
+					accept,
+					"isShown": true
+				}
+			})
+			// ! find proper type
+			const wrapperVM = wrapper.vm as any
+
+			const disabledSendBtn = wrapper.find(".send-btn")
+			expect(disabledSendBtn.attributes("disabled")).toBeDefined()
+
+			global.URL.createObjectURL = jest.fn()
+			wrapperVM.extractFile(event)
+			const sendBtn = wrapper.find(".send-btn")
+			await nextTick()
+			expect(sendBtn.attributes("src")).toBeUndefined()
+
+			await sendBtn.trigger("click")
+			await flushPromises()
+
+			const castFetch = fetch as jest.Mock<any, any>
+			const [ [ request ] ] = castFetch.mock.calls
+			expect(request).toHaveProperty("method", "POST")
+			expect(request).toHaveProperty("url", "/api/chat_message/create_with_file")
+			expect(wrapper.emitted()).toHaveProperty("close")
+		})
+	})
+
+	describe("file upload", () => {
+		const accept = "file/*"
+		it.only("can preview uploaded file", async() => {
+			const wrapper = shallowMount<any>(Component, {
+				"global": {
+					"provide": {
+						[CHAT_MESSAGE_ACTIVITY]: readonly(ref({ "id": "1" }))
+					},
+					"stubs": {
+						"Overlay": false
+					}
+				},
+				"props": {
+					accept,
+					"isShown": true
+				}
+			})
+			const fileInput = wrapper.find("input[type=file]")
+
+			await fileInput.setValue("")
+			const previewFile = wrapper.find(".preview-file-container")
+			expect(previewFile.exists()).toBeTruthy()
+		})
+
+		it("can send selected file", async() => {
+			const fileContents = "http://localhost:16000/api/attached_chat_file/1"
+			fetchMock.mockResponseOnce(
+				JSON.stringify({
+					"data": {
+						"attributes": {
+							"kind": "file"
+						},
+						"id": "1",
+						"relationships": {
+							"attachedChatFile": {
+								"data": {
+									"id": "1",
+									"type": "attached_chat_file"
+								}
+							}
+						},
+						"type": "chat_message"
+					} as ChatMessageResource<"read"> & ChatMessageRelationships<"read">,
+					"included": [
+						{
+							"attributes": {
+								fileContents
+							},
+							"id": "1",
+							"type": "attached_chat_file"
+						} as AttachedChatFileResource
+					]
+				} as ChatMessageDocument<"read">),
+				{ "status": RequestEnvironment.status.OK }
+			)
+
+			const event = {
+				"target": {
+					"files": {
+						"item": () => [
+							{
+								"name": "image.png",
+								"size": 10000,
+								"type": "image/png"
+							}
+						]
+					}
+				}
+			}
+			const wrapper = shallowMount<any>(Component, {
 				"global": {
 					"provide": {
 						[CHAT_MESSAGE_ACTIVITY]: readonly(ref({ "id": "1" }))
