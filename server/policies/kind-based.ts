fd3b8f1f94438f96045500fbe0b0dfa96d1b631f
Angelo Magtoto <19201.magtoto.angelo.l@gmail.com>
2022-10-12 17:14:21 +0800

    intrn(server): modify policy to require signatures

--- server/policies/kind-based.ts
+++ server/policies/kind-based.ts
@@ -1,24 +1,40 @@
 import type { UserKind } from "$/types/database"
 import type { AuthenticatedRequest } from "!/types/dependent"
 import type { DeserializedUserProfile } from "$/types/documents/user"
+import type { AdvanceAuthenticationOptions } from "!/types/independent"
 
 import deserialize from "$/object/deserialize"
 import AuthorizationError from "$!/errors/authorization"
 import AuthenticationBasedPolicy from "!/policies/authentication-based"
 
+
 /**
  * Creates middleware to only allow certain kinds of user.
  *
  * Automatically requires user to be authenticated.
+ * @param checkOthers Extra function used for checking other constraints.
  */
-export default class extends AuthenticationBasedPolicy {
+export default class <
+	V extends AuthenticatedRequest = AuthenticatedRequest
+> extends AuthenticationBasedPolicy {
 	private kinds: UserKind[]
+	private checkOthers: (request: V) => Promise<void>
 
 	/**
 	 * @param kinds Specific kinds of user to only allow.
 	 */
-	constructor(...kinds: UserKind[]) {
-		super(true)
+	constructor(
+		kinds: UserKind[],
+		{
+			checkOthers = (): Promise<void> => {
+				const promise = Promise.resolve()
+				return promise
+			},
+			...otherAuthenticationOptions
+		}: Partial<AdvanceAuthenticationOptions<V>> = {}
+	) {
+		super(true, otherAuthenticationOptions)
+		this.checkOthers = checkOthers
 		this.kinds = kinds
 	}
 
