062c997761fdc3e17877c95712a40147ce5541e4
Angelo Magtoto <19201.magtoto.angelo.l@gmail.com>
2022-10-13 13:59:20 +0800

    intrn(database): tag migration file

---
+++ database/migrations/20221012053000-create-tags.js
@@ -0,0 +1,47 @@
+/* eslint-disable vue/sort-keys */
+
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		await queryInterface.sequelize.transaction(async transaction => {
+			try {
+				await queryInterface.createTable("Tags", {
+					"id": {
+						"allowNull": false,
+						"autoIncrement": true,
+						"primaryKey": true,
+						"type": Sequelize.BIGINT
+					},
+					"name": {
+						"allowNull": false,
+						"type": Sequelize.STRING
+					},
+					"createdAt": {
+						"allowNull": false,
+						"type": Sequelize.DATE
+					},
+					"updatedAt": {
+						"allowNull": false,
+						"type": Sequelize.DATE
+					},
+					"deletedAt": {
+						"allowNull": true,
+						"type": Sequelize.DATE
+					}
+				}, { transaction })
+			} catch (err) {
+				await transaction.rollback()
+				throw err
+			}
+		})
+	},
+	async down(queryInterface, unusedSequelize) {
+		await queryInterface.sequelize.transaction(async transaction => {
+			try {
+				await queryInterface.dropTable("Tags", { transaction })
+			} catch (err) {
+				await transaction.rollback()
+				throw err
+			}
+		})
+	}
+}
