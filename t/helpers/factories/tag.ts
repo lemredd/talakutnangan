76de39f7cb410668ee10c5b2476410fb7b52c546
Angelo Magtoto <19201.magtoto.angelo.l@gmail.com>
2022-10-13 14:00:43 +0800

    intrn(helper): tag factory

---
+++ t/helpers/factories/tag.ts
@@ -0,0 +1,48 @@
+import type { ModelCtor } from "%/types/dependent"
+import type { GeneratedData } from "~/types/dependent"
+import type {
+	TagResourceIdentifier,
+	TagAttributes,
+	DeserializedTagResource,
+	DeserializedTagDocument,
+	DeserializedTagListDocument,
+	TagResource,
+	TagDocument,
+	TagListDocument
+} from "$/types/documents/tag"
+
+import { faker } from "@faker-js/faker"
+
+import Tag from "%/models/tag"
+import BaseFactory from "~/factories/base"
+import TagTransformer from "%/transformers/tag"
+
+export default class TagFactory extends BaseFactory<
+	Tag,
+	TagResourceIdentifier,
+	TagAttributes<"serialized">,
+	TagAttributes<"deserialized">,
+	TagResource,
+	DeserializedTagResource,
+	TagDocument,
+	TagListDocument,
+	DeserializedTagDocument,
+	DeserializedTagListDocument
+> {
+	#name: () => string = () => faker.name.jobTitle()
+
+	get model(): ModelCtor<Tag> { return Tag }
+
+	get transformer(): TagTransformer { return new TagTransformer() }
+
+	generate(): GeneratedData<Tag> {
+		return Promise.resolve({
+			"name": this.#name()
+		})
+	}
+
+	name(name: () => string): TagFactory {
+		this.#name = name
+		return this
+	}
+}
