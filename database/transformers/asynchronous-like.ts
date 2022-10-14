9ebc481fa9f890a8671566209fe3fa7d47dcb3d1
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-14 10:10:16 +0800

    intrn(transformer): include user by default in relationships

--- database/transformers/asynchronous-like.ts
+++ database/transformers/asynchronous-like.ts
@@ -1,10 +1,27 @@
+import type { IncludedRelationships } from "%/types/independent"
 import type { AttributesObject, TransformerOptions } from "%/types/dependent"
 
 import Transformer from "%/transformers/base"
+import UserTransformer from "%/transformers/user"
 import Serializer from "%/transformers/serializer"
 import AsynchronousLike from "%/models/asynchronous-like"
 
+type Relationships = "user"
+
 export default abstract class<T extends AsynchronousLike, U> extends Transformer<T, U> {
+	constructor(type: string, { included }: IncludedRelationships<Relationships> = {
+		"included": [ "user" ]
+	}) {
+		super(type, [
+			included.indexOf("user") > -1
+				? {
+					"attribute": "user",
+					"transformer": new UserTransformer()
+				}
+				: null
+		])
+	}
+
 	transform(
 		model: T|T[],
 		options: TransformerOptions<U>
