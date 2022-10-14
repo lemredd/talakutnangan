2f4360252086d667c60c286444c8aee549a50d7b
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-14 20:15:10 +0800

    unit(server): check if asynchronous operation can be run

---
+++ server/singletons/asynchronous_operation_manager.spec.ts
@@ -0,0 +1,29 @@
+import "~/setups/database.setup"
+import MockRequester from "~/setups/mock_requester"
+import AsynchronousFileManager from "%/managers/asynchronous_file"
+
+import Singleton from "./asynchronous_operation_manager"
+
+describe("Server singleton: Asynchronous operation manager", () => {
+	const requester = new MockRequester()
+
+	it("can initialize properly", async() => {
+		const singleton = new Singleton()
+		requester.customizeRequest({
+			"body": Buffer.alloc(0),
+			"user": {
+				"data": {
+					"id": "1"
+				}
+			}
+		})
+
+		await requester.runAsynchronousOperationInitializer(
+			singleton.initializeWithRequest.bind(singleton),
+			AsynchronousFileManager,
+			4
+		)
+
+		requester.expectSuccess()
+	})
+})
