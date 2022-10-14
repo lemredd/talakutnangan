5368296f1690ddd7fd9ddc443d8e91ff3f9982e1
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-14 17:21:30 +0800

    intrn(server): make singleton for asynchronous operation manager

---
+++ server/singletons/asynchronous_operation_manager.ts
@@ -0,0 +1,51 @@
+import type { Request } from "!/types/dependent"
+import type { Serializable } from "$/types/general"
+import type { TransactionManagerInterface } from "$!/types/dependent"
+
+import Log from "$!/singletons/log"
+import digest from "$!/helpers/digest"
+import BaseManager from "%/managers/base"
+import TransactionManager from "%/helpers/transaction_manager"
+
+/**
+ * Manages the transaction for asynchronous requests to be implementation-agnostic.
+ */
+export default class extends TransactionManager implements TransactionManagerInterface {
+	private manager: BaseManager<any, any, any, any, any, any>
+	private id = 0
+
+	constructor(manager: BaseManager<any, any, any, any, any, any>) {
+		super()
+		this.manager = manager
+	}
+
+	/**
+	 * Expects body of request to be raw.
+	 */
+	async initializeWithRequest(request: Request, totalStepCount: number): Promise<void> {
+		await super.initialize()
+
+		const hashedBody = await digest(request.body)
+		Log.trace("asynchronous", `digested body in ${request.url}`)
+
+		const possibleDocument = await this.manager.findOneOnColumn("token", hashedBody)
+		let possibleResource = possibleDocument.data
+
+		if (possibleResource === null) {
+			const createdDocument = await this.manager.create({
+				"extra": {},
+				"finishedStepCount": 0,
+				"hasStopped": false,
+				"origin": request.url,
+				"token": hashedBody,
+				totalStepCount
+			})
+
+			possibleResource = createdDocument.data
+		}
+
+		const castResource = possibleResource as Serializable
+
+		this.id = Number(castResource.id)
+	}
+}
