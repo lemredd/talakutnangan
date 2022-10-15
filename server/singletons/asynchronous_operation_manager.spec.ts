3e85f0365cb8ccd91a8298330c3baa80666d9d37
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-15 13:03:00 +0800

    unit(server): prepare test to regenerate document

--- server/singletons/asynchronous_operation_manager.spec.ts
+++ server/singletons/asynchronous_operation_manager.spec.ts
@@ -1,4 +1,5 @@
 import type { AuthenticatedRequest } from "!/types/dependent"
+import type { AsynchronousFileDocument } from "$/types/documents/asynchronous_file"
 
 import "~/setups/database.setup"
 import UserFactory from "~/factories/user"
@@ -6,8 +7,9 @@ import Factory from "~/factories/asynchronous_file"
 import MockRequester from "~/setups/mock_requester"
 import AsynchronousFileManager from "%/managers/asynchronous_file"
 
-import Singleton from "./asynchronous_operation_manager"
 import digest from "$!/helpers/digest"
+import deserialize from "$/object/deserialize"
+import Singleton from "./asynchronous_operation_manager"
 
 describe("Server singleton: Asynchronous operation manager", () => {
 	const requester = new MockRequester<AuthenticatedRequest>()
@@ -63,5 +65,36 @@ describe("Server singleton: Asynchronous operation manager", () => {
 		expect(singleton.isNew).toBeFalsy()
 		expect(singleton.finishedStepCount).toBe(finishedStepCount)
 		expect(singleton.totalStepCount).toBe(totalStepCount)
+	})
+
+	it("can regenerate document", async() => {
+		const singleton = new Singleton()
+		const userFactory = new UserFactory()
+		const user = await userFactory.insertOne()
+		const body = Buffer.alloc(0)
+		const finishedStepCount = 2
+		const totalStepCount = 4
+		await new Factory()
+		.token(() => digest(body))
+		.user(() => Promise.resolve(user))
+		.finishedStepCount(() => finishedStepCount)
+		.totalStepCount(() => totalStepCount)
+		.insertOne()
+		requester.customizeRequest({
+			body,
+			"user": await userFactory.serialize(user)
+		})
+		await requester.runAsynchronousOperationInitializer(
+			singleton.initializeWithRequest.bind(singleton),
+			AsynchronousFileManager,
+			totalStepCount
+		)
+
+		const document = await singleton.regenerateDocument()
+		const deserializedDocument = deserialize(document) as AsynchronousFileDocument
+		await singleton.destroySuccessfully()
+
+		expect(deserializedDocument).toHaveProperty("data.finishedStepCount", finishedStepCount)
+		expect(deserializedDocument).toHaveProperty("data.totalStepCount", totalStepCount)
 	})
 })
