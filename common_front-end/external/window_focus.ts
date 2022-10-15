ba9eade6f3653f5e3e790568687e58fd4013db8f
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-10-15 15:59:34 +0800

    intrn(front-end): make window focus listener
    Co-authored-by: Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>

---
+++ common_front-end/external/window_focus.ts
@@ -0,0 +1,39 @@
+import type { FocusListener } from "$@/types/dependent"
+
+import Stub from "$/singletons/stub"
+import RequestEnvironment from "$/singletons/request_environment"
+
+export default class DocumentVisibility extends RequestEnvironment {
+	private static rawListeners: FocusListener[] = []
+
+	static addEventListener(listener: FocusListener): void {
+		this.rawListeners.push(listener)
+		Stub.runConditionally(
+			() => {
+				window.addEventListener("blur", () => {
+					this.rawListeners.forEach(rawListener => rawListener("blur"))
+				})
+				window.addEventListener("focus", () => {
+					this.rawListeners.forEach(rawListener => rawListener("focus"))
+				})
+			},
+			() => [ {} as unknown as void, {
+				"arguments": [ listener ],
+				"functionName": "addEventListener"
+			} ]
+		)
+	}
+
+	static emitMockEvent(state: "blur" | "focus"): void {
+		Stub.runConditionally(
+			() => {
+				throw new Error("It is impossible to emit mock events in non-test environments.")
+			},
+			() => {
+				this.rawListeners.forEach(rawListener => rawListener(state))
+
+				return [ {} as unknown as void, null ]
+			}
+		)
+	}
+}
