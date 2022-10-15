f4de641bbd9adf0352b22d10716a5a7bb7bbb4fa
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-10-15 15:19:59 +0800

    share(constant): expose new symbol

--- common_front-end/constants/provided_keys.ts
+++ common_front-end/constants/provided_keys.ts
@@ -1,3 +1,4 @@
 export const CHAT_MESSAGE_ACTIVITY = Symbol("Key for chat activity resource of current user")
+export const CHAT_MESSAGE_ACTIVITIES_IN_CONSULTATION = Symbol("current chat message activity")
 
 export const BODY_CLASSES = Symbol("Key for body CSS manager")
