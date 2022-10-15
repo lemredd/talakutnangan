04534c0c7de5488131f4c1df8174541b9e91d2a2
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-15 12:01:26 +0800

    unit(server): ensure asynchronous operation manager works with old ones

--- server/singletons/asynchronous_operation_manager.spec.ts
+++ server/singletons/asynchronous_operation_manager.spec.ts
@@ -2,10 +2,12 @@ import type { AuthenticatedRequest } from "!/types/dependent"
 
 import "~/setups/database.setup"
 import UserFactory from "~/factories/user"
+import Factory from "~/factories/asynchronous_file"
 import MockRequester from "~/setups/mock_requester"
 import AsynchronousFileManager from "%/managers/asynchronous_file"
 
 import Singleton from "./asynchronous_operation_manager"
+import digest from "$!/helpers/digest"
 
 describe("Server singleton: Asynchronous operation manager", () => {
 	const requester = new MockRequester<AuthenticatedRequest>()
@@ -24,7 +26,33 @@ describe("Server singleton: Asynchronous operation manager", () => {
 			AsynchronousFileManager,
 			4
 		)
+		await singleton.destroySuccessfully()
 
 		expect(singleton.isNew).toBeTruthy()
+	})
+
+	it("can initialize properly with old operation", async() => {
+		const singleton = new Singleton()
+		const userFactory = new UserFactory()
+		const user = await userFactory.insertOne()
+		const body = Buffer.alloc(0)
+		await new Factory()
+		.token(() => digest(body))
+		.user(() => Promise.resolve(user))
+		.insertOne()
+
+		requester.customizeRequest({
+			body,
+			"user": await userFactory.serialize(user)
+		})
+
+		await requester.runAsynchronousOperationInitializer(
+			singleton.initializeWithRequest.bind(singleton),
+			AsynchronousFileManager,
+			4
+		)
+		await singleton.destroySuccessfully()
+
+		expect(singleton.isNew).toBeFalsy()
 	})
 })
