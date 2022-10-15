23b6b3047416a60904e3b796e078d40e7351eb72
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-15 19:04:08 +0800

    intrn(server): rename the middleware

--- server/middlewares/miscellaneous/asynchronous_operation_committer.ts
+++ server/middlewares/miscellaneous/asynchronous_operation_committer.ts
@@ -2,7 +2,7 @@ import type { AsynchronousRequest } from "!/types/dependent"
 
 import RequestFilter from "!/bases/request_filter"
 
-export default class AsynchronousOperationInitializer extends RequestFilter {
+export default class AsynchronousOperationCommitter extends RequestFilter {
 	async filterRequest(request: AsynchronousRequest): Promise<void> {
 		await request.asynchronousOperation.destroyConditionally()
 	}
