ca106a999d0b0466a12aeca5e8886557169827a6
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-16 02:16:05 +0800

    feat(consultation): use initializer to reply properly

--- routes/api/consultation/request(id)/as_pdf.post.ts
+++ routes/api/consultation/request(id)/as_pdf.post.ts
@@ -1,17 +1,21 @@
 import { generatePdf } from "html-pdf-node-ts"
 
+import { OptionalMiddleware } from "!/types/independent"
 import type { Request, Response, BaseManagerClass } from "!/types/dependent"
 
 import Log from "$!/singletons/log"
 import Policy from "!/bases/policy"
 import Manager from "%/managers/consultation"
 import URLMaker from "$!/singletons/url_maker"
-import OkResponseInfo from "!/response_infos/ok"
 import BoundController from "!/controllers/bound"
 import Merger from "!/middlewares/miscellaneous/merger"
+import AcceptedResponseInfo from "!/response_infos/accepted"
 
+import AsynchronousFile from "%/managers/asynchronous_file"
 import CommonMiddlewareList from "!/middlewares/common_middleware_list"
 import BelongsToCurrentUserPolicy from "!/policies/belongs_to_current_user"
+import AsynchronousOperationInitializer
+	from "!/middlewares/miscellaneous/asynchronous_operation_initializer"
 
 export default class extends BoundController {
 	get filePath(): string { return __filename }
@@ -23,9 +27,19 @@ export default class extends BoundController {
 		]) as unknown as Policy
 	}
 
+	get postPolicyMiddlewares(): OptionalMiddleware[] {
+		const numberOfTotalStepCount = 3
+		return [
+			new AsynchronousOperationInitializer(
+				AsynchronousFile,
+				numberOfTotalStepCount
+			)
+		]
+	}
+
 	get manager(): BaseManagerClass { return Manager }
 
-	async handle(request: Request, unusedResponse: Response): Promise<OkResponseInfo> {
+	async handle(request: Request, unusedResponse: Response): Promise<AcceptedResponseInfo> {
 		const { id } = request.params
 
 		const URL = URLMaker.makeURLFromPath("consultation/:id", { id })
@@ -36,6 +50,6 @@ export default class extends BoundController {
 			"url": URL
 		})
 
-		return new OkResponseInfo(document, "application/pdf")
+		return new AcceptedResponseInfo(document)
 	}
 }
