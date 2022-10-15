80a9c4dc44c4fa24fc139bbb7be8f8962e2a37d4
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-10-15 11:29:10 +0800

    intrn(component): use `$attrs` temporarily
    * `props` seems to be treated as undefined

--- components/helpers/progress_bar.spec.ts
+++ components/helpers/progress_bar.spec.ts
@@ -3,16 +3,19 @@ import { shallowMount } from "@vue/test-utils"
 import Component from "./progress_bar.vue"
 
 describe("Component: Helper/progress bar", () => {
+	// ? props seems to be undefined. use $attrs temporarily
+
 	it("can display current progress", () => {
 		const wrapper = shallowMount(Component, {
 			"props": {
-				"current": 0
+				"current": 50
 			}
 		})
-		const current = wrapper.find(".current")
+		const progress = wrapper.find("progress")
 		const castedWrapper = wrapper.vm as any
+		const expectedValue = String(castedWrapper.$attrs.current)
 
-		expect(castedWrapper.max).toEqual(100)
-		expect(current.exists()).toBeTruthy()
+		expect(progress.exists()).toBeTruthy()
+		expect(progress.attributes("value")).toEqual(expectedValue)
 	})
 })
