b911bda39ba39d98c469fa28a7de840b1086f021
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-16 14:16:11 +0800

    feat(consultation): validate the body

--- routes/api/consultation(id)/request/as_pdf.post.ts
+++ routes/api/consultation(id)/request/as_pdf.post.ts
@@ -1,14 +1,15 @@
 import { generatePdf } from "html-pdf-node-ts"
 
+import type { FieldRules } from "!/types/validation"
 import type { OptionalMiddleware } from "!/types/independent"
-import type { AsynchronousRequest, Response, BaseManagerClass } from "!/types/dependent"
+import type { AsynchronousRequest, Request, Response, BaseManagerClass } from "!/types/dependent"
 
 import Log from "$!/singletons/log"
 import Policy from "!/bases/policy"
 import Manager from "%/managers/consultation"
 import URLMaker from "$!/singletons/url_maker"
-import BoundController from "!/controllers/bound"
 import Merger from "!/middlewares/miscellaneous/merger"
+import BoundJSONController from "!/controllers/bound_json"
 
 import AsynchronousFile from "%/managers/asynchronous_file"
 import CommonMiddlewareList from "!/middlewares/common_middleware_list"
@@ -16,7 +17,10 @@ import BelongsToCurrentUserPolicy from "!/policies/belongs_to_current_user"
 import AsynchronousOperationInitializer
 	from "!/middlewares/miscellaneous/asynchronous_operation_initializer"
 
-export default class extends BoundController {
+import exists from "!/validators/manager/exists"
+import makeResourceIdentifierDocumentRules from "!/rule_sets/make_resource_identifier_document"
+
+export default class extends BoundJSONController {
 	get filePath(): string { return __filename }
 
 	get policy(): Policy {
@@ -26,8 +30,16 @@ export default class extends BoundController {
 		]) as unknown as Policy
 	}
 
+	makeBodyRuleGenerator(unusedRequest: Request): FieldRules {
+		return makeResourceIdentifierDocumentRules(
+			"consultation",
+			exists,
+			Manager
+		)
+	}
+
 	get postPolicyMiddlewares(): OptionalMiddleware[] {
-		const numberOfTotalStepCount = 2
+		const numberOfTotalStepCount = 3
 		return [
 			new AsynchronousOperationInitializer(
 				AsynchronousFile,
@@ -39,6 +51,8 @@ export default class extends BoundController {
 	get manager(): BaseManagerClass { return Manager }
 
 	async handle(request: AsynchronousRequest, unusedResponse: Response): Promise<void> {
+		await request.asynchronousOperation.incrementProgress()
+
 		const { id } = request.params
 
 		const URL = URLMaker.makeURLFromPath("consultation/:id", { id })
