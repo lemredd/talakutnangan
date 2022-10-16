1520fb79e6382f5dcc2909764c79aa96b57a3ba3
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-10-16 11:16:54 +0800

    unit(authentication): remove some test cases

--- components/authentication/log_in_form.spec.ts
+++ components/authentication/log_in_form.spec.ts
@@ -9,133 +9,118 @@ import RequestEnvironment from "$/singletons/request_environment"
 
 
 describe("Component: Log In Form", () => {
-	describe("general features", () => {
-		it("should log in with existing credentials", async() => {
-			fetchMock.mockResponse(
-				JSON.stringify({
-					"body": {
-						"token": v4()
-					},
-					"status": 200
-				}),
-				{ "status": RequestEnvironment.status.OK })
+	it("should log in with existing credentials", async() => {
+		fetchMock.mockResponse(
+			JSON.stringify({
+				"body": {
+					"token": v4()
+				},
+				"status": 200
+			}),
+			{ "status": RequestEnvironment.status.OK })
 
-			const wrapper = shallowMount(Component)
+		const wrapper = shallowMount(Component)
 
-			const emailField = wrapper.findComponent({ "name": "TextualField" })
-			const passwordField = wrapper.findComponent({ "name": "PasswordField" })
-			const submitBtn = wrapper.find("#submit-btn")
+		const emailField = wrapper.findComponent({ "name": "TextualField" })
+		const passwordField = wrapper.findComponent({ "name": "PasswordField" })
+		const submitBtn = wrapper.find("#submit-btn")
 
-			await emailField.setValue("dean@example.net")
-			await passwordField.setValue("password")
-			await submitBtn.trigger("click")
-			await flushPromises()
+		await emailField.setValue("dean@example.net")
+		await passwordField.setValue("password")
+		await submitBtn.trigger("click")
+		await flushPromises()
 
-			const castFetch = fetch as jest.Mock<any, any>
-			const [ [ request ] ] = castFetch.mock.calls
-			expect(request).toHaveProperty("method", "POST")
-			expect(request).toHaveProperty("url", "/api/user/log_in")
+		const castFetch = fetch as jest.Mock<any, any>
+		const [ [ request ] ] = castFetch.mock.calls
+		expect(request).toHaveProperty("method", "POST")
+		expect(request).toHaveProperty("url", "/api/user/log_in")
 
-			const previousCalls = Stub.consumePreviousCalls()
-			expect(previousCalls).toHaveProperty("0.functionName", "assignPath")
-			expect(previousCalls).toHaveProperty("0.arguments.0", "/")
-			expect(previousCalls).not.toHaveProperty("0.arguments.1")
-		})
-
-		it("should not log in with non-existing credentials", async() => {
-			fetchMock.mockResponse(
-				JSON.stringify({
-					"errors": [
-						{
-							"status": RequestEnvironment.status.BAD_REQUEST,
-							"code": "3",
-							"title": "Validation Error",
-							// eslint-disable-next-line max-len
-							"detail": "The sample@example.com in field \"email\" does not exists in the database\".",
-							"source": {
-								"pointer": "email"
-							}
-						}
-					] as UnitError[]
-				}),
-				{ "status": RequestEnvironment.status.BAD_REQUEST })
-
-			const wrapper = shallowMount(Component)
-
-			const submitBtn = wrapper.find("#submit-btn")
-			await submitBtn.trigger("click")
-
-			const castFetch = fetch as jest.Mock<any, any>
-			const [ [ request ] ] = castFetch.mock.calls
-			expect(request).toHaveProperty("method", "POST")
-			expect(request).toHaveProperty("url", "/api/user/log_in")
-			await flushPromises()
-
-			const error = wrapper.find(".error")
-			console.log(error.html())
-		})
-
-		it("should show error from authentication-guarded route", () => {
-			const wrapper = shallowMount<any>(Component, {
-				"props": {
-					"receivedErrorFromPageContext": {
-						"detail": "redirected from authentication-guarded route"
-					}
-				}
-			})
-			const error = wrapper.find(".error")
-			expect(error.html()).toContain(wrapper.props().receivedErrorFromPageContext.detail)
-		})
-		it("should not log in with non-existing credentials", async() => {
-			fetchMock.mockResponse(
-				JSON.stringify({
-					"errors": [
-						{
-							"status": RequestEnvironment.status.BAD_REQUEST,
-							"code": "3",
-							"title": "Validation Error",
-							// eslint-disable-next-line max-len
-							"detail": "The sample@example.com in field \"email\" does not exists in the database\".",
-							"source": {
-								"pointer": "email"
-							}
-						}
-					] as UnitError[]
-				}),
-				{ "status": RequestEnvironment.status.BAD_REQUEST })
-
-			const wrapper = shallowMount(Component)
-
-			const submitBtn = wrapper.find("#submit-btn")
-			await submitBtn.trigger("click")
-
-			const castFetch = fetch as jest.Mock<any, any>
-			const [ [ request ] ] = castFetch.mock.calls
-			expect(request).toHaveProperty("method", "POST")
-			expect(request).toHaveProperty("url", "/api/user/log_in")
-			await flushPromises()
-
-
-			const error = wrapper.find(".error")
-			const errorDetails
-			= "The sample@example.com in field \"email\" does not exists in the database\"."
-			console.log(errorDetails, "\n\n\n\n")
-			expect(error.exists()).toBeTruthy()
-			expect(error.html()).toContain(errorDetails)
-		})
+		const previousCalls = Stub.consumePreviousCalls()
+		expect(previousCalls).toHaveProperty("0.functionName", "assignPath")
+		expect(previousCalls).toHaveProperty("0.arguments.0", "/")
+		expect(previousCalls).not.toHaveProperty("0.arguments.1")
 	})
-	describe("development mode features", () => {
-		it("should fill details according to selected role", async() => {
-			let role = ""
-			const wrapper = shallowMount<any>(Component)
-			const emailField = wrapper.findComponent({ "name": "TextualField" })
-			const passwordField = wrapper.findComponent({ "name": "PasswordField" })
-			const roleSelector = wrapper.findComponent({ "name": "RoleSelector" })
 
-			role = "student"
-			await roleSelector.setValue(role)
-			expect(emailField.attributes("modelvalue")).toBe(`${role}@example.net`)
-			expect(passwordField.attributes("modelvalue")).toBe("password")
+	it("should not log in with non-existing credentials", async() => {
+		fetchMock.mockResponse(
+			JSON.stringify({
+				"errors": [
+					{
+						"status": RequestEnvironment.status.BAD_REQUEST,
+						"code": "3",
+						"title": "Validation Error",
+						// eslint-disable-next-line max-len
+						"detail": "The sample@example.com in field \"email\" does not exists in the database\".",
+						"source": {
+							"pointer": "email"
+						}
+					}
+				] as UnitError[]
+			}),
+			{ "status": RequestEnvironment.status.BAD_REQUEST })
+
+		const wrapper = shallowMount(Component)
+
+		const submitBtn = wrapper.find("#submit-btn")
+		await submitBtn.trigger("click")
+
+		const castFetch = fetch as jest.Mock<any, any>
+		const [ [ request ] ] = castFetch.mock.calls
+		expect(request).toHaveProperty("method", "POST")
+		expect(request).toHaveProperty("url", "/api/user/log_in")
+		await flushPromises()
+
+		const error = wrapper.find(".error")
+		console.log(error.html())
+	})
+
+	it("should show error from authentication-guarded route", () => {
+		const wrapper = shallowMount<any>(Component, {
+			"props": {
+				"receivedErrorFromPageContext": {
+					"detail": "redirected from authentication-guarded route"
+				}
+			}
 		})
+		const error = wrapper.find(".error")
+		expect(error.html()).toContain(wrapper.props().receivedErrorFromPageContext.detail)
+	})
+
+	it("should not log in with non-existing credentials", async() => {
+		fetchMock.mockResponse(
+			JSON.stringify({
+				"errors": [
+					{
+						"status": RequestEnvironment.status.BAD_REQUEST,
+						"code": "3",
+						"title": "Validation Error",
+						// eslint-disable-next-line max-len
+						"detail": "The sample@example.com in field \"email\" does not exists in the database\".",
+						"source": {
+							"pointer": "email"
+						}
+					}
+				] as UnitError[]
+			}),
+			{ "status": RequestEnvironment.status.BAD_REQUEST })
+
+		const wrapper = shallowMount(Component)
+
+		const submitBtn = wrapper.find("#submit-btn")
+		await submitBtn.trigger("click")
+
+		const castFetch = fetch as jest.Mock<any, any>
+		const [ [ request ] ] = castFetch.mock.calls
+		expect(request).toHaveProperty("method", "POST")
+		expect(request).toHaveProperty("url", "/api/user/log_in")
+		await flushPromises()
+
+
+		const error = wrapper.find(".error")
+		const errorDetails
+		= "The sample@example.com in field \"email\" does not exists in the database\"."
+		console.log(errorDetails, "\n\n\n\n")
+		expect(error.exists()).toBeTruthy()
+		expect(error.html()).toContain(errorDetails)
 	})
 })
