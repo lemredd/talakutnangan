2a695073d8549491bf6d083426ab872b2d10972f
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-16 14:02:25 +0800

    intrn(server): remove unnecessary route

--- routes/api/consultation(id)/request/router.ts
+++ routes/api/consultation(id)/request/router.ts
@@ -1,5 +1,4 @@
 import Router from "!/bases/router"
-import GetRequestAsPDF from "!%/api/consultation(id)/request/as_pdf.get"
 import PostRequestAsPDF from "!%/api/consultation(id)/request/as_pdf.post"
 
 export default class extends Router {
@@ -8,7 +7,6 @@ export default class extends Router {
 
 		this.useControllersAsync(new Promise(resolve => {
 			resolve([
-				new GetRequestAsPDF(),
 				new PostRequestAsPDF()
 			])
 		}))
