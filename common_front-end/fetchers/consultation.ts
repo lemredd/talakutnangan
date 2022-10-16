0c09d8c5a3534b6316f833479d0e6c96b3c1c6e6
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-16 13:35:52 +0800

    intrn(fetcher): make method to request generation of consultation as PDF

--- common_front-end/fetchers/consultation.ts
+++ common_front-end/fetchers/consultation.ts
@@ -1,4 +1,6 @@
+import type { Response } from "$@/types/independent"
 import type { ConsultationQueryParameters } from "$/types/query"
+import type { AsynchronousFileDocument } from "$/types/documents/asynchronous_file"
 import type {
 	ConsultationResourceIdentifier,
 	ConsultationAttributes,
@@ -13,6 +15,7 @@ import type {
 import { CONSULTATION_LINK } from "$/constants/template_links"
 
 import BaseFetcher from "$@/fetchers/base"
+import specializePath from "$/helpers/specialize_path"
 
 export default class ConsultationFetcher extends BaseFetcher<
 	ConsultationResourceIdentifier<"read">,
@@ -30,5 +33,26 @@ export default class ConsultationFetcher extends BaseFetcher<
 > {
 	constructor() {
 		super(CONSULTATION_LINK)
+	}
+
+	requestAsPDF(id: string): Promise<Response<
+		ConsultationResourceIdentifier,
+		ConsultationAttributes<"serialized">,
+		ConsultationAttributes<"deserialized">,
+		ConsultationResource,
+		DeserializedConsultationResource,
+		AsynchronousFileDocument<"read", "deserialized">
+	>> {
+		return this.handleResponse(
+			this.postJSON(specializePath(`${this.links.bound}/request/as_pdf`, { id }), {}),
+			true
+		) as Promise<Response<
+			ConsultationResourceIdentifier,
+			ConsultationAttributes<"serialized">,
+			ConsultationAttributes<"deserialized">,
+			ConsultationResource,
+			DeserializedConsultationResource,
+			AsynchronousFileDocument<"read", "deserialized">
+		>>
 	}
 }
