e52b1ec1e8cf3094671192c838125ec56002e054
Angelo Magtoto <19201.magtoto.angelo.l@gmail.com>
2022-10-18 14:56:41 +0800

    intrn(query): sift by slug for role

---
+++ database/queries/role/sift_by_slug.ts
@@ -0,0 +1,42 @@
+import type { SlugFilter } from "$/types/query"
+import type { FindOptions } from "%/types/dependent"
+
+import Log from "$!/singletons/log"
+
+import Condition from "%/helpers/condition"
+
+/**
+ * Sift user model which matches a certain column.
+ */
+export default function<T>(
+	currentState: FindOptions<T>,
+	constraints: SlugFilter
+): FindOptions<T> {
+	const newState = { ...currentState }
+
+	switch (constraints.filter.slug) {
+		case "":
+			// Do nothing
+			break
+		default: {
+			const condition = new Condition()
+			condition.or(
+				new Condition().search("name", constraints.filter.slug)
+			)
+
+			if (typeof newState.where === "undefined") {
+				newState.where = {}
+			}
+
+			newState.where = {
+				...newState.where,
+				...condition.build()
+			}
+			break
+		}
+	}
+
+	Log.trace("pipeline", "sift by slug")
+
+	return newState
+}
