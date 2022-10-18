e80f76343165548625deb26c807be52e9eeff621
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-18 23:16:59 +0800

    unit(resource): ensure updating department works

--- components/resource_management/resource_manager.spec.ts
+++ components/resource_management/resource_manager.spec.ts
@@ -1,11 +1,8 @@
-/* eslint-disable vue/sort-keys */
-import { mount } from "@vue/test-utils"
+import { shallowMount } from "@vue/test-utils"
 
 import type { DeserializedUserProfile } from "$/types/documents/user"
 
-import ResourceManager from "./resource_manager.vue"
 
-import "~/setups/database.setup"
 import Manager from "$/helpers/manager"
 import RoleFactory from "~/factories/role"
 import UserFactory from "~/factories/user"
@@ -17,58 +14,34 @@ import {
 	READ_ANYONE_ON_ALL_DEPARTMENTS
 } from "$/permissions/user_combinations"
 
-describe("UI Component: Resource Manager", () => {
-	describe("User Management", () => {
-		it("Should identify if resource type is of user profile", async() => {
-			async function generateUser() {
-				const department = await new DepartmentFactory().mayAdmit().insertOne()
-				const role = await new RoleFactory()
-				.userFlags(permissionGroup.generateMask(...READ_ANYONE_ON_OWN_DEPARTMENT))
-				.insertOne()
+import Component from "./resource_manager.vue"
 
-				return (
-					await new UserFactory()
-					.in(department)
-					.attach(role)
-					.deserializedMany(5)
-				).data
-			}
-
-			const department = await new DepartmentFactory().mayAdmit()
-			.insertOne()
-			const deanRole = await new RoleFactory()
-			.userFlags(permissionGroup.generateMask(...READ_ANYONE_ON_OWN_DEPARTMENT))
-			.insertOne()
-			const user = await new UserFactory().in(department)
-			.attach(deanRole)
-			.deserializedOne()
-
-			const sampleUserList = await generateUser()
-			const deserializedRoles = await new RoleFactory().deserializedMany(9)
-
-			const wrapper = mount(ResourceManager as object, {
-				"props": {
-					"isLoaded": true,
-					"resource": sampleUserList,
-					"isResourceTypeUser": true
-				},
-				"global": {
-					"provide": {
-						"pageContext": {
-							"pageProps": {
-								"roles": deserializedRoles
-							}
-						},
-						"managerKind": new Manager(
-							user as DeserializedUserProfile<"roles" | "department">
-						)
+describe("UI Component: resource_management/resource_manager", () => {
+	it.only("should update department", async() => {
+		const departmentID = "1"
+		const wrapper = shallowMount(Component as object, {
+			"props": {
+				"chosenDepartment": "",
+				"departmentNames": [
+					{
+						"label": "Hello",
+						"value": "2"
+					},
+					{
+						"label": "World",
+						"value": departmentID
 					}
-				}
-			})
-
-			const filters = wrapper.find(".filters")
-			expect(filters.exists()).toBeTruthy()
+				],
+				"isLoaded": true,
+				"roleNames": []
+			}
 		})
+
+		const departmentOptions = wrapper.findComponent({ "name": "SelectableOptionsField" })
+		await departmentOptions.setValue(departmentID)
+
+		const updateDepartment = wrapper.emitted("update:chosenDepartment")
+		expect(updateDepartment).toHaveProperty("0.0", departmentID)
 	})
 
 	describe("Additional Filters", () => {
@@ -114,7 +87,7 @@ describe("UI Component: Resource Manager", () => {
 			.mayAdmit()
 			.deserializedMany(7)
 
-			const wrapper = mount(ResourceManager, {
+			const wrapper = shallowMount(Component, {
 				"shallow": true,
 				"props": {
 					"isLoaded": true,
@@ -184,7 +157,7 @@ describe("UI Component: Resource Manager", () => {
 			.mayAdmit()
 			.deserializedMany(7)
 
-			const wrapper = mount(ResourceManager, {
+			const wrapper = shallowMount(Component, {
 				"shallow": true,
 				"props": {
 					"isLoaded": true,
