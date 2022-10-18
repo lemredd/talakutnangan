4d274469c47b4eeb356c02ecf11c067876094154
Angelo Magtoto <19201.magtoto.angelo.l@gmail.com>
2022-10-18 14:58:16 +0800

    chore(query): lint

--- database/queries/user/sift_by_slug.spec.ts
+++ database/queries/user/sift_by_slug.spec.ts
@@ -4,11 +4,11 @@ import UserFactory from "~/factories/user"
 import siftBySlug from "./sift_by_slug"
 
 describe("Database Pipe: Sift by slug", () => {
-	it("can find all", async () => {
+	it("can find all", async() => {
 		const user = await new UserFactory().insertOne()
 		const slug = ""
 
-		const options = siftBySlug({}, { filter: { slug } })
+		const options = siftBySlug({}, { "filter": { slug } })
 		const foundUsers = await User.findAll(options)
 
 		expect(options).not.toHaveProperty("include")
@@ -16,18 +16,18 @@ describe("Database Pipe: Sift by slug", () => {
 		expect(foundUsers).toHaveProperty("0.id", user.id)
 	})
 
-	it("can find on specific using name", async () => {
+	it("can find on specific using name", async() => {
 		const user = await new UserFactory()
-			.name(() => "alice")
-			.email(() => "zzzz@example.com")
-			.insertOne()
-		const otherUser = await new UserFactory()
-			.name(() => "bob")
-			.email(() => "yyyy@example.com")
-			.insertOne()
+		.name(() => "alice")
+		.email(() => "zzzz@example.com")
+		.insertOne()
+		await new UserFactory()
+		.name(() => "bob")
+		.email(() => "yyyy@example.com")
+		.insertOne()
 		const slug = "ali"
 
-		const options = siftBySlug({}, { filter: { slug } })
+		const options = siftBySlug({}, { "filter": { slug } })
 		const foundUsers = await User.findAll(options)
 
 		expect(options).toHaveProperty("where")
@@ -35,18 +35,18 @@ describe("Database Pipe: Sift by slug", () => {
 		expect(foundUsers).toHaveProperty("0.id", user.id)
 	})
 
-	it("can find on specific using email", async () => {
-		const user = await new UserFactory()
-			.name(() => "alice")
-			.email(() => "zzzz@example.com")
-			.insertOne()
+	it("can find on specific using email", async() => {
+		await new UserFactory()
+		.name(() => "alice")
+		.email(() => "zzzz@example.com")
+		.insertOne()
 		const otherUser = await new UserFactory()
-			.name(() => "bob")
-			.email(() => "yyyy@example.com")
-			.insertOne()
+		.name(() => "bob")
+		.email(() => "yyyy@example.com")
+		.insertOne()
 		const slug = "yyy"
 
-		const options = siftBySlug({}, { filter: { slug } })
+		const options = siftBySlug({}, { "filter": { slug } })
 		const foundUsers = await User.findAll(options)
 
 		expect(options).toHaveProperty("where")
@@ -54,18 +54,18 @@ describe("Database Pipe: Sift by slug", () => {
 		expect(foundUsers).toHaveProperty("0.id", otherUser.id)
 	})
 
-	it("cannot find on incorrect slug", async () => {
-		const user = await new UserFactory()
-			.name(() => "alice")
-			.email(() => "alice@example.com")
-			.insertOne()
-		const otherUser = await new UserFactory()
-			.name(() => "bob")
-			.email(() => "bob@example.com")
-			.insertOne()
+	it("cannot find on incorrect slug", async() => {
+		await new UserFactory()
+		.name(() => "alice")
+		.email(() => "alice@example.com")
+		.insertOne()
+		await new UserFactory()
+		.name(() => "bob")
+		.email(() => "bob@example.com")
+		.insertOne()
 		const slug = "xx"
 
-		const options = siftBySlug({}, { filter: { slug } })
+		const options = siftBySlug({}, { "filter": { slug } })
 		const foundUsers = await User.findAll(options)
 
 		expect(options).toHaveProperty("where")
