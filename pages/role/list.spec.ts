c18fefa5f9437e5885bfc674ec0ed901ebd1c68e
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-19 01:39:13 +0800

    intg(role): ensure listing new roles works properly

--- pages/role/list.spec.ts
+++ pages/role/list.spec.ts
@@ -1,8 +1,9 @@
-import { mount } from "@vue/test-utils"
+import { flushPromises, mount } from "@vue/test-utils"
 
 import type { DeserializedUserProfile } from "$/types/documents/user"
 
-import Manager from "$/helpers/manager"
+import { DEBOUNCED_WAIT_DURATION } from "$@/constants/time"
+
 import RoleFactory from "~/factories/role"
 import UserFactory from "~/factories/user"
 import DepartmentFactory from "~/factories/department"
@@ -23,7 +24,7 @@ import Page from "./list.page.vue"
 
 describe("Page: role/list", () => {
 	describe("Basic rendering", () => {
-		it.only("should display correct header for admin", async() => {
+		it("should display correct header for admin", async() => {
 			const departmentFactory = new DepartmentFactory()
 			const department = await departmentFactory.mayNotAdmit()
 			.insertOne()
@@ -80,8 +81,87 @@ describe("Page: role/list", () => {
 			})
 
 			const header = wrapper.find("header h1")
+			await flushPromises()
 
 			expect(header.text()).toContain("Admin")
+
+			const castFetch = fetch as jest.Mock<any, any>
+			expect(castFetch).toHaveBeenCalledTimes(3)
+		})
+	})
+
+	describe("Basic selection", () => {
+		it("can select specific department", async() => {
+			const departmentFactory = new DepartmentFactory()
+			const department = await departmentFactory.mayNotAdmit()
+			.insertOne()
+			const roleFactory = new RoleFactory()
+			const role = await roleFactory
+			.userFlags(userPermissionGroup.generateMask(...READ_ANYONE_ON_ALL_DEPARTMENTS))
+			.departmentFlags(departmentPermissionGroup.generateMask(...UPDATE_DEPARTMENTS))
+			.roleFlags(rolePermissionGroup.generateMask(...UPDATE_ROLES))
+			.insertOne()
+			const user = await new UserFactory()
+			.in(department)
+			.beUnreachableEmployee()
+			.attach(role)
+			.deserializedOne()
+			const userProfile = user as DeserializedUserProfile<"roles"|"department">
+
+			fetchMock.mockResponseOnce(
+				JSON.stringify({
+					"data": [
+						{
+							"id": String(role.id),
+							"meta": {
+								"userCount": 1
+							}
+						}
+					]
+				}),
+				{ "status": RequestEnvironment.status.OK }
+			)
+			fetchMock.mockResponseOnce(
+				JSON.stringify({
+					"data": []
+				}),
+				{ "status": RequestEnvironment.status.OK }
+			)
+			fetchMock.mockResponseOnce(
+				JSON.stringify({
+					"data": []
+				}),
+				{ "status": RequestEnvironment.status.OK }
+			)
+			fetchMock.mockResponseOnce(
+				JSON.stringify({
+					"data": []
+				}),
+				{ "status": RequestEnvironment.status.OK }
+			)
+			jest.useFakeTimers()
+			const wrapper = mount(Page, {
+				"global": {
+					"provide": {
+						"pageContext": {
+							"pageProps": {
+								"departments": await departmentFactory.deserialize([ department ]),
+								"roles": await roleFactory.deserialize([ role ]),
+								userProfile
+							}
+						}
+					}
+				}
+			})
+
+			await flushPromises()
+			const select = wrapper.find("select")
+			await select.setValue(String(department.id))
+			await jest.advanceTimersByTime(DEBOUNCED_WAIT_DURATION)
+			await flushPromises()
+
+			const castFetch = fetch as jest.Mock<any, any>
+			expect(castFetch).toHaveBeenCalledTimes(4)
 		})
 	})
 })
