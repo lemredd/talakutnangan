d2951d652a7d7ac04d1605cf1abf4a9bed72b41f
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-10-19 15:52:02 +0800

    types(consultation): add type for consultations
    Co-authored-by: Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>

--- shareable/types/documents/consolidated_time.ts
+++ shareable/types/documents/consolidated_time.ts
@@ -1,8 +1,10 @@
 import type { Serializable } from "$/types/general"
 import type { MetaDocument } from "$/types/documents/base"
+import { DeserializedConsultationListDocument } from "./consultation"
 
 interface SummedTimeMetaProperties extends Serializable {
 	totalMillisecondsConsumed: number
+	consultations: DeserializedConsultationListDocument
 }
 
 export type SummedTimeDocument = MetaDocument<SummedTimeMetaProperties>
