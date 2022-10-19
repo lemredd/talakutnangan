10698b9cdda6634170809bed9e267a0457ddf2f6
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-10-19 14:36:38 +0800

    feat(consultation): make page for report route

---
+++ pages/consultation/report.page.vue
@@ -0,0 +1,16 @@
+<template>
+	hello
+</template>
+
+<style>
+
+</style>
+
+<script setup lang="ts">
+import { inject } from "vue"
+
+import type { PageContext } from "$/types/renderer"
+
+const pageContext = inject("pageContext") as PageContext<"deserialized", "consultations">
+console.log(pageContext.pageProps.consultations)
+</script>
