6b32e3617373f7161b287a6de5da41ab29d1a9a4
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-10-19 15:44:27 +0800

    fix(server): pre-load consultations by `userID`
    Co-authored-by: Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>

--- routes/enhancer/consultation/report(id).get.ts
+++ routes/enhancer/consultation/report(id).get.ts
@@ -1,24 +1,17 @@
 import type { DocumentProps } from "$/types/server"
 import type { Serializable } from "$/types/general"
 import type { AuthenticatedRequest } from "!/types/dependent"
-import type { DeserializedUserProfile } from "$/types/documents/user"
-import type { ConsultationDocument, ConsultationListDocument } from "$/types/documents/consultation"
+import type { UserIdentifierListWithTimeConsumedDocument } from "$/types/documents/user"
 
 import Policy from "!/bases/policy"
 import Validation from "!/bases/validation"
-import Middleware from "!/bases/middleware"
 import Manager from "%/managers/consultation"
-import URLMaker from "$!/singletons/url_maker"
-import deserialize from "$/object/deserialize"
+import UserManager from "%/managers/user"
 import present from "!/validators/manager/present"
-import makeUnique from "$/array/make_unique"
 import Merger from "!/middlewares/miscellaneous/merger"
-import ChatMessageManager from "%/managers/chat_message"
 import IDParameterValidator from "!/validations/id_parameter"
 import PageMiddleware from "!/bases/controller-likes/page_middleware"
 import CommonMiddlewareList from "!/middlewares/common_middleware_list"
-import ChatMessageActivityManager from "%/managers/chat_message_activity"
-import DynamicGatedRedirector from "!/middlewares/miscellaneous/dynamic_gated_redirector"
 
 export default class extends PageMiddleware {
 	get filePath(): string { return __filename }
@@ -34,27 +27,8 @@ export default class extends PageMiddleware {
 		return [
 			// TODO: Allow adding other rules to check if consultation belongs to current user
 			new IDParameterValidator([
-				[ "id", Manager, present ]
+				[ "id", UserManager, present ]
 			])
-		]
-	}
-
-	get postValidationMiddlewares(): Middleware[] {
-		return [
-			new DynamicGatedRedirector(async(request: AuthenticatedRequest) => {
-				const { id } = request.params
-				const manager = new Manager(request)
-
-				const consultation = await manager.findWithID(Number(id))
-
-				if (consultation.data === null) {
-					return {
-						"location": URLMaker.makeURLFromPath("/consultation")
-					}
-				}
-
-				return null
-			})
 		]
 	}
 
@@ -66,76 +40,27 @@ export default class extends PageMiddleware {
 	}
 
 	async getPageProps(request: AuthenticatedRequest): Promise<Serializable> {
-		const NORMAL_MAXIMUM_CONSULTATION_COUNT = 10
-		const user = deserialize(request.user) as DeserializedUserProfile
-
 		const { id } = request.params
 		const manager = new Manager(request)
 
-		const consultations = await manager.list({
+		const consultations = await manager.sumTimePerStudents({
 			"filter": {
-				"consultationScheduleRange": "*",
+				"dateTimeRange": {
+					"begin": new Date("2022-09-01T00:00:00"),
+					"end": new Date("2022-09-30T11:59:59")
+				},
 				"existence": "exists",
-				"user": Number(user.data.id)
-			},
-			"page": {
-				"limit": NORMAL_MAXIMUM_CONSULTATION_COUNT,
-				"offset": 0
-			},
-			"sort": [ "-updatedAt" ]
-		}) as ConsultationListDocument
-		const consultation = await manager.findWithID(Number(id)) as ConsultationDocument
-
-		const consultationIDs = consultations.data.map(
-			consultationResource => Number(consultationResource.id)
-		)
-		const allConsultationIDs = makeUnique([
-			Number(consultation.data.id),
-			...consultationIDs
-		])
-
-		const INCLUSIVE_CONSULTATION_COUNT = allConsultationIDs.length
-		const MAXIMUM_STUDENT_COUNT = 5
-		const MAXIMUM_REACHABLE_EMPLOYEE_COUNT = 1
-		const MAXIMUM_PARTICIPANT_COUNT = MAXIMUM_STUDENT_COUNT + MAXIMUM_REACHABLE_EMPLOYEE_COUNT
-
-		const chatMessageActivityManager = new ChatMessageActivityManager(request)
-		const chatMessageActivities = await chatMessageActivityManager.list({
-			"filter": {
-				"consultationIDs": allConsultationIDs,
-				"existence": "*"
-			},
-			"page": {
-				"limit": INCLUSIVE_CONSULTATION_COUNT * MAXIMUM_PARTICIPANT_COUNT,
-				"offset": 0
-			},
-			"sort": [ "id" ]
-		})
-
-
-		const chatMessageManager = new ChatMessageManager(request)
-		const chatMessages = await chatMessageManager.list({
-			"filter": {
-				"chatMessageKinds": "*",
-				"consultationIDs": [ Number(consultation.data.id) ],
-				"existence": "exists",
-				"previewMessageOnly": false
+				"user": id
 			},
 			"page": {
 				"limit": 10,
 				"offset": 0
 			},
-			"sort": [ "-createdAt" ]
-		})
-
-		const previewMessages = await chatMessageManager.findPreviews(allConsultationIDs)
+			"sort": [ "-name" ]
+		}) as UserIdentifierListWithTimeConsumedDocument
 
 		return {
-			chatMessageActivities,
-			chatMessages,
-			consultation,
-			consultations,
-			previewMessages
+			consultations
 		}
 	}
 }
