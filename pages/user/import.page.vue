3c530c3843d48cb6baed36347a50a4469498266b
Ardrin Gregorio <kiritogregorio@gmail.com>
2022-10-19 21:30:12 +0800

    intrn(user): add more replacements
    Co-authored-by: Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>

--- pages/user/import.page.vue
+++ pages/user/import.page.vue
@@ -186,13 +186,26 @@ function importData(event: Event) {
 			const { errors } = body
 			receivedErrors.value = errors.map((error: UnitError) => {
 				const readableDetail = error.detail
+				// TODO(others): Generalize replacing substring using regex
 				.replace(
-					/ in field "meta\.importedCSV\.\d+\.(email|name)"/u,
+					/^(The |Field )/u,
 					""
 				)
 				.replace(
+					/( in field )?"meta\.importedCSV\.\d+\.(email|name|studentNumber)?"/u,
+					" is a $2"
+				)
+				.replace(
+					/^ is a studentNumber/u,
+					"Student number"
+				)
+				.replace(
+					/must match ".+?"/u,
+					" must be valid"
+				)
+				.replace(
 					/ does exists in the database"/u,
-					" already exists"
+					" that already exists"
 				)
 
 				return readableDetail
