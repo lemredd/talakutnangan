5eab36559c00ac1e1098a77bcc9ed45eae2645bc
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-10-20 18:04:49 +0800

    chore(server): rename route

--- routes/enhancer/consultation/report.get.ts
+++ routes/enhancer/consultation/report.get.ts
@@ -1,15 +1,15 @@
 import type { DocumentProps } from "$/types/server"
 import type { Serializable } from "$/types/general"
 import type { AuthenticatedRequest } from "!/types/dependent"
-import type { UserIdentifierListWithTimeConsumedDocument } from "$/types/documents/user"
+import type {
+	DeserializedUserProfile,
+	UserIdentifierListWithTimeConsumedDocument
+} from "$/types/documents/user"
+import deserialize from "$/object/deserialize"
 
 import Policy from "!/bases/policy"
-import Validation from "!/bases/validation"
 import Manager from "%/managers/consultation"
-import UserManager from "%/managers/user"
-import present from "!/validators/manager/present"
 import Merger from "!/middlewares/miscellaneous/merger"
-import IDParameterValidator from "!/validations/id_parameter"
 import PageMiddleware from "!/bases/controller-likes/page_middleware"
 import CommonMiddlewareList from "!/middlewares/common_middleware_list"
 
@@ -23,15 +23,6 @@ export default class extends PageMiddleware {
 		]) as unknown as Policy
 	}
 
-	get validations(): Validation[] {
-		return [
-			// TODO: Allow adding other rules to check if consultation belongs to current user
-			new IDParameterValidator([
-				[ "id", UserManager, present ]
-			])
-		]
-	}
-
 	getDocumentProps(): DocumentProps {
 		return {
 			"description": "Consultation chat platform for MCC",
@@ -39,9 +30,9 @@ export default class extends PageMiddleware {
 		}
 	}
 
-	async getPageProps(request: AuthenticatedRequest): Promise<Serializable> {
-		const { id } = request.params
+	async getPageProps(request: AuthenticatedRequest): Promise<Serializable | any> {
 		const manager = new Manager(request)
+		const user = deserialize(request.user) as DeserializedUserProfile
 
 		const totalMillisecondsConsumed = await manager.sumTimePerStudents({
 			"filter": {
@@ -50,7 +41,7 @@ export default class extends PageMiddleware {
 					"end": new Date("2022-10-30T11:59:59")
 				},
 				"existence": "exists",
-				"user": id
+				"user": Number(user.data.id)
 			},
 			"page": {
 				"limit": 10,
@@ -58,6 +49,8 @@ export default class extends PageMiddleware {
 			},
 			"sort": [ "-name" ]
 		}) as UserIdentifierListWithTimeConsumedDocument
+
+		console.log(totalMillisecondsConsumed)
 
 		return {
 			totalMillisecondsConsumed
--- routes/enhancer/consultation/router.ts
+++ routes/enhancer/consultation/router.ts
@@ -2,7 +2,7 @@ import Router from "!/bases/router"
 import GetIndexRoute from "!%/enhancer/consultation/index.get"
 import GetFormRoute from "!%/enhancer/consultation/form(id).get"
 import GetReadRoute from "!%/enhancer/consultation/read(id).get"
-import GetReportRoute from "!%/enhancer/consultation/report(id).get"
+import GetReportRoute from "!%/enhancer/consultation/report.get"
 
 export default class extends Router {
 	constructor() {
