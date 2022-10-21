429455ac7676a74b6e00cb3d9afa5aded54d008f
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-21 18:45:00 +0800

    intrn(post): prepare page to show the post and its comments

---
+++ pages/post/read.page.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="post-container">
+	</div>
+</template>
+
+<style lang="scss">
+</style>
+
+<script setup lang="ts">
+import { provide, inject, ref, readonly, computed, onMounted } from "vue"
+
+import type { PageContext } from "$/types/renderer"
+
+import makeSwitch from "$@/helpers/make_switch"
+
+type RequiredExtraProps =
+	| "userProfile"
+	| "post"
+	| "comment"
+const pageContext = inject("pageContext") as PageContext<"deserialized", RequiredExtraProps>
+const { pageProps } = pageContext
+
+const { userProfile } = pageProps
+
+</script>
