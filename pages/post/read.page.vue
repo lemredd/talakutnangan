795693956c7957d3144592d0f2b9325808f0f4cf
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-21 23:55:49 +0800

    intrn(comment): modify the page for viewing comments

--- pages/post/read.page.vue
+++ pages/post/read.page.vue
@@ -1,5 +1,7 @@
 <template>
 	<div class="post-container">
+		<Viewer v-model="post"/>
+		<Multiviewer v-model="comments"/>
 	</div>
 </template>
 
@@ -7,19 +9,30 @@
 </style>
 
 <script setup lang="ts">
-import { provide, inject, ref, readonly, computed, onMounted } from "vue"
+import { provide, inject, ref, computed } from "vue"
 
 import type { PageContext } from "$/types/renderer"
+import type { DeserializedPostResource } from "$/types/documents/post"
+import type { DeserializedCommentResource } from "$/types/documents/comment"
 
 import makeSwitch from "$@/helpers/make_switch"
+
+import Multiviewer from "@/comment/multiviewer.vue"
+import Viewer from "@/post/multiviewer/viewer.vue"
 
 type RequiredExtraProps =
 	| "userProfile"
 	| "post"
-	| "comment"
+	| "comments"
 const pageContext = inject("pageContext") as PageContext<"deserialized", RequiredExtraProps>
 const { pageProps } = pageContext
 
 const { userProfile } = pageProps
+const post = ref<DeserializedPostResource<"poster"|"posterRole">>(
+	pageProps.post.data as DeserializedPostResource<"poster"|"posterRole">
+)
+const comments = ref<DeserializedCommentResource<"user"|"parentComment">[]>(
+	pageProps.comments.data as DeserializedCommentResource<"user"|"parentComment">[]
+)
 
 </script>
