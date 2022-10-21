9983779e58b2585bad2e7b58f4c4675744902119
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-10-21 18:22:35 +0800

    intrn(server): register read route for user

--- routes/enhancer/user/router.ts
+++ routes/enhancer/user/router.ts
@@ -1,5 +1,6 @@
 import Router from "!/bases/router"
 import GetList from "!%/enhancer/user/list.get"
+import GetRead from "!%/enhancer/user/read(id).get"
 import GetImport from "!%/enhancer/user/import.get"
 import GetLogIn from "!%/enhancer/user/log_in.get"
 import GetVerify from "!%/enhancer/user/verify(data).get"
@@ -11,6 +12,7 @@ export default class extends Router {
 		this.useControllersAsync(new Promise(resolve => {
 			resolve([
 				new GetList(),
+				new GetRead(),
 				new GetLogIn(),
 				new GetImport(),
 				new GetVerify()
