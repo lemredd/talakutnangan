e7c4e6e43f79762b69b5ddd9beb8375fbe0eadac
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-22 22:14:49 +0800

    intrn(comment): use field status to control the comment

--- components/comment/field.vue
+++ components/comment/field.vue
@@ -3,7 +3,7 @@
 		<TextualField
 			v-model="content"
 			type="text"
-			:editable="editable"
+			:status="fieldStatus"
 			:may-save-implicitly="true"
 			@save-implicitly="submit"
 			@save="submit"/>
@@ -21,18 +21,21 @@
 <script setup lang="ts">
 import { computed } from "vue"
 
+import type { FieldStatus } from "@/fields/types"
 import type { DeserializedCommentResource } from "$/types/documents/comment"
 
 import TextualField from "@/fields/non-sensitive_text.vue"
 
 interface CustomEvents {
 	(event: "update:modelValue", data: string): void
+	(event: "update:status", status: FieldStatus): void
 	(event: "submitComment"): void
 }
 const emit = defineEmits<CustomEvents>()
 const props = defineProps<{
 	parentComment?: DeserializedCommentResource,
 	editable?: boolean,
+	status: FieldStatus,
 	modelValue: string
 }>()
 
@@ -42,6 +45,14 @@ const content = computed<string>({
 	},
 	set(newValue: string): void {
 		emit("update:modelValue", newValue)
+	}
+})
+const fieldStatus = computed<FieldStatus>({
+	get(): FieldStatus {
+		return props.status
+	},
+	set(newValue: FieldStatus): void {
+		emit("update:status", newValue)
 	}
 })
 
