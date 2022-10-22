c87f5cf4096b578a36bf56331c72903efa9d9d0e
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-22 22:43:58 +0800

    unit(comment): expect comment will require user

--- components/comment/field.spec.ts
+++ components/comment/field.spec.ts
@@ -6,9 +6,18 @@ import Component from "./field.vue"
 describe("Component: comment/field", () => {
 	it("may submit independently", async() => {
 		const content = "Hello world"
+		const user = {
+			"data": {
+				"id": "1",
+				"type": "user"
+			}
+		}
+
 		const wrapper = shallowMount<any>(Component, {
 			"props": {
-				"modelValue": content
+				"modelValue": content,
+				"status": "unlocked",
+				user
 			}
 		})
 
@@ -29,11 +38,19 @@ describe("Component: comment/field", () => {
 			"id": parentCommentID,
 			"type": "comment"
 		}
+		const user = {
+			"data": {
+				"id": "1",
+				"type": "user"
+			}
+		}
 
 		const wrapper = shallowMount<any>(Component, {
 			"props": {
 				"modelValue": content,
-				parentComment
+				parentComment,
+				"status": "unlocked",
+				user
 			}
 		})
 
