ca2a7414bb273a410eaff90ceca8a566d1bce9f0
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-22 18:50:11 +0800

    intrn(post): allow commenting if possible and permitted

--- pages/post/read.page.vue
+++ pages/post/read.page.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="post-container">
 		<Viewer v-model="post"/>
-		<CreateField :post="post"/>
+		<CreateField v-if="mayCreateComment" :post="post"/>
 		<Multiviewer v-model="comments"/>
 	</div>
 </template>
@@ -10,13 +10,18 @@
 </style>
 
 <script setup lang="ts">
-import { provide, inject, ref, computed } from "vue"
+import { inject, ref } from "vue"
 
 import type { PageContext } from "$/types/renderer"
 import type { DeserializedPostResource } from "$/types/documents/post"
 import type { DeserializedCommentResource } from "$/types/documents/comment"
 
-import makeSwitch from "$@/helpers/make_switch"
+import { comment as permissionGroup } from "$/permissions/permission_list"
+import {
+	CREATE_SOCIAL_COMMENT_ON_OWN_DEPARTMENT,
+	CREATE_PUBLIC_COMMENT_ON_ANY_DEPARTMENT,
+	CREATE_PERSONAL_COMMENT_ON_OWN_DEPARTMENT
+} from "$/permissions/comment_combinations"
 
 import Multiviewer from "@/comment/multiviewer.vue"
 import Viewer from "@/post/multiviewer/viewer.vue"
@@ -36,5 +41,28 @@ const post = ref<DeserializedPostResource<"poster"|"posterRole"|"department">>(
 const comments = ref<DeserializedCommentResource<"user"|"parentComment">[]>(
 	pageProps.comments.data as DeserializedCommentResource<"user"|"parentComment">[]
 )
+
+// TODO: Correct the specialization
+const mayCreateComment = computed<boolean>(() => {
+	const isLimitedPersonalScope = permissionGroup.hasOneRoleAllowed(userProfile.data.roles.data, [
+		CREATE_PERSONAL_COMMENT_ON_OWN_DEPARTMENT
+	])
+
+	const isLimitedUpToDepartmentScope = !isLimitedPersonalScope
+		&& permissionGroup.hasOneRoleAllowed(userProfile.data.roles.data, [
+			CREATE_SOCIAL_COMMENT_ON_OWN_DEPARTMENT
+		])
+
+	const isLimitedUpToGlobalScope = !isLimitedUpToDepartmentScope
+		&& permissionGroup.hasOneRoleAllowed(userProfile.data.roles.data, [
+			CREATE_PUBLIC_COMMENT_ON_ANY_DEPARTMENT
+		])
+
+	const isPermitted = isLimitedPersonalScope
+	|| isLimitedUpToDepartmentScope
+	|| isLimitedUpToGlobalScope
+
+	return isPermitted && post.value.deletedAt === null
+})
 
 </script>
