94e273cc5dc9a862c074dddf20f90fbdd091d61b
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-22 17:58:26 +0800

    fix(comment): include the information about the related IDs

--- routes/api/comment/create.post.ts
+++ routes/api/comment/create.post.ts
@@ -1,10 +1,11 @@
 import type { Rules, FieldRules } from "!/types/validation"
 import type { AuthenticatedRequest, Response } from "!/types/dependent"
+import type { CommentAttributes, CommentRelationships } from "$/types/documents/comment"
 
 import Policy from "!/bases/policy"
+import Manager from "%/managers/comment"
 import PostManager from "%/managers/post"
 import UserManager from "%/managers/user"
-import Manager from "%/managers/comment"
 import JSONController from "!/controllers/json"
 import CreatedResponseInfo from "!/response_infos/created"
 
@@ -100,7 +101,16 @@ export default class extends JSONController {
 	async handle(request: AuthenticatedRequest, unusedResponse: Response)
 	: Promise<CreatedResponseInfo> {
 		const manager = new Manager(request)
-		const commentInfo = await manager.create(request.body.data.attributes)
+		const attributes = request.body.data.attributes as CommentAttributes<"deserialized">
+		const relationships = request.body.data.relationships as CommentRelationships<
+			"create"
+		>["relationships"]
+		const commentInfo = await manager.create({
+			...attributes,
+			"parentCommentID": relationships.parentComment?.data.id ?? null,
+			"postID": relationships.post.data.id,
+			"userID": relationships.user.data.id
+		})
 
 		return new CreatedResponseInfo(commentInfo)
 	}
