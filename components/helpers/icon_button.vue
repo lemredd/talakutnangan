b86332746830e56da8ac6ff9b9b864d042b33c08
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-23 12:12:35 +0800

    style(component): make icon button

---
+++ components/helpers/icon_button.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<button class="material-icons" @click="emitIconClickEvent">
+			{{ iconName }}
+		</button>
+		<slot name="associated-pop-outs"></slot>
+	</div>
+</template>
+
+<style scoped lang="scss">
+	div {
+		@apply flex flex-row flex-nowrap justify-center items-center h-12 w-12;
+
+		> button:first-child {
+			@apply flex-1 m-auto;
+		}
+	}
+</style>
+
+<script setup lang="ts">
+const emit = defineEmits<{
+	(event: "iconClick"): void
+}>()
+defineProps<{
+	iconName: string
+}>()
+
+function emitIconClickEvent() {
+	emit("iconClick")
+}
+</script>
