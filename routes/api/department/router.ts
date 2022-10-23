9f1a8291359e5d782dc629d2650581cb5dae8ba8
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-24 00:49:05 +0800

    perf(server): optimize department router

--- routes/api/department/router.ts
+++ routes/api/department/router.ts
@@ -1,24 +1,23 @@
 import Router from "!/bases/router"
 import GetList from "!%/api/department/list.get"
 import PostCreate from "!%/api/department/create.post"
-import PatchUpdate from "!%/api/department/update(id).patch"
 import PatchRestore from "!%/api/department/restore.patch"
+import PatchUpdate from "!%/api/department/update(id).patch"
 import DeleteArchive from "!%/api/department/archive.delete"
 import GetCountUsers from "!%/api/department/count_users.get"
+import instantiateSimultaneously from "!/helpers/instantiate_simultaneously"
 
 export default class extends Router {
 	constructor() {
 		super()
 
-		this.useControllersAsync(new Promise(resolve => {
-			resolve([
-				new GetList(),
-				new PostCreate(),
-				new PatchUpdate(),
-				new PatchRestore(),
-				new DeleteArchive(),
-				new GetCountUsers()
-			])
-		}))
+		this.useControllersAsync(instantiateSimultaneously([
+			GetList,
+			PostCreate,
+			PatchUpdate,
+			PatchRestore,
+			DeleteArchive,
+			GetCountUsers
+		]))
 	}
 }
