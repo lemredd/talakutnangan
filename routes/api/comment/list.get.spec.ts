aa0dc975a76380c40d03ebd08c8eb42dad21c187
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-24 00:12:40 +0800

    unit(comment): prepare unit test list comments

---
+++ routes/api/comment/list.get.spec.ts
@@ -0,0 +1,50 @@
+import ErrorBag from "$!/errors/error_bag"
+import MockRequester from "~/setups/mock_requester"
+
+import Controller from "./list.get"
+
+const QUERY_VALIDATION_INDEX = 0
+
+describe("Controller: GET /api/comment", () => {
+	const requester = new MockRequester()
+
+	it("can accept valid info", async() => {
+		const controller = new Controller()
+		const { validations } = controller
+		const queryValidation = validations[QUERY_VALIDATION_INDEX]
+		const queryValidationFunction = queryValidation.intermediate.bind(queryValidation)
+		requester.customizeRequest({
+			"query": {
+				"filter": {
+					"existence": "*",
+					"postID": "1"
+				}
+			}
+		})
+
+		await requester.runMiddleware(queryValidationFunction)
+
+		requester.expectSuccess()
+	})
+
+	it("cannot accept invalid info", async() => {
+		const controller = new Controller()
+		const { validations } = controller
+		const queryValidation = validations[QUERY_VALIDATION_INDEX]
+		const queryValidationFunction = queryValidation.intermediate.bind(queryValidation)
+		requester.customizeRequest({
+			"query": {
+				"filter": {
+					"existence": "hello"
+				}
+			}
+		})
+
+		await requester.runMiddleware(queryValidationFunction)
+
+		const body = requester.expectFailure(ErrorBag).toJSON()
+		expect(body).toHaveLength(2)
+		expect(body).toHaveProperty("0.source.parameter", "filter.existence")
+		expect(body).toHaveProperty("0.source.parameter", "filter.postID")
+	})
+})
