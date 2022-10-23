5d9c8bd27b24dc8f39eaf4d2932e959b1e3ab8ca
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-24 00:51:51 +0800

    perf(server): optimize consultation routers

--- routes/api/consultation(id)/router.ts
+++ routes/api/consultation(id)/router.ts
@@ -1,16 +1,15 @@
 import Router from "!/bases/router"
 import ReadRouter from "!%/api/consultation(id)/read/router"
 import RequestRouter from "!%/api/consultation(id)/request/router"
+import instantiateSimultaneously from "!/helpers/instantiate_simultaneously"
 
 export default class extends Router {
 	constructor() {
 		super()
 
-		this.useRoutersAsync(new Promise(resolve => {
-			resolve([
-				new RequestRouter(),
-				new ReadRouter()
-			])
-		}))
+		this.useRoutersAsync(instantiateSimultaneously([
+			RequestRouter,
+			ReadRouter
+		]))
 	}
 }
--- routes/api/consultation/router.ts
+++ routes/api/consultation/router.ts
@@ -2,6 +2,7 @@ import Router from "!/bases/router"
 import GetList from "!%/api/consultation/list.get"
 import PostCreate from "!%/api/consultation/create.post"
 import PatchUpdate from "!%/api/consultation/update(id).patch"
+import instantiateSimultaneously from "!/helpers/instantiate_simultaneously"
 import GetReadTimeSumByWeek from "!%/api/consultation/read_time_sum_per_week.get"
 import GetReadTimeSumByStudent from "!%/api/consultation/read_time_sum_per_student.get"
 // import PatchRestore from "!%/api/consultation/restore.patch"
@@ -11,16 +12,14 @@ export default class extends Router {
 	constructor() {
 		super()
 
-		this.useControllersAsync(new Promise(resolve => {
-			resolve([
-				new GetList(),
-				new PostCreate(),
-				new PatchUpdate(),
-				new GetReadTimeSumByWeek(),
-				new GetReadTimeSumByStudent()
-				// new PatchRestore(),
-				// new DeleteArchive()
-			])
-		}))
+		this.useControllersAsync(instantiateSimultaneously([
+			GetList,
+			PostCreate,
+			PatchUpdate,
+			GetReadTimeSumByWeek,
+			GetReadTimeSumByStudent
+			// PatchRestore,
+			// DeleteArchive
+		]))
 	}
 }
