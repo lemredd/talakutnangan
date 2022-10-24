f286e673bb7ec8f1a4627bad87d168e64bb3b260
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-24 10:22:31 +0800

    perf(server): optimize registration of user page middlewares

--- routes/enhancer/router.ts
+++ routes/enhancer/router.ts
@@ -1,20 +1,21 @@
 import Router from "!/bases/router"
 import GetIndex from "!%/enhancer/index.get"
-import UserRouter from "!%/enhancer/user/router"
+import { controllers as userControllers } from "!%/enhancer/user/router"
 import RoleRouter from "!%/enhancer/role/router"
 import PostRouter from "!%/enhancer/post/router"
 import ForumRouter from "!%/enhancer/forum/router"
 import DepartmentRouter from "!%/enhancer/department/router"
 import UserSettingsRouter from "!%/enhancer/settings/router"
 import ConsultationRouter from "!%/enhancer/consultation/router"
-
+import instantiateSimultaneously from "!/helpers/instantiate_simultaneously"
 export default class extends Router {
 	constructor() {
 		super()
 
+		this.useControllersAsync(instantiateSimultaneously(userControllers))
+
 		this.useRoutersAsync(new Promise(resolve => {
 			resolve([
-				new UserRouter(),
 				new RoleRouter(),
 				new PostRouter(),
 				new ForumRouter(),
--- routes/enhancer/user/router.ts
+++ routes/enhancer/user/router.ts
@@ -4,6 +4,13 @@ import GetImport from "!%/enhancer/user/import.get"
 import GetLogIn from "!%/enhancer/user/log_in.get"
 import GetVerify from "!%/enhancer/user/verify(data).get"
 
+export const controllers = [
+	GetList,
+	GetLogIn,
+	GetImport,
+	GetVerify
+]
+
 export default class extends Router {
 	constructor() {
 		super()
