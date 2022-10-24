caa9890986f5dbf77a3c14895b6d61a18f841ec2
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-24 10:36:30 +0800

    perf(server): optimize registration of department page middlewares

--- routes/enhancer/router.ts
+++ routes/enhancer/router.ts
@@ -5,7 +5,7 @@ import { controllers as userControllers } from "!%/enhancer/user/router"
 import { controllers as roleControllers } from "!%/enhancer/role/router"
 import { controllers as postControllers } from "!%/enhancer/post/router"
 import { controllers as forumControllers } from "!%/enhancer/forum/router"
-import DepartmentRouter from "!%/enhancer/department/router"
+import { controllers as departmentControllers } from "!%/enhancer/department/router"
 import UserSettingsRouter from "!%/enhancer/settings/router"
 import ConsultationRouter from "!%/enhancer/consultation/router"
 import instantiateSimultaneously from "!/helpers/instantiate_simultaneously"
@@ -17,13 +17,13 @@ export default class extends Router {
 			...userControllers,
 			...roleControllers,
 			...postControllers,
-			...forumControllers
+			...forumControllers,
+			...departmentControllers
 			GetIndex
 		] as (new() => ControllerLike)[]))
 
 		this.useRoutersAsync(new Promise(resolve => {
 			resolve([
-				new DepartmentRouter(),
 				new ConsultationRouter(),
 				new UserSettingsRouter()
 			])
