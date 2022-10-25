2e79a1c0282f4c426c642d14edc8258191edce4a
Angelo Magtoto <19201.magtoto.angelo.l@gmail.com>
2022-10-25 18:10:09 +0800

    feat(user): make selectable radio component
    Co-authored-by: Jarlem Red de Peralta <lmoa.jhdp@gmail.com>

---
+++ components/fields/selectable_radio.vue
@@ -0,0 +1,41 @@
+<template>
+	<div class="selectable-radio">
+		{{ title }}
+		<label
+			v-for="option in options"
+			:key="option.value"
+			for="exists">
+			<input
+				:id="option.label"
+				:value="option.value"
+				type="radio"/>
+			{{ option.label ?? option.value }}
+		</label>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue"
+
+import type { OptionInfo } from "$@/types/component"
+
+type ComponentProps = {
+	title: string,
+	modelValue: string,
+	options: OptionInfo[]
+}
+
+const props = defineProps<ComponentProps>()
+
+const emit = defineEmits<{(e: "update:modelValue", value: string): void}>()
+
+const selectID = computed(() => props.options.map(info => info.value).join(" ").replace(" ", "_"))
+const value = computed({
+	get() {
+		return props.modelValue
+	},
+	set(newValue: string): void {
+		emit("update:modelValue", newValue)
+	}
+})
+</script>
