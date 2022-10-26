8a11be870147b6d8c436b5b115550987030c9013
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-10-26 17:25:38 +0800

    unit(listener): ensure incoming consultations
    Co-authored-by: Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>

--- components/consultation/listeners/register_consultation.spec.ts
+++ components/consultation/listeners/register_consultation.spec.ts
@@ -8,6 +8,7 @@ import type {
 import Stub from "$/singletons/stub"
 import Socket from "$@/external/socket"
 import makeConsultationNamespace from "$/namespace_makers/consultation"
+import makeConsultationListOfUserNamespace from "$/namespace_makers/consultation_list_of_user"
 
 import listener from "./register_consultation"
 
@@ -19,10 +20,12 @@ describe("Listener: Consultation", () => {
 		const consultationNamespace = makeConsultationNamespace(id)
 
 		const consultation = ref({ id }) as unknown
+		const consultations = ref([ consultation ]) as any
 		listener(
 			consultation as Ref<
 				DeserializedConsultationResource<"consultant"|"consultantRole">
 			>,
+			consultations,
 			"1"
 		)
 
@@ -45,5 +48,47 @@ describe("Listener: Consultation", () => {
 		expect(previousCalls).toHaveProperty("1.functionName", "addEventListeners")
 		expect(previousCalls).toHaveProperty("1.arguments.0", consultationNamespace)
 		expect(previousCalls).toHaveProperty("1.arguments.1.update")
+	})
+
+	it.only("can receive incoming consultation", async() => {
+		Socket.initialize()
+
+		const id = "1"
+		const consultationNamespace = makeConsultationNamespace(id)
+		const consultationListOfUserNamespace = makeConsultationListOfUserNamespace(id)
+
+		const consultation = ref({ id }) as unknown
+		const consultations = ref({
+			"data": [ consultation ]
+		}) as any
+		listener(
+			consultation as Ref<
+				DeserializedConsultationResource<"consultant"|"consultantRole">
+			>,
+			consultations,
+			"1"
+		)
+
+		jest.useFakeTimers()
+
+		Socket.emitMockEvent(consultationListOfUserNamespace, "create", {
+			"attributes": {
+				"startedAt": new Date().toJSON()
+			},
+			id,
+			"type": "consultation"
+		})
+		await nextTick()
+
+		console.log(consultationListOfUserNamespace, "\n\n\n")
+		const previousCalls = Stub.consumePreviousCalls()
+		expect(previousCalls).toHaveProperty("0.functionName", "initialize")
+		expect(previousCalls).toHaveProperty("0.arguments", [])
+		expect(previousCalls).toHaveProperty("1.functionName", "addEventListeners")
+		expect(previousCalls).toHaveProperty("1.arguments.0", consultationNamespace)
+		expect(previousCalls).toHaveProperty("1.arguments.1.update")
+		expect(previousCalls).toHaveProperty("2.functionName", "addEventListeners")
+		expect(previousCalls).toHaveProperty("2.arguments.0", consultationListOfUserNamespace)
+		expect(previousCalls).toHaveProperty("2.arguments.1.create")
 	})
 })
