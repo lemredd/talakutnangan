c30160a0add6eed68555e9211cb1b9d3e0a96620
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-26 13:20:04 +0800

    intrn(consultation): make page middleware to preload weekly consultation

---
+++ routes/enhancer/consultation/weekly_report.get.ts
@@ -0,0 +1,54 @@
+import type { DocumentProps } from "$/types/server"
+import type { Serializable } from "$/types/general"
+import type { AuthenticatedRequest } from "!/types/dependent"
+import type { DeserializedUserProfile } from "$/types/documents/user"
+
+import Policy from "!/bases/policy"
+import Manager from "%/managers/consultation"
+import deserialize from "$/object/deserialize"
+import Merger from "!/middlewares/miscellaneous/merger"
+import PageMiddleware from "!/bases/controller-likes/page_middleware"
+import CommonMiddlewareList from "!/middlewares/common_middleware_list"
+
+export default class extends PageMiddleware {
+	get filePath(): string { return __filename }
+
+	get policy(): Policy {
+		return new Merger([
+			CommonMiddlewareList.consultationParticipantsOnlyPolicy,
+			CommonMiddlewareList.reachableEmployeeOnlyPolicy
+		]) as unknown as Policy
+	}
+
+	getDocumentProps(): DocumentProps {
+		return {
+			"description": "Consultation chat platform for MCC",
+			"title": "Weekly Consultation Report | Talakutnangan"
+		}
+	}
+
+	async getPageProps(request: AuthenticatedRequest): Promise<Serializable | any> {
+		const manager = new Manager(request)
+		const user = deserialize(request.user) as DeserializedUserProfile
+
+		const weeklyTotalMillisecondsConsumed = await manager.sumTimePerWeek({
+			"filter": {
+				"dateTimeRange": {
+					"begin": new Date("2022-10-01T00:00:00"),
+					"end": new Date("2022-10-30T11:59:59")
+				},
+				"existence": "exists",
+				"user": Number(user.data.id)
+			},
+			"page": {
+				"limit": 10,
+				"offset": 0
+			},
+			"sort": [ "-name" ]
+		})
+
+		return {
+			weeklyTotalMillisecondsConsumed
+		}
+	}
+}
