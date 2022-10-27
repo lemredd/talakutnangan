1454236beaffc5559176a16943deaf2c0a92ebde
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-27 11:45:38 +0800

    fix(chat message): include missing paths

--- routes/api/chat_message/router.ts
+++ routes/api/chat_message/router.ts
@@ -1,9 +1,15 @@
+import GetList from "!%/api/chat_message/list.get"
 import PostCreate from "!%/api/chat_message/create.post"
+import PatchRestore from "!%/api/chat_message/restore.patch"
+import DeleteArchive from "!%/api/chat_message/archive.delete"
 import PatchUpdate from "!%/api/chat_message/update(id).patch"
 import PostCreateWithFile from "!%/api/chat_message/create_with_file.post"
 
 export const controllers = [
+	GetList,
 	PostCreate,
 	PatchUpdate,
+	PatchRestore,
+	DeleteArchive,
 	PostCreateWithFile
 ]
