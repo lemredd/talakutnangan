41cdcc39fc0627f3ad6121247c6a9863b4f80090
Angelo Magtoto <19201.magtoto.angelo.l@gmail.com>
2022-10-27 16:07:29 +0800

    unit(manager): prepare test to count votes

--- database/managers/comment.spec.ts
+++ database/managers/comment.spec.ts
@@ -1,4 +1,36 @@
+import type { CommentIdentifierListDocumentWithVotes } from "$/types/documents/comment"
+
+import Factory from "~/factories/comment"
+import UserFactory from "~/factories/user"
+import CommentVoteFactory from "~/factories/comment_vote"
+
 import Manager from "./comment"
+
+describe("Database Manager: Comment read operations", () => {
+	it("can read number of votes without self", async() => {
+		const manager = new Manager()
+		const user = await new UserFactory().insertOne()
+		const model = await new Factory().insertOne()
+		const upvoteCount = 4
+		const downvoteCount = 5
+		const commentVotefactory = new CommentVoteFactory()
+		await commentVotefactory
+		.kind(() => "upvote")
+		.comment(() => Promise.resolve(model))
+		.insertMany(upvoteCount)
+		await commentVotefactory
+		.kind(() => "downvote")
+		.comment(() => Promise.resolve(model))
+		.insertMany(downvoteCount)
+
+		const document = await manager.countVotes(user.id, [
+			model.id
+		]) as CommentIdentifierListDocumentWithVotes
+
+		expect(document).toHaveProperty("data.0.meta.upvoteCount", upvoteCount)
+		expect(document).toHaveProperty("data.0.meta.downvoteCount", downvoteCount)
+	})
+})
 
 describe("Database Manager: Miscellaneous comment operations", () => {
 	it("can get sortable columns", () => {
