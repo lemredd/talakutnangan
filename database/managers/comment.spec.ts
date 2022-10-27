667650b059ce1a5dffa439dd5ec2ba737e2d2b94
Angelo Magtoto <19201.magtoto.angelo.l@gmail.com>
2022-10-27 16:21:11 +0800

    unit(manager): ensure user has voted
    Co-authored-by: Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>

--- database/managers/comment.spec.ts
+++ database/managers/comment.spec.ts
@@ -29,6 +29,37 @@ describe("Database Manager: Comment read operations", () => {
 
 		expect(document).toHaveProperty("data.0.meta.upvoteCount", upvoteCount)
 		expect(document).toHaveProperty("data.0.meta.downvoteCount", downvoteCount)
+		expect(document).toHaveProperty("data.0.meta.currentUserVoteStatus", "unvoted")
+	})
+
+	it("can read number of votes with self", async() => {
+		const manager = new Manager()
+		const user = await new UserFactory().insertOne()
+		const model = await new Factory().insertOne()
+		const upvoteCount = 4
+		const downvoteCount = 2
+		const commentVotefactory = new CommentVoteFactory()
+		await commentVotefactory
+		.kind(() => "upvote")
+		.comment(() => Promise.resolve(model))
+		.insertMany(upvoteCount)
+		await commentVotefactory
+		.kind(() => "downvote")
+		.comment(() => Promise.resolve(model))
+		.insertMany(downvoteCount)
+		await commentVotefactory
+		.kind(() => "downvote")
+		.user(() => Promise.resolve(user))
+		.comment(() => Promise.resolve(model))
+		.insertOne()
+
+		const document = await manager.countVotes(user.id, [
+			model.id
+		]) as CommentIdentifierListDocumentWithVotes
+
+		expect(document).toHaveProperty("data.0.meta.upvoteCount", upvoteCount)
+		expect(document).toHaveProperty("data.0.meta.downvoteCount", downvoteCount + 1)
+		expect(document).toHaveProperty("data.0.meta.currentUserVoteStatus", "downvoted")
 	})
 })
 
