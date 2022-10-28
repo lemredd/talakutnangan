49a42200fef9535e3625fd704fd4862b1a46e74c
Ardrin Gregorio <kiritogregorio@gmail.com>
2022-10-28 13:14:17 +0800

    rfmt(component): revert decomposed state
    This reverts commit e559baf62566b958e140633a9c0dc1139e5ce855.

---
+++ components/consultation/chat_window/extra_controls.vue
@@ -0,0 +1,52 @@
+<template>
+	<Dropdown
+		:is-dropdown-shown="isHeaderControlDropdownShown"
+		class="additional-controls"
+		@toggle="toggleHeaderControlDropdownShown">
+		<template #toggler>
+			<button class="material-icons toggle-controls-btn">
+				more_horiz
+			</button>
+		</template>
+
+		<template #dropdown-contents>
+			<div class="links">
+				<a
+					href="#"
+					class="additional-control view-printable-form-btn">
+					View consultation form
+				</a>
+				<a
+					v-if="isCurrentUserConsultant"
+					href="#"
+					class="additional-control view-action-taken-overlay-btn"
+					@click="showActionTakenOverlay">Finish consultation</a>
+			</div>
+		</template>
+	</Dropdown>
+</template>
+
+<script setup lang="ts">
+
+import Dropdown from "@/page_shell/dropdown.vue"
+
+defineProps<{
+	isHeaderControlDropdownShown: boolean,
+	isCurrentUserConsultant: boolean
+}>()
+
+interface CustomEvents {
+	(eventName: "toggleHeaderControlDropdownShown"): void
+	(eventName: "showActionTakenOverlay"): void
+}
+
+const emit = defineEmits<CustomEvents>()
+
+function showActionTakenOverlay() {
+	emit("showActionTakenOverlay")
+}
+
+function toggleHeaderControlDropdownShown() {
+	emit("toggleHeaderControlDropdownShown")
+}
+</script>
