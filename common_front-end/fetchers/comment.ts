89a337605fb9f8c98dde8281fec43da93a25ef27
Angelo Magtoto <19201.magtoto.angelo.l@gmail.com>
2022-10-28 16:57:20 +0800

    intrn(fetcher): add method to count the votes
    Co-authored-by: Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>

--- common_front-end/fetchers/comment.ts
+++ common_front-end/fetchers/comment.ts
@@ -1,3 +1,4 @@
+import type { Response } from "$@/types/independent"
 import type { CommentQueryParameters } from "$/types/query"
 import type {
 	CommentResourceIdentifier,
@@ -8,12 +9,14 @@ import type {
 	CommentListDocument,
 	DeserializedCommentDocument,
 	DeserializedCommentListDocument,
-	CommentRelationships
+	CommentRelationships,
+	CommentIdentifierListDocumentWithVotes
 } from "$/types/documents/comment"
 
-import { COMMENT_LINK } from "$/constants/template_links"
+import { COMMENT_LINK, COUNT_COMMENT_VOTES } from "$/constants/template_links"
 
 import BaseFetcher from "$@/fetchers/base"
+import stringifyQuery from "$@/fetchers/stringify_query"
 
 export default class CommentFetcher extends BaseFetcher<
 	CommentResourceIdentifier<"read">,
@@ -32,5 +35,32 @@ export default class CommentFetcher extends BaseFetcher<
 > {
 	constructor() {
 		super(COMMENT_LINK)
+	}
+
+	countVotes(commentIDs: string[]): Promise<Response<
+		CommentResourceIdentifier,
+		CommentAttributes<"serialized">,
+		CommentAttributes<"deserialized">,
+		CommentResource,
+		DeserializedCommentResource,
+		CommentIdentifierListDocumentWithVotes
+	>> {
+		return this.handleResponse(
+			this.getJSON(
+				`${COUNT_COMMENT_VOTES}?${stringifyQuery({
+					"filter": {
+						"IDs": commentIDs
+					}
+				})}`
+			),
+			false
+		) as Promise<Response<
+			CommentResourceIdentifier,
+			CommentAttributes<"serialized">,
+			CommentAttributes<"deserialized">,
+			CommentResource,
+			DeserializedCommentResource,
+			CommentIdentifierListDocumentWithVotes
+		>>
 	}
 }
