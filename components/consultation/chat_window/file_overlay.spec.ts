66d31c4447c9b52cf85436e9db1bc6b6bc8b2608
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-10-28 23:34:02 +0800

    unit(chat): ensure image previewing in file repo

--- components/consultation/chat_window/file_overlay.spec.ts
+++ components/consultation/chat_window/file_overlay.spec.ts
@@ -1,7 +1,8 @@
 /* eslint-disable max-lines */
-
+import { nextTick } from "vue"
 import { shallowMount } from "@vue/test-utils"
-import consolaGlobalInstance from "consola"
+
+import { DeserializedChatMessageListDocument } from "$/types/documents/chat_message"
 
 import Component from "./file_overlay.vue"
 
@@ -14,6 +15,8 @@ describe("Component: chat_window/file_overlay", () => {
 				}
 			},
 			"props": {
+				"generalFiles": { "data": [] },
+				"imageFiles": { "data": [] },
 				"isFileRepoOverlayShown": true,
 				"mustShowPreview": false
 			}
@@ -24,7 +27,7 @@ describe("Component: chat_window/file_overlay", () => {
 		expect(wrapper.emitted()).toHaveProperty("switchTab")
 	})
 
-	it("should show close itself", async() => {
+	it("should close itself", async() => {
 		const wrapper = shallowMount(Component, {
 			"global": {
 				"stubs": {
@@ -32,6 +35,8 @@ describe("Component: chat_window/file_overlay", () => {
 				}
 			},
 			"props": {
+				"generalFiles": { "data": [] },
+				"imageFiles": { "data": [] },
 				"isFileRepoOverlayShown": true,
 				"mustShowPreview": false
 			}
@@ -40,5 +45,63 @@ describe("Component: chat_window/file_overlay", () => {
 		await closeBtn.trigger("click")
 
 		expect(wrapper.emitted()).toHaveProperty("hideFileRepoOverlay")
+	})
+
+	describe("image previewing", () => {
+		it("can select file to preview", async() => {
+			const firstImageFileContents = "first/image/file/contents"
+			const secondImageFileContents = "second/image/file/contents"
+			const imageFiles = {
+				"data": [
+					{
+						"attachedChatFile": {
+							"data": {
+								"fileContents": firstImageFileContents
+							}
+						},
+						"data": {
+							"name": "Image 1.jpg",
+							"subkind": "image"
+						},
+						"id": "1"
+					},
+					{
+						"attachedChatFile": {
+							"data": {
+								"fileContents": secondImageFileContents
+							}
+						},
+						"data": {
+							"name": "Image 2.jpg",
+							"subkind": "image"
+						},
+						"id": "2"
+					}
+				]
+			} as unknown as DeserializedChatMessageListDocument
+			const wrapper = shallowMount(Component, {
+				"global": {
+					"stubs": {
+						"Overlay": false
+					}
+				},
+				"props": {
+					"generalFiles": { "data": [] },
+					imageFiles,
+					"isFileRepoOverlayShown": true,
+					"mustShowPreview": true
+				}
+			})
+			const imageFileItems = wrapper.findAll(".file-item")
+			const [ firstImageFile, secondImageFile ] = imageFileItems
+			const imageToPreview = wrapper.find(".image-to-preview")
+
+			await firstImageFile.trigger("click")
+			await nextTick()
+			expect(imageToPreview.attributes("src")).toEqual(firstImageFileContents)
+			await secondImageFile.trigger("click")
+			await nextTick()
+			expect(imageToPreview.attributes("src")).toEqual(secondImageFileContents)
+		})
 	})
 })
