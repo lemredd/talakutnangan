eda9ee303f543fa6910f35473eb41605db2c4be2
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-28 15:49:42 +0800

    intrn(user)!: use a permission-based policy to manage access to list

--- routes/enhancer/user/index.get.ts
+++ routes/enhancer/user/index.get.ts
@@ -8,17 +8,29 @@ import deserialize from "$/object/deserialize"
 import PageMiddleware from "!/bases/controller-likes/page_middleware"
 import DynamicGatedRedirector from "!/middlewares/miscellaneous/dynamic_gated_redirector"
 
+import PermissionBasedPolicy from "!/policies/permission-based"
+import { user as permissionGroup } from "$/permissions/permission_list"
+import {
+	UPDATE_ANYONE_ON_OWN_DEPARTMENT,
+	UPDATE_ANYONE_ON_ALL_DEPARTMENTS,
+	ARCHIVE_AND_RESTORE_ANYONE_ON_OWN_DEPARTMENT,
+	ARCHIVE_AND_RESTORE_ANYONE_ON_ALL_DEPARTMENT,
+	RESET_PASSWORD,
+	IMPORT_USERS
+} from "$/permissions/user_combinations"
+
 export default class extends PageMiddleware {
 	get filePath(): string { return __filename }
 
 	get policy(): Policy {
-		return new DynamicGatedRedirector((request: AuthenticatedRequest) => {
-			const user = deserialize(request.user) as DeserializedUserProfile
-			const { kind } = user.data
-
-			if (kind === "student") return Promise.resolve({ "location": "/" })
-			return Promise.resolve({ "location": "/user/list" })
-		}) as unknown as Policy
+		return new PermissionBasedPolicy(permissionGroup, [
+			UPDATE_ANYONE_ON_OWN_DEPARTMENT,
+			UPDATE_ANYONE_ON_ALL_DEPARTMENTS,
+			ARCHIVE_AND_RESTORE_ANYONE_ON_OWN_DEPARTMENT,
+			ARCHIVE_AND_RESTORE_ANYONE_ON_ALL_DEPARTMENT,
+			RESET_PASSWORD,
+			IMPORT_USERS
+		])
 	}
 
 
