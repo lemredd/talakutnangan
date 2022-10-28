d2817d488a50625643e871658d10ac6bed19b6ed
Ardrin Gregorio <kiritogregorio@gmail.com>
2022-10-28 14:17:17 +0800

    unit(chat): ensure event emissions
    Co-authored-by: Jarlem Red de Peralta <lmoa.jhdp@gmail.com>

--- components/consultation/chat_window/extra_controls.spec.ts
+++ components/consultation/chat_window/extra_controls.spec.ts
@@ -3,14 +3,23 @@ import { shallowMount } from "@vue/test-utils"
 import Component from "./extra_controls.vue"
 
 describe("Component: chat_window/extra_controls", () => {
-	it("should emit custom events", async() => {
+	it("should emit toggling of action taken", async() => {
 		const wrapper = shallowMount(Component, {
+			"global": {
+				"stubs": {
+					"Dropdown": false
+				}
+			},
 			"props": {
 				"isCurrentUserConsultant": true,
 				"isHeaderControlDropdownShown": true
 			}
 		})
+		const showActionTakenOverlayBtn = wrapper.find(".show-action-taken-overlay-btn")
 
+		await showActionTakenOverlayBtn.trigger("click")
 
+		const emissions = wrapper.emitted()
+		expect(emissions).toHaveProperty("showActionTakenOverlay")
 	})
 })
