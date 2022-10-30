fe9f9e8119583b510e4c0f6837ce338f0c81e3ee
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-30 22:53:18 +0800

    intrn(fetcher): add methods to request other summaries

--- common_front-end/fetchers/consultation.ts
+++ common_front-end/fetchers/consultation.ts
@@ -2,6 +2,10 @@ import type { Response } from "$@/types/independent"
 import type { ConsultationQueryParameters, TimeSumQueryParameters } from "$/types/query"
 import type { DeserializedUserListWithTimeConsumedDocument } from "$/types/documents/user"
 import type {
+	WeeklySummedTimeDocument,
+	ConsolidatedSummedTimeDocument
+} from "$/types/documents/consolidated_time"
+import type {
 	ConsultationResourceIdentifier,
 	ConsultationAttributes,
 	ConsultationResource,
@@ -66,6 +70,62 @@ export default class ConsultationFetcher extends BaseFetcher<
 			ConsultationResource,
 			DeserializedConsultationResource,
 			DeserializedUserListWithTimeConsumedDocument
+		>>
+	}
+
+	readTimeSumPerWeek(parameters: TimeSumQueryParameters): Promise<Response<
+		ConsultationResourceIdentifier,
+		ConsultationAttributes<"serialized">,
+		ConsultationAttributes<"deserialized">,
+		ConsultationResource,
+		DeserializedConsultationResource,
+		WeeklySummedTimeDocument
+	>> {
+		const commaDelimitedSort = {
+			...parameters,
+			"sort": parameters.sort.join(",")
+		}
+
+		return this.handleResponse(
+			this.getJSON(specializePath(READ_TIME_SUM_PER_WEEK, {
+				"query": stringifyQuery(commaDelimitedSort)
+			})),
+			true
+		) as Promise<Response<
+			ConsultationResourceIdentifier,
+			ConsultationAttributes<"serialized">,
+			ConsultationAttributes<"deserialized">,
+			ConsultationResource,
+			DeserializedConsultationResource,
+			WeeklySummedTimeDocument
+		>>
+	}
+
+	readTimeSumForConsolidation(parameters: TimeSumQueryParameters): Promise<Response<
+		ConsultationResourceIdentifier,
+		ConsultationAttributes<"serialized">,
+		ConsultationAttributes<"deserialized">,
+		ConsultationResource,
+		DeserializedConsultationResource,
+		ConsolidatedSummedTimeDocument
+	>> {
+		const commaDelimitedSort = {
+			...parameters,
+			"sort": parameters.sort.join(",")
+		}
+
+		return this.handleResponse(
+			this.getJSON(specializePath(READ_TIME_SUM_FOR_CONSOLIDATION, {
+				"query": stringifyQuery(commaDelimitedSort)
+			})),
+			true
+		) as Promise<Response<
+			ConsultationResourceIdentifier,
+			ConsultationAttributes<"serialized">,
+			ConsultationAttributes<"deserialized">,
+			ConsultationResource,
+			DeserializedConsultationResource,
+			ConsolidatedSummedTimeDocument
 		>>
 	}
 }
