ffb80c91d1e4a3e3be19b601b891cd3d9d577efc
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-10-31 19:41:53 +0800

    feat(call): allow toggling of video track

--- components/consultation/call/self_participant.vue
+++ components/consultation/call/self_participant.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="self-participant" :id="selfParticipantId">
+	<div :id="selfParticipantId" class="self-participant">
 		<video ref="videoElement"></video>
 	</div>
 </template>
@@ -12,7 +12,7 @@
 </style>
 
 <script setup lang="ts">
-import { inject, onMounted, ref } from "vue"
+import { inject, ref, watch } from "vue"
 
 import type { PageContext } from "$/types/renderer"
 
@@ -21,20 +21,30 @@ const pageContext = inject("pageContext") as PageContext<"deserialized", Additio
 const { pageProps } = pageContext
 const { userProfile } = pageProps
 
+type DefinedProps = {
+	mustShowVideo: boolean
+	mustTransmitAudio: boolean
+}
+const props = defineProps<DefinedProps>()
+
 const selfParticipantId = `${userProfile.data.id}_${userProfile.data.name}`
 const videoElement = ref<HTMLVideoElement|null>(null)
 
-function addVideoStream(video: HTMLVideoElement, stream: MediaStream) {
+function addVideoStream(stream: MediaStream) {
+	const video = videoElement.value as HTMLVideoElement
 	video.srcObject = stream
 	video.autoplay = true
 	video.addEventListener("loadedmetadata", () => video.play())
 }
 
-onMounted(() => {
-	navigator.mediaDevices.getUserMedia({ "video": true })
-	.then(stream => addVideoStream(
-		videoElement.value as HTMLVideoElement,
-		stream
-	))
+function removeVideoStream(video: HTMLVideoElement) {
+	video.srcObject = null
+}
+
+watch(props, () => {
+	if (props.mustShowVideo) {
+		navigator.mediaDevices.getUserMedia({ "video": true })
+		.then(addVideoStream)
+	}
 })
 </script>
