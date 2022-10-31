1147c4984178b1bcb835f4c4059d8123bb8f5672
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-11-01 02:10:40 +0800

    unit(consultation): ensure normal operation works properly

---
+++ components/consultation/listeners/watch_consultation.spec.ts
@@ -0,0 +1,65 @@
+import { ref, nextTick, Ref } from "vue"
+
+import type { DeserializedConsultationResource } from "$/types/documents/consultation"
+
+import ConsultationTimerManager from "$@/helpers/consultation_timer_manager"
+
+import listener from "./watch_consultation"
+
+describe("Listener: Consultation watcher", () => {
+	afterEach(() => {
+		ConsultationTimerManager.clearAllListeners()
+	})
+
+	it("can register listeners", async() => {
+		const consultation = ref({
+			"finishedAt": null,
+			"id": 1,
+			"startedAt": null,
+			"type": "consultation"
+		}) as unknown as Ref<DeserializedConsultationResource<"consultant"|"consultantRole">>
+		const finishConsultation = jest.fn()
+		const registerListeners = jest.fn()
+		listener(consultation, registerListeners, finishConsultation)
+
+		consultation.value = {
+			...consultation.value,
+			"startedAt": new Date()
+		}
+		await nextTick()
+
+		expect(registerListeners).toHaveBeenCalled()
+	})
+
+	it("can force finish", async() => {
+		const consultation = ref({
+			"finishedAt": null,
+			"id": 1,
+			"startedAt": null,
+			"type": "consultation"
+		}) as unknown as Ref<DeserializedConsultationResource<"consultant"|"consultantRole">>
+		const finishConsultation = jest.fn()
+		const registerListeners = jest.fn(newConsultation => {
+			ConsultationTimerManager.listenConsultationTimeEvent(
+				newConsultation,
+				"finish",
+				finishConsultation
+			)
+		})
+		listener(consultation, registerListeners, finishConsultation)
+
+		consultation.value = {
+			...consultation.value,
+			"startedAt": new Date()
+		}
+		await nextTick()
+		consultation.value = {
+			...consultation.value,
+			"finishedAt": new Date()
+		}
+		await nextTick()
+
+		expect(registerListeners).toHaveBeenCalled()
+		expect(finishConsultation).toHaveBeenCalled()
+	})
+})
