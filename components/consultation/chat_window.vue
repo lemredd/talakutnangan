ca1b0f1e5ab570ae6cdd9bc6858594d9ab16c8b2
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-10-31 19:55:28 +0800

    refactor(consultation): separate the header

--- components/consultation/chat_window.vue
+++ components/consultation/chat_window.vue
@@ -7,73 +7,11 @@
 			{{ `chevron_${isConsultationListShown ? "left" : "right"}` }}
 		</button>
 
-		<div class="selected-consultation-header">
-			<div class="text">
-				<div class="selected-consultation-title">
-					{{ consultation.reason }}
-				</div>
-				<div class="selected-consultation-remaining-time">
-					Time remaining:
-					<span>{{ remainingTime.minutes }}m</span>
-					<span v-if="remainingTime.seconds > 0">{{ remainingTime.seconds }}s</span>
-				</div>
-				<div class="selected-consultation-user-status">
-					<!-- TODO(lead): must base on user active status -->
-					User is online
-				</div>
-			</div>
-			<div class="controls">
-				<!-- TODO(lead/button): Apply functionality -->
-				<button class="material-icons">
-					video_camera_back
-				</button>
-				<button class="material-icons toggle-controls-btn" @click="showFileRepoOverlay">
-					storage
-				</button>
-
-				<FileOverlay
-					:general-files="generalFileChatMessages"
-					:image-files="imageFileChatMessages"
-					:is-file-repo-overlay-shown="isFileRepoOverlayShown"
-					:must-show-preview="mustShowPreview"
-					@hide-file-repo-overlay="hideFileRepoOverlay"
-					@switch-tab="switchTab"/>
-
-				<ExtraControls
-					:is-header-control-dropdown-shown="isHeaderControlDropdownShown"
-					:is-current-user-consultant="isCurrentUserConsultant"
-					@show-action-taken-overlay="showActionTakenOverlay"
-					@toggle-header-control-dropdown-shown="toggleHeaderControlDropdownShown"/>
-
-				<Overlay
-					:is-shown="isActionTakenOverlayShown && isCurrentUserConsultant"
-					class="action-taken"
-					@close="hideActionTakenOverlay">
-					<template #header>
-						Mark this consultation as finished?
-					</template>
-
-					<template #default>
-						<p>If so, please provide the action taken to solve the consulter/s concern.</p>
-						<NonSensitiveTextField
-							v-model="actionTaken"
-							class="action-taken-field"
-							type="text"/>
-						<ReceivedErrors
-							v-if="receivedErrors.length"
-							:received-errors="receivedErrors"/>
-					</template>
-
-					<template #footer>
-						<button
-							class="finish-btn btn btn-primary"
-							@click="finishConsultation">
-							submit
-						</button>
-					</template>
-				</Overlay>
-			</div>
-		</div>
+		<ConsultationHeader
+			:consultation="consultation"
+			:chat-messages="chatMessages"
+			:remaining-time="remainingTime"
+			@finish-consultation="finishConsultation"/>
 		<div class="selected-consultation-chats">
 			<div class="selected-consultation-new">
 				<p class="consultation-details">
@@ -149,15 +87,6 @@
 			@apply flex flex-col;
 		}
 
-		.selected-consultation-header {
-			@apply flex py-4 px-2 dark:bg-true-gray-800;
-			.text {
-				@apply flex-1;
-				.selected-consultation-user-status { @apply row-start-2; }
-			}
-			.controls { @apply flex items-center; }
-		}
-
 		.selected-consultation-chats {
 			@apply px-3 py-5 flex-1 overflow-y-scroll;
 
@@ -179,10 +108,9 @@
 </style>
 
 <script setup lang="ts">
-import { ref, computed, watch, onMounted, inject } from "vue"
+import { ref, computed, watch, onMounted } from "vue"
 
 import type { UnitError } from "$/types/server"
-import type { PageContext } from "$/types/renderer"
 import type { FullTime } from "$@/types/independent"
 import type { DeserializedChatMessageListDocument } from "$/types/documents/chat_message"
 import type {
@@ -190,20 +118,15 @@ import type {
 	DeserializedConsultationResource
 } from "$/types/documents/consultation"
 
-import makeSwitch from "$@/helpers/make_switch"
 import assignPath from "$@/external/assign_path"
 import specializePath from "$/helpers/specialize_path"
 import ConsultationFetcher from "$@/fetchers/consultation"
 import ConsultationTimerManager from "$@/helpers/consultation_timer_manager"
 import convertMStoTimeObject from "$@/helpers/convert_milliseconds_to_full_time_object"
 
-import Overlay from "@/helpers/overlay.vue"
-import NonSensitiveTextField from "@/fields/non-sensitive_text.vue"
-import FileOverlay from "@/consultation/chat_window/file_overlay.vue"
-import ExtraControls from "@/consultation/chat_window/extra_controls.vue"
 import UserController from "@/consultation/chat_window/user_controller.vue"
-import ReceivedErrors from "@/helpers/message_handlers/received_errors.vue"
 import ChatMessageItem from "@/consultation/chat_window/chat_message_item.vue"
+import ConsultationHeader from "@/consultation/chat_window/consultation_header.vue"
 
 const fetcher = new ConsultationFetcher()
 
@@ -220,17 +143,6 @@ const props = defineProps<{
 	isConsultationListShown: boolean
 }>()
 
-const {
-	"pageProps": {
-		"userProfile": {
-			"data": {
-				kind
-			}
-		}
-	}
-} = inject("pageContext") as PageContext<"deserialized">
-const isCurrentUserConsultant = computed(() => kind === "reachable_employee")
-
 const sortedMessagesByTime = computed(() => {
 	const { "chatMessages": { "data": rawData } } = props
 	return [ ...rawData ].sort((left, right) => {
@@ -243,34 +155,20 @@ const sortedMessagesByTime = computed(() => {
 function loadPreviousMessages() {
 	emit("loadPreviousMessages")
 }
-const generalFileChatMessages = {
-	"data": props.chatMessages.data.filter(
-		chatMessage => chatMessage.data.subkind === "file"
-	)
-}
-const imageFileChatMessages = {
-	"data": props.chatMessages.data.filter(
-		chatMessage => chatMessage.data.subkind === "image"
-	)
-}
 
 const chatWindow = ref<HTMLElement|null>(null)
 function toggleConsultationList() {
 	emit("toggleConsultationList")
 }
 
-const {
-	"toggle": toggleHeaderControlDropdownShown,
-	"state": isHeaderControlDropdownShown
-} = makeSwitch(false)
-
-const remainingMilliseconds = ref<number>(0)
-const remainingTime = computed<FullTime>(() => convertMStoTimeObject(remainingMilliseconds.value))
 const consultation = computed<DeserializedConsultationResource<"consultant"|"consultantRole">>(
 	() => props.consultation
 )
 const consultationID = computed<string>(() => consultation.value.id)
 const consultationStatus = computed<string>(() => consultation.value.status)
+
+const remainingMilliseconds = ref<number>(0)
+const remainingTime = computed<FullTime>(() => convertMStoTimeObject(remainingMilliseconds.value))
 
 function restartRemainingTime(): void {
 	remainingMilliseconds.value = ConsultationTimerManager.MAX_EXPIRATION_TIME
@@ -283,29 +181,9 @@ function changeTime(
 	remainingMilliseconds.value = remainingMillisecondduration
 }
 
-const {
-	"on": showFileRepoOverlay,
-	"off": hideFileRepoOverlay,
-	"state": isFileRepoOverlayShown
-} = makeSwitch(false)
-
-const {
-	"on": showActionTakenOverlay,
-	"off": hideActionTakenOverlay,
-	"state": isActionTakenOverlayShown
-} = makeSwitch(false)
-const actionTaken = ref("")
-
-const fileRepoTab = ref("files")
 const receivedErrors = ref<string[]>([])
 
-const mustShowPreview = computed(() => fileRepoTab.value === "pictures")
-function switchTab(event: Event) {
-	const button = event.target as HTMLButtonElement
-	const { innerText } = button
-
-	fileRepoTab.value = innerText.toLocaleLowerCase()
-}
+const actionTaken = ref("")
 
 function finishConsultation(): void {
 	const { startedAt } = consultation.value
@@ -382,7 +260,6 @@ function finishConsultation(): void {
 	}
 }
 
-
 function registerListeners(resource: DeserializedConsultationResource): void {
 	ConsultationTimerManager.listenConsultationTimeEvent(resource, "finish", finishConsultation)
 	ConsultationTimerManager.listenConsultationTimeEvent(
@@ -446,7 +323,6 @@ function startConsultation() {
 		emit("updatedConsultationAttributes", deserializedConsultationData)
 	})
 }
-
 
 const startWatcher = watch(consultation, (newConsultation, oldConsultation) => {
 	if (oldConsultation.startedAt === null && newConsultation.startedAt instanceof Date) {
