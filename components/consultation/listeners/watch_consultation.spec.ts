37fb96342949ad31649ef6dd695611a27f04f8d3
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-11-01 02:12:58 +0800

    unit(consultation): ensure already started consultation may finish

--- components/consultation/listeners/watch_consultation.spec.ts
+++ components/consultation/listeners/watch_consultation.spec.ts
@@ -31,7 +31,7 @@ describe("Listener: Consultation watcher", () => {
 		expect(registerListeners).toHaveBeenCalled()
 	})
 
-	it("can force finish", async() => {
+	it("can force finish normally", async() => {
 		const consultation = ref({
 			"finishedAt": null,
 			"id": 1,
@@ -60,6 +60,32 @@ describe("Listener: Consultation watcher", () => {
 		await nextTick()
 
 		expect(registerListeners).toHaveBeenCalled()
+		expect(finishConsultation).toHaveBeenCalled()
+	})
+
+	it("can force finish an already started consultation", async() => {
+		const consultation = ref({
+			"finishedAt": null,
+			"id": 1,
+			"startedAt": new Date(),
+			"type": "consultation"
+		}) as unknown as Ref<DeserializedConsultationResource<"consultant"|"consultantRole">>
+		const finishConsultation = jest.fn()
+		const registerListeners = jest.fn()
+		ConsultationTimerManager.listenConsultationTimeEvent(
+			consultation.value,
+			"finish",
+			finishConsultation
+		)
+		listener(consultation, registerListeners, finishConsultation)
+
+		consultation.value = {
+			...consultation.value,
+			"finishedAt": new Date()
+		}
+		await nextTick()
+
+		expect(registerListeners).not.toHaveBeenCalled()
 		expect(finishConsultation).toHaveBeenCalled()
 	})
 })
