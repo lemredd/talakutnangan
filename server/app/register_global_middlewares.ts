2b463aebd28b638a592d0faa1f5f6415e47156eb
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-11-01 13:28:47 +0800

    chore(revert): unregister CORS middleware
    This reverts commit 54455793d116efeebaec3424a8f88713f9e749a4.

--- server/app/register_global_middlewares.ts
+++ server/app/register_global_middlewares.ts
@@ -1,13 +1,11 @@
 import type { Express as ExpressApp } from "express"
 import type { RequestHandler } from "!/types/dependent"
 
-import CORS from "!/middlewares/miscellaneous/cors"
 import makeGlobalMiddlewares from "!/helpers/make_global_middlewares"
 
 // eslint-disable-next-line require-await
 export default async function(app: ExpressApp): Promise<void> {
 	const middlewares = makeGlobalMiddlewares()
-	middlewares.push(new CORS())
 
 	middlewares.forEach(middleware => {
 		app.use(middleware.intermediate.bind(middleware) as unknown as RequestHandler)
