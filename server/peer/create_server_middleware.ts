6046ff51d573a9f9a33e4c0ac403fa9a7b93a46d
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-11-01 15:20:56 +0800

    intrn(peer): use constant links
    Co-authored-by: Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>

--- server/peer/create_server_middleware.ts
+++ server/peer/create_server_middleware.ts
@@ -2,15 +2,17 @@ import type { Server } from "http"
 import type { Express } from "express"
 import type { PeerServer } from "!/types/dependent"
 
+import { PEER_SERVER_PATH_PREFIX, PEER_SERVER_SUBPATH } from "$/constants/template_links"
+
 import { ExpressPeerServer } from "peer"
 
 export default function(app: Express, httpServer: Server): PeerServer {
 	const peerServer = ExpressPeerServer(httpServer, {
-		"path": "/server",
+		"path": PEER_SERVER_SUBPATH,
 		"proxied": process.env.WEB_PROXY === "true"
 	})
 
-	app.use("/p2p", peerServer)
+	app.use(PEER_SERVER_PATH_PREFIX, peerServer)
 
 	return peerServer
 }
