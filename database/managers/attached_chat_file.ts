d3ebc79faf17098dc1c6ccc08806a65e8fdca8d8
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-11-02 10:49:44 +0800

    intrn(manager): add query to include chat message

--- database/managers/attached_chat_file.ts
+++ database/managers/attached_chat_file.ts
@@ -1,24 +1,27 @@
+import type { Pipe } from "$/types/database"
 import type { GeneralObject } from "$/types/general"
 import type { RawAttachedChatFile } from "$!/types/independent"
 import type { FileLikeTransformerOptions } from "%/types/independent"
-import type { ModelCtor, Model as BaseModel } from "%/types/dependent"
+import type { Model as BaseModel, ModelCtor, FindAndCountOptions } from "%/types/dependent"
 
 import User from "%/models/user"
 import BaseManager from "%/managers/base"
 import ChatMessage from "%/models/chat_message"
-import AttachedChatFile from "%/models/attached_chat_file"
+import Model from "%/models/attached_chat_file"
+import Transformer from "%/transformers/attached_chat_file"
 import ChatMessageActivity from "%/models/chat_message_activity"
-import AttachedChatFileTransformer from "%/transformers/attached_chat_file"
+
+import includeDefaults from "%/queries/attached_chat_file/include_defaults"
 
 export default class extends BaseManager<
-	AttachedChatFile,
+	Model,
 	RawAttachedChatFile,
 	GeneralObject,
 	FileLikeTransformerOptions
 > {
-	get model(): ModelCtor<AttachedChatFile> { return AttachedChatFile }
+	get model(): ModelCtor<Model> { return Model }
 
-	get transformer(): AttachedChatFileTransformer { return new AttachedChatFileTransformer() }
+	get transformer(): Transformer { return new Transformer() }
 
 	get modelChainToUser(): readonly ModelCtor<BaseModel>[] {
 		return [
@@ -31,5 +34,15 @@ export default class extends BaseManager<
 	protected get exposableColumns(): string[] {
 		const excludedColumns = new Set([ "id", "chatMessageID" ])
 		return super.exposableColumns.filter(columnName => !excludedColumns.has(columnName))
+	}
+
+	get singleReadPipeline(): Pipe<
+		FindAndCountOptions<Model>,
+		GeneralObject
+	>[] {
+		return [
+			includeDefaults,
+			...super.singleReadPipeline
+		]
 	}
 }
