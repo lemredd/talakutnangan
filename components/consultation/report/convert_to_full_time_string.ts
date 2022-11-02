530dd95a279eeb12b8529f7f39ff852b779066fe
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-11-02 23:49:34 +0800

    fix(consultation): correct the path alias

--- components/consultation/report/convert_to_full_time_string.ts
+++ components/consultation/report/convert_to_full_time_string.ts
@@ -1,4 +1,4 @@
-import convertToRawFullTime from "$/consultation/convert_to_raw_full_time"
+import convertToRawFullTime from "@/consultation/report/convert_to_raw_full_time"
 
 export default function(timeInMilliseconds: number): string {
 	const {
