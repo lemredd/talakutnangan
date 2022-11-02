a3b3a40d71c5140c0b2dfdb6aeb50d072df61777
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-11-02 21:14:26 +0800

    intrn(call): get consultation-related data

--- pages/consultation/call.page.vue
+++ pages/consultation/call.page.vue
@@ -54,9 +54,15 @@
 </style>
 
 <script setup lang="ts">
-import { inject } from "vue"
+import { computed, inject, onMounted, ref } from "vue"
 
 import { PageContext } from "$/types/renderer"
+import type {
+	DeserializedChatMessageActivityResource,
+	DeserializedChatMessageActivityListDocument
+} from "$/types/documents/chat_message_activity"
+
+import isUndefined from "$/type_guards/is_undefined"
 
 import makeSwitch from "$@/helpers/make_switch"
 
@@ -65,7 +71,42 @@ import SelfParticipant from "@/consultation/call/self_participant.vue"
 
 type AdditionalPageProps = "mustUsePeerServer"|"chatMessageActivities"|"consultation"
 const pageContext = inject("pageContext") as PageContext<"deserialized", AdditionalPageProps>
+const { pageProps } = pageContext
+const { consultation, userProfile } = pageProps
 
+const chatMessageActivities = ref<
+	DeserializedChatMessageActivityListDocument<"user"|"consultation">
+>(
+	pageProps.chatMessageActivities as DeserializedChatMessageActivityListDocument<
+		"user"|"consultation"
+	>
+)
+
+const currentConsultationActivity = computed<
+	DeserializedChatMessageActivityResource<"user"|"consultation">[]
+>(() => {
+	const foundChatActivity = chatMessageActivities.value.data.filter(activity => {
+		const consultationID = activity.consultation.data.id
+		return String(consultationID) === String(consultation.data.id)
+	}) as DeserializedChatMessageActivityResource<"user"|"consultation">[]
+
+	return foundChatActivity
+})
+
+const ownCurrentConsultationActivityResource = computed<
+	DeserializedChatMessageActivityResource<"user"|"consultation">
+>(() => {
+	const foundChatActivity = currentConsultationActivity.value.find(activity => {
+		const ownerID = activity.user.data.id
+		return String(ownerID) === String(userProfile.data.id)
+	})
+
+	if (isUndefined(foundChatActivity)) {
+		throw new Error("Chat message activity of the current user was not found.")
+	}
+
+	return foundChatActivity
+})
 
 const {
 	"toggle": toggleVideo,
