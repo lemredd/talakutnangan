3e4088ef0b92f91fcafe7b3e3b47dfc98ec0f8cf
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-11-03 17:19:55 +0800

    intrn(front-end): make current time optional

--- common_front-end/helpers/format_to_friendly_past_time.ts
+++ common_front-end/helpers/format_to_friendly_past_time.ts
@@ -1,6 +1,6 @@
 import { DateTime } from "luxon"
 
-export default function(rawPreviousTime: Date, rawCurrentTime: Date): string {
+export default function(rawPreviousTime: Date, rawCurrentTime: Date = new Date()): string {
 	const previousTime = DateTime.fromJSDate(rawPreviousTime)
 	const currentTime = DateTime.fromJSDate(rawCurrentTime)
 	return previousTime.toRelative({
