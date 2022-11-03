77a68209e2631cdc69dd9bd75f2e36f2a2bee23c
Ardrin Gregorio <kiritogregorio@gmail.com>
2022-11-03 15:24:26 +0800

    style(footer): design and add more contents

--- components/page_shell/footer.vue
+++ components/page_shell/footer.vue
@@ -1,17 +1,54 @@
 <template>
 	<footer>
 		<div class="links">
-			<div
-				v-for="i in 4"
-				:key="i"
-				class="footer-list">
-				<h1>Footer</h1>
-				<ul>
-					<li v-for="j in 3" :key="j">
-						Sub Footer
-					</li>
-				</ul>
-			</div>
+			<ul>
+				<h1>GET IN TOUCH</h1>
+				<li>
+					<small>info@mcc.edu.ph</small>
+				</li>
+				<li>
+					<small>+63-4-5049-8720</small>
+				</li>
+				<li>
+					<small>+63-928-503-9622</small>
+				</li>
+			</ul>
+			<ul>
+				<h1>WEBSITE & SOCIAL MEDIA</h1>
+				<li>
+					<small>
+						<a href="https://mcc.edu.ph/">Mabalacat City College</a>
+					</small>
+				</li>
+				<li>
+					<small>
+						<a href="https://www.facebook.com/TheOfficial.MCC">Facebook</a>
+					</small>
+				</li>
+				<li>
+					<small>
+						<a
+							href="https://www.youtube.com
+							/channel/UCH5oJ-rBoD3LTGHYmYsfX7w?view_as=subscriber">YouTube
+						</a>
+					</small>
+				</li>
+			</ul>
+			<ul>
+				<h1>LOCATE US:</h1>
+				<li>
+					<span class="material-icons">
+						location_pin
+					</span>
+					Rizal St., Barangay Doroles, Mabalacat City, Pampanga
+				</li>
+				<li>
+					<span class="material-icons">
+						location_pin
+					</span>
+					Mawaque Rd, Barangay Dapdap, Mabalacat City, Pampanga
+				</li>
+			</ul>
 		</div>
 		<div class="copyright">
 			<span class="material-icons footer1">copyright</span>
@@ -27,9 +64,19 @@ footer {
 	color: white;
 	padding: 1em 3em;
 
+	@screen sm{
+		@apply flex flex-col items-center;
+		max-width:100%;
 	.links {
-		@apply flex justify-around;
+		@apply flex flex-row justify-around;
+		}
 	}
+		h1, small, li{
+			@apply text-sm text-center;
+		}
+		ul{
+			@apply py-8 px-2;
+		}
 }
 
 .copyright{
