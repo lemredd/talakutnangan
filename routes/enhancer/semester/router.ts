947404f1b246c66701495af1a10cd7667464b5be
Angelo Magtoto <19201.magtoto.angelo.l@gmail.com>
2022-11-04 00:01:38 +0800

    intrn(route): import enhancer route for semesters

---
+++ routes/enhancer/semester/router.ts
@@ -0,0 +1,11 @@
+import GetList from "!%/enhancer/semester/list.get"
+import GetIndex from "!%/enhancer/semester/index.get"
+// import GetRead from "!%/enhancer/semester/read(id).get"
+import GetCreate from "!%/enhancer/semester/create.get"
+
+export const controllers = [
+	GetIndex,
+	GetList,
+	GetCreate,
+	// GetRead
+]
