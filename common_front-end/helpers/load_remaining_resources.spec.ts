95cabbe0ccf63dfead0765f41e4568885be01bb7
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-11-03 20:15:17 +0800

    unit(front-end): ensure delays can be done

--- common_front-end/helpers/load_remaining_resources.spec.ts
+++ common_front-end/helpers/load_remaining_resources.spec.ts
@@ -79,4 +79,48 @@ describe("Helper: Load remaining resource", () => {
 
 		expect(fetch).toHaveBeenCalledTimes(1)
 	})
+
+	it("should delay", async() => {
+		jest.useFakeTimers()
+		fetchMock.mockResponseOnce(JSON.stringify({
+			"data": [],
+			"meta": {
+				"count": 0
+			}
+		}), { "status": RequestEnvironment.status.OK })
+
+		const listDocument = ref({
+			"data": [],
+			"meta": {
+				"count": 0
+			}
+		})
+		const fetcher = new DepartmentFetcher()
+		const queryMaker = () => ({
+			"filter": {
+				"existence": "exists",
+				"slug": ""
+			},
+			"page": {
+				"limit": 0,
+				"offset": 0
+			},
+			"sort": []
+		} as DepartmentQueryParameters)
+		const delayDuration = 200
+		const mockOperation = jest.fn()
+		const delayer = new Promise<void>(resolve => {
+			setTimeout(() => {
+				mockOperation()
+				resolve()
+			}, delayDuration)
+		})
+
+		const loadingProcess = loadRemainingResource(listDocument, fetcher, queryMaker, { delayer })
+		jest.advanceTimersByTime(delayDuration)
+		await loadingProcess
+
+		expect(fetch).toHaveBeenCalledTimes(1)
+		expect(mockOperation).toHaveBeenCalledTimes(1)
+	})
 })
