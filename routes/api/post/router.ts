d1a5882cb95fdaa5784101735171a49a52c72c8a
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-11-04 18:09:55 +0800

    fix(post): correct import location
    Co-authored-by: Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>

--- routes/api/post/router.ts
+++ routes/api/post/router.ts
@@ -1,8 +1,8 @@
-import GetList from "!%/api/department/list.get"
+import GetList from "!%/api/post/list.get"
 import PostCreate from "!%/api/post/create.post"
+import PatchRestore from "!%/api/post/restore.patch"
 import DeleteArchive from "!%/api/post/archive.delete"
 import PatchUpdate from "!%/api/post/update(id).patch"
-import PatchRestore from "!%/api/post/restore.patch"
 
 export const controllers = [
 	GetList,
