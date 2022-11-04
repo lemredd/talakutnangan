11ec7fda36fe02ecfc1691b4cc71ea46094952ec
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-11-04 23:50:54 +0800

    types(call): expose local and remote tracks

--- components/consultation/call/helpers/types/video_conference_manager.ts
+++ components/consultation/call/helpers/types/video_conference_manager.ts
@@ -17,3 +17,14 @@ export type LocalVideoTrack = ILocalVideoTrack
 
 export type RemoteAudioTrack = IRemoteAudioTrack
 export type RemoteVideoTrack = IRemoteVideoTrack
+
+export type LocalTracks = {
+	"localAudioTrack": LocalAudioTrack|null
+	"localVideoTrack": LocalVideoTrack|null
+}
+
+export type RemoteTracks = {
+	remoteID: string,
+	remoteAudioTrack: RemoteAudioTrack|null,
+	remoteVideoTrack: RemoteVideoTrack|null
+}
