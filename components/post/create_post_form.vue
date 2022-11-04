1e2832f5d80091d7bb197104a1a74939009afe57
Ardrin Gregorio <kiritogregorio@gmail.com>
2022-11-04 13:54:41 +0800

    intrn(forum): function for file upload

--- components/post/create_post_form.vue
+++ components/post/create_post_form.vue
@@ -1,5 +1,5 @@
 <template>
-	<Overlay :is-shown="isShown" @close="close">
+	<Overlay :is-shown="isShown" @close="emitClose">
 		<template #header>
 			<h1>Enter the post details</h1>
 		</template>
@@ -15,7 +15,9 @@
 						placeholder="Choose the role"
 						:options="roleNames"/>
 				</div>
-				<div v-if="maySelectOtherDepartments" class="row department-selector flex flex-row">
+				<div
+					v-if="maySelectOtherDepartments"
+					class="row department-selector flex flex-row">
 					<SelectableOptionsField
 						v-model="departmentID"
 						label="Department to post: "
@@ -23,24 +25,67 @@
 						:options="departmentNames"/>
 				</div>
 			</DraftForm>
-			<input type="file" @change="uploadPostAttachment"/>
+			<ReceivedErrors
+				v-if="receivedErrors.length"
+				:received-errors="receivedErrors"/>
+			<form ref="fileUploadForm" @submit.prevent>
+				<input
+					type="hidden"
+					name="data[type]"
+					value="post_attachment"/>
+				<label class="btn" for="choose-file-btn">
+					<input
+						id="choose-file-btn"
+						type="file"
+						name="meta[fileContents]"
+						:accept="accept"
+						@change="uploadPostAttachment"/>
+					CHOOSE FILE
+				</label>
+			</form>
+			<div v-if="hasExtracted" class="preview-file">
+				<div v-if="isAcceptingImage" class="preview-img-container">
+					<img class="preview-img" :src="previewFile"/>
+					<small class="preview-title">
+						{{ filename }}
+					</small>
+				</div>
+				<div
+					v-if="isAcceptingFile"
+					class="preview-file-container">
+					<span class="material-icons mr-2">
+						attachment
+					</span>
+					<small class="preview-file-title">
+						{{ filename }}
+					</small>
+					<span
+						class="remove-file-btn material-icons cursor-pointer"
+						@click="removeFile">
+						close
+					</span>
+				</div>
+			</div>
 		</template>
 		<template #footer>
 			<button
 				class="btn btn-back"
 				type="button"
-				@click="close">
+				@click="emitClose">
 				Back
 			</button>
 			<button
-				class="btn submit-btn btn-primary"
+				:disabled="!hasExtracted || isFileSizeGreaterThanLimit"
 				:form="CREATE_POST_FORM_ID"
-				type="submit">
+				class="btn submit-btn btn-primary"
+				type="submit"
+				@click="sendFile">
 				Create post
 			</button>
 		</template>
-	</Overlay>
+	</overlay>
 </template>
+
 
 <style lang="scss">
 @import "@styles/btn.scss";
@@ -50,9 +95,11 @@
 import { ref, computed, inject } from "vue"
 
 import type { PageContext } from "$/types/renderer"
+import type { UnitError } from "$/types/server"
 import type { OptionInfo } from "$@/types/component"
 import type { DeserializedRoleResource } from "$/types/documents/role"
 import type { DeserializedPostAttachmentResource } from "$/types/documents/post_attachment"
+import { MAXIMUM_FILE_SIZE } from "$/constants/measurement"
 
 import Fetcher from "$@/fetchers/post"
 import PostAttachmentFetcher from "$@/fetchers/post_attachment"
@@ -60,6 +107,7 @@ import { post as permissionGroup } from "$/permissions/permission_list"
 import { CREATE_PUBLIC_POST_ON_ANY_DEPARTMENT } from "$/permissions/post_combinations"
 
 import Overlay from "@/helpers/overlay.vue"
+import ReceivedErrors from "@/helpers/message_handlers/received_errors.vue"
 import DraftForm from "@/post/draft_form.vue"
 import SelectableOptionsField from "@/fields/selectable_options.vue"
 
@@ -71,8 +119,6 @@ const { userProfile, departments } = pageProps
 const CREATE_POST_FORM_ID = "create-post"
 const fetcher = new Fetcher()
 const postAttachmentFetcher = new PostAttachmentFetcher()
-
-defineProps<{ isShown: boolean }>()
 
 const hasMultipleRoles = userProfile.data.roles.data.length > 1
 const roleNames = computed<OptionInfo[]>(() => userProfile.data.roles.data.map(data => ({
@@ -107,30 +153,74 @@ const departmentNames = computed<OptionInfo[]>(() => {
 const departmentID = ref<string>(userProfile.data.department.data.id)
 
 const content = ref<string>("")
-const attachmentResources = ref<DeserializedPostAttachmentResource[]>([])
 
+const props = defineProps<{
+	accept: "image/*" | "*/*"
+	isShown: boolean
+}>()
 interface CustomEvents {
 	(event: "close"): void
 }
 const emit = defineEmits<CustomEvents>()
 
-function close() {
+const isAcceptingImage = props.accept.includes("image/")
+const isAcceptingFile = props.accept.includes("*/")
+
+const filename = ref<string|null>(null)
+const hasExtracted = computed<boolean>(() => filename.value !== null)
+const previewFile = ref<any>(null)
+const fileSize = ref<number|null>(null)
+const isFileSizeGreaterThanLimit = computed(() => {
+	const castedFileSize = fileSize.value as number
+	return castedFileSize > MAXIMUM_FILE_SIZE
+})
+const fileUploadForm = ref()
+const receivedErrors = ref<string[]>([])
+const attachmentResources = ref<DeserializedPostAttachmentResource[]>([])
+
+function removeFile() {
+	filename.value = null
+	previewFile.value = null
+	fileSize.value = null
+	receivedErrors.value = []
+}
+
+function emitClose() {
 	emit("close")
 }
 
 function uploadPostAttachment(event: Event): void {
 	const target = event.target as HTMLInputElement
-	const files = target.files as FileList
-	const fileCount = files.length
-	const latestSelectedFile = files.item(fileCount - 1) as File
-	const formData = new FormData()
-	formData.set("data[attributes][fileContent]", latestSelectedFile)
-	formData.set("data[attributes][fileType]", latestSelectedFile.type)
-	formData.set("data[type]", "post_attachment")
+	const file = target.files?.item(0)
+	const rawFilename = file?.name as ""
 
-	postAttachmentFetcher.createWithFile(formData).then(({ body }) => {
-		const { data } = body
-		attachmentResources.value.push(data)
+	fileSize.value = file?.size as number|null
+	if (isFileSizeGreaterThanLimit.value) receivedErrors.value.push("Maximum file size is 20mb")
+
+	previewFile.value = file ? URL.createObjectURL(file) : ""
+	filename.value = rawFilename
+}
+
+
+function sendFile() {
+	const formData = new FormData(fileUploadForm.value as HTMLFormElement)
+
+	postAttachmentFetcher.createWithFile(formData)
+	.then(() => {
+		emitClose()
+	})
+
+	.catch(({ body }) => {
+		if (body) {
+			const { errors } = body
+			receivedErrors.value = errors.map((error: UnitError) => {
+				const readableDetail = error.detail
+
+				return readableDetail
+			})
+		} else {
+			receivedErrors.value = [ "an error occured" ]
+		}
 	})
 }
 
