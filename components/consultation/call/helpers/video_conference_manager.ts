07cfb09ca29aa37526bb3d480a019fe94aababa1
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-11-05 11:59:50 +0800

    unit(call): ensure closing local tracks

--- components/consultation/call/helpers/video_conference_manager.ts
+++ components/consultation/call/helpers/video_conference_manager.ts
@@ -1,19 +1,18 @@
+import type { Ref } from "vue"
 import type {
 	VideoConferenceManager,
 	VideoConferenceEngine,
 
 	LocalTracks,
-
-	RemoteUser,
-	RemoteAudioTrack,
-	RemoteVideoTrack,
-	RemoteTracks,
-	MediaType
+	RemoteTracks
 } from "@/consultation/call/helpers/types/video_conference_manager"
 
 import Stub from "$/singletons/stub"
 import isUndefined from "$/type_guards/is_undefined"
-import { Ref } from "vue"
+
+import {
+	handleRemoteUserJoining
+} from "@/consultation/call/helpers/video_conference_event_listeners"
 
 let videoConferenceManager: VideoConferenceManager|null = null
 function manager(): VideoConferenceManager {
@@ -30,45 +29,6 @@ export const localTracks: LocalTracks = {
 	"localVideoTrack": null
 }
 
-function handleRemoteUserJoining(
-	user: RemoteUser,
-	mediaType: MediaType,
-	remoteParticipants: Ref<RemoteTracks[]>
-) {
-	Stub.runConditionally(
-		async() => {
-			await engine().subscribe(user, mediaType)
-			const remoteID = `user-${String(user.uid)}`
-			remoteParticipants.value.push({
-				"remoteAudioTrack": user.audioTrack as RemoteAudioTrack,
-				remoteID,
-				"remoteVideoTrack": user.videoTrack as RemoteVideoTrack
-			})
-
-			if (mediaType === "video") user.videoTrack?.play(remoteID)
-			if (mediaType === "audio") user.audioTrack?.play()
-		},
-		() => {
-			remoteParticipants.value.push({
-				"remoteAudioTrack": user.audioTrack as RemoteAudioTrack,
-				"remoteID": "user-remote-id",
-				"remoteVideoTrack": user.videoTrack as RemoteVideoTrack
-			})
-
-			return [
-				Promise.resolve(),
-				{
-					"arguments": [
-						user,
-						mediaType,
-						remoteParticipants
-					],
-					"functionName": "handleRemoteUserJoining"
-				}
-			]
-		}
-	)
-}
 export async function initiateVideoConferenceEngine(remoteParticipants: Ref<RemoteTracks[]>) {
 	if (!isUndefined(window)) {
 		// @ts-ignore
@@ -80,50 +40,76 @@ export async function initiateVideoConferenceEngine(remoteParticipants: Ref<Remo
 
 		videoConferenceEngine.on(
 			"user-published",
-			(user, mediaType) => handleRemoteUserJoining(user, mediaType, remoteParticipants)
+			(user, mediaType) => handleRemoteUserJoining(engine, user, mediaType, remoteParticipants)
 		)
 	}
 }
 
-export async function joinAndPresentLocalTracks(
+export function joinAndPresentLocalTracks(
 	appId: string,
 	channelName: string,
 	chatMessageActivityID: string,
 	localParticipantID: string,
 	token: string
 ) {
-	await engine().join(
-		appId,
-		channelName,
-		token,
-		Number(chatMessageActivityID)
-	)
-	localTracks.localAudioTrack = await manager().createMicrophoneAudioTrack()
-	localTracks.localVideoTrack = await manager().createCameraVideoTrack()
+	Stub.runConditionally(
+		async() => {
+			await engine().join(
+				appId,
+				channelName,
+				token,
+				Number(chatMessageActivityID)
+			)
+			localTracks.localAudioTrack = await manager().createMicrophoneAudioTrack()
+			localTracks.localVideoTrack = await manager().createCameraVideoTrack()
 
-	await engine().publish([
-		localTracks.localAudioTrack,
-		localTracks.localVideoTrack
-	])
-	localTracks.localVideoTrack.play(localParticipantID)
+			await engine().publish([
+				localTracks.localAudioTrack,
+				localTracks.localVideoTrack
+			])
+			localTracks.localVideoTrack.play(localParticipantID)
+		},
+		() => {
+			localTracks.localAudioTrack = "this runs on test" as any
+			localTracks.localVideoTrack = "this runs on test" as any
+
+			return [
+				Promise.resolve(),
+				{
+					"arguments": [
+						appId,
+						channelName,
+						chatMessageActivityID,
+						localParticipantID,
+						token
+					],
+					"functionName": "joinAndPresentLocalTracks"
+				}
+			]
+		}
+	)
 }
 
 export function leaveAndRemoveLocalTracks() {
-	videoConferenceEngine = videoConferenceEngine as VideoConferenceEngine
-	videoConferenceManager = videoConferenceManager as VideoConferenceManager
+	Stub.runConditionally(
+		async() => {
+			videoConferenceEngine = videoConferenceEngine as VideoConferenceEngine
+			videoConferenceManager = videoConferenceManager as VideoConferenceManager
 
-	localTracks.localAudioTrack?.close()
-	localTracks.localVideoTrack?.close()
-}
+			localTracks.localAudioTrack?.close()
+			localTracks.localVideoTrack?.close()
+		},
+		() => {
+			localTracks.localAudioTrack = null
+			localTracks.localVideoTrack = null
 
-export function mockJoining() {
-	videoConferenceManager = videoConferenceManager as VideoConferenceManager
-	localTracks.localAudioTrack = Stub.runConditionally(
-		videoConferenceManager.createMicrophoneAudioTrack,
-		(): any => "this runs on test"
-	) as unknown as any
-	localTracks.localVideoTrack = Stub.runConditionally(
-		videoConferenceManager.createMicrophoneAudioTrack,
-		(): any => "this runs on test"
-	) as unknown as any
+			return [
+				Promise.resolve(),
+				{
+					"arguments": [],
+					"functionName": "leaveAndRemoveLocalTracks"
+				}
+			]
+		}
+	)
 }
