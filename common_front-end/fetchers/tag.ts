7691a2626d4370858d54d5af0905ab343d2e3b25
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-11-05 23:28:38 +0800

    intrn(fetcher): make method to count posts
    * imitates `countUsers` in role and department fetchers

--- common_front-end/fetchers/tag.ts
+++ common_front-end/fetchers/tag.ts
@@ -1,3 +1,4 @@
+import type { Response } from "$@/types/independent"
 import type {
 	TagResourceIdentifier,
 	TagAttributes,
@@ -6,12 +7,14 @@ import type {
 	TagDocument,
 	TagListDocument,
 	DeserializedTagDocument,
-	DeserializedTagListDocument
+	DeserializedTagListDocument,
+	TagIdentifierListDocument
 } from "$/types/documents/tag"
 
 import { TAG_LINK } from "$/constants/template_links"
 
 import BaseFetcher from "$@/fetchers/base"
+import stringifyQuery from "$@/fetchers/stringify_query"
 
 export default class TagFetcher extends BaseFetcher<
 	TagResourceIdentifier<"read">,
@@ -26,5 +29,32 @@ export default class TagFetcher extends BaseFetcher<
 > {
 	constructor() {
 		super(TAG_LINK)
+	}
+
+	countPosts(IDs: string[]): Promise<Response<
+		TagResourceIdentifier<"read">,
+		TagAttributes<"serialized">,
+		TagAttributes<"deserialized">,
+		TagResource,
+		DeserializedTagResource,
+		TagIdentifierListDocument<"read">
+	>> {
+		return this.handleResponse(
+			this.getJSON(
+				`${this.links.unbound}/count_posts?${stringifyQuery({
+					"filter": {
+						IDs
+					}
+				})}`
+			),
+			false
+		) as Promise<Response<
+			TagResourceIdentifier<"read">,
+			TagAttributes<"serialized">,
+			TagAttributes<"deserialized">,
+			TagResource,
+			DeserializedTagResource,
+			TagIdentifierListDocument<"read">
+		>>
 	}
 }
