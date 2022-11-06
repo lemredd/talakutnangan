fe0aee10906a5b711e12e127f327b2f94bc4f934
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-11-06 11:09:07 +0800

    unit(query): generalize names
    Co-authored-by: Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>

--- database/queries/department/sift_by_slug.spec.ts
+++ database/queries/department/sift_by_slug.spec.ts
@@ -1,51 +1,51 @@
-import Department from "%/models/department"
-import DepartmentFactory from "~/factories/department"
+import Model from "%/models/department"
+import Factory from "~/factories/department"
 
 import siftBySlug from "./sift_by_slug"
 
 describe("Database Pipe: Sift by slug", () => {
 	it("can find all", async() => {
-		const department = await new DepartmentFactory().insertOne()
+		const model = await new Factory().insertOne()
 		const slug = ""
 
 		const options = siftBySlug({}, { "filter": { slug } })
-		const foundDepartments = await Department.findAll(options)
+		const foundModels = await Model.findAll(options)
 
 		expect(options).not.toHaveProperty("include")
-		expect(foundDepartments).toHaveLength(1)
-		expect(foundDepartments).toHaveProperty("0.id", department.id)
+		expect(foundModels).toHaveLength(1)
+		expect(foundModels).toHaveProperty("0.id", model.id)
 	})
 
 	it("can find on specific using fullName", async() => {
-		const department = await new DepartmentFactory()
+		const model = await new Factory()
 		.fullName(() => "firstDepartment")
 		.insertOne()
-		await new DepartmentFactory()
+		await new Factory()
 		.fullName(() => "secondDepartment")
 		.insertOne()
 		const slug = "fir"
 
 		const options = siftBySlug({}, { "filter": { slug } })
-		const foundDepartments = await Department.findAll(options)
+		const foundModels = await Model.findAll(options)
 
 		expect(options).toHaveProperty("where")
-		expect(foundDepartments).toHaveLength(1)
-		expect(foundDepartments).toHaveProperty("0.id", department.id)
+		expect(foundModels).toHaveLength(1)
+		expect(foundModels).toHaveProperty("0.id", model.id)
 	})
 
 	it("cannot find on incorrect slug", async() => {
-		await new DepartmentFactory()
+		await new Factory()
 		.fullName(() => "firstDepartment")
 		.insertOne()
-		await new DepartmentFactory()
+		await new Factory()
 		.fullName(() => "secondDepartment")
 		.insertOne()
 		const slug = "xx"
 
 		const options = siftBySlug({}, { "filter": { slug } })
-		const foundDepartments = await Department.findAll(options)
+		const foundModels = await Model.findAll(options)
 
 		expect(options).toHaveProperty("where")
-		expect(foundDepartments).toHaveLength(0)
+		expect(foundModels).toHaveLength(0)
 	})
 })
