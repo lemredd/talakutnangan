4202422da8a3cf727f9a16c53d08802db92921fb
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-11-06 20:42:05 +0800

    fix(server): remove looping of server

--- server.js
+++
@@ -1,6 +0,0 @@
-const process = require("process")
-const childProcessor = require("child_process")
-
-const childProcess = childProcessor.exec("npm run server:prod")
-childProcess.stdout.pipe(process.stdout)
-childProcess.stderr.pipe(process.stderr)
