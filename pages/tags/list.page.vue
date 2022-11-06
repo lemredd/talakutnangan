ad7c49f20f4e74e87d962b3dac2f8fbf7da2ea8f
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-11-06 22:01:45 +0800

    fix(page): adhere to type requirements

--- pages/tags/list.page.vue
+++ pages/tags/list.page.vue
@@ -1,10 +1,10 @@
 <template>
 	<ResourceManager
-j		v-model:slug="slug"
+		v-model:slug="slug"
 		v-model:existence="existence"
 		:is-loaded="isLoaded"
-		:department-names="[]"
-		:tag-names="[]">
+		:department-names="[] as OptionInfo[]"
+		:tag-names="[] as OptionInfo[]">
 		<template #header>
 			<TabbedPageHeader title="Admin Configuration" :tab-infos="resourceTabInfos">
 				<template #additional-controls>
@@ -18,7 +18,7 @@ j		v-model:slug="slug"
 			</TabbedPageHeader>
 		</template>
 		<template #resources>
-			<ResourceList :filtered-list="list" :may-edit="mayEdittag"/>
+			<ResourceList :filtered-list="list" :may-edit="mayEditTag"/>
 		</template>
 	</ResourceManager>
 </template>
@@ -27,21 +27,17 @@ j		v-model:slug="slug"
 </style>
 
 <script setup lang="ts">
-import { inject, onMounted, ref, computed, watch } from "vue"
+import { inject, ref, computed } from "vue"
 
+import { OptionInfo } from "$@/types/component"
 import type { PageContext } from "$/types/renderer"
-import type { OptionInfo } from "$@/types/component"
 import type { DeserializedTagResource } from "$/types/documents/tag"
-import type { DeserializedDepartmentListDocument } from "$/types/documents/department"
 
 import { tag as permissionGroup } from "$/permissions/permission_list"
 import { CREATE, UPDATE, ARCHIVE_AND_RESTORE } from "$/permissions/tag_combinations"
-import { DEBOUNCED_WAIT_DURATION } from "$@/constants/time"
 import resourceTabInfos from "@/resource_management/resource_tab_infos"
 
 import Fetcher from "$@/fetchers/tag"
-import debounce from "$@/helpers/debounce"
-import loadRemainingDepartments from "@/resource_management/load_remaining_departments"
 
 import TabbedPageHeader from "@/helpers/tabbed_page_header.vue"
 import ResourceManager from "@/resource_management/resource_manager.vue"
@@ -51,33 +47,37 @@ type RequiredExtraProps = "tags"
 const pageContext = inject("pageContext") as PageContext<"deserialized", RequiredExtraProps>
 const { pageProps } = pageContext
 
+const { userProfile } = pageProps
+const mayCreateTag = computed<boolean>(() => {
+	const roles = userProfile.data.roles.data
+	const isPermitted = permissionGroup.hasOneRoleAllowed(roles, [
+		CREATE
+	])
+
+	return isPermitted
+})
+const mayEditTag = computed<boolean>(() => {
+	const roles = userProfile.data.roles.data
+	const isPermitted = permissionGroup.hasOneRoleAllowed(roles, [
+		UPDATE,
+		ARCHIVE_AND_RESTORE
+	])
+
+	return isPermitted
+})
+
 const fetcher = new Fetcher()
-const isLoaded = ref<boolean>(false)
+const isLoaded = ref<boolean>(true)
 const list = ref<DeserializedTagResource[]>(pageProps.tags.data as DeserializedTagResource[])
 
 const slug = ref<string>("")
 const existence = ref<"exists"|"archived"|"*">("exists")
-async function countPostsPerTag(IDsToCount: string[]) {
-	await fetcher.countUsers(IDsToCount).then(response => {
-		const deserializedData = response.body.data
 
-		for (const identifierData of deserializedData) {
-			const { id, meta } = identifierData
-
-			const index = list.value.findIndex(data => data.id === id)
-
-			if (index > -1) {
-				list.value[index].meta = meta
-			}
-		}
-	})
-	await fetchRoleInfos()
-}
 async function fetchTagInfos(): Promise<number|void> {
 	await fetcher.list({
 		"filter": {
-			"department": chosenDepartment.value,
 			"existence": existence.value,
+			"mustHavePost": false,
 			"slug": slug.value
 		},
 		"page": {
@@ -85,17 +85,22 @@ async function fetchTagInfos(): Promise<number|void> {
 			"offset": list.value.length
 		},
 		"sort": [ "name" ]
-	}).then(response => {
+	}).then(({ body }) => {
+		const { data } = body
 		isLoaded.value = true
-		const deserializedData = response.body.data as DeserializedTagResource[]
-		const IDsToCount = deserializedData.map(data => data.id)
+		const deserializedData = data as DeserializedTagResource[]
 
 		if (deserializedData.length === 0) return Promise.resolve()
 
 		list.value = [ ...list.value, ...deserializedData ]
 
-		// eslint-disable-next-line no-use-before-define
-		return countPostsPerTag(IDsToCount)
+		return Promise.resolve()
 	})
+}
+
+async function refetchTags() {
+	isLoaded.value = false
+	list.value = []
+	await fetchTagInfos()
 }
 </script>
