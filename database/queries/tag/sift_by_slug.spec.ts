cda2f2c4e75069725e4545d5e7787c78d267a6cf
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-11-06 11:12:24 +0800

    unit(query): prepare test for sifting by slug
    Co-authored-by: Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>

---
+++ database/queries/tag/sift_by_slug.spec.ts
@@ -0,0 +1,51 @@
+import Model from "%/models/tag"
+import Factory from "~/factories/tag"
+
+import siftBySlug from "./sift_by_slug"
+
+describe("Database Pipe: Sift by slug", () => {
+	it("can find all", async() => {
+		const model = await new Factory().insertOne()
+		const slug = ""
+
+		const options = siftBySlug({}, { "filter": { slug } })
+		const foundModels = await Model.findAll(options)
+
+		expect(options).not.toHaveProperty("include")
+		expect(foundModels).toHaveLength(1)
+		expect(foundModels).toHaveProperty("0.id", model.id)
+	})
+
+	it("can find on specific using name", async() => {
+		const model = await new Factory()
+		.name(() => "firstTag")
+		.insertOne()
+		await new Factory()
+		.name(() => "firstTag")
+		.insertOne()
+		const slug = "fir"
+
+		const options = siftBySlug({}, { "filter": { slug } })
+		const foundModels = await Model.findAll(options)
+
+		expect(options).toHaveProperty("where")
+		expect(foundModels).toHaveLength(1)
+		expect(foundModels).toHaveProperty("0.id", model.id)
+	})
+
+	it("cannot find on incorrect slug", async() => {
+		await new Factory()
+		.name(() => "firstTag")
+		.insertOne()
+		await new Factory()
+		.name(() => "firstTag")
+		.insertOne()
+		const slug = "xx"
+
+		const options = siftBySlug({}, { "filter": { slug } })
+		const foundModels = await Model.findAll(options)
+
+		expect(options).toHaveProperty("where")
+		expect(foundModels).toHaveLength(0)
+	})
+})
