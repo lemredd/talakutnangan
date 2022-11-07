8cfda076cf419b35e1760c028ec3c6933cab9121
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-11-07 11:17:35 +0800

    style(home): ensure the consultation instructions are indented properly

--- components/guest_homepage/Instructions.vue
+++ components/guest_homepage/Instructions.vue
@@ -48,23 +48,32 @@
 			What are the features of consultation?
 		</h1>
 		<div class="flex flex-col md:flex-row justify-center md:justify-around">
-			<div class="ttl text right-auto py-3 md:order-2">
-				<p>1. In Consultations, the Student will create a consult and type the name of an employee.</p>
-
-				<p>2. Consultations will only last 15mins in every consultation that is created.</p>
-
-				<p>3. Students can specify the reasons.</p>
-
-				<p>4. Students can select the time and date of an employee when to consult.</p>
-
-				<p>5. Students and Employees can interact on chat in consultations.</p>
-
-				<p>6. Students and Employees can send documents, videos, and pictures during their consultation.</p>
-
-				<p>7. Students and Employees also can use Video-Calling.</p>
-
-				<p>8. Students and Employees can end their consultation if there are no more questions.</p>
-			</div>
+			<ol class="ttl text right-auto py-3 md:order-2 list-zero-decimal list-outside pl-4">
+				<li>
+					<p>In Consultations, the Student will create a consult and type the name of an employee.</p>
+				</li>
+				<li>
+					<p>Consultations will only last 15mins in every consultation that is created.</p>
+				</li>
+				<li>
+					<p>Students can specify the reasons.</p>
+				</li>
+				<li>
+					<p>Students can select the time and date of an employee when to consult.</p>
+				</li>
+				<li>
+					<p>Students and Employees can interact on chat in consultations.</p>
+				</li>
+				<li>
+					<p>Students and Employees can send documents, videos, and pictures during their consultation.</p>
+				</li>
+				<li>
+					<p>Students and Employees also can use Video-Calling.</p>
+				</li>
+				<li>
+					<p>Students and Employees can end their consultation if there are no more questions.</p>
+				</li>
+			</ol>
 			<div class="pt-2 order-2 md:order-1 flex self-center justify-between">
 				<Person
 					class="mr-5"
