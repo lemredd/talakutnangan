8b069d955fe958f8fb33ac3ae00695ab21bb98f5
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-11-07 11:14:46 +0800

    style(home): ensure the forum instructions are indented properly

--- components/guest_homepage/Instructions.vue
+++ components/guest_homepage/Instructions.vue
@@ -5,15 +5,28 @@
 			What are features in forum?
 		</h1>
 		<div class="flex flex-col md:flex-row justify-center md:justify-around">
-			<div class="ttl text right-auto py-3">
-				<p>1. Students and Employees can view, comment, and post on the Forum.</p>
-
-				<p>2. Employees can now limit the comments on the following post that is created.</p>
-
-				<p>3. Students and Employees can now use Vote for any particular post on the employees.</p>
-
-				<p>4. Also votes will determine that how many students or employees are agree or disagree.</p>
-			</div>
+			<ol class="ttl pl-4 py-3 list-zero-decimal list-outside">
+				<li>
+					<p>
+						Students and Employees can view, comment, and post on the Forum.
+					</p>
+				</li>
+				<li>
+					<p>
+						Employees can limit the comments on the following post that is created.
+					</p>
+				</li>
+				<li>
+					<p>
+						Students and Employees can use Vote for any particular post on the employees.
+					</p>
+				</li>
+				<li>
+					<p>
+						Also votes will determine that how many students or employees are agree or disagree.
+					</p>
+				</li>
+			</ol>
 			<div class="left-auto w-min h-min flex self-center pt-2">
 				<div class="crowd flex flex-row items-stretch">
 					<Person variant="default" class="self-center"/>
@@ -40,7 +53,7 @@
 
 				<p>2. Consultations will only last 15mins in every consultation that is created.</p>
 
-				<p>3. Students can now specify the reasons.</p>
+				<p>3. Students can specify the reasons.</p>
 
 				<p>4. Students can select the time and date of an employee when to consult.</p>
 
