53e8796030ced487cc66286ec5acb27b55f1dc61
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-11-07 10:43:49 +0800

    intrn(validator): require 8 characters in email username

--- server/validators/comparison/email.ts
+++ server/validators/comparison/email.ts
@@ -5,6 +5,8 @@ import type {
 	ValidationConstraints
 } from "!/types/validation"
 
+import extractEmailUsername from "$!/helpers/extract_email_username"
+
 /**
  * Validator to check if data is the proper acronym of the other field
  */
@@ -16,15 +18,27 @@ export default async function(
 
 	if (state.maySkip) return state
 
-	if (validator.isEmail(state.value)) {
-		return state
+	if (!validator.isEmail(state.value)) {
+		const error = {
+			"field": constraints.field,
+			"friendlyName": constraints.friendlyName,
+			"messageMaker": (field: string) => `Field "${field}" must be valid email.`
+		}
+
+		throw error
 	}
 
-	const error = {
-		"field": constraints.field,
-		"friendlyName": constraints.friendlyName,
-		"messageMaker": (field: string) => `Field "${field}" must be valid email.`
+	if (extractEmailUsername(state.value.trim()).length < 8) {
+		const error = {
+			"field": constraints.field,
+			"friendlyName": constraints.friendlyName,
+			"messageMaker": (
+				field: string
+			) => `Field "${field}" should have 8 characters before "@" character.`
+		}
+
+		throw error
 	}
 
-	throw error
+	return state
 }
