e83660eadc459d484bf6647eca166019c4819d4d
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-11-08 00:57:49 +0800

    intrn(password): disable scrolling

--- components/authentication/change_password.vue
+++ components/authentication/change_password.vue
@@ -18,15 +18,16 @@
 					v-model="confirmNewPassword"
 					label="Confirm new password"
 					placeholder="confirm your new password"/>
+
+				<button
+					class="btn btn-primary"
+					type="button"
+					@click="savePassword">
+					Save password
+				</button>
 			</form>
 		</template>
 		<template #footer>
-			<button
-				class="btn btn-primary"
-				type="button"
-				@click="savePassword">
-				Save password
-			</button>
 		</template>
 	</Overlay>
 </template>
@@ -54,13 +55,18 @@
 </style>
 
 <script setup lang="ts">
-import { ref, inject, onMounted } from "vue"
+import { Ref, ref, inject, onMounted } from "vue"
 
 import type { PageContext } from "$/types/renderer"
 import type { UnitError } from "$/types/server"
 
+import { BODY_CLASSES } from "$@/constants/provided_keys"
+
+import isUndefined from "$/type_guards/is_undefined"
+
 import Fetcher from "$@/fetchers/user"
 import makeSwitch from "$@/helpers/make_switch"
+import BodyCSSClasses from "$@/external/body_css_classes"
 
 import Overlay from "@/helpers/overlay.vue"
 import SensitiveTextField from "@/fields/sensitive_text.vue"
@@ -78,6 +84,7 @@ const {
 const currentPassword = ref<string>("")
 const newPassword = ref<string>("")
 const confirmNewPassword = ref<string>("")
+const bodyClasses = inject(BODY_CLASSES) as Ref<BodyCSSClasses>
 
 const fetcher: Fetcher = new Fetcher()
 
@@ -95,6 +102,7 @@ const receivedErrors = ref<string[]>([])
 const successMessages = ref<string[]>([])
 function cancel(): void {
 	if (receivedErrors.value.length) receivedErrors.value = []
+	bodyClasses.value.scroll(true)
 	clearPasswords()
 	closeDialog()
 }
@@ -123,7 +131,12 @@ function savePassword() {
 
 onMounted(() => {
 	if (userProfile.meta.hasDefaultPassword) {
-		openDialog()
+		const TIMEOUT = 3000
+
+		setTimeout(() => {
+			openDialog()
+			if (!isUndefined(window)) bodyClasses.value.scroll(false)
+		}, TIMEOUT)
 	}
 })
 </script>
