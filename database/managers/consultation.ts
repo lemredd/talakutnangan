fd6f79dcbd85c1f71519e691d4456462c1ffff21
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-11-09 01:13:37 +0800

    perf(manager): reduce the required tables to join

--- database/managers/consultation.ts
+++ database/managers/consultation.ts
@@ -196,12 +196,6 @@ export default class extends BaseManager<
 			const model = await this.model.findByPk(consultationID, {
 				"include": [
 					{
-						"include": [
-							{
-								"model": User,
-								"required": true
-							}
-						],
 						"model": AttachedRole,
 						"required": true
 					}
@@ -212,15 +206,9 @@ export default class extends BaseManager<
 			const activeConsultations = await this.model.findAll({
 				"include": [
 					{
-						"include": [
-							{
-								"model": User,
-								"required": true,
-								"where": new Condition().equal("id", model?.consultant?.id).build()
-							}
-						],
 						"model": AttachedRole,
-						"required": true
+						"required": true,
+						"where": new Condition().equal("userID", model?.consultantInfo?.userID).build()
 					}
 				],
 				"where": new Condition().and(
