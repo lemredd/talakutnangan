10d93a779d4efe90d553907b81842422207c0b18
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-11-11 22:15:19 +0800

    rfmt(component): prepare buttons

--- components/helpers/page_counter.vue
+++ components/helpers/page_counter.vue
@@ -1,4 +1,13 @@
 <template>
+	<button>
+		<span class="material-icons">chevron_left</span>
+		Previous
+	</button>
+	<button>1</button>
+	<button>
+		<span class="material-icons">chevron_right</span>
+		Previous
+	</button>
 </template>
 
 <style>
