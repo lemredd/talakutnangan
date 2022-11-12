6c02da9f889d5de9dfdfabef834c851745a54171
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-11-12 11:20:12 +0800

    unit(component): ensure page count generation

--- components/helpers/page_counter.spec.ts
+++ components/helpers/page_counter.spec.ts
@@ -3,13 +3,27 @@ import { shallowMount } from "@vue/test-utils"
 import Component from "./page_counter.vue"
 
 describe("Component: page counter", () => {
-	it("can generate page count buttons by given count", () => {
+	it("can generate page count buttons by balanced count", () => {
 		const wrapper = shallowMount(Component, {
 			"props": {
 				"maxCount": 50
 			}
 		})
 
-		console.log(wrapper.html(), "\n\n\n")
+		const pageCountBtns = wrapper.findAll(".page-count-btn")
+		const expectedLength = 5
+		expect(pageCountBtns.length).toEqual(expectedLength)
+	})
+
+	it("can generate page count buttons by imbalance count", () => {
+		const wrapper = shallowMount(Component, {
+			"props": {
+				"maxCount": 48
+			}
+		})
+
+		const pageCountBtns = wrapper.findAll(".page-count-btn")
+		const expectedLength = 5
+		expect(pageCountBtns.length).toEqual(expectedLength)
 	})
 })
