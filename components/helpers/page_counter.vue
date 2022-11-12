43905341ed1b941c716efed0bb921e86a3ce2bbb
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-11-12 11:00:23 +0800

    rfmt(component): render page button based on count

--- components/helpers/page_counter.vue
+++ components/helpers/page_counter.vue
@@ -3,7 +3,12 @@
 		<span class="material-icons">chevron_left</span>
 		Previous
 	</button>
-	<button>1</button>
+	<button
+		v-for="pageCount in pageLength"
+		:key="pageCount"
+		class="page-count-btn">
+		{{ pageCount }}
+	</button>
 	<button>
 		<span class="material-icons">chevron_right</span>
 		Previous
@@ -14,8 +19,16 @@
 </style>
 
 <script setup lang="ts">
+import { computed } from "vue"
+
+import { DEFAULT_LIST_LIMIT } from "$/constants/numerical"
+
 type DefinedProps = {
 	maxCount: number
 }
-defineProps<DefinedProps>()
+const props = defineProps<DefinedProps>()
+
+const pageLength = computed(
+	() => Math.ceil(props.maxCount / DEFAULT_LIST_LIMIT)
+)
 </script>
