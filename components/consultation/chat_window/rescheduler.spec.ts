83ba90dda3e203e81d457aea7e1dcd22af8ee258
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-11-14 00:39:23 +0800

    unit(consultation): prepare testing

--- components/consultation/chat_window/rescheduler.spec.ts
+++ components/consultation/chat_window/rescheduler.spec.ts
@@ -3,5 +3,5 @@ import { shallowMount } from "@vue/test-utils"
 import Component from "./rescheduler.vue"
 
 describe("Component: consultation rescheduler", () => {
-
+	it.todo("can emit new set schedule")
 })
