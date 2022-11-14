758068ca3411b52d07b7f0b3b31bc310be7f3354
Angelo Magtoto <19201.magtoto.angelo.l@gmail.com>
2022-11-14 18:09:55 +0800

    unit(tag): ensure tag create page works

---
+++ pages/tag/create.spec.ts
@@ -0,0 +1,51 @@
+import { mount } from "@vue/test-utils"
+import RequestEnvironment from "$/singletons/request_environment"
+
+import Page from "./create.page.vue"
+
+describe("Page: /tag", () => {
+	it("can create tag", async() => {
+		fetchMock.mockResponseOnce(JSON.stringify({
+			"data": {
+				"type": "tag",
+				"attributes": {
+					"deletedAt": null,
+					"name": "Xmaple"
+				}
+			}
+		}), { "status": RequestEnvironment.status.OK })
+		const wrapper = mount(Page, {
+			"global": {
+				"provide": {
+					"pageContext": {}
+				}
+			}
+		})
+		const tag = {
+			"data": {
+				"deletedAt": null,
+				"name": "Xmaple"
+			}
+		}
+
+		const nameInput = wrapper.find("input.name")
+		const submitBtn = wrapper.find("input[type=submit]")
+
+		await nameInput.setValue(tag.data.name)
+		await submitBtn.trigger("submit")
+
+		const castFetch = fetch as jest.Mock<any, any>
+		const [ [ request ] ] = castFetch.mock.calls
+		expect(request).toHaveProperty("method", "POST")
+		expect(request).toHaveProperty("url", "/api/tag")
+		expect(request.json()).resolves.toStrictEqual({
+			"data": {
+				"type": "tag",
+				"attributes": {
+					"deletedAt": null,
+					"name": "Xmaple"
+				}
+			}
+		})
+	})
+})
