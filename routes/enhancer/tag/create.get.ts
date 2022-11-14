a77cf3d7aded6302cf2a1527cdac79f94990f1f0
Angelo Magtoto <19201.magtoto.angelo.l@gmail.com>
2022-11-14 18:07:15 +0800

    intrn(route): tag create enhancer

---
+++ routes/enhancer/tag/create.get.ts
@@ -0,0 +1,24 @@
+import type { DocumentProps } from "$/types/server"
+
+import Policy from "!/bases/policy"
+import { CREATE } from "$/permissions/tag_combinations"
+import PageMiddleware from "!/bases/controller-likes/page_middleware"
+import { tag as permissionGroup } from "$/permissions/permission_list"
+import PermissionBasedPolicy from "!/policies/permission-based"
+
+export default class extends PageMiddleware {
+	get filePath(): string { return __filename }
+
+	get policy(): Policy {
+		return new PermissionBasedPolicy(permissionGroup, [
+			CREATE
+		])
+	}
+
+	getDocumentProps(): DocumentProps {
+		return {
+			"description": "Consultation chat platform for MCC",
+			"title": "Create Tags | Talakutnangan"
+		}
+	}
+}
