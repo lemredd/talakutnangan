f61865f0dd0a6d8b031fc5c47471212c70249a0a
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-11-14 20:01:23 +0800

    intrn(back-end): add regex for post content

--- common_back-end/constants/regex.ts
+++ common_back-end/constants/regex.ts
@@ -35,3 +35,6 @@ export const fileTypeDescription = "be a valid MIME type"
 
 export const channelName = /^\w+$/u
 export const channelNameDescription = "have alphanumeric or underscore characters only"
+
+export const postContent = /^.+<(script).+?>.+<\/\1{1}>.+$/u
+export const postContentDescription = "have no banned tags"
