3fb6ed90893f7c7b9d974987c2725a5f02d809c6
Angelo Magtoto <19201.magtoto.angelo.l@gmail.com>
2022-11-14 18:06:26 +0800

    intrn(route): tag list enhancer

---
+++ routes/enhancer/tag/list.get.ts
@@ -0,0 +1,50 @@
+import type { Serializable } from "$/types/general"
+import type { DocumentProps } from "$/types/server"
+import type { AuthenticatedRequest } from "!/types/dependent"
+
+import Policy from "!/bases/policy"
+import Manager from "%/managers/tag"
+import PageMiddleware from "!/bases/controller-likes/page_middleware"
+
+import PermissionBasedPolicy from "!/policies/permission-based"
+import { tag as permissionGroup } from "$/permissions/permission_list"
+import { CREATE, UPDATE, ARCHIVE_AND_RESTORE } from "$/permissions/tag_combinations"
+
+export default class extends PageMiddleware {
+	get filePath(): string { return __filename }
+
+	get policy(): Policy {
+		return new PermissionBasedPolicy(permissionGroup, [
+			CREATE,
+			UPDATE,
+			ARCHIVE_AND_RESTORE
+		])
+	}
+
+	getDocumentProps(): DocumentProps {
+		return {
+			"description": "List of tags in Talakutnangan",
+			"title": "Tag list | Talakutnangan"
+		}
+	}
+
+	async getPageProps(request: AuthenticatedRequest): Promise<Serializable> {
+		const manager = new Manager(request)
+
+		const pageProps = {
+			"tags": await manager.list({
+				"filter": {
+					"existence": "exists",
+					"slug": ""
+				},
+				"page": {
+					"limit": 10,
+					"offset": 0
+				},
+				"sort": [ "name" ]
+			})
+		}
+
+		return pageProps
+	}
+}
