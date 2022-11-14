bcb589d961a249d799e90f0be34a01cae473c6c7
Angelo Magtoto <19201.magtoto.angelo.l@gmail.com>
2022-11-14 18:13:05 +0800

    intrn(manager): initialize sift by slug and semester query parameters in tag manager

--- database/managers/tag.ts
+++ database/managers/tag.ts
@@ -1,18 +1,27 @@
-import type { CommonQueryParameters } from "$/types/query"
-import type { ModelCtor } from "%/types/dependent"
+import type { Pipe } from "$/types/database"
+import type { SemesterQueryParameters } from "$/types/query"
+import type { FindAndCountOptions, ModelCtor } from "%/types/dependent"
 import type { TagAttributes } from "$/types/documents/tag"
 
 import BaseManager from "%/managers/base"
 import Tag from "%/models/tag"
+import siftBySlug from "%/queries/tag/sift_by_slug"
 
 import TagTransformer from "%/transformers/tag"
 
 export default class extends BaseManager<
 	Tag,
 	TagAttributes<"deserialized">,
-	CommonQueryParameters
+	SemesterQueryParameters
 > {
 	get model(): ModelCtor<Tag> { return Tag }
 
 	get transformer(): TagTransformer { return new TagTransformer() }
+
+	get listPipeline(): Pipe<FindAndCountOptions<Tag>, SemesterQueryParameters>[] {
+		return [
+			siftBySlug,
+			...super.listPipeline
+		]
+	}
 }
