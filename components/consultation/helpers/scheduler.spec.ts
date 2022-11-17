8377d36938a546cbbe7ca4255eda50d47be7f34d
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-11-17 19:01:00 +0800

    unit(consultation): make test to ensure regeneration of chosen day

--- components/consultation/helpers/scheduler.spec.ts
+++ components/consultation/helpers/scheduler.spec.ts
@@ -9,6 +9,58 @@ import convertDateToRangeCompatibleDate
 	from "@/helpers/schedule_picker/convert_date_to_range_compatible_date"
 
 describe("Component: consultation/helpers/scheduler", () => {
+	it("can fill consultant schedules", async() => {
+		const currentTime = new Date()
+		const currentHour = 8
+		currentTime.setHours(currentHour)
+		currentTime.setMinutes(0)
+		currentTime.setSeconds(0)
+		currentTime.setMilliseconds(0)
+
+		const schedules = {
+			"data": [
+				{
+					"dayName": DayValues[currentTime.getDay()],
+					"id": "1",
+					"scheduleEnd": convertTimeToMinutes("09:00"),
+					"scheduleStart": convertTimeToMinutes("08:00"),
+					"type": "employee_schedule"
+				}
+			],
+			"meta": {
+				"count": 1
+			}
+		} as any
+
+		const defaultDay = new Date(currentTime)
+		defaultDay.setHours(0)
+
+		const wrapper = shallowMount(Component, {
+			"props": {
+				"chosenDay": "",
+				"chosenTime": "",
+				"consultantSchedules": {
+					"data": [],
+					"meta": {
+						"count": 0
+					}
+				}
+			}
+		})
+
+		// Load selectable days and its options
+		const castedWrapper = wrapper.vm as any
+		castedWrapper.dateToday = currentTime
+
+		await wrapper.setProps({
+			"consultantSchedules": schedules
+		})
+
+		// Customizable date
+		const emitted = wrapper.emitted()
+		expect(emitted).toHaveProperty("update:chosenDay.0.0", defaultDay.toJSON())
+	})
+
 	it("can select within employee's schedules", async() => {
 		const currentTime = new Date()
 		const currentHour = 8
