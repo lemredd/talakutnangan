654f80943123ddb38ebe9bdde235423a552b45cb
Angelo Magtoto <19201.magtoto.angelo.l@gmail.com>
2022-11-17 15:16:08 +0800

    front(component): add semester management info

---
+++ components/semester/make_management_info.ts
@@ -0,0 +1,31 @@
+import type { SemesterManagementInfo } from "$@/types/independent"
+import type { DeserializedUserProfile } from "$/types/documents/user"
+import type { DeserializedSemesterResource } from "$/types/documents/semester"
+
+import { semester as permissionGroup } from "$/permissions/permission_list"
+import {
+	UPDATE, ARCHIVE_AND_RESTORE
+} from "$/permissions/semester_combinations"
+
+export default function(
+	userProfile: DeserializedUserProfile<"roles"|"department">,
+	semester: DeserializedSemesterResource
+): SemesterManagementInfo {
+	const isDeleted = Boolean(semester.deletedAt)
+	const roles = userProfile.data.roles.data
+
+	const mayUpdateSemester = permissionGroup.hasOneRoleAllowed(roles, [ UPDATE ])
+
+	const mayArchiveSemester = permissionGroup.hasOneRoleAllowed(roles, [
+		ARCHIVE_AND_RESTORE
+	]) && !isDeleted
+
+	const mayRestoreSemester = !mayArchiveSemester
+
+	return {
+		isDeleted,
+		mayArchiveSemester,
+		mayRestoreSemester,
+		mayUpdateSemester
+	}
+}
