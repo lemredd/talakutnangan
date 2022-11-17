f92354cd2ab09b206a245f2a154004ebe943c926
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-11-17 23:18:45 +0800

    intrn(component)!: generalize searchable chip

--- components/helpers/filters/searchable_chip.vue
+++ components/helpers/filters/searchable_chip.vue
@@ -1,188 +1,120 @@
 <template>
-	<div class="field pb-5">
+	<div class="field">
 		<h2 class="text-lg uppercase">
 			{{ header }}
 		</h2>
 		<div
-			v-for="participant in selectedParticipants"
-			:key="participant.id"
-			class="chip selected-participants">
+			v-for="chip in selectedChips"
+			:key="chip.id"
+			class="chip selected">
 			<span>
-				{{ participant.name }}
+				{{ chip.data }}
 			</span>
 			<span
-				v-if="!isChipForCurrentUser(participant.id)"
+				v-if="chip.mayRemove"
 				id="close-btn"
 				class="material-icons"
-				@click="removeParticipant">
+				@click="removeChip(chip.id)">
 				close
 			</span>
 		</div>
 		<NonSensitiveTextField
-			v-if="mayAddOtherParticipants"
+			v-if="maySelectMore"
 			v-model="slug"
 			:label="textFieldLabel"
 			type="text"/>
 		<Suspensible :is-loaded="isLoaded">
 			<div
-				v-for="participant in otherParticipants"
-				:key="participant.id"
-				class="chip other-participants cursor-pointer hover:bg-gray-300"
-				@click="addParticipant">
-				{{ participant.name }}
+				v-for="chip in unselectedChips"
+				:key="chip.id"
+				class="chip unselected"
+				@click="addChip(chip.id)">
+				{{ chip.data }}
 			</div>
 		</Suspensible>
 	</div>
 </template>
 
 <style lang="scss">
-@import "@styles/btn.scss";
+	@import "@styles/btn.scss";
 
-.chip {
-	@apply inline-flex items-center text-sm;
+	.field {
+		@apply pb-5;
+	}
 
-	margin:5px;
-	border-radius: 25px;
-	padding: 0 15px;
+	.chip {
+		@apply inline-flex items-center text-sm;
 
-	height: 30px;
+		margin: 5px;
+		border-radius: 25px;
+		padding: 0 15px;
 
-	color: black;
-	background-color:#f1f1f1;
-}
+		height: 30px;
 
-#close-btn {
-  padding-left: 10px;
-  color: #888;
-  font-weight: bold;
-  float: right;
-  font-size: 20px;
-  cursor: pointer;
-}
+		color: black;
+		background-color: #f1f1f1;
 
-#close-btn:hover {
-  color: #000;
-}
+		&.unselected {
+			@apply cursor-pointer hover:bg-gray-300;
+		}
+	}
+
+	#close-btn {
+		padding-left: 10px;
+		color: #888;
+		font-weight: bold;
+		float: right;
+		font-size: 20px;
+		cursor: pointer;
+	}
+
+	#close-btn:hover {
+		color: #000;
+	}
 </style>
 
 <script setup lang="ts">
-import { ref, computed, watch, onMounted } from "vue"
+import { computed } from "vue"
 
-import type { UserKind } from "$/types/database"
-import type { DeserializedUserResource } from "$/types/documents/user"
-
-import { DEBOUNCED_WAIT_DURATION } from "$@/constants/time"
-
-import Fetcher from "$@/fetchers/user"
-import debounce from "$@/helpers/debounce"
+import type { ChipData } from "$@/types/component"
 
 import Suspensible from "@/helpers/suspensible.vue"
 import NonSensitiveTextField from "@/fields/non-sensitive_text.vue"
-import RequestEnvironment from "$/singletons/request_environment"
 
 const props = defineProps<{
-	currentUserId?: string
 	header: string,
-	modelValue: DeserializedUserResource[],
-	maximumParticipants: number,
-	textFieldLabel: string,
-	kind: UserKind
+	modelValue: string,
+	isLoaded: boolean,
+	unselectedChips: ChipData[],
+	selectedChips: ChipData[],
+	maximumSelectedChips: number,
+	textFieldLabel: string
 }>()
 
 interface CustomEvents {
-	(eventName: "update:modelValue", resource: DeserializedUserResource[]): void
+	(eventName: "update:modelValue", newSlug: string): void
+	(eventName: "addChip", id: string): void
+	(eventName: "removeChip", id: string): void
 }
 const emit = defineEmits<CustomEvents>()
 
-const otherParticipants = ref<DeserializedUserResource[]>([])
-const slug = ref<string>("")
-const selectedParticipants = computed<DeserializedUserResource[]>({
-	get(): DeserializedUserResource[] {
+const slug = computed<string>({
+	get(): string {
 		return props.modelValue
 	},
-	set(newValue: DeserializedUserResource[]): void {
+	set(newValue: string): void {
 		emit("update:modelValue", newValue)
 	}
 })
-const mayAddOtherParticipants = computed<boolean>(
-	() => props.modelValue.length < props.maximumParticipants
+const maySelectMore = computed<boolean>(
+	() => props.selectedChips.length < props.maximumSelectedChips
 )
 
-let rawFetcher: Fetcher|null = null
-
-function fetcher(): Fetcher {
-	if (rawFetcher === null) throw new Error("User cannot be processed yet")
-
-	return rawFetcher
+function addChip(id: string): void {
+	emit("addChip", id)
 }
 
-function isChipForCurrentUser(participantId: string) {
-	return props.currentUserId === participantId
+function removeChip(id: string): void {
+	emit("removeChip", id)
 }
-
-const isLoaded = ref(true)
-function findMatchedUsers() {
-	isLoaded.value = false
-	fetcher().list({
-		"filter": {
-			"department": "*",
-			"existence": "exists",
-			"kind": props.kind,
-			"role": "*",
-			"slug": slug.value
-		},
-		"page": {
-			"limit": 10,
-			"offset": 0
-		},
-		"sort": [ "name" ]
-	}).then(({ body }) => {
-		otherParticipants.value = body.data.filter(candidate => {
-			const isNotSelected = selectedParticipants.value.findIndex(selectedParticipant => {
-				const doesMatchSelectedParticipant = candidate.id === selectedParticipant.id
-				return doesMatchSelectedParticipant
-			}) === -1
-
-			return isNotSelected
-		})
-		isLoaded.value = true
-	})
-}
-
-watch(slug, debounce(findMatchedUsers, DEBOUNCED_WAIT_DURATION))
-
-function removeParticipant(event: Event): void {
-	const { target } = event
-	const castTarget = target as HTMLSpanElement
-	const button = castTarget.previousElementSibling as HTMLButtonElement
-	const text = RequestEnvironment.isOnTest
-		? button.innerHTML
-		: button.innerText
-
-	selectedParticipants.value = selectedParticipants.value.filter(user => {
-		const isNameMatching = text.includes(user.name)
-		return !isNameMatching
-	})
-}
-
-function addParticipant(event: Event): void {
-	const target = event.target as HTMLDivElement
-	const participantName = RequestEnvironment.isOnTest
-		? target.innerHTML
-		: target.innerText
-
-	const foundParticipant = otherParticipants.value.find(user => {
-		const isNameMatching = participantName.includes(user.name)
-		return isNameMatching
-	})
-
-	if (foundParticipant) {
-		selectedParticipants.value = [ ...selectedParticipants.value, foundParticipant ]
-	}
-
-	otherParticipants.value = []
-}
-onMounted(() => {
-	rawFetcher = new Fetcher()
-})
 </script>
