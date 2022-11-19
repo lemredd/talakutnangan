201c84890d8729310a4f4945eb9e8a406d2c649e
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-11-19 21:50:10 +0800

    unit(manager): ensure reattachment of tags works as expected

--- database/managers/tag.spec.ts
+++ database/managers/tag.spec.ts
@@ -1,4 +1,40 @@
+import Factory from "~/factories/tag"
+import PostTag from "%/models/post_tag"
+import PostFactory from "~/factories/post"
+
+import Condition from "%/helpers/condition"
+
 import Manager from "./tag"
+
+describe("Database Manager: Tag update operations", () => {
+	it("can reattach tags", async() => {
+		const tags = await new Factory().insertMany(4)
+		const manager = new Manager()
+		const post = await new PostFactory().insertOne()
+		await PostTag.bulkCreate([
+			tags[0].id,
+			tags[1].id,
+			tags[2].id
+		].map(id => ({
+			"postID": post.id,
+			"tagID": id
+		})))
+
+		await manager.reattach(post.id, [
+			tags[1].id,
+			tags[2].id,
+			tags[3].id
+		])
+
+		const postTags = await PostTag.findAll({
+			"where": new Condition().equal("postID", post.id).build()
+		})
+		expect(postTags).toHaveLength(3)
+		expect(postTags).toHaveProperty("0.tagID", tags[1].id)
+		expect(postTags).toHaveProperty("1.tagID", tags[2].id)
+		expect(postTags).toHaveProperty("2.tagID", tags[3].id)
+	})
+})
 
 describe("Database Manager: Miscellaneous tag operations", () => {
 	it("can get sortable columns", () => {
