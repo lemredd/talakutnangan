e54acdc2072ed2386fc556d8c5717ed9caea5662
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-11-19 19:31:27 +0800

    intrn(server): stop registering some routes during maintenance mode

--- routes/enhancer/router.ts
+++ routes/enhancer/router.ts
@@ -1,4 +1,5 @@
 import ControllerLike from "!/bases/controller-like"
+import RequestEnvironment from "$!/singletons/request_environment"
 
 import GetHome from "!%/enhancer/home.get"
 import GetIndex from "!%/enhancer/index.get"
@@ -7,23 +8,25 @@ import { controllers as userControllers } from "!%/enhancer/user/router"
 import { controllers as roleControllers } from "!%/enhancer/role/router"
 import { controllers as postControllers } from "!%/enhancer/post/router"
 import { controllers as forumControllers } from "!%/enhancer/forum/router"
-import { controllers as auditTrailControllers } from "!%/enhancer/audit_trail/router"
 import { controllers as semesterControllers } from "!%/enhancer/semester/router"
 import { controllers as departmentControllers } from "!%/enhancer/department/router"
 import { controllers as userSettingsControllers } from "!%/enhancer/settings/router"
+import { controllers as auditTrailControllers } from "!%/enhancer/audit_trail/router"
 import { controllers as consultationControllers } from "!%/enhancer/consultation/router"
 
-export const controllers: (new() => ControllerLike)[] = [
-	...tagControllers,
-	...userControllers,
-	...roleControllers,
-	...postControllers,
-	...forumControllers,
-	...auditTrailControllers,
-	...semesterControllers,
-	...departmentControllers,
-	...consultationControllers,
-	...userSettingsControllers,
-	GetHome,
-	GetIndex
-]
+export const controllers: (new() => ControllerLike)[] = RequestEnvironment.isInMaintenanceMode
+	? [ GetIndex ]
+	: [
+		...tagControllers,
+		...userControllers,
+		...roleControllers,
+		...postControllers,
+		...forumControllers,
+		...auditTrailControllers,
+		...semesterControllers,
+		...departmentControllers,
+		...consultationControllers,
+		...userSettingsControllers,
+		GetHome,
+		GetIndex
+	]
