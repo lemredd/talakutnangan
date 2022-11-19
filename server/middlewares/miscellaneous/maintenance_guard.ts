524fa3153b90c50a18a2a1455a7a3df081847dbc
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-11-19 17:04:18 +0800

    fix(server): redirect to index route properly
    Co-authored-by: Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>

--- server/middlewares/miscellaneous/maintenance_guard.ts
+++ server/middlewares/miscellaneous/maintenance_guard.ts
@@ -1,12 +1,11 @@
 import type { Request, Response, NextFunction } from "!/types/dependent"
 
-import { HOME } from "$/constants/template_page_paths"
 import RequestEnvironment from "$/singletons/request_environment"
 import ForceRedirector from "!/middlewares/miscellaneous/force_redirector"
 
 export default class MaintenanceGuard extends ForceRedirector {
 	constructor() {
-		super(HOME, RequestEnvironment.status.TEMPORARY_REDIRECT)
+		super("/", RequestEnvironment.status.TEMPORARY_REDIRECT)
 	}
 
 	async intermediate(request: Request, response: Response, next: NextFunction)
@@ -15,7 +14,7 @@ export default class MaintenanceGuard extends ForceRedirector {
 			process.env.IS_IN_MAINTENANCE
 			&& process.env.IS_IN_MAINTENANCE !== "false"
 			&& !(
-				request.url === HOME
+				request.url === "/"
 				&& request.method === "GET"
 			)
 		) {
