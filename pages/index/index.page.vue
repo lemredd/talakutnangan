0dd7e38d7cd18ed9a4185643f2b5d0d59d840904
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-11-19 16:25:08 +0800

    feat(user): add message for maintenance mode

--- pages/index/index.page.vue
+++ pages/index/index.page.vue
@@ -1,8 +1,15 @@
 <template>
-	<div>
+	<div v-if="isInMaintenance">
 		<Opening/>
 		<Instructions/>
 		<ChangePassword v-if="hasDefaultPassword"/>
+	</div>
+	<div v-else>
+		<div>
+			<p>The application is in maintenance mode.</p>
+			<p>Possible reason is due to updates being applied to the system like security updates.</p>
+			<p>Another reason is to defend from detected DDOS attacks.</p>
+		</div>
 	</div>
 </template>
 
@@ -18,7 +25,7 @@ import Instructions from "@/guest_homepage/instructions.vue"
 import ChangePassword from "@/authentication/change_password.vue"
 
 const { pageProps } = inject("pageContext") as PageContext<"deserialized">
-const { userProfile } = pageProps
+const { userProfile, isInMaintenance } = pageProps
 
 const hasDefaultPassword = userProfile !== null
 	&& !isUndefined(userProfile.meta.hasDefaultPassword)
