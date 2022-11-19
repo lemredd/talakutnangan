55f115c9424c5900bca7455d58eaa054eb3cd64c
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-11-19 16:56:35 +0800

    intrn(middleware): call other middlewares if not redirected

--- server/middlewares/miscellaneous/maintenance_guard.ts
+++ server/middlewares/miscellaneous/maintenance_guard.ts
@@ -1,11 +1,12 @@
 import type { Request, Response, NextFunction } from "!/types/dependent"
 
+import { HOME } from "$/constants/template_page_paths"
 import RequestEnvironment from "$/singletons/request_environment"
 import ForceRedirector from "!/middlewares/miscellaneous/force_redirector"
 
 export default class MaintenanceGuard extends ForceRedirector {
 	constructor() {
-		super("/", RequestEnvironment.status.TEMPORARY_REDIRECT)
+		super(HOME, RequestEnvironment.status.TEMPORARY_REDIRECT)
 	}
 
 	async intermediate(request: Request, response: Response, next: NextFunction)
@@ -14,11 +15,13 @@ export default class MaintenanceGuard extends ForceRedirector {
 			process.env.IS_IN_MAINTENANCE
 			&& process.env.IS_IN_MAINTENANCE !== "false"
 			&& !(
-				request.url === "/"
+				request.url === HOME
 				&& request.method === "GET"
 			)
 		) {
 			await super.intermediate(request, response, next)
+		} else {
+			next()
 		}
 	}
 }
