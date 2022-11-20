161c4889fad4d48da6cbd6dc2f371cf6f02190b8
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-11-20 14:56:43 +0800

    fix(consultation): retrieve the consultation even if archived

--- routes/enhancer/consultation/read(id).get.ts
+++ routes/enhancer/consultation/read(id).get.ts
@@ -80,7 +80,7 @@ export default class extends PageMiddleware {
 		const consultations = await manager.list({
 			"filter": {
 				"consultationScheduleRange": "*",
-				"existence": "exists",
+				"existence": "*",
 				"user": Number(user.data.id)
 			},
 			"page": {
@@ -89,7 +89,13 @@ export default class extends PageMiddleware {
 			},
 			"sort": [ "-updatedAt" ]
 		}) as ConsultationListDocument
-		const consultation = await manager.findWithID(Number(id)) as ConsultationDocument
+		const consultation = await manager.findWithID(Number(id), {
+			"constraints": {
+				"filter": {
+					"existence": "*"
+				}
+			}
+		}) as ConsultationDocument
 
 		const consultationIDs = consultations.data.map(
 			consultationResource => Number(consultationResource.id)
