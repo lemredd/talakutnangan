64158a7fe24bbe7311907456f908587b663b9754
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-11-25 12:32:33 +0800

    rfmt(consultation): put chat messages below

--- pages/consultation/form.page.vue
+++ pages/consultation/form.page.vue
@@ -78,29 +78,6 @@
 			</div>
 		</div>
 	</section>
-	<ul class="chat-messages mt-15">
-		<h1>Chat Messages</h1>
-		<li
-			v-for="chatMessage in chatMessages.data"
-			:key="chatMessage.id">
-			<span class="date-and-owner-details">
-				[
-				{{ formatToCompleteFriendlyTime(chatMessage.createdAt) }}
-				]
-				({{ chatMessage.user.data.name }})
-			</span>
-			<span v-if="isMessageKindStatus(chatMessage)" class="status-message">
-				{{ chatMessage.data.value }}
-			</span>
-			<span v-if="isMessageKindText(chatMessage)" class="text-message">
-				: {{ chatMessage.data.value }}
-			</span>
-			<span v-if="isMessageKindFile(chatMessage)" class="file-message">
-				sent an <a :href="chatMessage.attachedChatFile.data.fileContents">attachment</a>
-			</span>
-		</li>
-	</ul>
-
 	<section
 		v-if="consultation.data.finishedAt"
 		class="signatures mt-15">
@@ -127,15 +104,62 @@
 			Signatures will show here once the consultation has been finished.
 		</span>
 	</section>
+
+	<ul class="chat-messages mt-15">
+		<h1>Chat Messages</h1>
+		<li
+			v-for="chatMessage in chatMessages.data"
+			:key="chatMessage.id">
+			<span class="date-and-owner-details">
+				[
+				{{ formatToCompleteFriendlyTime(chatMessage.createdAt) }}
+				]
+				({{ chatMessage.user.data.name }})
+			</span>
+			<span v-if="isMessageKindStatus(chatMessage)" class="status-message">
+				{{ chatMessage.data.value }}
+			</span>
+			<span v-if="isMessageKindText(chatMessage)" class="text-message">
+				: {{ chatMessage.data.value }}
+			</span>
+			<span v-if="isMessageKindFile(chatMessage)" class="file-message">
+				sent an <a :href="chatMessage.attachedChatFile.data.fileContents">attachment</a>
+			</span>
+		</li>
+	</ul>
 </template>
 
-<style>
+<style lang="scss">
+	@media print {
+		.print-btn {
+			display: none;
+		}
+		h1 {
+			@apply text-2xl;
+		}
+
+		h2 {
+			@apply text-lg;
+		}
+
+		h6 {
+			border-bottom: 1px solid black;
+			@apply border-b mb-5;
+		}
+
+		.signatures {
+			clear: both;
+			break-after: always;
+		}
+		.file-message a { text-decoration: underline; }
+	}
+
 	body, #app {
 		min-height: 100vh;
 	}
 
 	#app {
-		@apply py-8 px-8
+		@apply py-8 px-8;
 	}
 </style>
 
@@ -167,22 +191,6 @@
 	}
 
 	.file-message a { text-decoration: underline; }
-	@media print {
-		h1 {
-			@apply text-2xl;
-		}
-
-		h2 {
-			@apply text-lg;
-		}
-
-		h6 {
-			border-bottom: 1px solid black;
-			@apply border-b mb-5;
-		}
-
-		.file-message a { text-decoration: underline; }
-	}
 </style>
 
 <script setup lang="ts">
