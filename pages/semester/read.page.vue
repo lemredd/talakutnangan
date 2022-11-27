e11844217c6c084bbe1aa1058fe26383242d0d6d
Angelo Magtoto <19201.magtoto.angelo.l@gmail.com>
2022-11-27 23:04:45 +0800

    intrn(semester): remove password confirmation in semester update

--- pages/semester/read.page.vue
+++ pages/semester/read.page.vue
@@ -5,7 +5,7 @@
 	<ReceivedSuccessMessages
 		v-if="successMessages.length"
 		:received-success-messages="successMessages"/>
-	<form @submit.prevent="openConfirmation">
+	<form @submit.prevent="updateSemester">
 		<TextualField
 			v-model="semester.data.name"
 			class="name field"
@@ -45,12 +45,6 @@
 				Archive
 			</button>
 		</div>
-
-		<ConfirmationPassword
-			v-model="password"
-			:must-confirm="isBeingConfirmed"
-			@cancel="closeConfirmation"
-			@confirm="updateSemester"/>
 	</form>
 </template>
 
@@ -75,7 +69,6 @@ import type { SemesterManagementInfo } from "$@/types/independent"
 import type { DeserializedSemesterDocument } from "$/types/documents/semester"
 
 import Fetcher from "$@/fetchers/semester"
-import makeSwitch from "$@/helpers/make_switch"
 import type { OptionInfo } from "$@/types/component"
 import makeManagementInfo from "@/semester/make_management_info"
 import RequestEnvironment from "$/singletons/request_environment"
@@ -88,7 +81,6 @@ import Selectable from "@/fields/selectable_options.vue"
 import ListRedirector from "@/helpers/list_redirector.vue"
 import TextualField from "@/fields/non-sensitive_text_capital.vue"
 import ReceivedErrors from "@/helpers/message_handlers/received_errors.vue"
-import ConfirmationPassword from "@/authentication/confirmation_password.vue"
 import ReceivedSuccessMessages from "@/helpers/message_handlers/received_success_messages.vue"
 
 import makeOptionInfo from "$@/helpers/make_option_info"
@@ -140,12 +132,6 @@ const successMessages = ref<string[]>([])
 
 const fetcher = new Fetcher()
 
-const {
-	"state": isBeingConfirmed,
-	"on": openConfirmation,
-	"off": closeConfirmation
-} = makeSwitch(false)
-
 const hasSubmittedSemester = ref<boolean>(true)
 
 async function updateSemester() {
@@ -164,9 +150,6 @@ async function updateSemester() {
 		}
 	})
 	.then(() => {
-		closeConfirmation()
-		password.value = ""
-
 		fillSuccessMessages(receivedErrors, successMessages)
 	})
 	.catch(responseWithErrors => extractAllErrorDetails(responseWithErrors, receivedErrors))
