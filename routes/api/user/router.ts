bd063a680c12cf1453998c2c203a8af5cbe880d4
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-12-07 13:29:33 +0800

    feat(server): register route to get a random image

--- routes/api/user/router.ts
+++ routes/api/user/router.ts
@@ -1,17 +1,18 @@
 import GetList from "!%/api/user/list.get"
 import GetRead from "!%/api/user/read(id).get"
 import PostLogIn from "!%/api/user/log_in.post"
+import GetLogIn from "!%/api/user/log_in.get"
 import PostImport from "!%/api/user/import.post"
 import PostLogOut from "!%/api/user/log_out.post"
 import PatchRestore from "!%/api/user/restore.patch"
 import ArchiveDelete from "!%/api/user/archive.delete"
 import PatchUpdate from "!%/api/user/update(id).patch"
-import DeleteArchive from "!%/api/user/archive.delete"
 import PatchResetPassword from "!%/api/user/reset_password(id).patch"
 
 export const controllers = [
 	GetList,
 	GetRead,
+	GetLogIn,
 	PostLogIn,
 	PostImport,
 	PostLogOut,
