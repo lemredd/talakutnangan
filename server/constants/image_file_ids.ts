1abb69bdc0ceae86d79f6b565a152844038f3fd8
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-12-07 13:13:08 +0800

    intrn(server): make route to get a random image

--- server/constants/image_file_ids.ts
+++ server/constants/image_file_ids.ts
@@ -1,7 +1,7 @@
-export const imageFileIDs = [
+export const IMAGE_FILE_IDS = [
 	"1WyDRslnxSNg0ECs45Idbq54lflXz74m-",
 	"1q4EaAZurwpy-tvMqe9NtBD-H1JSRA2w1",
 	"1vnD0ESPrw5meIlyjPJ3JDIILkqY934DH"
 ]
 
-export const driveLink = "https://drive.google.com/uc?id=:id"
+export const DRIVE_LINK = "https://drive.google.com/uc?id=:id"
