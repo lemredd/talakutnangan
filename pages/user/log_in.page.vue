7f36cb8492a3e8d8fa95a3092193ef0019656676
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-12-07 14:09:53 +0800

    intrn(server): make the URL more distinguishable

--- pages/user/log_in.page.vue
+++ pages/user/log_in.page.vue
@@ -23,7 +23,7 @@ body {
 			position: fixed;
 			inset: 0;
 
-			background-image: url("/api/user/log_in");
+			background-image: url("/api/user/log_in/image");
 			background-repeat: no-repeat;
 			background-size: cover;
 		}
