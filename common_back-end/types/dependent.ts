ac32a9ce0ba7edd8963653767b97cb8efbd4aee7
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-12-09 13:47:48 +0800

    types(back-end): remove asynchronous operation types

--- common_back-end/types/dependent.ts
+++ common_back-end/types/dependent.ts
@@ -1,6 +1,4 @@
 import { Transaction } from "sequelize"
-import type { Serializable } from "$/types/general"
-import type { AsynchronousLikeAttributes } from "$/types/documents/asynchronous-like"
 
 import CacheClient from "$!/helpers/cache_client"
 
@@ -13,22 +11,9 @@ export interface TransactionManagerInterface {
 	lockedTransactionObject: LockedTransactionObject
 }
 
-export interface AsynchronousOperationInterface extends TransactionManagerInterface {
-	regenerateDocument: () => Promise<Serializable>
-	incrementProgress: (attributes: Partial<AsynchronousLikeAttributes>) => Promise<void>
-	finish: (attributes: Partial<AsynchronousLikeAttributes>) => Promise<void>
-}
-
 export interface SharedManagerState<
 	T extends TransactionManagerInterface = TransactionManagerInterface
 > {
 	cache: CacheClient,
 	transaction?: T
-}
-
-export interface SharedAsynchronousOperationState<
-	T extends TransactionManagerInterface = TransactionManagerInterface,
-	U extends AsynchronousOperationInterface = AsynchronousOperationInterface
-> extends SharedManagerState<T> {
-	asynchronousOperation: U
 }
