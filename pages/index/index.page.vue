cc50ef4cd6e6017cd72c75831315c5040336707b
Kenneth Trecy Tobias <19201.tobias.kennethtrecy.c@gmail.com>
2022-12-09 17:50:38 +0800

    feat(home): show the posts

--- pages/index/index.page.vue
+++ pages/index/index.page.vue
@@ -1,28 +1,65 @@
 <template>
 	<MaintenanceMessage v-if="isInMaintenanceMode"/>
 	<div v-else>
-		<Opening/>
+		<div v-if="hasProfile" class="post-preview">
+			<Viewer
+				v-for="(post, i) in posts.data"
+				:key="post.id"
+				v-model="posts.data[i]"
+				:comment-count="posts.data[i].meta?.commentCount || 0"
+				class="viewer"
+				@archive="archivePost"
+				@restore="restorePost"/>
+		</div>
+		<Opening v-else/>
 		<Instructions/>
 		<ChangePassword v-if="hasDefaultPassword"/>
 	</div>
 </template>
 
+<style lang="scss" scoped>
+	.post-preview {
+		@apply flex flex-col;
+
+		.viewer {
+			@apply flex-1 my-4;
+		}
+	}
+</style>
+
 <script lang="ts" setup>
-import { inject } from "vue"
+import { inject, ref } from "vue"
 
 import type { PageContext } from "$/types/renderer"
+import type {
+	DeserializedPostResource,
+	DeserializedPostListDocument
+} from "$/types/documents/post"
 
 import isUndefined from "$/type_guards/is_undefined"
 
+import Viewer from "@/post/multiviewer/viewer.vue"
 import Opening from "@/guest_homepage/opening.vue"
 import Instructions from "@/guest_homepage/instructions.vue"
 import ChangePassword from "@/authentication/change_password.vue"
 import MaintenanceMessage from "@/helpers/maintenance_message.vue"
 
-const { pageProps } = inject("pageContext") as PageContext<"deserialized">
+type RequiredExtraProps = "posts"
+const { pageProps } = inject("pageContext") as PageContext<"deserialized", RequiredExtraProps>
 const { userProfile, isInMaintenanceMode } = pageProps
 
-const hasDefaultPassword = userProfile !== null
+const hasProfile = Boolean(userProfile)
+const posts = ref<DeserializedPostListDocument>(pageProps.posts)
+
+const hasDefaultPassword = hasProfile
 	&& !isUndefined(userProfile.meta.hasDefaultPassword)
 	&& userProfile.meta.hasDefaultPassword
+
+function archivePost(postToRemove: DeserializedPostResource) {
+	// To be done
+}
+
+function restorePost(postToRemove: DeserializedPostResource) {
+	// To be done
+}
 </script>
