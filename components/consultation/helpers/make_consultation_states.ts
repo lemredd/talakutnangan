27574b25757d019708798f19d7c9132d43c36908
Jarlem Red de Peralta <lmoa.jhdp@gmail.com>
2022-12-10 02:00:02 +0800

    intrn(consultation): make urgent state

--- components/consultation/helpers/make_consultation_states.ts
+++ components/consultation/helpers/make_consultation_states.ts
@@ -41,6 +41,8 @@ export default function(
 		const hasTerminated = isAfterScheduledStart.value && !hasDeleted.value
 		return hasTerminated && props.consultation.actionTaken === null
 	})
+	const isUrgent = computed(
+		() => props.consultation.reason.toLocaleLowerCase().includes("urgent:"))
 
 	setInterval(() => {
 		currentTime.value = new Date()
@@ -51,6 +53,7 @@ export default function(
 		isCanceled,
 		isDone,
 		isOngoing,
+		isUrgent,
 		willSoonStart,
 		willStart
 	}
